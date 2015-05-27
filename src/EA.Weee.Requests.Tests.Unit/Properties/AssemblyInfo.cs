20d886c7f3d097ffbc9b8f949e1868a60ffb3a62
Chris Smith <chriss@sfwltd.co.uk>
2015-05-27 17:00:10 +0100

    Added test projects from IWS

--- src/EA.Weee.Requests.Tests.Unit/Properties/AssemblyInfo.cs
+++ src/EA.Weee.Requests.Tests.Unit/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EA.Weee.RequestHandlers")]
+
+[assembly: AssemblyTitle("EA.Weee.Requests.Tests.Unit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EA.Weee.RequestHandlers")]
+[assembly: AssemblyProduct("EA.Weee.Requests.Tests.Unit")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,10 +17,12 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("800e3959-d4ba-43cd-b681-8cd835911b54")]
+
+[assembly: Guid("0ec37039-eef6-4227-a5ab-6e1550ea09b4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,6 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("EA.Weee.Requests.Tests.Unit")]
---
+++ src/EA.Weee.Web.Tests.Unit/TestHelpers/HttpContextMocker.cs
@@ -0,0 +1,86 @@
+﻿namespace EA.Weee.Web.Tests.Unit.TestHelpers
+{
+    using System.Collections.Generic;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using FakeItEasy;
+
+    internal class HttpContextMocker
+    {
+        public HttpContextBase HttpContextBase { get; set; }
+        public HttpRequestBase HttpRequestBase { get; set; }
+        public HttpResponseBase HttpResponseBase { get; set; }
+        public HttpSessionStateBase HttpSessionStateBase { get; set; }
+
+        public RouteData RouteData { get; set; }
+
+        public HttpContextMocker()
+        {
+            HttpContextBase = A.Fake<HttpContextBase>();
+        }
+
+        public virtual void AttachToController(Controller controller)
+        {
+            UseControllerDefaultsForMocksIfNull(controller);
+
+            A.CallTo(() => HttpContextBase.Request).Returns(HttpRequestBase);
+            A.CallTo(() => HttpContextBase.Response).Returns(HttpResponseBase);
+            A.CallTo(() => HttpContextBase.Session).Returns(HttpSessionStateBase);
+
+            RequestContext rc = new RequestContext(HttpContextBase, RouteData);
+
+            controller.ControllerContext = new ControllerContext(rc, controller);
+        }
+
+        protected virtual void UseControllerDefaultsForMocksIfNull(Controller controller)
+        {
+            if (HttpRequestBase == null)
+            {
+                HttpRequestBase = controller.Request;
+            }
+
+            if (HttpResponseBase == null)
+            {
+                HttpResponseBase = controller.Response;
+            }
+
+            if (HttpSessionStateBase == null)
+            {
+                if (controller.Session == null)
+                {
+                    HttpSessionStateBase sessionStateBase = A.Fake<HttpSessionStateBase>();
+
+                    A.CallTo(() => sessionStateBase[A<string>.Ignored]).Returns(null);
+
+                    HttpSessionStateBase = sessionStateBase;
+                }
+                else
+                {
+                    HttpSessionStateBase = controller.Session;
+                }
+            }
+
+            if (RouteData == null)
+            {
+                RouteData = controller.RouteData ?? new RouteData();
+            }
+        }
+
+        public virtual void AddMockSession(Dictionary<string, object> sessionObjects)
+        {
+            var mockSession = A.Fake<HttpSessionStateBase>();
+
+            foreach (var sessionObject in sessionObjects)
+            {
+                // Required for proper treatment in the closure.
+                var key = sessionObject.Key;
+                var value = sessionObject.Value;
+
+                A.CallTo(() => mockSession[key]).Returns(value);
+            }
+
+            HttpSessionStateBase = mockSession;
+        }
+    }
+}
