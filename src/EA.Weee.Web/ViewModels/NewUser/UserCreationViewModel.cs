01a20ce00e4f5b59c798eef50cb9b7c70cd006c6
rickpowell1311 <rickpowell1311@gmail.com>
2015-06-01 17:33:42 +0100

    Update user creation functionality for WEEE. UI tests not yet considered

--- src/EA.Weee.Web/ViewModels/NewUser/UserCreationViewModel.cs
+++ src/EA.Weee.Web/ViewModels/NewUser/UserCreationViewModel.cs
@@ -4,32 +4,32 @@
 
     public class UserCreationViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "First name is required.")]
         [Display(Name = "First name")]
         [StringLength(50)]
         [DataType(DataType.Text)]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Last name is required.")]
         [StringLength(50)]
         [DataType(DataType.Text)]
         [Display(Name = "Last name")]
         public string Surname { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Create password")]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Re-typing your password is required.")]
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Re-type password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
