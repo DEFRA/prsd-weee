f3d846e4ea75b69c356d2f94b8ee93ca09cc68c8
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-06-02 10:40:34 +0100

    added nuget and wct in tools

---
+++ tools/Scripts/update-machine-key.ps1
@@ -0,0 +1,37 @@
+﻿# Enable -Verbose option
+[CmdletBinding()]
+
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$configPath = $null,
+
+    [Parameter(Mandatory=$true)]
+    [string]$validationKey = $null,
+
+    [Parameter(Mandatory=$true)]
+    [string]$decryptionKey = $null,
+
+    [Parameter(Mandatory=$true)]
+    [string]$validationMethod = $null,
+
+    [Parameter(Mandatory=$true)]
+    [string]$decryptionMethod = $null
+)
+
+$doc = (gc $configPath) -as [xml]
+
+$systemWeb = $doc.SelectSingleNode("//system.web")
+$machineKey = $systemWeb.SelectSingleNode("machineKey")
+
+if (!$machineKey)
+{
+    $machineKey = $systemWeb.AppendChild($doc.CreateElement("machineKey"))
+}
+
+$machineKey.SetAttribute("validationKey", $validationKey)
+$machineKey.SetAttribute("decryptionKey", $decryptionKey)
+$machineKey.SetAttribute("validation", $validationMethod)
+$machineKey.SetAttribute("decryption", $decryptionMethod)
+
+$doc.Save($configPath)
