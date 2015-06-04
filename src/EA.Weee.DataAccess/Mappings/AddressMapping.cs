6a1d260b756e18d01fd056ae3f8d13be78331227
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-06-04 14:22:18 +0100

    Merged the mapping classes

--- src/EA.Weee.DataAccess/Mappings/AddressMapping.cs
+++ src/EA.Weee.DataAccess/Mappings/AddressMapping.cs
@@ -3,10 +3,12 @@
     using System.Data.Entity.ModelConfiguration;
     using Domain;
 
-    internal class AddressMapping : ComplexTypeConfiguration<Address>
+    internal class AddressMapping : EntityTypeConfiguration<Address>
     {
         public AddressMapping()
         {
+            ToTable("Address", "Organisation");
+
             Property(x => x.Address1).HasColumnName("Address1").IsRequired();
             Property(x => x.Address2).HasColumnName("Address2");
             Property(x => x.TownOrCity).HasColumnName("TownOrCity").IsRequired();
