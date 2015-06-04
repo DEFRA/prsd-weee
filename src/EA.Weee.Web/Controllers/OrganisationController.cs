ac93ba0773ad455a09222517bfcd15205a9d449c
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-06-04 11:04:53 +0100

    Added Organisation contact details view.

---
+++ src/EA.Weee.Web/Controllers/OrganisationController.cs
@@ -0,0 +1,31 @@
+﻿namespace EA.Weee.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using EA.Weee.Web.ViewModels.Organisation;
+
+    public class OrganisationController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult OrganisationContactDetails()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult OrganisationContactDetails(OrganisationContactDetailsViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {   
+                return RedirectToAction("OrganisationRegisteredOfficePrePopulate");
+            }
+            return View(viewModel);
+        }
+    }
+}
