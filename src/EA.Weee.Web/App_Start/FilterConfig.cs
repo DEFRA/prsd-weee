77a2c21bdf5ef2f13f08064a4d6a79ac8548f0e7
rickpowell1311 <rickpowell1311@gmail.com>
2015-06-04 11:23:21 +0100

    Presentation layer for 'Specify which type of organisation I work for' story.
    Still need to do:
       - Validation of Registered Company CRN
       - Save all data

--- src/EA.Weee.Web/App_Start/FilterConfig.cs
+++ src/EA.Weee.Web/App_Start/FilterConfig.cs
@@ -9,7 +9,7 @@
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleApiErrorAttribute());
-            filters.Add(new OrganisationRequiredAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
---
+++ src/EA.Weee.Web/Controllers/OrganisationRegistrationController.cs
@@ -0,0 +1,99 @@
+﻿namespace EA.Weee.Web.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using Prsd.Core.Extensions;
+    using ViewModels.Organisation.Type;
+    using ViewModels.OrganisationRegistration.Details;
+
+    [Authorize]
+    public class OrganisationRegistrationController : Controller
+    {
+        [HttpGet]
+        public ActionResult Type()
+        {
+            return View(new OrganisationTypeViewModel());
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Type(OrganisationTypeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var organisationType =
+                    model.OrganisationTypes.SelectedValue.GetValueFromDisplayName<OrganisationTypeEnum>();
+
+                switch (organisationType)
+                {
+                    case OrganisationTypeEnum.SoleTrader:
+                        return RedirectToAction("SoleTraderDetails");
+                    case OrganisationTypeEnum.RegisteredCompany:
+                        return RedirectToAction("RegisteredCompanyDetails");
+                    case OrganisationTypeEnum.Partnership:
+                        return RedirectToAction("PartnershipDetails");
+                }
+            }
+            
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult SoleTraderDetails()
+        {
+            return View(new SoleTraderDetailsViewModel());
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult SoleTraderDetails(SoleTraderDetailsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Save details 
+                return RedirectToAction("Search", "OrganisationRegistration"); // TODO: Change this to correct address
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult PartnershipDetails()
+        {
+            return View(new PartnershipDetailsViewModel());
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult PartnershipDetails(PartnershipDetailsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Save details 
+                return RedirectToAction("Search", "OrganisationRegistration"); // TODO: Change this to correct address
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult RegisteredCompanyDetails()
+        {
+            return View(new RegisteredCompanyDetailsViewModel());
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult RegisteredCompanyDetails(RegisteredCompanyDetailsViewModel model)
+        {
+            // TODO: Validate company registration number
+            if (ModelState.IsValid)
+            {
+                // TODO: Save details 
+                return RedirectToAction("Search", "OrganisationRegistration"); // TODO: Change this to correct address
+            }
+
+            return View(model);
+        }
+    }
+}
