b9a045f8f70e637e17c694b33cb2ae704a1528a4
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-06-04 13:40:12 +0100

    Modified contact person view model

--- src/EA.Weee.Requests/Organisations/ContactData.cs
+++ src/EA.Weee.Requests/Organisations/ContactData.cs
@@ -7,22 +7,10 @@
     {
         public Guid Id { get; set; }
     
-        [Required]
-        [StringLength(50)]
-        [Display(Name = "First name")]
-        [DataType(DataType.Text)]
         public string FirstName { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        [Display(Name = "Last name")]
-        [DataType(DataType.Text)]
         public string LastName { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        [Display(Name = "Position")]
-        [DataType(DataType.Text)]
         public string Position { get; set; }
     }
 }
