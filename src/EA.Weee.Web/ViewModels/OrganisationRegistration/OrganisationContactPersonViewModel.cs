dbb917d186f79011b16142a8eaa4f3af89ef2083
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-06-09 08:59:39 +0100

    Removed unsed usings

--- src/EA.Weee.Web/ViewModels/OrganisationRegistration/OrganisationContactPersonViewModel.cs
+++ src/EA.Weee.Web/ViewModels/OrganisationRegistration/OrganisationContactPersonViewModel.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.ComponentModel.DataAnnotations;
-    using System.IdentityModel.Metadata;
     using Requests.Organisations;
 
     public class OrganisationContactPersonViewModel
