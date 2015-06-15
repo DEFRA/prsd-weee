b7ec6e354277489776e2ee1ee1b05f02c2962eed
Owen Hall <owenh@sfwltd.co.uk>
2015-06-15 16:18:42 +0100

    Adding slightly weird paging to the handler.

--- src/EA.Weee.Requests/Organisations/FindMatchingOrganisations.cs
+++ src/EA.Weee.Requests/Organisations/FindMatchingOrganisations.cs
@@ -1,21 +1,35 @@
 ﻿namespace EA.Weee.Requests.Organisations
 {
-    using System;
     using System.Collections.Generic;
-    using Prsd.Core.Mediator;
+    using EA.Prsd.Core;
+    using EA.Prsd.Core.Mediator;
 
     public class FindMatchingOrganisations : IRequest<IList<OrganisationSearchData>>
     {
         public string CompanyName { get; private set; }
 
+        public bool Paged { get; private set; }
+
+        public int Page { get; private set; }
+
+        public int OrganisationsPerPage { get; set; }
+
         public FindMatchingOrganisations(string companyName)
         {
-            if (companyName == null)
-            {
-                throw new ArgumentNullException();
-            }
+            Guard.ArgumentNotNull(companyName);
 
             CompanyName = companyName;
+            Paged = false;
+        }
+
+        public FindMatchingOrganisations(string companyName, int page, int organisationsPerPage)
+        {
+            Guard.ArgumentNotNull(companyName);
+
+            CompanyName = companyName;
+            Paged = true;
+            Page = page;
+            OrganisationsPerPage = organisationsPerPage;
         }
     }
 }
