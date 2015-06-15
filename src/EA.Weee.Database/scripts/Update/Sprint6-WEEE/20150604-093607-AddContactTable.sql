6a006bf77efa53631a0bd45aa4f19369b23f851b
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-06-15 08:58:51 +0100

    Modification based on PR

--- src/EA.Weee.Database/scripts/Update/Sprint6-WEEE/20150604-093607-AddContactTable.sql
+++ src/EA.Weee.Database/scripts/Update/Sprint6-WEEE/20150604-093607-AddContactTable.sql
@@ -10,7 +10,6 @@ CREATE TABLE [Organisation].[Contact] (
     [FirstName] NVARCHAR (64)    NOT NULL,
     [LastName]  NVARCHAR (64)    NOT NULL,
     [Position]  NVARCHAR (128)   NOT NULL,
-	[RowVersion] ROWVERSION NOT NULL, 
     CONSTRAINT [PK_Contact_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
