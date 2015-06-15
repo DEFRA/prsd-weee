0d8e57258c06eb5544efb1b9c7ba5cf751d5f175
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-06-15 16:26:10 +0100

    Added dropdown for country

---
+++ src/EA.Weee.RequestHandlers/Mappings/UKCompetentAuthorityMap.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.RequestHandlers.Mappings
+{
+    using Domain;
+    using Prsd.Core.Mapper;
+    using Requests.Shared;
+
+    public class UKCompetentAuthorityMap : IMap<UKCompetentAuthority, UKCompetentAuthorityData>
+    {
+        public UKCompetentAuthorityData Map(UKCompetentAuthority source)
+        {
+            return new UKCompetentAuthorityData
+            {
+                Name = source.Name,
+                Abbreviation = source.Abbreviation,
+                Region = source.Region,
+                IsSystemUser = source.IsSystemUser,
+                Id = source.Id
+            };
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Infrastructure/ControllerExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Weee.Web.Infrastructure
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Api.Client;
+    using Weee.Requests.Shared;
+
+    public static class ControllerExtensions
+    {
+        public static async Task BindUKCompetentAuthorityRegionsList(this Controller controller,
+            Func<IWeeeClient> apiClient)
+        {
+            using (var client = apiClient())
+            {
+                await controller.BindUKCompetentAuthorityRegionsList(client);
+            }
+        }
+
+        public static async Task BindUKCompetentAuthorityRegionsList(this Controller controller, IWeeeClient apiClient)
+        {
+            var regions = await apiClient.SendAsync(new GetUKCompetentAuthorities());
+            controller.ViewBag.UKRegions = new SelectList(regions, "Id", "Region");
+        }
+    }
+}
