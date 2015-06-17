69d968d6df63f9afba7d205e9e177186c31c69a8
Rick Powell <richard.powell@sfwltd.co.uk>
2015-06-17 15:26:48 +0100

    #26016 - Loosen validation on phone number

---
+++ src/EA.Weee.Requests/Validation/GenericPhoneNumberAttribute.cs
@@ -0,0 +1,29 @@
+﻿namespace EA.Weee.Web.Validation
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.RegularExpressions;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class GenericPhoneNumberAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Checks that string is compromised only of digits
+        /// </summary>
+        /// <param name="value">The string to check</param>
+        /// <returns>A boolean confirming whether the string is a valid phone number</returns>
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                var phoneNumber = value.ToString().Trim()
+                    .Replace(" ", string.Empty)
+                    .Replace("-", string.Empty);
+
+                return new Regex("^[0-9]+$").IsMatch(phoneNumber);
+            }
+
+            return false;
+        }
+    }
+}
