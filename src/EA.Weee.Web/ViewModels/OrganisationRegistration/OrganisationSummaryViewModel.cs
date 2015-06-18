05c00136b777b7c524db6551767903c525a09017
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-06-18 15:31:41 +0530

    Stuff for get organisation details

---
+++ src/EA.Weee.Web/ViewModels/OrganisationRegistration/OrganisationSummaryViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Weee.Web.ViewModels.OrganisationRegistration
+{
+    using System;
+    using Weee.Requests.Organisations;
+
+    public class OrganisationSummaryViewModel
+    {
+        public OrganisationSummaryViewModel()
+        {
+            OrganisationData = new OrganisationData();
+        }
+
+        public Guid OrganisationId { get; set; }
+
+        public OrganisationData OrganisationData { get; set; }
+    }
+}
