f54b95d37755e0d31d9c065a513aac2996277573
Owen Hall <owenh@sfwltd.co.uk>
2015-06-19 17:07:11 +0100

    Ensuring that the organisation address is in the UK and pushing the user to the prepopulation view or straight to the data entry view. There might be a way to do this that doesn't involve hitting the database a second time, but it's Friday afternoon.

---
+++ src/EA.Weee.RequestHandlers/Organisations/IsUkOrganisationAddressHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace EA.Weee.RequestHandlers.Organisations
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.DataAccess;
+    using EA.Weee.Requests.Organisations;
+
+    internal class IsUkOrganisationAddressHandler : IRequestHandler<IsUkOrganisationAddress, bool>
+    {
+        private readonly WeeeContext context;
+
+        public IsUkOrganisationAddressHandler(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<bool> HandleAsync(IsUkOrganisationAddress message)
+        {
+            var organisation = context.Organisations.FirstOrDefault(o => o.Id == message.OrganisationId);
+
+            if (organisation == null)
+            {
+                throw new ArgumentException(string.Format("No organisation found with Id {0}", message.OrganisationId));
+            }
+
+            var organisationAddress = organisation.OrganisationAddress;
+            return Task.FromResult(organisationAddress.IsUkAddress);
+        }
+    }
+}
