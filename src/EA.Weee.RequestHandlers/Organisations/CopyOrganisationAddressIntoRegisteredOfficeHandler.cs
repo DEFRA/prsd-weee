4f82b28a42bccca998157aba1443452554f33c1c
Owen Hall <owenh@sfwltd.co.uk>
2015-06-19 11:43:20 +0100

    Setting up an option for business address to be a copy of organisation address. Will this mean that changes to organisation address in the future will affect business address, or will it make a copy? I'm guessing the former. May want to check on that.

---
+++ src/EA.Weee.RequestHandlers/Organisations/CopyOrganisationAddressIntoRegisteredOfficeHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace EA.Weee.RequestHandlers.Organisations
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.DataAccess;
+    using EA.Weee.Requests.Organisations;
+
+    internal class CopyOrganisationAddressIntoRegisteredOfficeHandler : IRequestHandler<CopyOrganisationAddressIntoRegisteredOffice, Guid>
+    {
+        private readonly WeeeContext context;
+
+        public CopyOrganisationAddressIntoRegisteredOfficeHandler(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<Guid> HandleAsync(CopyOrganisationAddressIntoRegisteredOffice message)
+        {
+            var organisation = context.Organisations.FirstOrDefault(o => o.Id == message.OrganisationId);
+
+            if (organisation == null)
+            {
+                throw new ArgumentException(string.Format("Could not find an organisation with Id {0}", message.OrganisationId));
+            }
+
+            organisation.BusinessAddressIsSameAsOrganisationAddress();
+
+            context.SaveChangesAsync();
+
+            return Task.FromResult(organisation.Id);
+        }
+    }
+}
---
+++ src/EA.Weee.Web/ViewModels/OrganisationRegistration/AddressPrepopulateViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Weee.Web.ViewModels.OrganisationRegistration
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using EA.Weee.Requests.Organisations;
+    using EA.Weee.Web.ViewModels.Shared;
+
+    public class AddressPrepopulateViewModel
+    {
+        [Required]
+        public RadioButtonStringCollectionViewModel ContactDetailsSameAs { get; set; }
+
+        public Guid OrganisationId { get; set; }
+
+        public OrganisationType OrganisationType { get; set; }
+    }
+}
