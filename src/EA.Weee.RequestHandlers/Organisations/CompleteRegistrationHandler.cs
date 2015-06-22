91caafbb1718c90b1127e1d3dd04b2e3513a7dd3
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-06-22 13:03:10 +0530

    Updated namspaces order

--- src/EA.Weee.RequestHandlers/Organisations/CompleteRegistrationHandler.cs
+++ src/EA.Weee.RequestHandlers/Organisations/CompleteRegistrationHandler.cs
@@ -4,11 +4,11 @@
     using System.Data.Entity;
     using System.Threading.Tasks;
     using DataAccess;
-    using Prsd.Core.Mediator;
-    using Requests.Organisations;
     using EA.Prsd.Core.Domain;
     using EA.Weee.Domain;
-
+    using Prsd.Core.Mediator;
+    using Requests.Organisations;
+    
     internal class CompleteRegistrationHandler : IRequestHandler<CompleteRegistration, Guid>
     {
         private readonly WeeeContext db;
