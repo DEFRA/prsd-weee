0ec51250f1acb6c141f665dabc8b43c67923db43
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-06-23 09:03:59 +0100

    minor text changes

--- src/EA.Weee.Web/Controllers/OrganisationRegistrationController.cs
+++ src/EA.Weee.Web/Controllers/OrganisationRegistrationController.cs
@@ -5,20 +5,20 @@
     using System.ComponentModel;
     using System.Threading.Tasks;
     using System.Web.Mvc;
-    using EA.Prsd.Core.Extensions;
-    using EA.Prsd.Core.Web.ApiClient;
-    using EA.Prsd.Core.Web.Mvc.Extensions;
-    using EA.Weee.Api.Client;
-    using EA.Weee.Requests.Organisations;
-    using EA.Weee.Requests.Organisations.Create;
-    using EA.Weee.Requests.Organisations.Create.Base;
-    using EA.Weee.Requests.Shared;
-    using EA.Weee.Web.Infrastructure;
-    using EA.Weee.Web.ViewModels.JoinOrganisation;
-    using EA.Weee.Web.ViewModels.OrganisationRegistration;
-    using EA.Weee.Web.ViewModels.OrganisationRegistration.Details;
-    using EA.Weee.Web.ViewModels.OrganisationRegistration.Type;
-    using EA.Weee.Web.ViewModels.Shared;
+    using Api.Client;
+    using Infrastructure;
+    using Prsd.Core.Extensions;
+    using Prsd.Core.Web.ApiClient;
+    using Prsd.Core.Web.Mvc.Extensions;
+    using ViewModels.JoinOrganisation;
+    using ViewModels.OrganisationRegistration;
+    using ViewModels.OrganisationRegistration.Details;
+    using ViewModels.OrganisationRegistration.Type;
+    using ViewModels.Shared;
+    using Weee.Requests.Organisations;
+    using Weee.Requests.Organisations.Create;
+    using Weee.Requests.Organisations.Create.Base;
+    using Weee.Requests.Shared;
 
     [Authorize]
     public class OrganisationRegistrationController : Controller
@@ -137,7 +137,7 @@
                 routeValues);
             var fallbackSelectOrganisationViewModel = BuildSelectOrganisationViewModel(name, tradingName,
                 companiesRegistrationNumber, type,
-                                new List<OrganisationSearchData>(), fallbackPagingViewModel);
+                new List<OrganisationSearchData>(), fallbackPagingViewModel);
 
             if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(tradingName))
             {
@@ -160,10 +160,10 @@
                     var pagingViewModel =
                         PagingViewModel.FromValues(organisationSearchResultData.TotalMatchingOrganisations,
                             OrganisationsPerPage,
-                        page, "SelectOrganisation", "OrganisationRegistration", routeValues);
+                            page, "SelectOrganisation", "OrganisationRegistration", routeValues);
 
                     return View(BuildSelectOrganisationViewModel(name, tradingName, companiesRegistrationNumber, type,
-                                    organisationSearchResultData.Results, pagingViewModel));
+                        organisationSearchResultData.Results, pagingViewModel));
                 }
                 catch (ApiBadRequestException ex)
                 {
@@ -211,7 +211,7 @@
                 {
                     throw new ArgumentException("No organisation found for supplied organisation Id", "organisationId");
                 }
-                
+
                 return View(new JoinOrganisationViewModel { OrganisationToJoin = organisationId });
             }
         }
@@ -500,7 +500,7 @@
 
         private async Task<AddressViewModel> GetAddressViewModel(Guid organisationId, IWeeeClient client)
         {
-            var organisation = await client.SendAsync(User.GetAccessToken(), new GetOrganisationInfo(organisationId)); // Check the organisation Id is valid
+            var organisation = await client.SendAsync(User.GetAccessToken(), new GetOrganisationInfo(organisationId));// Check the organisation Id is valid
             var model = new AddressViewModel
             {
                 OrganisationId = organisationId,
