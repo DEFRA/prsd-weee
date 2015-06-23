26d6302d21d578397e9dba6b08c7e2c7645b65fc
Chris Smith <chriss@sfwltd.co.uk>
2015-06-23 22:52:49 +0100

    Rolling back making Organisation.ToPending() private

--- src/EA.Weee.Domain/Organisation.cs
+++ src/EA.Weee.Domain/Organisation.cs
@@ -80,12 +80,12 @@
             return new Organisation(OrganisationType.RegisteredCompany, companyName, companyRegistrationNumber, tradingName);
         }
 
-        public void CompleteRegistration()
+        /*public void CompleteRegistration()
         {
             ToPending();
-        }
+        }*/
 
-        private void ToPending()
+        public void ToPending()
         {
             if (OrganisationStatus != OrganisationStatus.Incomplete)
             {
