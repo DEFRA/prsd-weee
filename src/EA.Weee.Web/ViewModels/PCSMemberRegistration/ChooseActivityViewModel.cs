47c99295a689f39e6d26c7dc2da0bf111a117281
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-06-26 11:39:51 +0530

    Implemented design of Choose Activity view

---
+++ src/EA.Weee.Web/ViewModels/PCSMemberRegistration/ChooseActivityViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace EA.Weee.Web.ViewModels.PCSMemberRegistration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using EA.Weee.Requests.Organisations;
+    using EA.Weee.Web.ViewModels.Shared;
+    
+    public class ChooseActivityViewModel
+    {
+        [Required]
+        public RadioButtonStringCollectionViewModel ActivityOptions { get; set; }
+
+        public Guid OrganisationId { get; set; }
+
+        public ChooseActivityViewModel()
+        {
+            List<string> collection = new List<string> { "Add / amend PCS members", "Manage organisation users" };
+            ActivityOptions = new RadioButtonStringCollectionViewModel(collection);
+        }
+    }
+}
