9004bdf9b3070f92890bd715ebf40f1db66a2cc5
Owen Hall <owenh@sfwltd.co.uk>
2015-06-30 15:35:00 +0100

    Storing errors/warnings in the database and regurgitating them into a test view.

--- src/EA.Weee.Domain/MemberUpload.cs
+++ src/EA.Weee.Domain/MemberUpload.cs
@@ -7,12 +7,22 @@
     {
         public string Data { get; private set; }
 
-        public List<string> Errors { get; private set; }
+        public virtual List<MemberUploadError> Errors { get; private set; }
 
-        public MemberUpload(string data, List<string> errors)
+        public MemberUpload(string data, List<MemberUploadError> errors)
         {
             Data = data;
             Errors = errors;
+        }
+
+        public MemberUpload(string data)
+        {
+            Data = data;
+            Errors = new List<MemberUploadError>();
+        }
+
+        protected MemberUpload()
+        {
         }
     }
 }
