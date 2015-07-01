91a0205fbcee2f714d3a330237dca763bbc837f4
Rick Powell <richard.powell@sfwltd.co.uk>
2015-07-01 11:02:20 +0100

    initial area creation

---
+++ src/EA.Weee.Web/Areas/PCS/PCSAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace EA.Weee.Web.Areas.PCS
+{
+    public class PCSAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "PCS";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "PCS_default",
+                "PCS/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
