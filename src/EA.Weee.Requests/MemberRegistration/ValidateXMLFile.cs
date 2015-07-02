8a5bdd8c2c9f0520889f341f133063a5c06d35af
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-07-02 18:55:55 +0530

    Added stuff for post manage members

---
+++ src/EA.Weee.Requests/MemberRegistration/ValidateXMLFile.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.Requests.MemberRegistration
+{
+    using Prsd.Core.Mediator;
+    using System;
+
+    public class ValidateXMLFile : IRequest<Guid>
+    {
+        public ValidateXMLFile(Guid organisationId, string data)
+        {
+        }   
+    }
+}
---
+++ src/EA.Weee.Web/Services/IFileConverter.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.Web.Services
+{
+    using System.Web;
+
+    public interface IFileConverter
+    {
+        string Convert(HttpPostedFileBase file);
+    }
+}
