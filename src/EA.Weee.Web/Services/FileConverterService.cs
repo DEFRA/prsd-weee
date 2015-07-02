4a26057d3e8d50016b3e5eccb6d123565c5e4f9e
Rick Powell <richard.powell@sfwltd.co.uk>
2015-07-02 15:34:21 +0100

    #27049 - implementation of post of XML file. Still needs a better file choose button and validation defining for file upload

---
+++ src/EA.Weee.Web/Services/FileConverterService.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Weee.Web.Services
+{
+    using System.IO;
+    using System.Text;
+    using System.Web;
+
+    public class FileConverterService : IFileConverterService
+    {
+        public string Convert(HttpPostedFileBase file)
+        {
+            var reader = new BinaryReader(file.InputStream);
+            var data = reader.ReadBytes((int)file.InputStream.Length);
+
+            return Encoding.UTF8.GetString(data);
+        }
+    }
+}
