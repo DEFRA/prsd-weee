c8ceaccd31bc90f2a334622ca965ee4178e6c5cc
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-07-03 16:59:24 +0530

    #27444 - Merged with Owen code and design view for Choose member registration file

--- src/EA.Weee.Web/Areas/PCS/Controllers/HomeController.cs
+++ src/EA.Weee.Web/Areas/PCS/Controllers/HomeController.cs
@@ -4,27 +4,20 @@
     using System.Threading.Tasks;
     using System.Web;
     using System.Web.Mvc;
-    using EA.Weee.Api.Client;
-    using EA.Weee.Requests.Organisations;
-    using EA.Weee.Web.Areas.PCS.ViewModels;
-    using EA.Weee.Web.Infrastructure;
     using Api.Client;
     using Infrastructure;
     using Services;
     using ViewModels;
-    using Weee.Requests.MemberRegistration;
     using Weee.Requests.Organisations;
 
     [Authorize]
     public class HomeController : Controller
     {
         private readonly Func<IWeeeClient> apiClient;
-        private readonly IFileConverterService fileConverter;
 
-        public HomeController(Func<IWeeeClient> apiClient, IFileConverterService fileConverter)
+        public HomeController(Func<IWeeeClient> apiClient)
         {
             this.apiClient = apiClient;
-            this.fileConverter = fileConverter;
         }
 
         [HttpGet]
@@ -54,7 +47,7 @@
             {
                 if (viewModel.ActivityOptions.SelectedValue == PcsAction.ManagePcsMembers)
                 {
-                    return RedirectToAction("ManageMembers", new { id = viewModel.OrganisationId });
+                    return RedirectToAction("AddOrAmendMembers", "MemberRegistration", new { id = viewModel.OrganisationId });
                 }
                 if (viewModel.ActivityOptions.SelectedValue == PcsAction.ManageOrganisationUsers)
                 {
@@ -63,33 +56,6 @@
             }
 
             return View(viewModel);
-        }
-
-        [HttpGet]
-        public async Task<ActionResult> ManageMembers(Guid id)
-        {
-            using (var client = apiClient())
-            {
-                var orgExists = await client.SendAsync(User.GetAccessToken(), new VerifyOrganisationExists(id));
-                if (orgExists)
-                {
-                    return View();
-                }
-            }
-
-            throw new InvalidOperationException(string.Format("'{0}' is not a valid organisation Id", id));
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> ManageMembers(Guid id, HttpPostedFileBase file)
-        {
-            var fileData = fileConverter.Convert(file);
-            using (var client = apiClient())
-            {
-                var validationId = await client.SendAsync(User.GetAccessToken(), new ValidateXmlFile(id, fileData));
-
-                return RedirectToAction("ViewErrorsAndWarnings", "Home", new { area = "PCS", memberUploadId = validationId });
-            }
         }
     }
 }
