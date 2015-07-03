a8ec46de2f128cc9afb0c6b3d180da25cc4f8be9
Owen Hall <owenh@sfwltd.co.uk>
2015-07-03 14:28:08 +0100

    Initial pass on using regex to create user-readable XML validation error messages. Changing v3-slightly-invalid.xml to represent the acceptance criteria.

--- src/EA.Weee.RequestHandlers/PCS/MemberRegistration/ValidateXmlFileHandler.cs
+++ src/EA.Weee.RequestHandlers/PCS/MemberRegistration/ValidateXmlFileHandler.cs
@@ -3,7 +3,9 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Reflection;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
     using System.Xml;
     using System.Xml.Linq;
@@ -30,7 +32,7 @@
 
             try
             {
-                var xmlDocument = XDocument.Parse(message.Data);
+                var xmlDocument = XDocument.Parse(message.Data, LoadOptions.SetLineInfo);
                 var schemas = new XmlSchemaSet();
                 var absoluteSchemaLocation = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase), SchemaLocation);
                 schemas.Add("http://www.environment-agency.gov.uk/WEEE/XMLSchema", absoluteSchemaLocation);
@@ -38,9 +40,13 @@
                 xmlDocument.Validate(
                     schemas,
                     (sender, args) =>
-                    {
-                        errors.Add(new MemberUploadError(ErrorLevel.Error, args.Exception.Message));
-                    },
+                        {
+                            var asXElement = sender as XElement;
+                            errors.Add(
+                                asXElement != null
+                                    ? new MemberUploadError(ErrorLevel.Error, MakeHumanReadableErrorMessage(asXElement, args))
+                                    : new MemberUploadError(ErrorLevel.Error, args.Exception.Message));
+                        },
                     false);
             }
             catch (XmlException ex)
@@ -55,6 +61,55 @@
             await context.SaveChangesAsync();
 
             return upload.Id;
+        }
+
+        private string MakeHumanReadableErrorMessage(XElement sender, ValidationEventArgs args)
+        {
+            const string GeneralConstraintFailurePattern =
+                @"^The '[^']*' element is invalid - The value '[^']*' is invalid according to its datatype '[^']*' - The ([^']*) constraint failed.$";
+
+            const string LengthConstraintFailurePattern =
+                @"^The '[^']*' element is invalid - The value '[^']*' is invalid according to its datatype '[^']*' - The actual length is (less|greater) than the (MinLength|MaxLength) value.$";
+
+            const string InvalidChildElementPattern =
+                @"^The element '([^']*)' in namespace '[^']*' has invalid child element '([^']*)' in namespace '[^']*'. List of possible elements expected: '[^']*' in namespace '[^']*'.$";
+
+            if (Regex.IsMatch(args.Exception.Message, GeneralConstraintFailurePattern))
+            {
+                var constraintWhichFailed = Regex.Match(args.Exception.Message, GeneralConstraintFailurePattern).Groups[1].ToString();
+
+                switch (constraintWhichFailed)
+                {
+                    case "MinInclusive":
+                        return string.Format("The value '{0}' supplied for type '{1}' on line {2} is too low.", sender.Value, sender.Name.LocalName, args.Exception.LineNumber);
+                    case "MaxInclusive":
+                        return string.Format("The value '{0}' supplied for type '{1}' on line {2} is too high.", sender.Value, sender.Name.LocalName, args.Exception.LineNumber);
+                    case "Pattern":
+                        return string.Format("The value '{0}' supplied for type '{1}' on line {2} doesn't match the required format.", sender.Value, sender.Name.LocalName, args.Exception.LineNumber);
+                    case "Enumeration":
+                        return string.Format("The value '{0}' supplied for type '{1}' on line {2} isn't one of the accepted values.", sender.Value, sender.Name.LocalName, args.Exception.LineNumber);
+                }
+            }
+
+            if (Regex.IsMatch(args.Exception.Message, LengthConstraintFailurePattern))
+            {
+                var lengthConstraintWhichFailed = Regex.Match(args.Exception.Message, LengthConstraintFailurePattern).Groups[2].ToString();
+
+                switch (lengthConstraintWhichFailed)
+                {
+                    case "MinLength":
+                        return string.Format("The value '{0}' supplied for type '{1}' on line {2} is too short.", sender.Value, sender.Name.LocalName, args.Exception.LineNumber);
+                    case "MaxLength":
+                        return string.Format("The value '{0}' supplied for type '{1}' on line {2} is too long.", sender.Value, sender.Name.LocalName, args.Exception.LineNumber);
+                }
+            }
+
+            if (Regex.IsMatch(args.Exception.Message, InvalidChildElementPattern))
+            {
+                return "Invalid child element oh no! " + args.Exception.Message;
+            }
+
+            return args.Exception.Message;
         }
     }
 }
