1d8b6e1b363c724440abb91cacb0f97b7a9e9960
Rick Powell <richard.powell@sfwltd.co.uk>
2015-07-06 16:33:32 +0100

    #27049 - Applying styling (for when JavaScript is enabled and disabled) and validation around file type

--- src/EA.Weee.Web.Tests.Unit/Areas/PCS/Controllers/HomeControllerTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/PCS/Controllers/HomeControllerTests.cs
@@ -9,6 +9,7 @@
     using Prsd.Core.Mediator;
     using Services;
     using Web.Areas.PCS.Controllers;
+    using Web.Areas.PCS.ViewModels;
     using Weee.Requests.MemberRegistration;
     using Weee.Requests.Organisations;
     using Xunit;
@@ -60,11 +61,22 @@
         }
 
         [Fact]
+        public async void PostManageMembers_ModelIsInvalid_ReturnsView()
+        {
+            var controller = HomeController();
+            controller.ModelState.AddModelError("ErrorKey", "Some kind of error goes here");
+
+            var result = await controller.ManageMembers(A<Guid>._, new ManageMembersViewModel());
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
         public async void PostManageMembers_ConvertsFileToString()
         {
             try
             {
-                await HomeController().ManageMembers(A<Guid>._, A<HttpPostedFileBase>._);
+                await HomeController().ManageMembers(A<Guid>._, new ManageMembersViewModel());
             }
             catch (Exception)
             {
@@ -89,7 +101,7 @@
 
             try
             {
-                await HomeController().ManageMembers(organisationId, A<HttpPostedFileBase>._);
+                await HomeController().ManageMembers(organisationId, new ManageMembersViewModel());
             }
             catch (Exception)
             {
@@ -111,7 +123,7 @@
             A.CallTo(() => weeeClient.SendAsync(A<string>._, A<ValidateXmlFile>._))
                 .Returns(validationId);
 
-            var result = await HomeController().ManageMembers(A<Guid>._, A<HttpPostedFileBase>._);
+            var result = await HomeController().ManageMembers(A<Guid>._, new ManageMembersViewModel());
             var redirect = (RedirectToRouteResult)result;
 
             Assert.Equal("PCS", redirect.RouteValues["area"]);
---
+++ src/EA.Weee.Web/Areas/PCS/ViewModels/ManageMembersViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Weee.Web.Areas.PCS.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+    using Validators;
+
+    public class ManageMembersViewModel
+    {
+        [Required]
+        [FileType("text/xml")]
+        public HttpPostedFileBase File { get; set; }
+    }
+}
