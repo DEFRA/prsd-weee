1cee6a15146d5ce3ae2157f3bd929ffc3f66baf0
Rick Powell <richard.powell@sfwltd.co.uk>
2015-07-07 17:12:31 +0100

    #27049 - updating nuget references and transferring validation to PRSD core

--- src/EA.Weee.Web/Areas/PCS/ViewModels/AddOrAmendMembersViewModel.cs
+++ src/EA.Weee.Web/Areas/PCS/ViewModels/AddOrAmendMembersViewModel.cs
@@ -2,7 +2,7 @@
 {
     using System.ComponentModel.DataAnnotations;
     using System.Web;
-    using Validators;
+    using Prsd.Core.Validation;
 
     public class AddOrAmendMembersViewModel
     {
