90dbebba511af3e49d2c4b6823a0c10fd7f7bf13
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-07-13 14:28:27 +0100

    Added property for list of producers in schema

--- src/EA.Weee.Domain/PCS/MemberUpload.cs
+++ src/EA.Weee.Domain/PCS/MemberUpload.cs
@@ -8,17 +8,20 @@
     public class MemberUpload : Entity
     {
         public Guid OrganisationId { get; private set; }
+
         public virtual Organisation Organisation { get; private set; }
+
+        public virtual List<MemberUploadError> Errors { get; private set; }
 
         public string Data { get; private set; }
 
         //public int ComplianceYear { get; private set; }
 
-        public virtual List<MemberUploadError> Errors { get; private set; }
-
         //public virtual Scheme Scheme { get; private set; }
 
         //public bool IsSubmitted { get; private set; }
+
+       // public virtual List<Producer> Producers { get; private set; }
 
         public MemberUpload(Guid organisationId, string data, List<MemberUploadError> errors)
         {
--- src/EA.Weee.Domain/PCS/Scheme.cs
+++ src/EA.Weee.Domain/PCS/Scheme.cs
@@ -2,12 +2,16 @@
 {
     using Prsd.Core.Domain;
     using System;
+    using System.Collections.Generic;
     using Organisation;
+    using Producer;
 
     public class Scheme : Entity
     {
         public virtual Organisation Organisation { get; private set; }
 
         public string ApprovalNumber { get; private set; }
+
+        public virtual List<Producer> Producers { get; private set; }
     }
 }
