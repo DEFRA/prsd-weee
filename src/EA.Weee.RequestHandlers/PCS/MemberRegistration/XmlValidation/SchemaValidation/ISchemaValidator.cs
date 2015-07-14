316845e2766405984f2cff1bca53f63656e89f1d
Rick Powell <richard.powell@sfwltd.co.uk>
2015-07-14 11:11:39 +0100

    #28073 - refactoring schema validation and adding business validation

---
+++ src/EA.Weee.RequestHandlers/PCS/MemberRegistration/XmlValidation/SchemaValidation/ISchemaValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.RequestHandlers.PCS.MemberRegistration.XmlValidation.SchemaValidation
+{
+    using System.Collections.Generic;
+    using System.Xml.Linq;
+    using Domain;
+    using Requests.PCS.MemberRegistration;
+
+    public interface ISchemaValidator
+    {
+        IEnumerable<MemberUploadError> Validate(ValidateXmlFile message);
+    }
+}
