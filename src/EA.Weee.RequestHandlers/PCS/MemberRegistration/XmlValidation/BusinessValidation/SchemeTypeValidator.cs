ca70373353036432f02f7c8ac21923aaf857d76c
Rick Powell <richard.powell@sfwltd.co.uk>
2015-07-15 17:08:46 +0100

    28597: PRN appears more than once in XML file

--- src/EA.Weee.RequestHandlers/PCS/MemberRegistration/XmlValidation/BusinessValidation/SchemeTypeValidator.cs
+++ src/EA.Weee.RequestHandlers/PCS/MemberRegistration/XmlValidation/BusinessValidation/SchemeTypeValidator.cs
@@ -1,6 +1,11 @@
 ﻿namespace EA.Weee.RequestHandlers.PCS.MemberRegistration.XmlValidation.BusinessValidation
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Domain;
     using FluentValidation;
+    using FluentValidation.Validators;
 
     public class SchemeTypeValidator : AbstractValidator<schemeType>
     {
@@ -8,6 +13,25 @@
         {
             RuleFor(st => st.producerList)
                 .SetCollectionValidator(new ProducerTypeValidator());
+
+            var duplicateRegistrationNumbers = new List<string>();
+            RuleForEach(st => st.producerList)
+                .Must((st, producer) =>
+                {
+                    var isDuplicate = st.producerList
+                        .Any(p => p != producer && p.registrationNo == producer.registrationNo);
+
+                    if (isDuplicate && !duplicateRegistrationNumbers.Contains(producer.registrationNo))
+                    {
+                        duplicateRegistrationNumbers.Add(producer.registrationNo);
+                        return false;
+                    }
+
+                    return true;
+                })
+                .WithState(st => ErrorLevel.Error)
+                .WithMessage("The Registration Number '{0}' appears more than once in the uploaded XML file",
+                    (st, producer) => producer.registrationNo);
         }
     }
 }
