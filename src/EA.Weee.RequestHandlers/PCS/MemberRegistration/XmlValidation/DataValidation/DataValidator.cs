7c99e7ae7009d3cbfe17d000a591ce9b69b4039c
Rick Powell <richard.powell@sfwltd.co.uk>
2015-07-16 10:59:38 +0100

    28598 - Refactor ready for validation against data

---
+++ src/EA.Weee.RequestHandlers/PCS/MemberRegistration/XmlValidation/DataValidation/DataValidator.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Weee.RequestHandlers.PCS.MemberRegistration.XmlValidation.DataValidation
+{
+    using System;
+    using System.Collections.Generic;
+    using DataAccess;
+    using Domain.PCS;
+
+    public class DataValidator : IDataValidator
+    {
+        private readonly WeeeContext context;
+
+        public DataValidator(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<MemberUploadError> Validate(schemeType deserializedXml)
+        {
+            // TODO: Add validation against existing data here:
+
+            return new List<MemberUploadError>();
+        }
+    }
+}
