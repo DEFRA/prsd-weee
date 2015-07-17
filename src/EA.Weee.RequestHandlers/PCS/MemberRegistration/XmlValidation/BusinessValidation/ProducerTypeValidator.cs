03e97d1e7021fa13fe532a67382a54aa6ace5893
Owen Hall <owenh@sfwltd.co.uk>
2015-07-17 15:07:22 +0100

    Adding logic (likely to be changed following meeting) around verifying that a producer's office is in the UK. Wrapping different sets of rules in named RuleSets. This is expressly to allow for more segregated unit testing, using RuleSetValidator. Per RuleSet change, adding RuleSetValidator to BusinessValidator execution to run all rules in actual use.

--- src/EA.Weee.RequestHandlers/PCS/MemberRegistration/XmlValidation/BusinessValidation/ProducerTypeValidator.cs
+++ src/EA.Weee.RequestHandlers/PCS/MemberRegistration/XmlValidation/BusinessValidation/ProducerTypeValidator.cs
@@ -1,27 +1,74 @@
 ﻿namespace EA.Weee.RequestHandlers.PCS.MemberRegistration.XmlValidation.BusinessValidation
 {
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
     using Domain;
     using FluentValidation;
 
     public class ProducerTypeValidator : AbstractValidator<producerType>
     {
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation", Justification = "It's the UK, silly.")]
+        private readonly IEnumerable<countryType> ukCountries = new List<countryType>
+        {
+            countryType.UKENGLAND, countryType.UKNORTHERNIRELAND, 
+            countryType.UKSCOTLAND, countryType.UKWALES
+        };
+
         public ProducerTypeValidator()
         {
-            RuleFor(pt => pt.registrationNo)
-                .NotEmpty()
-                .When(pt => pt.status == statusType.A)
-                .WithState(pt => ErrorLevel.Error)
-                .WithMessage(
-                    "The producer registration number for '{0}' has been left out of the xml file but the xml file is amending existing producer details. Check this producer's details. To amend this producer add the producer registration number or to add as a brand new producer use status \"I\" not \"A\".", 
-                    pt => pt.tradingName);
+            RuleSet(
+                "registrationNo",
+                () =>
+                    {
+                        RuleFor(pt => pt.registrationNo)
+                            .NotEmpty()
+                            .When(pt => pt.status == statusType.A)
+                            .WithState(pt => ErrorLevel.Error)
+                            .WithMessage(
+                                "The producer registration number for '{0}' has been left out of the xml file but the xml file is amending existing producer details. Check this producer's details. To amend this producer add the producer registration number or to add as a brand new producer use status \"I\" not \"A\".",
+                                pt => pt.tradingName);
 
-            RuleFor(pt => pt.registrationNo)
-                .Must((pt, registrationNo) => string.IsNullOrEmpty(registrationNo))
-                .When(pt => pt.status == statusType.I)
-                .WithState(pt => ErrorLevel.Error)
-                .WithMessage(
-                    "A producer registration number for: '{0}' has been entered in the xml file but you are trying to register this producer for the very first time. Check this producer's details. To add this as a new producer remove the producer registration number or to amend  an existing producer details use status \"A\" not \"I\".",
-                    pt => pt.tradingName);
+                        RuleFor(pt => pt.registrationNo)
+                            .Must((pt, registrationNo) => string.IsNullOrEmpty(registrationNo))
+                            .When(pt => pt.status == statusType.I)
+                            .WithState(pt => ErrorLevel.Error)
+                            .WithMessage(
+                                "A producer registration number for: '{0}' has been entered in the xml file but you are trying to register this producer for the very first time. Check this producer's details. To add this as a new producer remove the producer registration number or to amend  an existing producer details use status \"A\" not \"I\".",
+                                pt => pt.tradingName);
+                    });
+
+            RuleSet(
+                "authorisedRepresentative",
+                () =>
+                    {
+                        RuleFor(pt => pt.producerBusiness.Item).Must(
+                            (pt, producerBusinessItem) =>
+                                {
+                                    contactDetailsContainerType officeContactDetails;
+
+                                    if (producerBusinessItem.GetType() == typeof(companyType))
+                                    {
+                                        officeContactDetails = ((companyType)producerBusinessItem).registeredOffice;
+                                    }
+                                    else if (producerBusinessItem.GetType() == typeof(partnershipType))
+                                    {
+                                        officeContactDetails =
+                                            ((partnershipType)producerBusinessItem).principalPlaceOfBusiness;
+                                    }
+                                    else
+                                    {
+                                        return false; // TODO: throw exception instead? how will that affect validation?
+                                    }
+
+                                    // abusing law of demeter here, but schema requires all these fields to be present and correct
+                                    return ukCountries.Contains(officeContactDetails.contactDetails.address.country);
+                                })
+                            .WithState(pt => ErrorLevel.Error)
+                            .WithMessage(
+                                "{0} is an authorised representative but has a country in their address which is outside of the UK. An authorised representative must be based in the UK. In order to register or amend this producer please check they are an authorised representative and are based in the UK.",
+                                (pt, item) => pt.tradingName);
+                    });
         }
     }
 }
