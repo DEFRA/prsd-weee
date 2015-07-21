9b142f03cb9e09bc6b28f30ccfa5e670c3aceab6
Rick Powell <richard.powell@sfwltd.co.uk>
2015-07-21 14:46:43 +0100

    Initial plan for data migration from excel file

---
+++ src/EA.Weee.DataMigration/FileReader.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.DataMigration
+{
+    using System;
+    using LinqToExcel;
+
+    public class FileReader
+    {
+        private const string excelDirectory = ".\\DataFiles\\Excel\\";
+
+        public T ReadExcelFile<T>(string fileName)
+        {
+            var excel = new ExcelQueryFactory(excelDirectory + fileName);
+        }
+    }
+}
---
+++ src/EA.Weee.DataMigration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Weee.DataMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
