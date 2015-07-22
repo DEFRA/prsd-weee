3c7483f8566ac9e02da372091142a908cf6e1644
Owen Hall <owenh@sfwltd.co.uk>
2015-07-22 15:51:14 +0100

    Updating XSD schema to version 3.06. Adding PowerShell script to generate code version of XSD (requires xsd.exe to be on PATH).

---
+++ src/EA.Weee.RequestHandlers/GenerateSchemaCs.ps1
@@ -0,0 +1,5 @@
+# Note, this requires xsd.exe to be on your PATH
+# xsd.exe lives in: Program Files (x86)\Microsoft SDKs\Windows\{someVersion}\bin\NETFX 4.5.1 Tools
+# it comes with the Windows SDK
+
+xsd /c v3schema.xsd /n:EA.Weee.RequestHandlers
