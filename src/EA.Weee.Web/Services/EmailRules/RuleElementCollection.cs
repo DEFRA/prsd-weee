8027cfe51cb7613bef45dbe274ca984a8187d5aa
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-07-24 15:34:04 +0530

    Implemented email whitelist

---
+++ src/EA.Weee.Web/Services/EmailRules/RuleElementCollection.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Weee.Web.Services.EmailRules
+{
+    using System.Configuration;
+
+    public class RuleElementCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new RuleElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            RuleElement myElement = element as RuleElement;
+            if (myElement == null)
+            {
+                return null;
+            }
+
+            return myElement.Action.GetHashCode()
+                ^ myElement.Type.GetHashCode()
+                ^ myElement.Value.GetHashCode();
+        }
+    }
+}
