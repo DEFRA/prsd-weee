609c355df021033a6596040dee58b237d5e490b4
Rick Powell <richard.powell@sfwltd.co.uk>
2015-08-06 14:28:48 +0100

    30142 - View summary of PCS members screen: presentation layer

---
+++ src/EA.Weee.Core/Helpers/Extensions.cs
@@ -0,0 +1,27 @@
+﻿namespace EA.Weee.Core.Helpers
+{
+    using System;
+    using System.Globalization;
+
+    public static class Extensions
+    {
+        public static string ToStringWithXSignificantFigures(this double doub, int significantFigures)
+        {
+            if (doub == 0)
+            {
+                return "0.00";
+            }
+
+            double scale = Math.Pow(10, Math.Floor(Math.Log10(Math.Abs(doub))) + 1);
+            var value = (scale * Math.Round(doub / scale, significantFigures)).ToString(CultureInfo.InvariantCulture);
+
+            var appendedZeros = string.Empty;
+            for (var i = 0; i < significantFigures - value.Replace(".", string.Empty).Length; i++)
+            {
+                appendedZeros += "0";
+            }
+
+            return value + appendedZeros;
+        }
+    }
+}
---
+++ src/EA.Weee.Requests/PCS/MemberRegistration/GetLatestMemberUploadSummary.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Requests.PCS.MemberRegistration
+{
+    using System;
+    using Core.PCS;
+    using Prsd.Core.Mediator;
+
+    public class GetLatestMemberUploadSummary : IRequest<LatestMemberUploadSummary>
+    {
+        public Guid PcsId { get; private set; }
+
+        public GetLatestMemberUploadSummary(Guid pcsId)
+        {
+            PcsId = pcsId;
+        }
+    }
+}
