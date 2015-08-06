fa337141c4f297732f07349a684806185d0f92c8
Owen Hall <owenh@sfwltd.co.uk>
2015-08-06 17:12:12 +0100

    Adding presentation-side things.

--- src/EA.Weee.Core/PCS/PcsData.cs
+++ src/EA.Weee.Core/PCS/PcsData.cs
@@ -1,9 +1,11 @@
 ﻿namespace EA.Weee.Core.PCS
 {
+    using System;
     using Shared;
 
     public class PcsData
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public PcsStatus PcsStatus { get; set; }
     }
