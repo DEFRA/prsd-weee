b51475168ca2bc3f9456eb80941ff45f47f08db3
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-08-07 14:20:37 +0100

    New maintenance project and page

---
+++ src/EA.Weee.Web.Maintenance/ViewModels/MaintenanceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EA.Weee.Web.Maintenance.ViewModels
+{
+    public class MaintenanceViewModel
+    {
+    }
+}
