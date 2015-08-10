b8cdc5fca93bc54b7b6cc5e2341e9564ab27e1cd
Rick Powell <richard.powell@sfwltd.co.uk>
2015-08-10 11:29:15 +0100

    Update journey through manage pcs members story

--- src/EA.Weee.Web/Areas/Scheme/ViewModels/PcsAction.cs
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/PcsAction.cs
@@ -2,7 +2,7 @@
 {
     public static class PcsAction
     {
-        public static string ManagePcsMembers = "Add / amend PCS members";
+        public static string ManagePcsMembers = "Add, amend or view PCS members";
         public static string ManageOrganisationUsers = "Manage organisation users";
     }
 }
