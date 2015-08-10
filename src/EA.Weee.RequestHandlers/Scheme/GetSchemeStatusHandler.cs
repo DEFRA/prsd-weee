8c4b234c57544d260957a948edc7a600f0bf77e3
Rick Powell <richard.powell@sfwltd.co.uk>
2015-08-10 17:31:41 +0100

    #30143 - Pending authorization screen for PCS (presentation layer)

---
+++ src/EA.Weee.RequestHandlers/Scheme/GetSchemeStatusHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Weee.RequestHandlers.Scheme
+{
+    using System.Threading.Tasks;
+    using Core.Shared;
+    using DataAccess;
+    using Prsd.Core.Mediator;
+    using Requests.Scheme;
+
+    public class GetSchemeStatusHandler : IRequestHandler<GetSchemeStatus, SchemeStatus>
+    {
+        private readonly WeeeContext context;
+
+        public GetSchemeStatusHandler(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<SchemeStatus> HandleAsync(GetSchemeStatus message)
+        {
+            // TODO: Implement
+            return await Task.Run(() => SchemeStatus.Pending);
+        }
+    }
+}
