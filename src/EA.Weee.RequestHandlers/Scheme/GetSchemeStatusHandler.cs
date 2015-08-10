2b7726b2777cda424e5e4eb370daf29e9228d570
Rick Powell <richard.powell@sfwltd.co.uk>
2015-08-10 18:42:34 +0100

    Request handler and tests for getting scheme status

--- src/EA.Weee.RequestHandlers/Scheme/GetSchemeStatusHandler.cs
+++ src/EA.Weee.RequestHandlers/Scheme/GetSchemeStatusHandler.cs
@@ -1,24 +1,37 @@
 ﻿namespace EA.Weee.RequestHandlers.Scheme
 {
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
     using System.Threading.Tasks;
     using Core.Shared;
     using DataAccess;
+    using Prsd.Core.Mapper;
     using Prsd.Core.Mediator;
     using Requests.Scheme;
 
     public class GetSchemeStatusHandler : IRequestHandler<GetSchemeStatus, SchemeStatus>
     {
         private readonly WeeeContext context;
+        private readonly IMap<Domain.Scheme.SchemeStatus, SchemeStatus> mapper;
 
-        public GetSchemeStatusHandler(WeeeContext context)
+        public GetSchemeStatusHandler(WeeeContext context, IMap<Domain.Scheme.SchemeStatus, SchemeStatus> mapper)
         {
             this.context = context;
+            this.mapper = mapper;
         }
 
         public async Task<SchemeStatus> HandleAsync(GetSchemeStatus message)
         {
-            // TODO: Implement
-            return await Task.Run(() => SchemeStatus.Pending);
+            var scheme = await context.Schemes
+                .SingleOrDefaultAsync(s => s.OrganisationId == message.PcsId);
+
+            if (scheme != null)
+            {
+                return mapper.Map(scheme.SchemeStatus);
+            }
+
+            throw new InvalidOperationException(string.Format("Scheme with Id '{0}' does not exist", message.PcsId));
         }
     }
 }
