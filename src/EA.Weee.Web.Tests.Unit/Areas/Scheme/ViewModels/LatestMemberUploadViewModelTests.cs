79ff9327f4c9675f2993b8772a7538e63709ee54
Rick Powell <richard.powell@sfwltd.co.uk>
2015-08-11 10:50:50 +0100

    Fix for file size display problems

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/ViewModels/LatestMemberUploadViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Scheme.ViewModels
+{
+    using Web.Areas.PCS.ViewModels;
+    using Xunit;
+
+    public class LatestMemberUploadViewModelTests
+    {
+        [Theory]
+        [InlineData(99, "100", "bytes")]
+        [InlineData(100, "0.1", "kb")]
+        [InlineData(102399, "100", "kb")]
+        [InlineData(102400, "0.1", "Mb")]
+        [InlineData(104857599, "100", "Mb")]
+        [InlineData(104857600, "0.1", "Gb")]
+        public void Test1(double fileSizeInBytes, string expectedRoundedValue, string expectedSuffix)
+        {
+            var model = new LatestMemberUploadViewModel
+            {
+                CsvFileSizeEstimate = fileSizeInBytes
+            };
+
+            Assert.Equal(expectedSuffix, model.ScaledCsvFileSizeSuffix);
+            Assert.Equal(expectedRoundedValue, model.ScaledCsvFileSize);
+        }
+    }
+}
