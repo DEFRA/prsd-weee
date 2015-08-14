6701642a54fdf64662fd153eb9a20adcd83b4d92
Owen Hall <owenh@sfwltd.co.uk>
2015-08-14 09:58:48 +0100

    Moving OrganisationUser creation to start of organisation creation process so that claims might work.

--- src/EA.Weee.RequestHandlers/Organisations/Create/CreatePartnershipRequestHandler.cs
+++ src/EA.Weee.RequestHandlers/Organisations/Create/CreatePartnershipRequestHandler.cs
@@ -3,23 +3,33 @@
     using System;
     using System.Threading.Tasks;
     using DataAccess;
+    using Domain;
     using Domain.Organisation;
+    using Prsd.Core.Domain;
     using Prsd.Core.Mediator;
     using Requests.Organisations.Create;
 
     public class CreatePartnershipRequestHandler : IRequestHandler<CreatePartnershipRequest, Guid>
     {
         private readonly WeeeContext db;
+        private readonly IUserContext userContext;
 
-        public CreatePartnershipRequestHandler(WeeeContext db)
+        public CreatePartnershipRequestHandler(WeeeContext db, IUserContext userContext)
         {
             this.db = db;
+            this.userContext = userContext;
         }
 
         public async Task<Guid> HandleAsync(CreatePartnershipRequest message)
         {
             var organisation = Organisation.CreatePartnership(message.TradingName);
             db.Organisations.Add(organisation);
+
+            await db.SaveChangesAsync();
+
+            var organisationUser = new OrganisationUser(userContext.UserId, organisation.Id, UserStatus.Approved);
+            db.OrganisationUsers.Add(organisationUser);
+
             await db.SaveChangesAsync();
 
             return organisation.Id;
--- src/EA.Weee.RequestHandlers/Organisations/Create/CreateSoleTraderRequestHandler.cs
+++ src/EA.Weee.RequestHandlers/Organisations/Create/CreateSoleTraderRequestHandler.cs
@@ -3,23 +3,33 @@
     using System;
     using System.Threading.Tasks;
     using DataAccess;
+    using Domain;
     using Domain.Organisation;
+    using Prsd.Core.Domain;
     using Prsd.Core.Mediator;
     using Requests.Organisations.Create;
 
     public class CreateSoleTraderRequestHandler : IRequestHandler<CreateSoleTraderRequest, Guid>
     {
         private readonly WeeeContext db;
+        private readonly IUserContext userContext;
 
-        public CreateSoleTraderRequestHandler(WeeeContext db)
+        public CreateSoleTraderRequestHandler(WeeeContext db, IUserContext userContext)
         {
             this.db = db;
+            this.userContext = userContext;
         }
 
         public async Task<Guid> HandleAsync(CreateSoleTraderRequest message)
         {
             var organisation = Organisation.CreateSoleTrader(message.TradingName);
             db.Organisations.Add(organisation);
+
+            await db.SaveChangesAsync();
+
+            var organisationUser = new OrganisationUser(userContext.UserId, organisation.Id, UserStatus.Approved);
+            db.OrganisationUsers.Add(organisationUser);
+
             await db.SaveChangesAsync();
 
             return organisation.Id;
