e67d005fc357be07b151488215c45e01324924b8
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-08-19 21:13:12 +0100

    Refactored data access out of FindMatchingUsersHandler and added autho checks and unit tests.
    Related Work Items: #30891

--- src/EA.Weee.RequestHandlers/Admin/FindMatchingUsersHandler.cs
+++ src/EA.Weee.RequestHandlers/Admin/FindMatchingUsersHandler.cs
@@ -1,77 +1,47 @@
 ﻿namespace EA.Weee.RequestHandlers.Admin
 {
+    using Core.Admin;
+    using Core.Shared.Paging;
+    using DataAccess;
+    using EA.Weee.RequestHandlers.Security;
+    using Prsd.Core.Mediator;
+    using Requests.Admin;
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Linq;
     using System.Threading.Tasks;
-    using Core.Admin;
-    using Core.Shared.Paging;
-    using DataAccess;
-    using Prsd.Core.Mediator;
-    using Requests.Admin;
-    using userStatus = Core.Shared.UserStatus;
+    using UserStatus = Core.Shared.UserStatus;
 
     internal class FindMatchingUsersHandler : IRequestHandler<FindMatchingUsers, UserSearchDataResult>
     {
-          private readonly WeeeContext context;
-   
-          public FindMatchingUsersHandler(WeeeContext context)
-            {
-                this.context = context;
-            }
+        private readonly IWeeeAuthorization authorization;
+        private readonly IFindMatchingUsersDataAccess dataAccess;
+
+        public FindMatchingUsersHandler(IWeeeAuthorization authorization, IFindMatchingUsersDataAccess dataAccess)
+        {
+            this.authorization = authorization;
+            this.dataAccess = dataAccess;
+        }
 
         public async Task<UserSearchDataResult> HandleAsync(FindMatchingUsers query)
         {
-            var organisationsUsers = await GetOrganisationUsers();
-      
-            //internal users
-            var competentAuthorityUsers = await GetCompetentAuthorityUsers();
+            authorization.EnsureCanAccessInternalArea();
+
+            var organisationsUsers = await dataAccess.GetOrganisationUsers();
+            var competentAuthorityUsers = await dataAccess.GetCompetentAuthorityUsers();
+
             var totalUsersData = organisationsUsers.Concat(competentAuthorityUsers).OrderBy(u => u.FullName).ToList();
+            
             if (query.Paged)
             {
-               IList<UserSearchData> pagedMatchingUsersData =
-                    totalUsersData.Skip((query.Page - 1) * query.UsersPerPage)
-                        .Take(query.UsersPerPage)
-                        .ToList();
-               return new UserSearchDataResult(pagedMatchingUsersData, totalUsersData.Count);
+                IList<UserSearchData> pagedMatchingUsersData =
+                     totalUsersData.Skip((query.Page - 1) * query.UsersPerPage)
+                         .Take(query.UsersPerPage)
+                         .ToList();
+                return new UserSearchDataResult(pagedMatchingUsersData, totalUsersData.Count);
             }
+
             return new UserSearchDataResult(totalUsersData, totalUsersData.Count);
-        }
-
-        private async Task<UserSearchData[]> GetCompetentAuthorityUsers()
-        {
-            var competentAuthorityUsers = await(from u in context.Users
-                join cu in context.CompetentAuthorityUsers on u.Id equals cu.UserId into caUsers
-                from caUser in caUsers
-                join ca in context.UKCompetentAuthorities on caUser.CompetentAuthorityId equals ca.Id
-                select new UserSearchData
-                {
-                    Email = u.Email,
-                    FirstName = u.FirstName,
-                    Id = u.Id,
-                    LastName = u.Surname,
-                    OrganisationName = ca.Abbreviation,
-                    Status = (userStatus)caUser.UserStatus.Value
-                }).ToArrayAsync();
-            return competentAuthorityUsers;
-        }
-
-        private async Task<UserSearchData[]> GetOrganisationUsers()
-        {
-            var organisationsUsers = await(from u in context.Users
-                join ou in context.OrganisationUsers on u.Id equals ou.UserId into idOrgUsers
-                from orgUser in idOrgUsers
-                join org in context.Organisations on orgUser.OrganisationId equals org.Id
-                select new UserSearchData
-                {
-                    Email = u.Email,
-                    FirstName = u.FirstName,
-                    Id = u.Id,
-                    LastName = u.Surname,
-                    OrganisationName = org.Name ?? org.TradingName,
-                    Status = (userStatus)orgUser.UserStatus.Value
-                }).ToArrayAsync();
-            return organisationsUsers;
         }
     }
 }
