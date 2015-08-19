9a44f04ee8201cb9020cd8b0b06799a8edfa8da0
Owen Hall <owenh@sfwltd.co.uk>
2015-08-19 18:04:59 +0100

    Adding external user authorization checks to a the relevant handlers. The tests are super non-DRY at the moment, but time's tight.

--- src/EA.Weee.RequestHandlers/Organisations/Create/CreatePartnershipRequestHandler.cs
+++ src/EA.Weee.RequestHandlers/Organisations/Create/CreatePartnershipRequestHandler.cs
@@ -8,20 +8,25 @@
     using Prsd.Core.Domain;
     using Prsd.Core.Mediator;
     using Requests.Organisations.Create;
+    using Security;
 
     public class CreatePartnershipRequestHandler : IRequestHandler<CreatePartnershipRequest, Guid>
     {
+        private readonly IWeeeAuthorization authorization;
         private readonly WeeeContext db;
         private readonly IUserContext userContext;
 
-        public CreatePartnershipRequestHandler(WeeeContext db, IUserContext userContext)
+        public CreatePartnershipRequestHandler(IWeeeAuthorization authorization, WeeeContext db, IUserContext userContext)
         {
+            this.authorization = authorization;
             this.db = db;
             this.userContext = userContext;
         }
 
         public async Task<Guid> HandleAsync(CreatePartnershipRequest message)
         {
+            authorization.EnsureCanAccessExternalArea();
+
             var organisation = Organisation.CreatePartnership(message.TradingName);
             db.Organisations.Add(organisation);
 
