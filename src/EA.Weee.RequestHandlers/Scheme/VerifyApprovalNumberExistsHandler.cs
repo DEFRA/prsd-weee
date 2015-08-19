50321d8a32b88404e025e8c8605a44c0bf3499af
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-08-19 21:31:38 +0100

    Added internal user autho checks to request handlers.

--- src/EA.Weee.RequestHandlers/Scheme/VerifyApprovalNumberExistsHandler.cs
+++ src/EA.Weee.RequestHandlers/Scheme/VerifyApprovalNumberExistsHandler.cs
@@ -1,22 +1,27 @@
 ﻿namespace EA.Weee.RequestHandlers.Scheme
 {
-    using System.Data.Entity;
-    using System.Threading.Tasks;
     using DataAccess;
+    using EA.Weee.RequestHandlers.Security;
     using Prsd.Core.Mediator;
     using Requests.Scheme;
+    using System.Data.Entity;
+    using System.Threading.Tasks;
 
     internal class VerifyApprovalNumberExistsHandler : IRequestHandler<VerifyApprovalNumberExists, bool>
     {
         private readonly WeeeContext db;
+        private readonly IWeeeAuthorization authorization;
 
-        public VerifyApprovalNumberExistsHandler(WeeeContext context)
+        public VerifyApprovalNumberExistsHandler(WeeeContext context, IWeeeAuthorization authorization)
         {
             db = context;
+            this.authorization = authorization;
         }
 
         public async Task<bool> HandleAsync(VerifyApprovalNumberExists message)
         {
+            authorization.EnsureCanAccessInternalArea();
+
             return await ApprovalNumberExists(message.ApprovalNumber);
         }
 
