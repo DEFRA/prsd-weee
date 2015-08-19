c0437ba55dbc15a2f3643ea87d90030db1a3c1de
Owen Hall <owenh@sfwltd.co.uk>
2015-08-19 18:04:59 +0100

    Adding external user authorization checks to a the relevant handlers. The tests are super non-DRY at the moment, but time's tight.

--- src/EA.Weee.RequestHandlers/Organisations/Create/CreateSoleTraderRequestHandler.cs
+++ src/EA.Weee.RequestHandlers/Organisations/Create/CreateSoleTraderRequestHandler.cs
@@ -8,20 +8,25 @@
     using Prsd.Core.Domain;
     using Prsd.Core.Mediator;
     using Requests.Organisations.Create;
+    using Security;
 
     public class CreateSoleTraderRequestHandler : IRequestHandler<CreateSoleTraderRequest, Guid>
     {
+        private readonly IWeeeAuthorization authorization;
         private readonly WeeeContext db;
         private readonly IUserContext userContext;
 
-        public CreateSoleTraderRequestHandler(WeeeContext db, IUserContext userContext)
+        public CreateSoleTraderRequestHandler(IWeeeAuthorization authorization, WeeeContext db, IUserContext userContext)
         {
+            this.authorization = authorization;
             this.db = db;
             this.userContext = userContext;
         }
 
         public async Task<Guid> HandleAsync(CreateSoleTraderRequest message)
         {
+            authorization.EnsureCanAccessExternalArea();
+
             var organisation = Organisation.CreateSoleTrader(message.TradingName);
             db.Organisations.Add(organisation);
 
