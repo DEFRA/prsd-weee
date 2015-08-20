9fabe114c0fe9fb90afb5675560a3949be404c91
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-08-20 13:47:37 +0100

    Fix issue with user not linked to a complete organisation

--- src/EA.Weee.RequestHandlers.Tests.Unit/Organisations/GetOrganisationsByUserIdHandlerTest.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Organisations/GetOrganisationsByUserIdHandlerTest.cs
@@ -21,7 +21,7 @@
         private readonly Guid userId = new Guid("012F9664-5286-433A-8628-AAE13FD1C2F5");
 
         [Fact]
-        public async Task GetOrganisationsByUserIdHandler_RequestApprovedStatus_ReturnsApprovedOrganisationUsersOnly()
+        public async Task GetOrganisationsByUserIdHandler_RequestApprovedUserStatus_ReturnsApprovedOrganisationUsersOnly()
         {
             var organisationUsers = MakeOrganisationUser(userId);
 
@@ -49,7 +49,7 @@
 
         [Fact]
         public async Task
-            GetOrganisationsByUserIdHandler_RequestPendingAndRefusedStatus_ReturnsPendingAndRefusedOrganisationUsersOnly
+            GetOrganisationsByUserIdHandler_RequestPendingAndRefusedUserStatus_ReturnsPendingAndRefusedOrganisationUsersOnly
             ()
         {
             var organisationUsers = MakeOrganisationUser(userId);
@@ -78,7 +78,7 @@
         }
 
         [Fact]
-        public async Task GetOrganisationsByUserIdHandler_NoRequestStatus_ReturnsAllOrganisationUsers()
+        public async Task GetOrganisationsByUserIdHandler_NoRequestUserStatus_ReturnsAllOrganisationUsers()
         {
             var organisationUsers = MakeOrganisationUser(userId);
 
@@ -100,6 +100,79 @@
             Assert.Equal(3, orgUsers.Count);
         }
 
+        [Fact]
+        public async Task GetOrganisationsByUserIdHandler_NoRequestUserStatusCompletedOrganisation_ReturnsAllOrganisationUsersForCompletedOrganisation()
+        {
+            var organisationUsers = MakeOrganisationUserWithOrganisation(userId);
+
+            var context = A.Fake<WeeeContext>();
+            var addressMapper = new AddressMap();
+            var contactMapper = new ContactMap();
+            var userMapper = new UserMap();
+            var orgMapper = new OrganisationMap(addressMapper, contactMapper);
+            var orgUsermapper = new OrganisationUserMap(orgMapper, userMapper);
+
+            A.CallTo(() => context.OrganisationUsers).Returns(organisationUsers);
+
+            var handler = new GetOrganisationsByUserIdHandler(context, orgUsermapper);
+
+            var orgUsers = await handler.HandleAsync(new GetOrganisationsByUserId(userId.ToString(),
+                new int[] { },
+                new int[] { (int)Core.Shared.OrganisationStatus.Pending }));
+
+            Assert.Equal(2, orgUsers.Count);
+            Assert.True(orgUsers.First().UserStatus == UserStatus.Approved);
+            Assert.True(orgUsers.Last().UserStatus == UserStatus.Refused);
+        }
+
+        [Fact]
+        public async Task GetOrganisationsByUserIdHandler_NoRequestUserStatusIncompleteOrganisation_ReturnsAllOrganisationUsersForIncompleteOrganisation()
+        {
+            var organisationUsers = MakeOrganisationUserWithOrganisation(userId);
+
+            var context = A.Fake<WeeeContext>();
+            var addressMapper = new AddressMap();
+            var contactMapper = new ContactMap();
+            var userMapper = new UserMap();
+            var orgMapper = new OrganisationMap(addressMapper, contactMapper);
+            var orgUsermapper = new OrganisationUserMap(orgMapper, userMapper);
+
+            A.CallTo(() => context.OrganisationUsers).Returns(organisationUsers);
+
+            var handler = new GetOrganisationsByUserIdHandler(context, orgUsermapper);
+
+            var orgUsers = await handler.HandleAsync(new GetOrganisationsByUserId(userId.ToString(),
+                new int[] { },
+                new int[] { (int)Core.Shared.OrganisationStatus.Incomplete }));
+
+            Assert.Equal(1, orgUsers.Count);
+            Assert.True(orgUsers.First().UserStatus == UserStatus.Approved);
+        }
+
+        [Fact]
+        public async Task GetOrganisationsByUserIdHandler_RequestRefusedUserStatusCompletedOrganisation_ReturnsRefusedOrganisationUsersForCompletedOrganisation()
+        {
+            var organisationUsers = MakeOrganisationUserWithOrganisation(userId);
+
+            var context = A.Fake<WeeeContext>();
+            var addressMapper = new AddressMap();
+            var contactMapper = new ContactMap();
+            var userMapper = new UserMap();
+            var orgMapper = new OrganisationMap(addressMapper, contactMapper);
+            var orgUsermapper = new OrganisationUserMap(orgMapper, userMapper);
+
+            A.CallTo(() => context.OrganisationUsers).Returns(organisationUsers);
+
+            var handler = new GetOrganisationsByUserIdHandler(context, orgUsermapper);
+
+            var orgUsers = await handler.HandleAsync(new GetOrganisationsByUserId(userId.ToString(),
+                new int[] { (int)UserStatus.Refused },
+                new int[] { (int)Core.Shared.OrganisationStatus.Pending }));
+
+            Assert.Equal(1, orgUsers.Count);
+            Assert.True(orgUsers.First().UserStatus == UserStatus.Refused);
+        }
+
         private DbSet<OrganisationUser> MakeOrganisationUser(Guid userGuid)
         {
             return helper.GetAsyncEnabledDbSet(new[]
@@ -108,6 +181,28 @@
                 orgUserHelper.GetOrganisationUser(userGuid, Domain.UserStatus.Pending),
                 orgUserHelper.GetOrganisationUser(userGuid, Domain.UserStatus.Refused)
             });
+        }
+
+        private DbSet<OrganisationUser> MakeOrganisationUserWithOrganisation(Guid userGuid)
+        {
+            return helper.GetAsyncEnabledDbSet(new[]
+            {
+                MakeOrganisationUserWithOrganisation(userGuid, Domain.UserStatus.Approved, Domain.Organisation.OrganisationStatus.Complete),
+                MakeOrganisationUserWithOrganisation(userGuid, Domain.UserStatus.Approved, Domain.Organisation.OrganisationStatus.Incomplete),
+                MakeOrganisationUserWithOrganisation(userGuid, Domain.UserStatus.Refused, Domain.Organisation.OrganisationStatus.Complete)
+            });
+        }
+
+        private OrganisationUser MakeOrganisationUserWithOrganisation(Guid userGuid, Domain.UserStatus userStatus, Domain.Organisation.OrganisationStatus organisationStatus)
+        {
+            var fakeOrganisation = A.Fake<Organisation>();
+            A.CallTo(() => fakeOrganisation.OrganisationStatus).Returns(organisationStatus);
+            A.CallTo(() => fakeOrganisation.OrganisationType).Returns(A.Dummy<OrganisationType>());
+
+            var fakeOrganisationUser = A.Fake<OrganisationUser>(x => x.WithArgumentsForConstructor(() => new OrganisationUser(userGuid, Guid.NewGuid(), userStatus)));
+            A.CallTo(() => fakeOrganisationUser.Organisation).Returns(fakeOrganisation);
+
+            return fakeOrganisationUser;
         }
     }
 }
