287b4128edfc087bb5d8c2fff833adef0e966a56
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-08-21 12:23:55 +0530

    Added organisation access checks
    Related Work Items: #30893

--- src/EA.Weee.RequestHandlers/Organisations/VerifyOrganisationExistsHandler.cs
+++ src/EA.Weee.RequestHandlers/Organisations/VerifyOrganisationExistsHandler.cs
@@ -5,18 +5,23 @@
     using EA.Prsd.Core.Mediator;
     using EA.Weee.DataAccess;
     using EA.Weee.Requests.Organisations;
+    using Security;
 
     internal class VerifyOrganisationExistsHandler : IRequestHandler<VerifyOrganisationExists, bool>
     {
+        private readonly IWeeeAuthorization authorization;
         private readonly WeeeContext context;
 
-        public VerifyOrganisationExistsHandler(WeeeContext context)
+        public VerifyOrganisationExistsHandler(IWeeeAuthorization authorization, WeeeContext context)
         {
+            this.authorization = authorization;
             this.context = context;
         }
 
         public Task<bool> HandleAsync(VerifyOrganisationExists message)
         {
+            authorization.EnsureOrganisationAccess(message.OrganisationId);
+
             var organisationExists = context.Organisations.FirstOrDefault(o => o.Id == message.OrganisationId) != null;
 
             return Task.FromResult(organisationExists);
