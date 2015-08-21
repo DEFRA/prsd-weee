1d007dd1e0f9f2197f32d52330f277963b3b7eb4
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-08-21 12:23:55 +0530

    Added organisation access checks
    Related Work Items: #30893

--- src/EA.Weee.RequestHandlers.Tests.Unit/Organisations/VerifyOrganisationExistsHandlerTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Organisations/VerifyOrganisationExistsHandlerTests.cs
@@ -3,12 +3,14 @@
     using System;
     using System.Data.Entity;
     using System.Linq;
+    using System.Security;
     using System.Threading.Tasks;
     using DataAccess;
     using Domain.Organisation;
     using FakeItEasy;
     using Helpers;
     using RequestHandlers.Organisations;
+    using RequestHandlers.Security;
     using Requests.Organisations;
     using Xunit;
 
@@ -17,6 +19,19 @@
         private readonly DbContextHelper helper = new DbContextHelper();
 
         private readonly OrganisationHelper orgHelper = new OrganisationHelper();
+
+        private readonly IWeeeAuthorization permissiveAuthorization = AuthorizationBuilder.CreateUserAllowedToAccessOrganisation();
+
+        [Fact]
+        public async Task VerifyOrganisationExistsHandler_NotOrganisationUser_ThrowsSecurityException()
+        {
+            var authorization = AuthorizationBuilder.CreateUserDeniedFromAccessingOrganisation();
+
+            var handler = new VerifyOrganisationExistsHandler(authorization, A<WeeeContext>._);
+            var message = new VerifyOrganisationExists(Guid.NewGuid());
+
+            await Assert.ThrowsAsync<SecurityException>(async () => await handler.HandleAsync(message));
+        }
 
         [Fact]
         public async Task VerifyOrganisationExistsHandler_OrgExists_ReturnsTrue()
@@ -27,7 +42,7 @@
 
             A.CallTo(() => context.Organisations).Returns(organisations);
 
-            var handler = new VerifyOrganisationExistsHandler(context);
+            var handler = new VerifyOrganisationExistsHandler(permissiveAuthorization, context);
 
             var exists = await handler.HandleAsync(new VerifyOrganisationExists(organisations.FirstOrDefault().Id));
 
@@ -43,7 +58,7 @@
 
             A.CallTo(() => context.Organisations).Returns(organisations);
 
-            var handler = new VerifyOrganisationExistsHandler(context);
+            var handler = new VerifyOrganisationExistsHandler(permissiveAuthorization, context);
 
             var exists = await handler.HandleAsync(new VerifyOrganisationExists(Guid.NewGuid()));
 
