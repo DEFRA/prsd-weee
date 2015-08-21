0cd371c5a3bb8049114dcc7b6b36e252cd2319c6
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-08-21 19:36:58 +0100

    Added unit test for UpdateSchemeInformationHandler.
    Related Work Items: #31023

--- src/EA.Weee.RequestHandlers.Tests.Unit/Scheme/UpdateSchemeInformationHandlerTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Scheme/UpdateSchemeInformationHandlerTests.cs
@@ -13,6 +13,7 @@
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Linq;
+    using System.Security;
     using System.Threading.Tasks;
     using Xunit;
     using ObligationType = Domain.ObligationType;
@@ -20,7 +21,38 @@
     public class UpdateSchemeInformationHandlerTests
     {
         private readonly DbContextHelper helper = new DbContextHelper();
-        
+
+        /// <summary>
+        /// This test ensures that a non-internal user cannot execute requests to update
+        /// a scheme's information.
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        [Trait("Authorization", "Internal")]
+        public async Task UpdateSchemeInformationHandler_WithUnauthorizedUser_ThrowsSecurityException()
+        {
+            // Arrange
+            WeeeContext context = A.Fake<WeeeContext>();
+            IWeeeAuthorization authorization = AuthorizationBuilder.CreateUserWithNoRights();
+
+            UpdateSchemeInformationHandler handler = new UpdateSchemeInformationHandler(context, authorization);
+
+            UpdateSchemeInformation request = new UpdateSchemeInformation(
+                new Guid("3C367528-AE93-427F-A4C5-E23F0D317633"),
+                "New scheme name",
+                "New approval name",
+                "New IBIS customer reference",
+                Core.Shared.ObligationType.B2B,
+                new Guid("{291F9DE0-2600-481C-9C66-5ACB4033668C}"),
+                Core.Shared.SchemeStatus.Approved);
+
+            // Act
+            Func<Task<Guid>> action = () => handler.HandleAsync(request);
+
+            // Assert
+            await Assert.ThrowsAsync<SecurityException>(action);
+        }
+
         [Fact]
         public async Task UpdateSchemeInformationHandler_UpdateSchemeKeyInformation_ReturnsUpdatedScheme()
         {
