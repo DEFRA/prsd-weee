b243a76681d82344396b5999abcb88bcfd38e9ed
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-08-26 20:39:39 +0100

    Updated UI, domain objects and handlers.

--- src/EA.Weee.Requests/Scheme/MemberRegistration/GetComplianceYears.cs
+++ src/EA.Weee.Requests/Scheme/MemberRegistration/GetComplianceYears.cs
@@ -3,12 +3,13 @@
     using System;
     using Core.Scheme;
     using Prsd.Core.Mediator;
+using System.Collections.Generic;
 
-    public class GetLatestMemberUploadList : IRequest<LatestMemberUploadList>
+    public class GetComplianceYears : IRequest<List<int>>
     {
         public Guid PcsId { get; private set; }
 
-        public GetLatestMemberUploadList(Guid pcsId)
+        public GetComplianceYears(Guid pcsId)
         {
             PcsId = pcsId;
         }
---
+++ src/EA.Weee.Requests/Scheme/MemberRegistration/GetProducerCSV.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Weee.Requests.Scheme.MemberRegistration
+{
+    using System;
+    using Core.Scheme;
+    using Prsd.Core.Mediator;
+
+    public class GetProducerCSV : IRequest<ProducerCSVFileData>
+    {
+        public Guid OrganisationId { get; private set; }
+        public int ComplianceYear { get; private set; }
+
+        public GetProducerCSV(Guid organisationId, int complianceYear)
+        {
+            OrganisationId = organisationId;
+            ComplianceYear = complianceYear;
+        }
+    }
+}
