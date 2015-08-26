5b5dd4559c81ba49a03d6682460584be6e8db7b2
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-08-26 20:39:02 +0100

    Extended WeeeContext to allow access to stored procedure

---
+++ src/EA.Weee.DataAccess/StoredProcedure/StoredProcedures.cs
@@ -0,0 +1,32 @@
+﻿namespace EA.Weee.DataAccess.StoredProcedure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StoredProcedures : IStoredProcedures
+    {
+        private readonly WeeeContext context;
+
+        public StoredProcedures(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<ProducerCsvData>> SpgCSVDataByOrganisationIdAndComplianceYear(Guid organisationId, int complianceYear)
+        {
+            var organisationIdParameter = new SqlParameter("@OrganisationId", organisationId);
+            var complianceYearParameter = new SqlParameter("@ComplianceYear", complianceYear);
+
+            return await context.Database
+                .SqlQuery<ProducerCsvData>(
+                    "[Producer].[spgCSVDataByOrganisationIdAndComplianceYear] @OrganisationId, @ComplianceYear",
+                    organisationIdParameter,
+                    complianceYearParameter)
+                .ToListAsync();
+        }
+    }
+}
