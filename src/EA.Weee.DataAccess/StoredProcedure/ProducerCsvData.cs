2a35fe29192fcf738ca5dc063f02658cf305ca42
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-08-26 20:39:02 +0100

    Extended WeeeContext to allow access to stored procedure

---
+++ src/EA.Weee.DataAccess/StoredProcedure/ProducerCsvData.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Weee.DataAccess.StoredProcedure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This class maps to the results of [Producer].[spgCSVDataByOrganisationIdAndComplianceYear].
+    /// </summary>
+    public class ProducerCsvData
+    {
+        public string OrganisationName { get; set; }
+        public string TradingName { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string CompanyNumber { get; set; }
+        public string ChargeBand { get; set; }
+        public DateTime DateRegistered { get; set; }
+        public DateTime DateAmended { get; set; }
+        public string AuthorisedRepresentative { get; set; }
+        public string OverseasProducer { get; set; }
+    }
+}
