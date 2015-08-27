612b86b409183d3de5a88ec39da976ec48afc9e4
Rick Powell <richard.powell@sfwltd.co.uk>
2015-08-27 15:20:23 +0100

    Refactor of validation around producer name change warning

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Scheme/MemberRegistration/XmlValidation/BusinessValidation/Rules/RulesTests.cs
@@ -0,0 +1,123 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Scheme.MemberRegistration.XmlValidation.BusinessValidation.Rules
+{
+    using System;
+    using Domain;
+    using Domain.Producer;
+    using FakeItEasy;
+    using RequestHandlers.Scheme.MemberRegistration.XmlValidation.BusinessValidation.Queries;
+    using RequestHandlers.Scheme.MemberRegistration.XmlValidation.BusinessValidation.Rules;
+    using Xunit;
+
+    public class RulesTests
+    {
+        private readonly IQueries queries;
+
+        public RulesTests()
+        {
+            queries = A.Fake<IQueries>();
+        }
+
+        [Fact]
+        public void ShouldNotWarnOfProducerNameChange_Insert_AndProducerExistsWithMatchingPrnInComplianceYearAndScheme_ReturnsTrue()
+        {
+            A.CallTo(() => queries.GetLatestProducerForComplianceYearAndScheme(A<string>._, A<string>._, A<Guid>._))
+                .Returns(FakeProducer.Create(ObligationType.Both, "ABC12345"));
+
+            var producerName = "Test Producer Name";
+
+            var result = Rules().ShouldNotWarnOfProducerNameChange(new schemeType(), new producerType
+            {
+                status = statusType.I,
+                producerBusiness = new producerBusinessType
+                {
+                    Item = new partnershipType
+                    {
+                        partnershipName = "New Producer Name"
+                    }
+                }
+            }, A<Guid>._);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldNotWarnOfProducerNameChange_Amendment_AndProducerExistsWithMatchingPrnInComplianceYearAndScheme_ReturnsFalse()
+        {
+            A.CallTo(() => queries.GetLatestProducerForComplianceYearAndScheme(A<string>._, A<string>._, A<Guid>._))
+                .Returns(FakeProducer.Create(ObligationType.Both, "ABC12345"));
+
+            var producerName = "Test Producer Name";
+
+            var result = Rules().ShouldNotWarnOfProducerNameChange(new schemeType(), new producerType
+            {
+                status = statusType.A,
+                producerBusiness = new producerBusinessType
+                {
+                    Item = new partnershipType
+                    {
+                        partnershipName = "New Producer Name"
+                    }
+                }
+            }, A<Guid>._);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void
+            ShouldNotWarnOfProducerNameChange_Amendment_AndProducerExistsWithMatchingPrnInPreviousComplianceYear_ReturnsFalse
+            ()
+        {
+            A.CallTo(() => queries.GetLatestProducerFromPreviousComplianceYears(A<string>._))
+                .Returns(FakeProducer.Create(ObligationType.Both, "ABC12345"));
+
+            var producerName = "Test Producer Name";
+
+            var result = Rules().ShouldNotWarnOfProducerNameChange(new schemeType(), new producerType
+            {
+                status = statusType.A,
+                producerBusiness = new producerBusinessType
+                {
+                    Item = new partnershipType
+                    {
+                        partnershipName = "New Producer Name"
+                    }
+                }
+            }, A<Guid>._);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldNotWarnOfProducerNameChange_Amendment_MigratedProducerExistsWithPrn_ReturnsFalse()
+        {
+            A.CallTo(() => queries.GetMigratedProducer(A<string>._))
+                .Returns(new FakeMigratedProducer());
+
+            var producerName = "Test Producer Name";
+
+            var result = Rules().ShouldNotWarnOfProducerNameChange(new schemeType(), new producerType
+            {
+                status = statusType.A,
+                producerBusiness = new producerBusinessType
+                {
+                    Item = new partnershipType
+                    {
+                        partnershipName = "New Producer Name"
+                    }
+                }
+            }, A<Guid>._);
+
+            Assert.False(result);
+        }
+
+        private Rules Rules()
+        {
+            return new Rules(queries);
+        }
+
+        private class FakeMigratedProducer : MigratedProducer
+        {
+        }
+    }
+}
---
+++ src/EA.Weee.RequestHandlers/Scheme/MemberRegistration/XmlValidation/BusinessValidation/Queries/Queries.cs
@@ -0,0 +1,42 @@
+﻿namespace EA.Weee.RequestHandlers.Scheme.MemberRegistration.XmlValidation.BusinessValidation.Queries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DataAccess;
+    using Domain.Producer;
+
+    public class Queries : IQueries
+    {
+        private readonly IEnumerable<Producer> currentProducers;
+        private readonly IEnumerable<MigratedProducer> migratedProducers;
+
+        public Queries(WeeeContext context)
+        {
+            currentProducers = context.Producers.Where(p => p.IsCurrentForComplianceYear).ToList();
+            migratedProducers = context.MigratedProducers.ToList();
+        }
+
+        public Producer GetLatestProducerForComplianceYearAndScheme(string registrationNo, string schemeComplianceYear, Guid schemeOrgId)
+        {
+            return currentProducers.FirstOrDefault(p =>
+                                                        p.RegistrationNumber == registrationNo
+                                                        && p.MemberUpload.ComplianceYear == int.Parse(schemeComplianceYear)
+                                                        && p.Scheme.OrganisationId == schemeOrgId);
+        }
+
+        public Producer GetLatestProducerFromPreviousComplianceYears(string registrationNo)
+        {
+            return currentProducers
+                    .Where(p => p.RegistrationNumber == registrationNo)
+                    .OrderByDescending(p => p.MemberUpload.ComplianceYear)
+                    .ThenBy(p => p.UpdatedDate)
+                    .FirstOrDefault();
+        }
+
+        public MigratedProducer GetMigratedProducer(string registrationNo)
+        {
+            return migratedProducers.SingleOrDefault(p => p.ProducerRegistrationNumber == registrationNo);
+        }
+    }
+}
