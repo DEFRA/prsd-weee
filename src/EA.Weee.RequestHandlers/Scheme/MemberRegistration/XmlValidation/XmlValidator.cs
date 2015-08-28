6040b374c01a56a0e638fea6a9457d733d4523d0
Owen Hall <owenh@sfwltd.co.uk>
2015-08-28 14:28:39 +0100

    Catching an unusual error whereby the XML passes schema validation but then still fails on deserialization.

--- src/EA.Weee.RequestHandlers/Scheme/MemberRegistration/XmlValidation/XmlValidator.cs
+++ src/EA.Weee.RequestHandlers/Scheme/MemberRegistration/XmlValidation/XmlValidator.cs
@@ -1,7 +1,11 @@
 ﻿namespace EA.Weee.RequestHandlers.Scheme.MemberRegistration.XmlValidation
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Xml;
+    using Core.Helpers.Xml;
+    using Domain;
     using Domain.Scheme;
     using Interfaces;
     using Requests.Scheme.MemberRegistration;
@@ -10,28 +14,40 @@
     {
         private readonly ISchemaValidator schemaValidator;
         private readonly IBusinessValidator businessValidator;
-        private readonly IXmlConverter xmlConverter;
 
-        public XmlValidator(ISchemaValidator schemaValidator, IXmlConverter xmlConverter, IBusinessValidator businessValidator)
+        private readonly IXmlConverter xmlConverter;
+        private readonly IXmlErrorTranslator errorTranslator;
+
+        public XmlValidator(ISchemaValidator schemaValidator, IXmlConverter xmlConverter, IBusinessValidator businessValidator, IXmlErrorTranslator errorTranslator)
         {
             this.schemaValidator = schemaValidator;
             this.businessValidator = businessValidator;
+            this.errorTranslator = errorTranslator;
             this.xmlConverter = xmlConverter;
         }
 
         public IEnumerable<MemberUploadError> Validate(ProcessXMLFile message)
         {
             // Validate against the schema
-            var errors = schemaValidator.Validate(message);        
+            var errors = schemaValidator.Validate(message).ToList();
             if (errors.Any())
             {
                 return errors;
             }
-            
-            var deserializedXml = xmlConverter.Deserialize(xmlConverter.Convert(message));
 
-            // Validate against the deserialized XML
-            errors = businessValidator.Validate(deserializedXml, message.OrganisationId);            
+            try
+            {
+                // Validate deserialized XML against business rules
+                var document = xmlConverter.Convert(message);
+                var deserializedXml = xmlConverter.Deserialize(document);
+                errors = businessValidator.Validate(deserializedXml, message.OrganisationId).ToList();
+            }
+            catch (InvalidOperationException e)
+            {
+                var friendlyMessage = errorTranslator.MakeFriendlyErrorMessage(null, e.Message, -1);
+                errors.Add(new MemberUploadError(ErrorLevel.Error, MemberUploadErrorType.Schema, friendlyMessage));
+            }
+
             return errors;
         }
     }
