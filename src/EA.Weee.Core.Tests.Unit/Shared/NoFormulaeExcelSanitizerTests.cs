ab22b7232ba4a3f8f5e37491aa16b5f2cf879647
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-08-31 16:48:39 +0100

    Added Excel sanitizer

---
+++ src/EA.Weee.Core.Tests.Unit/Shared/NoFormulaeExcelSanitizerTests.cs
@@ -0,0 +1,87 @@
+﻿namespace EA.Weee.Core.Tests.Unit.Shared
+{
+    using EA.Weee.Core.Shared;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class NoFormulaeExcelSanitizerTests
+    {
+        /// <summary>
+        /// This test ensures that a safe input string is not identified as a threat.
+        /// </summary>
+        [Fact]
+        public void IsThreat_WithSafeInput_ReturnsFalse()
+        {
+            // Arrange
+            string input = "This is a safe input.";
+
+            NoFormulaeExcelSanitizer sanitizer = new NoFormulaeExcelSanitizer();
+
+            // Act
+            bool result = sanitizer.IsThreat(input);
+
+            // Assert
+            Assert.Equal(false, result);
+        }
+
+        /// <summary>
+        /// This test ensures that a string starting with an equals sign is
+        /// identified as a threat.
+        /// </summary>
+        [Fact]
+        public void IsThreat_WithUnsafeInput_ReturnsTrue()
+        {
+            // Arrange
+            string input = "=cmd|'/C ping 127.0.0.1'!A0";
+
+            NoFormulaeExcelSanitizer sanitizer = new NoFormulaeExcelSanitizer();
+
+            // Act
+            bool result = sanitizer.IsThreat(input);
+
+            // Assert
+            Assert.Equal(true, result);
+        }
+
+        /// <summary>
+        /// This test ensures that a safe input string is modified.
+        /// </summary>
+        [Fact]
+        public void Sanitize_WithSafeInput_ReturnsInputUnmodified()
+        {
+            // Arrange
+            string input = "This is a safe input.";
+
+            NoFormulaeExcelSanitizer sanitizer = new NoFormulaeExcelSanitizer();
+
+            // Act
+            string result = sanitizer.Sanitize(input);
+
+            // Assert
+            Assert.Equal("This is a safe input.", result);
+        }
+
+        /// <summary>
+        /// This test ensures that a string starting with an equals sign is
+        /// returned without the equals sign.
+        /// </summary>
+        [Fact]
+        public void Sanitize_WithUnsafeInput_RemovesLeadingEqualsSign()
+        {
+            // Arrange
+            string input = "=cmd|'/C ping 127.0.0.1'!A0";
+
+            NoFormulaeExcelSanitizer sanitizer = new NoFormulaeExcelSanitizer();
+
+            // Act
+            string result = sanitizer.Sanitize(input);
+
+            // Assert
+            Assert.Equal("cmd|'/C ping 127.0.0.1'!A0", result);
+        }
+    }
+}
