00e26029c0bec2733042b1df5c43c5fbefdf672c
Rick Powell <richard.powell@sfwltd.co.uk>
2015-09-01 15:04:42 +0100

    Refactor of rules and queries for XML Business validation

---
+++ src/EA.Weee.RequestHandlers/Scheme/MemberRegistration/XmlValidation/BusinessValidation/Rules/ProducerNameWarning.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Weee.RequestHandlers.Scheme.MemberRegistration.XmlValidation.BusinessValidation.Rules
+{
+    using System;
+
+    public class ProducerNameWarning
+    {
+        public schemeType Scheme;
+        public producerType Producer;
+        public Guid OrganisationId;
+
+        public ProducerNameWarning(schemeType scheme, producerType producer, Guid organisationId)
+        {
+            Scheme = scheme;
+            Producer = producer;
+            OrganisationId = organisationId;
+        }
+    }
+}
