30f404ec37f98cdd389f971097e5e0de42534562
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-09-03 21:30:05 +0100

    Refactored WEEE email functionality.

---
+++ src/EA.Weee.Email/IWeeeEmailService.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Email
+{
+    using EA.Weee.Domain;
+using EA.Weee.Domain.Scheme;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public interface IWeeeEmailService
+    {
+        Task<bool> SendActivateUserAccount(string emailAddress, string activationUrl);
+    }
+}
