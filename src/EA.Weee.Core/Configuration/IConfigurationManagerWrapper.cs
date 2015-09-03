5ca04811bd959b514fcc29ca7d5b4bbcaa98055b
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-09-03 22:32:11 +0100

    Updated InternalEmailaddressAtrribute to not depend on email rules.

--- src/EA.Weee.Core/Configuration/IConfigurationManagerWrapper.cs
+++ src/EA.Weee.Core/Configuration/IConfigurationManagerWrapper.cs
@@ -1,15 +1,12 @@
 ﻿namespace EA.Weee.Core.Configuration
 {
     using System.Collections.Specialized;
-    using EmailRules;
 
     public interface IConfigurationManagerWrapper
     {
         NameValueCollection AppSettings { get; }
 
-        RulesSection InternalEmailRules { get; }
-
-       string GetKeyValue(string key);
+        string GetKeyValue(string key);
 
         bool HasKey(string key);
     }
