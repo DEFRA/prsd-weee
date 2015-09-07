9de09d2cd632c6d5823047a03706834865e8beda
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-09-07 15:30:10 +0100

    Added organisation user request email.

---
+++ src/EA.Weee.Domain/Events/OrganisationUserRequestEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.Domain.Events
+{
+    using EA.Prsd.Core.Domain;
+    using EA.Weee.Domain.Organisation;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class OrganisationUserRequestEvent : IEvent
+    {
+        public OrganisationUser OrganisationUser { get; private set; }
+
+        internal OrganisationUserRequestEvent(OrganisationUser organisationUser)
+        {
+            OrganisationUser = organisationUser;
+        }
+    }
+}
