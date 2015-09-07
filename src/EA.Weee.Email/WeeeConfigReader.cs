7cf452944405584d4159730fe97546c72f61b033
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-09-07 15:45:39 +0100

    Added "Weee.SiteUrl" to API configuration.

---
+++ src/EA.Weee.Email/WeeeConfigReader.cs
@@ -0,0 +1,27 @@
+﻿namespace EA.Weee.Email
+{
+    using EA.Prsd.Email;
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class WeeeConfigReader : ConfigReader, IWeeeEmailConfiguration
+    {
+        public WeeeConfigReader()
+            : base("Weee")
+        {
+        }
+
+        public string SiteUrl
+        {
+            get
+            {
+                string settingName = GetAppSettingName("SiteUrl");
+                return ConfigurationManager.AppSettings.Get(settingName);
+            }
+        }
+    }
+}
