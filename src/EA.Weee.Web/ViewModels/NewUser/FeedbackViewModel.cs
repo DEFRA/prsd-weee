f9a8b20cbe946bc1c3f3a02fc22591f9ee65e468
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-09-16 14:46:06 +0530

    Added feedback page

---
+++ src/EA.Weee.Web/ViewModels/NewUser/FeedbackViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.Web.ViewModels.NewUser
+{
+    using System.ComponentModel.DataAnnotations;
+    using Shared;
+
+    public class FeedbackViewModel
+    {
+        [Required]
+        public RadioButtonStringCollectionViewModel FeedbackOptions { get; set; }
+
+        [StringLength(1200)]
+        public string FeedbackDescription { get; set; }
+    }
+}
