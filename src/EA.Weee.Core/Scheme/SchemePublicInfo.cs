884577dc13431a1672880ebd9c54d045f4c47bc8
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-09-16 21:11:49 +0100

    Added request handler for getting publically available scheme information.

---
+++ src/EA.Weee.Core/Scheme/SchemePublicInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Weee.Core.Scheme
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A DTO providing information about a scheme that is publically available.
+    /// </summary>
+    public class SchemePublicInfo
+    {
+        public Guid SchemeId { get; set; }
+        
+        public Guid OrganisationId { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string ApprovalNo { get; set; }
+
+        public string StatusName { get; set; }
+    }
+}
