d52fd51f4fc07f9c6e955be28efeeed83e09ee2e
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-09-25 21:14:28 +0100

    Moved data access out of ProducerChargeCalculator and updated to use efficient data retrieval.

---
+++ src/EA.Weee.RequestHandlers/Scheme/MemberRegistration/IProducerChargeCalculatorDataAccess.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.RequestHandlers.Scheme.MemberRegistration
+{
+    using EA.Weee.Domain;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IProducerChargeCalculatorDataAccess
+    {
+        decimal FetchChargeBandAmount(ChargeBandType chargeBand);
+        decimal FetchSumOfExistingCharges(string registrationNumber, int complianceYear);
+    }
+}
