9b17f9a81c8ee328b29b728574d2df5f832140d9
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-09-25 11:27:40 +0100

    Refactoring and re-adding charge band changes (in progress)

---
+++ src/EA.Weee.Xml/IProducerChargeBandCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Xml
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using EA.Weee.Domain;
+    using EA.Weee.Xml.Schemas;
+
+    public interface IProducerChargeBandCalculator
+    {
+        ChargeBandType GetProducerChargeBand(annualTurnoverBandType annualTurnoverBand, bool vatRegistered, eeePlacedOnMarketBandType eeePlacedOnMarketBand);
+    }
+}
