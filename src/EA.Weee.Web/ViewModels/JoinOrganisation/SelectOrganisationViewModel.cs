432d8ed91e36cb7a6776886b3849e3e1c6dd06d0
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-09-28 13:24:28 +0100

    Removed commented code

--- src/EA.Weee.Web/ViewModels/JoinOrganisation/SelectOrganisationViewModel.cs
+++ src/EA.Weee.Web/ViewModels/JoinOrganisation/SelectOrganisationViewModel.cs
@@ -1,22 +1,13 @@
 ﻿namespace EA.Weee.Web.ViewModels.JoinOrganisation
 {
-    using System;
     using System.ComponentModel.DataAnnotations;
  
     public class SelectOrganisationViewModel
     {
         public string Name { get; set; }
 
-        //public string TradingName { get; set; }
-
-        //public string CompaniesRegistrationNumber { get; set; }
-
         public string SearchedText { get; set; }
-
-      //  public OrganisationType Type { get; set; }
-
-        //public Guid? OrganisationId { get; set; }
-
+   
         [Required]
         public SelectOrganisationRadioButtons Organisations { get; set; }
     }
