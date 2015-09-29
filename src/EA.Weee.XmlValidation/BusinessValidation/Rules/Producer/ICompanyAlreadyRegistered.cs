fef5573aa20a8343bd8ef10ed1405497b33d75aa
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-09-29 14:28:12 +0100

    Fix formatting issue

--- src/EA.Weee.XmlValidation/BusinessValidation/Rules/Producer/ICompanyAlreadyRegistered.cs
+++ src/EA.Weee.XmlValidation/BusinessValidation/Rules/Producer/ICompanyAlreadyRegistered.cs
@@ -5,7 +5,7 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
-using EA.Weee.Xml.Schemas;
+    using EA.Weee.Xml.Schemas;
 
     public interface ICompanyAlreadyRegistered
     {
