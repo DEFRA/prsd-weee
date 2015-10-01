8d820fd550b8de3940f13058707c4f9fe89879fd
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-10-01 23:19:47 +0100

    Added find-producer UI (temporary data access).

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Producers/SearchResultsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Producers
+{
+    using EA.Weee.Core.Admin;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class SearchResultsViewModel
+    {
+        public IList<ProducerSearchResult> Results { get; set; }
+    }
+}
