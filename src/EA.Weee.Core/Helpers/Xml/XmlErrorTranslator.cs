d04684261a015ef9fbbe22e2570be45187348891
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-10-02 19:38:28 +0100

    Updated error message text and included line numbers.

--- src/EA.Weee.Core/Helpers/Xml/XmlErrorTranslator.cs
+++ src/EA.Weee.Core/Helpers/Xml/XmlErrorTranslator.cs
@@ -20,14 +20,17 @@
         private const string IncompleteContentPattern =
            @"^The element '([^']*)' in namespace '[^']*' has incomplete content. List of possible elements expected: '[^']*' in namespace '[^']*'.$";
 
-        private const string AmpersandWithNoCharacterEntityReferencePattern =
-            @"^An error occurred while parsing EntityName\.";
+        private static readonly Regex EntityNameParsingErrorPattern = new Regex(
+            @"^An error occurred while parsing EntityName\. Line (?<LineNumber>\d+), position \d+\.$",
+            RegexOptions.Compiled);
 
-        private const string UnterminatedCharacterEntityReferencePattern =
-            @"^'[^']*' is an unexpected token\. The expected token is ';'\.";
+        private static readonly Regex UnterminatedCharacterEntityReferencePattern = new Regex(
+            @"^'[^']*' is an unexpected token\. The expected token is ';'\. Line (?<LineNumber>\d+), position \d+\.$",
+            RegexOptions.Compiled);
 
-        private const string InvalidCharacterEntityReferencePattern =
-            @"^Reference to undeclared entity '[^']*'\.";
+        private static readonly Regex InvalidCharacterEntityReferencePattern = new Regex(
+            @"^Reference to undeclared entity '[^']*'\. Line (?<LineNumber>\d+), position \d+\.$",
+            RegexOptions.Compiled);
 
         private const string ErrorInXmlDocumentPattern = @"^There is an error in XML document \(([0-9]*)\,\s([0-9]*)\)\.$";
 
@@ -39,6 +42,7 @@
         public string MakeFriendlyErrorMessage(XElement sender, string message, int lineNumber)
         {
             string resultErrorMessage = message;
+            Match match = null;
 
             if (Regex.IsMatch(message, DataAtTheRoolLevelIsInvalid))
             {
@@ -64,17 +68,20 @@
             {
                 resultErrorMessage = MakeFriendlyErrorInXmlDocumentMessage(message);
             }
-            else if (Regex.IsMatch(message, AmpersandWithNoCharacterEntityReferencePattern))
+            else if (TestRegex(message, EntityNameParsingErrorPattern, out match))
             {
-                resultErrorMessage = "The XML file you're trying to upload has not been encoded correctly. Please check that any ampersand characters (&) have been replaced by &amp;.";
+                lineNumber = int.Parse(match.Groups["LineNumber"].Value);
+                resultErrorMessage = "Your XML file is not encoded correctly. Check for any characters which need to be encoded. For example, replace ampersand (&) characters with &amp;.";
             }
-            else if (Regex.IsMatch(message, UnterminatedCharacterEntityReferencePattern))
+            else if (TestRegex(message, UnterminatedCharacterEntityReferencePattern, out match))
             {
-                resultErrorMessage = "The XML file you're trying to upload has not been encoded correctly. Please check that any ampersand characters (&) have been replaced by &amp;.";
+                lineNumber = int.Parse(match.Groups["LineNumber"].Value);
+                resultErrorMessage = "Your XML file is not encoded correctly. Check for any characters which need to be encoded. For example, replace ampersand (&) characters with &amp;.";
             }
-            else if (Regex.IsMatch(message, InvalidCharacterEntityReferencePattern))
+            else if (TestRegex(message, InvalidCharacterEntityReferencePattern, out match))
             {
-                resultErrorMessage = "The XML file you're trying to upload has not been encoded correctly. Please check that any ampersand characters (&) have been replaced by &amp;.";
+                lineNumber = int.Parse(match.Groups["LineNumber"].Value);
+                resultErrorMessage = "Your XML file is not encoded correctly. Check for any characters which need to be encoded. For example, replace ampersand (&) characters with &amp;.";
             }
 
             var registrationNo = GetRegistrationNumber(sender);
@@ -83,6 +90,12 @@
             var lineNumberText = lineNumber > 0 ? string.Format(" (XML line {0})", lineNumber) : string.Empty;
 
             return string.Format("{0}{1}{2}", registrationNoText, resultErrorMessage, lineNumberText);
+        }
+
+        private static bool TestRegex(string message, Regex regex, out Match match)
+        {
+            match = regex.Match(message);
+            return match.Success;
         }
 
         private string MakeFriendlyGeneralConstraintFailureMessage(XElement sender, string exceptionMessage)
