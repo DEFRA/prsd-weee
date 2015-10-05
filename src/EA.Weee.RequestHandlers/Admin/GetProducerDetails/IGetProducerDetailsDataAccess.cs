75b465b8b846a5e80f0ae26a476818479af7a30f
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-10-05 21:50:40 +0100

    Added back-end for producer details.

---
+++ src/EA.Weee.RequestHandlers/Admin/GetProducerDetails/IGetProducerDetailsDataAccess.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.RequestHandlers.Admin.GetProducerDetails
+{
+    using EA.Weee.Domain.Producer;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IGetProducerDetailsDataAccess
+    {
+        Task<List<Producer>> Fetch(string registrationNumber);
+    }
+}
