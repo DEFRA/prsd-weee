b2d3aaa0c26d8b05683e6ae20cdbb2f9906c8b05
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-10-05 21:50:40 +0100

    Added back-end for producer details.

---
+++ src/EA.Weee.RequestHandlers/Admin/GetProducerDetails/GetProducerDetailsDataAccess.cs
@@ -0,0 +1,35 @@
+﻿namespace EA.Weee.RequestHandlers.Admin.GetProducerDetails
+{
+    using EA.Weee.DataAccess;
+    using EA.Weee.Domain.Producer;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GetProducerDetailsDataAccess : IGetProducerDetailsDataAccess
+    {
+        private readonly WeeeContext context;
+
+        public GetProducerDetailsDataAccess(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Producer>> Fetch(string registrationNumber)
+        {
+            return await context.Producers
+                .Where(p => p.MemberUpload.IsSubmitted)
+                .Where(p => p.RegistrationNumber == registrationNumber)
+                .OrderBy(p => p.RegistrationNumber)
+                .Include(p => p.MemberUpload)
+                .Include(p => p.ProducerBusiness)
+                .Include(p => p.ProducerBusiness.CompanyDetails)
+                .Include(p => p.ProducerBusiness.Partnership)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
