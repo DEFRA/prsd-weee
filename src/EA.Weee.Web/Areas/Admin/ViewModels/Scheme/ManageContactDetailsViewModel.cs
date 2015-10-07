7e468780d62540e9edb7264f10ab24581413fda3
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-10-07 15:15:24 +0530

    Stuff for view or edit org contact details for internal

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Scheme/ManageContactDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Scheme
+{
+    using System;
+    using Core.Organisations;
+
+    public class ManageContactDetailsViewModel
+    {
+        public OrganisationData OrganisationData { get; set; }
+
+        public Guid SchemeId { get; set; }
+    }
+}
