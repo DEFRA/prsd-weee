a33ef5b127a9659d5eb522effa0f0d8f9660ee17
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-10-09 18:01:48 +0100

    Added security event auditor.

---
+++ src/EA.Weee.Api/Identity/ApplicationUserManagerBase.cs
@@ -0,0 +1,52 @@
+﻿namespace EA.Weee.Api.Identity
+{
+    using Microsoft.AspNet.Identity;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+
+    /// <summary>
+    /// A base class which raises events on an IAuditSecurityAuditor relating to user creation,
+    /// user updates, account verification and claim issueing.
+    /// </summary>
+    /// <typeparam name="TUser"></typeparam>
+    public abstract class ApplicationUserManagerBase<TUser> : UserManager<TUser> where TUser : class, Microsoft.AspNet.Identity.IUser<string>
+    {
+        private readonly ISecurityEventAuditor auditSecurityEventService;
+
+        public ApplicationUserManagerBase(IUserStore<TUser> store, ISecurityEventAuditor auditSecurityEventService)
+            : base(store)
+        {
+            this.auditSecurityEventService = auditSecurityEventService;
+        }
+
+        public override async Task<IdentityResult> CreateAsync(TUser user)
+        {
+            IdentityResult result = await base.CreateAsync(user);
+
+            await auditSecurityEventService.UserCreated(user.Id);
+
+            return result;
+        }
+
+        public override async Task<IdentityResult> ResetPasswordAsync(string userId, string token, string newPassword)
+        {
+            IdentityResult result = await base.ResetPasswordAsync(userId, token, newPassword);
+
+            await auditSecurityEventService.PasswordReset(userId);
+
+            return result;
+        }
+
+        public override async Task<IdentityResult> ConfirmEmailAsync(string userId, string token)
+        {
+            IdentityResult result = await base.ConfirmEmailAsync(userId, token);
+
+            await auditSecurityEventService.EmailConfirmed(userId);
+
+            return result;
+        }
+    }
+}
---
+++ src/EA.Weee.Api/Identity/ISecurityEventAuditor.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Weee.Api.Identity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines events relating to security that can be audited.
+    /// </summary>
+    public interface ISecurityEventAuditor
+    {
+        Task LoginSuccess(string userId);
+
+        Task LoginFailure(string userName);
+
+        Task UserCreated(string userId);
+
+        Task PasswordReset(string userId);
+
+        Task EmailConfirmed(string userId);
+    }
+}
