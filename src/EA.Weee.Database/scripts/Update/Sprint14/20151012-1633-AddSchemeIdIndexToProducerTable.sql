28aa51a107cc4a0a904d4b510c615c415605a2aa
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-10-12 16:42:20 +0100

    Added [IX_Producer_SchemeId] index.

---
+++ src/EA.Weee.Database/scripts/Update/Sprint14/20151012-1633-AddSchemeIdIndexToProducerTable.sql
@@ -0,0 +1,10 @@
+﻿GO
+
+/****** Object:  Index [IX_Producer_SchemeId]    Script Date: 10/12/2015 16:39:00 ******/
+CREATE NONCLUSTERED INDEX [IX_Producer_SchemeId] ON [Producer].[Producer] 
+(
+	[SchemeId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+GO
+
+
