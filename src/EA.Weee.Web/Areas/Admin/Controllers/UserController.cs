64268becc025a2f25939af821335bbb84641b94b
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-10-12 14:11:56 +0100

    Prevent call to update user status if a user is updating his own details.

--- src/EA.Weee.Web/Areas/Admin/Controllers/UserController.cs
+++ src/EA.Weee.Web/Areas/Admin/Controllers/UserController.cs
@@ -120,7 +120,10 @@
 
                 if (model.IsCompetentAuthorityUser)
                 {
-                    await client.SendAsync(User.GetAccessToken(), new UpdateCompetentAuthorityUserStatus(model.Id, model.UserStatus));
+                    if (User.GetUserId() != model.UserId)
+                    {
+                        await client.SendAsync(User.GetAccessToken(), new UpdateCompetentAuthorityUserStatus(model.Id, model.UserStatus));
+                    }
                 }
                 else
                 {
