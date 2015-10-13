04f1ab325f5b3a9851de67211bd72e01defd2c4d
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-10-13 11:37:27 +0530

    Email template content changes and other content changes tasks
    Related Work Items: #35198, #35436, #35532, #35591, #35632

--- src/EA.Weee.Web/ViewModels/OrganisationRegistration/SearchOrganisationViewModel.cs
+++ src/EA.Weee.Web/ViewModels/OrganisationRegistration/SearchOrganisationViewModel.cs
@@ -5,7 +5,6 @@
     public class SearchOrganisationViewModel
     {
         [Required(ErrorMessage = "No organisation name supplied, unable to perform search")]
-        [Display(Name = "Search for your organisation on our system. If your organisation is not listed, create a new organisation.")]
         public string SearchedText { get; set; }
 
         public bool ShowPerformAnotherActivityLink { get; set; }
