44a35a56c9c5dfeeea57d02e5e7ab5db4858d147
Rick Powell <richard.powell@sfwltd.co.uk>
2015-10-15 10:27:24 +0100

    ProgressiveDisclosure change for screen reader users

--- src/EA.Weee.Web/RazorHelpers/WeeeGds.ProgressiveDisclosure.cs
+++ src/EA.Weee.Web/RazorHelpers/WeeeGds.ProgressiveDisclosure.cs
@@ -11,7 +11,7 @@
             this.gdsHelper = gdsHelper;
 
             var html = string.Format(
-                "<details><summary><span class=\"summary\">{0}</span></summary><div class=\"panel-indent\">", linkText);
+                "<details><summary><span class=\"summary\">{0}<span class=\"hidden-for-screen-reader\">This is an expandable link that will reveal more content upon activation</span></span></summary><div class=\"panel-indent\">", linkText);
 
             gdsHelper.HtmlHelper.ViewContext.Writer.Write(html);
         }
