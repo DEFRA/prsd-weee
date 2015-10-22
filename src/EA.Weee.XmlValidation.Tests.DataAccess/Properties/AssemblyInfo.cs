af3d31ac22db8d79673ffec3bc1b7ab72e50d015
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-10-22 10:11:31 +0100

    Remove references to SFW

--- src/EA.Weee.XmlValidation.Tests.DataAccess/Properties/AssemblyInfo.cs
+++ src/EA.Weee.XmlValidation.Tests.DataAccess/Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using Xunit;
 [assembly: AssemblyTitle("EA.Weee.XmlValidation.Tests.DataAccess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SFW Ltd")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EA.Weee.XmlValidation.Tests.DataAccess")]
-[assembly: AssemblyCopyright("Copyright © SFW Ltd 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
