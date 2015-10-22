df873ae82cc6eb5f16b0e3c861bdb5d545cac5fe
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-10-22 16:08:30 +0100

    #36325, removed <hr> tag and few more content changes  made on the call

--- src/EA.Weee.Web/Areas/Admin/ViewModels/Scheme/ConfirmSchemeRejection.cs
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Scheme/ConfirmSchemeRejection.cs
@@ -2,7 +2,7 @@
 {
     public static class ConfirmSchemeRejectionOptions
     {
-        public static string Yes = "Yes, reject this scheme";
+        public static string Yes = "Yes";
         public static string No = "No";  
     }
 }
