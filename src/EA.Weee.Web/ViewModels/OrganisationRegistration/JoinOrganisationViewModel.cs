91b8bb61f53dd01e899ea092fc6756b09117323b
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-10-23 15:31:09 +0530

    Content changes
    Related Work Items: #36488, #36489, #36497

--- src/EA.Weee.Web/ViewModels/OrganisationRegistration/JoinOrganisationViewModel.cs
+++ src/EA.Weee.Web/ViewModels/OrganisationRegistration/JoinOrganisationViewModel.cs
@@ -4,9 +4,11 @@ namespace EA.Weee.Web.ViewModels.OrganisationRegistration
     using System.ComponentModel.DataAnnotations;
     using Shared;
 
-    public class JoinOrganisationViewModel : RadioButtonStringCollectionViewModel
+    public class JoinOrganisationViewModel : YesNoChoiceViewModel
     {
         [Required]
         public Guid OrganisationId { get; set; }
+
+        public string OrganisationName { get; set; }
     }
 }
