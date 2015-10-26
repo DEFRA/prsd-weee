67cd795aa4b994ade67f3be962d6839f8dc81e73
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-10-26 15:40:48 +0530

    Updated error messages

--- src/EA.Weee.Web/ViewModels/NewUser/UserCreationViewModel.cs
+++ src/EA.Weee.Web/ViewModels/NewUser/UserCreationViewModel.cs
@@ -5,37 +5,37 @@
 
     public class UserCreationViewModel
     {
-        [Required(ErrorMessage = "First name is required.")]
+        [Required(ErrorMessage = "Enter your first name")]
         [Display(Name = "First name")]
         [StringLength(256)]
         [DataType(DataType.Text)]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Last name is required.")]
+        [Required(ErrorMessage = "Enter your last name")]
         [StringLength(256)]
         [DataType(DataType.Text)]
         [Display(Name = "Last name")]
         public string Surname { get; set; }
 
-        [Required(ErrorMessage = "Email is required.")]
+        [Required(ErrorMessage = "Enter your email address")]
         [EmailAddress(ErrorMessage = "The email address is not valid.")]
         [StringLength(256)]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
+        [Required(ErrorMessage = "Create a password for your account")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Retyping your password is required.")]
+        [Required(ErrorMessage = "Retype your password")]
         [DataType(DataType.Password)]
         [Display(Name = "Retype password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
-        [MustBeTrue(ErrorMessage = "Please confirm that you have read the terms and conditions.")]
+        [MustBeTrue(ErrorMessage = "Confirm that you've read and accepted the terms and conditions")]
         public bool TermsAndConditions { get; set; }
     }
 }
