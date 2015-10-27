9714c7443e4003d85e243e8262ac53007c856a23
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-10-27 11:19:33 +0000

    #36166 : Improve xml errors for data types.

--- src/EA.Weee.Core.Tests.Unit/Helpers/Xml/XmlErrorTranslatorTests.cs
+++ src/EA.Weee.Core.Tests.Unit/Helpers/Xml/XmlErrorTranslatorTests.cs
@@ -1,11 +1,7 @@
 ﻿namespace EA.Weee.Core.Tests.Unit.Helpers.Xml
 {
-    using EA.Weee.Core.Helpers.Xml;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.Xml.Linq;
+    using Core.Helpers.Xml;
     using Xunit;
 
     public class XmlErrorTranslatorTests
@@ -80,6 +76,58 @@
                 "which need to be encoded. For example, replace ampersand (&) characters with &amp;. (XML line 123)";
 
             Assert.Equal(expectedResult, result);
+        }
+
+        /// <summary>
+        /// Returns friendly error message for invalid data types
+        /// </summary>
+        [Theory]
+        [InlineData("123456", "boolean", "Boolean")]
+        [InlineData("2106.9", "integer", "Integer")]
+        [InlineData("27/10/2015", "date", "Date")]
+        [InlineData("12", "decimal", "Decimal")]
+        public void MakeFriendlyErrorMessage_InvalidDataType_ReturnsFriendlyMessage(string value, string dataType, string xmlType)
+        {
+            // Arrange
+            string xmlMessage = string.Format(
+                "The 'http://www.environment-agency.gov.uk/WEEE/XMLSchema:fieldName' element is invalid - The value '{0}' is invalid according to its datatype 'http://www.environment-agency.gov.uk/WEEE/XMLSchema:{1}' - The string '{0}' is not a valid {2} value.", value, dataType, xmlType);
+            XmlErrorTranslator translator = new XmlErrorTranslator();
+            XElement sender = new XElement("fieldName");
+            sender.Value = value;
+
+            // Act
+            string result = translator.MakeFriendlyErrorMessage(sender, xmlMessage, 12);
+
+            // Assert
+            string dataTypeMessage = GetFriendlyDataType(xmlType);
+            string expectedResult = string.Format("The value '{0}' supplied for field 'fieldName' doesn't match the required data type. The value '{0}' must be a {1}. (XML line 12)", value, dataTypeMessage);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        private string GetFriendlyDataType(string dataType)
+        {
+            string friendlyDataTypeMessage = string.Empty;
+            switch (dataType)
+            {
+                case "Integer":
+                    friendlyDataTypeMessage = "whole number";
+                    break;
+
+                case "Boolean":
+                    friendlyDataTypeMessage = "true, false, 0 or 1";
+                    break;
+
+                case "Decimal":
+                case "Float":
+                    friendlyDataTypeMessage = "decimal";
+                    break;
+
+                case "Date":
+                    friendlyDataTypeMessage = "date in the format YYYY-MM-DD";
+                    break;
+            }
+            return friendlyDataTypeMessage;
         }
     }
 }
