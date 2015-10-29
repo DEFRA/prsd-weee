bcaeaf6e1ca3eeb75af7e5308d4db647894b0d9d
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-10-29 10:56:56 +0000

    #36761 : annual turnover DB field update

---
+++ src/EA.Weee.Database/scripts/Update/Sprint16/20151028-0001-UpdateProducerAnnualturnoverDataType.sql
@@ -0,0 +1,10 @@
+﻿PRINT N'Altering [Producer].[Producer]...';
+GO
+ALTER TABLE [Producer].[Producer]
+  ALTER COLUMN AnnualTurnover [decimal](18, 8) NOT NULL 
+
+GO    
+PRINT N'Update complete.';
+
+GO
+
