2e258f46b57998272adbe2c29df81630b5d727d0
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-11-02 14:34:23 +0000

    36755: blank annual turnover gives error in XML document

---
+++ src/EA.Weee.Xml/Schemas/ProducerType.cs
@@ -0,0 +1,28 @@
+﻿namespace EA.Weee.Xml.Schemas
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.Xml.Serialization;
+    [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter",
+        Justification = "Reviewed")]
+    public partial class producerType
+    {
+        [XmlElement("annualTurnover")]
+        public string annualTurnoverField { get; set; }
+
+        /// <remarks/>
+        [XmlIgnore]
+        public float annualTurnover
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(annualTurnoverField))
+                {
+                    return 0F;
+                }
+                return float.Parse(annualTurnoverField);
+            }
+            set { annualTurnoverField = value.ToString(CultureInfo.InvariantCulture); }
+        }
+    }
+}
