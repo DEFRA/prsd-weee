da6889fcd3a4ea24caab662f48df7c15ddfcf335
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-11-03 15:14:34 +0000

    Google analytics tracking for acceptance criteria 1 - 6

--- src/EA.Weee.Web/RazorHelpers/WeeeGds.ActionLink.cs
+++ src/EA.Weee.Web/RazorHelpers/WeeeGds.ActionLink.cs
@@ -1,8 +1,12 @@
 ﻿namespace EA.Weee.Web.RazorHelpers
 {
     using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Web;
     using System.Web.Mvc;
     using System.Web.Mvc.Html;
+    using System.Web.Routing;
 
     public partial class WeeeGds<TModel>
     {
@@ -16,6 +20,33 @@
             var link = HtmlHelper.ActionLink(linkText, actionName, controllerName, routeValues, new { @target = "_blank", id });
 
             return new MvcHtmlString(label + link);
+        }
+
+        public MvcHtmlString ActionLinkWithEventTracking(string linkText, string actionName, string controllerName,
+                 string eventCategory, string eventAction, string eventLabel = null, RouteValueDictionary routeValues = null, IDictionary<string, object> htmlAttributes = null)
+        {
+            StringBuilder attributes = new StringBuilder();
+            if (htmlAttributes != null)
+            {
+                foreach (var item in htmlAttributes)
+                {
+                    attributes.AppendFormat(@"{0}=""{1}"" ", HtmlHelper.Encode(item.Key), HtmlHelper.Encode(item.Value));
+                }
+            }
+
+            if (string.IsNullOrEmpty(eventLabel))
+            {
+                attributes.AppendFormat(@"onclick=""ga('send', 'event', '{0}', '{1}');""", eventCategory, eventAction);
+            }
+            else
+            {
+                attributes.AppendFormat(@"onclick=""ga('send', 'event', '{0}', '{1}', '{2}');""", eventCategory, eventAction, eventLabel);
+            }
+
+            var action = UrlHelper.Action(actionName, controllerName, routeValues);
+            string link = string.Format(@"<a href=""{0}"" {1}>{2}</a>", action, attributes.ToString(), linkText);
+
+            return new MvcHtmlString(link);
         }
     }
 }
