c805d7253c3d4ce967007568ce2409d58d080e9a
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-11-06 18:40:19 +0530

    37059:Build UI for member detail download

--- src/EA.Weee.Web/Areas/Admin/Controllers/ReportsController.cs
+++ src/EA.Weee.Web/Areas/Admin/Controllers/ReportsController.cs
@@ -1,15 +1,19 @@
 ﻿namespace EA.Weee.Web.Areas.Admin.Controllers
 {
     using System;
+    using System.Linq;
     using System.Threading.Tasks;
     using System.Web.Mvc;
     using Api.Client;
     using Base;
     using Core.Shared;
     using Infrastructure;
+    using Prsd.Core.Web.ApiClient;
+    using Prsd.Core.Web.Mvc.Extensions;
     using ViewModels.Home;
     using ViewModels.Reports;
     using Weee.Requests.Admin;
+    using Weee.Requests.Shared;
 
     public class ReportsController : AdminController
     {
@@ -69,9 +73,55 @@
         }
 
         [HttpGet]
-        public ActionResult ProducerDetails()
+        public async Task<ActionResult> ProducerDetails()
         {
-            return View();
+            using (var client = apiClient())
+            {
+                try
+                {
+                    var allYears = await client.SendAsync(User.GetAccessToken(), new GetAllComplianceYears());
+                    var allSchemes = await client.SendAsync(User.GetAccessToken(), new GetAllApprovedSchemes());
+                    var appropriateauthorities = await client.SendAsync(User.GetAccessToken(), new GetUKCompetentAuthorities());
+
+                    ProducerDetailsViewModel model = new ProducerDetailsViewModel
+                    {
+                        ComplianceYears = new SelectList(allYears),
+                        SchemeNames = new SelectList(allSchemes, "Id", "SchemeName"),
+                        AppropriateAuthorities = new SelectList(appropriateauthorities, "Id", "Name")
+                    };
+                    return View("ProducerDetails", model);
+                }
+                catch (ApiBadRequestException ex)
+                {
+                    this.HandleBadRequest(ex);
+                    if (ModelState.IsValid)
+                    {
+                        throw;
+                    }
+                    return View("ProducerDetails");
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ProducerDetails(ProducerDetailsViewModel model)
+        {
+            using (var client = apiClient())
+            {
+                var allYears = await client.SendAsync(User.GetAccessToken(), new GetAllComplianceYears());
+                var allSchemes = await client.SendAsync(User.GetAccessToken(), new GetAllApprovedSchemes());
+                var appropriateauthorities = await client.SendAsync(User.GetAccessToken(), new GetUKCompetentAuthorities());
+
+                model.ComplianceYears = new SelectList(allYears);
+                model.SchemeNames = new SelectList(allSchemes, "Id", "SchemeName");
+                model.AppropriateAuthorities = new SelectList(appropriateauthorities, "Id", "Name");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View(model);
         }
     }
 }
