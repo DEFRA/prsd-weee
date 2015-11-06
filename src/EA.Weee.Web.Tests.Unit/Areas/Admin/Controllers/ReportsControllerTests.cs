51889c3137648b4e039d8ef3f77f680f6f6a0d64
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-11-06 18:40:19 +0530

    37059:Build UI for member detail download

--- src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/ReportsControllerTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/ReportsControllerTests.cs
@@ -1,13 +1,17 @@
 ﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.Controllers
 {
+    using System.Collections.Generic;
     using System.Web.Mvc;
     using Api.Client;
+    using Core.Scheme;
     using Core.Shared;
     using FakeItEasy;
     using Prsd.Core.Mediator;
     using Web.Areas.Admin.Controllers;
     using Web.Areas.Admin.ViewModels.Home;
     using Web.Areas.Admin.ViewModels.Reports;
+    using Weee.Requests.Admin;
+    using Weee.Requests.Shared;
     using Xunit;
 
     public class ReportsControllerTests
@@ -91,6 +95,38 @@
             var redirectToRouteResult = ((RedirectToRouteResult)result);
 
             Assert.Equal(action, redirectToRouteResult.RouteValues["action"]);
+        }
+
+        [Fact]
+        public async void HttpGet_ProducerDetails_ShouldReturnsProducerDetailsView()
+        {
+            var controller = ReportsController();
+
+            A.CallTo(() => apiClient.SendAsync(A<string>._, A<GetAllComplianceYears>._))
+                .Returns(new List<int> { 2015, 2016 });
+
+            A.CallTo(() => apiClient.SendAsync(A<string>._, A<GetAllApprovedSchemes>._))
+                .Returns(new List<SchemeData> { new SchemeData() });
+
+            A.CallTo(() => apiClient.SendAsync(A<string>._, A<GetUKCompetentAuthorities>._))
+                .Returns(new List<UKCompetentAuthorityData> { new UKCompetentAuthorityData() });
+
+            var result = await controller.ProducerDetails();
+
+            var viewResult = ((ViewResult)result);
+            Assert.Equal("ProducerDetails", viewResult.ViewName);
+        }
+
+        [Fact]
+        public async void HttpPost_ProducerDetails_ModelIsInvalid_ShouldRedirectViewWithError()
+        {
+            var controller = ReportsController();
+            controller.ModelState.AddModelError("Key", "Any error");
+
+            var result = await controller.ProducerDetails(new ProducerDetailsViewModel());
+
+            Assert.IsType<ViewResult>(result);
+            Assert.False(controller.ModelState.IsValid);
         }
 
         private ReportsController ReportsController()
---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Reports/ProducerDetailsViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Reports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+    using Core.Admin;
+
+    public class ProducerDetailsViewModel
+    {
+         [Required(ErrorMessage = "Compliance year is required.")]
+        [DisplayName("Compliance year")]
+        public int SelectedYear { get; set; }
+
+        [DisplayName("Scheme name")]
+        public Guid? SelectedScheme { get; set; }
+
+        [DisplayName("AA")]
+        public Guid? SelectedAA { get; set; }
+       
+        public IEnumerable<SelectListItem> ComplianceYears { get; set; }
+
+        public IEnumerable<SelectListItem> SchemeNames { get; set; }
+
+        public IEnumerable<SelectListItem> AppropriateAuthorities { get; set; }
+    }
+}
