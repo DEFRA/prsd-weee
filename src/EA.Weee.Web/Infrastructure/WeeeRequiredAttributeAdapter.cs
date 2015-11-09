5c7df4cb4513a083954c0ab3fe44be47d3b1a75f
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-11-09 15:55:36 +0000

    Default error message for the required attribute validator

---
+++ src/EA.Weee.Web/Infrastructure/WeeeRequiredAttributeAdapter.cs
@@ -0,0 +1,28 @@
+﻿namespace EA.Weee.Web.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class WeeeRequiredAttributeAdapter : RequiredAttributeAdapter
+    {
+        public WeeeRequiredAttributeAdapter(ModelMetadata metadata, ControllerContext context, RequiredAttribute attribute)
+        : base(metadata, context, attribute)
+        {
+            if (string.IsNullOrEmpty(attribute.ErrorMessage) &&
+                !string.IsNullOrEmpty(metadata.DisplayName))
+            {
+                string lower = metadata.DisplayName.ToLower();
+
+                attribute.ErrorMessage = string.Format("Enter {0} {1}.", IsVowel(lower[0]) ? "an" : "a", lower);
+            }
+        }
+        private static bool IsVowel(char character)
+        {
+            return new[] { 'a', 'e', 'i', 'o', 'u' }.Contains(char.ToLower(character));
+        }
+    }
+}
