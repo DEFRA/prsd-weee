ab152944275aa3138ac9355b866fe28668b6a722
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-11-09 13:03:11 +0000

    merged changes for reports UI

---
+++ src/EA.Weee.RequestHandlers/Shared/GetUKCompetentAuthorityIdHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace EA.Weee.RequestHandlers.Shared
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Shared;
+    using DataAccess;
+    using Domain;
+    using FluentValidation.Internal;
+    using Prsd.Core.Mapper;
+    using Prsd.Core.Mediator;
+    using Requests.Shared;
+
+    internal class GetUKCompetentAuthorityByIdHandler :
+        IRequestHandler<GetUKCompetentAuthorityById, UKCompetentAuthorityData>
+    {
+        private readonly WeeeContext context;
+        private readonly IMap<UKCompetentAuthority, UKCompetentAuthorityData> mapper;
+
+        public GetUKCompetentAuthorityByIdHandler(WeeeContext context, IMap<UKCompetentAuthority, UKCompetentAuthorityData> mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<UKCompetentAuthorityData> HandleAsync(GetUKCompetentAuthorityById request)
+        {
+            var ukCompetentAuthority =
+                await context.UKCompetentAuthorities.FindAsync(request.Id);
+
+            if (ukCompetentAuthority == null)
+            {
+                string message = string.Format("No authorised authority was found with id \"{0}\".", request.Id);
+                throw new ArgumentException(message);
+            }
+            return mapper.Map(ukCompetentAuthority);
+        }
+    }
+}
