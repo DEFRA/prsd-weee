b353c6e616d29bd2297f290e87fd3ddc743b1442
Rick Powell <richard.powell@sfwltd.co.uk>
2015-11-13 09:31:34 +0000

    New nullable xml generic type applied to annual turnover.

--- src/EA.Weee.Xml/Schemas/producerType.cs
+++ src/EA.Weee.Xml/Schemas/producerType.cs
@@ -1,28 +1,17 @@
 ﻿namespace EA.Weee.Xml.Schemas
 {
+    using System.ComponentModel;
     using System.Diagnostics.CodeAnalysis;
     using System.Globalization;
     using System.Xml.Serialization;
+    using CustomTypes;
+
     [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter",
         Justification = "Reviewed")]
     public partial class producerType
     {
-        [XmlElement("annualTurnover")]
-        public string annualTurnoverString { get; set; }
-
         /// <remarks/>
-        [XmlIgnore]
-        public decimal annualTurnover
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(annualTurnoverString))
-                {
-                    return 0M;
-                }
-                return decimal.Parse(annualTurnoverString);
-            }
-            set { annualTurnoverString = value.ToString(CultureInfo.InvariantCulture); }
-        }
+        [XmlElement("annualTurnover", typeof(XmlNullable<decimal>))]
+        public XmlNullable<decimal> annualTurnover { get; set; }
     }
 }
