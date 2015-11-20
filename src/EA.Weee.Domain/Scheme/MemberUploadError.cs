c8a95c83ed958afd2cd010648649bec1b4306f8f
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-11-20 17:21:02 +0530

    38185:Order XML errors by line number
    Related Work Items: #38185

--- src/EA.Weee.Domain/Scheme/MemberUploadError.cs
+++ src/EA.Weee.Domain/Scheme/MemberUploadError.cs
@@ -7,17 +7,17 @@
     {
         public ErrorLevel ErrorLevel { get; private set; }
         public MemberUploadErrorType ErrorType { get; private set; }
-
         public string Description { get; private set; }
-
+        public int LineNumber { get; private set; }
         public Guid MemberUploadId { get; private set; }
         public virtual MemberUpload MemberUpload { get; private set; }
-
-        public MemberUploadError(ErrorLevel errorLevel, MemberUploadErrorType errorType, string description)
+        
+        public MemberUploadError(ErrorLevel errorLevel, MemberUploadErrorType errorType, string description, int lineNumber = 0)
         {
             ErrorType = errorType;
             ErrorLevel = errorLevel;
             Description = description;
+            LineNumber = lineNumber;
         }
 
         protected MemberUploadError()
