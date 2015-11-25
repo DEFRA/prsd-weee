6209da48b22995e70a2cd4a1cde7facb775fa611
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-11-25 09:46:18 +0000

    Changes to the schema to correct the path expressions for the unique constraints. Also changed regulation element names.

---
+++ src/EA.Weee.Xml.Tests.Unit/DataReturn/XmlSchemaValidationTests.cs
@@ -0,0 +1,117 @@
+﻿namespace EA.Weee.Xml.Tests.Unit.DataReturn
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml;
+    using System.Xml.Linq;
+    using System.Xml.Schema;
+    using Xunit;
+
+    public class XmlSchemaValidationTests
+    {
+        [Fact]
+        public void DataReturnSchemaValidation_CollectedFromDCF_ContainsDuplicateCategoryObligationType_ReturnsValidationError()
+        {
+            var result = new XmlFileLoad().ValidateDataReturnXmlWithSingleResult(@"DataReturn\XmlFiles\v3-CollectedFromDCF-DuplicateCategoryObligationType.xml");
+
+            Assert.Equal(XmlSeverityType.Error, result.Severity);
+            Assert.Contains("uniqueCategoryCollectedFromDCF", result.Message);
+        }
+
+        [Fact]
+        public void DataReturnSchemaValidation_DeliveredToATF_ContainsDuplicateCategoryObligationType_ReturnsValidationError()
+        {
+            var result = new XmlFileLoad().ValidateDataReturnXmlWithSingleResult(@"DataReturn\XmlFiles\v3-DeliveredToATF-DuplicateCategoryObligationType.xml");
+
+            Assert.Equal(XmlSeverityType.Error, result.Severity);
+            Assert.Contains("uniqueCategoryDeliveredToATF", result.Message);
+        }
+
+        [Fact]
+        public void DataReturnSchemaValidation_DeliveredToAE_ContainsDuplicateCategoryObligationType_ReturnsValidationError()
+        {
+            var result = new XmlFileLoad().ValidateDataReturnXmlWithSingleResult(@"DataReturn\XmlFiles\v3-DeliveredToAE-DuplicateCategoryObligationType.xml");
+
+            Assert.Equal(XmlSeverityType.Error, result.Severity);
+            Assert.Contains("uniqueCategoryObligationDeliveredToAE", result.Message);
+        }
+
+        [Fact]
+        public void DataReturnSchemaValidation_B2CWEEEFromDistributors_ContainsDuplicateCategoryObligationType_ReturnsValidationError()
+        {
+            var result = new XmlFileLoad().ValidateDataReturnXmlWithSingleResult(@"DataReturn\XmlFiles\v3-B2CWEEEFromDistributors-DuplicateCategoryObligationType.xml");
+
+            Assert.Equal(XmlSeverityType.Error, result.Severity);
+            Assert.Contains("uniqueCategoryObligationB2CWEEEFromDistributors", result.Message);
+        }
+
+        [Fact]
+        public void DataReturnSchemaValidation_B2CWEEEFromFinalHolders_ContainsDuplicateCategoryObligationType_ReturnsValidationError()
+        {
+            var result = new XmlFileLoad().ValidateDataReturnXmlWithSingleResult(@"DataReturn\XmlFiles\v3-B2CWEEEFromFinalHolders-DuplicateCategoryObligationType.xml");
+
+            Assert.Equal(XmlSeverityType.Error, result.Severity);
+            Assert.Contains("uniqueCategoryObligationB2CWEEEFromFinalHolders", result.Message);
+        }
+
+        [Fact]
+        public void DataReturnSchemaValidation_ProducerList_ContainsDuplicateCategoryObligationType_ReturnsValidationError()
+        {
+            var result = new XmlFileLoad().ValidateDataReturnXmlWithSingleResult(@"DataReturn\XmlFiles\v3-ProducerList-DuplicateCategoryObligationType.xml");
+
+            Assert.Equal(XmlSeverityType.Error, result.Severity);
+            Assert.Contains("uniqueCategoryObligationProducerReturn", result.Message);
+        }
+
+        private class XmlFileLoad
+        {
+            private const string schemaFile = @"DataReturn\v3schema.xsd";
+            private static XmlSchemaSet schemaSet;
+
+            private static void LoadSchemas()
+            {
+                if (schemaSet == null)
+                {
+                    XmlSchema schema;
+                    using (var schemaFileStream = File.Open(schemaFile, FileMode.Open))
+                    {
+                        schema = XmlSchema.Read(schemaFileStream, null);
+                    }
+
+                    schemaSet = new XmlSchemaSet();
+                    schemaSet.Add(schema);
+                }
+            }
+
+            public ValidationEventArgs ValidateDataReturnXmlWithSingleResult(string filename)
+            {
+                var result = ValidateDataReturnXml(filename);
+
+                return result.Single();
+            }
+
+            public List<ValidationEventArgs> ValidateDataReturnXml(string filename)
+            {
+                LoadSchemas();
+
+                var validationEventArgs = new List<ValidationEventArgs>();
+
+                using (var reader = XmlReader.Create(filename))
+                {
+                    var xdoc = XDocument.Load(reader, LoadOptions.SetLineInfo);
+                    xdoc.Validate(schemaSet,
+                        (o, args) =>
+                        {
+                            validationEventArgs.Add(args);
+                        });
+                }
+
+                return validationEventArgs;
+            }
+        }
+    }
+}
