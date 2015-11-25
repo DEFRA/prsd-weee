9385d6f45f6e11f615cff06bddcf73fcd6f8dea9
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-11-25 22:12:14 +0000

    Updated database.

---
+++ src/EA.Weee.Database/scripts/Update/Sprint17-ProducerRefactoring/20151125-1717-UpdateDependentTables.sql
@@ -0,0 +1,78 @@
+﻿/*
+ * The brand name and SIC code tables depend on the producer table.
+ * This script updates these two tables to reference the new producer submission table.
+ */
+
+-- Create and populate producer submission ID column.
+ALTER TABLE [Producer].[BrandName]
+ADD [ProducerSubmissionId] UNIQUEIDENTIFIER NULL
+GO
+
+UPDATE [Producer].[BrandName]
+SET [ProducerSubmissionId] = [ProducerId]
+GO
+
+ALTER TABLE [Producer].[BrandName]
+ALTER COLUMN [ProducerSubmissionId] UNIQUEIDENTIFIER NOT NULL
+GO
+
+-- Create new foreign key and drop existing foreign key.
+ALTER TABLE [Producer].[BrandName] WITH CHECK
+ADD CONSTRAINT [FK_BrandName_ProducerSubmission] FOREIGN KEY([ProducerSubmissionId])
+REFERENCES [Producer].[ProducerSubmission] ([Id])
+GO
+
+ALTER TABLE [Producer].[BrandName]
+CHECK CONSTRAINT [FK_BrandName_ProducerSubmission]
+GO
+
+ALTER TABLE [Producer].[BrandName]
+DROP CONSTRAINT [FK_BrandNameList_Producer]
+GO
+
+-- Drop existing column.
+ALTER TABLE [Producer].[BrandName]
+DROP COLUMN [ProducerId]
+GO
+
+
+-- Create and populate producer submission ID column.
+ALTER TABLE [Producer].[SICCode]
+ADD [ProducerSubmissionId] UNIQUEIDENTIFIER NULL
+GO
+
+UPDATE [Producer].[SICCode]
+SET [ProducerSubmissionId] = [ProducerId]
+GO
+
+ALTER TABLE [Producer].[SICCode]
+ALTER COLUMN [ProducerSubmissionId] UNIQUEIDENTIFIER NOT NULL
+GO
+
+-- Create new foreign key and drop existing foreign key.
+ALTER TABLE [Producer].[SICCode] WITH CHECK
+ADD CONSTRAINT [FK_SICCode_ProducerSubmission] FOREIGN KEY([ProducerSubmissionId])
+REFERENCES [Producer].[ProducerSubmission] ([Id])
+GO
+
+ALTER TABLE [Producer].[SICCode]
+CHECK CONSTRAINT [FK_SICCode_ProducerSubmission]
+GO
+
+ALTER TABLE [Producer].[SICCode]
+DROP CONSTRAINT [FK_SICCodeList_Producer]
+GO
+
+-- Create new index and drop existing index.
+CREATE NONCLUSTERED INDEX [IX_SICCode_ProducerSubmissionId] ON [Producer].[SICCode] 
+(
+	[ProducerSubmissionId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+GO
+
+DROP INDEX [IX_SICCode_ProducerId] ON [Producer].[SICCode] WITH ( ONLINE = OFF )
+
+-- Drop existing column.
+ALTER TABLE [Producer].[SICCode]
+DROP COLUMN [ProducerId]
+GO
---
+++ src/EA.Weee.Database/scripts/Update/Sprint17-ProducerRefactoring/20151125-1815-Update-spgCSVDataByOrganisationIdAndComplianceYear.sql
@@ -0,0 +1,111 @@
+﻿/*
+ * This script updates the stored procedure 'spgCSVDataByOrganisationIdAndComplianceYear' to reference
+ * the new registered producer and producer submission tables.
+ */
+
+GO
+/*
+ * Returns data about all producers currently registered
+ * with the specified organisation in the specified year.
+ * This data is suitable for populating the CSV file
+ * which may be downloaded by users associated with the
+ * organisation.
+ */
+ALTER PROCEDURE [Producer].[spgCSVDataByOrganisationIdAndComplianceYear]
+	@OrganisationId UNIQUEIDENTIFIER,
+	@ComplianceYear INT
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	SELECT
+		COALESCE(PBC.Name, PBP.Name, '') AS 'OrganisationName',
+		
+		PS.TradingName AS 'TradingName',
+		
+		RP.ProducerRegistrationNumber AS 'RegistrationNumber',
+		
+		COALESCE(PBC.CompanyNumber, '') AS 'CompanyNumber',
+		
+		CASE CBA.[ChargeBand]
+			WHEN 0 THEN 'A'
+			WHEN 1 THEN 'B'
+			WHEN 2 THEN 'C'
+			WHEN 3 THEN 'D'
+			WHEN 4 THEN 'E'
+			ELSE ''
+		END AS 'ChargeBand',
+		
+		PS.[ObligationType],
+
+		P_First.UpdatedDate AS 'DateRegistered',
+		
+		PS.UpdatedDate AS 'DateAmended',
+		
+		CASE WHEN PS.AuthorisedRepresentativeId IS NOT NULL AND PAR.OverseasContactId IS NOT NULL AND PC.Email IS NOT NULL AND PC.Email != ''
+			THEN 'Yes'
+			ELSE 'No'
+		END AS 'AuthorisedRepresentative',
+		
+		COALESCE(PAR.OverseasProducerName, '') AS 'OverseasProducer'
+	FROM
+		Producer.RegisteredProducer RP
+	INNER JOIN
+		PCS.[Scheme] S
+			ON RP.[SchemeId] = S.[Id]
+	INNER JOIN
+		Producer.ProducerSubmission PS
+			ON RP.[CurrentSubmissionId] = PS.[Id]
+	LEFT JOIN
+		[Lookup].[ChargeBandAmount] CBA
+			ON PS.[ChargeBandAmountId] = CBA.[Id]
+	LEFT JOIN
+		Producer.Business PB
+			ON PS.ProducerBusinessId = PB.Id
+	LEFT JOIN
+		Producer.Company PBC
+			ON PB.CompanyId = PBC.Id
+	LEFT JOIN
+		Producer.Partnership PBP
+			ON PB.PartnershipId = PBP.Id
+	LEFT JOIN
+		Producer.AuthorisedRepresentative PAR
+			ON PS.AuthorisedRepresentativeId = PAR.Id
+	LEFT JOIN Producer.Contact PC
+			ON PAR.OverseasContactId = PC.Id
+	INNER JOIN
+		PCS.MemberUpload MU
+			ON PS.MemberUploadId = MU.Id
+	INNER JOIN
+		(
+			SELECT
+				MU.ComplianceYear,
+				PS.RegisteredProducerId,
+				PS.UpdatedDate,
+				ROW_NUMBER() OVER
+				(
+					PARTITION BY
+						PS.RegisteredProducerId
+					ORDER BY PS.UpdatedDate
+				) AS RowNumber
+			FROM
+				Producer.ProducerSubmission PS
+			INNER JOIN
+				PCS.MemberUpload MU
+					ON PS.MemberUploadId = MU.Id
+			WHERE
+				MU.IsSubmitted = 1
+			AND
+				MU.OrganisationId = @OrganisationId
+		) P_First
+			ON PS.RegisteredProducerId = P_First.RegisteredProducerId
+			AND P_First.RowNumber = 1
+	WHERE
+		S.[OrganisationId] = @OrganisationId
+	AND
+		RP.[ComplianceYear] = @ComplianceYear
+	ORDER BY
+		COALESCE(PBC.Name, PBP.Name, '')
+END
+GO
