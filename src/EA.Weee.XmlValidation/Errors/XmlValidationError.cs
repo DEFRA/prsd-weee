a2c6fd65432411e0d263219a0fb34565767bfd4b
Rick Powell <richard.powell@sfwltd.co.uk>
2015-11-26 10:03:19 +0000

    New generic xml error and xml validation error types

---
+++ src/EA.Weee.XmlValidation/Errors/XmlValidationError.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.XmlValidation.Errors
+{
+    using Core.Shared;
+
+    public class XmlValidationError
+    {
+        public ErrorLevel ErrorLevel { get; private set; }
+
+        public XmlErrorType ErrorType { get; private set; }
+
+        public string Message { get; private set; }
+
+        public XmlValidationError(ErrorLevel errorLevel, XmlErrorType errorType, string message)
+        {
+            ErrorLevel = errorLevel;
+            ErrorType = errorType;
+            Message = message;
+        }
+    }
+}
