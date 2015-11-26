1d7a536975bf9c9104c7d23a025b2c0e85d85879
Rick Powell <richard.powell@sfwltd.co.uk>
2015-11-26 09:02:31 +0000

    Update organisation field lengths

---
+++ src/EA.Weee.Database/scripts/Update/Sprint17/20151125-1530-DataStandards-UpdateOrganisationFieldLengths.sql
@@ -0,0 +1,8 @@
+ALTER TABLE [Producer].[MigratedProducer] 
+ALTER COLUMN ProducerName NVARCHAR(256)
+
+ALTER TABLE [Organisation].[Organisation]
+ALTER COLUMN TradingName NVARCHAR(256)
+
+ALTER TABLE [Organisation].[Organisation]
+ALTER COLUMN Name NVARCHAR(256)
