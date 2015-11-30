3bf3f1d824f7919d299766a3efe719ba2d2457df
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-11-30 13:13:41 +0530

    Added some more unit tests for producer public register report sp.

--- src/EA.Weee.DataAccess.Tests.DataAccess/StoredProcedure/SpgProducerPublicRegisterCSVDataByComplianceYearTests.cs
+++ src/EA.Weee.DataAccess.Tests.DataAccess/StoredProcedure/SpgProducerPublicRegisterCSVDataByComplianceYearTests.cs
@@ -37,7 +37,27 @@
                 producer1.IsCurrentForComplianceYear = true;
                 producer1.Business.Company.Contact1.Telephone = "55 5555 5555";
                 producer1.Business.Company.Contact1.Email = "email@test.com";
-                
+
+                Scheme scheme2 = helper.CreateScheme();
+
+                scheme2.OrganisationId = new Guid("84729d95-3c26-4c37-9730-f25fa2b6dbfc");
+                scheme2.Organisation.Id = new Guid("84729d95-3c26-4c37-9730-f25fa2b6dbfc");
+                scheme2.SchemeName = "A test scheme name";
+
+                scheme2.Organisation.Name = "PCS operator name 1";
+                scheme2.Organisation.BusinessAddressId = new Guid("ecdf60fc-1576-43e2-9bb4-e6c9dc9d721d");
+                scheme2.Organisation.Address = helper.CreateOrganisationAddress();
+                scheme2.Organisation.Address.Id = new Guid("ecdf60fc-1576-43e2-9bb4-e6c9dc9d721d");
+
+                MemberUpload memberUpload2 = helper.CreateMemberUpload(scheme2);
+                memberUpload2.ComplianceYear = 2017;
+                memberUpload2.IsSubmitted = true;
+
+                Producer producer2 = helper.CreateProducerAsCompany(memberUpload2, "WEE/11ZZZZ11");
+                producer2.IsCurrentForComplianceYear = true;
+                producer2.Business.Company.Contact1.Telephone = "55 5555 5555";
+                producer2.Business.Company.Contact1.Email = "email@test.com";
+
                 db.Model.SaveChanges();
 
                 // Act
@@ -48,7 +68,7 @@
                 Assert.NotNull(results);
                 Assert.True(results.TrueForAll(i => i.ComplianceYear == 2016));
 
-                Assert.False(results.Any(i => i.PRN == "WEE/99ZZZZ99" && i.ComplianceYear == 2017));
+                Assert.False(results.Any(i => i.PRN == "WEE/11ZZZZ11" && i.ComplianceYear == 2017));
 
                 var result = results.SingleOrDefault(i => i.PRN == "WEE/99ZZZZ99" && i.ComplianceYear == 2016);
 
@@ -172,6 +192,74 @@
 
                 Assert.NotNull(result);
                 Assert.Equal(result.ProducerName, producer1.Business.Partnership.Name);
+            }
+        }
+
+        [Fact]
+        public async Task Execute_ListOfProducers_ReturnsRecordsInCurrectOrder()
+        {
+            using (DatabaseWrapper db = new DatabaseWrapper())
+            {
+                // Arrange
+                ModelHelper helper = new ModelHelper(db.Model);
+
+                Scheme scheme1 = helper.CreateScheme();
+
+                scheme1.OrganisationId = new Guid("4EEE5942-01B2-4A4D-855A-34DEE1BBBF26");
+                scheme1.Organisation.Id = new Guid("4EEE5942-01B2-4A4D-855A-34DEE1BBBF26");
+                scheme1.SchemeName = "Lamborghini";
+
+                scheme1.Organisation.Name = "PCS operator name";
+                scheme1.Organisation.BusinessAddressId = new Guid("b58e9cb2-b97e-4141-ad32-73c70284fc77");
+                scheme1.Organisation.Address = helper.CreateOrganisationAddress();
+                scheme1.Organisation.Address.Id = new Guid("b58e9cb2-b97e-4141-ad32-73c70284fc77");
+
+                MemberUpload memberUpload1 = helper.CreateMemberUpload(scheme1);
+                memberUpload1.ComplianceYear = 2016;
+                memberUpload1.IsSubmitted = true;
+
+                Producer producer1 = helper.CreateProducerAsCompany(memberUpload1, "WEE/99ZZZZ99");
+                producer1.IsCurrentForComplianceYear = true;
+                producer1.Business.Company.Name = "A company name";
+
+                Producer producer2 = helper.CreateProducerAsCompany(memberUpload1, "WEE/88ZZZZ88");
+                producer2.IsCurrentForComplianceYear = true;
+                producer2.Business.Company.Name = "B company name";
+
+                Scheme scheme2 = helper.CreateScheme();
+
+                scheme2.OrganisationId = new Guid("84729d95-3c26-4c37-9730-f25fa2b6dbfc");
+                scheme2.Organisation.Id = new Guid("84729d95-3c26-4c37-9730-f25fa2b6dbfc");
+                scheme2.SchemeName = "Aston Martin";
+
+                scheme2.Organisation.Name = "PCS operator name 1";
+                scheme2.Organisation.BusinessAddressId = new Guid("ecdf60fc-1576-43e2-9bb4-e6c9dc9d721d");
+                scheme2.Organisation.Address = helper.CreateOrganisationAddress();
+                scheme2.Organisation.Address.Id = new Guid("ecdf60fc-1576-43e2-9bb4-e6c9dc9d721d");
+
+                MemberUpload memberUpload2 = helper.CreateMemberUpload(scheme2);
+                memberUpload2.ComplianceYear = 2016;
+                memberUpload2.IsSubmitted = true;
+
+                Producer producer3 = helper.CreateProducerAsCompany(memberUpload2, "WEE/11ZZZZ11");
+                producer3.IsCurrentForComplianceYear = true;
+                producer3.Business.Company.Name = "C company name";
+
+                Producer producer4 = helper.CreateProducerAsCompany(memberUpload2, "WEE/22ZZZZ22");
+                producer4.IsCurrentForComplianceYear = true;
+                producer4.Business.Company.Name = "D company name";
+
+                db.Model.SaveChanges();
+
+                // Act
+                List<ProducerPublicRegisterCSVData> results =
+                    await db.StoredProcedures.SpgProducerPublicRegisterCSVDataByComplianceYear(2016);
+
+                // Assert
+                Assert.NotNull(results);
+                Assert.True(results.FindIndex(i => i.SchemeName == "Aston Martin" && i.ProducerName == "C company name") < results.FindIndex(i => i.SchemeName == "Aston Martin" && i.ProducerName == "D company name"));
+                Assert.True(results.FindIndex(i => i.SchemeName == "Lamborghini" && i.ProducerName == "A company name") < results.FindIndex(i => i.SchemeName == "Lamborghini" && i.ProducerName == "B company name"));
+                Assert.True(results.FindIndex(i => i.SchemeName == "Aston Martin" && i.ProducerName == "C company name") < results.FindIndex(i => i.SchemeName == "Lamborghini" && i.ProducerName == "B company name"));
             }
         }
     }
