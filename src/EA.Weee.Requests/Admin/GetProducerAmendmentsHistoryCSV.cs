88a8d495cbdd26c8988cb837e93a29e0c130f140
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-12-03 14:17:57 +0530

    38692:Make organisation name cache duration configurable
    Related Work Items: #38692

---
+++ src/EA.Weee.Requests/Admin/GetProducerAmendmentsHistoryCSV.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Requests.Admin
+{
+    using Core.Admin;
+    using Prsd.Core.Mediator;
+
+    public class GetProducerAmendmentsHistoryCSV : IRequest<CSVFileData>
+    {
+        public string PRN { get; private set; }
+
+        public GetProducerAmendmentsHistoryCSV(string prn)
+        {
+            PRN = prn;
+        }
+    }
+}
