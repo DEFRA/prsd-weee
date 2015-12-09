3ccb01ed6273b1aeed23a15fb431858ef672724b
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-09 16:41:55 +0000

    Added skeleton UI for creating test data return XML files.

---
+++ src/EA.Weee.RequestHandlers/DataReturns/CreateTestXmlFile/CreateTestXmlFileHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Weee.RequestHandlers.DataReturns.CreateTestXmlFile
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Prsd.Core;
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.Core.Shared;
+    using EA.Weee.Requests.DataReturns;
+
+    public class CreateTestXmlFileHandler : IRequestHandler<CreateTestXmlFile, FileInfo>
+    {
+        public async Task<FileInfo> HandleAsync(Requests.DataReturns.CreateTestXmlFile message)
+        {
+            string fileName = string.Format("PCS Data Return XML - {0:yyyy MM dd HH mm ss}.xml", SystemTime.UtcNow);
+
+            // TODO: Use the specified settings to generate XML.
+
+            FileInfo file = new FileInfo(fileName, new byte[1]);
+
+            return await Task.FromResult(file);
+        }
+    }
+}
