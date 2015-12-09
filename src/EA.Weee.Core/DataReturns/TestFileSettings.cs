57a83fe6995eb7f0e7283cf106145e2f824894fd
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-09 16:41:55 +0000

    Added skeleton UI for creating test data return XML files.

---
+++ src/EA.Weee.Core/DataReturns/TestFileSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.Core.DataReturns
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TestFileSettings
+    {
+        public Guid OrganisationID { get; private set; }
+
+        public Quarter Quarter { get; private set; }
+
+        public TestFileSettings(Guid organisationID, Quarter quarter)
+        {
+            OrganisationID = organisationID;
+            Quarter = quarter;
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Test/ViewModels/CreatePcsDataReturnXmlFile/SpecifyOptionsViewModel.cs
@@ -0,0 +1,43 @@
+﻿namespace EA.Weee.Web.Areas.Test.ViewModels.CreatePcsDataReturnXmlFile
+{
+    using System;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using Core.DataReturns;
+
+    public class SpecifyOptionsViewModel
+    {
+        [Required]
+        public Guid OrganisationID { get; set; }
+
+        [Required]
+        [Range(2016, 2099, ErrorMessage = "The compliance year must be between 2016 and 2099.")]
+        [DisplayName("Compliance Year")]
+        public int ComplianceYear { get; set; }
+
+        [Required]
+        [DisplayName("Quarter")]
+        public QuarterType Quarter { get; set; }
+
+        // TODO: Add options.
+
+        public SpecifyOptionsViewModel()
+        {
+            // If acceptable, default to the current year.
+            int year = DateTime.UtcNow.Year;
+            
+            if (year < 2016)
+            {
+                year = 2016;
+            }
+
+            if (year > 2099)
+            {
+                year = 2099;
+            }
+
+            ComplianceYear = year;
+            Quarter = QuarterType.Q1;
+        }
+    }
+}
