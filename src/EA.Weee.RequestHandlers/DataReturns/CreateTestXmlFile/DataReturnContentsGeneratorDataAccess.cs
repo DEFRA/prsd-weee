90e6701488138a99c0d62369b6a1f2a3e8913abf
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-09 20:43:34 +0000

    Added implementation for generating a random data return.

--- src/EA.Weee.RequestHandlers/DataReturns/CreateTestXmlFile/DataReturnContentsGeneratorDataAccess.cs
+++ src/EA.Weee.RequestHandlers/DataReturns/CreateTestXmlFile/DataReturnContentsGeneratorDataAccess.cs
@@ -6,6 +6,7 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Domain.Producer;
     using EA.Weee.DataAccess;
     using EA.Weee.Domain.Scheme;
 
@@ -20,7 +21,20 @@
 
         public async Task<Domain.Scheme.Scheme> FetchSchemeAsync(Guid organisationID)
         {
-            return await context.Schemes.SingleAsync(s => s.OrganisationId == organisationID);
+            return await context
+                .Schemes
+                .SingleAsync(s => s.OrganisationId == organisationID);
+        }
+
+        public async Task<IList<RegisteredProducer>> FetchRegisteredProducersAsync(Scheme scheme, int year)
+        {
+            return await context
+                .RegisteredProducers
+                .Where(rp => rp.Scheme.Id == scheme.Id)
+                .Where(rp => rp.ComplianceYear == year)
+                .Where(rp => rp.CurrentSubmission != null)
+                .Include(rp => rp.CurrentSubmission)
+                .ToListAsync();
         }
     }
 }
