4018a3f3dc5f7bf4c6cab429556a5304f1e83b77
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-09 18:56:01 +0000

    Added skeletal request handler.

---
+++ src/EA.Weee.RequestHandlers/DataReturns/CreateTestXmlFile/XmlGenerator.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.RequestHandlers.DataReturns.CreateTestXmlFile
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml.Linq;
+    using EA.Weee.Domain.DataReturns;
+
+    public class XmlGenerator : IXmlGenerator
+    {
+        public XDocument GenerateXml(DataReturnContents dataReturnContents)
+        {
+            // TODO: Generate XML from the data return contents.
+
+            return XDocument.Parse("<xml>TEST</xml>");
+        }
+    }
+}
