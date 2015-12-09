0467e12699b622b8af35c60e9990c6ecd26474e4
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-09 18:55:31 +0000

    Added domain objects for data returns.

---
+++ src/EA.Weee.Domain/DataReturns/QuarterType.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Domain.DataReturns
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum QuarterType
+    {
+        Q1 = 0,
+        Q2 = 1,
+        Q3 = 2,
+        Q4 = 3
+    }
+}
---
+++ src/EA.Weee.Domain/DataReturns/ReturnItem.cs
@@ -0,0 +1,33 @@
+﻿namespace EA.Weee.Domain.DataReturns
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using EA.Prsd.Core.Domain;
+
+    public class ReturnItem : Entity
+    {
+        public Category Category { get; private set; }
+
+        public ObligationType ObligationType { get; private set; }
+
+        public decimal AmountInTonnes { get; private set; }
+
+        public ReturnItem(
+            Category category,
+            ObligationType obligationType,
+            decimal amountInTonnes)
+        {
+            Category = category;
+            ObligationType = obligationType;
+
+            if (amountInTonnes < 0)
+            {
+                throw new ArgumentOutOfRangeException("amountInTonnes");
+            }
+            AmountInTonnes = amountInTonnes;
+        }
+    }
+}
