4a27d361db43a7603b2c5e424e84fe0271407379
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-12-10 14:05:37 +0000

    Add new table for EEEOutputAmount

---
+++ src/EA.Weee.Database/scripts/Everytime/0007-InsertCategoryData.sql
@@ -0,0 +1,35 @@
+﻿GO
+PRINT N'Altering [Lookup].[Category]...';
+
+DECLARE @tblTempCategoryTable TABLE (
+[Id]            UNIQUEIDENTIFIER NOT NULL,
+[Name]          NVARCHAR(255) NOT NULL
+)
+
+INSERT INTO @tblTempCategoryTable([Id], [Name])
+VALUES
+('9080C0D8-D164-44C3-A6D4-997CA7D66F84', 'Large Household Appliances'),
+('5F0A9047-82D6-418A-A95D-68851F6C2D76', 'Small Household Appliances'),
+('121975E8-D168-4A8F-802B-DC7F68551CF9', 'IT and Telecomms Equipment'),
+('ACFD5B5D-AE31-46AC-93DE-853ECDFB35EB', 'Consumer Equipment'),
+('568BA399-72FB-4897-BD1B-170BF7D78D26', 'Lighting Equipment'),
+('91624FB0-E5D4-4BB4-86DD-365C67BD3A12', 'Electrical and Electronic Tools'),
+('1F2BB2DA-4242-4F3C-9595-C67B10B13CDF', 'Toys Leisure and Sports'),
+('EA26CE67-154A-409A-B80A-B03A5A5910D6', 'Medical Devices'),
+('23497C88-2371-4C3E-8A94-681B4484CBF2', 'Monitoring and Control Instruments'),
+('5B8C8B6D-888A-49A5-995A-5D3C660FB0D7', 'Automatic Dispensers'),
+('4D13450B-80E9-45AB-85D3-6AD662C9387D', 'Display Equipment'),
+('29FD4480-7385-4A6A-AE38-1AA47A5F555B', 'Cooling Appliances Containing Refrigerants'),
+('1552F07A-2F0A-4276-9D49-C42A24FAA2BF', 'Gas Discharge Lamps and LED light sources'),
+('0F0A5653-24A7-4DE0-BE0A-5EF0FC81D11E', 'Photovoltaic Panels');
+
+INSERT INTO [Lookup].[Category]([Id], [Name])
+SELECT tmp.[Id], tmp.[Name]
+FROM @tblTempCategoryTable tmp
+LEFT JOIN [Lookup].[Category] tbl ON tbl.[Id] = tmp.[Id]
+WHERE tbl.[Id] IS NULL
+
+GO
+PRINT N'Update complete.';
+
+GO
---
+++ src/EA.Weee.Database/scripts/Update/Sprint19/20151210-1130-CreateDataReturnsEEEOutputAmountTables.sql
@@ -0,0 +1,75 @@
+﻿-- Create [PCS].[EEEOutputAmount]
+CREATE TABLE [PCS].[EEEOutputAmount]
+(
+	[Id] [uniqueidentifier] NOT NULL,
+	[RegisteredProducerId] [uniqueidentifier] NOT NULL,
+	[CategoryId] [uniqueidentifier] NOT NULL,
+	[ObligationType] [nvarchar](4) NOT NULL,
+	[Tonnage] [decimal](38, 3) NOT NULL,
+	
+	CONSTRAINT [PK_EEEOutputAmount] PRIMARY KEY CLUSTERED ([Id] ASC)
+	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+-- This check constraint ensures that the obligation type can only have values of 'B2B' or 'B2C'.
+ALTER TABLE [PCS].[EEEOutputAmount]  WITH CHECK ADD
+	CONSTRAINT [CK_ProducerSubmission_ObligationType] CHECK ([ObligationType] IN ('B2B', 'B2C'))
+GO
+
+ALTER TABLE [PCS].[EEEOutputAmount]
+	CHECK CONSTRAINT [CK_EEEOutputAmount_ObligationType]
+GO
+
+-- Add Foreign Key constraints.
+ALTER TABLE [PCS].[EEEOutputAmount]  WITH CHECK ADD CONSTRAINT [FK_EEEOutputAmount_Category] FOREIGN KEY([CategoryId])
+REFERENCES [Lookup].[Category] ([Id])
+GO
+
+ALTER TABLE [PCS].[EEEOutputAmount] CHECK CONSTRAINT [FK_EEEOutputAmount_Category]
+GO
+
+ALTER TABLE [PCS].[EEEOutputAmount]  WITH CHECK ADD CONSTRAINT [FK_EEEOutputAmount_RegisteredProducer] FOREIGN KEY([RegisteredProducerId])
+REFERENCES [Producer].[RegisteredProducer] ([Id])
+GO
+
+ALTER TABLE [PCS].[EEEOutputAmount] CHECK CONSTRAINT [FK_EEEOutputAmount_RegisteredProducer]
+GO
+
+-- Create [PCS].[EEEOutputReturnVersion]
+CREATE TABLE [PCS].[EEEOutputReturnVersion]
+(
+	[Id] [uniqueidentifier] NOT NULL,
+	CONSTRAINT [PK_EEEOutputReturnVersion] PRIMARY KEY CLUSTERED([Id] ASC)
+	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+-- Create [PCS].[EEEOutputReturnVersionAmount]
+CREATE TABLE [PCS].[EEEOutputReturnVersionAmount]
+(
+	[Id] [uniqueidentifier] NOT NULL,
+	[EEEOutputReturnVersionId] [uniqueidentifier] NOT NULL,
+	[EEEOuputAmountId] [uniqueidentifier] NOT NULL,
+	CONSTRAINT [PK_EEEOutputReturnVersionAmount] PRIMARY KEY CLUSTERED ([Id] ASC)
+	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+-- Add Foreign Key constraints.
+ALTER TABLE [PCS].[EEEOutputReturnVersionAmount]  WITH CHECK ADD CONSTRAINT [FK_EEEOutputReturnVersionAmount_EEEOutputAmount] FOREIGN KEY([EEEOuputAmountId])
+REFERENCES [PCS].[EEEOutputAmount] ([Id])
+GO
+
+ALTER TABLE [PCS].[EEEOutputReturnVersionAmount] CHECK CONSTRAINT [FK_EEEOutputReturnVersionAmount_EEEOutputAmount]
+GO
+
+ALTER TABLE [PCS].[EEEOutputReturnVersionAmount]  WITH CHECK ADD CONSTRAINT [FK_EEEOutputReturnVersionAmount_EEEOutputReturnVersion] FOREIGN KEY([EEEOutputReturnVersionId])
+REFERENCES [PCS].[EEEOutputReturnVersion] ([Id])
+GO
+
+ALTER TABLE [PCS].[EEEOutputReturnVersionAmount] CHECK CONSTRAINT [FK_EEEOutputReturnVersionAmount_EEEOutputReturnVersion]
+GO
