d046dab38e5eb870f98c2f25dd22e644a65a8d9b
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-12-10 14:05:37 +0000

    Add new table for EEEOutputAmount

---
+++ src/EA.Weee.Database/scripts/Update/Sprint19/20151210-1130-CreateDataReturnsCategoryTable.sql
@@ -0,0 +1,11 @@
+﻿-- Create [Lookup].[Category]
+CREATE TABLE [Lookup].[Category]
+(
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](255) NOT NULL,
+	
+	CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED ([Id] ASC)
+	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
