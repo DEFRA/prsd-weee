d2d74e1be9878cff0fa412dee8012b1a616b4647
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-12-11 15:45:16 +0530

    Added remove producer UI

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Producers/ConfirmRemoveProducerViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Producers
+{
+    using System.ComponentModel.DataAnnotations;
+    using Web.ViewModels.Shared;
+
+    public class ConfirmRemoveProducerViewModel : YesNoChoiceViewModel
+    {
+        [Required(ErrorMessage = "Please confirm")]
+        public override string SelectedValue { get; set; }
+
+        public string RegistrationNumber { get; set; }
+    }
+}
