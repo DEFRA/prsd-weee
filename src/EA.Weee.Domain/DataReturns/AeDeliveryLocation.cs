64438e19d651163f3a7cbf8e7c374b60abf4bd5f
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-12-14 17:05:08 +0000

    Address review comments: - Separate delivered amounts from locations. - Add back ReturnItem class.

--- src/EA.Weee.Domain/DataReturns/AeDeliveryLocation.cs
+++ src/EA.Weee.Domain/DataReturns/AeDeliveryLocation.cs
@@ -1,44 +1,22 @@
 ﻿namespace EA.Weee.Domain.DataReturns
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using EA.Prsd.Core.Domain;
-    using Lookup;
     using Prsd.Core;
 
-    public class AeDeliveryLocation : Entity, IReturnItem
+    public class AeDeliveryLocation : Entity
     {
         public string ApprovalNumber { get; private set; }
 
         public string OperatorName { get; private set; }
 
-        public virtual ObligationType ObligationType { get; private set; }
-
-        public virtual WeeeCategory WeeeCategory { get; private set; }
-
-        public decimal Tonnage { get; private set; }
-
-        public virtual DataReturnVersion DataReturnVersion { get; private set; }
-
         protected AeDeliveryLocation()
         {
         }
 
-        public AeDeliveryLocation(string approvalNumber, string operatorName, ObligationType obligationType, WeeeCategory weeeCategory, decimal tonnage, DataReturnVersion dataReturnVersion)
+        public AeDeliveryLocation(string approvalNumber, string operatorName)
         {
             Guard.ArgumentNotNullOrEmpty(() => approvalNumber, approvalNumber);
             Guard.ArgumentNotNullOrEmpty(() => operatorName, operatorName);
-            Guard.ArgumentNotNull(() => dataReturnVersion, dataReturnVersion);
-
-            ApprovalNumber = approvalNumber;
-            OperatorName = operatorName;
-            ObligationType = obligationType;
-            WeeeCategory = weeeCategory;
-            Tonnage = tonnage;
-            DataReturnVersion = dataReturnVersion;
         }
     }
 }
