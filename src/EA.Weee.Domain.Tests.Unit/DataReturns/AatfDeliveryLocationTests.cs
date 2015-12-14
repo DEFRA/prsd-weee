448ebc72a7ebd7fab3dcad5ab323e7764384311a
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-12-14 14:09:05 +0000

    Add further unit tests for domain objects

---
+++ src/EA.Weee.Domain.Tests.Unit/DataReturns/AatfDeliveryLocationTests.cs
@@ -0,0 +1,42 @@
+﻿namespace EA.Weee.Domain.Tests.Unit.DataReturns
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using Domain.DataReturns;
+    using FakeItEasy;
+    using Lookup;
+    using Xunit;
+
+    public class AatfDeliveryLocationTests
+    {
+        [Fact]
+        public void ConstructsAatfDeliveryLocation_WithNullAatfApprovalNumber_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AatfDeliveryLocation(null, "Test", A<ObligationType>._, A<WeeeCategory>._, A<decimal>._, A.Fake<DataReturnVersion>()));
+        }
+
+        [Fact]
+        public void ConstructsAatfDeliveryLocation_WithEmptyAatfApprovalNumber_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new AatfDeliveryLocation(string.Empty, "Test", A<ObligationType>._, A<WeeeCategory>._, A<decimal>._, A.Fake<DataReturnVersion>()));
+        }
+
+        [Fact]
+        public void ConstructsAatfDeliveryLocation_WithNullFacilityName_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AatfDeliveryLocation("Test", null, A<ObligationType>._, A<WeeeCategory>._, A<decimal>._, A.Fake<DataReturnVersion>()));
+        }
+
+        [Fact]
+        public void ConstructsAatfDeliveryLocation_WithEmptyFacilityName_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new AatfDeliveryLocation("Test", string.Empty, A<ObligationType>._, A<WeeeCategory>._, A<decimal>._, A.Fake<DataReturnVersion>()));
+        }
+
+        [Fact]
+        public void ConstructsAatfDeliveryLocation_WithNullDataReturnVersion_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AatfDeliveryLocation("Test", "Test", A<ObligationType>._, A<WeeeCategory>._, A<decimal>._, null));
+        }
+    }
+}
