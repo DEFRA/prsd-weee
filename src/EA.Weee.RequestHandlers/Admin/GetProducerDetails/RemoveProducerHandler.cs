000785dc7c221b8a0c8b622c2965161f2409bd4f
Rick Powell <richard.powell@sfwltd.co.uk>
2015-12-15 19:53:13 +0000

    Implementation of unalignment of producer

--- src/EA.Weee.RequestHandlers/Admin/GetProducerDetails/RemoveProducerHandler.cs
+++ src/EA.Weee.RequestHandlers/Admin/GetProducerDetails/RemoveProducerHandler.cs
@@ -1,24 +1,31 @@
 ﻿namespace EA.Weee.RequestHandlers.Admin.GetProducerDetails
 {
     using System.Threading.Tasks;
+    using DataAccess.Repositories;
     using Prsd.Core.Mediator;
+    using Requests.Admin;
     using Security;
 
-    public class RemoveProducerHandler : IRequestHandler<Requests.Admin.RemoveProducer, bool>
+    public class RemoveProducerHandler : IRequestHandler<RemoveProducer, int>
     {
         private readonly IWeeeAuthorization authorization;
+        private readonly IRegisteredProducerRepository registeredProducerRepository;
 
-        public RemoveProducerHandler(IWeeeAuthorization authorization)
+        public RemoveProducerHandler(IWeeeAuthorization authorization, IRegisteredProducerRepository registeredProducerRepository)
         {
             this.authorization = authorization;
+            this.registeredProducerRepository = registeredProducerRepository;
         }
 
-        public async Task<bool> HandleAsync(Requests.Admin.RemoveProducer request)
+        public async Task<int> HandleAsync(RemoveProducer request)
         {
             authorization.EnsureCanAccessInternalArea();
 
-            // TODO: The actual implementation
-            return await Task.Run(() => true);
+            var producer = await registeredProducerRepository.Get(request.RegisteredProducerId);
+
+            producer.Unalign();
+
+            return await registeredProducerRepository.SaveChangesAsync();
         }
     }
 }
