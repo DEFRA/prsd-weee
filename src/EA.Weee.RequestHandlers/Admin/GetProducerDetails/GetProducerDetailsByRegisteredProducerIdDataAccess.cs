e619cb19c146a0415389b7188ff0f8ec77001594
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-12-15 14:26:34 +0530

    Remove incorrectly registered producer from a scheme for a compliance year

---
+++ src/EA.Weee.RequestHandlers/Admin/GetProducerDetails/GetProducerDetailsByRegisteredProducerIdDataAccess.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Weee.RequestHandlers.Admin.GetProducerDetails
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using Core.Admin;
+    using System.Threading.Tasks;
+    using DataAccess;
+    using Domain.Producer;
+
+    public class GetProducerDetailsByRegisteredProducerIdDataAccess : IGetProducerDetailsByRegisteredProducerIdDataAccess
+    {
+        private readonly WeeeContext context;
+
+        public GetProducerDetailsByRegisteredProducerIdDataAccess(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<RegisteredProducer> Fetch(Guid registeredProducerId)
+        {
+            return await context.RegisteredProducers.Where(p => p.Id == registeredProducerId).SingleOrDefaultAsync();
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Producers/RemovedProducerViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Producers
+{
+    using System;
+
+    public class RemovedProducerViewModel
+    {
+        public Guid RegisteredProducerId { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public int ComplianceYear { get; set; }
+
+        public string SchemeName { get; set; }
+    }
+}
