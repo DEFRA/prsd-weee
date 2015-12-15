6759b64291f22ce0d0438d1035e2f080468b2a15
Priety Mahajan <PrietyM@sfwltd.co.uk>
2015-12-15 15:24:41 +0000

    EF mapping for data returns delivered amount

---
+++ src/EA.Weee.DataAccess/Mappings/WeeeCollectedAmountMapping.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Weee.DataAccess.Mappings
+{
+    using System.Data.Entity.ModelConfiguration;
+    using EA.Weee.Domain.DataReturns;
+    internal class WeeeCollectedAmountMapping : EntityTypeConfiguration<WeeeCollectedAmount>
+    {
+        public WeeeCollectedAmountMapping()
+        {
+            ToTable("WeeeCollectedAmount", "PCS");
+            Ignore(w => w.DataReturnVersion);            
+        }
+    }
+}
---
+++ src/EA.Weee.DataAccess/Mappings/WeeeDeliveredAmountMapping.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Weee.DataAccess.Mappings
+{
+    using System.Data.Entity.ModelConfiguration;
+    using EA.Weee.Domain.DataReturns;
+    internal class WeeeDeliveredAmountMapping : EntityTypeConfiguration<WeeeDeliveredAmount>
+    {
+        public WeeeDeliveredAmountMapping()
+        {
+            ToTable("WeeeDeliveredAmount", "PCS");
+            Ignore(w => w.DataReturnVersion);
+            Map<AatfDeliveredAmount>(m =>
+            {
+                m.MapInheritedProperties();
+                m.Requires(p => p.AatfDeliveryLocation);
+                m.ToTable("WeeeDeliveredAmount", "PCS");
+            });
+            Map<AeDeliveredAmount>(m =>
+            {
+                m.MapInheritedProperties();
+                m.Requires(p => p.AeDeliveryLocation);
+                m.ToTable("WeeeDeliveredAmount", "PCS");
+            });            
+        }
+    }
+}
