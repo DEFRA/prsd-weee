70e0c45078426327318944ec1922b8e68d7c0d1f
Rick Powell <richard.powell@sfwltd.co.uk>
2015-12-16 14:07:09 +0000

    Update migrated producers

---
+++ src/EA.Weee.Database/scripts/Update/Sprint19/20151216-1400-UpdateMigratedProducers.sql
@@ -0,0 +1,61 @@
+SET NOCOUNT ON
+
+INSERT INTO Producer.MigratedProducer
+(
+	Id,
+	ProducerRegistrationNumber,
+	ProducerName,
+	CompanyNumber
+)
+VALUES
+(
+	 NEWID(),
+	'WEE/CH5704UQ',
+	'Prime Light Electrical Ltd',
+	'02754353'
+)
+
+INSERT INTO Producer.MigratedProducer
+(
+	Id,
+	ProducerRegistrationNumber,
+	ProducerName,
+	CompanyNumber
+)
+VALUES
+(
+	 NEWID(),
+	'WEE/DF5711UX',
+	'Principal Lighting Ltd.',
+	'4947891'
+)
+
+INSERT INTO Producer.MigratedProducer
+(
+	Id,
+	ProducerRegistrationNumber,
+	ProducerName,
+	CompanyNumber
+)
+VALUES
+(
+	 NEWID(),
+	'WEE/CK5698TU',
+	'Henrob Ltd',
+	'01873269'
+)
+
+INSERT INTO Producer.MigratedProducer
+(
+	Id,
+	ProducerRegistrationNumber,
+	ProducerName,
+	CompanyNumber
+)
+VALUES
+(
+	 NEWID(),
+	'WEE/CD5701TX',
+	'Robert Scott & Sons Ltd',
+	'1099088'
+)
