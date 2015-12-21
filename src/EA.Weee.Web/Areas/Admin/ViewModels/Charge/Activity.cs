c8738d1a514f86990469532e491120b89ef03e8c
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-21 22:31:54 +0000

    Added "ChooseActivity" action to charge controller.

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Charge/Activity.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Charge
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public enum Activity
+    {
+        ManagePendingCharges,
+        ManageIssuedCharges,
+        ViewInvoiceRunHistory
+    }
+}
