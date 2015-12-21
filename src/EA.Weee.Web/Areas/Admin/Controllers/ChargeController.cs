54bfb68ed2b302bb2a7ecc720d5fb09221d52c1d
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-21 20:10:25 +0000

    Updated admin HomeController to show "Manage PCS charges" option when config is enabled.

---
+++ src/EA.Weee.Web/Areas/Admin/Controllers/ChargeController.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Weee.Web.Areas.Admin.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using EA.Weee.Web.Areas.Admin.Controllers.Base;
+
+    public class ChargeController : AdminController
+    {
+        [HttpGet]
+        public ActionResult SelectAA()
+        {
+            return View();
+        }
+    }
+}
