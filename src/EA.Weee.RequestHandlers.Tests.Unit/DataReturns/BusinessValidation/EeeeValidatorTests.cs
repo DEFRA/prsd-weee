739a9192b4a223a76e89ec4dae17ac5471525717
Chris Smith <chriss@sfwltd.co.uk>
2015-12-21 11:53:20 +0000

    #39736: Add producer not PCS member validation in eee data return

--- src/EA.Weee.RequestHandlers.Tests.Unit/DataReturns/BusinessValidation/EeeeValidatorTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/DataReturns/BusinessValidation/EeeeValidatorTests.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.Threading.Tasks;
+    using Core.Shared;
     using Domain.DataReturns;
+    using Domain.Producer;
     using FakeItEasy;
     using RequestHandlers.DataReturns;
     using RequestHandlers.DataReturns.BusinessValidation;
@@ -16,13 +18,55 @@
         public async Task Validate_WithValidData_ReturnsEmptyErrorDataList()
         {
             // Arrange
-            Quarter quarter = new Quarter(2016, Domain.DataReturns.QuarterType.Q1);
+            int complianceYear = 2016;
+            Quarter quarter = new Quarter(complianceYear, Domain.DataReturns.QuarterType.Q1);
             Scheme scheme = A.Fake<Scheme>();
             IDataReturnVersionBuilderDataAccess dataAccess = A.Fake<IDataReturnVersionBuilderDataAccess>();
             Func<Domain.Scheme.Scheme, Quarter, IDataReturnVersionBuilderDataAccess> dataAccessDelegate;
             dataAccessDelegate = (s, q) => dataAccess;
+
+            //Setup producer that exists in scheme for compliance year
+            String prn = "Test PRN";
+            RegisteredProducer producer = new RegisteredProducer(prn, complianceYear, scheme);
+            A.CallTo(() => dataAccess.GetRegisteredProducer(prn)).Returns(producer);
+
             EeeValidator eeeValidator = new EeeValidator(scheme, quarter, dataAccessDelegate);
-            throw new NotImplementedException();
+
+            //Act
+            var errors = await eeeValidator.Validate(prn, "Fake producer", 
+                Domain.Lookup.WeeeCategory.LargeHouseholdAppliances, Domain.ObligationType.B2B, new Decimal(5));
+
+            //Assert
+            Assert.Empty(errors);
+        }
+
+        [Fact]
+        public async Task Validate_WithProducerNotRegisteredWithScheme_ReturnsError()
+        {
+            // Arrange
+            int complianceYear = 2016;
+            Quarter quarter = new Quarter(complianceYear, Domain.DataReturns.QuarterType.Q1);
+            Scheme scheme = A.Fake<Scheme>();
+            IDataReturnVersionBuilderDataAccess dataAccess = A.Fake<IDataReturnVersionBuilderDataAccess>();
+            Func<Domain.Scheme.Scheme, Quarter, IDataReturnVersionBuilderDataAccess> dataAccessDelegate;
+            dataAccessDelegate = (s, q) => dataAccess;
+
+            String prn = "Non-extistant PRN";
+
+            A.CallTo(() => dataAccess.GetRegisteredProducer(A<string>._)).Returns((RegisteredProducer)null);
+
+            EeeValidator eeeValidator = new EeeValidator(scheme, quarter, dataAccessDelegate);
+
+            //Act
+            var errors = await eeeValidator.Validate(prn, "Fake producer", 
+                Domain.Lookup.WeeeCategory.LargeHouseholdAppliances, Domain.ObligationType.B2B, new Decimal(5));
+
+            //Assert
+            Assert.Equal(1, errors.Count);
+            ErrorData error = errors[0];
+            Assert.Equal(ErrorLevel.Error, error.ErrorLevel);
+            Assert.Contains(prn, error.Description);
+            Assert.Contains(complianceYear.ToString(), error.Description);
         }
     }
 }
