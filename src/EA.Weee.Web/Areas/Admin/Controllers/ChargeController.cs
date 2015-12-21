4362a969b09307999514b3b76b74b397f1d4c423
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-21 20:15:41 +0000

    Added config check to Charge controller.

--- src/EA.Weee.Web/Areas/Admin/Controllers/ChargeController.cs
+++ src/EA.Weee.Web/Areas/Admin/Controllers/ChargeController.cs
@@ -6,9 +6,27 @@
     using System.Web;
     using System.Web.Mvc;
     using EA.Weee.Web.Areas.Admin.Controllers.Base;
+    using Services;
 
     public class ChargeController : AdminController
     {
+        private readonly IAppConfiguration configuration;
+
+        public ChargeController(IAppConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!configuration.EnableInvoicing)
+            {
+                throw new InvalidOperationException("Invoicing is not enabled.");
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
         [HttpGet]
         public ActionResult SelectAA()
         {
