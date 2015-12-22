d9e967a88a001292923282ef529f31d42c7e857d
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2015-12-22 09:12:05 +0000

    Mark a data returns as submitted in the database (in progress)

---
+++ src/EA.Weee.RequestHandlers/DataReturns/SubmitReturnVersion/SubmitReturnVersionDataAccess.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Weee.RequestHandlers.DataReturns.SubmitReturnVersion
+{
+    using System.Threading.Tasks;
+    using DataAccess;
+    using Domain.DataReturns;
+
+    public class SubmitReturnVersionDataAccess : ISubmitReturnVersionDataAccess
+    {
+        private readonly WeeeContext context;
+
+        public SubmitReturnVersionDataAccess(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public Task Submit(DataReturnVersion dataReturnVersion)
+        {
+            dataReturnVersion.Submit(context.GetCurrentUser());
+
+            return context.SaveChangesAsync();
+        }
+    }
+}
