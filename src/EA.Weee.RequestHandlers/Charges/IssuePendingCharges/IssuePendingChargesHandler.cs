f65155f42b9bb9e1087a772d64ee0b9712634d2a
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-22 22:20:41 +0000

    Added fetch and issue requet handlers for pending charges.

---
+++ src/EA.Weee.RequestHandlers/Charges/IssuePendingCharges/IssuePendingChargesHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace EA.Weee.RequestHandlers.Charges.IssuePendingCharges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Core.Shared;
+    using Domain;
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.Domain.Charges;
+    using EA.Weee.Domain.Scheme;
+    using Security;
+
+    public class IssuePendingChargesHandler : IRequestHandler<Requests.Charges.IssuePendingCharges, Guid>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly IIssuePendingChargesDataAccess dataAccess;
+
+        public IssuePendingChargesHandler(
+            IWeeeAuthorization authorization,
+            IIssuePendingChargesDataAccess dataAccess)
+        {
+            this.authorization = authorization;
+            this.dataAccess = dataAccess;
+        }
+
+        public async Task<Guid> HandleAsync(Requests.Charges.IssuePendingCharges message)
+        {
+            authorization.EnsureCanAccessInternalArea(true);
+
+            UKCompetentAuthority authority = await dataAccess.FetchCompetentAuthority(message.Authority);
+
+            IList<MemberUpload> memberUploads = await dataAccess.FetchSubmittedNonInvoicedMemberUploadsAsync(authority);
+
+            InvoiceRun invoiceRun = new InvoiceRun(authority, memberUploads);
+
+            await dataAccess.SaveAsync(invoiceRun);
+
+            return invoiceRun.Id;
+        }
+    }
+}
