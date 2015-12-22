a5836d10a28fd224f7b30e8205736e76ebfa2ca3
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-22 22:18:54 +0000

    Added [Charging] database schema and [InvoiceRun] table.

---
+++ src/EA.Weee.Database/scripts/Update/Sprint19/20151222-1719-CreateInvoiceRunTable.sql
@@ -0,0 +1,49 @@
+﻿/*
+ * This script creates the [Charging] schema and the [InvoiceRun] table.
+ * It adds a nullable column [InvoiceRunId] to the [PCS].[MemberUpload] table with a FK to the [InvoiceRun] table.
+ * It adds an index to the [PCS].[MemberUpload] table on the [InvoiceRunId] column.
+ */
+
+GO
+CREATE SCHEMA [Charging]
+GO
+
+GRANT DELETE ON SCHEMA::[Charging] TO [weee_application]
+GRANT INSERT ON SCHEMA::[Charging] TO [weee_application]
+GRANT SELECT ON SCHEMA::[Charging] TO [weee_application]
+GRANT UPDATE ON SCHEMA::[Charging] TO [weee_application]
+GO
+
+CREATE TABLE [Charging].[InvoiceRun]
+(
+	[Id]							UNIQUEIDENTIFIER	NOT NULL,
+	[RowVersion]					TIMESTAMP			NOT NULL,
+	[CompetentAuthorityId]			UNIQUEIDENTIFIER	NOT NULL,
+
+	CONSTRAINT [PK_InvoiceRun] PRIMARY KEY CLUSTERED ( [Id] ASC )
+	WITH ( PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON ) ON [PRIMARY]
+
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Charging].[InvoiceRun]  WITH CHECK ADD
+	CONSTRAINT [FK_InvoiceRun_CompetentAuthority] FOREIGN KEY([CompetentAuthorityId])
+	REFERENCES [Lookup].[CompetentAuthority] ([Id])
+GO
+
+ALTER TABLE [PCS].[MemberUpload]
+	ADD [InvoiceRunId] UNIQUEIDENTIFIER NULL
+GO
+
+ALTER TABLE [PCS].[MemberUpload]  WITH CHECK ADD
+	CONSTRAINT [FK_MemberUpload_InvoiceRun] FOREIGN KEY([InvoiceRunId])
+	REFERENCES [Charging].[InvoiceRun] ([Id])
+GO
+
+CREATE NONCLUSTERED INDEX [IX_MemberUpload_InvoiceRunId] ON [PCS].[MemberUpload] 
+(
+	[InvoiceRunId] ASC
+)
+INCLUDE ( [Id], [SchemeId], [ComplianceYear], [TotalCharges]) 
+WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+GO
