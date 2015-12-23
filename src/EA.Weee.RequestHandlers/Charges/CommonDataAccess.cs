ca0f6d51b3f69539825badbe76d94422f8f12686
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-23 15:26:14 +0000

    Add unit tests for InvoiceRun domain object.

--- src/EA.Weee.RequestHandlers/Charges/CommonDataAccess.cs
+++ src/EA.Weee.RequestHandlers/Charges/CommonDataAccess.cs
@@ -44,7 +44,7 @@
         /// Returns all member uploads for the specified authority which are submitted and not yet
         /// assigned to an invoice run.
         /// Results will be ordered by scheme name ascending and then compliance year descending.
-        /// The scheme domain object will be pre-loaded with each member upload returned.
+        /// The scheme and UK competent authority domain objects will be pre-loaded with each member upload returned.
         /// </summary>
         /// <param name="authority"></param>
         /// <returns></returns>
@@ -52,6 +52,7 @@
         {
             return await Context.MemberUploads
                 .Include(mu => mu.Scheme)
+                .Include(mu => mu.Scheme.CompetentAuthority)
                 .Where(mu => mu.IsSubmitted)
                 .Where(mu => mu.InvoiceRun == null)
                 .Where(mu => mu.Scheme.CompetentAuthority.Id == authority.Id)
