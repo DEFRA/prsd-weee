27272e31a35dc300ad84ce82d52f04daf2e311fd
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-24 18:56:38 +0000

    Added EF mapping for 1B1S file data.

---
+++ src/EA.Weee.DataAccess/Mappings/InvoiceRunIbisFileDataMapping.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Weee.DataAccess.Mappings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.ModelConfiguration;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using EA.Weee.Domain.Charges;
+
+    public class InvoiceRunIbisFileDataMapping : EntityTypeConfiguration<InvoiceRunIbisFileData>
+    {
+        public InvoiceRunIbisFileDataMapping()
+        {
+            ToTable("InvoiceRun", "Charging");
+
+            Property(e => e.CustomerFileName).HasColumnName("IbisCustomerFileName");
+            Property(e => e.CustomerFileData).HasColumnName("IbisCustomerFileData");
+            Property(e => e.TransactionFileName).HasColumnName("IbisTransactionFileName");
+            Property(e => e.TransactionFileData).HasColumnName("IbisTransactionFileData");
+        }
+    }
+}
