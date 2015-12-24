235134c654bc2e5149b5a4f47e71fedea046709a
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-24 18:53:51 +0000

    Added assembly providing an object model for 1B1S files.

---
+++ src/EA.Weee.Ibis.Tests.Unit/CustomerFileTests.cs
@@ -0,0 +1,36 @@
+﻿namespace EA.Weee.Ibis.Tests.Unit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class CustomerFileTests
+    {
+        [Fact]
+        public void Write_WithOneCustomer_GeneratesCorrectOutput()
+        {
+            // Arrange
+            Address address = new Address("1 High Street", "Flat 123", "Newtown", "Nr Oldtown", "Testshire", "England", "AA1 1AA");
+            Customer customer = new Customer("WEE0001", "Test customer", address);
+
+            CustomerFile file = new CustomerFile("WEE", 0);
+            
+            file.AddCustomer(customer);
+
+            // Act
+            string result = file.Write();
+
+            // Assert
+            string expectedDate = DateTime.UtcNow.Date.ToString("dd-MMM-yyyy").ToUpperInvariant();
+            string expectedOutput =
+                "\"H\",\"0000000\",\"WEE\",\"H\",\"C\",\"0\",\"" + expectedDate + "\"" + Environment.NewLine +
+                "\"D\",\"0000001\",\"WEE0001\",\"Test customer\",\"1 High Street\",\"Flat 123\",\"Newtown\",\"Nr Oldtown\",\"Testshire\",\"England\",\"AA1 1AA\"" + Environment.NewLine +
+                "\"T\",\"0000002\",\"3\"" + Environment.NewLine;
+
+            Assert.Equal(expectedOutput, result);
+        }
+    }
+}
---
+++ src/EA.Weee.Ibis/CustomerFile.cs
@@ -0,0 +1,72 @@
+﻿namespace EA.Weee.Ibis
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents an Ibis customer file.
+    /// </summary>
+    public sealed class CustomerFile : IbisFile
+    {
+        private List<Customer> mCustomers = new List<Customer>();
+        /// <summary>
+        /// The collection of customers associated with the customer file.
+        /// </summary>
+        public IReadOnlyList<Customer> Customers
+        {
+            get { return mCustomers; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerFile"/> class with the specified file source and file ID.
+        /// </summary>
+        /// <param name="fileSource">The 3 letter code for the feeder source assigned by 1B1S income section.</param>
+        /// <param name="fileID">The ID of the Ibis file.</param>
+        public CustomerFile(string fileSource, ulong fileID)
+            : base(fileSource, fileID)
+        {
+        }
+
+        /// <summary>
+        /// Adds a customer to the collection.
+        /// </summary>
+        /// <param name="customer">The customer to add to the collection.</param>
+        public void AddCustomer(Customer customer)
+        {
+            if (customer == null)
+            {
+                throw new ArgumentNullException("customer");
+            }
+
+            mCustomers.Add(customer);
+        }
+
+        /// <summary>
+        /// Returns a colection of IIbisFileLine items to represent the collection of customers. 
+        /// </summary>
+        protected override IEnumerable<IIbisFileLine> GetLines()
+        {
+            foreach (Customer customer in mCustomers)
+            {
+                yield return new CustomerFileLine(customer);
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of lines that will be written, excluding the header and footer lines.
+        /// </summary>
+        /// <returns></returns>
+        protected override int GetLineCount()
+        {
+            return mCustomers.Count;
+        }
+
+        /// <summary>
+        /// Returns the file type identifier of the Ibis file.
+        /// </summary>
+        protected override string GetFileTypeIdentifier()
+        {
+            return "C";
+        }
+    }
+}
