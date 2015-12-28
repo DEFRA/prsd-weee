9e033366430a2d76f5664b85b89fd71f85bc8b84
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-28 17:04:56 +0000

    Added generators for customer files and transaction files.

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Charges/IssuePendingCharges/IbisFileDataGeneratorTests.cs
@@ -0,0 +1,86 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Charges.IssuePendingCharges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using EA.Weee.Domain.Charges;
+    using EA.Weee.Domain.Scheme;
+    using EA.Weee.RequestHandlers.Charges.IssuePendingCharges;
+    using FakeItEasy;
+    using Ibis;
+    using Xunit;
+
+    public class IbisFileDataGeneratorTests
+    {
+        /// <summary>
+        /// This test ensures that calling CreateFileData will return a file data
+        /// object with some content in both the CustomerFileData and TransactionFileData
+        /// properties.
+        /// </summary>
+        [Fact]
+        public void CreateFileData_Always_CreatesFileData()
+        {
+            // Arrange
+            CustomerFile customerFile = new CustomerFile("WEE", (ulong)12345);
+            IIbisCustomerFileGenerator customerFileGenerator = A.Fake<IIbisCustomerFileGenerator>();
+            A.CallTo(() => customerFileGenerator.CreateCustomerFile(A<ulong>._, A<IReadOnlyList<MemberUpload>>._))
+                .Returns(customerFile);
+
+            TransactionFile transactionFile = new TransactionFile("WEE", (ulong)12345);
+            IIbisTransactionFileGenerator transactionFileGenerator = A.Fake<IIbisTransactionFileGenerator>();
+            A.CallTo(() => transactionFileGenerator.CreateTransactionFile(A<ulong>._, A<IReadOnlyList<MemberUpload>>._))
+                .Returns(transactionFile);
+
+            IbisFileDataGenerator generator = new IbisFileDataGenerator(
+                customerFileGenerator,
+                transactionFileGenerator);
+
+            // Act
+            InvoiceRunIbisFileData result = generator.CreateFileData(A.Dummy<ulong>(), A.Dummy<List<MemberUpload>>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.False(string.IsNullOrEmpty(result.CustomerFileData));
+            Assert.False(string.IsNullOrEmpty(result.TransactionFileData));
+        }
+
+        /// <summary>
+        /// This test ensures that calling CreateFileData will return a file data
+        /// object with customer and transaction filenames in the correct format.
+        /// Customer files should be a concatentaion of "WEECI" and the file ID
+        /// padded to 5 characters. The extension should be ".dat";
+        /// Transaction files should be a concatenation of "WEEHI" and the file ID
+        /// padded to 5 characters. The extension should be ".dat";
+        /// </summary>
+        [Fact]
+        public void CreateFileData_WithFileID_CreatesFilesWithCorrectFileNames()
+        {
+            // Arrange
+            ulong fileID = 123;
+
+            CustomerFile customerFile = new CustomerFile("WEE", (ulong)12345);
+            IIbisCustomerFileGenerator customerFileGenerator = A.Fake<IIbisCustomerFileGenerator>();
+            A.CallTo(() => customerFileGenerator.CreateCustomerFile(A<ulong>._, A<IReadOnlyList<MemberUpload>>._))
+                .Returns(customerFile);
+
+            TransactionFile transactionFile = new TransactionFile("WEE", (ulong)12345);
+            IIbisTransactionFileGenerator transactionFileGenerator = A.Fake<IIbisTransactionFileGenerator>();
+            A.CallTo(() => transactionFileGenerator.CreateTransactionFile(A<ulong>._, A<IReadOnlyList<MemberUpload>>._))
+                .Returns(transactionFile);
+
+            IbisFileDataGenerator generator = new IbisFileDataGenerator(
+                customerFileGenerator,
+                transactionFileGenerator);
+
+            // Act
+            InvoiceRunIbisFileData result = generator.CreateFileData(fileID, A.Dummy<List<MemberUpload>>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("WEECI00123.dat", result.CustomerFileName);
+            Assert.Equal("WEEHI00123.dat", result.TransactionFileName);
+        }
+    }
+}
---
+++ src/EA.Weee.RequestHandlers/Charges/IssuePendingCharges/IbisFileDataGenerator.cs
@@ -0,0 +1,60 @@
+﻿namespace EA.Weee.RequestHandlers.Charges.IssuePendingCharges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using EA.Weee.Domain.Charges;
+    using EA.Weee.Domain.Scheme;
+    using EA.Weee.Ibis;
+
+    /// <summary>
+    /// This is the default implementation of <see cref="IIbisFileDataGenerator"/>.
+    /// It creates an <see cref="InvoiceRunIbisFileData"/> using an
+    /// <see cref="IIbisCustomerFileGenerator"/> and an <see cref="IIbisTransactionFileGenerator"/>
+    /// to generate 1B1S files, which are then written to strings and returned with appropriate
+    /// files names based on the specified file ID.
+    /// </summary>
+    public class IbisFileDataGenerator : IIbisFileDataGenerator
+    {
+        private readonly IIbisCustomerFileGenerator ibisCustomerFileGenerator;
+        private readonly IIbisTransactionFileGenerator ibisTransactionFileGenerator;
+
+        public IbisFileDataGenerator(
+            IIbisCustomerFileGenerator ibisCustomerFileGenerator,
+            IIbisTransactionFileGenerator ibisTransactionFileGenerator)
+        {
+            this.ibisCustomerFileGenerator = ibisCustomerFileGenerator;
+            this.ibisTransactionFileGenerator = ibisTransactionFileGenerator;
+        }
+
+        /// <summary>
+        /// Creates the data representing 1B1S customer and transaction files for the specified list
+        /// of member uploads.
+        /// </summary>
+        /// <param name="fileID">The ID that the 1B1S files will use. This must be unique for every pair of 1B1S files
+        /// and must be in the range of 0 to 99999. To avoid clashes with IDs used by the incumbent system, a seed
+        /// value may need to be used.</param>
+        /// <param name="memberUploads">The list of member uploads to be included.</param>
+        /// <returns>Returns an <see cref="InvoiceRunIbisFileData"/> which provides the data and file names of the
+        /// generated 1B1S customer and transaction files.</returns>
+        public InvoiceRunIbisFileData CreateFileData(ulong fileID, IReadOnlyList<MemberUpload> memberUploads)
+        {
+            CustomerFile customerFile = ibisCustomerFileGenerator.CreateCustomerFile(fileID, memberUploads);
+            TransactionFile transactionFile = ibisTransactionFileGenerator.CreateTransactionFile(fileID, memberUploads);
+
+            string customerFileName = string.Format("WEECI{0:D5}.dat", fileID);
+            string transactionFileName = string.Format("WEEHI{0:D5}.dat", fileID);
+
+            string customerFileData = customerFile.Write();
+            string transactionFileData = transactionFile.Write();
+
+            return new InvoiceRunIbisFileData(
+                customerFileName,
+                customerFileData,
+                transactionFileName,
+                transactionFileData);
+        }
+    }
+}
