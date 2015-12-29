f784dee0f2358dfb6f81ac2c641b39effcd3828a
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2015-12-29 17:22:47 +0000

    Updated 1B1S files to pad file ID and line count.

--- src/EA.Weee.Ibis.Tests.Unit/CustomerFileTests.cs
+++ src/EA.Weee.Ibis.Tests.Unit/CustomerFileTests.cs
@@ -26,9 +26,9 @@
             // Assert
             string expectedDate = DateTime.UtcNow.Date.ToString("dd-MMM-yyyy").ToUpperInvariant();
             string expectedOutput =
-                "\"H\",\"0000000\",\"WEE\",\"H\",\"C\",\"0\",\"" + expectedDate + "\"" + Environment.NewLine +
+                "\"H\",\"0000000\",\"WEE\",\"H\",\"C\",\"00000\",\"" + expectedDate + "\"" + Environment.NewLine +
                 "\"D\",\"0000001\",\"WEE0001\",\"Test customer\",\"1 High Street\",\"Flat 123\",\"Newtown\",\"Nr Oldtown\",\"Testshire\",\"England\",\"AA1 1AA\"" + Environment.NewLine +
-                "\"T\",\"0000002\",\"3\"" + Environment.NewLine;
+                "\"T\",\"0000002\",\"0000003\"" + Environment.NewLine;
 
             Assert.Equal(expectedOutput, result);
         }
