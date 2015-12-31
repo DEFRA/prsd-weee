ced883ca4cb0e4bf4edb3bbd26255936faba8bad
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2015-12-31 12:24:36 +0530

    View data return submissions history for external site

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Shared/GetDataReturnSubmissionsHistoryResultsHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Shared
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Admin;
+    using FakeItEasy;
+    using RequestHandlers.Security;
+    using RequestHandlers.Shared;
+    using Requests.Shared;
+    using Xunit;
+
+    public class GetDataReturnSubmissionsHistoryResultsHandlerTests
+    {
+        [Fact]
+        public async Task GetDataReturnSubmissionsHistoryResultHandler_RequestByExternalUser_ReturnDataReturnSubmissionsHistoryData()
+        {
+            // Arrange
+            IGetDataReturnSubmissionsHistoryResultsDataAccess dataAccess = CreateFakeDataAccess();
+            IWeeeAuthorization authorization = A.Fake<IWeeeAuthorization>();
+            GetDataReturnSubmissionsHistoryResultsHandler handler = new GetDataReturnSubmissionsHistoryResultsHandler(authorization, dataAccess);
+            GetDataReturnSubmissionsHistoryResults request = new GetDataReturnSubmissionsHistoryResults(A<Guid>._, A<Guid>._);
+
+            // Act
+            List<DataReturnSubmissionsHistoryResult> results = await handler.HandleAsync(request);
+
+            // Assert
+            A.CallTo(() => authorization.EnsureInternalOrOrganisationAccess(A<Guid>._)).MustHaveHappened();
+            Assert.Equal(results.Count, 2);
+        }
+
+        private IGetDataReturnSubmissionsHistoryResultsDataAccess CreateFakeDataAccess()
+        {
+            IGetDataReturnSubmissionsHistoryResultsDataAccess dataAccess = A.Fake<IGetDataReturnSubmissionsHistoryResultsDataAccess>();
+
+            var results = new List<DataReturnSubmissionsHistoryResult>()
+            { 
+                new DataReturnSubmissionsHistoryResult(),
+                new DataReturnSubmissionsHistoryResult()
+            };
+
+            A.CallTo(() => dataAccess.GetDataReturnSubmissionsHistory(A<Guid>._)).Returns(results);
+
+            return dataAccess;
+        }
+    }
+}
