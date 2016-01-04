1df6e194a1df4b50acabc14b7cc6a241285fab67
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2016-01-04 18:02:10 +0000

    Refreshed test EDMX to include MemberUpload.SubmittedByUserId.

--- src/EA.Weee.Tests.Core/Model/MemberUpload.cs
+++ src/EA.Weee.Tests.Core/Model/MemberUpload.cs
@@ -34,6 +34,10 @@ namespace EA.Weee.Tests.Core.Model
         public Nullable<System.DateTime> UpdatedDate { get; set; }
         public string CreatedById { get; set; }
         public string UpdatedById { get; set; }
+        public Nullable<System.Guid> MemberUploadId { get; set; }
+        public Nullable<System.Guid> InvoiceRunId { get; set; }
+        public Nullable<System.DateTime> SubmittedDate { get; set; }
+        public string SubmittedByUserId { get; set; }
     
         public virtual Organisation Organisation { get; set; }
         public virtual Scheme Scheme { get; set; }
@@ -41,5 +45,6 @@ namespace EA.Weee.Tests.Core.Model
         public virtual ICollection<ProducerSubmission> ProducerSubmissions { get; set; }
         public virtual AspNetUser AspNetUser { get; set; }
         public virtual AspNetUser AspNetUser1 { get; set; }
+        public virtual AspNetUser AspNetUser2 { get; set; }
     }
 }
