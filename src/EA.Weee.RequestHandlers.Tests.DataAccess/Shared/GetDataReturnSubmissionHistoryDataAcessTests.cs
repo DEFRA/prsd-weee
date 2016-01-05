68937063924ba7358287ea8861c90be814829ca9
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2016-01-05 18:29:50 +0530

    Removed unnecessary unit test

--- src/EA.Weee.RequestHandlers.Tests.DataAccess/Shared/GetDataReturnSubmissionHistoryDataAcessTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.DataAccess/Shared/GetDataReturnSubmissionHistoryDataAcessTests.cs
@@ -169,59 +169,5 @@
                 Assert.Equal(result[2].FileName, "DataReturnUpload1.xml");
             }
         }
-
-        [Fact]
-        public async void GetDataReturnSubmissionHistory_ReturnsMatchingUserSubmissionHistoryData()
-        {
-            using (var database = new DatabaseWrapper())
-            {
-                Guid organisationId = new Guid("72BB14DF-DCD5-4DBB-BBA9-4CFC26AD80F9");
-                GetDataReturnSubmissionsHistoryResultsDataAccess dataAccess = new GetDataReturnSubmissionsHistoryResultsDataAccess(database.WeeeContext);
-
-                // Arrange
-                ModelHelper modelHelper = new ModelHelper(database.Model);
-
-                var user = modelHelper.CreateUser("Test", IdType.Guid);
-
-                var scheme1 = modelHelper.CreateScheme();
-                scheme1.Organisation.Id = organisationId;
-                database.Model.SaveChanges();
-
-                var drv1 = modelHelper.CreateDataReturnVersion(scheme1, 2015, 1, true);
-                database.Model.SaveChanges();
-
-                var dru1 = modelHelper.CreateDataReturnUpload(scheme1, drv1);
-                dru1.ComplianceYear = 2015;
-                dru1.Quarter = (int?)QuarterType.Q1;
-                dru1.FileName = "DataReturnUpload1.xml";
-                dru1.DataReturnVersion.SubmittingUserId = user.Id;
-
-                var drv2 = modelHelper.CreateDataReturnVersion(scheme1, 2015, 2, true);
-                database.Model.SaveChanges();
-
-                var dru2 = modelHelper.CreateDataReturnUpload(scheme1, drv2);
-                dru2.ComplianceYear = 2015;
-                dru2.Quarter = (int?)QuarterType.Q2;
-                dru2.FileName = "DataReturnUpload2.xml";
-                dru2.DataReturnVersion.SubmittingUserId = user.Id;
-
-                var drv3 = modelHelper.CreateDataReturnVersion(scheme1, 2016, 2, true);
-                database.Model.SaveChanges();
-
-                var dru3 = modelHelper.CreateDataReturnUpload(scheme1, drv3);
-                dru3.ComplianceYear = 2016;
-                dru3.Quarter = (int?)QuarterType.Q2;
-                dru2.DataReturnVersion.SubmittingUserId = Guid.NewGuid().ToString();
-
-                database.Model.SaveChanges();
-
-                // Act
-                var result = await dataAccess.GetDataReturnSubmissionsHistory(scheme1.Id);
-
-                // Assert
-                Assert.NotNull(result);
-                Assert.Equal(result.Count, 2);
-            }
-        }
     }
 }
