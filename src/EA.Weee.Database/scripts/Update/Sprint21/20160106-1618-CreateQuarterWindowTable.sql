d5a1fecdf4bb9a3808e268ac677fdf78bfcff7f8
Rick Powell <richard.powell@sfwltd.co.uk>
2016-01-06 16:49:55 +0000

    New QuarterWindow table, domain object and mapping

---
+++ src/EA.Weee.Database/scripts/Update/Sprint21/20160106-1618-CreateQuarterWindowTable.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [Lookup].[QuarterWindow] 
+(
+	Id UNIQUEIDENTIFIER NOT NULL, 
+	Quarter int NOT NULL,
+	AddStartYears int NOT NULL,
+	StartMonth int NOT NULL,
+	StartDay int NOT NULL,
+	AddEndYears int NOT NULL,
+	EndMonth int NOT NULL,
+	EndDay int NOT NULL
+	CONSTRAINT [PK_QuarterWindow] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)	
+	WITH 
+	(
+		PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON
+	) 
+	ON [PRIMARY]
+) ON [PRIMARY]
---
+++ src/EA.Weee.Domain/Lookup/QuarterWindow.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Weee.Domain.Lookup
+{
+    public class QuarterWindow
+    {
+        public int Quarter { get; private set; }
+
+        public int AddStartYears { get; private set; }
+
+        public int StartMonth { get; private set; }
+
+        public int StartDay { get; private set; }
+
+        public int AddEndYears { get; private set; }
+
+        public int EndMonth { get; private set; }
+
+        public int EndDay { get; private set; }
+
+        protected QuarterWindow()
+        {
+        }
+    }
+}
