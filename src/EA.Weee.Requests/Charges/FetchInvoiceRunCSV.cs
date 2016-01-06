2388b7172e7e87f973bf89d941f5fdd2e0b4fdc3
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2016-01-06 16:31:09 +0000

    Classes to produce the invoice run CSV

---
+++ src/EA.Weee.Requests/Charges/FetchInvoiceRunCSV.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.Requests.Charges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Core.Admin;
+    using Prsd.Core.Mediator;
+
+    public class FetchInvoiceRunCSV : IRequest<CSVFileData>
+    {
+    }
+}
