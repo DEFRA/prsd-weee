4c684a167fec06a3b1d09cf411c3021efffbd46c
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2016-01-06 16:31:09 +0000

    Classes to produce the invoice run CSV

---
+++ src/EA.Weee.RequestHandlers/Charges/FetchInvoiceRunCSV/FetchInvoiceRunCSVHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Weee.RequestHandlers.Charges.FetchInvoiceRunCSV
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Core.Admin;
+    using Prsd.Core.Mediator;
+    using Requests.Charges;
+
+    public class FetchInvoiceRunCSVHandler : IRequestHandler<Requests.Charges.FetchInvoiceRunCSV, CSVFileData>
+    {
+        public Task<CSVFileData> HandleAsync(FetchInvoiceRunCSV message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
