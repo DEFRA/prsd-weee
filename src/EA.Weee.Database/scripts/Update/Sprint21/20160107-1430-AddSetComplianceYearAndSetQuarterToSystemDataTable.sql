55b60bfad35eec6aad6a4f1b0bd4577986b01037
Rick Powell <richard.powell@sfwltd.co.uk>
2016-01-07 16:31:46 +0000

    Add static quarter and static compliance year settings to system data and access to them using QuarterFactory

---
+++ src/EA.Weee.Database/scripts/Update/Sprint21/20160107-1430-AddSetComplianceYearAndSetQuarterToSystemDataTable.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [dbo].[SystemData]
+ADD
+	[UseSetComplianceYearAndQuarter] [bit] NOT NULL DEFAULT ((0)),
+	[SetComplianceYear] [int] NOT NULL DEFAULT ((2016)),
+	[SetQuarter] [int] NOT NULL DEFAULT ((1))
+GO
