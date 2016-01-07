e45cf0fdb3aadd147228ab2d8f765ebe67d44485
Rick Powell <richard.powell@sfwltd.co.uk>
2016-01-07 16:31:46 +0000

    Add static quarter and static compliance year settings to system data and access to them using QuarterFactory

---
+++ src/EA.Weee.RequestHandlers/Factories/IQuarterFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.RequestHandlers.Factories
+{
+    using System.Threading.Tasks;
+    using Domain.DataReturns;
+
+    public interface IQuarterFactory
+    {
+        Task<Quarter> GetCurrent();
+
+        Task SetCurrent(Quarter quarter);
+    }
+}
