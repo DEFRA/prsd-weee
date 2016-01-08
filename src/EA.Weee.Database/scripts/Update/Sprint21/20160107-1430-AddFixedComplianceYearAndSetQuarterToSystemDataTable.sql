88167145c25003affe442948705b477d0473768d
Rick Powell <richard.powell@sfwltd.co.uk>
2016-01-08 10:43:41 +0000

    Make fixed compliance year and fixed quarter nullable

--- src/EA.Weee.Database/scripts/Update/Sprint21/20160107-1430-AddFixedComplianceYearAndSetQuarterToSystemDataTable.sql
+++ src/EA.Weee.Database/scripts/Update/Sprint21/20160107-1430-AddFixedComplianceYearAndSetQuarterToSystemDataTable.sql
@@ -1,6 +1,6 @@
 ﻿ALTER TABLE [dbo].[SystemData]
 ADD
 	[UseFixedComplianceYearAndQuarter] [bit] NOT NULL DEFAULT ((0)),
-	[FixedComplianceYear] [int] NOT NULL DEFAULT ((2016)),
-	[FixedQuarter] [int] NOT NULL DEFAULT ((1))
+	[FixedComplianceYear] [int] NULL,
+	[FixedQuarter] [int] NULL
 GO
