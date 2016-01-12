6c658ad0a693a6ad4e22e12acbb9fb5a12306967
Priety Mahajan <PrietyM@sfwltd.co.uk>
2016-01-12 12:39:39 +0000

    Renamed file

--- src/EA.Weee.Requests/Admin/Reports/GetProducerEEEDataCSV.cs
+++ src/EA.Weee.Requests/Admin/Reports/GetProducerEEEDataCSV.cs
@@ -4,12 +4,12 @@
     using Core.Shared;
     using Prsd.Core.Mediator;
 
-    public class GetProducersEEEDataCSV : IRequest<CSVFileData>
+    public class GetProducerEEEDataCSV : IRequest<CSVFileData>
     {
         public int ComplianceYear { get; private set; }
 
         public ObligationType ObligationType { get; private set; }
-        public GetProducersEEEDataCSV(int complianceYear, ObligationType obligationType)
+        public GetProducerEEEDataCSV(int complianceYear, ObligationType obligationType)
         {
             ComplianceYear = complianceYear;
             ObligationType = obligationType;
