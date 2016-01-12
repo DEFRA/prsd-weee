6d9f45531f3361feb3d873262fe5601bbc9a66bc
Graham Alexander-Thomson <GrahamT@sfwltd.co.uk>
2016-01-12 18:54:24 +0000

    Added request handlers to populate pre-filters.

---
+++ src/EA.Weee.Requests/Charges/FetchComplianceYearsWithInvoices.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Weee.Requests.Charges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.Core.Shared;
+
+    /// <summary>
+    /// This request returns each compliance year that has at least one invoiced member upload
+    /// for the specified authority.
+    /// Years are returned in ascending numerical order.
+    /// </summary>
+    public class FetchComplianceYearsWithInvoices : IRequest<IReadOnlyList<int>>
+    {
+        public CompetentAuthority Authority { get; private set; }
+
+        public FetchComplianceYearsWithInvoices(CompetentAuthority authority)
+        {
+            Authority = authority;
+        }
+    }
+}
