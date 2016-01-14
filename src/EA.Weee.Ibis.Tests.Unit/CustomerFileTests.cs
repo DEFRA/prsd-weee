7de98b04417cd3d4774b2ac0503795d6ff9e2125
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2016-01-14 16:41:49 +0000

    Refactor code to remove post code logic from Ibis classes.

--- src/EA.Weee.Ibis.Tests.Unit/CustomerFileTests.cs
+++ src/EA.Weee.Ibis.Tests.Unit/CustomerFileTests.cs
@@ -13,7 +13,7 @@
         public void Write_WithOneCustomer_GeneratesCorrectOutput()
         {
             // Arrange
-            Address address = new Address("1 High Street", "Flat 123", "Business Park", "Newtown", "Nr Oldtown", "Testshire", "AA1 1AA", "England", true);
+            Address address = new Address("1 High Street", "Flat 123", "Business Park", "Newtown", "Nr Oldtown", "Testshire", "AA1 1AA");
             Customer customer = new Customer("WEE0001", "Test customer", address);
 
             CustomerFile file = new CustomerFile("WEE", 0);
@@ -28,30 +28,6 @@
             string expectedOutput =
                 "\"H\",\"0000000\",\"WEE\",\"H\",\"C\",\"00000\",\"" + expectedDate + "\"" + Environment.NewLine +
                 "\"D\",\"0000001\",\"WEE0001\",\"Test customer\",\"1 High Street\",\"Flat 123\",\"Business Park\",\"Newtown\",\"Nr Oldtown\",\"Testshire\",\"AA1 1AA\"" + Environment.NewLine +
-                "\"T\",\"0000002\",\"0000003\"" + Environment.NewLine;
-
-            Assert.Equal(expectedOutput, result);
-        }
-
-        [Fact]
-        public void Write_WithOneCustomer_WithNonUkAddress_ConcatenatesPostCodeAndCountry()
-        {
-            // Arrange
-            Address address = new Address("1 High Street", "Flat 123", "Business Park", "Newtown", "Nr Oldtown", "Testshire", "AA1 1AA", "Netherlands", false);
-            Customer customer = new Customer("WEE0001", "Test customer", address);
-
-            CustomerFile file = new CustomerFile("WEE", 0);
-
-            file.AddCustomer(customer);
-
-            // Act
-            string result = file.Write();
-
-            // Assert
-            string expectedDate = DateTime.UtcNow.Date.ToString("dd-MMM-yyyy").ToUpperInvariant();
-            string expectedOutput =
-                "\"H\",\"0000000\",\"WEE\",\"H\",\"C\",\"00000\",\"" + expectedDate + "\"" + Environment.NewLine +
-                "\"D\",\"0000001\",\"WEE0001\",\"Test customer\",\"1 High Street\",\"Flat 123\",\"Business Park\",\"Newtown\",\"Nr Oldtown\",\"Testshire\",\"AA1 1AA  Netherlands\"" + Environment.NewLine +
                 "\"T\",\"0000002\",\"0000003\"" + Environment.NewLine;
 
             Assert.Equal(expectedOutput, result);
