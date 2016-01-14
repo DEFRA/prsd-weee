322a760999ea1dcaae3a6a83fa842e0ef2733616
Priety Mahajan <PrietyM@sfwltd.co.uk>
2016-01-14 12:20:43 +0000

    removed unused usings

--- src/EA.Weee.RequestHandlers/DataReturns/BusinessValidation/EeeValidator.cs
+++ src/EA.Weee.RequestHandlers/DataReturns/BusinessValidation/EeeValidator.cs
@@ -2,10 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
     using Core.Shared;
-    using Domain;
     using Domain.DataReturns;
     using Domain.Lookup;
     using Domain.Producer;
