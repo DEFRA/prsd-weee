bdfa5d88eb1ef74cc25d5edb2a7f1500e9700b78
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2016-01-15 14:43:59 +0000

    Fix EEE report stored procedure and add test to ensure that it works as expected.

---
+++ src/EA.Weee.DataAccess.Tests.DataAccess/StoredProcedure/SpgProducerEEECSVDataByComplianceYearAndObligationTypeTests.cs
@@ -0,0 +1,48 @@
+﻿namespace EA.Weee.DataAccess.Tests.DataAccess.StoredProcedure
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Weee.Tests.Core.Model;
+    using Xunit;
+
+    public class SpgProducerEEECSVDataByComplianceYearAndObligationTypeTests
+    {
+        [Fact]
+        public async Task Execute_ReturnsEeeData_ForNonRemovedProducersOnly()
+        {
+            using (DatabaseWrapper db = new DatabaseWrapper())
+            {
+                // Arrange
+                ModelHelper helper = new ModelHelper(db.Model);
+
+                var scheme = helper.CreateScheme();
+                var memberUpload = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload.ComplianceYear = 2016;
+
+                var producer1 = helper.CreateProducerAsCompany(memberUpload, "PRN123");
+                producer1.RegisteredProducer.Removed = true;
+
+                var producer2 = helper.CreateProducerAsCompany(memberUpload, "PRN789");
+
+                var dataReturnVersion = helper.CreateDataReturnVersion(scheme, 2016, 1);
+
+                helper.CreateEeeOutputAmount(dataReturnVersion, producer1.RegisteredProducer, "B2C", 1, 100);
+                helper.CreateEeeOutputAmount(dataReturnVersion, producer2.RegisteredProducer, "B2C", 1, 200);
+
+                db.Model.SaveChanges();
+
+                // Act
+                var results =
+                    await db.StoredProcedures.SpgProducerEEECSVDataByComplianceYearAndObligationType(2016, "B2C");
+
+                // Assert
+                Assert.Equal(1, results.Count);
+
+                var data = results.Single();
+
+                Assert.Equal("PRN789", data.PRN);
+                Assert.Equal(200, data.Cat1Q1);
+            }
+        }
+    }
+}
