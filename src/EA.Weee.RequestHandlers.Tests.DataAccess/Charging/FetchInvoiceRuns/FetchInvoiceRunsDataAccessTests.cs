a1e373d81e0ce029bbdcf604c60b46d05808450c
Priety Mahajan <PrietyM@sfwltd.co.uk>
2016-01-18 14:16:36 +0000

    #41607 : Data access tests for Issued charges

--- src/EA.Weee.RequestHandlers.Tests.DataAccess/Charging/FetchInvoiceRuns/FetchInvoiceRunsDataAccessTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.DataAccess/Charging/FetchInvoiceRuns/FetchInvoiceRunsDataAccessTests.cs
@@ -2,12 +2,9 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
     using Charges.FetchInvoiceRuns;
     using Domain;
-    using FakeItEasy;
     using Weee.Tests.Core.Model;
     using Xunit;
 
