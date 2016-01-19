88a9c80a0f4b8ff3c7c404b360df1c7bed440966
Rick Powell <richard.powell@sfwltd.co.uk>
2016-01-19 13:23:54 +0000

    split GetAllComplianceYears request into GetActiveComplianceYears for DataReturns and MemberRegistrations

---
+++ src/EA.Weee.RequestHandlers/Admin/GetActiveComplianceYears/IGetDataReturnsActiveComplianceYearsDataAccess.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Weee.RequestHandlers.Admin.GetActiveComplianceYears
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IGetDataReturnsActiveComplianceYearsDataAccess
+    {
+        Task<List<int>> Get();
+    }
+}
---
+++ src/EA.Weee.Requests/Admin/GetActiveComplianceYears/GetDataReturnsActiveComplianceYears.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.Requests.Admin.GetActiveComplianceYears
+{
+    using System.Collections.Generic;
+    using Prsd.Core.Mediator;
+
+    public class GetDataReturnsActiveComplianceYears : IRequest<List<int>>
+    {
+    }
+}
