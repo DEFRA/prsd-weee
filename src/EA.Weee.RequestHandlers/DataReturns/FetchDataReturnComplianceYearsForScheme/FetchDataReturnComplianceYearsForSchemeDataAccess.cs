d2b5a4524b73a5129de991835f7e86571b1e72a2
Priety Mahajan <PrietyM@sfwltd.co.uk>
2016-01-25 12:55:32 +0000

    42839 : Added UI form manage EEE/WEEE data return

---
+++ src/EA.Weee.RequestHandlers/DataReturns/FetchDataReturnComplianceYearsForScheme/FetchDataReturnComplianceYearsForSchemeDataAccess.cs
@@ -0,0 +1,36 @@
+﻿namespace EA.Weee.RequestHandlers.DataReturns.FetchDataReturnForSubmission
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using DataAccess;
+
+    public class FetchDataReturnComplianceYearsForSchemeDataAccess : IFetchDataReturnComplianceYearsForSchemeDataAccess
+    {
+        private readonly WeeeContext context;
+
+        public FetchDataReturnComplianceYearsForSchemeDataAccess(WeeeContext dbContext)
+        {
+            this.context = dbContext;
+        }
+
+        public async Task<List<int>> GetDataReturnComplianceYearsForScheme(Guid schemeId)
+        {
+            return await context.DataReturnsUploads
+                    .Where(dru => dru.Scheme.Id == schemeId)
+                    .Where(dru => dru.DataReturnVersion != null)
+                    .Where(dru => dru.ComplianceYear.HasValue)
+                    .Select(dru => (int)dru.ComplianceYear)
+                    .Distinct()
+                    .OrderByDescending(year => year)
+                    .ToListAsync();
+        }
+
+        public async Task<Domain.Scheme.Scheme> FetchSchemeByOrganisationIdAsync(Guid organisationId)
+        {
+            return await context.Schemes.SingleAsync(c => c.OrganisationId == organisationId);
+        }
+    }
+}
