9d67a3a5b5017fd3550f5939ff092c3f2556e41e
Rick Powell <richard.powell@sfwltd.co.uk>
2016-01-25 18:02:58 +0000

    Update after PR Review

--- src/EA.Weee.RequestHandlers.Tests.Unit/Mapping/ProducerEeeMapTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Mapping/ProducerEeeMapTests.cs
@@ -47,14 +47,14 @@
             var result = map.Map(data);
 
             Assert.NotEmpty(result.Q1EEE);
-            Assert.Equal(tonnage, result.Q1EEE.Sum(eee => eee.Tonnage));
-
-            Assert.NotEmpty(result.TotalEee);
-            Assert.Equal(tonnage, result.TotalEee.Sum(eee => eee.Tonnage));
+            Assert.Single(result.Q1EEE);
+            Assert.Equal(tonnage, result.Q1EEE.Single().Tonnage);
         }
 
         [Fact]
-        public void FirstAndSecondQuarterHasEeeDataForTheSameCategoryAndObligationType_Q1ContainsTonnage_AndQ2ContainsTonnage_AndTotalEeeContainsSumOfTonnage()
+        public void
+            FirstAndSecondQuarterHasEeeDataForTheSameCategoryAndObligationType_Q1ContainsTonnage_AndQ2ContainsTonnage_AndTotalEeeContainsSingleSumOfTonnage
+            ()
         {
             var map = new ProducerEeeMap();
 
@@ -85,14 +85,104 @@
 
             var result = map.Map(data);
 
-            Assert.NotEmpty(result.Q1EEE);
-            Assert.Equal(tonnageForQ1, result.Q1EEE.Sum(eee => eee.Tonnage));
+            Assert.Single(result.Q1EEE);
+            Assert.Equal(tonnageForQ1, result.Q1EEE.Single().Tonnage);
 
-            Assert.NotEmpty(result.Q1EEE);
-            Assert.Equal(tonnageForQ2, result.Q2EEE.Sum(eee => eee.Tonnage));
+            Assert.Single(result.Q2EEE);
+            Assert.Equal(tonnageForQ2, result.Q2EEE.Single().Tonnage);
 
-            Assert.NotEmpty(result.TotalEee);
-            Assert.Equal(tonnageForQ1 + tonnageForQ2, result.TotalEee.Sum(eee => eee.Tonnage));
+            Assert.Single(result.TotalEee);
+            Assert.Equal(tonnageForQ1 + tonnageForQ2, result.TotalEee.Single().Tonnage);
+        }
+
+        [Fact]
+        public void FirstAndSecondQuarterHasEeeDataForTheSameCategoryButDifferentObligationType_Q1ContainsTonnage_AndQ2ContainsTonnage_AndTotalEeeTonnageIsSeparatedIntoObligationTypes()
+        {
+            var map = new ProducerEeeMap();
+
+            var scheme = new Scheme(Guid.NewGuid());
+            var producer = new RegisteredProducer("ABC12345", 2016, scheme);
+            const decimal tonnageForQ1 = 10M;
+            const decimal tonnageForQ2 = 25.6M;
+            const ObligationType obligationTypeForQ1 = ObligationType.B2B;
+            const ObligationType obligationTypeForQ2 = ObligationType.B2C;
+
+            var data = new List<ProducerEeeByQuarter>
+            {
+                new ProducerEeeByQuarter
+                {
+                    Eee = new List<EeeOutputAmount>
+                    {
+                        new EeeOutputAmount(obligationTypeForQ1, WeeeCategory.AutomaticDispensers, tonnageForQ1, producer)
+                    },
+                    Quarter = new Quarter(2016, QuarterType.Q1)
+                },
+                new ProducerEeeByQuarter
+                {
+                    Eee = new List<EeeOutputAmount>
+                    {
+                        new EeeOutputAmount(obligationTypeForQ2, WeeeCategory.AutomaticDispensers, tonnageForQ2, producer)
+                    },
+                    Quarter = new Quarter(2016, QuarterType.Q2)
+                }
+            };
+
+            var result = map.Map(data);
+
+            Assert.Single(result.Q1EEE);
+            Assert.Equal(tonnageForQ1, result.Q1EEE.Single().Tonnage);
+
+            Assert.Single(result.Q2EEE);
+            Assert.Equal(tonnageForQ2, result.Q2EEE.Single().Tonnage);
+
+            Assert.Equal(2, result.TotalEee.Count);
+            Assert.Single(result.TotalEee.Where(eee => eee.Tonnage == tonnageForQ1 && (ObligationType)(int)eee.ObligationType == obligationTypeForQ1));
+            Assert.Single(result.TotalEee.Where(eee => eee.Tonnage == tonnageForQ2 && (ObligationType)(int)eee.ObligationType == obligationTypeForQ2));
+        }
+
+        [Fact]
+        public void FirstAndSecondQuarterHasEeeDataForTheSameObligationTypeButDifferentCategory_Q1ContainsTonnage_AndQ2ContainsTonnage_AndTotalEeeTonnageIsSeparatedIntoTwoCategoryTypes()
+        {
+            var map = new ProducerEeeMap();
+
+            var scheme = new Scheme(Guid.NewGuid());
+            var producer = new RegisteredProducer("ABC12345", 2016, scheme);
+            const decimal tonnageForQ1 = 10M;
+            const decimal tonnageForQ2 = 25.6M;
+            const WeeeCategory categoryForQ1 = WeeeCategory.AutomaticDispensers;
+            const WeeeCategory categoryForQ2 = WeeeCategory.ConsumerEquipment;
+
+            var data = new List<ProducerEeeByQuarter>
+            {
+                new ProducerEeeByQuarter
+                {
+                    Eee = new List<EeeOutputAmount>
+                    {
+                        new EeeOutputAmount(ObligationType.B2B, categoryForQ1, tonnageForQ1, producer)
+                    },
+                    Quarter = new Quarter(2016, QuarterType.Q1)
+                },
+                new ProducerEeeByQuarter
+                {
+                    Eee = new List<EeeOutputAmount>
+                    {
+                        new EeeOutputAmount(ObligationType.B2B, categoryForQ2, tonnageForQ2, producer)
+                    },
+                    Quarter = new Quarter(2016, QuarterType.Q2)
+                }
+            };
+
+            var result = map.Map(data);
+
+            Assert.Single(result.Q1EEE);
+            Assert.Equal(tonnageForQ1, result.Q1EEE.Single().Tonnage);
+
+            Assert.Single(result.Q2EEE);
+            Assert.Equal(tonnageForQ2, result.Q2EEE.Single().Tonnage);
+
+            Assert.Equal(2, result.TotalEee.Count);
+            Assert.Single(result.TotalEee.Where(eee => eee.Tonnage == tonnageForQ1 && (WeeeCategory)(int)eee.Category == categoryForQ1));
+            Assert.Single(result.TotalEee.Where(eee => eee.Tonnage == tonnageForQ2 && (WeeeCategory)(int)eee.Category == categoryForQ2));
         }
     }
 }
