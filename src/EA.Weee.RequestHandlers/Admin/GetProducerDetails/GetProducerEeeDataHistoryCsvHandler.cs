974169d4092937a1d3241d4d24c0d40c7eecf1d9
Priety Mahajan <PrietyM@sfwltd.co.uk>
2016-01-27 11:05:59 +0000

    Removed unused using statements

---
+++ src/EA.Weee.RequestHandlers/Admin/GetProducerDetails/GetProducerEeeDataHistoryCsvHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace EA.Weee.RequestHandlers.Admin
+{
+    using System;
+    using System.Threading.Tasks;
+    using Core.Admin;
+    using Core.Shared;
+    using DataAccess;
+    using Prsd.Core.Mediator;
+    using Requests.Admin;
+    using Security;
+
+    internal class GetProducerEeeDataHistoryCsvHandler : IRequestHandler<GetProducerEeeDataHistoryCsv, CSVFileData>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly WeeeContext context;
+        private readonly CsvWriterFactory csvWriterFactory;
+
+        public GetProducerEeeDataHistoryCsvHandler(IWeeeAuthorization authorization, WeeeContext context,
+            CsvWriterFactory csvWriterFactory)
+        {
+            this.authorization = authorization;
+            this.context = context;
+            this.csvWriterFactory = csvWriterFactory;
+        }
+
+        public async Task<CSVFileData> HandleAsync(GetProducerEeeDataHistoryCsv request)
+        {
+            authorization.EnsureCanAccessInternalArea();
+            if (string.IsNullOrEmpty(request.PRN))
+            {
+                throw new ArgumentException("PRN is required.");
+            }
+            var fileName = string.Format("{0}_producerEeehistory_{1:ddMMyyyy_HHmm}.csv",
+                request.PRN,
+                DateTime.UtcNow);
+
+            return new CSVFileData
+            {
+                FileContent = string.Empty,
+                FileName = fileName
+            };
+        }        
+    }
+}
