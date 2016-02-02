05b4a0f6248d9015f264317a99b0d9646ad5901e
Rick Powell <richard.powell@sfwltd.co.uk>
2016-02-02 19:37:54 +0000

    More efficient handler for SchemeWeee report

---
+++ src/EA.Weee.RequestHandlers.Tests.DataAccess/Admin/Reports/GetSchemeWeeeCsvHandlerTests.cs
@@ -0,0 +1,53 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.DataAccess.Admin.Reports
+{
+    using System;
+    using System.Diagnostics;
+    using Core.Shared;
+    using FakeItEasy;
+    using Prsd.Core.Domain;
+    using RequestHandlers.Admin.Reports;
+    using Requests.Admin.Reports;
+    using Security;
+    using Weee.DataAccess;
+    using Weee.Tests.Core.Model;
+    using Xunit;
+
+    public class GetSchemeWeeeCsvHandlerTests
+    {
+        [Theory(Skip = "This should be used only to test the processing time of this handler")]
+        [InlineData(2017, ObligationType.B2B)]
+        [InlineData(2017, ObligationType.B2C)]
+        [InlineData(2016, ObligationType.B2B)]
+        [InlineData(2016, ObligationType.B2C)]
+        public async void GivenVariousDifferingParameters_RunningTimeIsUnderTenSeconds(int complianceYear, ObligationType obligationType)
+        {
+            long processingTime;
+
+            using (var databaseWrapper = new DatabaseWrapper())
+            {
+                var stopWatch = Stopwatch.StartNew();
+                    await
+                        Handler(databaseWrapper.WeeeContext)
+                            .HandleAsync(new GetSchemeWeeeCsv(complianceYear, obligationType));
+                stopWatch.Stop();
+                processingTime = stopWatch.ElapsedMilliseconds;
+            }
+
+            Assert.InRange(processingTime, 0, 10000);
+        }
+
+        private GetSchemeWeeeCsvHandler Handler(WeeeContext context)
+        {
+            var userId = context.GetCurrentUser();
+
+            var userContext = A.Fake<IUserContext>();
+            A.CallTo(() => userContext.UserId)
+                .Returns(Guid.Parse(userId));
+
+            var authorization = A.Fake<IWeeeAuthorization>();
+
+            return new GetSchemeWeeeCsvHandler(context, authorization,
+                new CsvWriterFactory(new NoFormulaeExcelSanitizer()));
+        }
+    }
+}
