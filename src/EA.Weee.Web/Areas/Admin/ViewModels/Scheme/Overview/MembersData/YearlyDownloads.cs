7ad2f8ab5bbeecca96e9520f55bdec78f3c8d32f
Rick Powell <richard.powell@sfwltd.co.uk>
2016-02-04 14:33:21 +0000

    Initial UI development

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Scheme/Overview/MembersData/YearlyDownloads.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Scheme.Overview.MembersData
+{
+    public class YearlyDownloads
+    {
+        public int Year { get; set; }
+
+        public bool IsMembersDownloadAvailable { get; set; }
+
+        public bool IsDataReturnsDownloadAvailable { get; set; }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Scheme/Overview/PcsDetails/PcsDetailsOverviewViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Scheme.Overview.PcsDetails
+{
+    using System;
+
+    public class PcsDetailsOverviewViewModel : OverviewViewModel
+    {
+        public string ApprovalNumber { get; set; }
+
+        public string BillingReference { get; set; }
+
+        public string ObligationType { get; set; }
+
+        public string AppropriateAuthority { get; set; }
+
+        public string Status { get; set; }
+
+        public PcsDetailsOverviewViewModel(Guid schemeId, string schemeName) 
+            : base(schemeId, schemeName, OverviewDisplayOption.PcsDetails)
+        {
+        }
+    }
+}
