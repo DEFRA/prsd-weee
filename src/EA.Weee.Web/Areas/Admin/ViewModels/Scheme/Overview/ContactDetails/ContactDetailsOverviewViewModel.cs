de29b321913704f865c2543ac8ae45048e27a01f
Rick Powell <richard.powell@sfwltd.co.uk>
2016-02-04 17:47:53 +0000

    UI ready for data

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Scheme/Overview/ContactDetails/ContactDetailsOverviewViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Scheme.Overview.ContactDetails
+{
+    using System;
+
+    public class ContactDetailsOverviewViewModel : OverviewViewModel
+    {
+        public Guid OrganisationId { get; set; }
+
+        public ContactDetailsOverviewViewModel(Guid schemeId, string schemeName)
+            : base(schemeId, schemeName, OverviewDisplayOption.ContactDetails)
+        {
+        }
+    }
+}
