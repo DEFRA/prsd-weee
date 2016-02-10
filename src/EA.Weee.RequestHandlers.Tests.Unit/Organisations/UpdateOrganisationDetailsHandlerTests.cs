0eb71a6635528f94e1949bfadddf154b70513239
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2016-02-10 14:16:56 +0530

    Added hide edit organisation details link when user is standard role.

--- src/EA.Weee.RequestHandlers.Tests.Unit/Organisations/UpdateOrganisationDetailsHandlerTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Organisations/UpdateOrganisationDetailsHandlerTests.cs
@@ -1,18 +1,65 @@
 ﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Organisations
 {
     using System;
+    using System.Security;
     using System.Threading.Tasks;
+    using Core.Security;
+    using DataAccess.Identity;
     using EA.Weee.Core.Organisations;
     using EA.Weee.Domain;
     using EA.Weee.Domain.Organisation;
     using EA.Weee.RequestHandlers.Organisations;
     using EA.Weee.Requests.Organisations;
     using FakeItEasy;
+    using Microsoft.AspNet.Identity;
+    using RequestHandlers.Admin;
+    using RequestHandlers.Security;
+    using Requests.Admin;
+    using Weee.Tests.Core;
     using Xunit;
     using OrganisationType = Core.Organisations.OrganisationType;
 
     public class UpdateOrganisationDetailsHandlerTests
-    {   
+    {
+        [Theory]
+        [Trait("Authorization", "Internal")]
+        [InlineData(AuthorizationBuilder.UserType.Unauthenticated)]
+        [InlineData(AuthorizationBuilder.UserType.External)]
+        public async Task HandleAsync_WithNonInternalAccess_ThrowsSecurityException(AuthorizationBuilder.UserType userType)
+        {
+            // Arrange
+            var authorization = AuthorizationBuilder.CreateFromUserType(userType);
+            var dataAccess = A.Fake<IOrganisationDetailsDataAccess>();
+
+            var handler = new UpdateOrganisationDetailsHandler(dataAccess, authorization);
+            
+            // Act
+            Func<Task> action = async () => await handler.HandleAsync(A<UpdateOrganisationDetails>._);
+
+            // Assert
+            await Assert.ThrowsAsync<SecurityException>(action);
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithNonInternalAdminRole_ThrowsSecurityException()
+        {
+            // Arrange
+            IWeeeAuthorization authorization = new AuthorizationBuilder()
+                .AllowInternalAreaAccess()
+                .DenyRole(Roles.InternalAdmin)
+                .Build();
+            
+            var dataAccess = A.Fake<IOrganisationDetailsDataAccess>();
+
+            var handler = new UpdateOrganisationDetailsHandler(dataAccess, authorization);
+
+            // Act
+            Func<Task> action = async () => await handler.HandleAsync(A<UpdateOrganisationDetails>._);
+
+            // Assert
+            await Assert.ThrowsAsync<SecurityException>(action);
+        }
+
         [Fact]
         public async Task UpdateOrganisationDetailsHandler_WithValidData_FetchesOrganisationAndUpdatesAndSaves()
         {
@@ -36,6 +83,7 @@
             UpdateOrganisationDetails request = new UpdateOrganisationDetails(organisationData);
 
             IOrganisationDetailsDataAccess dataAccess = A.Fake<IOrganisationDetailsDataAccess>();
+            IWeeeAuthorization weeeAuthorization = A.Fake<IWeeeAuthorization>();
 
             Organisation organisation = A.Dummy<Organisation>();
             A.CallTo(() => dataAccess.FetchOrganisationAsync(new Guid("9a310218-311b-460d-bd50-9d246c237dcc")))
@@ -47,7 +95,7 @@
             A.CallTo(() => dataAccess.FetchCountryAsync(new Guid("79b70dfb-bbfd-4801-9849-880f66ee48e4")))
                 .Returns(country);
 
-            UpdateOrganisationDetailsHandler handler = new UpdateOrganisationDetailsHandler(dataAccess);
+            UpdateOrganisationDetailsHandler handler = new UpdateOrganisationDetailsHandler(dataAccess, weeeAuthorization);
 
             // Act
             bool result = await handler.HandleAsync(request);
