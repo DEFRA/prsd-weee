f62a430b5eb493c7e28e106ad1e879d0dd2c3f8f
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2016-02-12 10:31:06 +0000

    Raise event on submission

--- src/EA.Weee.Domain.Tests.Unit/DataReturns/DataReturnVersionTests.cs
+++ src/EA.Weee.Domain.Tests.Unit/DataReturns/DataReturnVersionTests.cs
@@ -1,9 +1,11 @@
 ﻿namespace EA.Weee.Domain.Tests.Unit.DataReturns
 {
     using System;
+    using System.Linq;
     using Domain.DataReturns;
     using Domain.Producer;
     using Domain.Scheme;
+    using Events;
     using FakeItEasy;
     using Lookup;
     using Xunit;
@@ -41,6 +43,22 @@
 
             // Assert
             Assert.Throws<InvalidOperationException>(action);
+        }
+
+        [Fact]
+        public void Submit_AddSubmissionEventToEventsList()
+        {
+            // Arrange
+            var dataReturn = new DataReturn(A.Dummy<Scheme>(), A.Dummy<Quarter>());
+            var dataReturnVersion = new DataReturnVersion(dataReturn);
+
+            // Act
+            dataReturnVersion.Submit("test@co.uk");
+
+            // Assert
+            Assert.Equal(1, dataReturnVersion.Events.Count());
+            Assert.IsType<SchemeDataReturnSubmissionEvent>(dataReturnVersion.Events.Single());
+            Assert.Same(dataReturnVersion, ((SchemeDataReturnSubmissionEvent)dataReturnVersion.Events.Single()).DataReturnVersion);
         }
 
         [Fact]
