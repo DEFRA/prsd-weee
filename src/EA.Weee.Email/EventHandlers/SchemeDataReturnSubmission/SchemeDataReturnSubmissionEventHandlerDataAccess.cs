e0a2414e2f8d6796c56e011b75361191540e7ab0
Muhammad Ismail <MuhammadI@sfwltd.co.uk>
2016-02-12 16:29:32 +0000

    Add functionality to send notification emails when a data return has been submitted

---
+++ src/EA.Weee.Email/EventHandlers/SchemeDataReturnSubmission/SchemeDataReturnSubmissionEventHandlerDataAccess.cs
@@ -0,0 +1,29 @@
+﻿namespace EA.Weee.Email.EventHandlers.SchemeDataReturnSubmission
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using DataAccess;
+    using Domain.DataReturns;
+    using Domain.Scheme;
+
+    public class SchemeDataReturnSubmissionEventHandlerDataAccess : ISchemeDataReturnSubmissionEventHandlerDataAccess
+    {
+        private readonly WeeeContext weeeContext;
+
+        public SchemeDataReturnSubmissionEventHandlerDataAccess(WeeeContext weeeContext)
+        {
+            this.weeeContext = weeeContext;
+        }
+
+        public Task<int> GetNumberOfDataReturnSubmissionsAsync(Scheme scheme, int complianceYear, QuarterType quarter)
+        {
+            return weeeContext.DataReturnVersions
+                .Where(d => d.SubmittedDate.HasValue)
+                .Where(d => d.DataReturn.Scheme.Id == scheme.Id)
+                .Where(d => d.DataReturn.Quarter.Year == complianceYear)
+                .Where(d => d.DataReturn.Quarter.Q == quarter)
+                .CountAsync();
+        }
+    }
+}
