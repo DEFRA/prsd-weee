002d0aabd7c36f013664b9df2febbbf6d17e53c8
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2016-02-15 16:06:33 +0530

    Restrict Issue charges for standard user

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Charge/ManagePendingChargesViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Charge
+{
+    using System.Collections.Generic;
+    using Core.Charges;
+
+    public class ManagePendingChargesViewModel
+    {
+        public IList<PendingCharge> PendingCharges { get; set; }
+
+        public bool CanUserIssueCharges { get; set; }
+    }
+}
