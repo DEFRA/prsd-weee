e06f7791c85a2fc028d8da27346b4adb2e562ba8
Rick Powell <richard.powell@sfwltd.co.uk>
2016-02-17 09:55:03 +0000

    revert spacing

--- src/EA.Weee.Web/RazorHelpers/WeeeGds.Tab.cs
+++ src/EA.Weee.Web/RazorHelpers/WeeeGds.Tab.cs
@@ -1,6 +1,7 @@
 ﻿namespace EA.Weee.Web.RazorHelpers
 {
     using System.Web.Mvc;
+
     public partial class WeeeGds<TModel>
     {
         public MvcHtmlString Tab(string displayText, string url, bool isActive)
@@ -8,12 +9,15 @@
             var linkBuilder = new TagBuilder("a");
             linkBuilder.Attributes.Add("href", url);
             linkBuilder.SetInnerText(displayText);
+
             var tagBuilder = new TagBuilder("li");
             tagBuilder.InnerHtml = linkBuilder.ToString();
+
             if (isActive)
             {
                 tagBuilder.Attributes.Add("class", "active");
             }
+
             return new MvcHtmlString(tagBuilder.ToString());
         }
     }
