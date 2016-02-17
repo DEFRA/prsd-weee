477a158aeb3a2440d91078441db3ff4d1c45698d
Dhavalkumar Shah <DhavalkumarS@sfwltd.co.uk>
2016-02-17 15:25:33 +0530

    45709:Add compliance year and quarter to the "successfully submitted" screen

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/DataReturns/FetchDataReturnForSubmission/GetQuarterInfoByDataReturnUploadIdTests.cs
@@ -0,0 +1,39 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.DataReturns.FetchDataReturnForSubmission
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Domain.DataReturns;
+    using Domain.Scheme;
+    using FakeItEasy;
+    using RequestHandlers.DataReturns.FetchDataReturnForSubmission;
+    using Xunit;
+
+    public class GetQuarterInfoByDataReturnUploadIdTests
+    {
+        [Fact]
+        public async Task HandleAsync_Happypath_ReturnsQuarterInfo()
+        {
+            DataReturnUpload dataReturnsUpload = new DataReturnUpload(
+               A.Dummy<Scheme>(),
+               A.Dummy<string>(),
+               new List<DataReturnUploadError>(),
+               A.Dummy<string>(),
+               2016,
+               1);
+
+            IFetchDataReturnForSubmissionDataAccess dataAccess = A.Fake<IFetchDataReturnForSubmissionDataAccess>();
+            A.CallTo(() => dataAccess.FetchDataReturnUploadByIdAsync(A<Guid>._)).Returns(dataReturnsUpload);
+
+            GetQuarterInfoByDataReturnUploadIdHandler handler = new GetQuarterInfoByDataReturnUploadIdHandler(dataAccess);
+
+            Requests.DataReturns.GetQuarterInfoByDataReturnUploadId request = new Requests.DataReturns.GetQuarterInfoByDataReturnUploadId(
+                A.Dummy<Guid>());
+
+            var result = await handler.HandleAsync(request);
+
+            Assert.Equal(2016, result.Year);
+            Assert.Equal(1, (int)result.Quarter.Value);
+        }
+    }
+}
---
+++ src/EA.Weee.Requests/DataReturns/GetQuarterInfoByDataReturnUploadId.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Requests.DataReturns
+{
+    using System;
+    using Core.DataReturns;
+    using Prsd.Core.Mediator;
+
+    public class GetQuarterInfoByDataReturnUploadId : IRequest<QuarterInfo>
+    {
+        public Guid DataReturnUploadId { get; private set; }
+
+        public GetQuarterInfoByDataReturnUploadId(Guid dataReturnUploadId)
+        {
+            DataReturnUploadId = dataReturnUploadId;
+        }
+    }
+}
