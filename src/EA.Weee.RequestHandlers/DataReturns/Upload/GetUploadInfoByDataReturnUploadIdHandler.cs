17ed9e860dc1f7d9f8e9e4279461eee28e959c97
Priety Mahajan <PrietyM@sfwltd.co.uk>
2016-02-17 16:04:38 +0000

    check for scheme access and updated tests to check for security exception.

--- src/EA.Weee.RequestHandlers/DataReturns/Upload/GetUploadInfoByDataReturnUploadIdHandler.cs
+++ src/EA.Weee.RequestHandlers/DataReturns/Upload/GetUploadInfoByDataReturnUploadIdHandler.cs
@@ -5,20 +5,24 @@
     using Domain.DataReturns;
     using EA.Prsd.Core.Mediator;
     using Requests.DataReturns;
+    using Security;
     using QuarterType = Core.DataReturns.QuarterType;
 
     public class GetUploadInfoByDataReturnUploadIdHandler : IRequestHandler<GetUploadInfoByDataReturnUploadId, DataReturnUploadInfo>
     {
         private readonly IFetchDataReturnUploadDataAccess dataAccess;
+        private readonly IWeeeAuthorization authorization;
 
-        public GetUploadInfoByDataReturnUploadIdHandler(IFetchDataReturnUploadDataAccess dataAccess)
+        public GetUploadInfoByDataReturnUploadIdHandler(IWeeeAuthorization authorization, IFetchDataReturnUploadDataAccess dataAccess)
         {   
             this.dataAccess = dataAccess;
+            this.authorization = authorization;
         }
 
         public async Task<DataReturnUploadInfo> HandleAsync(GetUploadInfoByDataReturnUploadId message)
         {   
             DataReturnUpload dataReturnUpload = await dataAccess.FetchDataReturnUploadByIdAsync(message.DataReturnUploadId);
+            authorization.EnsureSchemeAccess(dataReturnUpload.Scheme.Id);
 
             var quarter = new DataReturnUploadInfo
             {
