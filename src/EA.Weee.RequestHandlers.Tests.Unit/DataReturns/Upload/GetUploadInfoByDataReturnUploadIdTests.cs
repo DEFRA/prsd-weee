5be24838476ccefa7f8d1005759af0413cfd467b
Priety Mahajan <PrietyM@sfwltd.co.uk>
2016-02-17 16:04:38 +0000

    check for scheme access and updated tests to check for security exception.

--- src/EA.Weee.RequestHandlers.Tests.Unit/DataReturns/Upload/GetUploadInfoByDataReturnUploadIdTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/DataReturns/Upload/GetUploadInfoByDataReturnUploadIdTests.cs
@@ -2,15 +2,28 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Security;
     using System.Threading.Tasks;
     using Domain.DataReturns;
     using Domain.Scheme;
     using FakeItEasy;
     using RequestHandlers.DataReturns.Upload;
+    using RequestHandlers.Security;
+    using Weee.Tests.Core;
     using Xunit;
 
     public class GetUploadInfoByDataReturnUploadIdTests
     {
+        [Fact]
+        public async Task HandleAsync_NoAccessToScheme_ThrowsSecurityException()
+        {
+            var authorization = new AuthorizationBuilder().DenySchemeAccess().Build();
+
+            var handler = new GetUploadInfoByDataReturnUploadIdHandler(authorization, A.Dummy<IFetchDataReturnUploadDataAccess>());
+
+            await Assert.ThrowsAsync<SecurityException>(() => handler.HandleAsync(A.Dummy<Requests.DataReturns.GetUploadInfoByDataReturnUploadId>()));
+        }
+
         [Fact]
         public async Task HandleAsync_Happypath_ReturnsQuarterInfo()
         {
@@ -21,11 +34,11 @@
                A.Dummy<string>(),
                2016,
                1);
-
+          
             IFetchDataReturnUploadDataAccess dataAccess = A.Fake<IFetchDataReturnUploadDataAccess>();
             A.CallTo(() => dataAccess.FetchDataReturnUploadByIdAsync(A<Guid>._)).Returns(dataReturnsUpload);
 
-            GetUploadInfoByDataReturnUploadIdHandler handler = new GetUploadInfoByDataReturnUploadIdHandler(dataAccess);
+            GetUploadInfoByDataReturnUploadIdHandler handler = new GetUploadInfoByDataReturnUploadIdHandler(A.Dummy<IWeeeAuthorization>(), dataAccess);
 
             Requests.DataReturns.GetUploadInfoByDataReturnUploadId request = new Requests.DataReturns.GetUploadInfoByDataReturnUploadId(
                 A.Dummy<Guid>());
