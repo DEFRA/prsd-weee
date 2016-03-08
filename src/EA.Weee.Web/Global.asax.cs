2b298b04a12ff21b56f78cf27a9f35248ae35305
Chris Smith <chriss@sfwltd.co.uk>
2016-03-08 15:45:51 +0000

    #47361 - Fix logging of passwords on website

--- src/EA.Weee.Web/Global.asax.cs
+++ src/EA.Weee.Web/Global.asax.cs
@@ -2,12 +2,24 @@
 {
     using System;
     using System.Web;
+    using Elmah;
+    using Logging;
 
     public class Global : HttpApplication
     {
         protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
         {
             HttpContext.Current.Response.Headers.Remove("Server");
+        }
+
+        protected void ErrorLog_Filtering(object sender, ExceptionFilterEventArgs args)
+        {
+            ElmahFilter.FilterSensitiveData(args);
+        }
+
+        protected void ErrorMail_Filtering(object sender, ExceptionFilterEventArgs args)
+        {
+            ElmahFilter.FilterSensitiveData(args);
         }
     }
 }
