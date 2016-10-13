db30345ea076137444b174f43cf8660c224cfdb0
Muhammad Ismail <muhammadi@sfwltd.co.uk>
2016-10-13 11:49:39 +0100

    Initial changes for adding CRN warning on producer amendment

---
+++ src/EA.Weee.XmlValidation/BusinessValidation/MemberRegistration/Rules/Producer/CompanyRegistrationNumberChange.cs
@@ -0,0 +1,39 @@
+﻿namespace EA.Weee.XmlValidation.BusinessValidation.MemberRegistration.Rules.Producer
+{
+    using Core.Helpers;
+    using QuerySets;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xml.MemberRegistration;
+
+    public class CompanyRegistrationNumberChange : ICompanyRegistrationNumberChange
+    {
+        private readonly IProducerQuerySet producerQuerySet;
+
+        public CompanyRegistrationNumberChange(IProducerQuerySet producerQuerySet)
+        {
+            this.producerQuerySet = producerQuerySet;
+        }
+
+        public RuleResult Evaluate(producerType element)
+        {
+            var result = RuleResult.Pass();
+
+            if (element.status == statusType.A &&
+                element.producerBusiness != null)
+            {
+                var company = element.producerBusiness.Item as companyType;
+                if (company != null)
+                {
+                    var companyNumber = CompanyRegistrationNumberFormatter
+                        .FormatCompanyRegistrationNumber(company.companyNumber);
+                }
+            }
+
+            return result;
+        }
+    }
+}
