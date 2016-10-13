3a596b5ce5f92e90d8da9a96a2bc0ac6001b2315
Muhammad Ismail <muhammadi@sfwltd.co.uk>
2016-10-13 11:49:39 +0100

    Initial changes for adding CRN warning on producer amendment

---
+++ src/EA.Weee.XmlValidation.Tests.Unit/BusinessValidation/MemberRegistration/Rules/Producer/CompanyRegistrationNumberChangeTests.cs
@@ -0,0 +1,46 @@
+﻿namespace EA.Weee.XmlValidation.Tests.Unit.BusinessValidation.MemberRegistration.Rules.Producer
+{
+    using EA.Weee.XmlValidation.BusinessValidation.MemberRegistration.QuerySets;
+    using EA.Weee.XmlValidation.Tests.Unit.BusinessValidation.MemberRegistration.Domain;
+    using FakeItEasy;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Weee.Domain.Obligation;
+    using Weee.Domain.Producer;
+    using Xml.MemberRegistration;
+    using XmlValidation.BusinessValidation.MemberRegistration.Rules.Producer;
+    using Xunit;
+
+    public class CompanyRegistrationNumberChangeTests
+    {
+        private readonly IProducerQuerySet producerQuerySet;
+
+        public CompanyRegistrationNumberChangeTests()
+        {
+            producerQuerySet = A.Fake<IProducerQuerySet>();
+        }
+
+        [Fact]
+        public void Evaluate_Insertion_ReturnsPass()
+        {
+            var result = new CompanyRegistrationNumberChange(producerQuerySet).Evaluate(new producerType() { status = statusType.I });
+
+            A.CallTo(() => producerQuerySet.GetLatestCompanyProducers()).MustNotHaveHappened();
+
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Evaluate_AmendmentNotCompanyProducer_ReturnsPass()
+        {
+            var result = new CompanyRegistrationNumberChange(producerQuerySet).Evaluate(new producerType() { status = statusType.A });
+
+            A.CallTo(() => producerQuerySet.GetLatestCompanyProducers()).MustNotHaveHappened();
+
+            Assert.True(result.IsValid);
+        }
+    }
+}
---
+++ src/EA.Weee.XmlValidation/BusinessValidation/MemberRegistration/Rules/Producer/ICompanyRegistrationNumberChange.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.XmlValidation.BusinessValidation.MemberRegistration.Rules.Producer
+{
+    using Xml.MemberRegistration;
+
+    public interface ICompanyRegistrationNumberChange
+    {
+        RuleResult Evaluate(producerType element);
+    }
+}
