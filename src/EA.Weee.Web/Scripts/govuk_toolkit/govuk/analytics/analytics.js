7bd22338c2d0b53d19db55ff824a7dfc5c394bf3
Chris Smith <chriss@sfwltd.co.uk>
2016-10-17 12:04:41 +0100

    #54552 - Update frontend toolkit (minus breadcrumb)

--- src/EA.Weee.Web/Scripts/govuk_toolkit/govuk/analytics/analytics.js
+++ src/EA.Weee.Web/Scripts/govuk_toolkit/govuk/analytics/analytics.js
@@ -1,6 +1,7 @@
-(function() {
+(function(global) {
   "use strict";
-  window.GOVUK = window.GOVUK || {};
+
+  var GOVUK = global.GOVUK || {};
 
   // For usage and initialisation see:
   // https://github.com/alphagov/govuk_frontend_toolkit/blob/master/docs/analytics.md#create-an-analytics-tracker
@@ -8,7 +9,9 @@
   var Analytics = function(config) {
     this.trackers = [];
     if (typeof config.universalId != 'undefined') {
-      this.trackers.push(new GOVUK.GoogleAnalyticsUniversalTracker(config.universalId, config.cookieDomain));
+      var universalId = config.universalId;
+      delete config.universalId;
+      this.trackers.push(new GOVUK.GoogleAnalyticsUniversalTracker(universalId, config));
     }
   };
 
@@ -61,4 +64,6 @@
   };
 
   GOVUK.Analytics = Analytics;
-})();
+
+  global.GOVUK = GOVUK;
+})(window);
