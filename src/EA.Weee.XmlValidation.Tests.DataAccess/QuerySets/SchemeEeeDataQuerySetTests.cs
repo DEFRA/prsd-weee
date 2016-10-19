dbeebcdb9a4a490fbf234d20e51c8e646e99918a
Muhammad Ismail <muhammadi@sfwltd.co.uk>
2016-10-19 10:45:51 +0100

    Add rule for obligation type change on member submission (In progress)

---
+++ src/EA.Weee.XmlValidation.Tests.DataAccess/QuerySets/SchemeEeeDataQuerySetTests.cs
@@ -0,0 +1,253 @@
+﻿namespace EA.Weee.XmlValidation.Tests.DataAccess.QuerySets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Domain.Obligation;
+    using Weee.Tests.Core.Model;
+    using XmlValidation.BusinessValidation.MemberRegistration.QuerySets;
+    using Xunit;
+
+    public class SchemeEeeDataQuerySetTests
+    {
+        [Fact]
+        public async Task HasProducerEeeDataForObligationType_ReturnsTrue_WhenDataAvailableForSpecifiedProducer()
+        {
+            using (DatabaseWrapper database = new DatabaseWrapper())
+            {
+                // Arrange
+                var schemeApprovalNumber = "WEE/AA0001YZ/SCH";
+
+                var helper = new ModelHelper(database.Model);
+
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = schemeApprovalNumber;
+
+                var memberUpload = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload.ComplianceYear = 2016;
+
+                var producer = helper.CreateProducerAsCompany(memberUpload, "WEE/MM0001AA");
+
+                var dataReturnVersion = helper.CreateDataReturnVersion(scheme, 2016, 1, isSubmitted: true);
+                var producerEee = helper.CreateEeeOutputAmount(dataReturnVersion, producer.RegisteredProducer, "B2C", 1, 1000);
+
+                database.Model.SaveChanges();
+
+                // Act
+                var querySet = new SchemeEeeDataQuerySet(schemeApprovalNumber, 2016, database.WeeeContext);
+                var result = await querySet.HasProducerEeeDataForObligationType("WEE/MM0001AA", ObligationType.B2C);
+
+                // Assert
+                Assert.True(result);
+            }
+        }
+
+        [Fact]
+        public async Task HasProducerEeeDataForObligationType_ReturnsFalse_WhenDataNotAvailableForSpecifiedProducer()
+        {
+            using (DatabaseWrapper database = new DatabaseWrapper())
+            {
+                // Arrange
+                var schemeApprovalNumber = "WEE/AA0001YZ/SCH";
+
+                var helper = new ModelHelper(database.Model);
+
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = schemeApprovalNumber;
+
+                var memberUpload = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload.ComplianceYear = 2016;
+
+                var producer = helper.CreateProducerAsCompany(memberUpload, "WEE/MM0001AA");
+
+                var dataReturnVersion = helper.CreateDataReturnVersion(scheme, 2016, 1, isSubmitted: true);
+                var producerEee = helper.CreateEeeOutputAmount(dataReturnVersion, producer.RegisteredProducer, "B2C", 1, 1000);
+
+                database.Model.SaveChanges();
+
+                // Act
+                var querySet = new SchemeEeeDataQuerySet(schemeApprovalNumber, 2016, database.WeeeContext);
+                var result = await querySet.HasProducerEeeDataForObligationType("WEE/MM0001BB", ObligationType.B2C);
+
+                // Assert
+                Assert.False(result);
+            }
+        }
+
+        [Fact]
+        public async Task HasProducerEeeDataForObligationType_ReturnsTrue_WhenDataAvailableForSpecifiedScheme()
+        {
+            using (DatabaseWrapper database = new DatabaseWrapper())
+            {
+                // Arrange
+                var helper = new ModelHelper(database.Model);
+
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/AA0001YZ/SCH";
+
+                var memberUpload = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload.ComplianceYear = 2016;
+
+                var producer = helper.CreateProducerAsCompany(memberUpload, "WEE/MM0001AA");
+                var dataReturnVersion = helper.CreateDataReturnVersion(scheme, 2016, 1, isSubmitted: true);
+                var producerEee = helper.CreateEeeOutputAmount(dataReturnVersion, producer.RegisteredProducer, "B2C", 1, 1000);
+
+                database.Model.SaveChanges();
+
+                // Act
+                var querySet = new SchemeEeeDataQuerySet("WEE/AA0001YZ/SCH", 2016, database.WeeeContext);
+                var result = await querySet.HasProducerEeeDataForObligationType("WEE/MM0001AA", ObligationType.B2C);
+
+                // Assert
+                Assert.True(result);
+            }
+        }
+
+        [Fact]
+        public async Task HasProducerEeeDataForObligationType_ReturnsFalse_WhenDataNotAvailableForSpecifiedScheme()
+        {
+            using (DatabaseWrapper database = new DatabaseWrapper())
+            {
+                // Arrange
+                var helper = new ModelHelper(database.Model);
+
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/AA0001YZ/SCH";
+
+                var memberUpload = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload.ComplianceYear = 2016;
+
+                var producer = helper.CreateProducerAsCompany(memberUpload, "WEE/MM0001AA");
+                var dataReturnVersion = helper.CreateDataReturnVersion(scheme, 2016, 1, isSubmitted: true);
+                var producerEee = helper.CreateEeeOutputAmount(dataReturnVersion, producer.RegisteredProducer, "B2C", 1, 1000);
+
+                database.Model.SaveChanges();
+
+                // Act
+                var querySet = new SchemeEeeDataQuerySet("WEE/AA0001AA/SCH", 2016, database.WeeeContext);
+                var result = await querySet.HasProducerEeeDataForObligationType("WEE/MM0001AA", ObligationType.B2C);
+
+                // Assert
+                Assert.False(result);
+            }
+        }
+
+        [Fact]
+        public async Task HasProducerEeeDataForObligationType_ReturnsTrue_WhenDataAvailableForSpecifiedComplianceYear()
+        {
+            using (DatabaseWrapper database = new DatabaseWrapper())
+            {
+                // Arrange
+                var helper = new ModelHelper(database.Model);
+
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/AA0001YZ/SCH";
+
+                var memberUpload = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload.ComplianceYear = 2016;
+
+                var producer = helper.CreateProducerAsCompany(memberUpload, "WEE/MM0001AA");
+                var dataReturnVersion = helper.CreateDataReturnVersion(scheme, 2016, 1, isSubmitted: true);
+                var producerEee = helper.CreateEeeOutputAmount(dataReturnVersion, producer.RegisteredProducer, "B2C", 1, 1000);
+
+                database.Model.SaveChanges();
+
+                // Act
+                var querySet = new SchemeEeeDataQuerySet("WEE/AA0001YZ/SCH", 2016, database.WeeeContext);
+                var result = await querySet.HasProducerEeeDataForObligationType("WEE/MM0001AA", ObligationType.B2C);
+
+                // Assert
+                Assert.True(result);
+            }
+        }
+
+        [Fact]
+        public async Task HasProducerEeeDataForObligationType_ReturnsFalse_WhenDataNotAvailableForSpecifiedComplianceYear()
+        {
+            using (DatabaseWrapper database = new DatabaseWrapper())
+            {
+                // Arrange
+                var helper = new ModelHelper(database.Model);
+
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/AA0001YZ/SCH";
+
+                var memberUpload = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload.ComplianceYear = 2016;
+
+                var producer = helper.CreateProducerAsCompany(memberUpload, "WEE/MM0001AA");
+                var dataReturnVersion = helper.CreateDataReturnVersion(scheme, 2016, 1, isSubmitted: true);
+                var producerEee = helper.CreateEeeOutputAmount(dataReturnVersion, producer.RegisteredProducer, "B2C", 1, 1000);
+
+                database.Model.SaveChanges();
+
+                // Act
+                var querySet = new SchemeEeeDataQuerySet("WEE/AA0001YZ/SCH", 2017, database.WeeeContext);
+                var result = await querySet.HasProducerEeeDataForObligationType("WEE/MM0001AA", ObligationType.B2C);
+
+                // Assert
+                Assert.False(result);
+            }
+        }
+
+        [Fact]
+        public async Task HasProducerEeeDataForObligationType_ReturnsTrue_WhenDataAvailableForSpecifiedObligationType()
+        {
+            using (DatabaseWrapper database = new DatabaseWrapper())
+            {
+                // Arrange
+                var helper = new ModelHelper(database.Model);
+
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/AA0001YZ/SCH";
+
+                var memberUpload = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload.ComplianceYear = 2016;
+
+                var producer = helper.CreateProducerAsCompany(memberUpload, "WEE/MM0001AA");
+                var dataReturnVersion = helper.CreateDataReturnVersion(scheme, 2016, 1, isSubmitted: true);
+                var producerEee = helper.CreateEeeOutputAmount(dataReturnVersion, producer.RegisteredProducer, "B2C", 1, 1000);
+
+                database.Model.SaveChanges();
+
+                // Act
+                var querySet = new SchemeEeeDataQuerySet("WEE/AA0001YZ/SCH", 2016, database.WeeeContext);
+                var result = await querySet.HasProducerEeeDataForObligationType("WEE/MM0001AA", ObligationType.B2C);
+
+                // Assert
+                Assert.True(result);
+            }
+        }
+
+        [Fact]
+        public async Task HasProducerEeeDataForObligationType_ReturnsFalse_WhenDataNotAvailableForSpecifiedObligationType()
+        {
+            using (DatabaseWrapper database = new DatabaseWrapper())
+            {
+                // Arrange
+                var helper = new ModelHelper(database.Model);
+
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/AA0001YZ/SCH";
+
+                var memberUpload = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload.ComplianceYear = 2016;
+
+                var producer = helper.CreateProducerAsCompany(memberUpload, "WEE/MM0001AA");
+                var dataReturnVersion = helper.CreateDataReturnVersion(scheme, 2016, 1, isSubmitted: true);
+                var producerEee = helper.CreateEeeOutputAmount(dataReturnVersion, producer.RegisteredProducer, "B2C", 1, 1000);
+
+                database.Model.SaveChanges();
+
+                // Act
+                var querySet = new SchemeEeeDataQuerySet("WEE/AA0001YZ/SCH", 2016, database.WeeeContext);
+                var result = await querySet.HasProducerEeeDataForObligationType("WEE/MM0001AA", ObligationType.B2B);
+
+                // Assert
+                Assert.False(result);
+            }
+        }
+    }
+}
---
+++ src/EA.Weee.XmlValidation/BusinessValidation/MemberRegistration/QuerySets/SchemeEeeDataQuerySet.cs
@@ -0,0 +1,49 @@
+﻿namespace EA.Weee.XmlValidation.BusinessValidation.MemberRegistration.QuerySets
+{
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using DataAccess;
+    using Domain.Obligation;
+    using EA.Weee.Domain.DataReturns;
+
+    public class SchemeEeeDataQuerySet : ISchemeEeeDataQuerySet
+    {
+        private readonly string schemeApprovalNumber;
+        private readonly int complianceYear;
+        private readonly WeeeContext context;
+        private Dictionary<string, List<EeeOutputAmount>> producerEeeOutputAmount;
+
+        public SchemeEeeDataQuerySet(string schemeApprovalNumber, int complianceYear, WeeeContext context)
+        {
+            this.schemeApprovalNumber = schemeApprovalNumber;
+            this.complianceYear = complianceYear;
+            this.context = context;
+        }
+
+        public async Task<bool> HasProducerEeeDataForObligationType(string registrationNumber, ObligationType obligationType)
+        {
+            if (producerEeeOutputAmount == null)
+            {
+                producerEeeOutputAmount = await
+                    context.DataReturnVersions
+                    .Where(d => d.DataReturn.Scheme.ApprovalNumber == schemeApprovalNumber)
+                    .Where(d => d.DataReturn.Quarter.Year == complianceYear)
+                    .SelectMany(d => d.EeeOutputReturnVersion.EeeOutputAmounts)
+                    .GroupBy(e => e.RegisteredProducer.ProducerRegistrationNumber)
+                    .ToDictionaryAsync(k => k.Key, e => e.ToList());
+            }
+
+            bool result = false;
+            List<EeeOutputAmount> producerAmounts;
+
+            if (producerEeeOutputAmount.TryGetValue(registrationNumber, out producerAmounts))
+            {
+                result = producerAmounts.Any(p => p.ObligationType == obligationType);
+            }
+
+            return result;
+        }
+    }
+}
