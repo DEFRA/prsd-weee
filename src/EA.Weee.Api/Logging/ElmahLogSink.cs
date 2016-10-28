e3ec7fda4b57c40b0c32ff6da690db0c3d5dc3c2
Chris Smith <chriss@sfwltd.co.uk>
2016-10-28 15:21:49 +0100

    #55014 - Updates to allow logging and login authentication (user creation remains broken)

---
+++ src/EA.Weee.Api/Logging/ElmahLogSink.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.Api.Logging
+{
+    using Elmah;
+    using Serilog.Core;
+    using Serilog.Events;
+
+    internal class ElmahLogSink : ILogEventSink
+    {
+        public void Emit(LogEvent logEvent)
+        {
+            ErrorSignal.FromCurrentContext().Raise(logEvent.Exception);
+        }
+    }
+}
