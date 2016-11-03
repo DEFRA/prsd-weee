fd6497e0636ec9b8961d8c8eda11ecd83a058f41
Chris Smith <chriss@sfwltd.co.uk>
2016-11-03 16:20:29 +0000

    #54604 - Add UI elements and API for Obligation Report (no stored proc.)

---
+++ src/EA.Weee.RequestHandlers/Admin/Reports/GetSchemeObligationDataCsv/GetSchemeObligationCsvDataAccess.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Weee.RequestHandlers.Admin.Reports.GetSchemeObligationDataCsv
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using EA.Weee.DataAccess;
+    using EA.Weee.DataAccess.StoredProcedure;
+
+    public class GetSchemeObligationCsvDataAccess : IGetSchemeObligationCsvDataAccess
+    {
+        private readonly WeeeContext context;
+
+        public GetSchemeObligationCsvDataAccess(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<SchemeObligationCsvData>> FetchObligationsForComplianceYearAsync(int complianceYear)
+        {
+            //TODO: Implement stored procedure
+            //return await context.StoredProcedures.SpgSchemeObligationDataCsv(complianceYear);
+            return new List<SchemeObligationCsvData>();
+        }
+    }
+}
