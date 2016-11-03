0e23c8547f3c5356abdcaaee673d77d76cdabde0
Muhammad Ismail <muhammadi@sfwltd.co.uk>
2016-11-03 10:58:18 +0000

    Retrieve previously submitted data return version

---
+++ src/EA.Weee.RequestHandlers/Shared/DataReturnSubmissionsData.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Weee.RequestHandlers.Shared
+{
+    using Core.DataReturns;
+    using Domain.DataReturns;
+
+    public class DataReturnSubmissionsData : DataReturnSubmissionsHistoryData
+    {
+        public DataReturnVersion DataReturnVersion { get; set; }
+    }
+}
