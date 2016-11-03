37e804f32422335521e31c18cd262c2f5ff1647b
Muhammad Ismail <muhammadi@sfwltd.co.uk>
2016-11-03 12:31:26 +0000

    Workaround for JSON serialisation issue

--- src/EA.Weee.RequestHandlers/Shared/GetDataReturnSubmissionsHistoryResultsHandler.cs
+++ src/EA.Weee.RequestHandlers/Shared/GetDataReturnSubmissionsHistoryResultsHandler.cs
@@ -1,6 +1,7 @@
 ﻿namespace EA.Weee.RequestHandlers.Shared
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Core.DataReturns;
     using Prsd.Core.Mediator;
@@ -22,12 +23,33 @@
         {
             authorization.EnsureInternalOrOrganisationAccess(request.OrganisationId);
 
-            var data = await dataAccess.GetDataReturnSubmissionsHistory(request.SchemeId, request.ComplianceYear,
+            var result = await dataAccess.GetDataReturnSubmissionsHistory(request.SchemeId, request.ComplianceYear,
                 request.Ordering, request.IncludeSummaryData);
+
+            var data = result.Select(x =>
+            new DataReturnSubmissionsHistoryData
+            {
+                SchemeId = x.SchemeId,
+                OrganisationId = x.OrganisationId,
+                DataReturnUploadId = x.DataReturnUploadId,
+                SubmittedBy = x.SubmittedBy,
+                ComplianceYear = x.ComplianceYear,
+                SubmissionDateTime = x.SubmissionDateTime,
+                FileName = x.FileName,
+                Quarter = x.Quarter,
+                EeeOutputB2b = x.EeeOutputB2b,
+                EeeOutputB2c = x.EeeOutputB2c,
+                WeeeCollectedB2b = x.WeeeCollectedB2b,
+                WeeeCollectedB2c = x.WeeeCollectedB2c,
+                WeeeDeliveredB2b = x.WeeeDeliveredB2b,
+                WeeeDeliveredB2c = x.WeeeDeliveredB2c,
+                DataReturnVersionId = x.DataReturnVersion.Id
+            })
+            .ToList();
 
             return new DataReturnSubmissionsHistoryResult()
             {
-                Data = data.ConvertAll(x => (DataReturnSubmissionsHistoryData)x),
+                Data = data,
                 ResultCount = data.Count
             };
         }
