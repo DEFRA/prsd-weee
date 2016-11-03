f3d951d0459e7ae41d99b418f5df819c3631ad6e
Muhammad Ismail <muhammadi@sfwltd.co.uk>
2016-11-03 17:32:08 +0000

    Initial implementation to show link when EEE data changes are present. Downloading of the CSV file is not currently implemented

--- src/EA.Weee.RequestHandlers/Shared/GetDataReturnSubmissionsHistoryResultsHandler.cs
+++ src/EA.Weee.RequestHandlers/Shared/GetDataReturnSubmissionsHistoryResultsHandler.cs
@@ -1,9 +1,9 @@
 ﻿namespace EA.Weee.RequestHandlers.Shared
 {
-    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using Core.DataReturns;
+    using DataReturns;
     using Prsd.Core.Mediator;
     using Requests.Shared;
     using Security;
@@ -11,46 +11,80 @@
     public class GetDataReturnSubmissionsHistoryResultsHandler : IRequestHandler<GetDataReturnSubmissionsHistoryResults, DataReturnSubmissionsHistoryResult>
     {
         private readonly IWeeeAuthorization authorization;
-        private readonly IGetDataReturnSubmissionsHistoryResultsDataAccess dataAccess;
+        private readonly IGetDataReturnSubmissionsHistoryResultsDataAccess historyResultsDataAccess;
+        private readonly IDataReturnSubmissionsDataAccess submissionsDataAccess;
+        private readonly IDataReturnVersionComparer dataReturnVersionComparer;
 
-        public GetDataReturnSubmissionsHistoryResultsHandler(IWeeeAuthorization authorization, IGetDataReturnSubmissionsHistoryResultsDataAccess dataAccess)
+        public GetDataReturnSubmissionsHistoryResultsHandler(
+            IWeeeAuthorization authorization,
+            IGetDataReturnSubmissionsHistoryResultsDataAccess historyResultsDataAccess,
+            IDataReturnSubmissionsDataAccess submissionsDataAccess,
+            IDataReturnVersionComparer dataReturnVersionComparer)
         {
             this.authorization = authorization;
-            this.dataAccess = dataAccess;
+            this.historyResultsDataAccess = historyResultsDataAccess;
+            this.submissionsDataAccess = submissionsDataAccess;
+            this.dataReturnVersionComparer = dataReturnVersionComparer;
         }
 
         public async Task<DataReturnSubmissionsHistoryResult> HandleAsync(GetDataReturnSubmissionsHistoryResults request)
         {
             authorization.EnsureInternalOrOrganisationAccess(request.OrganisationId);
 
-            var result = await dataAccess.GetDataReturnSubmissionsHistory(request.SchemeId, request.ComplianceYear,
+            var historyResult = await historyResultsDataAccess.GetDataReturnSubmissionsHistory(request.SchemeId, request.ComplianceYear,
                 request.Ordering, request.IncludeSummaryData);
 
-            var data = result.Select(x =>
-            new DataReturnSubmissionsHistoryData
+            var historyData = historyResult.Select(x =>
+            new
             {
-                SchemeId = x.SchemeId,
-                OrganisationId = x.OrganisationId,
-                DataReturnUploadId = x.DataReturnUploadId,
-                SubmittedBy = x.SubmittedBy,
-                ComplianceYear = x.ComplianceYear,
-                SubmissionDateTime = x.SubmissionDateTime,
-                FileName = x.FileName,
-                Quarter = x.Quarter,
-                EeeOutputB2b = x.EeeOutputB2b,
-                EeeOutputB2c = x.EeeOutputB2c,
-                WeeeCollectedB2b = x.WeeeCollectedB2b,
-                WeeeCollectedB2c = x.WeeeCollectedB2c,
-                WeeeDeliveredB2b = x.WeeeDeliveredB2b,
-                WeeeDeliveredB2c = x.WeeeDeliveredB2c,
-                DataReturnVersionId = x.DataReturnVersion.Id
+                Result = x,
+                HistoryData =
+                   new DataReturnSubmissionsHistoryData
+                   {
+                       SchemeId = x.SchemeId,
+                       OrganisationId = x.OrganisationId,
+                       DataReturnUploadId = x.DataReturnUploadId,
+                       SubmittedBy = x.SubmittedBy,
+                       ComplianceYear = x.ComplianceYear,
+                       SubmissionDateTime = x.SubmissionDateTime,
+                       FileName = x.FileName,
+                       Quarter = x.Quarter,
+                       EeeOutputB2b = x.EeeOutputB2b,
+                       EeeOutputB2c = x.EeeOutputB2c,
+                       WeeeCollectedB2b = x.WeeeCollectedB2b,
+                       WeeeCollectedB2c = x.WeeeCollectedB2c,
+                       WeeeDeliveredB2b = x.WeeeDeliveredB2b,
+                       WeeeDeliveredB2c = x.WeeeDeliveredB2c,
+                       DataReturnVersionId = x.DataReturnVersion.Id
+                   }
             })
             .ToList();
 
+            if (request.CompareEeeOutputData)
+            {
+                foreach (var item in historyData)
+                {
+                    var currentDataVersion = item.Result.DataReturnVersion;
+                    var previousSubmission = await submissionsDataAccess.GetPreviousSubmission(currentDataVersion);
+
+                    var eeeDataChanged = dataReturnVersionComparer.EeeDataChanged(currentDataVersion, previousSubmission);
+
+                    item.HistoryData.EeeDataChanged = eeeDataChanged;
+                    if (eeeDataChanged)
+                    {
+                        item.HistoryData.PreviousSubmissionDataReturnVersionId = previousSubmission.Id;
+                    }
+                }
+            }
+
+            var historyDataList = historyData
+                .Select(x => x.HistoryData)
+                .ToList();
+
             return new DataReturnSubmissionsHistoryResult()
             {
-                Data = data,
-                ResultCount = data.Count
+                Data = historyDataList,
+                ResultCount = historyDataList.Count
             };
         }
     }
