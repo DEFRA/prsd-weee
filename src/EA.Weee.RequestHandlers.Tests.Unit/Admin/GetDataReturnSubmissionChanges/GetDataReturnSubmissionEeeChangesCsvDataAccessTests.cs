b90b592edb5b334d1a4636330aace8b72fb24931
Muhammad Ismail <muhammadi@sfwltd.co.uk>
2016-11-07 13:08:20 +0000

    Download of CSV file for EEE changes between data return submissions

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/GetDataReturnSubmissionChanges/GetDataReturnSubmissionEeeChangesCsvDataAccessTests.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Admin.GetDataReturnSubmissionChanges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using DataAccess;
+    using Domain.DataReturns;
+    using FakeItEasy;
+    using RequestHandlers.Admin.GetDataReturnSubmissionChanges;
+    using Weee.Tests.Core;
+    using Xunit;
+
+    public class GetDataReturnSubmissionEeeChangesCsvDataAccessTests
+    {
+        [Fact]
+        public async Task GetChanges_WithSameValuesForCurrentAndPreviousArguments_ThrowsArgumentException()
+        {
+            var dataAccess =
+                new GetDataReturnSubmissionEeeChangesCsvDataAccess(A.Dummy<WeeeContext>());
+
+            var id = Guid.NewGuid();
+
+            await Assert.ThrowsAsync<ArgumentException>(() => dataAccess.GetChanges(id, id));
+        }
+    }
+}
---
+++ src/EA.Weee.Requests/Admin/GetDataReturnSubmissionChanges/GetDataReturnSubmissionEeeChangesCsv.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Weee.Requests.Admin.GetDataReturnSubmissionChanges
+{
+    using System;
+    using Core.Admin;
+    using Prsd.Core.Mediator;
+
+    public class GetDataReturnSubmissionEeeChangesCsv : IRequest<CSVFileData>
+    {
+        public Guid CurrentDataReturnVersionId { get; set; }
+
+        public Guid PreviousDataReturnVersionId { get; set; }
+
+        public GetDataReturnSubmissionEeeChangesCsv(Guid currentDataReturnVersionId, Guid previousDataReturnVersionId)
+        {
+            CurrentDataReturnVersionId = currentDataReturnVersionId;
+            PreviousDataReturnVersionId = previousDataReturnVersionId;
+        }
+    }
+}
