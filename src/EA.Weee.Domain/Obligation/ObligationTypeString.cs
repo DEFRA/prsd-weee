e143cb67f2f3449d116565dfc5738248df5a0421
Muhammad Ismail <muhammadi@sfwltd.co.uk>
2016-11-08 13:05:59 +0000

    - Fix file name for producer EEE changes - Perform count of tonnage on the data server to reduce data transfer

---
+++ src/EA.Weee.Domain/Obligation/ObligationTypeString.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.Domain.Obligation
+{
+    public static class ObligationTypeString
+    {
+        public const string B2B = "B2B";
+
+        public const string B2C = "B2C";
+    }
+}
