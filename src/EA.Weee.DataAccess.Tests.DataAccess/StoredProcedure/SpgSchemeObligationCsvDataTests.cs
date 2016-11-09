1a8cc8c39d9b4fac1b3523fdfd14521e34db3a7f
Chris Smith <chriss@sfwltd.co.uk>
2016-11-09 09:22:04 +0000

    #54604 - Fix for database script, additional data access test

--- src/EA.Weee.DataAccess.Tests.DataAccess/StoredProcedure/SpgSchemeObligationCsvDataTests.cs
+++ src/EA.Weee.DataAccess.Tests.DataAccess/StoredProcedure/SpgSchemeObligationCsvDataTests.cs
@@ -63,6 +63,100 @@
         }
 
         [Fact]
+        public async Task Execute_ReturnsCorrectTotalsForCategories_OfB2COnlyForPreviousYearOfCorrectProducer()
+        {
+            using (DatabaseWrapper db = new DatabaseWrapper())
+            {
+                //Arrange
+                ModelHelper helper = new ModelHelper(db.Model);
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/TE0000ST/SCH";
+
+                //Previous Year Data
+                var memberUpload2000 = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload2000.ComplianceYear = 2000;
+                var prod1_2000 = helper.CreateProducerAsCompany(memberUpload2000, "PRN123", "Both");
+                var prod2_2000 = helper.CreateProducerAsCompany(memberUpload2000, "PRN456", "B2C");
+                var dataReturnVersion1 = helper.CreateDataReturnVersion(scheme, 2000, 1);
+                var dataReturnVersion2 = helper.CreateDataReturnVersion(scheme, 2000, 2);
+                helper.CreateEeeOutputAmount(dataReturnVersion1, prod1_2000.RegisteredProducer, "B2C", 1, 100);
+                helper.CreateEeeOutputAmount(dataReturnVersion1, prod1_2000.RegisteredProducer, "B2C", 2, 500);
+                helper.CreateEeeOutputAmount(dataReturnVersion2, prod1_2000.RegisteredProducer, "B2C", 2, 1000);
+                helper.CreateEeeOutputAmount(dataReturnVersion1, prod1_2000.RegisteredProducer, "B2B", 2, 50);
+                helper.CreateEeeOutputAmount(dataReturnVersion1, prod2_2000.RegisteredProducer, "B2C", 2, 400);
+
+                //Current Year Data
+                var memberUpload2001 = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload2001.ComplianceYear = 2001;
+                var prod1_2001 = helper.CreateProducerAsCompany(memberUpload2001, "PRN123", "B2C");
+                var prod2_2001 = helper.CreateProducerAsCompany(memberUpload2001, "PRN456", "B2B");
+                var dataReturnVersion3 = helper.CreateDataReturnVersion(scheme, 2001, 1);
+                var dataReturnVersion4 = helper.CreateDataReturnVersion(scheme, 2001, 2);
+                helper.CreateEeeOutputAmount(dataReturnVersion3, prod1_2001.RegisteredProducer, "B2C", 1, 3000);
+                helper.CreateEeeOutputAmount(dataReturnVersion4, prod1_2001.RegisteredProducer, "B2C", 2, 600);
+                helper.CreateEeeOutputAmount(dataReturnVersion3, prod2_2001.RegisteredProducer, "B2B", 2, 9000);
+
+                db.Model.SaveChanges();
+
+                // Act
+                var results = await db.StoredProcedures.SpgSchemeObligationDataCsv(2001);
+
+                //Assert
+                Assert.NotNull(results);
+                Assert.Equal(2, results.Count);
+
+                SchemeObligationCsvData firstProducer = results.Find(x => (x.PRN == "PRN123"));
+
+                Assert.Equal("PRN123", firstProducer.PRN);
+                Assert.Equal(100, firstProducer.Cat1B2CTotal);
+                Assert.Equal(1500, firstProducer.Cat2B2CTotal);
+                Assert.Null(firstProducer.Cat3B2CTotal);
+            }
+        }
+
+        [Fact]
+        public async Task Execute_ReturnsPreviousYearDataFromOldScheme_WhenProducerRegisteredWithNewScheme()
+        {
+            using (DatabaseWrapper db = new DatabaseWrapper())
+            {
+                //Arrange
+                ModelHelper helper = new ModelHelper(db.Model);
+                var scheme1 = helper.CreateScheme();
+                scheme1.ApprovalNumber = "WEE/TE0001ST/SCH";
+                var scheme2 = helper.CreateScheme();
+                scheme2.ApprovalNumber = "WEE/TE0002ST/SCH";
+
+                //Previous Year Data
+                var memberUpload2000s1 = helper.CreateSubmittedMemberUpload(scheme1);
+                memberUpload2000s1.ComplianceYear = 2000;
+                var prod1_2000s1 = helper.CreateProducerAsCompany(memberUpload2000s1, "PRN123", "B2C");
+                var dataReturnVersion1 = helper.CreateDataReturnVersion(scheme1, 2000, 1);
+                helper.CreateEeeOutputAmount(dataReturnVersion1, prod1_2000s1.RegisteredProducer, "B2C", 1, 500);
+
+                //Current Year Data
+                var memberUpload2001s2 = helper.CreateSubmittedMemberUpload(scheme2);
+                memberUpload2001s2.ComplianceYear = 2001;
+                var prod1_2001s2 = helper.CreateProducerAsCompany(memberUpload2001s2, "PRN123", "B2C");
+                var dataReturnVersion2 = helper.CreateDataReturnVersion(scheme2, 2001, 1);
+                helper.CreateEeeOutputAmount(dataReturnVersion2, prod1_2001s2.RegisteredProducer, "B2C", 1, 5000);
+
+                db.Model.SaveChanges();
+
+                // Act
+                var results = await db.StoredProcedures.SpgSchemeObligationDataCsv(2001);
+
+                //Assert
+                Assert.NotNull(results);
+                Assert.Equal(1, results.Count);
+                SchemeObligationCsvData result = results[0];
+                Assert.Equal("test scheme name", result.SchemeName);
+                Assert.Equal("WEE/TE0002ST/SCH", result.ApprovalNumber);
+                Assert.Equal("PRN123", result.PRN);
+                Assert.Equal(500, result.Cat1B2CTotal);
+            }
+        }
+
+        [Fact]
         public async Task Execute_ReturnsCurrentYearB2CProducers_WhenTheyWerePreviousYearB2B_WithoutEeeData()
         {
             using (DatabaseWrapper db = new DatabaseWrapper())
@@ -309,6 +403,123 @@
                 Assert.Equal("B2C", result.ObligationTypeForPreviousYear);
                 Assert.Equal("B2C", result.ObligationTypeForSelectedYear);
                 Assert.Equal(70, result.Cat1B2CTotal);
+            }
+        }
+
+        [Fact]
+        public async Task Execute_ReturnsCurrentYearProducers_WhenTheyWereNotRegisteredInPreviousYear_WithoutEeeData()
+        {
+            using (DatabaseWrapper db = new DatabaseWrapper())
+            {
+                //Arrange
+                ModelHelper helper = new ModelHelper(db.Model);
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/TE0000ST/SCH";
+
+                //Current Year Data
+                var memberUpload2001 = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload2001.ComplianceYear = 2001;
+                var prod1_2001 = helper.CreateProducerAsCompany(memberUpload2001, "PRN123", "B2C");
+                var dataReturnVersion2 = helper.CreateDataReturnVersion(scheme, 2001, 1);
+                helper.CreateEeeOutputAmount(dataReturnVersion2, prod1_2001.RegisteredProducer, "B2C", 1, 5000);
+
+                db.Model.SaveChanges();
+
+                // Act
+                var results = await db.StoredProcedures.SpgSchemeObligationDataCsv(2001);
+
+                //Assert
+                Assert.NotNull(results);
+                Assert.Equal(1, results.Count);
+                SchemeObligationCsvData result = results[0];
+                Assert.Equal("PRN123", result.PRN);
+                Assert.Null(result.ObligationTypeForPreviousYear);
+                Assert.Equal("B2C", result.ObligationTypeForSelectedYear);
+                Assert.Null(result.Cat1B2CTotal);
+            }
+        }
+
+        [Fact]
+        public async Task Execute_ReturnsCurrentYearProducers_WhenNoPreviousYearDataReturnUpload_WithoutEeeData()
+        {
+            using (DatabaseWrapper db = new DatabaseWrapper())
+            {
+                //Arrange
+                ModelHelper helper = new ModelHelper(db.Model);
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/TE0000ST/SCH";
+
+                //Previous Year Data
+                var memberUpload2000 = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload2000.ComplianceYear = 2000;
+                helper.CreateProducerAsCompany(memberUpload2000, "PRN123", "B2C");
+
+                //Current Year Data
+                var memberUpload2001 = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload2001.ComplianceYear = 2001;
+                var prod1_2001 = helper.CreateProducerAsCompany(memberUpload2001, "PRN123", "Both");
+                var dataReturnVersion2 = helper.CreateDataReturnVersion(scheme, 2001, 1);
+                helper.CreateEeeOutputAmount(dataReturnVersion2, prod1_2001.RegisteredProducer, "B2C", 1, 3000);
+                helper.CreateEeeOutputAmount(dataReturnVersion2, prod1_2001.RegisteredProducer, "B2B", 1, 5000);
+
+                db.Model.SaveChanges();
+
+                // Act
+                var results = await db.StoredProcedures.SpgSchemeObligationDataCsv(2001);
+
+                //Assert
+                Assert.NotNull(results);
+                Assert.Equal(1, results.Count);
+                SchemeObligationCsvData result = results[0];
+                Assert.Equal("PRN123", result.PRN);
+                Assert.Equal("B2C", result.ObligationTypeForPreviousYear);
+                Assert.Equal("Both", result.ObligationTypeForSelectedYear);
+                Assert.Null(result.Cat1B2CTotal);
+            }
+        }
+
+        [Fact]
+        public async Task Execute_DoesNotReturnUnregisteredCurrentYearProducers_WhenTheyHavePreviousYearData()
+        {
+            using (DatabaseWrapper db = new DatabaseWrapper())
+            {
+                //Arrange
+                ModelHelper helper = new ModelHelper(db.Model);
+                var scheme = helper.CreateScheme();
+                scheme.ApprovalNumber = "WEE/TE0000ST/SCH";
+
+                //Previous Year Data
+                var memberUpload2000 = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload2000.ComplianceYear = 2000;
+                var prod1_2000 = helper.CreateProducerAsCompany(memberUpload2000, "PRN123", "B2C");
+                var prod2_2000 = helper.CreateProducerAsCompany(memberUpload2000, "PRN456", "B2C");
+                var dataReturnVersion1 = helper.CreateDataReturnVersion(scheme, 2000, 1);
+                var dataReturnVersion2 = helper.CreateDataReturnVersion(scheme, 2000, 2);
+                helper.CreateEeeOutputAmount(dataReturnVersion1, prod1_2000.RegisteredProducer, "B2C", 1, 100);
+                helper.CreateEeeOutputAmount(dataReturnVersion2, prod1_2000.RegisteredProducer, "B2C", 2, 1000);
+                helper.CreateEeeOutputAmount(dataReturnVersion1, prod2_2000.RegisteredProducer, "B2C", 2, 400);
+
+                //Current Year Data
+                var memberUpload2001 = helper.CreateSubmittedMemberUpload(scheme);
+                memberUpload2001.ComplianceYear = 2001;
+                var prod1_2001 = helper.CreateProducerAsCompany(memberUpload2001, "PRN123", "B2C");
+                var dataReturnVersion3 = helper.CreateDataReturnVersion(scheme, 2001, 1);
+                var dataReturnVersion4 = helper.CreateDataReturnVersion(scheme, 2001, 2);
+                helper.CreateEeeOutputAmount(dataReturnVersion3, prod1_2001.RegisteredProducer, "B2C", 1, 3000);
+                helper.CreateEeeOutputAmount(dataReturnVersion4, prod1_2001.RegisteredProducer, "B2C", 2, 600);
+
+                db.Model.SaveChanges();
+
+                // Act
+                var results = await db.StoredProcedures.SpgSchemeObligationDataCsv(2001);
+
+                //Assert
+                Assert.NotNull(results);
+
+                //Producer with only B2B (both years) should not be in data
+                SchemeObligationCsvData unregisteredProd = results.Find(x => (x.PRN == "PRN456"));
+                Assert.Null(unregisteredProd);
+                Assert.Equal(1, results.Count);
             }
         }
     }
