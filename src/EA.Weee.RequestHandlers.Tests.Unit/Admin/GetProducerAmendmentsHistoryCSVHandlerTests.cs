89c76ccb2864ba9cae5489c6aee028e24fdb9d76
Chris Smith <chriss@sfwltd.co.uk>
2016-11-21 17:37:21 +0000

    #56409 - Add brand names column to producer registration details and changes csv

--- src/EA.Weee.RequestHandlers.Tests.Unit/Admin/GetProducerAmendmentsHistoryCSVHandlerTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/GetProducerAmendmentsHistoryCSVHandlerTests.cs
@@ -1,18 +1,20 @@
 ﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Admin
 {
     using System;
+    using System.Collections.Generic;
     using System.Security;
     using System.Threading.Tasks;
     using Core.Admin;
     using Core.Shared;
     using DataAccess;
+    using DataAccess.StoredProcedure;
     using FakeItEasy;
     using RequestHandlers.Admin;
     using RequestHandlers.Admin.Reports;
     using Requests.Admin;
     using Weee.Tests.Core;
     using Xunit;
-
+    
     public class GetProducerAmendmentsHistoryCSVHandlerTests
     {
         [Fact]
@@ -73,6 +75,55 @@
 
             // Assert
             Assert.NotEmpty(data.FileContent);
+        }
+
+        [Fact]
+        public async Task GetProducerAmendmentsHistoryCSVHandler_WithBrandNamesLongerThanMaxLength_ThrowsException()
+        {
+            var authorization = new AuthorizationBuilder().AllowInternalAreaAccess().Build();
+            var context = A.Fake<WeeeContext>();
+            var storedProcedures = A.Fake<IStoredProcedures>();
+            var csvWriterFactory = A.Fake<CsvWriterFactory>();
+
+            A.CallTo(() => context.StoredProcedures)
+                .Returns(storedProcedures);
+
+            var csvData1 = new ProducerAmendmentsHistoryCSVData
+            {
+                ProducerName = "Producer1",
+                DateRegistered = new DateTime(2000, 3, 2),
+                BrandNames = new string('A', GetMembersDetailsCsvHandler.MaxBrandNamesLength + 1)
+            };
+
+            var csvData2 = new ProducerAmendmentsHistoryCSVData
+            {
+                ProducerName = "Producer1",
+                DateRegistered = new DateTime(2001, 7, 12),
+                BrandNames = "BrandName2"
+            };
+
+            var csvData3 = new ProducerAmendmentsHistoryCSVData
+            {
+                ProducerName = "Producer1",
+                DateRegistered = new DateTime(2002, 8, 5),
+                BrandNames = new string('A', GetMembersDetailsCsvHandler.MaxBrandNamesLength + 1)
+            };
+
+            A.CallTo(() => storedProcedures
+            .SpgProducerAmendmentsCSVDataByPRN(A<string>._))
+            .Returns(new List<ProducerAmendmentsHistoryCSVData> { csvData1, csvData2, csvData3 });
+
+            var handler = new GetProducerAmendmentsHistoryCSVHandler(authorization, context, csvWriterFactory);
+            var request = new GetProducerAmendmentsHistoryCSV("PRN");
+
+            // Act
+            var exception = await Record.ExceptionAsync(() => handler.HandleAsync(request));
+
+            // Assert
+            Assert.NotNull(exception);
+            Assert.Contains("02/03/2000", exception.Message);
+            Assert.Contains("05/08/2002", exception.Message);
+            Assert.Contains("brand names", exception.Message);
         }
     }
 }
