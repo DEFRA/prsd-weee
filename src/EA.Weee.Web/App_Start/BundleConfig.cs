a6a1a315d40902d9afec6f8f5f0191835e8d7b65
JohnPadgettCivica <JohnPadgettCivica>
2019-01-10 16:40:08 +0000

    Resolve issue with progressive disclosure

--- src/EA.Weee.Web/App_Start/BundleConfig.cs
+++ src/EA.Weee.Web/App_Start/BundleConfig.cs
@@ -9,35 +9,34 @@
         {
             // JQuery 1.x.x is used for IE 8 and earlier.
             bundles.Add(new ScriptBundle("~/bundles/jquery-1").Include(
-                "~/Scripts/jquery-1*",
-                "~/Scripts/jquery.unobtrusive-ajax.js",
-                "~/Scripts/jquery-ui-{version}.js",
-                "~/Scripts/jquery.select-to-autocomplete.js",
-                "~/Scripts/jquery.validate*",
-                "~/Scripts/custom-validation.js"));
+                    "~/Scripts/jquery-1*",
+                    "~/Scripts/jquery.unobtrusive-ajax.js",
+                    "~/Scripts/jquery-ui-{version}.js",
+                    "~/Scripts/jquery.select-to-autocomplete.js",
+                    "~/Scripts/jquery.validate*",
+                    "~/Scripts/custom-validation.js"));
 
             // JQuery 3.x.x is used for allother browsers.
             bundles.Add(new ScriptBundle("~/bundles/jquery-3").Include(
-                "~/Scripts/jquery-3.3.1.js",
-                "~/Scripts/jquery-migrate-3.0.0.js",
-                "~/Scripts/jquery.unobtrusive-ajax.js",
-                "~/Scripts/jquery-ui-{version}.js",
-                "~/Scripts/jquery.select-to-autocomplete.js",
-                "~/Scripts/jquery.validate*",
-                "~/Scripts/custom-validation.js"));
+                    "~/Scripts/jquery-3.3.1.js",
+                    "~/Scripts/jquery-migrate-3.0.0.js",
+                    "~/Scripts/jquery.unobtrusive-ajax.js",
+                    "~/Scripts/jquery-ui-{version}.js",
+                    "~/Scripts/jquery.select-to-autocomplete.js",
+                    "~/Scripts/jquery.validate*",
+                    "~/Scripts/custom-validation.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/govuk_frontend").Include(
-                      "~/Scripts/govuk_frontend/govuk-frontend-2.3.0.min.js",
-                      "~/Scripts/govuk_frontend/html5shiv.js",
-                        "~/Scripts/setup-govuk-frontend.js"));
+                    "~/Scripts/govuk_frontend/govuk-frontend-2.3.0.min.js",
+                    "~/Scripts/govuk_frontend/html5shiv.js",
+                    "~/Scripts/setup-govuk-frontend.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/govuk_weee").Include(
-                      "~/Scripts/vendor/modernizr.custom.77028.js",
-                      "~/Scripts/vendor/details.polyfill.js",
-                      "~/Scripts/weee-application.js"));
+                    "~/Scripts/vendor/modernizr.custom.77028.js",
+                    "~/Scripts/weee-application.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/govuk_frontend_script").Include(
-                "~/Scripts/govuk_frontend/govuk-frontend-2.3.0.min.js"));
+                    "~/Scripts/govuk_frontend/govuk-frontend-2.3.0.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/weee-page-ie6").Include(
                       "~/Content/weee-page-ie6.css"));
@@ -47,7 +46,7 @@
 
             bundles.Add(new StyleBundle("~/Content/weee-page-ie8").Include(
                       "~/Content/weee-page-ie8.css"));
-            
+
             bundles.Add(new StyleBundle("~/Content/weee-page").Include(
                       "~/Content/weee-page.css"));
 
