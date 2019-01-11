1b3c9aa9b8227811d35eccac0fd67c67ad037d0e
JohnPadgettCivica <JohnPadgettCivica>
2019-01-11 16:48:19 +0000

    Further updates to the maintenance website

--- src/EA.Weee.Web.Maintenance/App_Start/BundleConfig.cs
+++ src/EA.Weee.Web.Maintenance/App_Start/BundleConfig.cs
@@ -7,40 +7,43 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/jquery-3").Include(
+                        "~/Scripts/jquery-3.3.1.js",
+                        "~/Scripts/jquery-migrate-3.0.0.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                     "~/Scripts/jquery-{version}.js",
-                     "~/Scripts/jquery.unobtrusive-ajax.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
-                "~/Scripts/jquery-ui-{version}.js",
-                "~/Scripts/jquery.select-to-autocomplete.js"));
+                        "~/Scripts/jquery-ui-{version}.js",
+                        "~/Scripts/jquery.select-to-autocomplete.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*",
                         "~/Scripts/custom-validation.js"));
 
-
             bundles.Add(new ScriptBundle("~/bundles/govuk_frontend").Include(
-        "~/Scripts/govuk_frontend/govuk-frontend-2.3.0.min.js",
-        "~/Scripts/govuk_frontend/html5shiv.js",
-        "~/Scripts/setup-govuk-frontend.js"));
+                        "~/Scripts/govuk_frontend/govuk-frontend-2.3.0.min.js",
+                        "~/Scripts/govuk_frontend/html5shiv.js",
+                        "~/Scripts/setup-govuk-frontend.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/govuk_weee").Include(
-                      "~/Scripts/vendor/modernizr.custom.77028.js",
-                      "~/Scripts/vendor/details.polyfill.js",
-                      "~/Scripts/weee-application.js"));
+                        "~/Scripts/vendor/modernizr.custom.77028.js",
+                        "~/Scripts/weee-application.js"));
 
            bundles.Add(new StyleBundle("~/Content/weee-page-ie6").Include(
-                      "~/Content/weee-page-ie6.css"));
+                        "~/Content/weee-page-ie6.css"));
 
             bundles.Add(new StyleBundle("~/Content/weee-page-ie7").Include(
-                      "~/Content/weee-page-ie7.css"));
+                        "~/Content/weee-page-ie7.css"));
 
             bundles.Add(new StyleBundle("~/Content/weee-page-ie8").Include(
-                      "~/Content/weee-page-ie8.css"));
+                        "~/Content/weee-page-ie8.css"));
 
             bundles.Add(new StyleBundle("~/Content/weee-page").Include(
-                      "~/Content/weee-page.css"));
+                        "~/Content/weee-page.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
