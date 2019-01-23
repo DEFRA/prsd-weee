e9e5653fc74e2edecb3cb8169230c5237f7906d7
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-01-23 14:24:27 +0000

    Start of non obligated page

---
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/NonObligatedController.cs
@@ -0,0 +1,39 @@
+﻿namespace EA.Weee.Web.Areas.AATF_Return.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Api.Client;
+    using Core.DataReturns;
+    using Core.Scheme;
+    using Core.Shared;
+    using Infrastructure;
+    using Prsd.Core;
+    using Prsd.Core.Mapper;
+    using Services;
+    using Services.Caching;
+    using ViewModels;
+    using Web.Controllers.Base;
+    using Weee.Requests.DataReturns;
+    using Weee.Requests.Organisations;
+    using Weee.Requests.Scheme;
+    using Weee.Requests.Shared;
+
+    public class NonObligatedController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            IndexViewModel viewModel = new IndexViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
