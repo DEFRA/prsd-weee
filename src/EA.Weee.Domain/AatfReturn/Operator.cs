660714844b3ad928ac5f1ec8fe1527dcfb1ce4fc
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-01-25 09:13:22 +0000

    Non obligated save db changes

---
+++ src/EA.Weee.Domain/AatfReturn/Operator.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Weee.Domain.AatfReturn
+{
+    using System;
+    using EA.Prsd.Core;
+    using EA.Prsd.Core.Domain;
+
+    public partial class Operator : Entity
+    {
+        public Operator(Guid operatorid, Guid organisationid)
+        {
+            OperatorId = operatorid;
+            OrganisationId = organisationid;
+        }
+
+        public Guid OperatorId { get; private set; }
+
+        public Guid OrganisationId { get; private set; }
+    }
+}
---
+++ src/EA.Weee.Domain/AatfReturn/Return.cs
@@ -0,0 +1,28 @@
+﻿namespace EA.Weee.Domain.AatfReturn
+{
+    using System;
+    using EA.Prsd.Core;
+    using EA.Prsd.Core.Domain;
+
+    public partial class Return : Entity
+    {
+        public Return(Guid returnid, Guid operatorid, int complianceyear, int period, int status)
+        {
+            ReturnId = returnid;
+            OperatorId = operatorid;
+            ComplianceYear = complianceyear;
+            Period = period;
+            Status = status;
+        }
+
+        public Guid ReturnId { get; private set; }
+
+        public Guid OperatorId { get; private set; }
+
+        public int ComplianceYear { get; private set; }
+
+        public int Period { get; private set; }
+
+        public int Status { get; private set; }
+    }
+}
