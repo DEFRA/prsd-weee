a8c9222141df21fa7d3a6be07e94270fb1dcb227
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-01-25 09:13:22 +0000

    Non obligated save db changes

---
+++ src/EA.Weee.RequestHandlers/AatfReturn/CreateNonObligatedRequestHandler.cs
@@ -0,0 +1,49 @@
+﻿namespace EA.Weee.RequestHandlers.AatfReturn
+{
+    using DataAccess;
+    using Domain.AatfReturn;
+    using Domain.Organisation;
+    using Domain.User;
+    using Prsd.Core.Domain;
+    using Prsd.Core.Mediator;
+    using Requests.AatfReturn;
+    using Requests.Organisations.Create;
+    using Security;
+    using System;
+    using System.Threading.Tasks;
+
+    public class CreateNonObligatedRequestHandler
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly WeeeContext db;
+        private readonly IUserContext userContext;
+
+        public CreateNonObligatedRequestHandler(IWeeeAuthorization authorization, WeeeContext db, IUserContext userContext)
+        {
+            this.authorization = authorization;
+            this.db = db;
+            this.userContext = userContext;
+        }
+
+        public async Task<Guid> HandleAsync(NonObligatedRequest message)
+        {
+            authorization.EnsureCanAccessExternalArea();
+            var organisation = Organisation.CreateSoleTrader("TESTINGTRADING");
+            db.Organisations.Add(organisation);
+
+            var operatorTest = new Operator(Guid.NewGuid(), organisation.Id);
+            db.Operator.Add(operatorTest);
+
+            var returnTest = new Return(Guid.NewGuid(), operatorTest.Id, 1, 1, 1);
+            db.Return.Add(returnTest);
+
+            var nonobligated = new NonObligatedWeee(message.NonObligatedId, returnTest.Id, message.CategoryId, message.Dcf, message.Tonnage);
+
+            db.NonObligatedWeee.Add(nonobligated);
+
+            await db.SaveChangesAsync();
+
+            return nonobligated.Id;
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/AATFReturn/ViewModels/NonObligatedViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Weee.Web.Areas.AATF_Return.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using Prsd.Core;
+
+    public class IndexViewModel
+    {
+        public Guid NonObligatedId { get; private set; }
+
+        public Guid ReturnId { get; private set; }
+
+        public int CategoryId { get; private set; }
+
+        public bool Dcf { get; private set; }
+
+        public decimal Tonnage { get; private set; }
+    }
+}
