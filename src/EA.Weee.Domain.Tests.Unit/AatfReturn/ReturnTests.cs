316de7f0a9140c17829b6e360cc9e4eff4525c48
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-01-29 16:50:14 +0000

    Unit tests

---
+++ src/EA.Weee.Domain.Tests.Unit/AatfReturn/ReturnTests.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.Domain.Tests.Unit.AatfReturn
+{
+    using Domain.AatfReturn;
+    using FakeItEasy;
+    using Xunit;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ReturnTests
+    {
+        [Fact]
+        public void Return_OperatorNotDefined_ThrowsArugmentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Return(Guid.NewGuid(), 1, 1, 1, null));
+        }
+    }
+}
---
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Requests/NonObligatedWeeRequestCreatorTests.cs
@@ -0,0 +1,53 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.AatfReturn.Requests
+{
+    using EA.Prsd.Core.Domain;
+    using EA.Prsd.Core.Mapper;
+    using EA.Prsd.Core.Web.OAuth;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Web.Areas.AatfReturn.Requests;
+    using EA.Weee.Web.Services;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Microsoft.Owin.Security;
+    using System;
+    using System.Security;
+    using System.Security.Principal;
+    using System.Threading.Tasks;
+    using Web.Areas.AatfReturn.Controllers;
+    using Web.Areas.AatfReturn.ViewModels;
+    using Web.Controllers.Base;
+    using Xunit;
+
+    public class NonObligatedWeeRequestCreatorTests
+    {
+        private readonly INonObligatedWeeRequestCreator requestCreator;
+
+        public NonObligatedWeeRequestCreatorTests()
+        {
+            requestCreator = new NonObligatedWeeRequestCreator();
+        }
+
+        [Fact]
+        public async void AddNonObligatedRequest_GivenValidViewModel_CorrectlyMapToARequest()
+        {
+            var categoryValues = new CategoryValues();
+
+            for (var i = 0; i < categoryValues.Count; i++)
+            {
+                categoryValues[i].Tonnage = i;
+            }
+
+            var viewModel = new NonObligatedValuesViewModel(categoryValues);
+
+            var request = requestCreator.ViewModelToRequest(viewModel);
+            Assert.NotNull(request);
+
+            for (var i = 0; i < categoryValues.Count; i++)
+            {
+                Assert.Equal(request.CategoryValues[i].Tonnage, viewModel.CategoryValues[i].Tonnage);
+                Assert.Equal(request.CategoryValues[i].CategoryId, viewModel.CategoryValues[i].CategoryId);
+            }
+        }
+    }
+}
