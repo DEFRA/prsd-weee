79fd74aca7b2760b7f0fd73708f0f37588821dfe
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-01-30 14:26:08 +0000

    DCF changes

--- src/EA.Weee.Requests/AatfReturn/AddNonObligatedRequest.cs
+++ src/EA.Weee.Requests/AatfReturn/AddNonObligatedRequest.cs
@@ -9,6 +9,8 @@
     {
         public IList<NonObligatedCategoryValue> CategoryValues { get; set; }
 
+        public bool Dcf { get; set; }
+
         public Guid NonObligatedId { get; set; }
 
         public Guid ReturnId { get; set; }
