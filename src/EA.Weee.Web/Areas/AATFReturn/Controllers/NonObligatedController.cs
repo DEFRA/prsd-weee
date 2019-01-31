3580725eae793ac5b77a5e5e10044960a009dc45
Sam Goodey <samuelg@sfwltd.co.uk>
2019-01-31 09:52:20 +0000

    Feature/nonobligatedweeereceiveddcf (#70)
    * DCF Url change
    
    * DCF changes
    
    * dcf changes
    
    * Changed constructor
    
    * Fix for tonnage javascript calculations / fix for non obligated total when invalid values
    
    * DCF Url change
    
    * DCF changes
    
    * dcf changes
    
    * Changed constructor
    
    * Fix for tonnage javascript calculations / fix for non obligated total when invalid values
    
    * Fix issue with copy and paste
    
    * More tests
    
    * Tidy up the non obligated controller and add holding page

--- src/EA.Weee.Web/Areas/AATFReturn/Controllers/NonObligatedController.cs
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/NonObligatedController.cs
@@ -1,63 +1,39 @@
 ﻿namespace EA.Weee.Web.Areas.AatfReturn.Controllers
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
     using System.Web.Mvc;
     using Api.Client;
     using Core.AatfReturn;
-    using Core.DataReturns;
-    using Core.Scheme;
-    using Core.Shared;
     using Infrastructure;
     using Microsoft.Owin.Security;
-    using Prsd.Core;
     using Prsd.Core.Mapper;
-    using Prsd.Core.Web.ApiClient;
-    using Prsd.Core.Web.Mvc.Extensions;
     using Prsd.Core.Web.OAuth;
     using Requests;
     using Services;
     using Services.Caching;
     using ViewModels;
     using Web.Controllers.Base;
-    using Web.ViewModels.Shared;
-    using Weee.Requests.AatfReturn;
-    using Weee.Requests.DataReturns;
-    using Weee.Requests.Organisations;
-    using Weee.Requests.Scheme;
-    using Weee.Requests.Shared;
 
     public class NonObligatedController : ExternalSiteController
     {
         private readonly Func<IWeeeClient> apiClient;
-        private readonly IAuthenticationManager authenticationManager;
-        private readonly Func<IOAuthClient> oauthClient;
-        private readonly IExternalRouteService externalRouteService;
-        private readonly IAppConfiguration appConfig;
-        public List<AddNonObligatedRequest> NonObligatedRequestList;
         private readonly INonObligatedWeeRequestCreator requestCreator;
         private readonly BreadcrumbService breadcrumb;
         private readonly IWeeeCache cache;
 
-        public NonObligatedController(IWeeeCache cache, BreadcrumbService breadcrumb, IMapper mapper, Func<IOAuthClient> oauthClient, Func<IWeeeClient> apiClient, IAuthenticationManager authenticationManager, IExternalRouteService externalRouteService, IAppConfiguration appConfig, INonObligatedWeeRequestCreator requestCreator)
+        public NonObligatedController(IWeeeCache cache, BreadcrumbService breadcrumb, Func<IWeeeClient> apiClient, INonObligatedWeeRequestCreator requestCreator)
         {
-            this.oauthClient = oauthClient;
             this.apiClient = apiClient;
-            this.authenticationManager = authenticationManager;
-            this.externalRouteService = externalRouteService;
-            this.appConfig = appConfig;
             this.requestCreator = requestCreator;
             this.breadcrumb = breadcrumb;
             this.cache = cache;
         }
 
         [HttpGet]
-        public virtual async Task<ActionResult> Index(Guid organisationId)
+        public virtual async Task<ActionResult> Index(Guid organisationId, bool dcf)
         {
-            var viewModel = new NonObligatedValuesViewModel(new NonObligatedCategoryValues()) { OrganisationId = organisationId };
+            var viewModel = new NonObligatedValuesViewModel(new NonObligatedCategoryValues()) { OrganisationId = organisationId, Dcf = dcf };
             await SetBreadcrumb(organisationId, "AATF Return");
             return View(viewModel);
         }
@@ -71,8 +47,15 @@
                 using (var client = apiClient())
                 {
                     var request = requestCreator.ViewModelToRequest(viewModel);
+
                     await client.SendAsync(User.GetAccessToken(), request);
-                    return View(viewModel);
+
+                    if (viewModel.Dcf)
+                    {
+                        return RedirectToAction("Index", "Holding", new { area = "AatfReturn" });
+                    }
+
+                    return RedirectToAction("Index", "NonObligated", new { area = "AatfReturn", dcf = true });
                 }
             }
 
