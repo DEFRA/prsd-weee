7af1d7e58cf55a484dbff649b0304cad3b78176c
Sam Goodey <samuelg@sfwltd.co.uk>
2019-01-31 09:52:20 +0000

    Feature/nonobligatedweeereceiveddcf (#70)
    * DCF Url change
    
    * DCF changes
    
    * dcf changes
    
    * Changed constructor
    
    * Fix for tonnage javascript calculations / fix for non obligated total when invalid values
    
    * DCF Url change
    
    * DCF changes
    
    * dcf changes
    
    * Changed constructor
    
    * Fix for tonnage javascript calculations / fix for non obligated total when invalid values
    
    * Fix issue with copy and paste
    
    * More tests
    
    * Tidy up the non obligated controller and add holding page

--- src/EA.Weee.Web/Areas/AATFReturn/ViewModels/NonObligatedValuesViewModel.cs
+++ src/EA.Weee.Web/Areas/AATFReturn/ViewModels/NonObligatedValuesViewModel.cs
@@ -11,6 +11,8 @@
 
         public Guid OrganisationId { get; set; }
 
+        public bool Dcf { get; set; }
+
         public NonObligatedValuesViewModel()
         {
             AddCategoryValues(new NonObligatedCategoryValues());
@@ -36,7 +38,7 @@
             get
             {
                 var total = 0.000m;
-                var values = CategoryValues.Where(c => !string.IsNullOrWhiteSpace(c.Tonnage)).Select(c => c.Tonnage).ToList();
+                var values = CategoryValues.Where(c => !string.IsNullOrWhiteSpace(c.Tonnage) && decimal.TryParse(c.Tonnage, out var output)).Select(c => c.Tonnage).ToList();
 
                 if (values.Any())
                 {
