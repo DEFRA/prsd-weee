f3c4c7c9bdc87513c725adff7202ecd13f2f9991
Sam Goodey <samuelg@sfwltd.co.uk>
2019-02-08 16:57:45 +0000

    Feature/selectpcs (#80)
    * Summary screen creation
    
    * Added conversion to string for quarter, plus unit test
    
    * resolving issues with cproj files, breadcrumb fix
    
    * converting fetch to return decimal rather than int
    
    * Support for decimals and unit tests
    
    * formatting
    
    * Submitted return updates
    
    * Merge from origin
    
    * Authorization added, unit tests added
    
    * Changes to make the merge work
    
    * Check that user has organisation access
    
    * Submitted return updates
    
    * Added validate anti forgery tag
    
    * Submitted return updates - fix view
    
    * Check return updates
    
    * Compliance year and reporting period added, added functionality for get return to get non ob weee
    
    * Updates to the quarter description calculation
    
    * Change to AATF return test
    
    * Fix for build issue
    
    * Updates to the way the Nonobligated data values are returned
    
    * Added breadcrumb to check my return, unit test for submitted return
    
    * resolve build errors
    
    * Break the summary for sam  to fix :)
    
    * Create return view model base
    
    * Check your return total mapping
    
    * Check your return controller unit tests added
    
    * Added support for operator name in check your return view model
    
    * OperatorData creation
    
    * Altered ooperator data to hold organisationId
    
    * Created unit test for mapping to operator data
    
    * Turn off autocomplete on tonnage values (#83)
    
    * start of pcs selection
    
    * further pcs selection development
    
    * refactoring checkyourreturn, resolving referencing issues
    
    * last little fixes

---
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/SelectYourPCSController.cs
@@ -0,0 +1,56 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Controllers
+{
+    using EA.Weee.Api.Client;
+    using EA.Weee.Core.Scheme;
+    using EA.Weee.Requests.Scheme;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Controllers.Base;
+    using EA.Weee.Web.Infrastructure;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class SelectYourPCSController : ExternalSiteController
+    {
+        private readonly Func<IWeeeClient> apiClient;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+        private List<SchemeData> schemeList;
+
+        public SelectYourPCSController(Func<IWeeeClient> apiclient, BreadcrumbService breadcrumb, IWeeeCache cache)
+        {
+            this.apiClient = apiclient;
+            this.breadcrumb = breadcrumb;
+            this.cache = cache;
+        }
+
+        [HttpGet]
+        public virtual async Task<ActionResult> Index(Guid organisationId)
+        {
+            var viewModel = new SelectYourPCSViewModel();
+
+            using (var client = apiClient())
+            {
+                var schemeRequest = new GetSchemes();
+                schemeList = await client.SendAsync(User.GetAccessToken(), schemeRequest);
+                viewModel.SchemeList = schemeList;
+            }
+            await SetBreadcrumb(organisationId, BreadCrumbConstant.AatfReturn);
+
+            return View(viewModel);
+        }
+
+        private async Task SetBreadcrumb(Guid organisationId, string activity)
+        {
+            breadcrumb.ExternalOrganisation = await cache.FetchOrganisationName(organisationId);
+            breadcrumb.ExternalActivity = activity;
+            breadcrumb.SchemeInfo = await cache.FetchSchemePublicInfo(organisationId);
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/AATFReturn/ViewModels/CheckYourReturnViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using Core.AatfReturn;
+    using EA.Prsd.Core;
+    using EA.Weee.Core.DataReturns;
+
+    public class CheckYourReturnViewModel : ReturnViewModelBase
+    {
+        /*
+        private readonly QuarterWindow quarterWindow;
+        private readonly Quarter quarter;
+        private readonly int year;
+        */
+        public CheckYourReturnViewModel()
+        {
+        }
+
+        public string NonObliagtedTotal { get; set; }
+
+        public string NonObligatedDcfTotal { get; set; }
+
+        public OperatorData ReturnOperator { get; set; }
+
+        public Guid ReturnId { get; set; }
+
+        public CheckYourReturnViewModel(OperatorData returnOperator, decimal? total, decimal? dcftotal, Quarter quarter, QuarterWindow window, int year) : base(quarter, window, year)
+        {
+            this.NonObliagtedTotal = total.ToString();
+            this.NonObligatedDcfTotal = dcftotal.ToString();
+            this.ReturnOperator = returnOperator;
+        }
+    }
+}
