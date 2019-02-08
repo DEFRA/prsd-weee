42b2af48daa408156ce01d670a580e165b8f474d
JohnPadgettCivica <43237115+JohnPadgettCivica@users.noreply.github.com>
2019-02-08 11:02:47 +0000

    Feature/aatf operator link (#81)
    * Default AATF changes
    
    * Further updates to default AATF
    
    * Fix build issue

---
+++ src/EA.Weee.DataAccess/Mappings/AatfStatusMapping.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.DataAccess.Mappings
+{
+    using System.Data.Entity.ModelConfiguration;
+    using Domain.AatfReturn;
+    using Domain.User;
+
+    internal class AatfStatusMapping : ComplexTypeConfiguration<AatfStatus>
+    {
+        public AatfStatusMapping()
+        {
+            Ignore(x => x.DisplayName);
+            Property(x => x.Value).HasColumnName("Status");
+        }
+    }
+}
