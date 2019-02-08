5ae25185f48822d7e1303ca60a8722c023d76e76
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-02-08 14:40:52 +0000

    start of pcs selection

---
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/SelectYourPCSController.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Controllers
+{
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Controllers.Base;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+
+    public class SelectYourPCSController : ExternalSiteController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var viewModel = new SelectYourPCSViewModel();
+            return View("Index", viewModel);
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/AATFReturn/ViewModels/SelectYourPCSViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class SelectYourPCSViewModel
+    {
+        public SelectYourPCSViewModel() { 
+
+        }
+    }
+}
