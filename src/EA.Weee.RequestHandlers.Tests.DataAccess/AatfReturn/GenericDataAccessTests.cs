cb6435af2ddac58f4968de91c9f5a4cc43f1caf2
JohnPadgettCivica <JohnPadgettCivica>
2019-02-13 16:13:01 +0000

    Re-instate code lost during merge.

--- src/EA.Weee.RequestHandlers.Tests.DataAccess/AatfReturn/GenericDataAccessTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.DataAccess/AatfReturn/GenericDataAccessTests.cs
@@ -2,13 +2,16 @@
 {
     using System;
     using System.Threading.Tasks;
+    using Charges;
     using Domain;
     using Domain.AatfReturn;
     using FluentAssertions;
     using RequestHandlers.AatfReturn;
+    using RequestHandlers.Organisations;
     using Weee.Tests.Core;
     using Weee.Tests.Core.Model;
     using Xunit;
+    using CompetentAuthority = Core.Shared.CompetentAuthority;
     using Country = Domain.Country;
     using Operator = Domain.AatfReturn.Operator;
     using Organisation = Domain.Organisation.Organisation;
@@ -23,9 +26,12 @@
                 var helper = new ModelHelper(database.Model);
                 var domainHelper = new DomainHelper(database.WeeeContext);
 
-                var aatf = CreateAatf();
-
                 var dataAccess = new GenericDataAccess(database.WeeeContext);
+                var countryId = new Guid("B5EBE1D1-8349-43CD-9E87-0081EA0A8463");
+                var competantAuthorityDataAccess = new CommonDataAccess(database.WeeeContext);
+                var competantAuthority = await competantAuthorityDataAccess.FetchCompetentAuthority(CompetentAuthority.England);
+
+                var aatf = CreateAatf(competantAuthority);
 
                 var result = await dataAccess.Add<Aatf>(aatf);
 
@@ -33,11 +39,10 @@
             }
         }
 
-        private Aatf CreateAatf()
+        private Aatf CreateAatf(UKCompetentAuthority competentAuthority)
         {
-            return new Aatf("name", 
-                new UKCompetentAuthority(Guid.NewGuid(), "competantauth", "abbv", 
-                    new Country(Guid.NewGuid(), "country"), "email"), 
+            return new Aatf("name",
+                competentAuthority,
                 "12345678",
                 AatfStatus.Approved,
                 new Operator(Organisation.CreatePartnership("trading")));
