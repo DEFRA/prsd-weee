36a27660144ce4b32b7b8b042eb0ebe5ba70ea69
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-02-14 15:42:40 +0000

    removed unused using statements

--- src/EA.Weee.Requests/AatfReturn/GetReturnScheme.cs
+++ src/EA.Weee.Requests/AatfReturn/GetReturnScheme.cs
@@ -1,13 +1,9 @@
 ﻿namespace EA.Weee.Requests.AatfReturn
 {
     using EA.Prsd.Core.Mediator;
-    using EA.Weee.Core.AatfReturn;
     using EA.Weee.Core.Scheme;
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     public class GetReturnScheme : IRequest<List<SchemeData>>
     {
         public Guid ReturnId { get; set; }
