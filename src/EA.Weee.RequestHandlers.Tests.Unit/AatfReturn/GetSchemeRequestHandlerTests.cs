e5d3863d024e71b0200785b9c1fd599e29b69f81
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-02-15 16:14:41 +0000

    unit tests for PBI: 67010 Wee received

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/AatfReturn/GetSchemeRequestHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.DataAccess.DataAccess;
+    using EA.Weee.RequestHandlers.AatfReturn;
+    using EA.Weee.RequestHandlers.Security;
+    using EA.Weee.Requests.AatfReturn;
+    using EA.Weee.Tests.Core;
+    using FakeItEasy;
+    using FluentAssertions;
+    using System;
+    using System.Security;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class GetSchemeRequestHandlerTests
+    {
+        private readonly IReturnSchemeDataAccess returnSchemeDataAccess;
+        private readonly IReturnDataAccess returnDataAccess;
+        private readonly ISchemeDataAccess schemeDataAccess;
+        private GetSchemeRequestHandler handler;
+        private readonly IMapper mapper;
+   
+        public GetSchemeRequestHandlerTests()
+        {
+            var weeeAuthorization = A.Fake<IWeeeAuthorization>();
+            schemeDataAccess = A.Fake<ISchemeDataAccess>();
+            returnDataAccess = A.Fake<IReturnDataAccess>();
+            mapper = A.Fake<IMapper>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_NoExternalAccess_ThrowsSecurityException()
+        {
+            var authorization = new AuthorizationBuilder().DenyExternalAreaAccess().Build();
+            
+            handler = new GetSchemeRequestHandler(authorization, A.Dummy<IReturnSchemeDataAccess>(), A.Dummy<RequestHandlers.Scheme.IGetSchemesDataAccess>(), mapper);
+
+            Func<Task> action = async () => await handler.HandleAsync(A.Dummy<GetReturnScheme>());
+            
+            await action.Should().ThrowAsync<SecurityException>();
+        }
+
+    }
+}
---
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Controller/ReceivedPCSListControllerTests.cs
@@ -0,0 +1,84 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.AatfReturn.Controller
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Core.Scheme;
+    using EA.Weee.Requests.AatfReturn;
+    using EA.Weee.Web.Areas.AatfReturn.Controllers;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Controllers.Base;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ReceivedPCSListControllerTests
+    {
+        private readonly IWeeeClient weeeClient;
+        private readonly ReceivedPCSListController controller;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IMapper mapper;
+
+        public ReceivedPCSListControllerTests()
+        {
+            weeeClient = A.Fake<IWeeeClient>();
+            breadcrumb = A.Fake<BreadcrumbService>();
+            mapper = A.Fake<IMapper>();
+            controller = new ReceivedPCSListController(() => weeeClient, A.Fake<IWeeeCache>(), breadcrumb, mapper);
+        }
+
+        [Fact]
+        public void CheckReceivedPCSListControllerInheritsExternalSiteController()
+        {
+            typeof(ReceivedPCSListController).BaseType.Name.Should().Be(typeof(ExternalSiteController).Name);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenValidViewModel_BreadcrumbShouldBeSet()
+        {
+            var organisationId = Guid.NewGuid();
+
+            await controller.Index(organisationId, A.Dummy<Guid>());
+
+            Assert.Equal(breadcrumb.ExternalActivity, BreadCrumbConstant.AatfReturn);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenActionExecutes_DefaultViewShouldBeReturned()
+        {
+            var result = await controller.Index(A.Dummy<Guid>(), A.Dummy<Guid>()) as ViewResult;
+
+            result.ViewName.Should().Be("Index");        
+        }
+
+        [Fact]
+        public async void IndexGet_GivenReturn_ReceivedPCSListViewModelShouldBeBuilt()
+        {
+            var returnData = new List<SchemeData>();
+
+            A.CallTo(() => weeeClient.SendAsync(A<string>._, A<GetReturnScheme>._)).Returns(returnData);
+
+            await controller.Index(A.Dummy<Guid>(), A.Dummy<Guid>());
+
+            A.CallTo(() => mapper.Map<ReceivedPCSListViewModel>(returnData)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenActionAndParameters_ReceivedPCSListViewModelShouldBeReturned()
+        {
+            var model = A.Fake<ReceivedPCSListViewModel>();
+
+            A.CallTo(() => mapper.Map<ReceivedPCSListViewModel>(A<ReturnData>._)).Returns(model);
+
+            var result = await controller.Index(A.Dummy<Guid>(), A.Dummy<Guid>()) as ViewResult;
+
+            result.Model.Should().BeEquivalentTo(model);
+        }
+    }
+}
