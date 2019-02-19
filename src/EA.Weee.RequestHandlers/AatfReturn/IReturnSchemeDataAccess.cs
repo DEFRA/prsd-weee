36bd623480c9befad59c4d1d3dc4a72fb2787ba2
jaswinder2303 <19706898+jaswinder2303@users.noreply.github.com>
2019-02-19 16:27:59 +0000

    Feature/selectpcs (#94)
    * Summary screen creation
    
    * Added conversion to string for quarter, plus unit test
    
    * resolving issues with cproj files, breadcrumb fix
    
    * converting fetch to return decimal rather than int
    
    * Support for decimals and unit tests
    
    * formatting
    
    * Submitted return updates
    
    * Merge from origin
    
    * Authorization added, unit tests added
    
    * Changes to make the merge work
    
    * Check that user has organisation access
    
    * Submitted return updates
    
    * Added validate anti forgery tag
    
    * Submitted return updates - fix view
    
    * Check return updates
    
    * Compliance year and reporting period added, added functionality for get return to get non ob weee
    
    * Updates to the quarter description calculation
    
    * Change to AATF return test
    
    * Fix for build issue
    
    * Updates to the way the Nonobligated data values are returned
    
    * Added breadcrumb to check my return, unit test for submitted return
    
    * resolve build errors
    
    * Break the summary for sam  to fix :)
    
    * Create return view model base
    
    * Check your return total mapping
    
    * Check your return controller unit tests added
    
    * Added support for operator name in check your return view model
    
    * OperatorData creation
    
    * Altered ooperator data to hold organisationId
    
    * Created unit test for mapping to operator data
    
    * start of pcs selection
    
    * further pcs selection development
    
    * refactoring checkyourreturn, resolving referencing issues
    
    * last little fixes
    
    * Made text boxes vertically centered
    
    * Added ability for selected schemes to be passed to viewmodel, redirects
    
    * changes to AATFReturnScheme table
    
    * Start of submission to return scheme
    
    * more changes to data access
    
    * Added return scheme mapping, removed test info from pcs selection screen
    
    * Integration and unit tests
    
    * selectpcs controller tests
    
    * custom validation
    
    * Resolving validation issues, few more tests
    
    * Alterations
    
    * Changes related to:PBI 67010:WEEE received: show list of PCSs the AATF received from
    
    * added routevalues to actionlink
    
    * received pcs request handlers and requests
    
    * Changes to HandleAsync method
    
    * added Breadcrumb and RedirectAction and removed unused using statments
    
    * removed unused using statements
    
    * added organistionId & organisationName to the viewModel
    
    * amended Aatf Task list button for redirection to Task list page & added Organisation name to the page
    
    * Additional unit tests and changed based on code review
    
    * Removed route to resolve redirect issue
    
    * updated to govuk-frontend 2.7
    
    * Updated files to support govuk-frontend 2.7.0
    
    * Edited drop constraint to check if it exists first
    
    * unit tests for PBI: 67010 Wee received
    
    * unit tests for PBI: 67010:WEEE received: show list of PCSs the AATF received from
    
    * now calls the correct scheme IReturnSchemeDataAccess
    
    * added authorization handler
    
    * tests model properties
    
    * changed to SelectYourPCS routevalue
    
    * removed PCSData class
    
    * code review changes - now returns ReturnScheme from ReturnSchemeDataAccess.GetSelectedSchemesByReturnId
    
    * removed call to GetSchemeBasedOnId as not used in the handler logic.
    
    * Merge from develop
    
    * Fix test

--- src/EA.Weee.RequestHandlers/AatfReturn/IReturnSchemeDataAccess.cs
+++ src/EA.Weee.RequestHandlers/AatfReturn/IReturnSchemeDataAccess.cs
@@ -3,14 +3,12 @@
     using EA.Weee.Domain.AatfReturn;
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
 
     public interface IReturnSchemeDataAccess
     {
         Task<Guid> Submit(ReturnScheme scheme);
 
-        Task<List<Guid>> GetSelectedSchemesByReturnId(Guid returnId);
+        Task<List<ReturnScheme>> GetSelectedSchemesByReturnId(Guid returnId);
     }
 }
