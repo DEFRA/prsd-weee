70ffda1621ae92d5721c6c7786fb0c3a64c2d2a0
Jan Biddle <jan.biddle@civica.co.uk>
2019-02-25 14:56:50 +0000

    * Added basic functionality

--- src/EA.Weee.Web/Areas/AATFReturn/Controllers/ReusedOffSiteController.cs
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/ReusedOffSiteController.cs
@@ -4,62 +4,59 @@
     using System.Threading.Tasks;
     using System.Web.Mvc;
     using EA.Weee.Api.Client;
-    using EA.Weee.Core.AatfReturn;
-    using EA.Weee.Web.Areas.AatfReturn.Requests;
     using EA.Weee.Web.Areas.AatfReturn.ViewModels;
     using EA.Weee.Web.Constant;
     using EA.Weee.Web.Controllers.Base;
-    using EA.Weee.Web.Infrastructure;
     using EA.Weee.Web.Services;
     using EA.Weee.Web.Services.Caching;
 
-    public class ObligatedReusedController : ExternalSiteController
+    public class ReusedOffSiteController : ExternalSiteController
     {
         private readonly Func<IWeeeClient> apiClient;
-        private readonly IObligatedReusedWeeeRequestCreator requestCreator;
         private readonly BreadcrumbService breadcrumb;
         private readonly IWeeeCache cache;
 
-        public ObligatedReusedController(IWeeeCache cache, BreadcrumbService breadcrumb, Func<IWeeeClient> apiClient, IObligatedReusedWeeeRequestCreator requestCreator)
+        public ReusedOffSiteController(Func<IWeeeClient> apiClient, BreadcrumbService breadcrumb, IWeeeCache cache)
         {
             this.apiClient = apiClient;
-            this.requestCreator = requestCreator;
             this.breadcrumb = breadcrumb;
             this.cache = cache;
         }
 
         [HttpGet]
-        public virtual async Task<ActionResult> Index(Guid organisationId, Guid returnId, Guid aatfId)
+        public virtual async Task<ActionResult> Index(Guid organisationId, Guid returnId)
         {
-            var viewModel = new ObligatedViewModel(new ObligatedCategoryValues())
+            var viewModel = new ReusedOffSiteViewModel()
             {
                 OrganisationId = organisationId,
-                ReturnId = returnId,
-                AatfId = aatfId
+                ReturnId = returnId
             };
 
             await SetBreadcrumb(organisationId, BreadCrumbConstant.AatfReturn);
 
-            return View(viewModel);
+            return View("Index", viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public virtual async Task<ActionResult> Index(ObligatedViewModel viewModel)
+        public virtual async Task<ActionResult> Index(ReusedOffSiteViewModel viewModel)
         {
             if (ModelState.IsValid)
             {
-                using (var client = apiClient())
+                if (viewModel.IsReusedOffSite){
+                    return await Task.Run(() => RedirectToAction("Index", "Holding"));
+                }
+                else
                 {
-                    var request = requestCreator.ViewModelToRequest(viewModel);
-
-                    await client.SendAsync(User.GetAccessToken(), request);
-
-                    return RedirectToAction("Index", "ReusedOffSite", new { area = "AatfReturn", organisationId = viewModel.OrganisationId, returnId = viewModel.ReturnId });
+                    return RedirectToAction("Index", "AatfTaskList",
+                                            new { area = "AatfReturn", organisationId = viewModel.OrganisationId, returnId = viewModel.ReturnId });
                 }
             }
-            await SetBreadcrumb(viewModel.OrganisationId, BreadCrumbConstant.AatfReturn);
-            return View(viewModel);
+            else
+            {
+                await SetBreadcrumb(viewModel.OrganisationId, BreadCrumbConstant.AatfReturn);
+                return View(viewModel);
+            }
         }
 
         private async Task SetBreadcrumb(Guid organisationId, string activity)
---
+++ src/EA.Weee.Web/Areas/AATFReturn/ViewModels/ReusedOffSiteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EA.Weee.Web.Areas.AatfReturn.ViewModels
+{
+    public class ReusedOffSiteViewModel
+    {
+        public bool IsReusedOffSite { get; set; }
+
+        public Guid OrganisationId { get; set; }
+
+        public Guid ReturnId { get; set; }
+    }
+}
