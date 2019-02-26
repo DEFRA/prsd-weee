62cd201b71a9398e7adeefeda06f7d8eeb7dee74
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-02-26 14:12:23 +0000

    Added ability for standard non obligated to check against dcf values

--- src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Validation/NonObligatedValuesViewModelValidatorTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Validation/NonObligatedValuesViewModelValidatorTests.cs
@@ -72,5 +72,63 @@
 
             validationResult.IsValid.Should().BeTrue();
         }
+
+        [Fact]
+        public void RuleForEach_ErrorShouldOccurWhenNonObligatedValueIsLowerThanDcfValueOfSameCategoryType()
+        {
+            var model = new NonObligatedValuesViewModel();
+            model.Dcf = true;
+            List<NonObligatedData> nonObligatedList = new List<NonObligatedData>();
+            for (var count = 0; count < model.CategoryValues.Count; count++)
+            {
+                model.CategoryValues.ElementAt(count).Tonnage = (count + 2).ToString();
+                var @decimal = decimal.Parse(model.CategoryValues.ElementAt(count).Tonnage);
+                var nonObligatedData = new NonObligatedData(model.CategoryValues.ElementAt(count).CategoryId, @decimal, true);
+                nonObligatedList.Add(nonObligatedData);
+            }
+            var returnData = new ReturnData();
+            returnData.NonObligatedData = nonObligatedList;
+
+            for (var count = 0; count < model.CategoryValues.Count; count++)
+            {
+                model.CategoryValues.ElementAt(count).Tonnage = (count + 1).ToString();
+                model.CategoryValues.ElementAt(count).Dcf = false;
+            }
+            model.Dcf = false;
+
+            validator = new NonObligatedValuesViewModelValidator(returnData);
+            ValidationResult validationResult = validator.Validate(model);
+
+            validationResult.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void RuleForEach_NoErrorShouldOccurWhenNonObligatedValueIsHigherThanDcfValueOfSameCategoryType()
+        {
+            var model = new NonObligatedValuesViewModel();
+            model.Dcf = true;
+            List<NonObligatedData> nonObligatedList = new List<NonObligatedData>();
+            for (var count = 0; count < model.CategoryValues.Count; count++)
+            {
+                model.CategoryValues.ElementAt(count).Tonnage = (count + 1).ToString();
+                var @decimal = decimal.Parse(model.CategoryValues.ElementAt(count).Tonnage);
+                var nonObligatedData = new NonObligatedData(model.CategoryValues.ElementAt(count).CategoryId, @decimal, true);
+                nonObligatedList.Add(nonObligatedData);
+            }
+            var returnData = new ReturnData();
+            returnData.NonObligatedData = nonObligatedList;
+
+            for (var count = 0; count < model.CategoryValues.Count; count++)
+            {
+                model.CategoryValues.ElementAt(count).Tonnage = (count + 2).ToString();
+                model.CategoryValues.ElementAt(count).Dcf = false;
+            }
+            model.Dcf = false;
+
+            validator = new NonObligatedValuesViewModelValidator(returnData);
+            ValidationResult validationResult = validator.Validate(model);
+
+            validationResult.IsValid.Should().BeTrue();
+        }
     }
 }
--- src/EA.Weee.Web/Areas/AATFReturn/ViewModels/Validation/NonObligatedValuesViewModelValidator.cs
+++ src/EA.Weee.Web/Areas/AATFReturn/ViewModels/Validation/NonObligatedValuesViewModelValidator.cs
@@ -26,8 +26,9 @@
 
                     if (instance != null && instance.Dcf)
                     {
-                        var orderedReturnWeee = returnData.NonObligatedData.OrderBy(r => r.CategoryId).ToList();
-                        var returnTonnage = returnData.NonObligatedData.Where(r => r.CategoryId == o.CategoryId).Select(r => r.Tonnage).FirstOrDefault();
+                        //var nonDcfOnly = returnData.NonObligatedData.Where(r => r.Dcf == false).OrderBy(r => r.CategoryId).ToList();
+                        //var orderedReturnWeee = returnData.NonObligatedData.OrderBy(r => r.CategoryId).ToList();
+                        var returnTonnage = returnData.NonObligatedData.Where(r => r.CategoryId == o.CategoryId && r.Dcf == false).Select(r => r.Tonnage).FirstOrDefault();
                         var value = 0.000m;
 
                         if (o.Tonnage != null)
@@ -39,6 +40,22 @@
                         {
                             context.AddFailure(new ValidationFailure($"o.Tonnage",
                                 $"Category {o.CategoryId} tonnage must be less or equal to {returnTonnage}"));
+                        }
+                    } 
+                    else if (instance != null && !instance.Dcf)
+                    {
+                        var returnTonnage = returnData.NonObligatedData.Where(r => r.CategoryId == o.CategoryId && r.Dcf == true).Select(r => r.Tonnage).FirstOrDefault();
+                        var value = 0.000m;
+
+                        if (o.Tonnage != null)
+                        {
+                            value = decimal.Parse(o.Tonnage);
+                        }
+
+                        if (value < returnTonnage)
+                        {
+                            context.AddFailure(new ValidationFailure($"o.Tonnage",
+                                $"Category {o.CategoryId} tonnage must be more than or equal to {returnTonnage}"));
                         }
                     }
                 });
