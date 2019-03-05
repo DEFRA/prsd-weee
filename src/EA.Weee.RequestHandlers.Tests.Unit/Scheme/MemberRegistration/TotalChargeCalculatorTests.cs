10baa9206522d9e487023fdb81a5ee217a0b8405
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-03-05 10:20:20 +0000

    added test TotalCalculatedCharges_GivenProducerCharges_TotalShouldBeCalculated()

--- src/EA.Weee.RequestHandlers.Tests.Unit/Scheme/MemberRegistration/TotalChargeCalculatorTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Scheme/MemberRegistration/TotalChargeCalculatorTests.cs
@@ -1,19 +1,20 @@
 ﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Scheme.MemberRegistration
 {
     using System;
-    using Domain.Error;
+    using Domain;
     using Domain.Scheme;
-    using EA.Weee.RequestHandlers.DataReturns.ProcessDataReturnXmlFile;
+    using EA.Weee.Domain.Lookup;
     using EA.Weee.RequestHandlers.Scheme.Interfaces;
     using EA.Weee.Requests.Scheme.MemberRegistration;
+    using EA.Weee.Xml.Deserialization;
+    using EA.Weee.Xml.MemberRegistration;
     using FakeItEasy;
     using RequestHandlers.Scheme.MemberRegistration;
+    using RequestHandlers.Security;
     using System.Collections.Generic;
     using System.IO;
     using System.Reflection;
     using System.Text;
-    using Domain;
-    using RequestHandlers.Security;
     using Xml.Converter;
     using Xunit;
 
@@ -21,20 +22,29 @@
     {
         private readonly IXMLChargeBandCalculator xmlChargeBandCalculator;
         private readonly ITotalChargeCalculatorDataAccess totalChargeCalculatorDataAccess;
+        private readonly IProducerChargeCalculator producerChargeCalculator;
         private readonly TotalChargeCalculator totalChargeCalculator;
         private readonly IWeeeAuthorization authorization;
         private bool hasAnnualCharge;
         private decimal? totalCharge;
         private readonly ProcessXmlFile file;
+        private readonly IXmlConverter xmlConverter;
+        private readonly IProducerChargeCalculatorDataAccess producerChargeCalculatorDataAccess;
+        private readonly IProducerChargeBandCalculator producerChargeBandCalculator;
 
         public TotalChargeCalculatorTests()
         {
             xmlChargeBandCalculator = A.Fake<IXMLChargeBandCalculator>();
             totalChargeCalculatorDataAccess = A.Fake<ITotalChargeCalculatorDataAccess>();
+            producerChargeCalculator = A.Fake<IProducerChargeCalculator>();
+            producerChargeCalculatorDataAccess = A.Fake<IProducerChargeCalculatorDataAccess>();
+            producerChargeBandCalculator = A.Fake<IProducerChargeBandCalculator>();
+
             authorization = A.Fake<IWeeeAuthorization>();
             file = ProcessTestXmlFile();
 
             totalChargeCalculator = new TotalChargeCalculator(xmlChargeBandCalculator, authorization, totalChargeCalculatorDataAccess);
+            producerChargeCalculator = new ProducerChargeCalculator(producerChargeCalculatorDataAccess, producerChargeBandCalculator);
         }
 
         [Fact]
@@ -76,6 +86,33 @@
         [Fact]
         public void TotalCalculatedCharges_GivenProducerCharges_TotalShouldBeCalculated()
         {
+            var scheme = Scheme();
+            var producerCharges = A.Fake<Dictionary<string, ProducerCharge>>();
+            var producerCharge = A.Fake<ProducerCharge>();
+            var anyAmount = 700;
+
+            var anyChargeBandAmount = A.Dummy<ChargeBandAmount>();
+
+            hasAnnualCharge = true;
+            totalCharge = 0;
+
+            producerCharge.Amount = anyAmount;
+            producerCharge.ChargeBandAmount = anyChargeBandAmount;
+
+            producerType producer = new producerType
+            {
+                status = statusType.A,
+                registrationNo = "WEE/AB1234CD"
+            };
+
+            A.CallTo(() => producerChargeCalculator.CalculateCharge(string.Empty, producer, 2019)).Returns(producerCharge);
+
+            A.CallTo(() => xmlChargeBandCalculator.Calculate(file)).Returns(producerCharges);
+
+           producerCharges = xmlChargeBandCalculator.Calculate(file);
+
+            var result = totalChargeCalculator.TotalCalculatedCharges(file, scheme, 2019, ref hasAnnualCharge, ref totalCharge);
+            Assert.Equal(125, totalCharge);
         }
 
         [Fact]
@@ -100,6 +137,14 @@
             byte[] xml = Encoding.ASCII.GetBytes(File.ReadAllText(new Uri(absoluteFilePath).LocalPath));
             ProcessXmlFile request = new ProcessXmlFile(A.Dummy<Guid>(), xml, "File name");
             return request;
+        }
+
+        private XMLChargeBandCalculator XmlChargeBandCalculator()
+        {
+            var xmlConverter = new XmlConverter(A.Fake<IWhiteSpaceCollapser>(), new Deserializer());
+
+            IProducerChargeCalculator producerChargerCalculator = null;
+            return new XMLChargeBandCalculator(xmlConverter, producerChargerCalculator);
         }
     }
 }
