53c6a070d21fcae15d682a2d9ec9a64858040a60
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-03-07 09:43:10 +0000

    added new charge bands to ChargeBand table

---
+++ src/EA.Weee.Database/scripts/Update/sprint990-hot-fix/UpdateSROCChargeBands.sql
@@ -0,0 +1,16 @@
+﻿ INSERT INTO Lookup.ChargeBandAmount
+  (Id, Amount, ChargeBand)
+  VALUES('DC8545CD-0C38-40B8-87CF-30AFF1990742', 750, 5)
+  GO
+  INSERT INTO Lookup.ChargeBandAmount
+  (Id, Amount, ChargeBand)
+  VALUES('A8B0DB11-4158-4607-BED4-D476CCCCEB06', 100,6)
+  GO
+  INSERT INTO Lookup.ChargeBandAmount
+  (Id, Amount, ChargeBand)
+  VALUES('7D196466-4DE5-47B4-9D27-1CD3D09571C7',375,7)
+  GO
+  INSERT INTO Lookup.ChargeBandAmount
+  (Id, Amount, ChargeBand)
+  VALUES('D8B5CD20-84AE-4495-A012-3994D8DB921C',100,8)
+  GO
