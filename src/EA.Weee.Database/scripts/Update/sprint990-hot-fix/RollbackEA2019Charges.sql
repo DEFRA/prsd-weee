9dfed0bb5f2fff32c3130483d714ce3963b334cf
JohnPadgettCivica <JohnPadgettCivica>
2019-03-08 16:23:27 +0000

    Add EA rollback script

---
+++ src/EA.Weee.Database/scripts/Update/sprint990-hot-fix/RollbackEA2019Charges.sql
@@ -0,0 +1,28 @@
+﻿
+BEGIN TRANSACTION
+DECLARE @MemberUploadsId TABLE(Id UNIQUEIDENTIFIER NOT NULL)
+SELECT
+	 m.Id
+INTO 
+	MemberUploadsId
+FROM
+	[PCS].[MemberUpload] m
+	INNER JOIN [Organisation].Organisation o ON o.Id = m.OrganisationId
+	INNER JOIN [PCS].[Scheme] s ON s.OrganisationId = o.Id
+	INNER JOIN [Lookup].CompetentAuthority c ON c.Id = s.CompetentAuthorityId 
+WHERE
+	c.Abbreviation = 'EA'
+	AND m.ComplianceYear = 2019
+	AND m.IsSubmitted = 1
+
+UPDATE [Producer].ProducerSubmission SET Invoiced = 0 WHERE MemberUploadId IN (SELECT * FROM MemberUploadsId)
+UPDATE [PCS].[MemberUpload] SET InvoiceRunId = NULL WHERE Id IN (SELECT * FROM MemberUploadsId)
+
+UPDATE [Charging].IbisFileData
+SET 
+	CustomerFileData = '',
+	TransactionFileData = ''
+WHERE
+	FileId = ''
+	
+COMMIT TRANSACTION
