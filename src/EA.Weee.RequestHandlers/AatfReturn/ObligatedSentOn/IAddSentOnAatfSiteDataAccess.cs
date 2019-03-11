08dc64feaac5a0a10599530314492a0ff4c2524e
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-03-11 17:03:12 +0000

    include files from old branch

---
+++ src/EA.Weee.RequestHandlers/AatfReturn/ObligatedSentOn/IAddSentOnAatfSiteDataAccess.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Weee.RequestHandlers.AatfReturn.ObligatedSentOn
+{
+    using EA.Weee.Domain.AatfReturn;
+    using System.Threading.Tasks;
+
+    public interface IAddSentOnAatfSiteDataAccess
+    {
+        Task Submit(WeeeSentOn weeeSentOn);
+    }
+}
---
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/SentOnCreateSiteController.cs
@@ -0,0 +1,87 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Controllers
+{
+    using EA.Weee.Api.Client;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Requests.AatfReturn;
+    using EA.Weee.Requests.Shared;
+    using EA.Weee.Web.Areas.AatfReturn.Requests;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Controllers.Base;
+    using EA.Weee.Web.Infrastructure;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    public class SentOnCreateSiteController : AatfReturnBaseController
+    {
+        private readonly Func<IWeeeClient> apiClient;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+        private readonly IAddSentOnAatfSiteRequestCreator requestCreator;
+
+        public SentOnCreateSiteController(Func<IWeeeClient> apiClient, BreadcrumbService breadcrumb, IWeeeCache cache, IAddSentOnAatfSiteRequestCreator requestCreator)
+        {
+            this.apiClient = apiClient;
+            this.breadcrumb = breadcrumb;
+            this.cache = cache;
+            this.requestCreator = requestCreator;
+        }
+
+        [HttpGet]
+        public virtual async Task<ActionResult> Index(Guid returnId, Guid aatfId)
+        {
+            using (var client = apiClient())
+            {
+                var @return = await client.SendAsync(User.GetAccessToken(), new GetReturn(returnId));
+
+                var viewModel = new SentOnCreateSiteViewModel()
+                {
+                    OrganisationId = @return.ReturnOperatorData.OrganisationId,
+                    ReturnId = returnId,
+                    AatfId = aatfId,
+                    SiteAddressData = new AddressData(),
+                    OperatorAddressData = new AddressData()
+                };
+
+                viewModel.SiteAddressData.Countries = await client.SendAsync(User.GetAccessToken(), new GetCountries(false));
+
+                await SetBreadcrumb(@return.ReturnOperatorData.OrganisationId, BreadCrumbConstant.AatfReturn);
+
+                return View(viewModel);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public virtual async Task<ActionResult> Index(SentOnCreateSiteViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = apiClient())
+                {
+                    var request = requestCreator.ViewModelToRequest(viewModel);
+
+                    await client.SendAsync(User.GetAccessToken(), request);
+                }
+            }
+
+            using (var client = apiClient())
+            {
+                viewModel.SiteAddressData.Countries = await client.SendAsync(User.GetAccessToken(), new GetCountries(false));
+            }
+
+            await SetBreadcrumb(viewModel.OrganisationId, BreadCrumbConstant.AatfReturn);
+            return View(viewModel);
+        }
+
+        private async Task SetBreadcrumb(Guid organisationId, string activity)
+        {
+            breadcrumb.ExternalOrganisation = await cache.FetchOrganisationName(organisationId);
+            breadcrumb.ExternalActivity = activity;
+            breadcrumb.SchemeInfo = await cache.FetchSchemePublicInfo(organisationId);
+        }
+    }
+}
