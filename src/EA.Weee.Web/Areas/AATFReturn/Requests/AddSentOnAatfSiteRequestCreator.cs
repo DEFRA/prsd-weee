1b35a898374bf873608e812a11fcfc3398daa404
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-03-11 17:03:12 +0000

    include files from old branch

---
+++ src/EA.Weee.Web/Areas/AATFReturn/Requests/AddSentOnAatfSiteRequestCreator.cs
@@ -0,0 +1,22 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Requests
+{
+    using EA.Weee.Requests.AatfReturn.Obligated;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+
+    public class AddSentOnAatfSiteRequestCreator : IAddSentOnAatfSiteRequestCreator
+    {
+        public AddSentOnAatfSite ViewModelToRequest(SentOnCreateSiteViewModel viewModel)
+        {
+            var aatfSite = new AddSentOnAatfSite()
+            {
+                OrganisationId = viewModel.OrganisationId,
+                ReturnId = viewModel.ReturnId,
+                AatfId = viewModel.AatfId,
+                SiteAddressData = viewModel.SiteAddressData,
+                OperatorAddressData = viewModel.OperatorAddressData
+            };
+
+            return aatfSite;
+        }
+    }
+}
