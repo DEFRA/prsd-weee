8e320ba551564412d5a63f23bffeda5f5aa436e4
Jan Biddle <jan.biddle@civica.co.uk>
2019-03-11 14:21:48 +0000

    *more tests

--- src/EA.Weee.Web/Areas/AATFReturn/Controllers/ObligatedValueCopyPasteController.cs
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/ObligatedValueCopyPasteController.cs
@@ -1,12 +1,44 @@
 ﻿namespace EA.Weee.Web.Areas.AatfReturn.Controllers
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
-    using EA.Weee.Web.Controllers.Base;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Requests.AatfReturn;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Infrastructure;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
 
-    public class ObligatedValueCopyPasteController : ExternalSiteController
+    public class ObligatedValueCopyPasteController : AatfReturnBaseController
     {
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+        private readonly Func<IWeeeClient> apiClient;
+
+        public ObligatedValueCopyPasteController(Func<IWeeeClient> apiClient, BreadcrumbService breadcrumb, IWeeeCache cache)
+        {
+            this.apiClient = apiClient;
+            this.breadcrumb = breadcrumb;
+            this.cache = cache;
+        }
+
+        [HttpGet]
+        public virtual async Task<ActionResult> Index(Guid returnId, Guid aatfId, Guid schemeId)
+        {
+            using (var client = apiClient())
+            {
+                var @return = await client.SendAsync(User.GetAccessToken(), new GetReturn(returnId));
+                await SetBreadcrumb(@return.ReturnOperatorData.OrganisationId, BreadCrumbConstant.AatfReturn);
+                return View();
+            }
+        }
+
+        private async Task SetBreadcrumb(Guid organisationId, string activity)
+        {
+            breadcrumb.ExternalOrganisation = await cache.FetchOrganisationName(organisationId);
+            breadcrumb.ExternalActivity = activity;
+            breadcrumb.SchemeInfo = await cache.FetchSchemePublicInfo(organisationId);
+        }
     }
 }
