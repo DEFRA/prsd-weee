5cbda38328db8a0ac94bed552ef77804d6bd3e53
Jan Biddle <jan.biddle@civica.co.uk>
2019-03-11 10:11:38 +0000

    Added controller

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Controller/ObligatedValueCopyPasteControllerTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.AatfReturn.Controller
+{
+    using EA.Weee.Web.Areas.AatfReturn.Controllers;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ObligatedValueCopyPasteControllerTests
+    {
+        [Fact]
+        public void CheckObligatedValueCopyPasteControllerInheritsExternalSiteController()
+        {
+            typeof(ObligatedValueCopyPasteController).BaseType.Name.Should().Be(typeof(AatfReturnBaseController).Name);
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/ObligatedValueCopyPasteController.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using EA.Weee.Web.Controllers.Base;
+
+    public class ObligatedValueCopyPasteController : ExternalSiteController
+    {
+    }
+}
