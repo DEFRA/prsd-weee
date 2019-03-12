d142bbdbc9fd820899d02b9b16a93add3000355b
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-03-12 15:42:42 +0000

    paramerterised SetUpProducer method for reuse

--- src/EA.Weee.Xml.Tests.Unit/MemberRegistration/EnvironmentAgencyProducerChargeBandCalculatorTests.cs
+++ src/EA.Weee.Xml.Tests.Unit/MemberRegistration/EnvironmentAgencyProducerChargeBandCalculatorTests.cs
@@ -8,21 +8,39 @@
     public class EnvironmentAgencyProducerChargeBandCalculatorTests
     {
         [Fact]
-        public void GetProducerChargeBand_UKEngland_Morethanorequalto5TEEEplacedonmarket_GreaterthanonemillionpoundsTurnover_VATRegistered_ReturnsChargeBandA2()
+        public void GetProducerChargeBand_UKEngland_Morethanorequalto5TEEEplacedonmarket_GreaterthanonemillionpoundsTurnover_VATRegistered_ReturnsChargeBandA()
         {
             countryType country = countryType.UKENGLAND;
-            producerType producer = SetUpProducer(country);
+            eeePlacedOnMarketBandType eeePlacedOnMarketBandType = eeePlacedOnMarketBandType.Morethanorequalto5TEEEplacedonmarket;
+            annualTurnoverBandType annualTurnoverBandType = annualTurnoverBandType.Greaterthanonemillionpounds;
+            producerType producer = SetUpProducer(country, eeePlacedOnMarketBandType, annualTurnoverBandType, true);
 
             // Act
             ChargeBand result = new EnvironmentAgencyProducerChargeBandCalculator().GetProducerChargeBand(producer);
 
             // Assert
-            Assert.Equal(ChargeBand.A2, result);
+            Assert.Equal(ChargeBand.A, result);
         }
 
-        private static producerType SetUpProducer(countryType countryType)
+        public void GetProducerChargeBand_NonUKEngland_Morethanorequalto5TEEEplacedonmarket_NotVATRegistered_ReturnsChargeBandD2()
+        {
+            countryType country = countryType.FRANCE;
+            eeePlacedOnMarketBandType eeePlacedOnMarketBandType = eeePlacedOnMarketBandType.Morethanorequalto5TEEEplacedonmarket;
+            annualTurnoverBandType annualTurnoverBandType = 0;
+            producerType producer = SetUpProducer(country, eeePlacedOnMarketBandType, annualTurnoverBandType, false);
+
+            // Act
+            ChargeBand result = new EnvironmentAgencyProducerChargeBandCalculator().GetProducerChargeBand(producer);
+
+            // Assert
+            Assert.Equal(ChargeBand.D2, result);
+        }
+        private static producerType SetUpProducer(countryType countryType, eeePlacedOnMarketBandType eeePlacedOnMarketBandType, annualTurnoverBandType annualTurnoverBandType, bool vatRegistered)
         {
             countryType = new countryType();
+            eeePlacedOnMarketBandType = new eeePlacedOnMarketBandType();
+            annualTurnoverBandType = new annualTurnoverBandType();
+
             var producerCountry = new companyType()
             {
                 companyName = "Test company",
@@ -46,9 +64,9 @@
 
             var producer = new producerType
             {
-                annualTurnoverBand = annualTurnoverBandType.Greaterthanonemillionpounds,
-                VATRegistered = true,
-                eeePlacedOnMarketBand = eeePlacedOnMarketBandType.Morethanorequalto5TEEEplacedonmarket,
+                annualTurnoverBand = annualTurnoverBandType,
+                VATRegistered = vatRegistered,
+                eeePlacedOnMarketBand = eeePlacedOnMarketBandType,
                 producerBusiness = producerBusiness
             };
             return producer;
