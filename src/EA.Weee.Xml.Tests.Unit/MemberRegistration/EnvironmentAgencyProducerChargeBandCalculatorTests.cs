b7379ffb49af042e71742083e7c160ceefe456d2
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-03-12 15:12:32 +0000

    added test for UK authority test

---
+++ src/EA.Weee.Xml.Tests.Unit/MemberRegistration/EnvironmentAgencyProducerChargeBandCalculatorTests.cs
@@ -0,0 +1,57 @@
+﻿namespace EA.Weee.Xml.Tests.Unit.MemberRegistration
+{
+    using Domain.Lookup;
+    using EA.Weee.Xml.MemberRegistration;
+    using FakeItEasy;
+    using Xunit;
+
+    public class EnvironmentAgencyProducerChargeBandCalculatorTests
+    {
+        [Fact]
+        public void GetProducerChargeBand_UKEngland_Morethanorequalto5TEEEplacedonmarket_GreaterthanonemillionpoundsTurnover_VATRegistered_ReturnsChargeBandA2()
+        {
+            countryType country = countryType.UKENGLAND;
+            producerType producer = SetUpProducer(country);
+
+            // Act
+            ChargeBand result = new EnvironmentAgencyProducerChargeBandCalculator().GetProducerChargeBand(producer);
+
+            // Assert
+            Assert.Equal(ChargeBand.A2, result);
+        }
+
+        private static producerType SetUpProducer(countryType countryType)
+        {
+            countryType = new countryType();
+            var producerCountry = new companyType()
+            {
+                companyName = "Test company",
+                companyNumber = "Test CRN",
+                registeredOffice = new contactDetailsContainerType()
+                {
+                    contactDetails = new contactDetailsType()
+                    {
+                        address = new addressType()
+                        {
+                            country = countryType
+                        }
+                    }
+                }
+            };
+
+            var producerBusiness = new producerBusinessType()
+            {
+                Item = producerCountry
+            };
+
+            var producer = new producerType
+            {
+                annualTurnoverBand = annualTurnoverBandType.Greaterthanonemillionpounds,
+                VATRegistered = true,
+                eeePlacedOnMarketBand = eeePlacedOnMarketBandType.Morethanorequalto5TEEEplacedonmarket,
+                producerBusiness = producerBusiness
+            };
+            return producer;
+        }
+    }
+}
