8019d505a2648d71f5e7090a2ee0ec4c68780938
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-03-13 14:26:28 +0000

    wip

---
+++ src/EA.Weee.Xml.Tests.Unit/MemberRegistration/ProducerChargeBandCalculatorChooserTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Xml.Tests.Unit.MemberRegistration
+{
+    using Domain.Lookup;
+    using EA.Weee.Xml.MemberRegistration;
+    using Xunit;
+    public class ProducerChargeBandCalculatorChooserTests
+    {
+        public ProducerChargeBandCalculatorChooserTests()
+        {
+        }
+        public void ChooseEACalculatorForCalculatingChargeBands()
+        {
+        }
+    }
+}
