c0bf832e2fc2a1b3f43aa5246828729807ce675c
JohnPadgettCivica <JohnPadgettCivica>
2019-03-13 11:11:33 +0000

    Fix for member upload validation

--- src/EA.Weee.Web/Scripts/custom-validation.js
+++ src/EA.Weee.Web/Scripts/custom-validation.js
@@ -3,6 +3,7 @@
     onkeyup: false,
     onclick: false,
     focusInvalid: false,
+    errorClass: "",
     // Create a custom error summary that will appear before the form
     showErrors: function (errorMap, errorList) {
         if (errorList.length !== 0) {
@@ -16,27 +17,29 @@
                 headerErrorCount = errorList.length + " errors";
             }
 
-            summary = "<h2 class='heading-medium error-summary-heading'>You have " + headerErrorCount + " on this page</h2>";
-
-            $(".govuk-form-group").removeClass("error");
-
-            //Generate our error summary list
+            summary = "<h2 class='error-summary-heading heading-medium govuk-error-summary__title' id=\"error-summary-title\">You have " + headerErrorCount + " on this page</h2>";
+            summary += '<div class="govuk-error-summary__body">';
+            summary += '<ul class="govuk-list govuk-error-summary__list"';
+            
+            
             for (error in errorList) {
                 summary += '<li><a href="#' + errorList[error].element.id + '">' + errorList[error].message + '</a></li>';
 
-                $(errorList[error].element).parents(".govuk-form-group").last().addClass("error");
+                $(errorList[error].element).parents(".govuk-form-group").last().addClass("govuk-form-group--error");
             }
 
-            // Toggle classes on validation summary
-            $('.error-summary-valid').addClass('error-summary-errors').removeClass('error-summary-valid');
-
-            $('.error-summary-errors').addClass('error-summary');
-
-            // Output our error summary and place it in the error container
-            $('.error-summary-errors ul').html(summary);
-
-            // Focus on first error link in the error container 
-            $('.error-summary-errors a').first().focus();
+            summary += "</ul>";
+            summary += "</div>";
+            //// Toggle classes on validation summary
+            $('.error-summary-valid').removeClass('validation-summary-errors');
+            $('.error-summary-valid').removeClass('error-summary');
+            $('.error-summary-valid').addClass('govuk-error-summary');
+            //// Output our error summary and place it in the error container
+            $('.error-summary-valid').html(summary);
+            
+            //$(".input-validation-error").removeClass("govuk-file-upload input-validation-errror");
+            //// Focus on first error link in the error container 
+            $('.error-summary-valid').first().focus();
         }
         this.defaultShowErrors();
     }
