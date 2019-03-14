b44f79ed8ffdcb3a04352badfdb00867e66912d5
JohnPadgettCivica <JohnPadgettCivica>
2019-03-14 11:39:58 +0000

    Fix error

--- src/EA.Weee.Database/scripts/Update/SROC-hot-fix/1.annual-charge.sql
+++ src/EA.Weee.Database/scripts/Update/SROC-hot-fix/1.annual-charge.sql
@@ -13,8 +13,8 @@ IF NOT EXISTS(SELECT 1 FROM sys.columns
           WHERE Name = N'AnnualChargeAmount'
           AND Object_ID = Object_ID(N'[Lookup].[CompetentAuthority]'))
 BEGIN
-    ALTER TABLE [PCS].[MemberUpload]
-	ADD HasAnnualCharge bit NOT NULL DEFAULT 0
+    ALTER TABLE [Lookup].[CompetentAuthority]	
+	ADD AnnualChargeAmount [decimal](18, 2)
 END
 GO
 
