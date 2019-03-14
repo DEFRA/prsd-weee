d5c319d76a48e4fc4564bd58fc8792e4419359b2
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-03-14 08:03:48 +0000

    tests to choose which calculator to use

--- src/EA.Weee.Xml.Tests.Unit/MemberRegistration/ProducerChargeBandCalculatorChooserTests.cs
+++ src/EA.Weee.Xml.Tests.Unit/MemberRegistration/ProducerChargeBandCalculatorChooserTests.cs
@@ -1,15 +1,41 @@
 ﻿namespace EA.Weee.Xml.Tests.Unit.MemberRegistration
 {
-    using Domain.Lookup;
     using EA.Weee.Xml.MemberRegistration;
+    using FakeItEasy;
     using Xunit;
     public class ProducerChargeBandCalculatorChooserTests
     {
+        private readonly IProducerChargeBandCalculatorChooser producerChargeBandCalculatorChooser;
+        private readonly IEnvironmentAgencyProducerChargeBandCalculator environmentAgencyProducerChargeBandCalculator;
+        private readonly IProducerChargeBandCalculator producerChargeBandCalculator;
+
         public ProducerChargeBandCalculatorChooserTests()
         {
+            environmentAgencyProducerChargeBandCalculator = A.Fake<IEnvironmentAgencyProducerChargeBandCalculator>();
+            producerChargeBandCalculator = A.Fake<IProducerChargeBandCalculator>();
+            producerChargeBandCalculatorChooser = new ProducerChargeBandCalculatorChooser(environmentAgencyProducerChargeBandCalculator, producerChargeBandCalculator);
         }
-        public void ChooseEACalculatorForCalculatingChargeBands()
+
+        [Fact]
+        public void ChoosePost2018_CalculatorForCalculatingChargeBands_Returns_EnvironmentAgencyProducerChargeBandCalculator()
         {
+            var producerType = A.Fake<producerType>();
+            var schemeType = A.Fake<schemeType>();
+         
+            var calculatorToUse = producerChargeBandCalculatorChooser.GetCalculator(schemeType, producerType, 2019);
+            
+            Assert.Equal(calculatorToUse, environmentAgencyProducerChargeBandCalculator);
+        }
+
+        [Fact]
+        public void ChoosePre2018_CalculatorForCalculatingChargeBands_Returns_ProducerChargeBandCalculator()
+        {
+            var producerType = A.Fake<producerType>();
+            var schemeType = A.Fake<schemeType>();
+
+            var calculatorToUse = producerChargeBandCalculatorChooser.GetCalculator(schemeType, producerType, 2018);
+
+            Assert.Equal(calculatorToUse, producerChargeBandCalculator);
         }
     }
 }
