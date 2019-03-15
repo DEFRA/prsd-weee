ec9369f38b30c7c4409b6ae6701cd51cc532da25
JohnPadgettCivica <JohnPadgettCivica>
2019-03-15 16:57:02 +0000

    Scheme by scheme id to cache

---
+++ src/EA.Weee.Requests/Scheme/GetSchemePublicInfoBySchemeId.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Requests.Scheme
+{
+    using System;
+    using Core.Scheme;
+    using Prsd.Core.Mediator;
+
+    public class GetSchemePublicInfoBySchemeId : IRequest<SchemePublicInfo>
+    {
+        public Guid SchemeId { get; private set; }
+
+        public GetSchemePublicInfoBySchemeId(Guid schemeId)
+        {
+            SchemeId = schemeId;
+        }
+    }
+}
