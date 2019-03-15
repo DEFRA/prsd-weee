4847b88194daf734d36129fd52a1d4f083e1971d
Jan Biddle <jan.biddle@civica.co.uk>
2019-03-15 10:12:29 +0000

    switch branch

---
+++ src/EA.Weee.Core/AatfReturn/ReceivedPcsData.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Core.AatfReturn
+{
+    using System;
+    public class ReceivedPcsData
+    {
+        public Guid SchemeId { get; set; }
+
+        public string SchemeName { get; set; }
+
+        public string ApprovalNumber { get; set; }
+
+        public string B2bTotal { get; set; }
+
+        public string B2cTotal { get; set; }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/AATFReturn/Mappings/ToViewModel/ReturnAndSchemeDataToReceivedPcsViewModelMap.cs
@@ -0,0 +1,50 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel
+{
+    using System.Collections.Generic;
+    using EA.Prsd.Core;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Core.Helpers;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Services.Caching;
+
+    public class ReturnAndSchemeDataToReceivedPcsViewModelMap : IMap<ReturnAndSchemeDataToReceivedPcsViewModelMapTransfer, ReceivedPcsListViewModel>
+    {
+        private readonly IWeeeCache cache;
+        private readonly IMap<ObligatedDataToObligatedValueMapTransfer, IList<ObligatedCategoryValue>> obligatedMap;
+        private readonly ICategoryValueTotalCalculator calculator;
+
+        public ReceivedPcsListViewModel Map(ReturnAndSchemeDataToReceivedPcsViewModelMapTransfer source)
+        {
+            Guard.ArgumentNotNull(() => source, source);
+
+            var viewModel = new ReceivedPcsListViewModel()
+            {
+                AatfId = source.AatfId,
+                AatfName = source.AatfName,
+                OrganisationId = source.OrganisationId,
+                ReturnId = source.ReturnId
+            };
+
+            var schemeList = new List<ReceivedPcsData>();
+
+            calculator.Total(source.ReturnData.ObligatedWeeeReceivedData)
+
+            foreach (var scheme in source.SchemeDataItems)
+            {
+                var receivedPcsData = new ReceivedPcsData()
+                {
+                    ApprovalNumber = scheme.ApprovalName,
+                    SchemeId = scheme.Id,
+                    SchemeName = scheme.SchemeName
+                };
+
+                schemeList.Add(receivedPcsData);
+            }
+
+            viewModel.SchemeList = schemeList;
+
+            return viewModel;
+        }
+    }
+}
