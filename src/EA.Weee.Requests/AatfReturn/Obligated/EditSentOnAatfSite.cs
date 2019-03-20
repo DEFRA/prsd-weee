7b8c8be1d5afb5721b076998e21169c9d344f2fb
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-03-20 14:18:58 +0000

    Post action now updates relevant item, start of get request to obtain current site address data

--- src/EA.Weee.Requests/AatfReturn/Obligated/EditSentOnAatfSite.cs
+++ src/EA.Weee.Requests/AatfReturn/Obligated/EditSentOnAatfSite.cs
@@ -1,13 +1,9 @@
 ﻿namespace EA.Weee.Requests.AatfReturn.Obligated
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class EditSentOnAatfSite : AddSentOnAatfSite
     {
-        public Guid WeeeSentOnId { get; private set; }
+        public Guid WeeeSentOnId { get; set; }
     }
 }
