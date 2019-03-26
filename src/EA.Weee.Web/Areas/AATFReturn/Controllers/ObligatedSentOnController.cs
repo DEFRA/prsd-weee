2a8aa027a498ff4c70d89248f6665c13c9c85bd5
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-03-26 16:19:44 +0000

    Setting up view model, added data access function

--- src/EA.Weee.Web/Areas/AATFReturn/Controllers/ObligatedSentOnController.cs
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/ObligatedSentOnController.cs
@@ -1,7 +1,12 @@
 ﻿namespace EA.Weee.Web.Areas.AatfReturn.Controllers
 {
+    using EA.Prsd.Core.Mapper;
     using EA.Weee.Api.Client;
+    using EA.Weee.Requests.AatfReturn;
+    using EA.Weee.Requests.AatfReturn.Obligated;
+    using EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel;
     using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Infrastructure;
     using EA.Weee.Web.Services;
     using EA.Weee.Web.Services.Caching;
     using System;
@@ -16,10 +21,12 @@
         private readonly Func<IWeeeClient> apiClient;
         private readonly BreadcrumbService breadcrumb;
         private readonly IWeeeCache cache;
+        private readonly IMap<ReturnToObligatedViewModelMapTransfer, ObligatedViewModel> mapper;
 
         public ObligatedSentOnController(IWeeeCache cache,
             BreadcrumbService breadcrumb,
-            Func<IWeeeClient> apiClient)
+            Func<IWeeeClient> apiClient,
+            IMap<ReturnToObligatedViewModelMapTransfer, ObligatedViewModel> mapper)
         {
             this.apiClient = apiClient;
             this.breadcrumb = breadcrumb;
@@ -31,7 +38,19 @@
         {
             using (var client = apiClient())
             {
-                var model = new ObligatedSentOnViewModel();
+                var weeeSentOnOperator = await client.SendAsync(User.GetAccessToken(), new GetSentOnOperatorSite(weeeSentOnId));
+
+                var @return = await client.SendAsync(User.GetAccessToken(), new GetReturn(returnId));
+
+                var model = mapper.Map(new ReturnToObligatedViewModelMapTransfer()
+                {
+                    AatfId = aatfId,
+                    OrganisationId = @return.ReturnOperatorData.OrganisationId,
+                    ReturnId = returnId,
+                    ReturnData = @return
+                }) as ObligatedSentOnViewModel;
+
+                model.OperatorName = weeeSentOnOperator.Name;
 
                 return View(model);
             }
