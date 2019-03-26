c7b2d369ed8ceb79302956729c353e30019694c6
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-03-26 13:35:32 +0000

    Start of controller, view model, and view

---
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/ObligatedSentOnController.cs
@@ -0,0 +1,40 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Controllers
+{
+    using EA.Weee.Api.Client;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class ObligatedSentOnController : AatfReturnBaseController
+    {
+        private readonly Func<IWeeeClient> apiClient;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+
+        public ObligatedSentOnController(IWeeeCache cache,
+            BreadcrumbService breadcrumb,
+            Func<IWeeeClient> apiClient)
+        {
+            this.apiClient = apiClient;
+            this.breadcrumb = breadcrumb;
+            this.cache = cache;
+        }
+
+        [HttpGet]
+        public virtual async Task<ActionResult> Index(Guid returnId, Guid aatfId, Guid weeeSentOnId)
+        {
+            using (var client = apiClient())
+            {
+                var model = new ObligatedSentOnViewModel();
+
+                return View(model);
+            }
+        }
+    }
+}
