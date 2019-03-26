e802c261f7d028f294c6c1deccec493293f9843e
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-03-26 13:35:32 +0000

    Start of controller, view model, and view

---
+++ src/EA.Weee.Web/Areas/AATFReturn/ViewModels/ObligatedSentOnViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class ObligatedSentOnViewModel
+    {
+    }
+}
