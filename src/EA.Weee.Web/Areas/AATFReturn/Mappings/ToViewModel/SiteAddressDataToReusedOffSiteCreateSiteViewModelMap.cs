502b2d75238f1440888cacc47190007b129205ac
JohnPadgettCivica <JohnPadgettCivica>
2019-03-27 11:34:38 +0000

    Edit site update

--- src/EA.Weee.Web/Areas/AATFReturn/Mappings/ToViewModel/SiteAddressDataToReusedOffSiteCreateSiteViewModelMap.cs
+++ src/EA.Weee.Web/Areas/AATFReturn/Mappings/ToViewModel/SiteAddressDataToReusedOffSiteCreateSiteViewModelMap.cs
@@ -1,6 +1,7 @@
 ﻿namespace EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel
 {
     using System.Linq;
+    using Core.AatfReturn;
     using EA.Prsd.Core;
     using EA.Prsd.Core.Mapper;
     using EA.Weee.Web.Areas.AatfReturn.ViewModels;
@@ -10,16 +11,27 @@
         public ReusedOffSiteCreateSiteViewModel Map(SiteAddressDataToReusedOffSiteCreateSiteViewModelMapTransfer source)
         {
             Guard.ArgumentNotNull(() => source, source);
+            Guard.ArgumentNotNull(() => source.Countries, source.Countries);
 
-            var viewModel = new ReusedOffSiteCreateSiteViewModel()
+            var viewModel = new ReusedOffSiteCreateSiteViewModel
             {
                 AatfId = source.AatfId,
                 OrganisationId = source.OrganisationId,
-                ReturnId = source.ReturnId
+                ReturnId = source.ReturnId,
+                SiteId = source.SiteId,
+                AddressData = new SiteAddressData()
             };
 
-            viewModel.AddressData = source.ReturnedSites.AddressData.Where(s => s.Id == source.SiteId).FirstOrDefault();
             viewModel.AddressData.Countries = source.Countries;
+
+            if (source.SiteId.HasValue)
+            {
+                viewModel.AddressData = source.ReturnedSites.AddressData.FirstOrDefault(s => s.Id == source.SiteId);
+                if (viewModel.AddressData != null)
+                {
+                    viewModel.AddressData.Countries = source.Countries;
+                }
+            }
 
             return viewModel;
         }
