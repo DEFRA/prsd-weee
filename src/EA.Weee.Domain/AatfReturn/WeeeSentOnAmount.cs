a987f491c835b3d050b20b4612cbe6b32c723c07
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-03-28 11:28:01 +0000

    controllers, requets handlers etc.

---
+++ src/EA.Weee.Domain/AatfReturn/WeeeSentOnAmount.cs
@@ -0,0 +1,37 @@
+﻿namespace EA.Weee.Domain.AatfReturn
+{
+    using EA.Prsd.Core;
+    using System;
+
+    public class WeeeSentOnAmount : ObligatedAmount, IObligatedAmount
+    {
+        public virtual WeeeSentOn WeeeSentOn { get; private set; }
+
+        public virtual Guid WeeeSentOnId { get; private set; }
+
+        public int CategoryId { get; set; }
+
+        public WeeeSentOnAmount(WeeeSentOn aatfWeeeSentOn, int categoryId, decimal? householdTonnage, decimal? nonHouseholdTonnage, Guid weeeSentOnId)
+        {
+            Guard.ArgumentNotNull(() => aatfWeeeSentOn, aatfWeeeSentOn);
+
+            WeeeSentOn = aatfWeeeSentOn;
+            CategoryId = categoryId;
+            HouseholdTonnage = householdTonnage;
+            NonHouseholdTonnage = nonHouseholdTonnage;
+            WeeeSentOnId = weeeSentOnId;
+        }
+
+        public WeeeSentOnAmount(int categoryId, decimal? householdTonnage, decimal? nonHouseholdTonnage, Guid weeeSentOnId)
+        {
+            CategoryId = categoryId;
+            HouseholdTonnage = householdTonnage;
+            NonHouseholdTonnage = nonHouseholdTonnage;
+            WeeeSentOnId = weeeSentOnId;
+        }
+
+        protected WeeeSentOnAmount()
+        {
+        }
+    }
+}
---
+++ src/EA.Weee.RequestHandlers/AatfReturn/ObligatedSentOn/AddObligatedSentOnHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace EA.Weee.RequestHandlers.AatfReturn.ObligatedSentOn
+{
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.Domain.AatfReturn;
+    using EA.Weee.RequestHandlers.Security;
+    using EA.Weee.Requests.AatfReturn.Obligated;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class AddObligatedSentOnHandler : IRequestHandler<AddObligatedSentOn, bool>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly IObligatedSentOnDataAccess obligatedSentOnDataAccess;
+
+        public AddObligatedSentOnHandler(IWeeeAuthorization authorization,
+            IObligatedSentOnDataAccess obligatedSentOnDataAccess)
+        {
+            this.authorization = authorization;
+            this.obligatedSentOnDataAccess = obligatedSentOnDataAccess;
+        }
+
+        public async Task<bool> HandleAsync(AddObligatedSentOn message)
+        {
+            authorization.EnsureCanAccessExternalArea();
+
+            var aatfWeeeSentOn = new WeeeSentOn(
+                message.AatfId,
+                message.ReturnId,
+                message.SiteAddressId);
+
+            var aatfWeeeSentOnAmount = new List<WeeeSentOnAmount>();
+
+            foreach (var categoryValue in message.CategoryValues)
+            {
+                aatfWeeeSentOnAmount.Add(new WeeeSentOnAmount(categoryValue.CategoryId, categoryValue.HouseholdTonnage, categoryValue.NonHouseholdTonnage, message.WeeeSentOnId));
+            }
+
+            await obligatedSentOnDataAccess.Submit(aatfWeeeSentOnAmount);
+
+            return true;
+        }
+    }
+}
