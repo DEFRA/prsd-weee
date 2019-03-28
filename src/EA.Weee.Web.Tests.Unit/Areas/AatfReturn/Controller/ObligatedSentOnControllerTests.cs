f66460dd355ff7f6466d3aac86a1ac639a499757
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-03-28 14:46:10 +0000

    obligated sent on controller tests

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Controller/ObligatedSentOnControllerTests.cs
@@ -0,0 +1,100 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.AatfReturn.Controller
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Core.Helpers;
+    using EA.Weee.Core.Scheme;
+    using EA.Weee.Requests.AatfReturn;
+    using EA.Weee.Requests.AatfReturn.Obligated;
+    using EA.Weee.Web.Areas.AatfReturn.Controllers;
+    using EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel;
+    using EA.Weee.Web.Areas.AatfReturn.Requests;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using FakeItEasy;
+    using FluentAssertions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Xunit;
+
+    public class ObligatedSentOnControllerTests
+    {
+        private readonly IWeeeClient weeeClient;
+        private readonly IObligatedSentOnWeeeRequestCreator requestCreator;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly ObligatedSentOnController controller;
+        private readonly IWeeeCache cache;
+        private readonly IMap<ReturnToObligatedViewModelMapTransfer, ObligatedViewModel> mapper;
+        private readonly ICategoryValueTotalCalculator calculator;
+
+        public ObligatedSentOnControllerTests()
+        {
+            weeeClient = A.Fake<IWeeeClient>();
+            requestCreator = A.Fake<IObligatedSentOnWeeeRequestCreator>();
+            breadcrumb = A.Fake<BreadcrumbService>();
+            cache = A.Fake<IWeeeCache>();
+            mapper = A.Fake<IMap<ReturnToObligatedViewModelMapTransfer, ObligatedViewModel>>();
+            calculator = A.Fake<ICategoryValueTotalCalculator>();
+
+            controller = new ObligatedSentOnController(cache, breadcrumb, () => weeeClient, mapper, requestCreator);
+        }
+
+        [Fact]
+        public void CheckObligatedReusedControllerInheritsExternalSiteController()
+        {
+            typeof(ObligatedReusedController).BaseType.Name.Should().Be(typeof(AatfReturnBaseController).Name);
+        }
+
+        [Fact]
+        public async void IndexPost_GivenValidViewModel_ApiSendShouldBeCalled()
+        {
+            var model = new ObligatedViewModel(calculator);
+            var request = new AddObligatedSentOn();
+
+            A.CallTo(() => requestCreator.ViewModelToRequest(model)).Returns(request);
+
+            await controller.Index(model);
+
+            A.CallTo(() => weeeClient.SendAsync(A<string>._, request)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public async void IndexPost_GivenInvalidViewModel_ApiShouldNotBeCalled()
+        {
+            controller.ModelState.AddModelError("error", "error");
+
+            await controller.Index(A.Dummy<ObligatedViewModel>());
+
+            A.CallTo(() => weeeClient.SendAsync(A<string>._, A<AddObligatedReused>._)).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async void IndexGet_GivenValidViewModel_BreadcrumbShouldBeSet()
+        {
+            var organisationId = Guid.NewGuid();
+            const string orgName = "orgName";
+
+            A.CallTo(() => cache.FetchOrganisationName(organisationId)).Returns(orgName);
+
+            await controller.Index(A.Dummy<Guid>(), organisationId, A.Dummy<Guid>(), A.Dummy<Guid>());
+
+            breadcrumb.ExternalActivity.Should().Be(BreadCrumbConstant.AatfReturn);
+            breadcrumb.ExternalOrganisation.Should().Be(orgName);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenAction_DefaultViewShouldBeReturned()
+        {
+            var result = await controller.Index(A.Dummy<Guid>(), A.Dummy<Guid>(), A.Dummy<Guid>(), A.Dummy<Guid>()) as ViewResult;
+
+            result.ViewName.Should().BeEmpty();
+        }
+    }
+}
