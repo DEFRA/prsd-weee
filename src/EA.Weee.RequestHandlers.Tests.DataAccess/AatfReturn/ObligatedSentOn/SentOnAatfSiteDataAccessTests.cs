e5f9c438b8e62426203c6a827e55f984786d6ccd
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-04-09 14:19:34 +0100

    Data access and request handler tests

--- src/EA.Weee.RequestHandlers.Tests.DataAccess/AatfReturn/ObligatedSentOn/SentOnAatfSiteDataAccessTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.DataAccess/AatfReturn/ObligatedSentOn/SentOnAatfSiteDataAccessTests.cs
@@ -7,7 +7,9 @@
     using EA.Weee.Tests.Core;
     using FakeItEasy;
     using FluentAssertions;
+    using System;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
     using Xunit;
 
     public class SentOnAatfSiteDataAccessTests
@@ -58,6 +60,82 @@
 
             A.CallTo(() => context.WeeeSentOn.Add(site)).MustHaveHappened(Repeated.Exactly.Once)
                 .Then(A.CallTo(() => context.SaveChangesAsync()).MustHaveHappened(Repeated.Exactly.Once));
+        }
+
+        [Fact]
+        public async Task GetWeeeSentOnOperatorAddress_GivenWeeeSentOnId_AddressShouldBeReturned()
+        {
+            var aatfId = Guid.NewGuid();
+            var returnId = Guid.NewGuid();
+
+            var aatfAddressMatch = A.Fake<AatfAddress>();
+            var weeeSentOn = A.Fake<WeeeSentOn>();
+
+            A.CallTo(() => context.AatfAddress).Returns(dbContextHelper.GetAsyncEnabledDbSet(new List<AatfAddress>() { aatfAddressMatch }));
+            A.CallTo(() => weeeSentOn.AatfId).Returns(aatfId);
+            A.CallTo(() => weeeSentOn.ReturnId).Returns(returnId);
+            A.CallTo(() => weeeSentOn.OperatorAddress).Returns(aatfAddressMatch);
+            A.CallTo(() => context.WeeeSentOn).Returns(dbContextHelper.GetAsyncEnabledDbSet(new List<WeeeSentOn>() { weeeSentOn }));
+
+            var result = await dataAccess.GetWeeeSentOnOperatorAddress(weeeSentOn.Id);
+
+            result.Should().BeEquivalentTo(aatfAddressMatch);
+        }
+
+        [Fact]
+        public async Task GetWeeeSentOnSiteAddress_GivenWeeeSentOnId_AddressShouldBeReturned()
+        {
+            var aatfId = Guid.NewGuid();
+            var returnId = Guid.NewGuid();
+
+            var aatfAddressMatch = A.Fake<AatfAddress>();
+            var weeeSentOn = A.Fake<WeeeSentOn>();
+
+            A.CallTo(() => context.AatfAddress).Returns(dbContextHelper.GetAsyncEnabledDbSet(new List<AatfAddress>() { aatfAddressMatch }));
+            A.CallTo(() => weeeSentOn.AatfId).Returns(aatfId);
+            A.CallTo(() => weeeSentOn.ReturnId).Returns(returnId);
+            A.CallTo(() => weeeSentOn.SiteAddress).Returns(aatfAddressMatch);
+            A.CallTo(() => context.WeeeSentOn).Returns(dbContextHelper.GetAsyncEnabledDbSet(new List<WeeeSentOn>() { weeeSentOn }));
+
+            var result = await dataAccess.GetWeeeSentOnSiteAddress(weeeSentOn.Id);
+
+            result.Should().BeEquivalentTo(aatfAddressMatch);
+        }
+
+        [Fact]
+        public async Task GetWeeeSentOnByReturnAndAatf_GivenAatfAndReturnId_WeeeSentOnListReturned()
+        {
+            var aatfId = Guid.NewGuid();
+            var returnId = Guid.NewGuid();
+
+            var weeeSentOn = A.Fake<WeeeSentOn>();
+
+            A.CallTo(() => weeeSentOn.AatfId).Returns(aatfId);
+            A.CallTo(() => weeeSentOn.ReturnId).Returns(returnId);
+            A.CallTo(() => context.WeeeSentOn).Returns(dbContextHelper.GetAsyncEnabledDbSet(new List<WeeeSentOn>() { weeeSentOn }));
+
+            var result = await dataAccess.GetWeeeSentOnByReturnAndAatf(aatfId, returnId);
+
+            result.Should().BeEquivalentTo(weeeSentOn);
+        }
+
+        [Fact]
+        public async Task UpdateWithOperatorAddress_GivenAatfAndReturnId_WeeeSentOnShouldContainOperatorAddress()
+        {
+            var aatfId = Guid.NewGuid();
+            var returnId = Guid.NewGuid();
+
+            var operatorAddress = new AatfAddress("TEST", "TEST", "TEST", "TEST", "TEST", "GU22 7UY", A.Dummy<Country>());
+            var weeeSentOn = A.Fake<WeeeSentOn>();
+
+            A.CallTo(() => context.AatfAddress).Returns(dbContextHelper.GetAsyncEnabledDbSet(new List<AatfAddress>() { operatorAddress }));
+            A.CallTo(() => weeeSentOn.AatfId).Returns(aatfId);
+            A.CallTo(() => weeeSentOn.ReturnId).Returns(returnId);
+            A.CallTo(() => context.WeeeSentOn).Returns(dbContextHelper.GetAsyncEnabledDbSet(new List<WeeeSentOn>() { weeeSentOn }));
+
+            await dataAccess.UpdateWithOperatorAddress(weeeSentOn, operatorAddress);
+
+            A.CallTo(() => weeeSentOn.UpdateWithOperatorAddress(operatorAddress)).MustHaveHappened(Repeated.Exactly.Once).Then(A.CallTo(() => context.SaveChangesAsync()).MustHaveHappened(Repeated.Exactly.Once));
         }
     }
 }
