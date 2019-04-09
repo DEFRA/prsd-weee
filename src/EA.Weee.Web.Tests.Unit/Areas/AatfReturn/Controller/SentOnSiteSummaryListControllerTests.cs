26cfebe7bb6ce8520d9f2276e649c287ae402270
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-04-09 09:24:56 +0100

    Additional unit tests

--- src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Controller/SentOnSiteSummaryListControllerTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Controller/SentOnSiteSummaryListControllerTests.cs
@@ -1,12 +1,75 @@
 ﻿namespace EA.Weee.Web.Tests.Unit.Areas.AatfReturn.Controller
 {
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Requests.AatfReturn;
+    using EA.Weee.Web.Areas.AatfReturn.Controllers;
+    using EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Controllers.Base;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using FakeItEasy;
+    using FluentAssertions;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Xunit;
 
     public class SentOnSiteSummaryListControllerTests
     {
+        private readonly IWeeeClient apiClient;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+        private readonly IMap<ReturnAndAatfToSentOnSummaryListViewModelMapTransfer, SentOnSiteSummaryListViewModel> mapper;
+        private readonly SentOnSiteSummaryListController controller;
+
+        public SentOnSiteSummaryListControllerTests()
+        {
+            this.apiClient = A.Fake<IWeeeClient>();
+            this.breadcrumb = A.Fake<BreadcrumbService>();
+            this.cache = A.Fake<IWeeeCache>();
+            this.mapper = A.Fake<IMap<ReturnAndAatfToSentOnSummaryListViewModelMapTransfer, SentOnSiteSummaryListViewModel>>();
+
+            controller = new SentOnSiteSummaryListController(() => apiClient, breadcrumb, cache, mapper);
+        }
+
+        [Fact]
+        public void CheckSentOnSiteSummaryListControllerInheritsFromExternalSiteController()
+        {
+            typeof(SentOnSiteSummaryListController).BaseType.Name.Should().Be(typeof(ExternalSiteController).Name);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenValidViewModel_BreadcrumbShouldBeSet()
+        {
+            var organisationId = Guid.NewGuid();
+            var @return = A.Fake<ReturnData>();
+            var operatorData = A.Fake<OperatorData>();
+            const string orgName = "orgName";
+
+            A.CallTo(() => apiClient.SendAsync(A<string>._, A<GetReturn>._)).Returns(@return);
+            A.CallTo(() => operatorData.OrganisationId).Returns(organisationId);
+            A.CallTo(() => @return.ReturnOperatorData).Returns(operatorData);
+            A.CallTo(() => cache.FetchOrganisationName(organisationId)).Returns(orgName);
+
+            await controller.Index(organisationId, @return.Id, Guid.NewGuid());
+
+            breadcrumb.ExternalActivity.Should().Be(BreadCrumbConstant.AatfReturn);
+            breadcrumb.ExternalOrganisation.Should().Be(orgName);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenAction_DefaultViewShouldBeReturned()
+        {
+            var result = await controller.Index(A.Dummy<Guid>(), A.Dummy<Guid>(), A.Dummy<Guid>()) as ViewResult;
+
+            result.ViewName.Should().BeEmpty();
+        }
     }
 }
