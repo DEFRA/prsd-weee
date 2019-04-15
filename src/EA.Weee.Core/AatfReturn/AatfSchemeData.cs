fb7466aa26a9ff525368659acb5b7abffd310366
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-04-15 08:19:12 +0100

    added approvalName to SchemeData

--- src/EA.Weee.Core/AatfReturn/AatfSchemeData.cs
+++ src/EA.Weee.Core/AatfReturn/AatfSchemeData.cs
@@ -1,16 +1,24 @@
 ﻿namespace EA.Weee.Core.AatfReturn
 {
+    using EA.Prsd.Core;
     public class AatfSchemeData
     {
         public Scheme Scheme { get; private set; }
 
         public ObligatedCategoryValue Received { get; private set; }
 
-        public AatfSchemeData(Scheme scheme, ObligatedCategoryValue received)
+        public string ApprovalName { get; set; }
+
+        public AatfSchemeData(Scheme scheme, ObligatedCategoryValue received, string approvalName)
         {
             // GUARD AGAINST Scheme and ObligatedCategoryValue BEING NULL AND CREATE UNIT TEST TO TEST THE GUARD
+            Guard.ArgumentNotNull(() => scheme, scheme);
+            Guard.ArgumentNotNull(() => received, received);            
+
             Scheme = scheme;
             Received = received;
+
+            ApprovalName = approvalName;
         }
     }
 }
