2b032a986e2675db70b4f2d64313f041d6023d74
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-04-15 08:59:54 +0100

    checks null argument for AatfSchemeData and tests properties are set

---
+++ src/EA.Weee.Core.Tests.Unit/AatfReturn/AatfSchemeDataTests.cs
@@ -0,0 +1,35 @@
+﻿namespace EA.Weee.Core.Tests.Unit.AatfReturn
+{
+    using EA.Weee.Core.AatfReturn;
+    using FakeItEasy;
+    using FluentAssertions;
+    using System;
+    using Xunit;
+
+    public class AatfSchemeDataTests
+    {
+        [Fact]
+        public void Construct_GivenNullSchemeAndObligatedReceivedData_ArgumentNullExceptionExpected()
+        {
+            Action act = () =>
+            {
+                var aatfSchemeData = new AatfSchemeData(null, null, "ABC123");
+            };
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GivenSource_AatfSchemeDataPropertiesShouldBeSet()
+        {
+            var testScheme = new Scheme(Guid.NewGuid(), "Test Scheme");
+            var obligatedReceivedValues = new ObligatedCategoryValue("25.00", "50.00");
+
+            var aatfSchemeData = new AatfSchemeData(testScheme, obligatedReceivedValues, "ABC123");
+
+            aatfSchemeData.Scheme.Should().Be(testScheme);
+            aatfSchemeData.ApprovalName.Should().Be("ABC123");
+            aatfSchemeData.Received.B2C.Should().Be(obligatedReceivedValues.B2C);
+            aatfSchemeData.Received.B2B.Should().Be(obligatedReceivedValues.B2B);
+        }
+    }
+}
