75090fec4fc1557a57df95777519969fc7e659d7
JohnPadgettCivica <JohnPadgettCivica>
2019-04-16 15:36:26 +0100

    Create new GDS link that has inner span

--- src/EA.Weee.Web/RazorHelpers/WeeeGds.Routelink.cs
+++ src/EA.Weee.Web/RazorHelpers/WeeeGds.Routelink.cs
@@ -1,54 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace EA.Weee.Web.RazorHelpers
+﻿namespace EA.Weee.Web.RazorHelpers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using System.Reflection;
+    using System.Web;
     using System.Web.Mvc;
     using System.Web.Routing;
+    using System.Web.WebPages;
 
-    public static class WeeeGds
+    public static class ExampleRouteLinkExtensions
     {
-        public static MvcHtmlString NavigationRouteLink(this HtmlHelper<dynamic> htmlHelper, string linkText, string routeName, RouteValueDictionary routeValues = null, IDictionary<string, object> htmlAttributes = null)
+        public static MvcHtmlString NavigationRouteLink(this HtmlHelper helper, string linkText, string descriptiveText, string routeName,
+            object routeValues, object htmlAttributes = null)
         {
             var dict = htmlAttributes ?? new Dictionary<string, object>();
 
-            if (htmlHelper.ViewContext.RequestContext.HttpContext.Request.Url.AbsolutePath == new UrlHelper(htmlHelper.ViewContext.RequestContext).RouteUrl(routeName))
-                dict.Add("class", "current");
+            var url = UrlHelper.GenerateUrl(routeName, null, null, null, null, null, TypeHelper.ObjectToDictionary(routeValues), RouteTable.Routes,
+                helper.ViewContext.RequestContext, false);
 
-            //var generateLInk = GenerateLinkInternal2(htmlHelper.ViewContext.RequestContext, RouteTable.Routes, linkText, routeName, 
+            var linkTagBuilder = new TagBuilder("a");
 
-                string ur2l = UrlHelper.GenerateUrl(routeName, null, null, null, null, null, routeValues, RouteTable.Routes, htmlHelper.ViewContext.RequestContext, false);
-      TagBuilder tagBuilder = new TagBuilder("a");
-      tagBuilder.InnerHtml = !string.IsNullOrEmpty(linkText) ? HttpUtility.HtmlEncode(linkText) : string.Empty;
-      tagBuilder.MergeAttributes<string, object>(htmlAttributes);
-      tagBuilder.MergeAttribute("href", ur2l);
-      //return tagBuilder.ToString(TagRenderMode.Normal);
-
-            return new MvcHtmlString(HtmlHelper.GenerateRouteLink(htmlHelper.ViewContext.RequestContext, RouteTable.Routes, linkText, routeName, routeValues, dict));
-        }
-
-        private static string GenerateLinkInternal2(
-            RequestContext requestContext,
-            RouteCollection routeCollection,
-            string linkText,
-            string routeName,
-            string actionName,
-            string controllerName,
-            string protocol,
-            string hostName,
-            string fragment,
-            RouteValueDictionary routeValues,
-            IDictionary<string, object> htmlAttributes,
-            bool includeImplicitMvcValues)
-        {
-            string url = UrlHelper.GenerateUrl(routeName, actionName, controllerName, protocol, hostName, fragment, routeValues, routeCollection, requestContext, includeImplicitMvcValues);
-            TagBuilder tagBuilder = new TagBuilder("a");
-            tagBuilder.InnerHtml = !string.IsNullOrEmpty(linkText) ? HttpUtility.HtmlEncode(linkText) : string.Empty;
-            tagBuilder.MergeAttributes<string, object>(htmlAttributes);
-            tagBuilder.MergeAttribute("href", url);
-            return tagBuilder.ToString(TagRenderMode.Normal);
+            var spanTagBuilder = new TagBuilder("span");
+            spanTagBuilder.AddCssClass("govuk-visually-hidden");
+            spanTagBuilder.InnerHtml = !string.IsNullOrEmpty(descriptiveText) ? HttpUtility.HtmlEncode(descriptiveText) : string.Empty;
+            var encodedLinkText = !string.IsNullOrEmpty(linkText) ? HttpUtility.HtmlEncode(linkText) : string.Empty;
+            linkTagBuilder.InnerHtml = $"{spanTagBuilder.ToString()}{encodedLinkText}";
+            linkTagBuilder.AddCssClass("govuk-link");
+            linkTagBuilder.AddCssClass("govuk-link--no-visited-state");
+            linkTagBuilder.MergeAttributes<string, object>((IDictionary<string, object>)HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            linkTagBuilder.MergeAttribute("href", url);
+            return new MvcHtmlString(linkTagBuilder.ToString(TagRenderMode.Normal));
         }
     }
 }
