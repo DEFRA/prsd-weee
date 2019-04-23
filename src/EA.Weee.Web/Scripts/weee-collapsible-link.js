15d323259f67b9e5773ed29be65289c9e597f17c
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-04-23 13:22:29 +0100

    bug fixes changes so far

--- src/EA.Weee.Web/Scripts/weee-collapsible-link.js
+++ src/EA.Weee.Web/Scripts/weee-collapsible-link.js
@@ -1,18 +1,18 @@
 ﻿function CollapsibleLinkHref() {
-        if (document.getElementById('collapsibleHref').innerText == "Open All") {
+        if (document.getElementById('collapsibleHref').innerText == "Open all") {
             var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
             for (i = 0; i < schemaData.length; i++) {
                 document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = "inherit";
                 document.getElementsByClassName('govuk-details')[i].open = "open";
-                document.getElementById('collapsibleHref').innerText = "Close All";
+                document.getElementById('collapsibleHref').innerText = "Close all";
             }
         }
-        else if (document.getElementById('collapsibleHref').innerText == "Close All") {
+        else if (document.getElementById('collapsibleHref').innerText == "Close all") {
             var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
             for (i = 0; i < schemaData.length; i++) {
                 document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = 'none';
                 document.getElementsByClassName('govuk-details')[i].open = false;
-                document.getElementById('collapsibleHref').innerText = "Open All";
+                document.getElementById('collapsibleHref').innerText = "Open all";
             }
         }
 }
