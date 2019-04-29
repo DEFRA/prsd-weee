c572928612a8a9f0fd30f02cba535b42ecf281df
jaswinder2303 <19706898+jaswinder2303@users.noreply.github.com>
2019-04-29 10:39:13 +0100

    Feature/sprint6demoissues (#185)
    * Task 69687:Update content on PCS selection screen: minor content change
    
    * few tweaks to the grid
    
    * collapse link changes
    
    * reverted changes relating to chk your return as these should be in another branch!
    
    * added padding

--- src/EA.Weee.Web/Scripts/weee-collapsible-link.js
+++ src/EA.Weee.Web/Scripts/weee-collapsible-link.js
@@ -1,36 +1,36 @@
 ﻿function CollapsibleLinkHref() {
-        if (document.getElementById('collapsibleHref').innerText == "Open all") {
-            var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
-            for (i = 0; i < schemaData.length; i++) {
-                document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = "inherit";
-                var divopen = document.getElementsByClassName('govuk-details')[i].open;
-                if (divopen) {
-                    document.getElementsByClassName('govuk-details')[i].open = false
-                }
-                else if (!divopen) {
-                    document.getElementsByClassName('govuk-details')[i].open = true;
-                }
+    if (document.getElementById('collapsibleHref').innerText == "Open all") {
+        var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
+        for (i = 0; i < schemaData.length; i++) {
+            document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = "inherit";
+            var divopen = document.getElementsByClassName('govuk-details')[i].open;
+            if (divopen) {
+                document.getElementsByClassName('govuk-details')[i].open = false
             }
-            document.getElementById('collapsibleHref').innerText = "Close all";
-        }
-        else if (document.getElementById('collapsibleHref').innerText == "Close all") {
-            var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
-            for (i = 0; i < schemaData.length; i++) {
-                var cdisplay = document.getElementsByClassName('collapsible-govuk-grid')[i].style.display;
-                document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = 'none';
-                var copen = document.getElementsByClassName('govuk-details')[i].open;
-                document.getElementsByClassName('govuk-details')[i].open = false;             
+            else if (!divopen) {
+                document.getElementsByClassName('govuk-details')[i].open = true;
             }
-            document.getElementById('collapsibleHref').innerText = "Open all";
         }
+        document.getElementById('collapsibleHref').innerText = "Close all";
+    }
+    else if (document.getElementById('collapsibleHref').innerText == "Close all") {
+        var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
+        for (i = 0; i < schemaData.length; i++) {
+            var cdisplay = document.getElementsByClassName('collapsible-govuk-grid')[i].style.display;
+            document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = 'none';
+            var copen = document.getElementsByClassName('govuk-details')[i].open;
+            document.getElementsByClassName('govuk-details')[i].open = false;
+        }
+        document.getElementById('collapsibleHref').innerText = "Open all";
+    }
 }
 
 function CollapsibleLinkHefSummary() {
-        var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
-        for (i = 0; i < schemaData.length; i++) {
-            document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = "inherit";   
-        }
-        document.getElementById('collapsibleHref').innerText = "Close all";
+    var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
+    for (i = 0; i < schemaData.length; i++) {
+        document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = "inherit";
+    }
+    document.getElementById('collapsibleHref').innerText = "Close all";
 }
 
 function InitialStartup() {
