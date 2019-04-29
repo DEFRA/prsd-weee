642e29764d8159ce9cfb36ae1943540da7b075b3
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-04-29 09:14:13 +0100

    reverted changes relating to chk your return as these should be in another branch!

--- src/EA.Weee.Web/Scripts/weee-collapsible-link.js
+++ src/EA.Weee.Web/Scripts/weee-collapsible-link.js
@@ -1,56 +1,34 @@
 ﻿function CollapsibleLinkHref() {
-        if (document.getElementById('collapsibleHref').innerText == "Open all") {
-            var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
-            for (i = 0; i < schemaData.length; i++) {
-                document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = "inherit";
-                var divopen = document.getElementsByClassName('govuk-details')[i].open;
-                if (divopen) {
-                    document.getElementsByClassName('govuk-details')[i].open = false
-                }
-                else if (!divopen) {
-                    document.getElementsByClassName('govuk-details')[i].open = true;
-                }
+    if (document.getElementById('collapsibleHref').innerText == "Open all") {
+        var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
+        for (i = 0; i < schemaData.length; i++) {
+            document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = "inherit";
+            var divopen = document.getElementsByClassName('govuk-details')[i].open;
+            if (divopen) {
+                document.getElementsByClassName('govuk-details')[i].open = false
             }
-            document.getElementById('collapsibleHref').innerText = "Close all";
-        }
-        else if (document.getElementById('collapsibleHref').innerText == "Close all") {
-            var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
-            for (i = 0; i < schemaData.length; i++) {
-                document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = 'none';
-                document.getElementsByClassName('govuk-details')[i].open = false;             
+            else if (!divopen) {
+                document.getElementsByClassName('govuk-details')[i].open = true;
             }
-            document.getElementById('collapsibleHref').innerText = "Open all";
         }
-}
-
-function CheckLinksAreClosed() {
-
-    var linkclosed = false;
-    var collapsibleElements = document.getElementsByClassName('govuk-details');
-
-    for (i = 0; i < collapsibleElements.length; i++) {
-        var divopen = document.getElementsByClassName('govuk-details')[i].open;
-
-        if (!divopen) {
-            linkclosed = true;
-        }
-        else if (divopen) {
-            linkclosed = false;
-        }
+        document.getElementById('collapsibleHref').innerText = "Close all";
     }
-    return linkclosed;
+    else if (document.getElementById('collapsibleHref').innerText == "Close all") {
+        var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
+        for (i = 0; i < schemaData.length; i++) {
+            var cdisplay = document.getElementsByClassName('collapsible-govuk-grid')[i].style.display;
+            document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = 'none';
+            var copen = document.getElementsByClassName('govuk-details')[i].open;
+            document.getElementsByClassName('govuk-details')[i].open = false;
+        }
+        document.getElementById('collapsibleHref').innerText = "Open all";
+    }
 }
 
 function CollapsibleLinkHefSummary() {
-    var linksClosed = CheckLinksAreClosed();
-    if (linksClosed) {
-        alert("linksClosed " + linksClosed);
-        document.getElementById('collapsibleHref').innerText = "Open all";
-    }
-
     var schemaData = document.getElementsByClassName('collapsible-govuk-grid');
     for (i = 0; i < schemaData.length; i++) {
-        document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = "inherit";   
+        document.getElementsByClassName('collapsible-govuk-grid')[i].style.display = "inherit";
     }
     document.getElementById('collapsibleHref').innerText = "Close all";
 }
