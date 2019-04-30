b9d1eb76780499766b703f090dbbbdbac9ef2d74
Jan Biddle <Jan.Biddle@ipl.com>
2019-04-30 16:30:47 +0100

    COP 2019-04-30

--- src/EA.Weee.Web/Scripts/weee-checkbox-toggle-visibility.js
+++ src/EA.Weee.Web/Scripts/weee-checkbox-toggle-visibility.js
@@ -3,10 +3,15 @@
         $(section).css('display', 'none');
         $(toggle).change(function () {
             if (this.checked) {
-                console.log("Toggled - On")
                 $(section).css('display', 'block');
             } else {
                 $(section).css('display', 'none');
+                $('#error_explanation').css('display', 'none');
+                $('.govuk-form-group').removeClass('govuk-form-group--error').removeClass('error');
+                $('.govuk-radios__input').removeClass('input-validation-error');
+                if ($('.govuk-radios__input').is('checked')) {
+                    $('.govuk - radios__input').prop("checked", false);
+                }
             }
         });
     });
