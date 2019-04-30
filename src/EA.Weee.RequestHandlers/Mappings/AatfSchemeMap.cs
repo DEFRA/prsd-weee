660bcbbbd66680db80e05f685d5c6a8755294325
Jaswinder Collingridge <JaswinderC@sfwltd.co.uk>
2019-04-30 13:28:35 +0100

    added mapper

---
+++ src/EA.Weee.RequestHandlers/Mappings/AatfSchemeMap.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.RequestHandlers.Mappings
+{
+    using Core.AatfReturn;
+    using Domain.AatfReturn;
+    using Prsd.Core;
+    using Prsd.Core.Mapper;
+    public class AatfSchemeMap : IMap<Aatf, AatfDataList>
+    {
+        public AatfDataList Map(Aatf source)
+        {
+            Guard.ArgumentNotNull(() => source, source);
+
+            return new AatfDataList(source.Id, source.Name, source.CompetentAuthority, source.ApprovalNumber, source.AatfStatus, source.Operator);
+        }
+    }
+}
