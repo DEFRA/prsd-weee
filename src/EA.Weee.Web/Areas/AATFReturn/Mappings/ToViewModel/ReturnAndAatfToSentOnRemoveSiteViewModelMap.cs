8150da7ef3f094e43281a9d9651b3b9fec25e825
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-05-02 17:35:53 +0100

    Start of unit tests and added mapper for view model

---
+++ src/EA.Weee.Web/Areas/AATFReturn/Mappings/ToViewModel/ReturnAndAatfToSentOnRemoveSiteViewModelMap.cs
@@ -0,0 +1,39 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel
+{
+    using EA.Prsd.Core;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Services.Caching;
+
+    public class ReturnAndAatfToSentOnRemoveSiteViewModelMap : IMap<ReturnAndAatfToSentOnRemoveSiteViewModelMapTransfer, SentOnRemoveSiteViewModel>
+    {
+        private readonly IWeeeCache cache;
+        private readonly ITonnageUtilities tonnageUtilities;
+
+        public ReturnAndAatfToSentOnRemoveSiteViewModelMap(
+            IWeeeCache cache,
+            ITonnageUtilities tonnageUtilities)
+        {
+            this.cache = cache;
+            this.tonnageUtilities = tonnageUtilities;
+        }
+
+        public SentOnRemoveSiteViewModel Map(ReturnAndAatfToSentOnRemoveSiteViewModelMapTransfer source)
+        {
+            Guard.ArgumentNotNull(() => source, source);
+
+            var viewModel = new SentOnRemoveSiteViewModel()
+            {
+                WeeeSentOn = source.WeeeSentOn,
+                OrganisationId = source.OrganisationId,
+                ReturnId = source.ReturnId,
+                AatfId = source.AatfId,
+                SiteAddress = source.SiteAddress,
+                OperatorAddress = source.OperatorAddress,
+                Tonnages = tonnageUtilities.SumObligatedValues(source.WeeeSentOn.Tonnages)
+            };
+
+            return viewModel;
+        }
+    }
+}
