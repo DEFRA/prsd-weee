5bcb91ae7888bfb80abba5b5d84924459fb7077f
JohnPadgettCivica <JohnPadgettCivica>
2019-05-02 16:57:06 +0100

    Test and domain updates

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/AatfReturn/ReturnHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.AatfReturn
+{
+    using System;
+    using Domain.AatfReturn;
+    using Domain.DataReturns;
+    using Domain.Organisation;
+    using Domain.Scheme;
+    using FakeItEasy;
+
+    public static class ReturnHelper
+    {
+        public static Return GetReturn()
+        {
+            var organisation = A.Fake<Organisation>();
+            var scheme = A.Fake<Scheme>();
+            var @operator = new Operator(organisation);
+
+            return new Return(@operator, new Quarter(2019, QuarterType.Q1), ReturnStatus.Created, Guid.NewGuid().ToString());
+        }
+    }
+}
