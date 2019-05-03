e2f5aca91752294242fa374dfa7c303b089c06a7
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-05-03 13:22:38 +0100

    Additional unit tests, replaced handler with a more specific weee sent on handler

--- src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Controller/SentOnRemoveSiteControllerTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Controller/SentOnRemoveSiteControllerTests.cs
@@ -3,9 +3,11 @@
     using EA.Prsd.Core.Mapper;
     using EA.Weee.Api.Client;
     using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Core.Scheme;
     using EA.Weee.Web.Areas.AatfReturn.Controllers;
     using EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel;
     using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Constant;
     using EA.Weee.Web.Controllers.Base;
     using EA.Weee.Web.Services;
     using EA.Weee.Web.Services.Caching;
@@ -33,13 +35,30 @@
             this.cache = A.Fake<IWeeeCache>();
             this.mapper = A.Fake<IMap<ReturnAndAatfToSentOnRemoveSiteViewModelMapTransfer, SentOnRemoveSiteViewModel>>();
 
-               controller = new SentOnRemoveSiteController(apiClient, breadcrumb, cache, mapper);
+            controller = new SentOnRemoveSiteController(apiClient, breadcrumb, cache, mapper);
         }
 
         [Fact]
         public void CheckSentOnCreateSiteOperatorControllerInheritsExternalSiteController()
         {
             typeof(SentOnRemoveSiteController).BaseType.Name.Should().Be(typeof(ExternalSiteController).Name);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenValidViewModel_BreadcrumbShouldBeSet()
+        {
+            var organisationId = Guid.NewGuid();
+            var schemeInfo = A.Fake<SchemePublicInfo>();
+            const string orgName = "orgName";
+
+            A.CallTo(() => cache.FetchOrganisationName(organisationId)).Returns(orgName);
+            A.CallTo(() => cache.FetchSchemePublicInfo(organisationId)).Returns(schemeInfo);
+
+            await controller.Index(organisationId, A.Dummy<Guid>(), A.Dummy<Guid>(), A.Dummy<Guid>());
+
+            breadcrumb.ExternalActivity.Should().Be(BreadCrumbConstant.AatfReturn);
+            breadcrumb.ExternalOrganisation.Should().Be(orgName);
+            breadcrumb.SchemeInfo.Should().Be(schemeInfo);
         }
     }
 }
