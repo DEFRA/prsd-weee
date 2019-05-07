36f41f67789775e513b15cebe9669480ecbac03d
Will Andrews <will7989@hotmail.com>
2019-05-07 09:43:04 +0100

    Added view with layout but no data

---
+++ src/EA.Weee.Web/Areas/Admin/Controllers/AatfController.cs
@@ -0,0 +1,48 @@
+﻿namespace EA.Weee.Web.Areas.Admin.Controllers
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Web.Areas.Admin.Controllers.Base;
+    using EA.Weee.Web.Areas.Admin.ViewModels.Aatf;
+    using EA.Weee.Web.Areas.Admin.ViewModels.Home;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    public class AatfController : AdminController
+    {
+        private readonly Func<IWeeeClient> apiClient;
+        private readonly IWeeeCache cache;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IMapper mapper;
+
+        public AatfController(Func<IWeeeClient> apiClient, IWeeeCache cache, BreadcrumbService breadcrumb, IMapper mapper)
+        {
+            this.apiClient = apiClient;
+            this.cache = cache;
+            this.breadcrumb = breadcrumb;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(Guid id)
+        {
+            SetBreadcrumb();
+
+            AatfDetailsViewModel viewModel = new AatfDetailsViewModel()
+            {
+                AatfId = id,
+                AatfName = ""
+            };
+
+            return View(viewModel);
+        }
+
+        private void SetBreadcrumb()
+        {
+            breadcrumb.InternalActivity = InternalUserActivity.ManageAatfs;
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Aatf/AatfDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Aatf
+{
+    using System;
+    public class AatfDetailsViewModel
+    {
+        public Guid AatfId { get; set; }
+
+        public string AatfName { get; set; }
+    }
+}
