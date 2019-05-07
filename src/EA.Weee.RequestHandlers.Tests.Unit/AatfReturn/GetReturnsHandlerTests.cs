5bb6a5b548f0efd534867205af5ea050d927f68e
JohnPadgettCivica <JohnPadgettCivica>
2019-05-07 14:03:12 +0100

    Get returns change

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/AatfReturn/GetReturnsHandlerTests.cs
@@ -0,0 +1,88 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.AatfReturn
+{
+    using System;
+    using System.Linq;
+    using System.Security;
+    using System.Threading.Tasks;
+    using Core.AatfReturn;
+    using Domain.AatfReturn;
+    using FakeItEasy;
+    using FluentAssertions;
+    using RequestHandlers.AatfReturn;
+    using Requests.AatfReturn;
+    using Weee.Tests.Core;
+    using Xunit;
+
+    public class GetReturnsHandlerTests
+    {
+        private GetReturnsHandler handler;
+        private readonly IGetPopulatedReturn populatedReturn;
+        private readonly IReturnDataAccess returnDataAccess;
+
+        public GetReturnsHandlerTests()
+        {
+            populatedReturn = A.Fake<IGetPopulatedReturn>();
+            returnDataAccess = A.Fake<IReturnDataAccess>();
+
+            handler = new GetReturnsHandler(new AuthorizationBuilder()
+                .AllowExternalAreaAccess()
+                .AllowOrganisationAccess().Build(),
+                populatedReturn,
+                returnDataAccess);
+        }
+
+        [Fact]
+        public async Task HandleAsync_NoExternalAccess_ThrowsSecurityException()
+        {
+            var authorization = new AuthorizationBuilder().DenyExternalAreaAccess().Build();
+
+            handler = new GetReturnsHandler(authorization,
+                A.Dummy<IGetPopulatedReturn>(),
+                A.Dummy<IReturnDataAccess>());
+
+            Func<Task> action = async () => await handler.HandleAsync(A.Dummy<GetReturns>());
+
+            await action.Should().ThrowAsync<SecurityException>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenOrganisation_ReturnsForOrganisationShouldBeRetrieved()
+        {
+            var organisationId = Guid.NewGuid();
+
+            var result = await handler.HandleAsync(new GetReturns(organisationId));
+
+            A.CallTo(() => returnDataAccess.GetByOrganisationId(organisationId)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenOrganisation_GetPopulatedReturnsShouldBeCalled()
+        {
+            var organisationId = Guid.NewGuid();
+            //var returns = new List<Return> { A.Fake<Return>(), A.Fake<Return>() };
+            var returns = A.CollectionOfFake<Return>(2);
+
+            A.CallTo(() => returnDataAccess.GetByOrganisationId(organisationId)).Returns(returns);
+
+            var result = await handler.HandleAsync(new GetReturns(organisationId));
+
+            A.CallTo((() => populatedReturn.GetReturnData(returns.ElementAt(0).Id))).MustHaveHappened(Repeated.Exactly.Once);
+            A.CallTo((() => populatedReturn.GetReturnData(returns.ElementAt(1).Id))).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenOrganisation_PopulatedReturnsShouldBeReturned()
+        {
+            var returnData = A.CollectionOfFake<ReturnData>(2);
+
+            A.CallTo((() => populatedReturn.GetReturnData(A<Guid>._))).Returns(returnData.ElementAt(0));
+            A.CallTo((() => populatedReturn.GetReturnData(A<Guid>._))).Returns(returnData.ElementAt(1));
+
+            var result = await handler.HandleAsync(A.Dummy<GetReturns>());
+
+            result.Should().Contain(returnData.ElementAt(0));
+            result.Should().Contain(returnData.ElementAt(1));
+            result.Should().HaveCount(2);
+        }
+    }
+}
---
+++ src/EA.Weee.RequestHandlers/AatfReturn/GetPopulatedReturn.cs
@@ -0,0 +1,83 @@
+﻿namespace EA.Weee.RequestHandlers.AatfReturn
+{
+    using System;
+    using System.Threading.Tasks;
+    using AatfTaskList;
+    using CheckYourReturn;
+    using Core.AatfReturn;
+    using DataAccess.DataAccess;
+    using Domain.AatfReturn;
+    using Factories;
+    using ObligatedSentOn;
+    using Prsd.Core.Mapper;
+    using Security;
+
+    public class GetPopulatedReturn : IGetPopulatedReturn
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly IReturnDataAccess returnDataAccess;
+        private readonly IMap<ReturnQuarterWindow, ReturnData> mapper;
+        private readonly IQuarterWindowFactory quarterWindowFactory;
+        private readonly IFetchNonObligatedWeeeForReturnDataAccess nonObligatedDataAccess;
+        private readonly IFetchObligatedWeeeForReturnDataAccess obligatedDataAccess;
+        private readonly ISentOnAatfSiteDataAccess getSentOnAatfSiteDataAccess;
+        private readonly IFetchAatfByOrganisationIdDataAccess aatfDataAccess;
+        private readonly IReturnSchemeDataAccess returnSchemeDataAccess;
+
+        public GetPopulatedReturn(IWeeeAuthorization authorization, 
+            IReturnDataAccess returnDataAccess,  
+            IMap<ReturnQuarterWindow, ReturnData> mapper, 
+            IQuarterWindowFactory quarterWindowFactory, 
+            IFetchNonObligatedWeeeForReturnDataAccess nonObligatedDataAccess, 
+            IFetchObligatedWeeeForReturnDataAccess obligatedDataAccess, 
+            ISentOnAatfSiteDataAccess getSentOnAatfSiteDataAccess, 
+            IFetchAatfByOrganisationIdDataAccess aatfDataAccess, 
+            IReturnSchemeDataAccess returnSchemeDataAccess)
+        {
+            this.authorization = authorization;
+            this.returnDataAccess = returnDataAccess;
+            this.mapper = mapper;
+            this.quarterWindowFactory = quarterWindowFactory;
+            this.nonObligatedDataAccess = nonObligatedDataAccess;
+            this.obligatedDataAccess = obligatedDataAccess;
+            this.getSentOnAatfSiteDataAccess = getSentOnAatfSiteDataAccess;
+            this.aatfDataAccess = aatfDataAccess;
+            this.returnSchemeDataAccess = returnSchemeDataAccess;
+        }
+
+        public async Task<ReturnData> GetReturnData(Guid returnId)
+        {
+            authorization.EnsureCanAccessExternalArea();
+
+            var @return = await returnDataAccess.GetById(returnId);
+
+            authorization.EnsureOrganisationAccess(@return.Operator.Organisation.Id);
+
+            var quarterWindow = await quarterWindowFactory.GetAnnualQuarter(@return.Quarter);
+
+            var returnNonObligatedValues = await nonObligatedDataAccess.FetchNonObligatedWeeeForReturn(returnId);
+
+            var returnObligatedReceivedValues = await obligatedDataAccess.FetchObligatedWeeeReceivedForReturn(returnId);
+
+            var returnObligatedReusedValues = await obligatedDataAccess.FetchObligatedWeeeReusedForReturn(returnId);
+
+            var aatfList = await aatfDataAccess.FetchAatfByOrganisationId(@return.Operator.Organisation.Id);
+
+            var sentOn = await obligatedDataAccess.FetchObligatedWeeeSentOnForReturnByReturn(returnId);
+
+            var returnSchemeList = await returnSchemeDataAccess.GetSelectedSchemesByReturnId(returnId);
+
+            var returnQuarterWindow = new ReturnQuarterWindow(@return,
+                quarterWindow,
+                aatfList,
+                returnNonObligatedValues,
+                returnObligatedReceivedValues,
+                returnObligatedReusedValues,
+                @return.Operator,
+                sentOn,
+                returnSchemeList);
+
+            return mapper.Map(returnQuarterWindow);
+        }
+    }
+}
