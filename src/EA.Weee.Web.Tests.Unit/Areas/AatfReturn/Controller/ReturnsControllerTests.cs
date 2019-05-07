1e85ecabbabe0d9aa2ffb4430b3c51da0a8b128b
JohnPadgettCivica <JohnPadgettCivica>
2019-05-07 15:34:33 +0100

    Viewmodel mapping

--- src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Controller/ReturnsControllerTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/Controller/ReturnsControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace EA.Weee.Web.Tests.Unit.Areas.AatfReturn.Controller
 {
     using System;
+    using System.Collections.Generic;
     using System.Web.Mvc;
     using Api.Client;
     using Constant;
@@ -33,11 +34,60 @@
         }
 
         [Fact]
-        public void CheckReturnsControllerInheritsAatfReturnBaseController()
+        public void ReturnsControllerInheritsAatfReturnBaseController()
         {
             typeof(ReturnsController).BaseType.Name.Should().Be(typeof(AatfReturnBaseController).Name);
         }
 
+        [Fact]
+        public async void IndexGet_GivenOrganisation_DefaultViewShouldBeReturned()
+        {
+            var result = await controller.Index(A.Dummy<Guid>()) as ViewResult;
 
+            result.ViewName.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async void IndexGet_GivenOrganisation_BreadcrumbShouldBeSet()
+        {
+            await controller.Index(A.Dummy<Guid>());
+
+            Assert.Equal(breadcrumb.ExternalActivity, BreadCrumbConstant.AatfReturn);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenOrganisation_ReturnsShouldBeRetrieved()
+        {
+            var organisationId = Guid.NewGuid();
+
+            await controller.Index(organisationId);
+
+            A.CallTo(() => weeeClient.SendAsync(A<string>._, A<GetReturns>.That.Matches(r => r.OrganisationId.Equals(organisationId))))
+                .MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenOrganisation_ReturnsViewModelShouldBeBuilt()
+        {
+            var returns = new List<ReturnData>();
+
+            A.CallTo(() => weeeClient.SendAsync(A<string>._, A<GetReturns>._)).Returns(returns);
+
+            await controller.Index(A.Dummy<Guid>());
+
+            A.CallTo(() => mapper.Map<ReturnsViewModel>(returns)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenOrganisation_ReturnsViewModelShouldBeReturned()
+        {
+            var model = new ReturnsViewModel();
+
+            A.CallTo(() => mapper.Map<ReturnsViewModel>(A<IList<ReturnData>>._)).Returns(model);
+
+            var result = await controller.Index(A.Dummy<Guid>()) as ViewResult;
+
+            result.Model.Should().Be(model);
+        }
     }
 }
---
+++ src/EA.Weee.Web/Areas/AatfReturn/ViewModels/ReturnsViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.ViewModels
+{
+    using System.Collections.Generic;
+
+    public class ReturnsViewModel
+    {
+        public string OrganisationName { get; set; }
+
+        public IList<ReturnViewModel> Returns { get; set; }
+
+        public ReturnsViewModel()
+        {
+            Returns = new List<ReturnViewModel>();
+        }
+    }
+}
