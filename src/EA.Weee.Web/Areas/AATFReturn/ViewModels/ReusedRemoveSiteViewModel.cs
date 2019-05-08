eb4d1d445733125c26326762bfae379485f617fe
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-05-08 13:13:29 +0100

    Added controller etc. Added request and handler

---
+++ src/EA.Weee.Web/Areas/AATFReturn/ViewModels/ReusedRemoveSiteViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.ViewModels
+{
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Web.ViewModels.Shared;
+    using System;
+    using System.Collections.Generic;
+
+    public class ReusedRemoveSiteViewModel : RadioButtonStringCollectionViewModel
+    {
+        public Guid SiteId { get; set; }
+
+        public Guid ReturnId { get; set; }
+
+        public Guid AatfId { get; set; }
+
+        public Guid OrganisationId { get; set; }
+
+        public AddressData Site { get; set; }
+
+        public string SiteAddress { get; set; }
+
+        public override string SelectedValue { get; set; }
+
+        public ReusedRemoveSiteViewModel() : base(new List<string> { "Yes", "No" })
+        {
+        }
+    }
+}
