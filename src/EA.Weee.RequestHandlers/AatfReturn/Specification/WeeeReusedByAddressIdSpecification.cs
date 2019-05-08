c4b83b5136416589a12415545deb0fd1e7e2702a
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-05-08 13:13:29 +0100

    Added controller etc. Added request and handler

---
+++ src/EA.Weee.RequestHandlers/AatfReturn/Specification/WeeeReusedByAddressIdSpecification.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.RequestHandlers.AatfReturn.Specification
+{
+    using EA.Weee.Domain.AatfReturn;
+    using System;
+    using System.Linq.Expressions;
+
+    public class WeeeReusedByAddressIdSpecification : Specification<WeeeReusedSite>
+    {
+        public Guid SiteId { get; private set; }
+
+        public WeeeReusedByAddressIdSpecification(Guid siteId)
+        {
+            SiteId = siteId;
+        }
+
+        public override Expression<Func<WeeeReusedSite, bool>> ToExpression()
+        {
+            return @weeeReusedSite => @weeeReusedSite.Address.Id == SiteId;
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/AATFReturn/Controllers/ReusedRemoveSiteController.cs
@@ -0,0 +1,111 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Requests.AatfReturn;
+    using EA.Weee.Requests.AatfReturn.Obligated;
+    using EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel;
+    using EA.Weee.Web.Areas.AatfReturn.Requests;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Controllers.Base;
+    using EA.Weee.Web.Infrastructure;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+
+    public class ReusedRemoveSiteController : ExternalSiteController
+    {
+        private readonly Func<IWeeeClient> apiClient;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+
+        public ReusedRemoveSiteController(Func<IWeeeClient> apiClient, BreadcrumbService breadcrumb, IWeeeCache cache)
+        {
+            this.apiClient = apiClient;
+            this.breadcrumb = breadcrumb;
+            this.cache = cache;
+        }
+
+        [HttpGet]
+        public virtual async Task<ActionResult> Index(Guid organisationId, Guid returnId, Guid aatfId, Guid siteId)
+        {
+            using (var client = apiClient())
+            {
+                var sites = await client.SendAsync(User.GetAccessToken(), new GetAatfSite(aatfId, returnId));
+                var site = sites.AddressData.Select(s => s).Where(s => s.Id == siteId).Single();
+                
+                var viewModel = new ReusedRemoveSiteViewModel()
+                {
+                    SiteAddress = GenerateAddress(site),
+                    Site = site,
+                    SiteId = site.Id,
+                    AatfId = aatfId,
+                    ReturnId = returnId,
+                    OrganisationId = organisationId
+                };
+
+                await SetBreadcrumb(organisationId, BreadCrumbConstant.AatfReturn);
+
+                return View(viewModel);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public virtual async Task<ActionResult> Index(ReusedRemoveSiteViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = apiClient())
+                {
+                    if (viewModel.SelectedValue == "Yes")
+                    {
+                        await client.SendAsync(User.GetAccessToken(), new RemoveAatfSite(viewModel.SiteId)
+                        {
+                            AatfId = viewModel.AatfId,
+                            ReturnId = viewModel.ReturnId
+                        });
+                    }
+
+                    return await Task.Run<ActionResult>(() =>
+                        RedirectToAction("Index", "ReusedOffSiteSummaryList", new { area = "AatfReturn", organisationId = viewModel.OrganisationId, returnId = viewModel.ReturnId, aatfId = viewModel.AatfId }));
+                }
+            }
+
+            return View(viewModel);
+        }
+
+        private async Task SetBreadcrumb(Guid organisationId, string activity)
+        {
+            breadcrumb.ExternalOrganisation = await cache.FetchOrganisationName(organisationId);
+            breadcrumb.ExternalActivity = activity;
+            breadcrumb.SchemeInfo = await cache.FetchSchemePublicInfo(organisationId);
+        }
+
+        public virtual string GenerateAddress(AddressData address)
+        {
+            var siteAddressLong = address.Name + "<br/>" + address.Address1;
+
+            if (address.Address2 != null)
+            {
+                siteAddressLong += "<br/>" + address.Address2;
+            }
+
+            siteAddressLong += "<br/>" + address.TownOrCity;
+
+            if (address.CountyOrRegion != null)
+            {
+                siteAddressLong += "<br/>" + address.CountyOrRegion;
+            }
+
+            siteAddressLong += "<br/>" + address.CountryName + "<br/>" + address.Postcode;
+
+            return siteAddressLong;
+        }
+    }
+}
