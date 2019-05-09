73b70f5b7a9b50187ca697b748ef3e1f2e9fd794
Will Andrews <will7989@hotmail.com>
2019-05-09 13:03:13 +0100

    Feature/view aatf details (#215)
    * Added view with layout but no data
    
    * Added  request handler to get aatf by if. With tests
    
    * Database schema changes
    
    * Added in new mappers and wired in the data to the html
    
    * added update script. Made approval date nullable in models
    
    * merged with develop
    
    * renamed test file and removed commented out code
    
    * updated csproj now included
    
    * added more tests
    
    * added extra test

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Aatf/AatfDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Aatf
+{
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Core.Shared;
+    using System;
+    public class AatfDetailsViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public string ApprovalNumber { get; set; }
+
+        public UKCompetentAuthorityData CompetentAuthority { get; set; }
+
+        public AatfStatus AatfStatus { get; set; }
+        public virtual AatfAddressData SiteAddress { get; set; }
+        public AatfSize Size { get; set; }
+        public DateTime? ApprovalDate { get; set; }
+    }
+}
