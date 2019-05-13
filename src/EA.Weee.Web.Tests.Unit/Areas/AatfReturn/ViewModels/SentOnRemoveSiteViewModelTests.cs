0c719fb2095723208cd0af40002262a3a3fb5ca2
Sam Goodey <samuelg@sfwltd.co.uk>
2019-05-13 13:24:07 +0100

    Feature/weee sent on remove site bug fixes

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfReturn/ViewModels/SentOnRemoveSiteViewModelTests.cs
@@ -0,0 +1,38 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.AatfReturn.ViewModels
+{
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+    using FluentAssertions;
+    using System;
+    using System.Web.Mvc;
+    using Xunit;
+
+    public class SentOnRemoveSiteViewModelTests
+    {
+        private readonly SentOnRemoveSiteViewModel model;
+
+        public SentOnRemoveSiteViewModelTests()
+        {
+            model = new SentOnRemoveSiteViewModel();
+        }
+
+        [Fact]
+        public void SentOnRemoveSiteViewModel_SiteAddressShouldHaveAllowHtmlAttribute()
+        {
+            var t = typeof(SentOnRemoveSiteViewModel);
+            var pi = t.GetProperty("SiteAddress");
+            var hasAttribute = Attribute.IsDefined(pi, typeof(AllowHtmlAttribute));
+
+            hasAttribute.Should().Be(true);
+        }
+
+        [Fact]
+        public void SentOnRemoveSiteViewModel_OperatorAddressShouldHaveAllowHtmlAttribute()
+        {
+            var t = typeof(SentOnRemoveSiteViewModel);
+            var pi = t.GetProperty("OperatorAddress");
+            var hasAttribute = Attribute.IsDefined(pi, typeof(AllowHtmlAttribute));
+
+            hasAttribute.Should().Be(true);
+        }
+    }
+}
