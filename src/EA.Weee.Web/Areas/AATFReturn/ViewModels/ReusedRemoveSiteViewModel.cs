d25608dc755cf8ed1d156b3101818b82b26aecff
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-05-13 09:35:17 +0100

    Bug fixes

--- src/EA.Weee.Web/Areas/AATFReturn/ViewModels/ReusedRemoveSiteViewModel.cs
+++ src/EA.Weee.Web/Areas/AATFReturn/ViewModels/ReusedRemoveSiteViewModel.cs
@@ -4,6 +4,7 @@
     using EA.Weee.Web.ViewModels.Shared;
     using System;
     using System.Collections.Generic;
+    using System.Web.Mvc;
 
     public class ReusedRemoveSiteViewModel : RadioButtonStringCollectionViewModel
     {
@@ -17,6 +18,7 @@
 
         public AddressData Site { get; set; }
 
+        [AllowHtml]
         public string SiteAddress { get; set; }
 
         public override string SelectedValue { get; set; }
