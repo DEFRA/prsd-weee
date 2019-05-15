713ca45ede7d57f714e171c13fdea32ec7a22822
Will Andrews <will7989@hotmail.com>
2019-05-15 11:13:20 +0100

    started on creating view model

--- src/EA.Weee.Core/AatfReturn/AatfStatus.cs
+++ src/EA.Weee.Core/AatfReturn/AatfStatus.cs
@@ -5,6 +5,8 @@
     public class AatfStatus : Enumeration
     {
         public static readonly AatfStatus Approved = new AatfStatus(2, "Approved");
+        public static readonly AatfStatus Suspended = new AatfStatus(3, "Suspended");
+        public static readonly AatfStatus Cancelled = new AatfStatus(4, "Cancelled");
         protected AatfStatus()
         {
         }
---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/AddAatfViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.AddAatf
+{
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Core.DataStandards;
+    using System.ComponentModel.DataAnnotations;
+    public class AddAatfViewModel
+    {
+        [Required]
+        [StringLength(EnvironmentAgencyMaxFieldLengths.SchemeName)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Name of Aatf")]
+        public string AatfName { get; set; }
+
+        public AatfAddressData SiteAddressData { get; set; }
+
+        [Required]
+        [RegularExpression(@"WEE/([A-Z]{2}[0-9]{4}[A-Z]{2})/SCH",
+            ErrorMessage = "Approval number is not in correct format.")]
+        [StringLength(EnvironmentAgencyMaxFieldLengths.SchemeApprovalNumber)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Approval number")]
+        public string ApprovalNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Status")]
+        public AatfStatus Status { get; set; }
+
+        [Required]
+        [Display(Name = "Size")]
+        public AatfSize Size { get; set; }
+    }
+}
