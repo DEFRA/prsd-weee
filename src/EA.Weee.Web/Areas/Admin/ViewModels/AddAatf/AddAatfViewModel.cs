3f1207a78f310667760639904df78cc3efdd579e
Will Andrews <will7989@hotmail.com>
2019-05-15 13:23:42 +0100

    completed view model

--- src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/AddAatfViewModel.cs
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/AddAatfViewModel.cs
@@ -14,7 +14,7 @@
         public AatfAddressData SiteAddressData { get; set; }
 
         [Required]
-        [RegularExpression(@"WEE/([A-Z]{2}[0-9]{4}[A-Z]{2})/SCH",
+        [RegularExpression(@"WEE/([A-Z]{2}[0-9]{4}[A-Z]{2})/ATF",
             ErrorMessage = "Approval number is not in correct format.")]
         [StringLength(EnvironmentAgencyMaxFieldLengths.SchemeApprovalNumber)]
         [DataType(DataType.Text)]
@@ -28,5 +28,7 @@
         [Required]
         [Display(Name = "Size")]
         public AatfSize Size { get; set; }
+
+        public AatfContactData ContactData { get; set; }
     }
 }
