5fe787d813007a2d34c8aac6d1cfc5537650ee52
Will Andrews <will7989@hotmail.com>
2019-05-22 09:18:35 +0100

    only show address lines that are optional if they have values (#250)
    * only show address lines that are optional if they have values
    
    * Added the creation of address to the view model so that it can be tested

--- src/EA.Weee.Web/Areas/AatfReturn/ViewModels/SentOnSiteSummaryListViewModel.cs
+++ src/EA.Weee.Web/Areas/AatfReturn/ViewModels/SentOnSiteSummaryListViewModel.cs
@@ -21,5 +21,31 @@
         public SentOnSiteSummaryListViewModel()
         {
         }
+
+        public string CreateLongAddress(AatfAddressData address)
+        {
+            string siteAddressLong = address.Name + "<br/>" + address.Address1;
+
+            if (address.Address2 != null)
+            {
+                siteAddressLong += "<br/>" + address.Address2;
+            }
+
+            siteAddressLong += "<br/>" + address.TownOrCity;
+
+            if (address.CountyOrRegion != null)
+            {
+                siteAddressLong += "<br/>" + address.CountyOrRegion;
+            }
+
+            if (address.Postcode != null)
+            {
+                siteAddressLong += "<br/>" + address.Postcode;
+            }
+
+            siteAddressLong += "<br/>" + address.CountryName;
+
+            return siteAddressLong;
+        }
     }
 }
