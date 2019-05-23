7cfb8b4b9d3afc77699cbd997d7943feb8fe3a86
Stephen Spinks <stephen.spinks@civica.co.uk>
2019-05-23 12:38:42 +0100

    Standardise full stop usage

--- src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/AddAatfViewModel.cs
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/AddAatfViewModel.cs
@@ -20,7 +20,7 @@
         public AatfAddressData SiteAddressData { get; set; }
 
         [Required]
-        [RegularExpression(@"WEE/([A-Z]{2}[0-9]{4}[A-Z]{2})/ATF", ErrorMessage = "Approval number is not in correct format.")]
+        [RegularExpression(@"WEE/([A-Z]{2}[0-9]{4}[A-Z]{2})/ATF", ErrorMessage = "Approval number is not in correct format")]
         [StringLength(EnvironmentAgencyMaxFieldLengths.SchemeApprovalNumber)]
         [DataType(DataType.Text)]
         [Display(Name = "Approval number")]
