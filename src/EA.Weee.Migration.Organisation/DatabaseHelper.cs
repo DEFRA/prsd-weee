57958a265853b546c384419366c9154f6555e351
Jan Biddle <46448988+JB12298@users.noreply.github.com>
2019-05-24 16:00:40 +0100

    Feature/migration tool (#268)
    * added base structure
    
    * Switch branch
    
    * swap
    
    * Switch
    
    * Reorganised files
    
    * Reformat
    
    * Wrapped sql in transaction
    
    * Added validator tests
    
    * removed extra test
    
    * Removed unreachable code
    
    * Deleted default aatf handler
    
    * Add clear down script

---
+++ src/EA.Weee.Migration.Organisation/DatabaseHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Weee.Migration.Organisation
+{
+    using System.Configuration;
+    using System.Data.SqlClient;
+
+    public class DatabaseHelper
+    {
+        public static SqlConnection CreateConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["Weee.DefaultConnection"].ConnectionString);
+        }
+    }
+}
---
+++ src/EA.Weee.Migration.Organisation/Model/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.Migration.Organisation.Model
+{
+    using System;
+
+    public class Country
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
