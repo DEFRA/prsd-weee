38a70c2754a2b864db792cd26ab52861fecf7f99
Jan Biddle <46448988+JB12298@users.noreply.github.com>
2019-05-24 16:00:40 +0100

    Feature/migration tool (#268)
    * added base structure
    
    * Switch branch
    
    * swap
    
    * Switch
    
    * Reorganised files
    
    * Reformat
    
    * Wrapped sql in transaction
    
    * Added validator tests
    
    * removed extra test
    
    * Removed unreachable code
    
    * Deleted default aatf handler
    
    * Add clear down script

---
+++ src/EA.Weee.Migration.Organisation/Validation/OrganisationDataListValidator.cs
@@ -0,0 +1,48 @@
+﻿namespace EA.Weee.Migration.Organisation.Validation
+{
+    using System.Collections.Generic;
+    using FluentValidation;
+    using Model;
+    using NLog;
+
+    public class OrganisationDataListValidator
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static readonly OrganisationDataValidator OrganisationValidator = new OrganisationDataValidator();
+        private static readonly AddressDataValidator AddressValidator = new AddressDataValidator();
+
+        public static bool CheckHasErrors(IList<Organisation> organisations)
+        {
+            var hasErrors = false;
+
+            foreach (var organisation in organisations)
+            {
+                var addressResult = AddressValidator.Validate(organisation.AddressData, ruleSet: "Address");
+                if (!addressResult.IsValid)
+                {
+                    Logger.Error("Address validation errors for organisation {0}", organisation.Name);
+                    foreach (var error in addressResult.Errors)
+                    {
+                        Logger.Error(error.ErrorMessage);
+                    }
+
+                    hasErrors = true;
+                }
+
+                var organisationResult = OrganisationValidator.Validate(organisation, ruleSet: "Organisation");
+                if (!organisationResult.IsValid)
+                {
+                    Logger.Error("Other organisation validation errors for organisation {0}", organisation.Name);
+                    foreach (var error in organisationResult.Errors)
+                    {
+                        Logger.Error(error.ErrorMessage);
+                    }
+
+                    hasErrors = true;
+                }
+            }
+
+            return hasErrors;
+        }
+    }
+}
