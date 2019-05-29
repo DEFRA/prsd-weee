dec283a051923d66ff853784b460ed7d72d384ea
Jan Biddle <46448988+JB12298@users.noreply.github.com>
2019-05-29 15:12:05 +0100

    Feature/aatf details bug (#281)
    * cop
    
    * Fixed bug with address being created rather than edited
    
    * Added tests
    Added method and call to reset aatf cache when details updated
    
    * fixed semicolon
    
    * Fixed async error
    
    * fixed test

--- src/EA.Weee.RequestHandlers.Tests.Unit/AatfReturn/Internal/EditAatfDetailsRequestHandlerTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/AatfReturn/Internal/EditAatfDetailsRequestHandlerTests.cs
@@ -7,9 +7,11 @@
     using EA.Prsd.Core.Mapper;
     using EA.Weee.Core.AatfReturn;
     using EA.Weee.Core.Shared;
+    using EA.Weee.Domain;
     using EA.Weee.Domain.AatfReturn;
     using EA.Weee.RequestHandlers.AatfReturn;
     using EA.Weee.RequestHandlers.AatfReturn.Internal;
+    using EA.Weee.RequestHandlers.Organisations;
     using EA.Weee.RequestHandlers.Security;
     using EA.Weee.Requests.AatfReturn.Internal;
     using EA.Weee.Security;
@@ -25,6 +27,7 @@
         private readonly IGenericDataAccess genericDataAccess;
         private readonly IAatfDataAccess aatfDataAccess;
         private readonly IMap<AatfAddressData, AatfAddress> addressMapper;
+        private readonly IOrganisationDetailsDataAccess organisationDetailsDataAccess;
         private readonly EditAatfDetailsRequestHandler handler;
 
         public EditAatfDetailsRequestHandlerTests()
@@ -34,8 +37,9 @@
             genericDataAccess = A.Fake<IGenericDataAccess>();
             aatfDataAccess = A.Fake<IAatfDataAccess>();
             addressMapper = A.Fake<IMap<AatfAddressData, AatfAddress>>();
+            organisationDetailsDataAccess = A.Fake<IOrganisationDetailsDataAccess>();
 
-            handler = new EditAatfDetailsRequestHandler(authorization, aatfDataAccess, genericDataAccess, addressMapper);
+            handler = new EditAatfDetailsRequestHandler(authorization, aatfDataAccess, genericDataAccess, addressMapper, organisationDetailsDataAccess);
         }
 
         [Fact]
@@ -43,7 +47,7 @@
         {
             var authorization = new AuthorizationBuilder().DenyInternalAreaAccess().Build();
 
-            var handler = new EditAatfDetailsRequestHandler(authorization, aatfDataAccess, genericDataAccess, addressMapper);
+            var handler = new EditAatfDetailsRequestHandler(authorization, aatfDataAccess, genericDataAccess, addressMapper, organisationDetailsDataAccess);
 
             Func<Task> action = async () => await handler.HandleAsync(A.Dummy<EditAatfDetails>());
 
@@ -55,7 +59,7 @@
         {
             var authorization = new AuthorizationBuilder().AllowInternalAreaAccess().DenyRole(Roles.InternalAdmin).Build();
 
-            var handler = new EditAatfDetailsRequestHandler(authorization, aatfDataAccess, genericDataAccess, addressMapper);
+            var handler = new EditAatfDetailsRequestHandler(authorization, aatfDataAccess, genericDataAccess, addressMapper, organisationDetailsDataAccess);
 
             Func<Task> action = async () => await handler.HandleAsync(A.Dummy<EditAatfDetails>());
 
@@ -63,14 +67,29 @@
         }
 
         [Fact]
+        public async Task HandleAsync_GivenMessageContainingUpdatedAddress_MapperIsCalled()
+        {
+            var data = CreateAatfData(out var competentAuthority);
+            var updateRequest = fixture.Build<EditAatfDetails>().With(e => e.Data, data).Create();
+            var siteAddress = A.Fake<AatfAddress>();
+
+            A.CallTo(() => addressMapper.Map(data.SiteAddress)).Returns(siteAddress);
+
+            var aatf = A.Fake<Aatf>();
+
+            A.CallTo(() => genericDataAccess.GetById<Aatf>(updateRequest.Data.Id)).Returns(aatf);
+
+            var result = await handler.HandleAsync(updateRequest);
+
+            Assert.True(result);
+
+            A.CallTo(() => addressMapper.Map(data.SiteAddress)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
         public async Task HandleAsync_GivenMessageContainingUpdatedAddress_DetailsAreUpdatedCorrectly()
         {
-            var competentAuthority = fixture.Create<UKCompetentAuthorityData>();
-            var data = fixture.Build<AatfData>()
-                .With(e => e.CompetentAuthority, competentAuthority)
-                .With(e => e.AatfStatus, Core.AatfReturn.AatfStatus.Approved)
-                .With(e => e.Size, Core.AatfReturn.AatfSize.Large)
-                .Create();
+            var data = CreateAatfData(out var competentAuthority);
             var updateRequest = fixture.Build<EditAatfDetails>().With(e => e.Data, data).Create();
             var siteAddress = fixture.Create<AatfAddress>();
             A.CallTo(() => addressMapper.Map(data.SiteAddress)).Returns(siteAddress);
@@ -82,13 +101,42 @@
             var result = await handler.HandleAsync(updateRequest);
 
             Assert.True(result);
+
             A.CallTo(() => aatfDataAccess.UpdateDetails(aatf, A<Aatf>.That.Matches(a => a.Name == data.Name &&
                 a.CompetentAuthorityId == competentAuthority.Id &&
                 a.ApprovalNumber == data.ApprovalNumber &&
                 a.AatfStatus == Domain.AatfReturn.AatfStatus.Approved &&
-                a.SiteAddress == siteAddress &&
                 a.Size == Domain.AatfReturn.AatfSize.Large &&
                 a.ApprovalDate == data.ApprovalDate.GetValueOrDefault()))).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenMessageContainingUpdatedAddress_AddressIsUpdatedCorrectly()
+        {
+            var data = CreateAatfData(out var competentAuthority);
+            var updateRequest = fixture.Build<EditAatfDetails>().With(e => e.Data, data).Create();
+            var siteAddress = fixture.Create<AatfAddress>();
+            A.CallTo(() => addressMapper.Map(data.SiteAddress)).Returns(siteAddress);
+
+            var aatf = A.Fake<Aatf>();
+
+            A.CallTo(() => genericDataAccess.GetById<Aatf>(updateRequest.Data.Id)).Returns(aatf);
+
+            var result = await handler.HandleAsync(updateRequest);
+
+            Assert.True(result);
+
+            A.CallTo(() => aatfDataAccess.UpdateAddress(A<AatfAddress>._, siteAddress, A<Country>._)).MustHaveHappenedOnceExactly();
+        }
+        private AatfData CreateAatfData(out UKCompetentAuthorityData competentAuthority)
+        {
+            competentAuthority = fixture.Create<UKCompetentAuthorityData>();
+            var data = fixture.Build<AatfData>()
+                .With(e => e.CompetentAuthority, competentAuthority)
+                .With(e => e.AatfStatus, Core.AatfReturn.AatfStatus.Approved)
+                .With(e => e.Size, Core.AatfReturn.AatfSize.Large)
+                .Create();
+            return data;
         }
     }
 }
