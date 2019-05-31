d7633c2675b56aa07baf5e2c83c2d230d9199c9e
Will Andrews <will7989@hotmail.com>
2019-05-31 09:29:12 +0100

    modified the list of activities so that only scheme options are returned if the org has a scheme and AATF options are returned if org has at least 1 aatf

--- src/EA.Weee.RequestHandlers.Tests.Unit/Organisations/OrganisationByIdHandlerTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Organisations/OrganisationByIdHandlerTests.cs
@@ -2,11 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Security;
     using System.Threading.Tasks;
     using Core.Organisations;
     using DataAccess;
     using Domain.Organisation;
+    using EA.Weee.Domain;
+    using EA.Weee.Domain.AatfReturn;
     using FakeItEasy;
     using Mappings;
     using Prsd.Core.Mapper;
@@ -62,6 +65,12 @@
                 GetOrganisationWithId(organisationId)
             }));
 
+            List<Aatf> aatfs = new List<Aatf>();
+            List<Domain.Scheme.Scheme> schemes = new List<Domain.Scheme.Scheme> { new Domain.Scheme.Scheme(organisationId) };
+
+            A.CallTo(() => context.Schemes).Returns(dbHelper.GetAsyncEnabledDbSet(schemes));
+            A.CallTo(() => context.Aatfs).Returns(dbHelper.GetAsyncEnabledDbSet(aatfs));
+
             OrganisationData expectedReturnValue = new OrganisationData();
             Organisation mappedOrganisation = null;
             var organisationMap = A.Fake<IMap<Organisation, OrganisationData>>();
@@ -79,11 +88,65 @@
             Assert.Same(expectedReturnValue, result);
         }
 
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task HandleAsync_NoSchemesOrAatfs_OrganisationDataSetCorrectly(bool hasAatfOrScheme)
+        {
+            var authorization = AuthorizationBuilder.CreateUserAllowedToAccessOrganisation();
+
+            var organisationId = Guid.NewGuid();
+            var context = A.Fake<WeeeContext>();
+            Organisation organisation = GetOrganisationWithId(organisationId);
+
+            A.CallTo(() => context.Organisations).Returns(dbHelper.GetAsyncEnabledDbSet(new List<Organisation>
+            {
+                organisation
+            }));
+
+            OrganisationData expectedReturnValue = new OrganisationData();
+            Organisation mappedOrganisation = null;
+            var organisationMap = A.Fake<IMap<Organisation, OrganisationData>>();
+            A.CallTo(() => organisationMap.Map(A<Organisation>._))
+                .Invokes((Organisation o) => mappedOrganisation = o)
+                .Returns(expectedReturnValue);
+
+            List<Aatf> aatfs = new List<Aatf>();
+            List<Domain.Scheme.Scheme> schemes = new List<Domain.Scheme.Scheme>();
+
+            if (hasAatfOrScheme)
+            {
+                aatfs.Add(CreateAatf(organisation));
+                schemes.Add(new Domain.Scheme.Scheme(organisationId));
+            }
+
+            A.CallTo(() => context.Schemes).Returns(dbHelper.GetAsyncEnabledDbSet(schemes));
+            A.CallTo(() => context.Aatfs).Returns(dbHelper.GetAsyncEnabledDbSet(aatfs));
+
+            var handler = new OrganisationByIdHandler(authorization, context, organisationMap);
+            var message = new GetOrganisationInfo(organisationId);
+
+            var result = await handler.HandleAsync(message);
+
+            Assert.Equal(hasAatfOrScheme, result.HasAatfs);
+            Assert.Equal(hasAatfOrScheme, result.SchemeId != null);
+
+            if (hasAatfOrScheme)
+            {
+                Assert.Equal(schemes.FirstOrDefault().Id, result.SchemeId);
+            }
+        }
+
         private Organisation GetOrganisationWithId(Guid id)
         {
             var organisation = A.Fake<Organisation>();
             A.CallTo(() => organisation.Id).Returns(id);
             return organisation;
+        }
+
+        private Aatf CreateAatf(Organisation organisation)
+        {
+            return new Aatf("name", A.Dummy<UKCompetentAuthority>(), "number", A.Dummy<AatfStatus>(), organisation, A.Dummy<AatfAddress>(), A.Dummy<AatfSize>(), DateTime.Now, A.Dummy<AatfContact>(), A.Dummy<FacilityType>());
         }
     }
 }
