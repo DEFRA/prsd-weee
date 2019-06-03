164c055c9dd6b5c6e4f5fd157d693b363c5abb38
Jan Biddle <46448988+JB12298@users.noreply.github.com>
2019-06-03 11:18:33 +0100

    Feature/approval date validation (#300)
    * +Validator
    +Model Base Class & Prop Rename
    +ProgresiveDisclosure
    +UnitTests
    -CommentedOutText
    
    * Merged
    
    * Updated csproj
    
    * Fixed non persisting facility type
    
    * Added wihtihn bounds tests
    
    * -Added error message check
    -Removed wrappers and references
    
    * updated tests

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/AatfViewModelBase.cs
@@ -0,0 +1,50 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.AddAatf
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Core.DataStandards;
+    using EA.Weee.Core.Shared;
+
+    public abstract class AatfViewModelBase
+    {
+        [Required]
+        [RegularExpression(@"WEE/([A-Z]{2}[0-9]{4}[A-Z]{2})/ATF", ErrorMessage = "Approval number is not in correct format")]
+        [StringLength(EnvironmentAgencyMaxFieldLengths.SchemeApprovalNumber)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Approval number")]
+        public string ApprovalNumber { get; set; }
+
+        public IEnumerable<UKCompetentAuthorityData> CompetentAuthoritiesList { get; set; }
+
+        [Required]
+        [Display(Name = "Appropriate authority")]
+        public Guid CompetentAuthorityId { get; set; }
+
+        [Display(Name = "Approval date")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime? ApprovalDate { get; set; }
+
+        [Required]
+        [Display(Name = "Compliance year")]
+        public Int16 ComplianceYear { get; set; }
+
+        public IEnumerable<AatfStatus> StatusList { get; set; }
+
+        [Required]
+        [Display(Name = "Status")]
+        public int StatusValue { get; set; }
+
+        public IEnumerable<AatfSize> SizeList { get; set; }
+
+        public FacilityType FacilityType { get; set; }
+
+        [Required]
+        [Display(Name = "Size")]
+        public int SizeValue { get; set; }
+    }
+}
