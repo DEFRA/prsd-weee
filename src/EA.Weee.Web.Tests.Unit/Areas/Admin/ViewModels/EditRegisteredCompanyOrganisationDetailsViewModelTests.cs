9944f0e44865f6647642c43fc5bc5f16720858e2
JohnPadgettCivica <JohnPadgettCivica>
2019-06-03 11:10:12 +0100

    Additional unit tests

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/ViewModels/EditRegisteredCompanyOrganisationDetailsViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.ViewModels
+{
+    using EA.Weee.Web.Areas.Admin.ViewModels.Organisation;
+    using FluentAssertions;
+    using System.ComponentModel.DataAnnotations;
+    using Xunit;
+
+    public class EditRegisteredCompanyOrganisationDetailsViewModelTests
+    {
+        [Fact]
+        public void GivenModel_CompanyNameShouldHaveRequiredFieldAttribute()
+        {
+            typeof(EditRegisteredCompanyOrganisationDetailsViewModel).GetProperty("CompanyName")
+                .Should().BeDecoratedWith<RequiredAttribute>();
+        }
+
+        [Fact]
+        public void GivenModel_CompanyNameShouldHaveDisplayNameAttribute()
+        {
+        }
+    }
+}
