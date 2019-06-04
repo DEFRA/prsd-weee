5e3a626c2af85a4023b8afe6efbb9c7fc825f68e
JohnPadgettCivica <JohnPadgettCivica>
2019-06-04 09:29:38 +0100

    Add aatf returns enabled check to returns controller

--- src/EA.Weee.Web/Areas/AatfReturn/Controllers/ReturnsController.cs
+++ src/EA.Weee.Web/Areas/AatfReturn/Controllers/ReturnsController.cs
@@ -14,6 +14,7 @@
     using Weee.Requests.AatfReturn;
     using Weee.Requests.Organisations;
 
+    [ValidateOrganisationActionFilterAttribute]
     public class ReturnsController : AatfReturnBaseController
     {
         private readonly Func<IWeeeClient> apiClient;
