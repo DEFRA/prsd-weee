603ac8cea17877e99945669bfae93a94bc12c37a
Stephen Spinks <stephen.spinks@civica.co.uk>
2019-06-06 14:10:21 +0100

    Correctly display date validation message (#323)
    * Correctly display date validation message (checks whether the date validation has been run and runs it if not)
    
    * Remove unnecessary validation check

---
+++ src/EA.Weee.Web/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.Web.Extensions
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public static class ModelStateExtensions
+    {
+        public static void RunCustomValidation(this ModelStateDictionary modelState, IValidatableObject viewModel)
+        {
+            var errors = viewModel.Validate(new ValidationContext(viewModel, null, null));
+            foreach (var error in errors)
+            {
+                foreach (var memberName in error.MemberNames)
+                {
+                    modelState.AddModelError(memberName, error.ErrorMessage);
+                }
+            }
+        }
+    }
+}
