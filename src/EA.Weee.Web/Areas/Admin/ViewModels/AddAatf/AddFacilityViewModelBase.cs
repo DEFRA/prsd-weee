5092d1f8dd41c04efd77d278c83def8340351d5d
Stephen Spinks <stephen.spinks@civica.co.uk>
2019-06-10 09:19:26 +0100

    Fix validation summary ordering (#334)
    * Allow custom ordering of the validation summary
    
    * Add missing tests

--- src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/AddFacilityViewModelBase.cs
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/AddFacilityViewModelBase.cs
@@ -15,6 +15,34 @@
 
         public IEnumerable<short> ComplianceYearList => new List<short> { 2019 };
 
+        public static new IEnumerable<string> ValidationMessageDisplayOrder => new List<string>
+        {
+            nameof(Name),
+            $"{nameof(SiteAddressData)}.{nameof(AatfAddressData.Address1)}",
+            $"{nameof(SiteAddressData)}.{nameof(AatfAddressData.Address2)}",
+            $"{nameof(SiteAddressData)}.{nameof(AatfAddressData.TownOrCity)}",
+            $"{nameof(SiteAddressData)}.{nameof(AatfAddressData.CountyOrRegion)}",
+            $"{nameof(SiteAddressData)}.{nameof(AatfAddressData.Postcode)}",
+            $"{nameof(SiteAddressData)}.{nameof(AatfAddressData.CountryId)}",
+            nameof(ApprovalNumber),
+            nameof(CompetentAuthorityId),
+            nameof(StatusValue),
+            nameof(SizeValue),
+            nameof(ApprovalDate),
+            nameof(ComplianceYear),
+            $"{nameof(ContactData)}.{nameof(AatfContactData.FirstName)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.LastName)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.Position)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.AddressData)}.{nameof(AatfContactAddressData.Address1)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.AddressData)}.{nameof(AatfContactAddressData.Address2)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.AddressData)}.{nameof(AatfContactAddressData.TownOrCity)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.AddressData)}.{nameof(AatfContactAddressData.CountyOrRegion)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.AddressData)}.{nameof(AatfContactAddressData.Postcode)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.AddressData)}.{nameof(AatfContactAddressData.CountryId)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.Telephone)}",
+            $"{nameof(ContactData)}.{nameof(AatfContactData.Email)}"
+        };
+
         public AddFacilityViewModelBase()
         {
             ContactData = new AatfContactData();
