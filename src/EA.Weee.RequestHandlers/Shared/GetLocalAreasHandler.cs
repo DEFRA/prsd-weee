01211da7080889a7c0a4638cc62d085394759cb2
JohnPadgettCivica <JohnPadgettCivica>
2019-06-13 10:42:29 +0100

    Add local area code

---
+++ src/EA.Weee.RequestHandlers/Shared/GetLocalAreasHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace EA.Weee.RequestHandlers.Shared
+{
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Admin;
+    using DataAccess;
+    using EA.Weee.RequestHandlers.Security;
+    using Prsd.Core.Mediator;
+    using Requests.Admin;
+
+    internal class GetLocalAreasHandler : IRequestHandler<GetLocalAreas, List<LocalAreaData>>
+    {
+        private readonly WeeeContext context;
+        private readonly IWeeeAuthorization authorization;
+
+        public GetLocalAreasHandler(WeeeContext context, IWeeeAuthorization authorization)
+        {
+            this.context = context;
+            this.authorization = authorization;
+        }
+
+        public async Task<List<LocalAreaData>> HandleAsync(GetLocalAreas query)
+        {
+            authorization.EnsureCanAccessInternalArea();
+
+            return await context.LocalAreas.OrderBy(x => x.Name)
+                .Select(p => new LocalAreaData() { Id = p.Id, Name = p.Name, CompetentAuthorityId = p.CompetentAuthorityId})
+                .ToListAsync();
+        }
+    }
+}
