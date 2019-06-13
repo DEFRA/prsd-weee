859d345a668432fcf318ee9244a89f94993371bc
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-06-13 15:15:19 +0100

    Set up for PAN area requests, domain, handler etc.

---
+++ src/EA.Weee.DataAccess/Mappings/PanAreaMapping.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Weee.DataAccess.Mappings
+{
+    using EA.Weee.Domain;
+    using System.Data.Entity.ModelConfiguration;
+
+    internal class PanAreaMapping : EntityTypeConfiguration<PanArea>
+    {
+        public PanAreaMapping()
+        {
+            this.ToTable("PanArea", "Lookup");
+        }
+    }
+}
