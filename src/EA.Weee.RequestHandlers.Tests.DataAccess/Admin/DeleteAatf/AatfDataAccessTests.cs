49e9518bb8306023eae33b4209b87a2efe77e9dd
Will Andrews <will7989@hotmail.com>
2019-06-14 15:47:38 +0100

    adding in handler to check if aatf can be deleted

---
+++ src/EA.Weee.RequestHandlers.Tests.DataAccess/Admin/DeleteAatf/AatfDataAccessTests.cs
@@ -0,0 +1,40 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.DataAccess.Admin.DeleteAatf
+{
+    using EA.Weee.RequestHandlers.Admin.DeleteAatf;
+    using EA.Weee.Tests.Core.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class AatfDataAccessTests
+    {
+        [Fact]
+        public async void DoesAatfHaveData_HasReusedAndSentOnAndReceived_ReturnsTrue()
+        {
+            using (var dbWrapper = new DatabaseWrapper())
+            {
+                var modelHelper = new ModelHelper(dbWrapper.Model);
+
+                var organisation = modelHelper.CreateOrganisation();
+                var scheme = modelHelper.CreateScheme(organisation);
+                var @return = modelHelper.CreateReturn();
+                var aatf = modelHelper.CreateAatf();
+                dbWrapper.Model.SaveChanges();
+
+                Domain.AatfReturn.WeeeReceived received = new Domain.AatfReturn.WeeeReceived(scheme.Id, aatf.Id, @return.Id);
+
+                dbWrapper.WeeeContext.WeeeReceived.Add(received);
+                dbWrapper.WeeeContext.SaveChanges();
+
+                var dataAccess = new AatfDataAccess(dbWrapper.WeeeContext);
+
+                var result = (await dataAccess.DoesAatfHaveData(aatf.Id));
+
+                Assert.True(result);
+            }
+        }
+    }
+}
---
+++ src/EA.Weee.Requests/Admin/DeleteAatf/CheckAatfCanBeDeleted.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.Requests.Admin.DeleteAatf
+{
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.Core.Admin;
+    using System;
+
+    public class CheckAatfCanBeDeleted : IRequest<CanAatfBeDeletedFlags>
+    {
+        public Guid AatfId { get; set; }
+    }
+}
