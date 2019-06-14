fb8fa8cea412b645e73ed234ac828f7e3e481043
JohnPadgettCivica <JohnPadgettCivica>
2019-06-14 13:56:11 +0100

    Bug 72252:Manage AE returns external menu Fix Test url authentication

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Test/TestControllerBaseTests.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Test
+{
+    using AutoFixture;
+    using FluentAssertions;
+    using Security;
+    using Web.Areas.Test.Controllers;
+    using Web.Filters;
+    using Xunit;
+
+    public class TestControllerBaseTests
+    {
+        [Fact]
+        public void TestControllerBase_SholdBeDecoratedWithAuthorizeInternalClaimsAttribute()
+        {
+            typeof(TestControllerBase).Should()
+                .BeDecoratedWith<AuthorizeInternalClaimsAttribute>(a => a.Match(new AuthorizeInternalClaimsAttribute(Claims.CanAccessInternalArea)));
+        }
+    }
+}
