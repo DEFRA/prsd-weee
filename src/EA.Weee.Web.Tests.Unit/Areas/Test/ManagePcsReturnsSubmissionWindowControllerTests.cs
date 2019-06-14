c8c21f9e6980c3d45805f6ec1d05fab186664496
JohnPadgettCivica <JohnPadgettCivica>
2019-06-14 13:56:11 +0100

    Bug 72252:Manage AE returns external menu Fix Test url authentication

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Test/ManagePcsReturnsSubmissionWindowControllerTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Test
+{
+    using FluentAssertions;
+    using Web.Areas.Test.Controllers;
+    using Xunit;
+
+    public class ManagePcsReturnsSubmissionWindowControllerTests
+    {
+        [Fact]
+        public void ManagePcsReturnsSubmissionWindowController_ShouldHaveTestControllerBase()
+        {
+            typeof(ManagePcsReturnsSubmissionWindowController).BaseType.Name.Should().Be(typeof(TestControllerBase).Name);
+        }
+    }
+}
