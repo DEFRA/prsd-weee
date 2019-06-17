97f0f6620c3319667cfc58818389e9bbcafb1152
Will Andrews <will7989@hotmail.com>
2019-06-17 09:46:47 +0100

    moved aatfDataAccess to existing class. Data access unit tests done

--- src/EA.Weee.RequestHandlers.Tests.DataAccess/AatfReturn/Internal/AatfDataAccessTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.DataAccess/AatfReturn/Internal/AatfDataAccessTests.cs
@@ -2,12 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using AutoFixture;
     using EA.Weee.Core.AatfReturn;
     using EA.Weee.DataAccess;
     using EA.Weee.Domain;
     using EA.Weee.Domain.AatfReturn;
+    using EA.Weee.Domain.Organisation;
     using EA.Weee.RequestHandlers.AatfReturn.Internal;
     using EA.Weee.Tests.Core;
     using FakeItEasy;
@@ -123,6 +125,116 @@
                 newSite.Telephone,
                 newSite.Email)).MustHaveHappened(Repeated.Exactly.Once)
             .Then(A.CallTo(() => context.SaveChangesAsync()).MustHaveHappened(Repeated.Exactly.Once));
+        }
+
+        [Theory]
+        [InlineData(false, false, false, false)]
+        [InlineData(true, false, false, true)]
+        [InlineData(false, true, false, true)]
+        [InlineData(false, false, true, true)]
+        [InlineData(true, true, true, true)]
+        public async void DoesAatfHaveData_GivenId_ReturnsBasedOnData(bool hasReceived, bool hasReused, bool hasSentOn, bool expectedResult)
+        {
+            List<WeeeReceived> weeeReceived = new List<WeeeReceived>();
+            List<WeeeReused> weeeReused = new List<WeeeReused>();
+            List<WeeeSentOn> weeeSentOn = new List<WeeeSentOn>();
+
+            Guid aatfId = Guid.NewGuid();
+
+            if (hasReceived)
+            {
+                WeeeReceived weee = A.Dummy<WeeeReceived>();
+                A.CallTo(() => weee.AatfId).Returns(aatfId);
+                weeeReceived.Add(weee);
+            }
+
+            if (hasReused)
+            {
+                WeeeReused weee = A.Dummy<WeeeReused>();
+                A.CallTo(() => weee.AatfId).Returns(aatfId);
+                weeeReused.Add(weee);
+            }
+
+            if (hasSentOn)
+            {
+                WeeeSentOn weee = A.Dummy<WeeeSentOn>();
+                A.CallTo(() => weee.AatfId).Returns(aatfId);
+                weeeSentOn.Add(weee);
+            }
+
+            A.CallTo(() => context.WeeeReceived).Returns(dbContextHelper.GetAsyncEnabledDbSet(weeeReceived));
+            A.CallTo(() => context.WeeeReused).Returns(dbContextHelper.GetAsyncEnabledDbSet(weeeReused));
+            A.CallTo(() => context.WeeeSentOn).Returns(dbContextHelper.GetAsyncEnabledDbSet(weeeSentOn));
+
+            bool result = await dataAccess.DoesAatfHaveData(aatfId);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async void DoesAatfOrganisationHaveActiveUsers_ReturnsResultBasedOnActiveUsers(bool hasActiveUsers)
+        {
+            Guid aatfId = Guid.NewGuid();
+            Guid organisationId = Guid.NewGuid();
+
+            Organisation organisation = A.Fake<Organisation>();
+            A.CallTo(() => organisation.Id).Returns(organisationId);
+
+            Aatf aatf = A.Fake<Aatf>();
+            A.CallTo(() => aatf.Id).Returns(aatfId);
+            A.CallTo(() => aatf.Organisation).Returns(organisation);
+
+            A.CallTo(() => context.Aatfs).Returns(dbContextHelper.GetAsyncEnabledDbSet(new List<Aatf>() { aatf }));
+
+            List<OrganisationUser> organisationUsers = new List<OrganisationUser>();
+            if (hasActiveUsers)
+            {
+                OrganisationUser user = A.Fake<OrganisationUser>();
+                A.CallTo(() => user.OrganisationId).Returns(organisationId);
+                organisationUsers.Add(user);
+            }
+
+            A.CallTo(() => context.OrganisationUsers).Returns(dbContextHelper.GetAsyncEnabledDbSet(organisationUsers));
+
+            bool result = await dataAccess.DoesAatfOrganisationHaveActiveUsers(aatfId);
+
+            Assert.Equal(hasActiveUsers, result);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async void DoesAatfOrganisationHaveMoreAatfs_ReturnsResultBasedOnOtherAatfs(bool hasOtherAatfs)
+        {
+            Guid aatfId = Guid.NewGuid();
+            Guid organisationId = Guid.NewGuid();
+
+            Organisation organisation = A.Fake<Organisation>();
+            A.CallTo(() => organisation.Id).Returns(organisationId);
+
+            Aatf aatf = A.Fake<Aatf>();
+            A.CallTo(() => aatf.Id).Returns(aatfId);
+            A.CallTo(() => aatf.Organisation).Returns(organisation);
+
+            List<Aatf> aatfs = new List<Aatf>();
+
+            aatfs.Add(aatf);
+
+            if (hasOtherAatfs)
+            {
+                Aatf otherAatf = A.Fake<Aatf>();
+                A.CallTo(() => otherAatf.Organisation).Returns(organisation);
+
+                aatfs.Add(otherAatf);
+            }
+
+            A.CallTo(() => context.Aatfs).Returns(dbContextHelper.GetAsyncEnabledDbSet(aatfs));
+
+            bool result = await dataAccess.DoesAatfOrganisationHaveMoreAatfs(aatfId);
+
+            Assert.Equal(hasOtherAatfs, result);
         }
     }
 }
