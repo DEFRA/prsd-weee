d18e5d76bfc84afad5be06b5c9983ea93bf7c119
Stephen Spinks <stephen.spinks@civica.co.uk>
2019-06-18 14:39:54 +0100

    Update some 'Sole trader or individual' labels to 'Sole trader / individual name'

--- src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/Details/SoleTraderDetailsViewModel.cs
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/AddAatf/Details/SoleTraderDetailsViewModel.cs
@@ -7,7 +7,7 @@
     public class SoleTraderDetailsViewModel : OrganisationViewModel
     {
         [Required]
-        [DisplayName("Sole trader or individual")]
+        [DisplayName("Sole trader / individual name")]
         [StringLength(CommonMaxFieldLengths.DefaultString)]
         public string CompanyName { get; set; }
     }
