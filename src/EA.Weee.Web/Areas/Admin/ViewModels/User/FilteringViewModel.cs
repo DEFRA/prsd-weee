0b4502b4614a1eeaf6c3881d8e61c2379b57d8ba
Stephen Spinks <stephen.spinks@civica.co.uk>
2019-06-19 12:50:04 +0100

    Add filter to user page (UI only)

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/User/FilteringViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.User
+{
+    using System.ComponentModel.DataAnnotations;
+    using EA.Weee.Core.Shared;
+
+    public class FilteringViewModel
+    {
+        public string Name { get; set; }
+
+        [Display(Name = "Organisation name")]
+        public string OrganisationName { get; set; }
+
+        public UserStatus Status { get; set; }
+    }
+}
