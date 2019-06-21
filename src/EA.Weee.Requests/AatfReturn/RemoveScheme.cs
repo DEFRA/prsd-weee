a1b306a801844c7ce1199b1a1e1a4937dfadcfc4
Will Andrews <will7989@hotmail.com>
2019-06-21 11:26:40 +0100

    added handler to start removal of scheme. Redirect back to select PCs if user selects no

---
+++ src/EA.Weee.Requests/AatfReturn/RemoveScheme.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Weee.Requests.AatfReturn
+{
+    using EA.Prsd.Core.Mediator;
+    using System;
+
+    public class RemoveScheme : IRequest<bool>
+    {
+        public Guid SchemeId { get; set; }
+    }
+}
