d7259c7912a7d492f746072e92b1baedb9a6e255
JohnPadgettCivica <JohnPadgettCivica>
2019-06-24 09:10:53 +0100

    Changes after review.

--- src/EA.Weee.RequestHandlers/AatfReturn/AatfTaskList/IFetchAatfDataAccess.cs
+++ src/EA.Weee.RequestHandlers/AatfReturn/AatfTaskList/IFetchAatfDataAccess.cs
@@ -7,7 +7,7 @@
 
     public interface IFetchAatfDataAccess
     {
-        Task<List<Aatf>> FetchAatfByOrganisationIdAndQuarter(Guid organisationId, int complianceYear, DateTime approvalDateStart);
+        Task<List<Aatf>> FetchAatfByOrganisationIdAndQuarter(Guid organisationId, int complianceYear, DateTime windowStartDate);
 
         Task<List<Aatf>> FetchAatfByReturnId(Guid returnId);
     }
