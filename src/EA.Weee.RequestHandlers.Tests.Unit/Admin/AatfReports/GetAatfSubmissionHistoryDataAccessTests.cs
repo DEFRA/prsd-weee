3c67bd7a67d21f1a31320543deef1d70a3373c1b
JohnPadgettCivica <JohnPadgettCivica>
2019-07-01 17:17:22 +0100

    aatf submission history Unit tests

--- src/EA.Weee.RequestHandlers.Tests.Unit/Admin/AatfReports/GetAatfSubmissionHistoryDataAccessTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/AatfReports/GetAatfSubmissionHistoryDataAccessTests.cs
@@ -1,8 +1,14 @@
 ﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Admin.AatfReports
 {
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
     using DataAccess;
+    using DataAccess.StoredProcedure;
     using FakeItEasy;
+    using FluentAssertions;
     using RequestHandlers.Admin.AatfReports;
+    using Xunit;
 
     public class GetAatfSubmissionHistoryDataAccessTests
     {
@@ -14,6 +20,28 @@
             context = A.Fake<WeeeContext>();
 
             dataAccess = new GetAatfSubmissionHistoryDataAccess(context);
+        }
+
+        [Fact]
+        public async Task GetItemsAsync_GivenAatfId_ContextStoredProcedureShouldBeCalled()
+        {
+            var id = Guid.NewGuid();
+
+            await dataAccess.GetItemsAsync(id);
+
+            A.CallTo(() => context.StoredProcedures.GetAatfSubmissions(id)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task GetItemsAsync_GivenAatfData_DataShouldBeReturned()
+        {
+            var aatfHistory = A.CollectionOfFake<AatfSubmissionHistory>(2).ToList();
+
+            A.CallTo(() => context.StoredProcedures.GetAatfSubmissions(A<Guid>._)).Returns(aatfHistory);
+
+            var results = await dataAccess.GetItemsAsync(A.Dummy<Guid>());
+
+            results.Should().BeSameAs(aatfHistory);
         }
     }
 }
