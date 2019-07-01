97aaeb3684430bcfb227a63ad64e3774f84650c5
JohnPadgettCivica <JohnPadgettCivica>
2019-07-01 15:47:28 +0100

    Fix javascript to only display relevant error

--- src/EA.Weee.Web/Scripts/weee-checkbox-toggle-visibility.js
+++ src/EA.Weee.Web/Scripts/weee-checkbox-toggle-visibility.js
@@ -2,7 +2,7 @@
     $(document).ready(function () {
         var attr = $(toggle).attr('checked');
         console.log(attr);
-        if (!$('.govuk-form-group').hasClass('error')) {
+        if (!$('#WeeeReusedOptions.govuk-form-group').hasClass('dcf-section-error')) {
             if (!(attr !== undefined)) {
                 $(section).css('display', 'none');
             }
@@ -13,9 +13,6 @@
                 $(section).css('display', 'block');
             } else {
                 $(section).css('display', 'none');
-                $('#error_explanation').css('display', 'none');
-                $('.govuk-form-group').removeClass('govuk-form-group--error').removeClass('error');
-                $('.govuk-radios__input').removeClass('input-validation-error');
             }
         });
     });
