45354d7b845d46049155dcda0fc21708d88e2a3f
SumeN <sumen@sfwltd.co.uk>
2019-07-02 08:45:11 +0100

    Update the action filter tocheck facility type

---
+++ src/EA.Weee.Requests/AatfReturn/GetAatfByOrganisationFacilityType.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.Requests.AatfReturn
+{
+    using System;
+    using System.Collections.Generic;
+    using Core.AatfReturn;
+    using Prsd.Core.Mediator;
+
+    public class GetAatfByOrganisationFacilityType : IRequest<List<AatfData>>
+    {
+        public Guid OrganisationId { get; set; }
+
+        public FacilityType FacilityType { get; set; }
+
+        public GetAatfByOrganisationFacilityType(Guid organisationId, FacilityType facilityType)
+        {
+            OrganisationId = organisationId;
+            FacilityType = facilityType;
+        }
+    }
+}
