619c85d565b33201d23d3ce8d0213ad9a9f6ea87
JohnPadgettCivica <JohnPadgettCivica>
2019-07-05 11:16:38 +0100

    AE test updates

---
+++ src/EA.Weee.Database/scripts/Update/Release 2/Sprint12/20190701-1300-create-getAeSubmissions-storedprocedure.sql
@@ -0,0 +1,28 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [AATF].getAeSubmissions
+	@AeId UNIQUEIDENTIFIER
+AS
+BEGIN
+SET NOCOUNT ON;
+
+SELECT
+	r.Id AS ReturnId,
+	r.ComplianceYear,
+	r.Quarter,
+	CONCAT(u.FirstName , ' ', u.Surname) AS SubmittedBy,
+	r.SubmittedDate AS SubmittedDate
+FROM
+	[AATF].[Return] r WITH (NOLOCK)
+	INNER JOIN [AATF].[ReturnAatf] ra ON ra.ReturnId = r.Id
+	INNER JOIN [Identity].AspNetUsers u ON r.SubmittedById = u.Id
+WHERE
+	r.ReturnStatus = 2
+	AND r.FacilityType = 2
+	AND ra.AatfId = @AeId
+
+END
+GO
