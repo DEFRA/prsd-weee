5d95a809239e2fb2ec9622c199085cdc9452e58c
SumeN <sumen@sfwltd.co.uk>
2019-07-05 16:56:48 +0100

    Aatf/AE return report

---
+++ src/EA.Weee.DataAccess/StoredProcedure/AatfAeReturnData.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Weee.DataAccess.StoredProcedure
+{
+    using System;
+
+    /// <summary>
+    /// This class maps to the results of [AATF].[spgAatfAeReturnDataCsvData]
+    /// </summary>
+    public class AatfAeReturnData
+    {
+        public string Name { get; set; }
+
+        public string ApprovalNumber { get; set; }
+
+        public string OrganisationName { get; set; }
+
+        public string ReturnStatus { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? SubmittedDate { get; set; }
+        public string SubmittedBy { get; set; }
+        public string CompetentAuthorityAbbr { get; set; }
+    }
+}
