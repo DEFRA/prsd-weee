d79b6a723c4988da78024284479b838e9e7966b5
Will Andrews <will7989@hotmail.com>
2019-07-05 14:10:58 +0100

    done most of the tracking links

--- src/EA.Weee.Web/RazorHelpers/WeeeGds.ProgressiveDisclosure.cs
+++ src/EA.Weee.Web/RazorHelpers/WeeeGds.ProgressiveDisclosure.cs
@@ -1,25 +1,14 @@
 ﻿namespace EA.Weee.Web.RazorHelpers
 {
-    using System;
+    using System.Web.Mvc;
 
-    public class ProgressiveDisclosure<TModel> : IDisposable
+    public partial class WeeeGds<TModel>
     {
-        private readonly WeeeGds<TModel> gdsHelper;
-
-        public ProgressiveDisclosure(WeeeGds<TModel> gdsHelper, string linkText)
+        public MvcHtmlString CreateProgressiveDisclosure(string linkText, string hiddenContent, string eventCategory, string eventAction, string eventLabel)
         {
-            this.gdsHelper = gdsHelper;
-
-            var html = string.Format(
-                @"<details class=""govuk-details"" aria-live=""polite"" role=""group""><summary class=""govuk-details__summary""><span class=""govuk-details__summary-text"">{1}<span class=""hidden-for-screen-reader"">This is an expandable link that will reveal more content upon activation</span></span></summary><div class=""govuk-details__text"">",
-                gdsHelper.EventTrackingFunction("Hidden content", "Progressive disclosure", linkText), linkText);
-
-            gdsHelper.HtmlHelper.ViewContext.Writer.Write(html);
-        }
-
-        public void Dispose()
-        {
-            gdsHelper.HtmlHelper.ViewContext.Writer.Write("</div></details>");
+            return new MvcHtmlString(string.Format(
+                @"<details class=""govuk-details"" aria-live=""polite"" role=""group""><summary class=""govuk-details__summary""><span class=""govuk-details__summary-text"">{1}<span class=""hidden-for-screen-reader"">This is an expandable link that will reveal more content upon activation</span></span></summary><div class=""govuk-details__text""id=""details - content"" aria-hidden=""true""><div class=""govuk - body govuk - !-font - size - 16"">{2}</div></div></details>",
+                EventTrackingFunction(eventCategory, eventAction, eventLabel), linkText, hiddenContent));
         }
     }
 }
