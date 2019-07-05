60d9f6e64789c6c41a484b788ba0db0b8189ebc8
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-07-05 14:48:03 +0100

    Moving the operator checkbox bool into editor template for operator to move it underneath the name

--- src/EA.Weee.Core/AatfReturn/OperatorAddressData.cs
+++ src/EA.Weee.Core/AatfReturn/OperatorAddressData.cs
@@ -11,6 +11,9 @@
         [Display(Name = "Operator name")]
         public override string Name { get; set; }
 
+        [Display(Name = "Is the operator address the same as the AATF / ATF address?")]
+        public bool IsOperatorTheSameAsAatf { get; set; }
+
         public OperatorAddressData(string name, string address1, string address2, string townOrCity, string countyOrRegion, string postcode, Guid countryId, string countryName)
         : base(name, address1, address2, townOrCity, countyOrRegion, postcode, countryId, countryName)
         {
