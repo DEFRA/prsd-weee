fa012dc9452af61c4290a0b1afc8f40bc2fcdf72
Will Andrews <will7989@hotmail.com>
2019-07-08 10:43:31 +0100

    added screen reader text back in for other files or added a null paramter value if none was provided before

--- src/EA.Weee.Web/RazorHelpers/WeeeGds.EventTracking.cs
+++ src/EA.Weee.Web/RazorHelpers/WeeeGds.EventTracking.cs
@@ -63,6 +63,11 @@
 
             attributes.AppendFormat(@"onclick=""{0}{1}""", EventTrackingFunction(eventCategory, eventAction, eventLabel), additionalOnclickContent);
 
+            if (explanationText == null)
+            {
+                explanationText = "This link opens in a new browser window";
+            }
+
             string link = string.Format(@"<a href=""{0}"" {1}><span class=""hidden-for-screen-reader"">{2}</span>{3}</a>", url, attributes.ToString(), explanationText, linkText);
 
             return new MvcHtmlString(link);
