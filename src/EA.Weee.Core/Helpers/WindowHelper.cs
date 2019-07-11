983a55f1e710e7f970650bdafc09bf4c82db17d6
Will Andrews <will7989@hotmail.com>
2019-07-11 07:54:13 +0100

    started working out what message to display

---
+++ src/EA.Weee.Core/Helpers/WindowHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Core.Helpers
+{
+    using EA.Prsd.Core;
+    using System;
+
+    public static class WindowHelper
+    {
+        public static bool IsWindowClosed()
+        {
+            DateTime closedPeriodStart = new DateTime(SystemTime.UtcNow.Year, 03, 17);
+            DateTime closedPeriodEnd = new DateTime(SystemTime.UtcNow.Year, 04, 01);
+
+            return SystemTime.UtcNow >= closedPeriodStart && SystemTime.UtcNow < closedPeriodEnd;
+        }
+    }
+}
