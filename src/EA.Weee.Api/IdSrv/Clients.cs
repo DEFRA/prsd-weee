b74e24e3693c2e208462f8475c6873d29c044314
JohnPadgettCivica <JohnPadgettCivica>
2019-07-11 11:10:14 +0100

    Task 73035:Bug: Alignment distorted when B2C/B2B is large Task 73030:Bug: AATF return summary screen> Text alignment Task 73029:Bug: Check your AATF return screen> text alignment

--- src/EA.Weee.Api/IdSrv/Clients.cs
+++ src/EA.Weee.Api/IdSrv/Clients.cs
@@ -30,7 +30,7 @@
                     {
                         new Secret(config.ApiSecret.Sha256())
                     },
-                    AccessTokenLifetime = 14400, // 3 hour,
+                    AccessTokenLifetime = 10805, // 3 hour,
                     AllowAccessToAllScopes = true
                 }
             };
