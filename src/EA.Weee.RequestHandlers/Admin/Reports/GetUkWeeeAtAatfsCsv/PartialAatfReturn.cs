601449ab52196794b5d5473008265e6991010cca
Stephen Spinks <stephen.spinks@civica.co.uk>
2019-07-17 10:44:44 +0100

    Generate the CSV

---
+++ src/EA.Weee.RequestHandlers/Admin/Reports/GetUkWeeeAtAatfsCsv/PartialAatfReturn.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Weee.RequestHandlers.Admin.Reports.GetUkWeeeAtAatfsCsv
+{
+    using System.Collections.Generic;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Domain.DataReturns;
+
+    public class PartialAatfReturn
+    {
+        public Quarter Quarter { get; set; }
+
+        public IList<WeeeObligatedData> ObligatedWeeeReceivedData { get; set; }
+
+        public IList<WeeeObligatedData> ObligatedWeeeReusedData { get; set; }
+
+        public IList<WeeeObligatedData> ObligatedWeeeSentOnData { get; set; }
+    }
+}
