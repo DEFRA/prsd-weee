f4f8fb84197d08286dbabddfe1882863611f778d
JohnPadgettCivica <JohnPadgettCivica>
2019-07-22 13:24:13 +0100

    Analytics updates

--- src/EA.Weee.Web/RazorHelpers/WeeeGds.EventTracking.cs
+++ src/EA.Weee.Web/RazorHelpers/WeeeGds.EventTracking.cs
@@ -10,8 +10,8 @@
         public MvcHtmlString CreateProgressiveDisclosure(string linkText, string hiddenContent, string eventCategory, string eventAction, string eventLabel)
         {
             return new MvcHtmlString(string.Format(
-                @"<details class=""govuk-details"" aria-live=""polite"" role=""group""><summary class=""govuk-details__summary""><span class=""govuk-details__summary-text"">{1}<span class=""hidden-for-screen-reader"">This is an expandable link that will reveal more content upon activation</span></span></summary><div class=""govuk-details__text""id=""details - content"" aria-hidden=""true""><div class=""govuk - body govuk - !-font - size - 16"">{2}</div></div></details>",
-                EventTrackingFunction(eventCategory, eventAction, eventLabel), linkText, hiddenContent));
+                @"<details class=""govuk-details"" aria-live=""polite"" role=""group"" onclick=""{0}""><summary class=""govuk-details__summary""><span class=""govuk-details__summary-text"">{1}<span class=""hidden-for-screen-reader"">This is an expandable link that will reveal more content upon activation</span></span></summary><div class=""govuk-details__text"" id=""details-content"" aria-hidden=""true""><div class=""govuk-body govuk-!-font-size-16"">{2}</div></div></details>",
+                 EventTrackingFunction(eventCategory, eventAction, eventLabel), linkText, hiddenContent));
         }
 
         public MvcHtmlString CreateButtonWithEventTracking(string buttonText, string eventCategory, string eventAction, string eventLabel)
