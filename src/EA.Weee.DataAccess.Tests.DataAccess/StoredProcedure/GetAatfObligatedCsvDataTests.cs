2b68b95253ff703077c74791b5b95b358908556e
SumeN <sumen@sfwltd.co.uk>
2019-07-26 16:48:25 +0100

    Sent on Weee report

--- src/EA.Weee.DataAccess.Tests.DataAccess/StoredProcedure/GetAatfObligatedCsvDataTests.cs
+++ src/EA.Weee.DataAccess.Tests.DataAccess/StoredProcedure/GetAatfObligatedCsvDataTests.cs
@@ -46,17 +46,17 @@
                 var data = from x in results.AsEnumerable()
                            where x.Field<string>("Name of AATF") == aatf.Name
                            select x;
-                data.AsQueryable().Count().Should().Be(4);
+                data.AsQueryable().Count().Should().Be(28);
 
                 var dataB2B = from x in results.AsEnumerable()
                               where x.Field<string>("Name of AATF") == aatf.Name && x.Field<string>("Obligation") == "B2B"
                               select x;
-                dataB2B.AsQueryable().Count().Should().Be(2);
+                dataB2B.AsQueryable().Count().Should().Be(14);
 
                 var dataB2C = from x in results.AsEnumerable()
                               where x.Field<string>("Name of AATF") == aatf.Name && x.Field<string>("Obligation") == "B2C"
                               select x;
-                dataB2C.AsQueryable().Count().Should().Be(2);
+                dataB2C.AsQueryable().Count().Should().Be(14);
             }
         }
 
