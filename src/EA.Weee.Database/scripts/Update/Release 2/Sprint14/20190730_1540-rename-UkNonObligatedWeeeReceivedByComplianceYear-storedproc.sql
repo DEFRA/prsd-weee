f5f3083b906d07a7ed5967080baa780457149b4e
JohnPadgettCivica <JohnPadgettCivica>
2019-07-30 15:43:13 +0100

    Try fix order of execution for database scripts causing build weee to fail

---
+++ src/EA.Weee.Database/scripts/Update/Release 2/Sprint14/20190730_1540-rename-UkNonObligatedWeeeReceivedByComplianceYear-storedproc.sql
@@ -0,0 +1,7 @@
+﻿IF OBJECT_ID('[AATF].getUkNonObligatedWeeeReceivedByComplianceYear', 'P') IS NOT NULL BEGIN
+	DROP PROCEDURE [AATF].[getUkNonObligatedWeeeReceivedByComplianceYear]
+END
+GO
+IF OBJECT_ID('[AATF].UkNonObligatedWeeeReceivedByComplianceYear', 'P') IS NOT NULL BEGIN
+	EXEC sp_rename '[AATF].UkNonObligatedWeeeReceivedByComplianceYear', 'getUkNonObligatedWeeeReceivedByComplianceYear'
+END
