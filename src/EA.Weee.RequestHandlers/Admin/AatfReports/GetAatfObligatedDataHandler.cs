ffc8884a29bfbfcf4b1edeae498357af3a63f4a4
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-07-30 17:15:06 +0100

    Altering file names

--- src/EA.Weee.RequestHandlers/Admin/AatfReports/GetAatfObligatedDataHandler.cs
+++ src/EA.Weee.RequestHandlers/Admin/AatfReports/GetAatfObligatedDataHandler.cs
@@ -65,9 +65,12 @@
                 }
             }
 
-            var fileName = string.Format("{0}-{1}{2}-{3:ddMMyyyy_HHmm}.csv",
-                 aatf.Name, request.ComplianceYear, (QuarterType)request.Quarter,
-                 DateTime.UtcNow);
+            var fileName = string.Format("{1}_{2}_{4}_Full data_{3:ddMMyyyy}_{3:HHmm}.csv",
+                 aatf.Name, 
+                 request.ComplianceYear, 
+                 (QuarterType)request.Quarter,
+                 DateTime.UtcNow,
+                 aatf.ApprovalNumber);
 
             string fileContent = DataTableCsvHelper.DataTableToCSV(obligatedData);
 
