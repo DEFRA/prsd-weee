6a8120be4c790a60519a804fc57af2719d116c17
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-08-05 14:44:50 +0100

    Start of editing home controller, and retrieving aatf lists

---
+++ src/EA.Weee.Web/Areas/AatfReturn/ViewModels/ViewAATFContactDetailsListViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.ViewModels
+{
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Web.ViewModels.Shared;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+
+    public class ViewAATFContactDetailsListViewModel : RadioButtonStringCollectionViewModel
+    {
+        [Required(ErrorMessage = "Select an activity")]
+        public override string SelectedValue { get; set; }
+
+        public Guid OrganisationId { get; set; }
+
+        public List<AatfData> AatfList { get; set; }
+
+        public ViewAATFContactDetailsListViewModel(List<string> activites) : base(activites)
+        {
+        }
+    }
+}
