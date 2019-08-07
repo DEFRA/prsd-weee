5a620b411944374530f7e49b4f306d1e30d2fa2b
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-08-07 17:22:14 +0100

    Added requests, handlers, and data access. Created index page for contact details

---
+++ src/EA.Weee.RequestHandlers/Aatf/GetAatfExternal/IGetAatfExternalDataAccess.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.RequestHandlers.Aatf.GetAatfExternal
+{
+    using EA.Weee.Domain.AatfReturn;
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IGetAatfExternalDataAccess
+    {
+        Task<Aatf> GetAatfById(Guid id);
+    }
+}
