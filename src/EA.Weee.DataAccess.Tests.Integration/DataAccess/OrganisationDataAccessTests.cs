8ae46a698f47c8dfb8cc13a96ad39d5a597902dc
JohnPadgettCivica <JohnPadgettCivica>
2019-08-08 16:45:57 +0100

    Organisation and aatf removed integration tests

--- src/EA.Weee.DataAccess.Tests.Integration/DataAccess/OrganisationDataAccessTests.cs
+++ src/EA.Weee.DataAccess.Tests.Integration/DataAccess/OrganisationDataAccessTests.cs
@@ -5,9 +5,13 @@
     using System.Collections.Generic;
     using System.Linq;
     using AutoFixture;
+    using Domain.AatfReturn;
+    using Domain.DataReturns;
+    using Domain.Organisation;
     using Domain.User;
     using FluentAssertions;
     using Weee.DataAccess.DataAccess;
+    using Weee.Tests.Core;
     using Weee.Tests.Core.Model;
     using Xunit;
 
@@ -41,6 +45,33 @@
 
                 databaseWrapper.WeeeContext.Organisations.Where(o => o.Id == organisation.Id).Should().BeEmpty();
                 databaseWrapper.WeeeContext.Organisations.Where(o => o.Id == organisationDontRemove.Id).Should().NotBeEmpty();
+            }
+        }
+
+        [Fact]
+        public async void DeleteOrganisation_GivenOrganisationWithOrganisationUsers_OrganisationAndOrganisationUsersShouldBeRemoved()
+        {
+            using (var databaseWrapper = new DatabaseWrapper())
+            {
+                var organisationDataAccess = new OrganisationDataAccess(databaseWrapper.WeeeContext);
+
+                var organisation = Domain.Organisation.Organisation.CreateSoleTrader(fixture.Create<string>());
+
+                databaseWrapper.WeeeContext.Organisations.Add(organisation);
+                await databaseWrapper.WeeeContext.SaveChangesAsync();
+
+                databaseWrapper.WeeeContext.OrganisationUsers.Add(
+                    new Domain.Organisation.OrganisationUser(Guid.Parse(databaseWrapper.Model.AspNetUsers.First().Id), organisation.Id, UserStatus.Active));
+
+                await databaseWrapper.WeeeContext.SaveChangesAsync();
+
+                databaseWrapper.WeeeContext.Organisations.Where(o => o.Id == organisation.Id).Should().NotBeEmpty();
+                databaseWrapper.WeeeContext.OrganisationUsers.Where(o => o.OrganisationId == organisation.Id).Should().NotBeEmpty();
+
+                await organisationDataAccess.Delete(organisation.Id);
+
+                databaseWrapper.WeeeContext.Organisations.Where(o => o.Id == organisation.Id).Should().BeEmpty();
+                databaseWrapper.WeeeContext.OrganisationUsers.Where(o => o.Id == organisation.Id).Should().BeEmpty();
             }
         }
 
@@ -126,6 +157,46 @@
                 await databaseWrapper.WeeeContext.SaveChangesAsync();
 
                 var result = await organisationDataAccess.HasActiveUsers(organisation.Id);
+
+                result.Should().BeFalse();
+            }
+        }
+
+        [Fact]
+        public async void HasReturns_GivenOrganisationHasReturnsForComplianceYear_TrueShouldBeReturned()
+        {
+            using (var databaseWrapper = new DatabaseWrapper())
+            {
+                var organisationDataAccess = new OrganisationDataAccess(databaseWrapper.WeeeContext);
+                var organisation = Domain.Organisation.Organisation.CreateSoleTrader(fixture.Create<string>());
+                var @return = ObligatedWeeeIntegrationCommon.CreateReturn(organisation, databaseWrapper.Model.AspNetUsers.First().Id,
+                    FacilityType.Aatf, 2019, QuarterType.Q1);
+
+                databaseWrapper.WeeeContext.Returns.Add(@return);
+
+                await databaseWrapper.WeeeContext.SaveChangesAsync();
+
+                var result = await organisationDataAccess.HasReturns(organisation.Id, 2019);
+
+                result.Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public async void HasReturns_GivenOrganisationDoesNotHaveReturnsForComplianceYear_FalseShouldBeReturned()
+        {
+            using (var databaseWrapper = new DatabaseWrapper())
+            {
+                var organisationDataAccess = new OrganisationDataAccess(databaseWrapper.WeeeContext);
+                var organisation = Domain.Organisation.Organisation.CreateSoleTrader(fixture.Create<string>());
+                var @return = ObligatedWeeeIntegrationCommon.CreateReturn(organisation, databaseWrapper.Model.AspNetUsers.First().Id,
+                    FacilityType.Aatf, 2020, QuarterType.Q1);
+
+                databaseWrapper.WeeeContext.Returns.Add(@return);
+
+                await databaseWrapper.WeeeContext.SaveChangesAsync();
+
+                var result = await organisationDataAccess.HasReturns(organisation.Id, 2019);
 
                 result.Should().BeFalse();
             }
