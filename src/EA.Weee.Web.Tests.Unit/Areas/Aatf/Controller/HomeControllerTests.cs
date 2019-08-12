0276e56869eac93271e9e3cfe2e954f40298c8e8
Sam Goodey <samuelg@sfwltd.co.uk>
2019-08-12 15:48:11 +0100

    Feature/view contact details external (#509)
    * Start of editing home controller, and retrieving aatf lists
    
    * Additional changes
    
    * Resolved routing issues, created new area for Aatf, create home controller as landing page
    
    * Added requests, handlers, and data access. Created index page for contact details
    
    * Finishing off changes and starting unit tests
    
    * Additional unit tests
    
    * Tweaks to breadcrumb service to support new banner, adjusted unit tests
    
    * Remove commented out code
    
    * Added view model mapper and unit tests
    
    * Removing redundant data access and reusing other data access
    
    * Added additional unit tests
    
    * Resolving some test issues
    
    * Reorganised functions in controller into mapper to help with testing and to clean up the controller

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Aatf/Controller/HomeControllerTests.cs
@@ -0,0 +1,175 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Aatf.Controller
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Core.Shared;
+    using EA.Weee.Requests.AatfReturn;
+    using EA.Weee.Web.Areas.Aatf.Controllers;
+    using EA.Weee.Web.Areas.Aatf.Mappings.ToViewModel;
+    using EA.Weee.Web.Areas.Aatf.ViewModels;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Controllers.Base;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using FakeItEasy;
+    using FluentAssertions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Xunit;
+
+    public class HomeControllerTests
+    {
+        private readonly IWeeeClient apiClient;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+        private readonly HomeController controller;
+        private readonly IMap<AatfDataToHomeViewModelMapTransfer, HomeViewModel> mapper;
+
+        public HomeControllerTests()
+        {
+            this.apiClient = A.Fake<IWeeeClient>();
+            this.breadcrumb = A.Fake<BreadcrumbService>();
+            this.cache = A.Fake<IWeeeCache>();
+            this.mapper = A.Fake<IMap<AatfDataToHomeViewModelMapTransfer, HomeViewModel>>();
+
+            controller = new HomeController(cache, breadcrumb, () => apiClient, mapper);
+        }
+
+        [Fact]
+        public void HomeControllerInheritsExternalSiteController()
+        {
+            typeof(HomeController).BaseType.Name.Should().Be(typeof(ExternalSiteController).Name);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenValidViewModel_BreadcrumbShouldBeSet()
+        {
+            var organisationName = "Organisation";
+            var model = new HomeViewModel()
+            {
+                AatfList = A.Fake<List<AatfData>>()
+            };
+
+            A.CallTo(() => mapper.Map(A<AatfDataToHomeViewModelMapTransfer>._)).Returns(model);
+            A.CallTo(() => cache.FetchOrganisationName(A<Guid>._)).Returns(organisationName);
+
+            await controller.Index(A.Dummy<Guid>(), false);
+
+            breadcrumb.ExternalOrganisation.Should().Be(organisationName);
+        }
+
+        [Fact]
+        public async void IndexPost_GivenInValidViewModel_BreadcrumbShouldBeSet()
+        {
+            var organisationName = "organisation";
+            controller.ModelState.AddModelError("error", "error");
+
+            A.CallTo(() => cache.FetchOrganisationName(A<Guid>._)).Returns(organisationName);
+
+            await controller.Index(new HomeViewModel());
+
+            breadcrumb.ExternalOrganisation.Should().Be(organisationName);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenAction_DefaultViewShouldBeReturned()
+        {
+            var model = new HomeViewModel()
+            {
+                AatfList = A.Fake<List<AatfData>>()
+            };
+
+            A.CallTo(() => mapper.Map(A<AatfDataToHomeViewModelMapTransfer>._)).Returns(model);
+
+            var result = await controller.Index(A.Dummy<Guid>(), false) as ViewResult;
+
+            result.ViewName.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async void IndexGet_GivenOrganisationId_ApiShouldBeCalled()
+        {
+            var organisationId = Guid.NewGuid();
+            var model = new HomeViewModel()
+            {
+                AatfList = A.Fake<List<AatfData>>()
+            };
+
+            A.CallTo(() => mapper.Map(A<AatfDataToHomeViewModelMapTransfer>._)).Returns(model);
+
+            await controller.Index(organisationId, false);
+
+            A.CallTo(() => apiClient.SendAsync(A<string>._, A<GetAatfByOrganisation>.That.Matches(w => w.OrganisationId == organisationId))).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public async void IndexGet_GivenOrganisationId_MapperShouldBeCalled()
+        {
+            var organisationId = Guid.NewGuid();
+            var model = new HomeViewModel()
+            {
+                AatfList = A.Fake<List<AatfData>>()
+            };
+
+            A.CallTo(() => mapper.Map(A<AatfDataToHomeViewModelMapTransfer>._)).Returns(model);
+
+            await controller.Index(organisationId, false);
+
+            A.CallTo(() => mapper.Map(A<AatfDataToHomeViewModelMapTransfer>.That.Matches(a => a.IsAE == false && a.OrganisationId == organisationId))).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public async void IndexGet_OnSingleAatf_PageRedirectsContactDetails()
+        {
+            var organisationId = Guid.NewGuid();
+            var aatfList = new List<AatfData>();
+
+            var aatfData = new AatfData(Guid.NewGuid(), "AATF", "approval number", 2019, A.Dummy<Core.Shared.UKCompetentAuthorityData>(),
+                   Core.AatfReturn.AatfStatus.Approved, A.Dummy<AatfAddressData>(), Core.AatfReturn.AatfSize.Large, DateTime.Now,
+                   A.Dummy<Core.Shared.PanAreaData>(), null)
+            {
+                FacilityType = FacilityType.Aatf
+            };
+
+            aatfList.Add(aatfData);
+
+            var model = new HomeViewModel()
+            {
+                AatfList = aatfList
+            };
+
+            A.CallTo(() => apiClient.SendAsync(A<string>._, A<GetAatfByOrganisation>._)).Returns(aatfList);
+            A.CallTo(() => mapper.Map(A<AatfDataToHomeViewModelMapTransfer>._)).Returns(model);
+
+            var result = await controller.Index(organisationId, false) as RedirectToRouteResult;
+
+            result.RouteValues["action"].Should().Be("Index");
+            result.RouteValues["controller"].Should().Be("ViewAatfContactDetails");
+            result.RouteValues["organisationId"].Should().Be(organisationId);
+        }
+
+        [Fact]
+        public async void IndexPost_ValidViewModel_PageRedirectsContactDetails()
+        {
+            var model = new HomeViewModel()
+            {
+                OrganisationId = Guid.NewGuid(),
+                SelectedAatfId = Guid.NewGuid(),
+                IsAE = false
+            };
+
+            var result = await controller.Index(model) as RedirectToRouteResult;
+
+            result.RouteValues["action"].Should().Be("Index");
+            result.RouteValues["controller"].Should().Be("ViewAatfContactDetails");
+            result.RouteValues["organisationId"].Should().Be(model.OrganisationId);
+            result.RouteValues["aatfId"].Should().Be(model.SelectedAatfId);
+            result.RouteValues["isAE"].Should().Be(model.IsAE);
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Aatf/AatfAreaRegistration.cs
@@ -0,0 +1,29 @@
+﻿namespace EA.Weee.Web.Areas.Aatf
+{
+    using System.Security.Policy;
+    using Aatf.Controllers;
+    using Infrastructure;
+    using System.Web.Mvc;
+
+    public class AatfAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Aatf";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.Routes.MapMvcAttributeRoutes();
+
+            context.MapLowercaseDashedRoute(
+                name: "Aatf_default",
+                url: "Aatf/{organisationId}/{controller}/{action}/",
+                defaults: new { action = "Index", controller = "Home"},
+                namespaces: new[] { typeof(HomeController).Namespace });
+
+            context.MapLowercaseDashedRoute(
+                name: "Aatf_ContactDetails",
+                url: "Aatf/{organisationId}/{controller}/{action}/{aatfId}",
+                defaults: new { action = "Index", controller = "ViewAatfContactDetails" },
+                namespaces: new[] { typeof(ViewAatfContactDetailsController).Namespace });
+        }
+    }
+}
