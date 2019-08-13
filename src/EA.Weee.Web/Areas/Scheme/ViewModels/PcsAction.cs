0425326b5c00f2aabb1c2283e616bd0bf8129278
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-08-13 11:35:59 +0100

    Adding in custom validation for home view model, bug fixes

--- src/EA.Weee.Web/Areas/Scheme/ViewModels/PcsAction.cs
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/PcsAction.cs
@@ -7,7 +7,7 @@
         public static string ViewSubmissionHistory = "View PCS submissions history";
         public static string ViewRegisteredOfficeDetails = "View registered office details";
         public static string ViewPrinciplePlaceOfBusinessDetails = "View principal place of business";
-        public static string ManageContactDetails = "Change PCS contact details";
+        public static string ManageContactDetails = "Manage PCS contact details";
         public static string ManageOrganisationUsers = "Manage organisation users";
         public static string ManageAatfReturns = "Manage AATF returns";
         public static string ManageAeReturns = "Manage AE returns";
