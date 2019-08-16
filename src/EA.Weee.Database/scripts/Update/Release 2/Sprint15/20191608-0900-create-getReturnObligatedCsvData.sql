d515ff1b61d77b35d5eae7439644d6293efb77a9
JohnPadgettCivica <JohnPadgettCivica>
2019-08-16 09:49:27 +0100

    Add empty getReturnObligatedCsvData script file

---
+++ src/EA.Weee.Database/scripts/Update/Release 2/Sprint15/20191608-0900-create-getReturnObligatedCsvData.sql
@@ -0,0 +1,25 @@
+﻿IF OBJECT_ID('[AATF].getReturnObligatedCsvData', 'P') IS NOT NULL BEGIN
+	DROP PROCEDURE [AATF].[getReturnObligatedCsvData]
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- Description:	This stored procedure is used to provide the data for the admin report of obligatde data
+--				that have/haven't submitted a data return within
+--				the limits of the specified parameters.Get the latest submitted return
+
+-- =============================================
+CREATE PROCEDURE [AATF].[getReturnObligatedCsvData]
+	@ReturnId UNIQUEIDENTIFIER
+AS
+BEGIN
+
+SET NOCOUNT ON;
+
+
+
+
+END
