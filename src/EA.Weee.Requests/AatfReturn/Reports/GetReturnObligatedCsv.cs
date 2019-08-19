813f23340de920d9e79c74b5bc0e33589b859abe
JohnPadgettCivica <JohnPadgettCivica>
2019-08-19 10:36:53 +0100

    Add GetReturnObligatedCsvHandler handler

---
+++ src/EA.Weee.Requests/AatfReturn/Reports/GetReturnObligatedCsv.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Requests.AatfReturn.Reports
+{
+    using System;
+    using Core.Admin;
+    using Prsd.Core.Mediator;
+
+    public class GetReturnObligatedCsv : IRequest<CSVFileData>
+    {
+        public Guid ReturnId { get; private set; }
+
+        public GetReturnObligatedCsv(Guid returnId)
+        {
+            ReturnId = returnId;
+        }
+    }
+}
