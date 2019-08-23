575f0b3f2241b1ab096d16a467e01ac5752cee1b
Will Andrews <will7989@hotmail.com>
2019-08-23 09:48:53 +0100

    front end completed

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/AatfReports/AatfAeDetailsViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.AatfReports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public class AatfAeDetailsViewModel
+    {
+        [Required]
+        [DisplayName("Compliance year")]
+        public int SelectedYear { get; set; }
+
+        public IEnumerable<SelectListItem> ComplianceYears { get; set; }
+
+        [Required(ErrorMessage = "Enter AATF or AE")]
+        [Display(Name = "AATF or AE")]
+        public string SelectedFacilityType { get; set; }
+
+        public IEnumerable<SelectListItem> FacilityTypes { get; set; }
+
+        [Display(Name = "Appropriate authority")]
+        public Guid? CompetentAuthorityId { get; set; }
+
+        public IEnumerable<SelectListItem> CompetentAuthoritiesList { get; set; }
+
+        [Display(Name = "WROS Pan Area Team")]
+        public Guid? PanAreaId { get; set; }
+        public IEnumerable<SelectListItem> PanAreaList { get; set; }
+
+        [Display(Name = "EA area")]
+        public Guid? LocalAreaId { get; set; }
+
+        public IEnumerable<SelectListItem> LocalAreaList { get; set; }
+    }
+}
