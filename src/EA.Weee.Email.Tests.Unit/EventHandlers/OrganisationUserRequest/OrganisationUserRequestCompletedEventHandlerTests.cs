308c58e48ec9903764b9332e1616caf8454423bb
Samuel Goodey <SamuelG@sfwltd.co.uk>
2019-08-23 14:04:00 +0100

    Made changes based on code review comments

---
+++ src/EA.Weee.Email.Tests.Unit/EventHandlers/OrganisationUserRequest/OrganisationUserRequestCompletedEventHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿namespace EA.Weee.Email.Tests.Unit.EventHandlers.OrganisationUserRequest
+{
+    using EA.Weee.Domain.Events;
+    using EA.Weee.Domain.Organisation;
+    using EA.Weee.Email.EventHandlers;
+    using FakeItEasy;
+    using FluentAssertions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class OrganisationUserRequestCompletedEventHandlerTests
+    {
+        private IWeeeEmailService emailService;
+        private readonly IOrganisationUserRequestEventHandlerDataAccess dataAccess;
+        private OrganisationUserRequestCompletedEventHandler handler;
+
+        public OrganisationUserRequestCompletedEventHandlerTests()
+        {
+            this.emailService = A.Fake<IWeeeEmailService>();
+            this.dataAccess = A.Fake<IOrganisationUserRequestEventHandlerDataAccess>();
+
+            this.handler = new OrganisationUserRequestCompletedEventHandler(dataAccess, emailService);
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenRequest_DataAccessIsCalled()
+        {
+            var request = A.Dummy<OrganisationUserRequestCompletedEvent>();
+
+            await handler.HandleAsync(request);
+
+            A.CallTo(() => dataAccess.FetchActiveOrganisationUsers(request.OrganisationUser.OrganisationId)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task HandleAsync_GivenRequest_SendOrganisationUserRequestCompletedIsCalledWithCorrectParameters(bool activeUsers)
+        {
+            var request = A.Dummy<OrganisationUserRequestCompletedEvent>();
+            var activeOrganisationUsers = new List<OrganisationUser>();
+
+            if (activeUsers)
+            {
+                activeOrganisationUsers.Add(A.Fake<OrganisationUser>());
+            }
+
+            A.CallTo(() => dataAccess.FetchActiveOrganisationUsers(request.OrganisationUser.OrganisationId)).Returns(activeOrganisationUsers);
+
+            await handler.HandleAsync(request);
+
+            A.CallTo(() => emailService.SendOrganisationUserRequestCompleted(request.OrganisationUser, activeUsers)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+    }
+}
