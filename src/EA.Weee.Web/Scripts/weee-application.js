c1502da1c95bb4793070aaec25cf655a2f80abb8
JohnPadgettCivica <JohnPadgettCivica>
2019-08-28 12:01:09 +0100

    Changes for yellow tab focus on pcs tab

--- src/EA.Weee.Web/Scripts/weee-application.js
+++ src/EA.Weee.Web/Scripts/weee-application.js
@@ -110,6 +110,18 @@
 	    }
     );
 
+    function getUrlParameter(name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        var results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    }
+
+    var parameter = getUrlParameter("clickedTab");
+    if (parameter !== "") {
+        $("#" + parameter).focus();
+        console.log(parameter);
+    }
 });
 
 
