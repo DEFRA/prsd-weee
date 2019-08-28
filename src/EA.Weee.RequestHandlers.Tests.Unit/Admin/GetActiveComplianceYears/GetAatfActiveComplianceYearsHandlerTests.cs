efe52b469399d4c9537404f7fa68183916cb3c12
Will Andrews <will7989@hotmail.com>
2019-08-28 12:40:25 +0100

    new handler to get active compliance years currently used by aatfs/aes

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/GetActiveComplianceYears/GetAatfActiveComplianceYearsHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Admin.GetActiveComplianceYears
+{
+    using FakeItEasy;
+    using RequestHandlers.Admin.GetActiveComplianceYears;
+    using RequestHandlers.Security;
+    using Requests.Admin.GetActiveComplianceYears;
+    using System.Security;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class GetAatfActiveComplianceYearsHandlerTests
+    {
+        private readonly IGetAatfReturnsActiveComplianceYearsDataAccess dataAccess;
+        private readonly IWeeeAuthorization authorization;
+
+        public GetAatfActiveComplianceYearsHandlerTests()
+        {
+            authorization = A.Fake<IWeeeAuthorization>();
+            dataAccess = A.Fake<IGetAatfReturnsActiveComplianceYearsDataAccess>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_UserIsNotAuthorized_ThrowsSecurityException_AndDoesNotUseDataAccess()
+        {
+            A.CallTo(() => authorization.EnsureCanAccessInternalArea()).Throws<SecurityException>();
+
+            await Assert.ThrowsAsync<SecurityException>(() => Handler().HandleAsync(A.Dummy<GetAatfAeActiveComplianceYears>()));
+
+            A.CallTo(() => dataAccess.Get()).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task HandleAsync_UserIsAuthorized_UsesDataAccess()
+        {
+            await Handler().HandleAsync(A.Dummy<GetAatfAeActiveComplianceYears>());
+
+            A.CallTo(() => authorization.EnsureCanAccessInternalArea()).MustHaveHappenedOnceExactly();
+            A.CallTo(() => dataAccess.GetAatfAe()).MustHaveHappenedOnceExactly();
+        }
+
+        private GetAatfAeActiveComplianceYearsHandler Handler()
+        {
+            return new GetAatfAeActiveComplianceYearsHandler(authorization, dataAccess);
+        }
+    }
+}
