df49f42c08bff9c6d690bc5acbbf1e13296c79a8
JohnPadgettCivica <JohnPadgettCivica>
2019-09-04 13:25:18 +0100

    Fix issue with empty scheme name and add year to aatf/ae name on organisation details

---
+++ src/EA.Weee.Core.Tests.Unit/AatfReturn/AatfDataListTests.cs
@@ -0,0 +1,40 @@
+﻿namespace EA.Weee.Core.Tests.Unit.AatfReturn
+{
+    using System;
+    using AutoFixture;
+    using Core.AatfReturn;
+    using Core.Organisations;
+    using Core.Shared;
+    using FluentAssertions;
+    using Xunit;
+
+    public class AatfDataListTests
+    {
+        private readonly Fixture fixture;
+
+        public AatfDataListTests()
+        {
+            fixture = new Fixture();
+        }
+
+        [Fact]
+        public void NameWithComplianceYear_GivenNameAndComplianceYear_PropertyShouldBeCorrect()
+        {
+            var complianceYear = fixture.Create<short>();
+            var name = fixture.Create<string>();
+
+            var model = new AatfDataList(fixture.Create<Guid>(),
+                name,
+                fixture.Create<UKCompetentAuthorityData>(),
+                fixture.Create<string>(),
+                fixture.Create<AatfStatus>(),
+                fixture.Create<OrganisationData>(),
+                fixture.Create<FacilityType>(),
+                complianceYear,
+                fixture.Create<Guid>(),
+                fixture.Create<DateTime>());
+
+            model.NameWithComplianceYear.Should().Be($"{name} ({complianceYear})");
+        }
+    }
+}
