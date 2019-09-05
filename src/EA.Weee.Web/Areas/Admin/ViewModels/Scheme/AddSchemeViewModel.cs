b652ff736fe3dc304abb45de6c64b0cdb2227de6
Will Andrews <will7989@hotmail.com>
2019-09-05 10:32:24 +0100

    bugs completed as well as saving contact and address data when creating a scheme

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Scheme/AddSchemeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Scheme
+{
+    using EA.Weee.Core.Organisations;
+    using EA.Weee.Core.Shared;
+
+    public class AddSchemeViewModel : SchemeViewModelBase
+    {
+        public AddressData OrganisationAddress { get; set; }
+
+        public ContactData Contact { get; set; }
+    }
+}
