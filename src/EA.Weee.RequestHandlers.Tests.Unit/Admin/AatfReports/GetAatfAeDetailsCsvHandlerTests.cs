e6bf74b2838ac2076a262892aded77f0657dcf8d
SumeN <sumen@sfwltd.co.uk>
2019-09-09 14:02:05 +0100

    Add PCS details to aatf ae report

--- src/EA.Weee.RequestHandlers.Tests.Unit/Admin/AatfReports/GetAatfAeDetailsCsvHandlerTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/AatfReports/GetAatfAeDetailsCsvHandlerTests.cs
@@ -7,6 +7,7 @@
     using DataAccess.StoredProcedure;
     using Domain.Lookup;
     using EA.Weee.Core.Admin;
+    using EA.Weee.Core.Admin.AatfReports;
     using EA.Weee.Domain;
     using EA.Weee.RequestHandlers.Security;
     using FakeItEasy;
@@ -53,7 +54,7 @@
             IWeeeAuthorization authorization = new AuthorizationBuilder().DenyInternalAreaAccess().Build();
 
             GetAatfAeDetailsCsvHandler handler = new GetAatfAeDetailsCsvHandler(authorization, context, csvWriterFactory, commonDataAccess);
-            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<FacilityType>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<bool>());
+            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<ReportFacilityType>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<bool>());
 
             Func<Task> action = async () => await handler.HandleAsync(request);
 
@@ -65,7 +66,7 @@
         {
             const int complianceYear = 0;
 
-            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(complianceYear, fixture.Create<FacilityType>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<bool>());
+            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(complianceYear, fixture.Create<ReportFacilityType>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<bool>());
 
             Func<Task> action = async () => await handler.HandleAsync(request);
 
@@ -75,7 +76,7 @@
         [Fact]
         public async Task HandleAsync_VariousParameters_ReturnsFileContent()
         {
-            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<FacilityType>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<bool>());
+            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<ReportFacilityType>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<Guid>(), fixture.Create<bool>());
 
             CSVFileData data = await handler.HandleAsync(request);
 
@@ -86,7 +87,7 @@
         public async Task HandleAsync_GivenStoredProcedureReturnItems_MatchingFileContent()
         {
             int complianceYear = fixture.Create<int>();
-            FacilityType facilityType = fixture.Create<FacilityType>();
+            ReportFacilityType facilityType = fixture.Create<ReportFacilityType>();
             Guid authority = fixture.Create<Guid>();
             Guid area = fixture.Create<Guid>();
             Guid pat = fixture.Create<Guid>();
@@ -107,15 +108,15 @@
             string facilityTypeString = facilityType.ToString().ToUpper();
 
             data.FileContent.Should().Contain($"Compliance year,Appropriate authority,WROS Pan Area Team,EA Area,Name of {facilityTypeString},{facilityTypeString} address1,{facilityTypeString} address2,{facilityTypeString} town or city,{facilityTypeString} county or region,{facilityTypeString} country,{facilityTypeString} postcode,{facilityTypeString} approval number,Date of approval,{facilityTypeString} size,{facilityTypeString} status,Contact name,Contact position,Contact address1,Contact address2,Contact town or city,Contact county or region,Contact country,Contact postcode,Contact email,Contact phone number,Organisation name,Organisation address1,Organisation address2,Organisation town or city,Organisation county or region,Organisation country,Organisation postcode");
-            data.FileContent.Should().Contain($"{csvData1.ComplianceYear},{csvData1.AppropriateAuthorityAbbr},{csvData1.PanAreaTeam},{csvData1.EaArea},{csvData1.Name},{csvData1.Address1},{csvData1.Address2},{csvData1.TownCity},{csvData1.CountyRegion},{csvData1.Country},{csvData1.PostCode},{csvData1.ApprovalNumber},{csvData1.ApprovalDateString},{csvData1.Size},{csvData1.Status},{csvData1.ContactName},{csvData1.ContactPosition},{csvData1.ContactAddress1},{csvData1.ContactAddress2},{csvData1.ContactTownCity},{csvData1.ContactCountyRegion},{csvData1.ContactCountry},{csvData1.ContactPostcode},{csvData1.ContactEmail},{csvData1.ContactPhone},{csvData1.OrganisationName},{csvData1.OrganisationAddress1},{csvData1.OrganisationAddress2},{csvData1.OrganisationTownCity},{csvData1.OrganisationCountyRegion},{csvData1.OrganisationCountry},{csvData1.OrganisationPostcode}");
-            data.FileContent.Should().Contain($"{csvData2.ComplianceYear},{csvData2.AppropriateAuthorityAbbr},{csvData2.PanAreaTeam},{csvData2.EaArea},{csvData2.Name},{csvData2.Address1},{csvData2.Address2},{csvData2.TownCity},{csvData2.CountyRegion},{csvData2.Country},{csvData2.PostCode},{csvData2.ApprovalNumber},{csvData2.ApprovalDateString},{csvData2.Size},{csvData2.Status},{csvData2.ContactName},{csvData2.ContactPosition},{csvData2.ContactAddress1},{csvData2.ContactAddress2},{csvData2.ContactTownCity},{csvData2.ContactCountyRegion},{csvData2.ContactCountry},{csvData2.ContactPostcode},{csvData2.ContactEmail},{csvData2.ContactPhone},{csvData2.OrganisationName},{csvData2.OrganisationAddress1},{csvData2.OrganisationAddress2},{csvData2.OrganisationTownCity},{csvData2.OrganisationCountyRegion},{csvData2.OrganisationCountry},{csvData2.OrganisationPostcode}");
-            data.FileContent.Should().Contain($"{csvData3.ComplianceYear},{csvData3.AppropriateAuthorityAbbr},{csvData3.PanAreaTeam},{csvData3.EaArea},{csvData3.Name},{csvData3.Address1},{csvData3.Address2},{csvData3.TownCity},{csvData3.CountyRegion},{csvData3.Country},{csvData3.PostCode},{csvData3.ApprovalNumber},{csvData3.ApprovalDateString},{csvData3.Size},{csvData3.Status},{csvData3.ContactName},{csvData3.ContactPosition},{csvData3.ContactAddress1},{csvData3.ContactAddress2},{csvData3.ContactTownCity},{csvData3.ContactCountyRegion},{csvData3.ContactCountry},{csvData3.ContactPostcode},{csvData3.ContactEmail},{csvData3.ContactPhone},{csvData3.OrganisationName},{csvData3.OrganisationAddress1},{csvData3.OrganisationAddress2},{csvData3.OrganisationTownCity},{csvData3.OrganisationCountyRegion},{csvData3.OrganisationCountry},{csvData3.OrganisationPostcode}");
+            data.FileContent.Should().Contain($"{csvData1.ComplianceYear},{csvData1.AppropriateAuthorityAbbr},{csvData1.PanAreaTeam},{csvData1.EaArea},{csvData1.Name},{csvData1.Address1},{csvData1.Address2},{csvData1.TownCity},{csvData1.CountyRegion},{csvData1.Country},{csvData1.PostCode},{csvData1.ApprovalNumber},{csvData1.ApprovalDateString},{csvData1.Size},{csvData1.Status},{csvData1.FirstName},{csvData1.ContactPosition},{csvData1.ContactAddress1},{csvData1.ContactAddress2},{csvData1.ContactTownCity},{csvData1.ContactCountyRegion},{csvData1.ContactCountry},{csvData1.ContactPostcode},{csvData1.ContactEmail},{csvData1.ContactPhone},{csvData1.OrganisationName},{csvData1.OrganisationAddress1},{csvData1.OrganisationAddress2},{csvData1.OrganisationTownCity},{csvData1.OrganisationCountyRegion},{csvData1.OrganisationCountry},{csvData1.OrganisationPostcode}");
+            data.FileContent.Should().Contain($"{csvData2.ComplianceYear},{csvData2.AppropriateAuthorityAbbr},{csvData2.PanAreaTeam},{csvData2.EaArea},{csvData2.Name},{csvData2.Address1},{csvData2.Address2},{csvData2.TownCity},{csvData2.CountyRegion},{csvData2.Country},{csvData2.PostCode},{csvData2.ApprovalNumber},{csvData2.ApprovalDateString},{csvData2.Size},{csvData2.Status},{csvData2.FirstName},{csvData2.ContactPosition},{csvData2.ContactAddress1},{csvData2.ContactAddress2},{csvData2.ContactTownCity},{csvData2.ContactCountyRegion},{csvData2.ContactCountry},{csvData2.ContactPostcode},{csvData2.ContactEmail},{csvData2.ContactPhone},{csvData2.OrganisationName},{csvData2.OrganisationAddress1},{csvData2.OrganisationAddress2},{csvData2.OrganisationTownCity},{csvData2.OrganisationCountyRegion},{csvData2.OrganisationCountry},{csvData2.OrganisationPostcode}");
+            data.FileContent.Should().Contain($"{csvData3.ComplianceYear},{csvData3.AppropriateAuthorityAbbr},{csvData3.PanAreaTeam},{csvData3.EaArea},{csvData3.Name},{csvData3.Address1},{csvData3.Address2},{csvData3.TownCity},{csvData3.CountyRegion},{csvData3.Country},{csvData3.PostCode},{csvData3.ApprovalNumber},{csvData3.ApprovalDateString},{csvData3.Size},{csvData3.Status},{csvData3.FirstName},{csvData3.ContactPosition},{csvData3.ContactAddress1},{csvData3.ContactAddress2},{csvData3.ContactTownCity},{csvData3.ContactCountyRegion},{csvData3.ContactCountry},{csvData3.ContactPostcode},{csvData3.ContactEmail},{csvData3.ContactPhone},{csvData3.OrganisationName},{csvData3.OrganisationAddress1},{csvData3.OrganisationAddress2},{csvData3.OrganisationTownCity},{csvData3.OrganisationCountyRegion},{csvData3.OrganisationCountry},{csvData3.OrganisationPostcode}");
         }
 
         [Fact]
         public async Task HandleAsync_GivenMandatoryParameters_FileNameShouldBeCorrect()
         {
-            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<FacilityType>(), null, null, null, false);
+            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<ReportFacilityType>(), null, null, null, false);
 
             DateTime date = new DateTime(2019, 05, 18, 11, 12, 0);
 
@@ -133,7 +134,7 @@
         {
             UKCompetentAuthority ca = fixture.Create<UKCompetentAuthority>();
 
-            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<FacilityType>(), ca.Id, null, null, false);
+            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<ReportFacilityType>(), ca.Id, null, null, false);
 
             A.CallTo(() => commonDataAccess.FetchCompetentAuthorityById(request.AuthorityId.Value)).Returns(ca);
 
@@ -151,7 +152,7 @@
         [Fact]
         public async Task HandleAsync_GivenMandatoryParametersAndPanArea_FileNameShouldBeCorrect()
         {
-            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<FacilityType>(), null, fixture.Create<Guid>(), null, false);
+            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<ReportFacilityType>(), null, fixture.Create<Guid>(), null, false);
 
             PanArea panArea = fixture.Create<PanArea>();
             A.CallTo(() => commonDataAccess.FetchLookup<PanArea>(request.PanArea.Value)).Returns(panArea);
@@ -170,7 +171,7 @@
         [Fact]
         public async Task HandleAsync_GivenMandatoryParametersAndLocalArea_FileNameShouldNotContainLocalArea()
         {
-            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<FacilityType>(), null, null, fixture.Create<Guid>(), false);
+            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<ReportFacilityType>(), null, null, fixture.Create<Guid>(), false);
 
             LocalArea localArea = fixture.Create<LocalArea>();
             A.CallTo(() => commonDataAccess.FetchLookup<LocalArea>(request.LocalArea.Value)).Returns(localArea);
@@ -193,7 +194,7 @@
             LocalArea localArea = fixture.Create<LocalArea>();
             PanArea panArea = fixture.Create<PanArea>();
 
-            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<FacilityType>(), ca.Id, panArea.Id, localArea.Id, false);
+            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<ReportFacilityType>(), ca.Id, panArea.Id, localArea.Id, false);
 
             A.CallTo(() => commonDataAccess.FetchCompetentAuthorityById(request.AuthorityId.Value)).Returns(ca);
 
@@ -216,7 +217,7 @@
         public async Task HandleAsyncForPublicRegister_GivenStoredProcedureReturnItems_MatchingFileContent()
         {
             int complianceYear = fixture.Create<int>();
-            FacilityType facilityType = fixture.Create<FacilityType>();
+            ReportFacilityType facilityType = fixture.Create<ReportFacilityType>();
             Guid authority = fixture.Create<Guid>();
 
             AatfAeDetailsData csvData1 = CreateCsvData();
@@ -245,7 +246,7 @@
         {
             UKCompetentAuthority ca = fixture.Create<UKCompetentAuthority>();
 
-            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<FacilityType>(), ca.Id, null, null, true);
+            GetAatfAeDetailsCsv request = new GetAatfAeDetailsCsv(fixture.Create<int>(), fixture.Create<ReportFacilityType>(), ca.Id, null, null, true);
 
             A.CallTo(() => commonDataAccess.FetchCompetentAuthorityById(request.AuthorityId.Value)).Returns(ca);
 
@@ -279,7 +280,8 @@
                 ApprovalDate = fixture.Create<DateTime>(),
                 Size = fixture.Create<string>(),
                 Status = fixture.Create<string>(),
-                ContactName = fixture.Create<string>(),
+                FirstName = fixture.Create<string>(),
+                LastName = fixture.Create<string>(),
                 ContactPosition = fixture.Create<string>(),
                 ContactAddress1 = fixture.Create<string>(),
                 ContactAddress2 = fixture.Create<string>(),
