37381d03d9c66cb4d6dc7a5144bf8979719675b2
SumeN <sumen@sfwltd.co.uk>
2019-09-11 16:33:43 +0100

    Add Tests. Correct merge result.

--- src/EA.Weee.DataAccess.Tests.DataAccess/StoredProcedure/GetAatfAeDetailsCsvTests.cs
+++ src/EA.Weee.DataAccess.Tests.DataAccess/StoredProcedure/GetAatfAeDetailsCsvTests.cs
@@ -1,20 +1,21 @@
 ﻿namespace EA.Weee.DataAccess.Tests.DataAccess.StoredProcedure
 {
-    using EA.Weee.DataAccess.StoredProcedure;
-    using EA.Weee.Domain.AatfReturn;
-    using EA.Weee.Tests.Core;
-    using EA.Weee.Tests.Core.Model;
-    using FluentAssertions;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using AutoFixture;
-    using Domain;
+    using EA.Weee.Core.Shared;
+    using EA.Weee.DataAccess.StoredProcedure;
+    using EA.Weee.Domain.AatfReturn;
+    using EA.Weee.Tests.Core;
+    using EA.Weee.Tests.Core.Model;
+    using FluentAssertions;
     using Xunit;
     using Aatf = Domain.AatfReturn.Aatf;
     using Address = Domain.Organisation.Address;
     using Organisation = Domain.Organisation.Organisation;
+    using Scheme = Domain.Scheme.Scheme;
 
     public class GetAatfAeDetailsCsvTests
     {
@@ -97,6 +98,42 @@
             }
         }
 
+        [Fact]
+        public async Task Execute_GivenPCSData_ReturnsData()
+        {
+            using (var db = new DatabaseWrapper())
+            {
+                await CreateScheme(db);
+                var results = await db.StoredProcedures.GetAatfAeDetailsCsvData(2019, 3, null, null, null);
+
+                VerifyPCSRecord(results);
+            }
+        }
+
+        [Fact]
+        public async Task Execute_GivenAllData_ReturnsData()
+        {
+            using (var db = new DatabaseWrapper())
+            {
+                await CreateScheme(db);
+
+                var org = OrgWithAddress(db);
+                var ae = ObligatedWeeeIntegrationCommon.CreateAe(db, org);
+                var aatf = ObligatedWeeeIntegrationCommon.CreateAatf(db, org, 2019, false, false, fixture.Create<string>());
+                var aatfs = new List<Aatf>() { ae, aatf };
+
+                db.WeeeContext.Organisations.Add(org);
+                db.WeeeContext.Aatfs.Add(ae);
+                db.WeeeContext.Aatfs.Add(aatf);
+                await db.WeeeContext.SaveChangesAsync();
+
+                var results = await db.StoredProcedures.GetAatfAeDetailsCsvData(2019, 4, null, null, null);
+
+                VerifyResult(results, aatfs);
+                VerifyPCSRecord(results);
+            }
+        }
+
         private void VerifyResult(List<AatfAeDetailsData> results, List<Aatf> aatfs)
         {
             foreach (var verifyAatf in aatfs)
@@ -140,6 +177,10 @@
                 aatf.OperatorTradingName.Should().Be(verifyAatf.Organisation.TradingName);
                 aatf.AatfAddress.Should().Be(GetSiteAddressString(verifyAatf.SiteAddress));
                 aatf.OperatorAddress.Should().Be(GetOrganisationAddressString(verifyAatf.Organisation.BusinessAddress));
+                aatf.CompanyRegistrationNumber.Should().Be(verifyAatf.Organisation.CompanyRegistrationNumber);
+                aatf.OrganisationTelephone.Should().Be(verifyAatf.Organisation.BusinessAddress.Telephone);
+                aatf.OrganisationEmail.Should().Be(verifyAatf.Organisation.BusinessAddress.Email);
+                aatf.RecordType.Equals(verifyAatf.FacilityType.DisplayName);
             }
         }
 
@@ -185,8 +226,41 @@
         {
             var org = Organisation.CreatePartnership("trading");
             var address = new Address("1", "street", "Woking", "Hampshire", "GU21 5EE", db.WeeeContext.Countries.First(), "12345678", "test@co.uk");
-            org.AddOrUpdateAddress(AddressType.RegisteredOrPPBAddress, address);
+            org.AddOrUpdateAddress(Domain.AddressType.RegisteredOrPPBAddress, address);
             return org;
+        }
+
+        private async Task CreateScheme(DatabaseWrapper db)
+        {
+            const Domain.Obligation.ObligationType obligationType = Domain.Obligation.ObligationType.B2B;
+
+            var contact = new Domain.Organisation.Contact("firstName", "LastName", "Position");
+
+            Organisation organisation = Organisation.CreateSoleTrader("Test Organisation");
+            var address = new Address("address1", "address2", "town", "county", "postcode", db.WeeeContext.Countries.First(), "telephone", "email");
+
+            organisation.AddOrUpdateAddress(Domain.AddressType.RegisteredOrPPBAddress, address);
+
+            Domain.UKCompetentAuthority authority = db.WeeeContext.UKCompetentAuthorities.Single(c => c.Abbreviation == UKCompetentAuthorityAbbreviationType.EA);
+
+            Scheme scheme1 = new Scheme(organisation);
+
+            scheme1.UpdateScheme("BBB", "TT", "Test", obligationType, authority);
+            scheme1.AddOrUpdateMainContactPerson(contact);
+            scheme1.AddOrUpdateAddress(address);
+            db.WeeeContext.Organisations.Add(organisation);
+            db.WeeeContext.Schemes.Add(scheme1);
+            await db.WeeeContext.SaveChangesAsync();
+        }
+
+        private void VerifyPCSRecord(List<AatfAeDetailsData> results)
+        {
+            var pcsRecord = results.Where(x => x.Name == "BBB").FirstOrDefault();
+
+            pcsRecord.Name.Equals("BBB");
+            pcsRecord.IbisCustomerReference.Equals("Test");
+            pcsRecord.ObligationType.Equals("B2B");
+            pcsRecord.RecordType.Equals("PCS");
         }
     }
 }
