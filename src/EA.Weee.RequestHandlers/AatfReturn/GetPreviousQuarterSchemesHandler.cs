0b8f60d273e8deb11b02b3398f5bbc1e4b40bea6
Will Andrews <will7989@hotmail.com>
2019-09-12 15:52:17 +0100

    added handler to get the previous quater schemes

---
+++ src/EA.Weee.RequestHandlers/AatfReturn/GetPreviousQuarterSchemesHandler.cs
@@ -0,0 +1,50 @@
+﻿namespace EA.Weee.RequestHandlers.AatfReturn
+{
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.Domain.AatfReturn;
+    using EA.Weee.RequestHandlers.Security;
+    using EA.Weee.Requests.AatfReturn;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    internal class GetPreviousQuarterSchemesHandler : IRequestHandler<GetPreviousQuarterSchemes, List<Guid>>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly IGenericDataAccess dataAccess;
+
+        public GetPreviousQuarterSchemesHandler(IWeeeAuthorization authorization, IGenericDataAccess dataAccess)
+        {
+            this.authorization = authorization;
+            this.dataAccess = dataAccess;
+        }
+
+        public async Task<List<Guid>> HandleAsync(GetPreviousQuarterSchemes message)
+        {
+            authorization.EnsureCanAccessExternalArea();
+
+            List<Return> allReturns = await dataAccess.GetAll<Return>();
+
+            Return lastReturn = allReturns.Where(p => p.Organisation.Id == message.OrganisationId).OrderByDescending(p => p.Quarter).FirstOrDefault();
+
+            List<ReturnScheme> returnSchemes = await dataAccess.GetAll<ReturnScheme>();
+
+            if (returnSchemes == null)
+            {
+                return new List<Guid>();
+            }
+
+            List<ReturnScheme> lastSchemes = returnSchemes.Where(p => p.ReturnId == lastReturn.Id).ToList();
+
+            List<Guid> schemeIds = new List<Guid>();
+
+            foreach (ReturnScheme scheme in lastSchemes)
+            {
+                schemeIds.Add(scheme.Id);
+            }
+
+            return schemeIds;
+        }
+    }
+}
---
+++ src/EA.Weee.Requests/AatfReturn/GetPreviousQuarterSchemes.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.Requests.AatfReturn
+{
+    using EA.Prsd.Core.Mediator;
+    using System;
+    using System.Collections.Generic;
+
+    public class GetPreviousQuarterSchemes : IRequest<List<Guid>>
+    {
+        public Guid OrganisationId { get; set; }
+    }
+}
