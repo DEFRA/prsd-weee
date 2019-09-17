83ea5ffd12a6feadbd91682df5346a7250ff9aa7
SumeN <sumen@sfwltd.co.uk>
2019-09-17 16:23:23 +0100

    Filter by authority

--- src/EA.Weee.RequestHandlers/Admin/GetAatfs/GetAatfsDataAccess.cs
+++ src/EA.Weee.RequestHandlers/Admin/GetAatfs/GetAatfsDataAccess.cs
@@ -29,14 +29,16 @@
 
         public async Task<List<Aatf>> GetFilteredAatfs(AatfFilter filter)
         {
-            return await context.Aatfs.GroupBy(a => a.AatfId)
+            var aatfList = await context.Aatfs.GroupBy(a => a.AatfId)
                 .Select(x => x.OrderByDescending(a => a.ComplianceYear).FirstOrDefault())
                 .Where(a =>
                 (filter.Name == null || filter.Name.Trim() == string.Empty ||
                     a.Name.ToLower().Contains(filter.Name.ToLower())) &&
                 (filter.ApprovalNumber == null || filter.ApprovalNumber.Trim() == string.Empty ||
-                    a.ApprovalNumber.ToLower().Contains(filter.ApprovalNumber.ToLower())))
+                    a.ApprovalNumber.ToLower().Contains(filter.ApprovalNumber.ToLower()))) 
                 .ToListAsync();
+
+            return aatfList.Where(x => filter.SelectedAuthority.Contains(x.CompetentAuthority.Id)).ToList();
         }
 
         public async Task<List<Aatf>> GetLatestAatfs()
