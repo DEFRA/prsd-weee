ec8d9ce0af9f7360c5efbfebf8b207af56f96098
SumeN <sumen@sfwltd.co.uk>
2019-09-17 11:05:33 +0100

    Add authority

--- src/EA.Weee.Core/AatfReturn/AatfFilter.cs
+++ src/EA.Weee.Core/AatfReturn/AatfFilter.cs
@@ -1,5 +1,8 @@
 ﻿namespace EA.Weee.Core.AatfReturn
 {
+    using System;
+    using System.Collections.Generic;
+
     public class AatfFilter
     {
         public AatfFilter()
@@ -9,5 +12,7 @@
         public string Name { get; set; }
 
         public string ApprovalNumber { get; set; }
+
+        public List<Guid> SelectedAuthority { get; set; }
     }
 }
--- src/EA.Weee.Web/Areas/Admin/ViewModels/Aatf/FilteringViewModel.cs
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Aatf/FilteringViewModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Aatf
 {
+    using System;
     using System.Collections.Generic;
-
-    using EA.Weee.Core.AatfReturn;
     using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Core.Shared;
 
     public class FilteringViewModel
     {
@@ -14,6 +16,10 @@
 
         public FacilityType FacilityType { get; set; }
 
-        public IEnumerable<int> SelectedReturnStatus { get; set; }
+        public List<int> SelectedReturnStatus { get; set; }
+
+        public List<Guid> SelectedAuthority { get; set; }
+
+        public List<UKCompetentAuthorityData> CompetentAuthorityOptions { get; set; }
     }
 }
