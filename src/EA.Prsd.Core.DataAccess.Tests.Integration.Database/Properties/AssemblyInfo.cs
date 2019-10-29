48f51a2e6ed1543f3a03a942982b11ba8d6f6256
Alisdair Fyfe <alisdair.fyfe@civica.co.uk>
2019-10-29 12:06:17 +0000

    Updated copyright year to 2019

--- src/EA.Prsd.Core.DataAccess.Tests.Integration.Database/Properties/AssemblyInfo.cs
+++ src/EA.Prsd.Core.DataAccess.Tests.Integration.Database/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EA.Prsd.Core.DataAccess.Tests.Integration.Database")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
