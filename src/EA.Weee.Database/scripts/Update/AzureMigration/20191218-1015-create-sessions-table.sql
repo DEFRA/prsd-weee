1cc1e7a73327e43f3651109630f23d266e3fc34a
JohnPadgettCivica <john.padgett@civica.co.uk>
2019-12-18 10:35:33 +0000

    SQL session storage changes

---
+++ src/EA.Weee.Database/scripts/Update/AzureMigration/20191218-1015-create-sessions-table.sql
@@ -0,0 +1,27 @@
+
+
+/****** Object:  Table [dbo].[Sessions]    Script Date: 17/12/2019 16:28:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Sessions](
+	[SessionId] [nvarchar](88) NOT NULL,
+	[Created] [datetime] NOT NULL,
+	[Expires] [datetime] NOT NULL,
+	[LockDate] [datetime] NOT NULL,
+	[LockCookie] [int] NOT NULL,
+	[Locked] [bit] NOT NULL,
+	[SessionItem] [varbinary](max) NULL,
+	[Flags] [int] NOT NULL,
+	[Timeout] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[SessionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
