6c48b1036c24213f5741998477b58529ffba2423
JohnPadgettCivica <john.padgett@civica.co.uk>
2019-12-19 09:41:12 +0000

    Add web maintenance mode filter tests

--- src/EA.Weee.Web/Filters/MaintenanceModeFilterAttribute.cs
+++ src/EA.Weee.Web/Filters/MaintenanceModeFilterAttribute.cs
@@ -1,13 +1,10 @@
 ﻿namespace EA.Weee.Web.Filters
 {
-    using EA.Weee.Web.Services;
     using System.Web.Mvc;
     using System.Web.Routing;
 
     public class MaintenanceModeFilterAttribute : ActionFilterAttribute
     {
-        public ConfigurationService ConfigService { get; set; }
-
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
             base.OnActionExecuted(filterContext);
