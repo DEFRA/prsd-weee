98b9d07450943102c77ed1c128740822fd40f0d1
JohnPadgettCivica <john.padgett@civica.co.uk>
2020-01-23 12:04:26 +0000

    Powershell script to backup SQL Managed instance to a Azure storage container

---
+++ tools/Scripts/backup-azure-sql.ps1
@@ -0,0 +1,41 @@
+﻿[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$User= $null,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Password = $null,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Instance = $null,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$Database = $null,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Container  = $null,
+
+    [Parameter(Mandatory=$true)]
+    [string]$StorageAccount  = $null,
+
+    [Parameter(Mandatory=$true)]
+    [string]$StorageAccountKey  = $null
+)
+
+$securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$sqlCredentials = New-Object System.Management.Automation.PSCredential ($User, $securePassword)
+
+$targetInstance = Connect-DbaInstance -SqlInstance $Instance -SqlCredential $sqlCredentials
+
+$storageAccountContext = New-AzStorageContext -StorageAccountName $StorageAccount -StorageAccountKey $StorageAccountKey
+$storageContainer = Get-AzStorageContainer -Context $storageAccountContext -Name $Container
+
+$ts = New-TimeSpan -Minutes 30
+$sas = (New-AzStorageAccountSASToken -Service Blob -ResourceType Object -Permission "rw" -Context $storageAccountContext -ExpiryTime ((get-date) + $ts)).TrimStart('?')  
+
+New-DbaCredential -SqlInstance $targetInstance -Name "https://$StorageAccount.blob.core.windows.net/$Container" -Identity “SHARED ACCESS SIGNATURE” -SecurePassword (ConvertTo-SecureString $sas -AsPlainText -Force) -Force
+
+$file = $Database + " " + (Get-Date).ToString("yyyy-MM-dd HH-mm") + ".bak"
+Backup-DbaDatabase -SqlInstance $targetInstance -Database $Database -AzureBaseUrl “https://$StorageAccount.blob.core.windows.net/$Container/” -BackupFileName $file -Type Full -Checksum -CopyOnly
+
