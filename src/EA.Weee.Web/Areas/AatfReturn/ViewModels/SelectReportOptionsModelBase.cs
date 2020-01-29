e7f756e6e47ea936408a9d1383be852b239a5de1
JohnPadgettCivica <john.padgett@civica.co.uk>
2020-01-29 13:11:00 +0000

    Additional serializable attributes session

--- src/EA.Weee.Web/Areas/AatfReturn/ViewModels/SelectReportOptionsModelBase.cs
+++ src/EA.Weee.Web/Areas/AatfReturn/ViewModels/SelectReportOptionsModelBase.cs
@@ -5,6 +5,7 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    [Serializable]
     public abstract class SelectReportOptionsModelBase
     {
         protected SelectReportOptionsModelBase()
