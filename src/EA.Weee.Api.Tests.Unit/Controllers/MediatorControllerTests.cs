71c4d85ae56441c08990785d1a9bb1ca1aaedbb9
JohnPadgettCivica <john.padgett@civica.co.uk>
2020-02-18 15:18:28 +0000

    Add unit tests for api authorisation

---
+++ src/EA.Weee.Api.Tests.Unit/Controllers/MediatorControllerTests.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Weee.Api.Tests.Unit.Controllers
+{
+    using System.Web.Http;
+    using Api.Controllers;
+    using EA.Weee.Tests.Core;
+    using FluentAssertions;
+    using Xunit;
+
+    public class MediatorControllerTests
+    {
+        [Fact]
+        public void Controller_ShouldHaveAuthorizeAttribute()
+        {
+            typeof(MediatorController).Should().BeDecoratedWith<AuthorizeAttribute>();
+        }
+
+        [Fact]
+        public void Controller_ShouldNotHaveAnonymousActions()
+        {
+            AttributeHelper.ShouldNotHaveAnonymousMethods(typeof(MediatorController));
+        }
+    }
+}
