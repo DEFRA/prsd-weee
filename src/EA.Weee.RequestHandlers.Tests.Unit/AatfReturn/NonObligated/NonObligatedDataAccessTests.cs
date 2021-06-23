627a488b7bc2cf61db7ff379669198668396d80a
Alisdair Fyfe <alisdair.fyfe@civica.co.uk>
2021-06-23 12:05:16 +0100

    Updated non-Obligated WEEE Data Access failing unit test

--- src/EA.Weee.RequestHandlers.Tests.Unit/AatfReturn/NonObligated/NonObligatedDataAccessTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/AatfReturn/NonObligated/NonObligatedDataAccessTests.cs
@@ -14,6 +14,7 @@
 
     public class NonObligatedDataAccessTests
     {
+        private const int CategoryCount = 14;
         private readonly Return aatfReturn = new ReturnWrapper(Guid.NewGuid());
 
         private readonly NonObligatedDataAccess dataAccess;
@@ -35,7 +36,7 @@
         public async Task InsertNonObligatedWeee(bool dcf)
         {
             // Arrange
-            var nonObligatedWeees = Enumerable.Range(1, 14).Select(n => new NonObligatedWeee(aatfReturn, n, dcf, n * 15)).ToArray();
+            var nonObligatedWeees = Enumerable.Range(1, CategoryCount).Select(n => new NonObligatedWeee(aatfReturn, n, dcf, n * 15)).ToArray();
             var dcfNonObligatedWeee = dbContextHelper.GetAsyncEnabledDbSet(new List<NonObligatedWeee>());
             A.CallTo(() => context.NonObligatedWeee).Returns(dcfNonObligatedWeee);
             var callList = new List<IReturnValueArgumentValidationConfiguration<NonObligatedWeee>>();
@@ -62,8 +63,8 @@
         public async Task InsertNonObligatedWeee_ExistingUpdated(bool dcf)
         {
             // Arrange
-            var newNonObligatedWeees = Enumerable.Range(1, 14).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 15)).ToArray();
-            var existingNonObligatedWeees = Enumerable.Range(1, 14).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 150)).ToArray();
+            var newNonObligatedWeees = Enumerable.Range(1, CategoryCount).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 15)).ToArray();
+            var existingNonObligatedWeees = Enumerable.Range(1, CategoryCount).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 150)).ToArray();
             var dcfNonObligatedWeee = dbContextHelper.GetAsyncEnabledDbSet<NonObligatedWeee>(existingNonObligatedWeees);
             A.CallTo(() => context.NonObligatedWeee).Returns(dcfNonObligatedWeee);
             var saveCall = A.CallTo(() => context.SaveChangesAsync());
@@ -88,7 +89,7 @@
         public async Task UpdateNonObligatedWeeeAmounts_ExistingAmountsUpdated(bool dcf)
         {
             // Arrange
-            var existingNonObligatedWeees = Enumerable.Range(1, 14).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 15)).ToArray();
+            var existingNonObligatedWeees = Enumerable.Range(1, CategoryCount).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 15)).ToArray();
             var updateArg = existingNonObligatedWeees.Select(n => new Tuple<Guid, decimal?>(n.Id, n.Tonnage * 10)).ToArray();
             var saveCall = A.CallTo(() => context.SaveChangesAsync());
             var dcfNonObligatedWeee = dbContextHelper.GetAsyncEnabledDbSet<NonObligatedWeee>(existingNonObligatedWeees);
@@ -108,40 +109,44 @@
         public async Task UpdateNonObligatedWeeeAmounts_DuplicatesRemoved(bool dcf)
         {
             // Arrange
-            var existingNonObligatedWeees = Enumerable.Range(1, 14).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 15));
-            var duplicateExistingNonObligatedWeees = Enumerable.Range(1, 14).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 100));
-            var updateArg = existingNonObligatedWeees.Select(n => new Tuple<Guid, decimal?>(n.Id, n.Tonnage * 5)).ToArray();
-            var repoNonObligatedWeee = duplicateExistingNonObligatedWeees.Concat(existingNonObligatedWeees).ToArray();
+            const int amountUpdateMultiplier = 5;
+            var existingNonObligatedWeees = Enumerable.Range(1, CategoryCount).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 15)).ToArray();
+            var duplicateExistingNonObligatedWeees = Enumerable.Range(1, CategoryCount).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, n, dcf, n * 100)).ToArray();
+            var updateArg = existingNonObligatedWeees.Select(n => new Tuple<Guid, decimal?>(n.Id, n.Tonnage * amountUpdateMultiplier)).ToArray();
+            var repoNonObligatedWeee = existingNonObligatedWeees.Concat(duplicateExistingNonObligatedWeees).ToArray();
             var saveCall = A.CallTo(() => context.SaveChangesAsync());
             var dcfNonObligatedWeee = dbContextHelper.GetAsyncEnabledDbSet<NonObligatedWeee>(repoNonObligatedWeee);
             A.CallTo(() => context.NonObligatedWeee).Returns(dcfNonObligatedWeee);
+            var removeCall = A.CallTo(() => dcfNonObligatedWeee.RemoveRange(A<IEnumerable<NonObligatedWeee>>.Ignored));
+            IEnumerable<NonObligatedWeee> removeElements = null;
+            removeCall.Invokes(c => removeElements = c.Arguments.First() as IEnumerable<NonObligatedWeee>);
 
             // Act
             await dataAccess.UpdateNonObligatedWeeeAmounts(ReturnId, updateArg);
 
             // Assert
             saveCall.MustHaveHappenedOnceExactly();
-            Assert.Equal(existingNonObligatedWeees.Count(), repoNonObligatedWeee.Count());
-            Assert.False(existingNonObligatedWeees
-                .OrderBy(n => n.CategoryId)
-                .Zip(repoNonObligatedWeee.OrderBy(n => n.CategoryId), (expected, repo) => new { expected, repo })
-                .Any(p => p.expected.CategoryId != p.repo.CategoryId
-                    || p.expected.Dcf != p.repo.Dcf
-                    || p.expected.EntityId != p.repo.EntityId
-                    || p.expected.Tonnage != p.repo.Tonnage), "Non-obligated WEEE collection does not match expected.");
+            // Assert that the removed elements are correct
+            Assert.NotNull(removeElements);
+            Assert.Equal(duplicateExistingNonObligatedWeees.Count(), removeElements.Count());
+            Assert.False(duplicateExistingNonObligatedWeees.Any(n => !removeElements.Any(r => r.Id == n.Id)), "Removed list does not contain all expected items.");
+            // Assert that the updated elements are correct
+            var matched = existingNonObligatedWeees.Select(n => new { initial = n, repo = dcfNonObligatedWeee.FirstOrDefault(r => r.Id == n.Id) });
+            Assert.False(matched.Any(m => m.repo == null), "Not all updated elements are in the actual repository collection.");
+            Assert.False(matched.Any(m => (m.initial.Tonnage * amountUpdateMultiplier) == m.repo.Tonnage), "Not all amounts are correct.");
         }
 
         [Fact]
         public async Task FetchNonObligatedWeeeForReturnWithoutDuplicates_NoDuplicates()
         {
             // Arrange
-            var existingNonObligatedWeees = Enumerable.Range(0, 28).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, 1 + (n % 14), n > 13, n * 15));
+            var existingNonObligatedWeees = Enumerable.Range(0, CategoryCount * 2).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, 1 + (n % CategoryCount), n > 13, n * 15));
             var saveCall = A.CallTo(() => context.SaveChangesAsync());
             var dcfNonObligatedWeee = dbContextHelper.GetAsyncEnabledDbSet<NonObligatedWeee>(existingNonObligatedWeees);
             A.CallTo(() => context.NonObligatedWeee).Returns(dcfNonObligatedWeee);
 
             // Act
-            var response = await dataAccess.FetchNonObligatedWeeeForReturn(aatfReturn.Id); // Placeholder
+            var response = await dataAccess.FetchNonObligatedWeeeForReturn(aatfReturn.Id);
 
             // Assert
             Assert.Equal(existingNonObligatedWeees.Count(), response.Count());
@@ -151,9 +156,8 @@
         public async Task FetchNonObligatedWeeeForReturnWithoutDuplicates_WithDuplicates()
         {
             // Arrange
-            const int CategoryCount = 14;
-            var existingNonObligatedWeees = Enumerable.Range(0, CategoryCount * 2).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, 1 + (n % 14), n > 13, (1 + n) * 15));
-            var existingDuplicateNonObligatedWeees = Enumerable.Range(0, CategoryCount * 2).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, 1 + (n % 14), n > 13, (1 + n) * 15));
+            var existingNonObligatedWeees = Enumerable.Range(0, CategoryCount * 2).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, 1 + (n % CategoryCount), n > (CategoryCount - 1), (1 + n) * 15));
+            var existingDuplicateNonObligatedWeees = Enumerable.Range(0, CategoryCount * 2).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, 1 + (n % CategoryCount), n > (CategoryCount - 1), (1 + n) * 15));
             var repoWeee = existingNonObligatedWeees.Concat(existingDuplicateNonObligatedWeees);
             // Check to prove the initial data is correct, 
             Assert.Equal(CategoryCount * 4, repoWeee.Count());
@@ -162,7 +166,7 @@
             A.CallTo(() => context.NonObligatedWeee).Returns(dcfNonObligatedWeee);
 
             // Act
-            var response = await dataAccess.FetchNonObligatedWeeeForReturn(aatfReturn.Id); // Placeholder
+            var response = await dataAccess.FetchNonObligatedWeeeForReturn(aatfReturn.Id);
 
             // Assert
             Assert.Equal(existingNonObligatedWeees.Count(), response.Count());
@@ -172,6 +176,34 @@
             Assert.Equal(CategoryCount, responseNotDcfWeee.Count());
             Assert.False(responseNotDcfWeee.Any(n => n.Tonnage != n.CategoryId * 15));
             Assert.False(responseDcfWeee.Any(n => n.Tonnage != (n.CategoryId + 14) * 15));
+        }
+
+        [Fact]
+        public async Task FetchNonObligatedWeeeForReturnWithoutDuplicates_FetchesOnlySpecifiedReturnWeee()
+        {
+            // Arrange
+            int numberWeeeToCreate = CategoryCount * 2;
+            // Arrange Return to fetch
+            var existingNonObligatedWeees = Enumerable.Range(0, numberWeeeToCreate).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), aatfReturn, 1 + (n % CategoryCount), n > (CategoryCount - 1), (1 + n) * 15));
+
+            // Arrange another Return data
+            var otherAatfReturn = new ReturnWrapper(Guid.NewGuid());
+            var existingNonObligatedWeeesOtherReturn = Enumerable.Range(0, numberWeeeToCreate).Select(n => new NonObligatedWeeeWrapper(Guid.NewGuid(), otherAatfReturn, 1 + (n % CategoryCount), n > (CategoryCount - 1), (1 + n) * 200));
+
+            // Build the Repo collection
+            var repoWeee = existingNonObligatedWeees.Concat(existingNonObligatedWeeesOtherReturn);
+            var saveCall = A.CallTo(() => context.SaveChangesAsync());
+            var dcfNonObligatedWeee = dbContextHelper.GetAsyncEnabledDbSet<NonObligatedWeee>(repoWeee);
+            A.CallTo(() => context.NonObligatedWeee).Returns(dcfNonObligatedWeee);
+
+            // Act
+            var response = await dataAccess.FetchAllNonObligateWeeeForReturn(aatfReturn.Id);
+
+            // Assert
+            saveCall.MustNotHaveHappened();
+            Assert.Equal(numberWeeeToCreate, response.Count());
+            Assert.False(response.Any(n => n.ReturnId != aatfReturn.Id));
+            Assert.False(response.Any(n => n.ReturnId == otherAatfReturn.Id));
         }
 
         public class NonObligatedWeeeWrapper : NonObligatedWeee
