9c14998b7113f9f7142cc5c6acf1e9079325f4f6
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-02-01 11:55:29 +0000

    Integration test framework implementation

---
+++ src/EA.Weee.Integration.Tests/TestUserContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EA.Weee.Integration.Tests
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using Prsd.Core.Domain;
+
+    public class TestUserContext : IUserContext
+    {
+        public TestUserContext(Guid userId)
+        {
+            this.UserId = userId;
+        }
+
+        public Guid UserId { get; }
+
+        public ClaimsPrincipal Principal
+        {
+            get
+            {
+                var identity = new ClaimsIdentity(new List<Claim>(), "integration");
+                
+                var principal = new ClaimsPrincipal(identity);
+                
+                return principal;
+            }
+        }
+    }
+}
