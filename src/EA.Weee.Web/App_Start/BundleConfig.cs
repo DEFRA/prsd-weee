206ef3c588fb38994724ce9f7cd14486bc0e5df7
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-02-08 11:44:45 +0000

    GOV UK Design System WIP

--- src/EA.Weee.Web/App_Start/BundleConfig.cs
+++ src/EA.Weee.Web/App_Start/BundleConfig.cs
@@ -27,8 +27,7 @@
                     "~/Scripts/custom-validation.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/govuk_frontend").Include(
-                    "~/Scripts/govuk_frontend/govuk-frontend-2.7.0.min.js",
-                    "~/Scripts/govuk_frontend/html5shiv.js",
+                    "~/Scripts/govuk_frontend/govuk-frontend-4.0.0.min.js",
                     "~/Scripts/setup-govuk-frontend.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/govuk_weee").Include(
