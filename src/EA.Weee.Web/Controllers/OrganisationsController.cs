0713417de60c1bca0b30b3d0e0136b468aac24ab
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-03 15:12:08 +0000

    new starter

---
+++ src/EA.Weee.Web/Controllers/OrganisationsController.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Weee.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class OrganisationsController : Controller
+    {
+        // GET: Organisations
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
