8bd8214fa5f2692af2f0d5b721e536cc7b5e7235
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-07 10:38:52 +0000

    Update nunit test verification

--- tools/Scripts/verify-nunit-test-results.ps1
+++ tools/Scripts/verify-nunit-test-results.ps1
@@ -10,5 +10,45 @@ param
     [string]$resultsFile = $null
 )
 
-write-host "Failig build"
-exit 1
+$xmlFile = dir $resultsPath | where { $_.Name -like "$resultsFile" } | foreach { $_.FullName }
+
+if ($xmlFile -eq $null) {
+    write-host "No results file - failing build"
+
+    exit 1
+}
+
+write-host $xmlFile
+
+[Xml]$xml = Get-Content -Path $xmlFile
+$xml."test-run"
+
+if ($xml -eq $null) {
+    write-host "nunit results file could not be parsed - failing build"
+
+    exit 1
+}
+
+if ($xml."test-run".total -eq 0) {
+    write-host "nunit results file contains no results - failing build"
+
+    exit 1
+}
+
+[DateTime]$endDate = $xml."test-run"."end-time"
+
+if ($endDate -lt (Get-Date).AddMinutes(-10)) {
+    write-host "nunit test results has end time that is not within tolerance - check test run - failing build"
+
+    exit 1
+}
+
+
+if ($xml."test-run".result -eq "Failed") {
+    write-host "nunit test results has $($xml."test-run".failed) failed tests - failing build"
+
+    exit 1
+}
+
+
+write host "nunit test run succeeded"
