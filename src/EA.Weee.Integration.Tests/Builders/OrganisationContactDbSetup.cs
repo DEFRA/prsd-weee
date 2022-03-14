d57e332b858488862b92dc604dafdc7d67312836
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-14 10:27:04 +0000

    Add PCS builder

---
+++ src/EA.Weee.Integration.Tests/Builders/OrganisationContactDbSetup.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Weee.Integration.Tests.Builders
+{
+    using System.Linq;
+    using Base;
+    using Domain.Organisation;
+
+    public class OrganisationContactDbSetup : DbTestDataBuilder<Contact, OrganisationContactDbSetup>
+    {
+        protected override Contact Instantiate()
+        {
+            instance = new Contact(Faker.Name.First(),
+                Faker.Name.Last(),
+                Faker.Company.CatchPhrase().Substring(0, 20));
+
+            return instance;
+        }
+    }
+}
