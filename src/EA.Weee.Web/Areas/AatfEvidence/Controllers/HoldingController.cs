7ba19a9e0ce9bee4aed4e08494a89bb40d39256b
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-15 16:49:54 +0000

    Add in initial aatf evidence area

---
+++ src/EA.Weee.Web/Areas/AatfEvidence/Controllers/HoldingController.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.Web.Areas.AatfEvidence.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    public class HoldingController : AatfEvidenceBaseController
+    {
+        // GET: AatfReturn/Holding
+        public ActionResult Index(Guid organisationId)
+        {
+            return View(organisationId);
+        }
+    }
+}
