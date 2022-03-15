33a727032c05ee1299501b446ae8cf30e845b544
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-15 16:49:54 +0000

    Add in initial aatf evidence area

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfEvidence/Controller/AatfEvidenceBaseControllerTests.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.AatfEvidence.Controller
+{
+    using FluentAssertions;
+    using Web.Areas.AatfEvidence.Controllers;
+    using Web.Controllers.Base;
+    using Xunit;
+
+    public class AatfReturnEvidenceBaseControllerTests
+    {
+        [Fact]
+        public void CheckAatfTaskListControllerInheritsExternalSiteController()
+        {
+            typeof(AatfEvidenceBaseController).BaseType.Name.Should().Be(nameof(ExternalSiteController));
+        }
+    }
+}
