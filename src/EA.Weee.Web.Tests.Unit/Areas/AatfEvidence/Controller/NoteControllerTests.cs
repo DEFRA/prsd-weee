7ebb05b204d484a239197a934cbe667d12125714
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-16 17:45:46 +0000

    Start some basic layout for create evidence note screen

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/AatfEvidence/Controller/NoteControllerTests.cs
@@ -0,0 +1,47 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.AatfEvidence.Controller
+{
+    using System.Web.Mvc;
+    using Api.Client;
+    using AutoFixture;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Services;
+    using Services.Caching;
+    using Web.Areas.AatfEvidence.Controllers;
+    using Xunit;
+
+    public class NoteControllerTests
+    {
+        private readonly IWeeeClient weeeClient;
+        private readonly NoteController controller;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+        private readonly Fixture fixture;
+
+        public NoteControllerTests()
+        {
+            weeeClient = A.Fake<IWeeeClient>();
+            breadcrumb = A.Fake<BreadcrumbService>();
+            cache = A.Fake<IWeeeCache>();
+            fixture = new Fixture();
+
+            controller = new NoteController();
+        }
+
+        [Fact]
+        public void NoteControllerInheritsExternalSiteController()
+        {
+            typeof(NoteController).BaseType.Name.Should().Be(nameof(AatfEvidenceBaseController));
+        }
+
+        [Fact]
+        public void CreateGet_DefaultViewShouldBeReturned()
+        {
+            //act
+            var result = controller.Create() as ViewResult;
+
+            //assert
+            result.ViewName.Should().BeEmpty();
+        }
+    }
+}
