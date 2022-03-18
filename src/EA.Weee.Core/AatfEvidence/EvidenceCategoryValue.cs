5ad133e243bdfd7cd1a3e51861e3debe9ac9d5fa
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-18 14:34:27 +0000

    Refactor the category values and add Evidence category

---
+++ src/EA.Weee.Core/AatfEvidence/EvidenceCategoryValue.cs
@@ -0,0 +1,31 @@
+﻿namespace EA.Weee.Core.AatfEvidence
+{
+    using System;
+    using AatfReturn;
+    using DataReturns;
+    using Validation;
+
+    [Serializable]
+    public class EvidenceCategoryValue : CategoryValue
+    {
+        [TonnageValue("CategoryId", "Total received")]
+        public string Received { get; set; }
+
+        [TonnageValue("CategoryId", "Reused as whole appliances")]
+        public string Reused { get; set; }
+
+        public EvidenceCategoryValue()
+        {
+        }
+
+        public EvidenceCategoryValue(string received, string reused)
+        {
+            Received = received;
+            Reused = reused;
+        }
+
+        public EvidenceCategoryValue(WeeeCategory category) : base(category)
+        {
+        }
+    }
+}
