37b82038499b7c2f4f3c8fa6abe116d05a9395a5
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-21 16:58:18 +0000

    Updates after code review

--- src/EA.Weee.Core/AatfEvidence/Protocol.cs
+++ src/EA.Weee.Core/AatfEvidence/Protocol.cs
@@ -6,8 +6,8 @@
     {
         public static readonly Protocol Actual = new Protocol(1, "Actual");
         public static readonly Protocol NationalProtocol = new Protocol(2, "National protocol");
-        public static readonly Protocol SiteProtocol = new Protocol(2, "Site protocol");
-        public static readonly Protocol ReuseNetworkWeights = new Protocol(2, "Re-use network weights");
+        public static readonly Protocol SiteProtocol = new Protocol(3, "Site protocol");
+        public static readonly Protocol ReuseNetworkWeights = new Protocol(4, "Re-use network weights");
 
         protected Protocol()
         {
