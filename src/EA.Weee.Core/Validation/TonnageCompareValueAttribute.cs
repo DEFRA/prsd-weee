ccfbd01bb41571319eb1ff4e4b53f4d165055558
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-22 14:49:39 +0000

    Add Compare Tonnage Value attribute validation

---
+++ src/EA.Weee.Core/Validation/TonnageCompareValueAttribute.cs
@@ -0,0 +1,87 @@
+﻿namespace EA.Weee.Core.Validation
+{
+    using DataReturns;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class TonnageCompareValueAttribute : ValidationAttribute
+    {
+        private string categoryPropertyErrorMessage;
+
+        public string ComparePropertyName { get; set; }
+
+        public string CategoryProperty { get; private set; }
+
+        public TonnageCompareValueAttribute(string category, string compareProperty)
+        {
+            CategoryProperty = category;
+            ComparePropertyName = compareProperty;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(value?.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var instance = validationContext.ObjectInstance;
+            var type = instance.GetType();
+            var categoryProperty = type.GetProperty(CategoryProperty);
+
+            if (categoryProperty == null)
+            {
+                throw new ValidationException($"Property {CategoryProperty} does not exist");
+            }
+
+            var categoryId = Enum.GetValues(typeof(WeeeCategory)).Cast<int>().ToList();
+            var categoryPropertyValue = (int)categoryProperty.GetValue(validationContext.ObjectInstance, null) as int?;
+
+            categoryPropertyErrorMessage = $"Property {CategoryProperty} should be of type {nameof(WeeeCategory)}";
+
+            if (categoryPropertyValue == null)
+            {
+                throw new ValidationException(categoryPropertyErrorMessage);
+            }
+
+            if (categoryPropertyValue.HasValue && !categoryId.Contains(categoryPropertyValue.Value))
+            {
+                throw new ValidationException(categoryPropertyErrorMessage);
+            }
+
+            var dependentProperty = type.GetProperty(ComparePropertyName);
+
+            if (dependentProperty == null)
+            {
+                throw new ValidationException($"Compare Property {ComparePropertyName} does not exist");
+            }
+
+            var dependentPropertyValue = dependentProperty.GetValue(instance, null);
+
+            if ((decimal.TryParse(value.ToString(), NumberStyles.Number & ~NumberStyles.AllowTrailingSign,
+                    CultureInfo.InvariantCulture, out var decimalResult) && (dependentPropertyValue == null || string.IsNullOrWhiteSpace(dependentPropertyValue.ToString()))))
+            {
+                return new ValidationResult(GenerateMessage(categoryPropertyValue.Value));
+            }
+
+            if ((decimal.TryParse(dependentPropertyValue.ToString(), NumberStyles.Number & ~NumberStyles.AllowTrailingSign,
+                    CultureInfo.InvariantCulture, out var decimalDependentResult)))
+            {
+                if (decimalResult > decimalDependentResult)
+                {
+                    return new ValidationResult(GenerateMessage(categoryPropertyValue.Value));
+                }
+            }
+            
+            return ValidationResult.Success;
+        }
+
+        private string GenerateMessage(int categoryId)
+        {
+            return $"The reused tonnage for category {categoryId} must be equivalent or lower than the received tonnage.";
+        }
+    }
+}
