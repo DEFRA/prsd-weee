25cf3cab76956c5aa8ed95688c1be909514df202
Sreedhar B <Sreedhar.Bangarugari@civica.co.uk>
2022-03-23 10:32:29 +0000

    Initial check-in

---
+++ src/EA.Weee.Web/Areas/AatfReturn/Mappings/ToViewModel/ReturnToSearchedAnAatfViewModelMap.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel
+{
+    using EA.Prsd.Core;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Web.Areas.AatfReturn.ViewModels;
+
+    public class ReturnToSearchedAnAatfViewModelMap : IMap<ReturnToSearchedAnAatfViewModelMapTransfer, SearchedAatfResultListViewModel>
+    {
+        public SearchedAatfResultListViewModel Map(ReturnToSearchedAnAatfViewModelMapTransfer source)
+        {
+            Guard.ArgumentNotNull(() => source, source);
+
+            var viewModel = new SearchedAatfResultListViewModel()
+            {
+                ReturnId = source.Return.Id,
+                AatfId = source.AatfId,
+                OrganisationId = source.Return.OrganisationData.Id,
+            };
+
+            return viewModel;
+        }
+    }
+}
