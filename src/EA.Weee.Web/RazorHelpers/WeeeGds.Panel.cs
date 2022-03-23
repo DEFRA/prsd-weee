4ad7f77dff6894a37a5530f354cc0bebb565b8da
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-23 15:50:22 +0000

    Add GDS panel control

---
+++ src/EA.Weee.Web/RazorHelpers/WeeeGds.Panel.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Weee.Web.RazorHelpers
+{
+    using System.Web.Mvc;
+
+    public partial class WeeeGds<TModel>
+    {
+        public MvcHtmlString Panel(string displayText, string headerText = null)
+        {
+            var outerDiv = new TagBuilder("div");
+            outerDiv.AddCssClass("govuk-panel--confirmation");
+            outerDiv.AddCssClass("govuk-panel");
+
+            if (!string.IsNullOrWhiteSpace(headerText))
+            {
+                var header = new TagBuilder("h1");
+                header.AddCssClass("govuk-panel__title");
+                header.SetInnerText(headerText);
+                outerDiv.InnerHtml += header.ToString();
+            }
+
+            var bodyDiv = new TagBuilder("div");
+            bodyDiv.AddCssClass("govuk-panel__body");
+            bodyDiv.SetInnerText(displayText);
+
+            outerDiv.InnerHtml += bodyDiv.ToString();
+
+            return new MvcHtmlString(outerDiv.ToString());
+        }
+    }
+}
