808b2a1b7681b6c6cceafd29470544fb688e9512
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-23 14:32:43 +0000

    Updates to create evidence to setup redirect to view page

--- src/EA.Weee.Web/Areas/Aatf/AatfAreaRegistration.cs
+++ src/EA.Weee.Web/Areas/Aatf/AatfAreaRegistration.cs
@@ -13,6 +13,12 @@
             context.Routes.MapMvcAttributeRoutes();
 
             context.MapLowercaseDashedRoute(
+                name: AatfEvidenceRedirect.ViewEvidenceRouteName,
+                url: "Aatf/{organisationId}/{controller}/{action}/{evidenceNoteId}",
+                defaults: new { action = "ViewDraftEvidenceNote", controller = "ManageEvidenceNotes" },
+                namespaces: new[] { typeof(ManageEvidenceNotesController).Namespace });
+
+            context.MapLowercaseDashedRoute(
                 name: AatfEvidenceRedirect.ManageEvidenceRouteName,
                 url: "Aatf/{organisationId}/{controller}/{action}/",
                 defaults: new { action = "Index", controller = "Home" },
