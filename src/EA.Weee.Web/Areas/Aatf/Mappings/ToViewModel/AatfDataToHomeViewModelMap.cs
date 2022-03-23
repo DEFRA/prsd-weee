1278a591e76d24b03e6dccfaafa580c04533ae3a
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-03-23 13:47:11 +0000

    Code Review changes for Choose Site Removes uneccessary ViewModel property Puts duplicate code into new filter class that both maps now call Adds more unit tests Puts model generation code in ChooseSiteController into separate method

--- src/EA.Weee.Web/Areas/Aatf/Mappings/ToViewModel/AatfDataToHomeViewModelMap.cs
+++ src/EA.Weee.Web/Areas/Aatf/Mappings/ToViewModel/AatfDataToHomeViewModelMap.cs
@@ -2,9 +2,8 @@
 {
     using EA.Prsd.Core;
     using EA.Prsd.Core.Mapper;
-    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Web.Areas.Aatf.Mappings.Filters;
     using EA.Weee.Web.Areas.Aatf.ViewModels;
-    using System.Collections.Generic;
     using System.Linq;
 
     public class AatfDataToHomeViewModelMap : IMap<AatfDataToHomeViewModelMapTransfer, HomeViewModel>
@@ -12,49 +11,16 @@
         public HomeViewModel Map(AatfDataToHomeViewModelMapTransfer source)
         {
             Guard.ArgumentNotNull(() => source, source);
-
-            var filteredAatfList = RemoveOlderAatfs(source.AatfList);
-
-            var selectedAatfsOrAes = new List<AatfData>();
-
-            selectedAatfsOrAes.AddRange(filteredAatfList.Where(m => m.FacilityType == source.FacilityType));
-
-            foreach (var aatf in selectedAatfsOrAes)
-            {
-                aatf.AatfContactDetailsName = aatf.Name + " (" + aatf.ApprovalNumber + ") - " + aatf.AatfStatus;
-            }
+            var aatfDataFilter = new AatfDataFilter();
 
             var model = new HomeViewModel
             {
                 FacilityType = source.FacilityType,
                 OrganisationId = source.OrganisationId,
-                AatfList = selectedAatfsOrAes.OrderBy(o => o.Name).ToList()
+                AatfList = aatfDataFilter.Filter(source.AatfList, source.FacilityType).OrderBy(o => o.Name).ToList(),
             };
 
             return model;
-        }
-
-        private IEnumerable<AatfData> RemoveOlderAatfs(IReadOnlyCollection<AatfData> source)
-        {
-            var listToBeReturned = new List<AatfData>();
-
-            foreach (var aatf in source)
-            {
-                if (source.Any(m => m.AatfId == aatf.AatfId && m.Id != aatf.Id))
-                {
-                    var latestAatf = source.Where(m => m.AatfId == aatf.AatfId).OrderByDescending(m => m.ApprovalDate).First();
-
-                    if (!listToBeReturned.Contains(latestAatf))
-                    {
-                        listToBeReturned.Add(latestAatf);
-                    }
-                }
-                else
-                {
-                    listToBeReturned.Add(aatf);
-                }
-            }
-            return listToBeReturned;
         }
     }
 }
