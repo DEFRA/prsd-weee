b888869400d49a4f1475646c096c95c075742289
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-24 11:31:21 +0000

    Move the autocompte css to use sass and update so validation is displayed correctly

--- src/EA.Weee.Web/Scripts/weee-application.js
+++ src/EA.Weee.Web/Scripts/weee-application.js
@@ -134,7 +134,24 @@
         $("#SelectPcsForm").submit();
     });
 
+    $("#save-as-draft-button").click(function() {
+        //event.preventDefault();
+        //var form = $(this).parents("form:first");
+        //form.submit();
+
+        //var autocompletes = $(".autocomplete__input");
+        //autocompletes.each(function(auto) {
+        //    console.log($(this));
+        //});
+    });
     var selectElements = document.querySelectorAll(".gds-auto-complete");
+/*    if (selectElements.length > 0) {
+        window.onload = new function() {
+            selectElements.forEach(function(element) {
+                console.log(element.parent);
+            });
+        }
+    }*/
 
     selectElements.forEach(function(element) {
         var items = Array.from(element.options).map(el => el.textContent || el.innerText);
@@ -151,9 +168,10 @@
         
         var newElement = document.createElement("div");
         newElement.setAttribute("style", "width: 100%");
+
         element.parentNode.insertBefore(newElement, element);
         var existingId = element.id;
-
+        
         accessibleAutocomplete({
             showAllValues: true,
             id: element.id,
@@ -190,6 +208,11 @@
                 return '<svg class="autocomplete__dropdown-arrow-down" viewBox="0 0 512 512" ><path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z"/></svg>'
             }
         });
+
+        if (element.classList.contains("input-validation-error")) {
+            var autoCompletes = $(element.parentNode).find(".autocomplete__input");
+            autoCompletes[0].classList.add("autocomplete__error");
+        }
 
         element.style.display = "none";
         element.id = element.id + "-select";
