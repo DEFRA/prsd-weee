b54e19d800c8c590f7d05fadacd4529bb5225e2a
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-25 13:39:45 +0000

    Initial view draft evidence note tests

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Aatf/Controller/ManageEvidenceNotesControllerViewDraftEvidenceNoteTests.cs
@@ -0,0 +1,48 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Aatf.Controller
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Web.Areas.Aatf.Controllers;
+    using Xunit;
+
+    public class ManageEvidenceNotesControllerViewDraftEvidenceNoteTests : ManageEvidenceNotesControllerTestsBase
+    {
+        [Fact]
+        public void ViewDraftEvidenceNoteGet_ShouldHaveHttpGetAttribute()
+        {
+            typeof(ManageEvidenceNotesController).GetMethod("ViewDraftEvidenceNote", new[] { typeof(Guid), typeof(int) }).Should()
+                .BeDecoratedWith<HttpGetAttribute>();
+        }
+
+        [Fact]
+        public async Task ViewDraftEvidenceNoteGet_DefaultViewShouldBeReturned()
+        {
+            //act
+            var result = await Controller.ViewDraftEvidenceNote(OrganisationId, EvidenceNoteId) as ViewResult;
+
+            //assert
+            result.ViewName.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task ViewDraftEvidenceNoteGet_BreadcrumbShouldBeSet()
+        {
+            //arrange
+            var organisationName = Faker.Company.Name();
+            var organisationId = Guid.NewGuid();
+
+            A.CallTo(() => Cache.FetchOrganisationName(organisationId)).Returns(organisationName);
+
+            //act
+            await Controller.ViewDraftEvidenceNote(organisationId, EvidenceNoteId);
+
+            //assert
+            Breadcrumb.ExternalActivity.Should().Be("TODO:fix");
+            Breadcrumb.ExternalOrganisation.Should().Be(organisationName);
+            Breadcrumb.OrganisationId.Should().Be(organisationId);
+        }
+    }
+}
