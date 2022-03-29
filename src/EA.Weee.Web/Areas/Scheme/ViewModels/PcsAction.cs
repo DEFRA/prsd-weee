d5cf5bc2005e77a63357acb44c8b4ed5ae92689b
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-03-29 12:12:38 +0100

    moved Pcs Evidence Note constant into own attribute. corrected unit tests and completed task 42643 and 42675.

--- src/EA.Weee.Web/Areas/Scheme/ViewModels/PcsAction.cs
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/PcsAction.cs
@@ -2,17 +2,22 @@
 {
     public static class PcsAction
     {
+        // PCS menu items
         public static string ManagePcsEvidenceNotes = "Manage PCS evidence notes";  // CCCI_00007 in catalogue has not been updated but this has been confirmed
         public static string ManagePcsMembers = "Manage PCS members";
         public static string ManageEeeWeeeData = "Manage PCS EEE/WEEE data";
         public static string ViewSubmissionHistory = "View PCS submissions history";
-        public static string ManageContactDetails = "Manage PCS contact details";
-        public static string ManageOrganisationUsers = "Manage organisation users";
+        public static string ManagePcsContactDetails = "Manage PCS contact details";
+
+        // AATF menu items
         public static string ManageAatfEvidenceNotes = "Manage AATF evidence notes";
         public static string ManageAatfReturns = "Manage AATF returns";
         public static string ManageAeReturns = "Manage AE returns";
-        public static string ViewOrganisationDetails = "View organisation details";
         public static string ManageAatfContactDetails = "Manage AATF contact details";
         public static string ManageAeContactDetails = "Manage AE contact details";
+
+        // these belong to both PCS and AATF menus
+        public static string ManageOrganisationUsers = "Manage organisation users";
+        public static string ViewOrganisationDetails = "View organisation details";
     }
 }
