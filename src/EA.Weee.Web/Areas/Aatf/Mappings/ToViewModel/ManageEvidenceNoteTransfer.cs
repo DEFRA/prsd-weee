86b75de05f78f4de403c8fae25c9f4465e478229
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-29 13:03:51 +0100

    Refactor the Manage evidence index action to use mapper

---
+++ src/EA.Weee.Web/Areas/Aatf/Mappings/ToViewModel/ManageEvidenceNoteTransfer.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Weee.Web.Areas.Aatf.Mappings.ToViewModel
+{
+    using System;
+    using System.Collections.Generic;
+    using Core.AatfReturn;
+    using Prsd.Core;
+
+    public class ManageEvidenceNoteTransfer
+    {
+        public Guid OrganisationId { get; protected set; }
+        public Guid AatfId { get; protected set; }
+
+        public AatfData AatfData { get; protected set; }
+
+        public List<AatfData> Aatfs { get; protected set; }
+
+        public ManageEvidenceNoteTransfer(Guid organisationId, Guid aatfId, AatfData aatfData, List<AatfData> aatfs)
+        {
+            Guard.ArgumentNotDefaultValue(() => organisationId, organisationId);
+            Guard.ArgumentNotNull(() => aatfData, aatfData);
+            Guard.ArgumentNotNull(() => aatfs, aatfs);
+            Guard.ArgumentNotDefaultValue(() => aatfId, aatfId);
+            
+            OrganisationId = organisationId;
+            AatfData = aatfData;
+            Aatfs = aatfs;
+            AatfId = aatfId;
+        }
+    }
+}
