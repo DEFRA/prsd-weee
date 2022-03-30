e87ceda79976a48523546324c3fd4eacdf1efa83
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-30 12:45:16 +0100

    Domain enum tests for evidence note

---
+++ src/EA.Weee.Domain.Tests.Unit/AatfEvidence/WasteTypeEnumTests.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.Domain.Tests.Unit.AatfEvidence
+{
+    using System;
+    using Evidence;
+    using FluentAssertions;
+    using Prsd.Core.Helpers;
+    using Xunit;
+
+    public class WasteTypeEnumTests
+    {
+        [Fact]
+        public void WasteType_ShouldHaveValues()
+        {
+            var values = EnumHelper.GetValues(typeof(WasteType));
+            values.Count.Should().Be(2);
+
+            values.Should().Contain(c => c.Key.Equals(1) && c.Value.Equals("HouseHold"));
+            values.Should().Contain(c => c.Key.Equals(2) && c.Value.Equals("NonHouseHold"));
+        }
+    }
+}
