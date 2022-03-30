75a3402540da61769bf89d073db1de309f7913a8
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-30 16:40:10 +0100

    Remove house hold non household from validation

--- src/EA.Weee.Core/AatfEvidence/EvidenceCategoryValue.cs
+++ src/EA.Weee.Core/AatfEvidence/EvidenceCategoryValue.cs
@@ -8,10 +8,10 @@
     [Serializable]
     public class EvidenceCategoryValue : CategoryValue
     {
-        [TonnageValue(nameof(CategoryId), "The tonnage value", "household", true)]
+        [TonnageValue(nameof(CategoryId), "The tonnage value", true)]
         public string Received { get; set; }
 
-        [TonnageValue(nameof(CategoryId), "The tonnage value", "non-household", true)]
+        [TonnageValue(nameof(CategoryId), "The tonnage value", true)]
         [TonnageCompareValue(nameof(CategoryId), nameof(Received))]
         public string Reused { get; set; }
 
