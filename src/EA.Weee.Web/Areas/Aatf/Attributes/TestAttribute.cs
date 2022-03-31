4585f101a9410b1c7aa68f842da74c0edd149da3
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-31 14:16:41 +0100

    WIP

---
+++ src/EA.Weee.Web/Areas/Aatf/Attributes/TestAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.Web.Areas.Aatf.Attributes
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Prsd.Core;
+    using ViewModels;
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TestAttribute : RequiredAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = validationContext.ObjectInstance as EvidenceNoteViewModel;
+
+            Guard.ArgumentNotNull(() => model, model, "RequiredSubmitActionAttribute EvidenceViewModel IsNull");
+
+            return new ValidationResult("error");
+        }
+    }
+}
