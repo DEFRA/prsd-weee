c3cfd0b49e6fea1b038fffb3f5f83cf962b14912
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-03-31 09:08:09 +0100

    started 42742

---
+++ src/EA.Weee.RequestHandlers/Aatf/IViewEvidenceNoteRequestHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.RequestHandlers.Aatf
+{
+    using EA.Prsd.Core.Mediator;
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IViewEvidenceNoteRequestHandler<Guid id, ViewEvidenceNoteViewModel> : IRequestHandler<Guid, ViewEvidenceNoteViewModel>
+    {
+        Task<ViewEvidenceNoteViewModel> HandleAsync(Guid noteId);
+    }
+}
