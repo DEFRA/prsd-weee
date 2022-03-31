0442915653f5c1628f170cc61641c6843e530711
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-03-31 18:36:53 +0100

    made _ViewEvidenceNotePartial

--- src/EA.Weee.Web/Areas/Aatf/ViewModels/ViewEvidenceNoteViewModel.cs
+++ src/EA.Weee.Web/Areas/Aatf/ViewModels/ViewEvidenceNoteViewModel.cs
@@ -1,17 +1,28 @@
 ﻿namespace EA.Weee.Web.Areas.Aatf.ViewModels
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.Linq;
-    using System.Web.Mvc;
-    using Attributes;
-    using Core.AatfEvidence;
-    using Core.Helpers;
-    using Core.Scheme;
-
     public class ViewEvidenceNoteViewModel : EvidenceNoteViewModel
     {
         public bool DisplaySuccessMessage { get; set; }
+
+        public string ReferenceId { get; set; } = "E20";
+
+        public string Status { get; set; } = "Draft";
+
+        public int ComplianceYear { get; set; } = 2022;
+
+        public string TypeOfWaste { get; set; } = "Household";
+
+        public string Protocol { get; set; } = "Actual";
+
+        public string SiteName { get; set; } = "Recyling Team LTD";
+        public string SiteCode { get; set; } = "WEE/AB0001YZ/ATF";
+        public string SiteAddress { get; set; } = "12 May Lane <br />Hammersmith<br />London<br />SW13 TYH";
+
+        public string OperatorName { get; set; } = "Recycling Team LTD";
+        public string OperatorAddress { get; set; } = "12 May Lane <br />Hammersmith <br /> London <br /> SW13 TYH";
+
+        public string RecipientName { get; set; } = "ERP UK LTD";
+        public string RecipientAddress { get; set; } = "1 Bart Simpson Avenue <br /> Reading <br /> RG1 ATR";
+
     }
 }
