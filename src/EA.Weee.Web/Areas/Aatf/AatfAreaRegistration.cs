324648fff91bc010df21afc7d132da870fd16e43
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-31 17:08:57 +0100

    WIP routing

--- src/EA.Weee.Web/Areas/Aatf/AatfAreaRegistration.cs
+++ src/EA.Weee.Web/Areas/Aatf/AatfAreaRegistration.cs
@@ -3,6 +3,7 @@
     using Aatf.Controllers;
     using Infrastructure;
     using System.Web.Mvc;
+    using Core.AatfEvidence;
 
     public class AatfAreaRegistration : AreaRegistration
     {
@@ -15,13 +16,13 @@
             context.MapLowercaseDashedRoute(
                 name: AatfEvidenceRedirect.ViewSubmittedEvidenceRouteName,
                 url: "Aatf/{organisationId}/manage-evidence-notes/{aatfId}/view-submitted-evidence-note/{evidenceNoteId}",
-                defaults: new { action = "ViewDraftEvidenceNote", controller = "ManageEvidenceNotes" },
+                defaults: new { action = "ViewDraftEvidenceNote", controller = "ManageEvidenceNotes", noteStatus = NoteStatus.Submitted },
                 namespaces: new[] { typeof(ManageEvidenceNotesController).Namespace });
 
             context.MapLowercaseDashedRoute(
                 name: AatfEvidenceRedirect.ViewEvidenceRouteName,
                 url: "Aatf/{organisationId}/manage-evidence-notes/{aatfId}/view-draft-evidence-note/{evidenceNoteId}",
-                defaults: new { action = "ViewDraftEvidenceNote", controller = "ManageEvidenceNotes" },
+                defaults: new { action = "ViewDraftEvidenceNote", controller = "ManageEvidenceNotes", noteStatus = NoteStatus.Draft },
                 namespaces: new[] { typeof(ManageEvidenceNotesController).Namespace });
 
             context.MapLowercaseDashedRoute(
