c22638136aca408a1ee27858b6523e18781fa63f
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-03-31 13:17:49 +0100

    Updates to retrieval of an evidence note

--- src/EA.Weee.RequestHandlers/Mappings/EvidenceNoteMappingTransfer.cs
+++ src/EA.Weee.RequestHandlers/Mappings/EvidenceNoteMappingTransfer.cs
@@ -1,9 +1,12 @@
 ﻿namespace EA.Weee.RequestHandlers.Mappings
 {
+    using Core.Scheme;
     using EA.Weee.Domain.Evidence;
 
     public class EvidenceNoteMappingTransfer
     {
         public Note Note { get; set; }
+
+        public SchemeData SchemeData { get; set; }
     }
 }
