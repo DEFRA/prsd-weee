cd3c077adf19af820cb011a7fd304f0309221e85
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-03-31 11:42:42 +0100

    Get evidence note request handler

---
+++ src/EA.Weee.RequestHandlers/AatfEvidence/GetEvidenceNoteHandler.cs
@@ -0,0 +1,60 @@
+﻿namespace EA.Weee.RequestHandlers.AatfEvidence
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Aatf;
+    using AatfEvidence;
+    using Core.Helpers;
+    using Domain.Evidence;
+    using Domain.Lookup;
+    using Domain.Organisation;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.AatfEvidence;
+    using EA.Weee.DataAccess.DataAccess;
+    using EA.Weee.RequestHandlers.AatfReturn;
+    using EA.Weee.RequestHandlers.AatfReturn.Internal;
+    using EA.Weee.RequestHandlers.Mappings;
+    using Prsd.Core;
+    using Prsd.Core.Domain;
+    using Prsd.Core.Mediator;
+    using Requests.AatfEvidence;
+    using Security;
+    using Protocol = Domain.Evidence.Protocol;
+    using WasteType = Domain.Evidence.WasteType;
+
+    public class GetEvidenceNoteHandler : IRequestHandler<GetEvidenceNoteRequest, EvidenceNoteData>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly IGenericDataAccess genericDataAccess;
+        private readonly IAatfDataAccess aatfDataAccess;
+        private readonly IUserContext userContext;
+        private readonly IEvidenceDataAccess evidenceDataAccess;
+        private readonly IMapper mapper;
+
+        public GetEvidenceNoteHandler(IWeeeAuthorization authorization,
+            IGenericDataAccess genericDataAccess, 
+            IAatfDataAccess aatfDataAccess, 
+            IUserContext userContext,
+            IEvidenceDataAccess evidenceDataAccess) 
+        {
+            this.authorization = authorization;
+            this.genericDataAccess = genericDataAccess;
+            this.aatfDataAccess = aatfDataAccess;
+            this.userContext = userContext;
+            this.evidenceDataAccess = evidenceDataAccess;
+        }
+
+        public async Task<EvidenceNoteData> HandleAsync(GetEvidenceNoteRequest message)
+        {
+            authorization.EnsureCanAccessExternalArea();
+            authorization.EnsureOrganisationAccess(message.OrganisationId);
+
+            var evidenceNote = await evidenceDataAccess.GetNoteById(message.EvidenceNoteId);
+            var transfer = new EvidenceNoteMappingTransfer() { Note = evidenceNote };
+            var evidenceNoteData = mapper.Map<EvidenceNoteMappingTransfer, EvidenceNoteData>(transfer);
+
+            return evidenceNoteData;
+        }
+    }
+}
---
+++ src/EA.Weee.RequestHandlers/Mappings/EvidenceNoteMappingTransfer.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.RequestHandlers.Mappings
+{
+    using EA.Weee.Domain.Evidence;
+
+    public class EvidenceNoteMappingTransfer
+    {
+        public Note Note { get; set; }
+    }
+}
