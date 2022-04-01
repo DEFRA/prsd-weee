15a40fae6edaafdd75bf3072ba96570e4f351f39
Sreedhar B <Sreedhar.Bangarugari@civica.co.uk>
2022-04-01 11:33:08 +0100

    Worked on the search aatf search result screen changes

--- src/EA.Weee.Web/Areas/AatfReturn/Controllers/SearchedAatfResultListController.cs
+++ src/EA.Weee.Web/Areas/AatfReturn/Controllers/SearchedAatfResultListController.cs
@@ -4,12 +4,9 @@
     using EA.Prsd.Core.Mapper;
     using EA.Weee.Api.Client;
     using EA.Weee.Core.AatfReturn;
-    using EA.Weee.Core.DataReturns;
     using EA.Weee.Requests.AatfReturn;
     using EA.Weee.Requests.AatfReturn.Obligated;
-    using EA.Weee.Requests.Shared;
     using EA.Weee.Web.Areas.AatfReturn.Mappings.ToViewModel;
-    using EA.Weee.Web.Areas.AatfReturn.Requests;
     using EA.Weee.Web.Areas.AatfReturn.ViewModels;
     using EA.Weee.Web.Constant;
     using EA.Weee.Web.Controllers.Base;
@@ -17,6 +14,7 @@
     using EA.Weee.Web.Services;
     using EA.Weee.Web.Services.Caching;
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using System.Web.Mvc;
 
@@ -26,9 +24,9 @@
         private readonly Func<IWeeeClient> apiClient;
         private readonly BreadcrumbService breadcrumb;
         private readonly IWeeeCache cache;
-        private readonly IMap<ReturnToSearchedAnAatfViewModelMapTransfer, ReturnToSearchedAnAatfViewModelMap> mapper;
+        private readonly IMap<ReturnAndAatfToSearchedAatfViewModelMapTransfer, SearchedAatfResultListViewModel> mapper;
 
-        public SearchedAatfResultListController(Func<IWeeeClient> apiClient, BreadcrumbService breadcrumb, IWeeeCache cache, IMap<ReturnToSearchedAnAatfViewModelMapTransfer, ReturnToSearchedAnAatfViewModelMap> mapper)
+        public SearchedAatfResultListController(Func<IWeeeClient> apiClient, BreadcrumbService breadcrumb, IWeeeCache cache, IMap<ReturnAndAatfToSearchedAatfViewModelMapTransfer, SearchedAatfResultListViewModel> mapper)
         {
             this.apiClient = apiClient;
             this.breadcrumb = breadcrumb;
@@ -37,22 +35,34 @@
         }
 
         [HttpGet]
-        public virtual async Task<ActionResult> Index(Guid organisationId, Guid returnId, Guid aatfId)
+        public virtual async Task<ActionResult> Index(Guid organisationId, Guid returnId, Guid aatfId, Guid selectedAatfId, string selectedAatfName)
         {
             using (var client = apiClient())
             {
                 var weeeSentOn = await client.SendAsync(User.GetAccessToken(), new GetWeeeSentOn(aatfId, returnId, null));
-
-                var model = mapper.Map(new ReturnToSearchedAnAatfViewModelMapTransfer()
-                {                    
-                    AatfId = aatfId                    
-                });
-
                 var @return = await client.SendAsync(User.GetAccessToken(), new GetReturn(returnId, false));
 
                 await SetBreadcrumb(organisationId, BreadCrumbConstant.AatfReturn, aatfId, DisplayHelper.YearQuarterPeriodFormat(@return.Quarter, @return.QuarterWindow));
+                var aatfAddressList = await client.SendAsync(User.GetAccessToken(), new GetAatfAddressBySearchId(selectedAatfId));
 
-                return View(model);
+                var model = mapper.Map(new ReturnAndAatfToSearchedAatfViewModelMapTransfer()
+                {
+                    AatfId = aatfId,
+                    ReturnId = returnId,
+                    OrganisationId = organisationId,
+                    Sites = aatfAddressList,
+                    AatfName = selectedAatfName
+                });
+
+                if (model.Sites.Count > 0)
+                {
+                    return View(model);
+                }
+                else
+                {
+                    return await Task.Run<ActionResult>(() =>
+                    RedirectToAction("Index", "CanNotFoundTreatmentFacility", new { area = "AatfReturn", returnId = returnId, aatfId = aatfId, aatfName = selectedAatfName, isCanNotFindLinkClick = false }));
+                }
             }
         }
 
