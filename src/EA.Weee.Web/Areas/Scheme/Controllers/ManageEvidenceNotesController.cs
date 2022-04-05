08b0ae38a04eeee053d2cb1c277893a40773e31f
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-04-05 16:04:11 +0100

    Partial changes for review submitted evidence tab Adds in the routing and skeleton of the tab

---
+++ src/EA.Weee.Web/Areas/Scheme/Controllers/ManageEvidenceNotesController.cs
@@ -0,0 +1,58 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Controllers
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Requests.Scheme;
+    using EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels;
+    using EA.Weee.Web.Areas.Scheme.ViewModels.ManageEvidenceNotes;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Infrastructure;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class ManageEvidenceNotesController : SchemeEvidenceBaseController
+    {
+        private readonly Func<IWeeeClient> apiClient;
+        private readonly IMapper mapper;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+
+        public ManageEvidenceNotesController(IMapper mapper,
+            BreadcrumbService breadcrumb,
+            IWeeeCache cache,
+            Func<IWeeeClient> apiClient)
+        {
+            this.mapper = mapper;
+            this.breadcrumb = breadcrumb;
+            this.cache = cache;
+            this.apiClient = apiClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index(Guid organisationId, ManageEvidenceNotesDisplayOptions? activeDisplayOption = null)
+        {
+            using (var client = this.apiClient())
+            {
+                await SetBreadcrumb(organisationId, BreadCrumbConstant.SchemeManageEvidence);
+                var scheme = await client.SendAsync(this.User.GetAccessToken(), new GetSchemeByOrganisationId(organisationId));
+
+                var model = mapper.Map<ManageEvidenceNoteViewModel>(new SchemeDataToManageEvidenceNoteViewModelMapTransfer() { OrganisationId = scheme.OrganisationId, OrganisationName = scheme.Name });
+
+                return this.View("ReviewSubmittedEvidence", model);
+            }
+        }
+
+        private async Task SetBreadcrumb(Guid organisationId, string activity)
+        {
+            breadcrumb.ExternalOrganisation = await cache.FetchOrganisationName(organisationId);
+            breadcrumb.ExternalActivity = activity;
+            breadcrumb.OrganisationId = organisationId;
+        }
+    }
+}
