f3017a739392bcbc4f5600f5cc34e34c172329fd
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-04-06 16:31:52 +0100

    Added tests fro EditDraftReturnNotesViewModelMap

--- src/EA.Weee.Web/Areas/Aatf/Mappings/ToViewModel/EditDraftReturnNotesViewModelTransfer.cs
+++ src/EA.Weee.Web/Areas/Aatf/Mappings/ToViewModel/EditDraftReturnNotesViewModelTransfer.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using Core.AatfEvidence;
-    using Core.AatfReturn;
     using Prsd.Core;
 
     public class EditDraftReturnNotesViewModelTransfer
