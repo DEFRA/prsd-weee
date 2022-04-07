693bcf5ab72bbc8bd6961850140893f6fc4ee22f
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-04-07 22:51:39 +0100

    Partial changes for the review submitted evidence tab

---
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/ReviewSubmittedEvidenceNotesViewModelMapTransfer.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
+{
+    using EA.Prsd.Core;
+    using EA.Weee.Core.AatfEvidence;
+    using System;
+    using System.Collections.Generic;
+
+    public class ReviewSubmittedEvidenceNotesViewModelMapTransfer
+    {
+        public Guid OrganisationId { get; protected set; }
+
+        public List<EvidenceNoteData> Notes { get; protected set; }
+
+        public ReviewSubmittedEvidenceNotesViewModelMapTransfer(Guid organisationId, List<EvidenceNoteData> notes)
+        {
+            Guard.ArgumentNotDefaultValue(() => organisationId, organisationId);
+            Guard.ArgumentNotNull(() => notes, notes);
+
+            OrganisationId = organisationId;
+            Notes = notes;
+        }
+    }
+}
