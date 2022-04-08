ab895c19a45b2668e3b1b02f810dfa2508cc3696
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-04-08 09:09:56 +0100

    Code cleanup - removes redundant classes and using references

--- src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/ReviewSubmittedEvidenceNotesViewModelMap.cs
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/ReviewSubmittedEvidenceNotesViewModelMap.cs
@@ -4,10 +4,7 @@
     using EA.Weee.Web.Areas.Aatf.Mappings.ToViewModel;
     using EA.Weee.Web.Areas.Aatf.ViewModels;
     using EA.Weee.Web.Areas.Scheme.ViewModels.ManageEvidenceNotes;
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Web;
 
     public class ReviewSubmittedEvidenceNotesViewModelMap : IMap<ReviewSubmittedEvidenceNotesViewModelMapTransfer, ReviewSubmittedEvidenceNotesViewModel>
     {
