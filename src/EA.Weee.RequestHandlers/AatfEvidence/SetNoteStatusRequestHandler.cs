a854a6154aca09e2cedf9fc9012b9bfecca4895f
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-04-12 10:17:44 +0100

    all commits for tasks 42759 after merging develop into local branch.

---
+++ src/EA.Weee.RequestHandlers/AatfEvidence/SetNoteStatusRequestHandler.cs
@@ -0,0 +1,53 @@
+﻿namespace EA.Weee.RequestHandlers.AatfEvidence
+{
+    using EA.Prsd.Core;
+    using EA.Prsd.Core.Domain;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.DataAccess;
+    using EA.Weee.DataAccess.DataAccess;
+    using EA.Weee.RequestHandlers.Security;
+    using EA.Weee.Requests.Note;
+    using Prsd.Core.Mediator;
+    using System;
+    using System.Threading.Tasks;
+
+    public class SetNoteStatusRequestHandler : IRequestHandler<SetNoteStatus, Guid>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly WeeeContext context;
+        private readonly IUserContext userContext;
+
+        public SetNoteStatusRequestHandler(
+            WeeeContext context,
+            IUserContext userContext,
+            IWeeeAuthorization authorization)
+        {
+            this.authorization = authorization;
+            this.context = context;
+            this.userContext = userContext;
+        }
+
+        public async Task<Guid> HandleAsync(SetNoteStatus message)
+        {
+            authorization.EnsureCanAccessExternalArea();
+
+            var evidenceNote = await context.Notes.FindAsync(message.NoteId);
+
+            Guard.ArgumentNotNull(() => evidenceNote, evidenceNote, $"Evidence note {message.NoteId} not found");
+            
+            authorization.EnsureSchemeAccess(evidenceNote.Recipient.Id);
+
+            string changedBy = userContext.UserId.ToString();
+
+            if (message.Status.Equals(Core.AatfEvidence.NoteStatus.Submitted))
+            {
+                // status business rule will be checked in that method
+                evidenceNote.UpdateStatus(Domain.Evidence.NoteStatus.Approved, changedBy);
+            }
+
+            await context.SaveChangesAsync();
+
+            return message.NoteId;
+        }
+    }
+}
