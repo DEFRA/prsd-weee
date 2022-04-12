f4233a85ec79993dc608771a0fa2302b7b4a4ea2
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-04-12 10:17:44 +0100

    all commits for tasks 42759 after merging develop into local branch.

---
+++ src/EA.Weee.RequestHandlers/AatfEvidence/GetEvidenceNoteRequestHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace EA.Weee.RequestHandlers.AatfEvidence
+{
+    using System.Threading.Tasks;
+    using Domain.Evidence;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.AatfEvidence;
+    using EA.Weee.DataAccess.DataAccess;
+    using Prsd.Core;
+    using Prsd.Core.Mediator;
+    using Requests.AatfEvidence;
+    using Security;
+
+    internal class GetEvidenceNoteRequestHandler : IRequestHandler<GetEvidenceNoteRequest, EvidenceNoteData>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly IEvidenceDataAccess evidenceDataAccess;
+        private readonly IMapper mapper;
+
+        public GetEvidenceNoteRequestHandler(IWeeeAuthorization authorization,
+            IEvidenceDataAccess evidenceDataAccess,
+            IMapper mapper)
+        {
+            this.authorization = authorization;
+            this.evidenceDataAccess = evidenceDataAccess;
+            this.mapper = mapper;
+        }
+
+        public async Task<EvidenceNoteData> HandleAsync(GetEvidenceNoteRequest message)
+        {
+            authorization.EnsureCanAccessExternalArea();
+
+            var evidenceNote = await evidenceDataAccess.GetNoteById(message.EvidenceNoteId);
+
+            Guard.ArgumentNotNull(() => evidenceNote, evidenceNote, $"Evidence note {message.EvidenceNoteId} not found");
+
+            authorization.EnsureOrganisationAccess(evidenceNote.OrganisationId);
+
+            var evidenceNoteData = mapper.Map<Note, EvidenceNoteData>(evidenceNote);
+
+            return evidenceNoteData;
+        }
+    }
+}
