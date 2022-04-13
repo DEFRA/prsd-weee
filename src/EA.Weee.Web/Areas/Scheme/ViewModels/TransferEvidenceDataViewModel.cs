df199e78bcc0bae175ca8f83eefc8b9d0c60ba70
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-04-13 17:21:13 +0100

    Initial work started on Transfer evidence, selecte categories

---
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/TransferEvidenceDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.ViewModels
+{
+    using EA.Weee.Domain.Evidence;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    public class TransferEvidenceDataViewModel
+    {
+        public int SelectedNoteId { get; set; }
+        public Note SelectedNote { get; set; }
+        public IEnumerable<SelectListItem> NotesToDisplay { get; set; }
+    }
+}
