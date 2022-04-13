a2e23ba787f215dd861186a88bb354502357d0a0
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-04-13 17:21:13 +0100

    Initial work started on Transfer evidence, selecte categories

---
+++ src/EA.Weee.Web/Areas/Scheme/Controllers/TrasferEvidenceController.cs
@@ -0,0 +1,63 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Controllers
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Requests.AatfEvidence;
+    using EA.Weee.Requests.Organisations;
+    using EA.Weee.Requests.Scheme;
+    using EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels;
+    using EA.Weee.Web.Areas.Scheme.ViewModels;
+    using EA.Weee.Web.Areas.Scheme.ViewModels.ManageEvidenceNotes;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Infrastructure;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    public class TrasferEvidenceController : SchemeEvidenceBaseController
+    {
+        private readonly Func<IWeeeClient> apiClient;
+        private readonly IMapper mapper;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+
+        public TrasferEvidenceController(IMapper mapper,
+         BreadcrumbService breadcrumb,
+         IWeeeCache cache,
+         Func<IWeeeClient> apiClient)
+        {
+            this.mapper = mapper;
+            this.breadcrumb = breadcrumb;
+            this.cache = cache;
+            this.apiClient = apiClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index(Guid organisationId)
+        {
+            using (var client = this.apiClient())
+            {
+                await SetBreadcrumb(organisationId, BreadCrumbConstant.SchemeManageEvidence);
+
+                var result = await client.SendAsync(User.GetAccessToken(), new GetEvidenceNotesByOrganisationRequest(organisationId));
+
+                var scheme = await client.SendAsync(User.GetAccessToken(), new GetSchemeByOrganisationId(organisationId));
+
+                // build a list to pass 
+
+                var modelToDisplay = new TransferEvidenceDataViewModel();
+
+                return this.View(modelToDisplay);
+            }
+        }
+
+        private async Task SetBreadcrumb(Guid organisationId, string activity)
+        {
+            breadcrumb.ExternalOrganisation = await cache.FetchOrganisationName(organisationId);
+            breadcrumb.ExternalActivity = activity;
+            breadcrumb.OrganisationId = organisationId;
+        }
+    }
+}
