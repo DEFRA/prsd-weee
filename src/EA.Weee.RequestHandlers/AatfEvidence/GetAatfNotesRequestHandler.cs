02368dabbd88f76e430793335480e671d6bcac8d
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-04-14 14:32:46 +0100

    Updates to the reference display matching and some related unit tests

--- src/EA.Weee.RequestHandlers/AatfEvidence/GetAatfNotesRequestHandler.cs
+++ src/EA.Weee.RequestHandlers/AatfEvidence/GetAatfNotesRequestHandler.cs
@@ -1,18 +1,17 @@
 ﻿namespace EA.Weee.RequestHandlers.AatfEvidence
 {
     using Core.AatfEvidence;
+    using Core.Helpers;
+    using DataAccess.DataAccess;
+    using Domain.Evidence;
     using EA.Prsd.Core.Mapper;
     using EA.Prsd.Core.Mediator;
-    using EA.Weee.RequestHandlers.AatfReturn.Internal;
-    using EA.Weee.RequestHandlers.Mappings;
-    using EA.Weee.RequestHandlers.Security;
     using EA.Weee.Requests.AatfEvidence;
+    using Mappings;
+    using Security;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-    using Core.Helpers;
-    using DataAccess.DataAccess;
-    using NoteStatus = Core.AatfEvidence.NoteStatus;
 
     public class GetAatfNotesRequestHandler : IRequestHandler<GetAatfNotesRequest, List<EvidenceNoteData>>
     {
