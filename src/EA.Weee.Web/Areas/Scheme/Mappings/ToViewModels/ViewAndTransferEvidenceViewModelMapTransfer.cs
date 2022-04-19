da93229d05eb84b0888bc965b4df889c300fca21
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-04-19 16:20:33 +0100

    Review Changes

--- src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/ViewAndTransferEvidenceViewModelMapTransfer.cs
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/ViewAndTransferEvidenceViewModelMapTransfer.cs
@@ -1,27 +1,13 @@
 ﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
 {
-    using EA.Prsd.Core;
     using EA.Weee.Core.AatfEvidence;
     using System;
     using System.Collections.Generic;
 
-    public class ViewAndTransferEvidenceViewModelMapTransfer
+    public class ViewAndTransferEvidenceViewModelMapTransfer : BaseEvidenceNotesViewModelMapTransfer
     {
-        public Guid OrganisationId { get; protected set; }
-
-        public string SchemeName { get; protected set; }
-
-        public List<EvidenceNoteData> Notes { get; protected set; }
-
-        public ViewAndTransferEvidenceViewModelMapTransfer(Guid organisationId, List<EvidenceNoteData> notes, string schemeName)
+        public ViewAndTransferEvidenceViewModelMapTransfer(Guid organisationId, List<EvidenceNoteData> notes, string schemeName) : base(organisationId, notes, schemeName)
         {
-            Guard.ArgumentNotDefaultValue(() => organisationId, organisationId);
-            Guard.ArgumentNotNull(() => notes, notes);
-            Guard.ArgumentNotNull(() => schemeName, schemeName);
-
-            OrganisationId = organisationId;
-            Notes = notes;
-            SchemeName = schemeName;
         }
     }
 }
