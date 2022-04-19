e42b1e07bea1182d91ab1aa47c97ef9c27da99e7
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-04-19 13:25:38 +0100

    Partial Changes for View and Transfer Evidence Tab

---
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/ViewAndTransferEvidenceViewModelMapTransfer.cs
@@ -0,0 +1,27 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
+{
+    using EA.Prsd.Core;
+    using EA.Weee.Core.AatfEvidence;
+    using System;
+    using System.Collections.Generic;
+
+    public class ViewAndTransferEvidenceViewModelMapTransfer
+    {
+        public Guid OrganisationId { get; protected set; }
+
+        public string SchemeName { get; protected set; }
+
+        public List<EvidenceNoteData> Notes { get; protected set; }
+
+        public ViewAndTransferEvidenceViewModelMapTransfer(Guid organisationId, List<EvidenceNoteData> notes, string schemeName)
+        {
+            Guard.ArgumentNotDefaultValue(() => organisationId, organisationId);
+            Guard.ArgumentNotNull(() => notes, notes);
+            Guard.ArgumentNotNull(() => schemeName, schemeName);
+
+            OrganisationId = organisationId;
+            Notes = notes;
+            SchemeName = schemeName;
+        }
+    }
+}
