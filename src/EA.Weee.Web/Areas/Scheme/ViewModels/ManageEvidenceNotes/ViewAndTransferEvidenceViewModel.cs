e1c13556c3ef5d9af945fae3d153b629d5a90525
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-04-20 11:24:16 +0100

    Merge from develop

--- src/EA.Weee.Web/Areas/Scheme/ViewModels/ManageEvidenceNotes/ViewAndTransferEvidenceViewModel.cs
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/ManageEvidenceNotes/ViewAndTransferEvidenceViewModel.cs
@@ -1,19 +1,14 @@
 ﻿namespace EA.Weee.Web.Areas.Scheme.ViewModels.ManageEvidenceNotes
 {
-    using EA.Weee.Web.Areas.Aatf.ViewModels;
     using System;
-    using System.Collections.Generic;
 
     public class ViewAndTransferEvidenceViewModel : ManageEvidenceNoteViewModel
     {
         public Guid? SelectedId { get; set; }
 
-        public IList<EditDraftReturnedNote> EvidenceNotesDataList { get; set; }
-
         public ViewAndTransferEvidenceViewModel()
             : base(ManageEvidenceNotesDisplayOptions.ViewAndTransferEvidence)
         {
-            EvidenceNotesDataList = new List<EditDraftReturnedNote>();
         }
     }
 }
