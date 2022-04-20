56a72431889b8d20fa7dd797fca04b4db90e08a8
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-04-20 11:15:35 +0100

    Add review evidence view

--- src/EA.Weee.Web/Areas/Scheme/ViewModels/ManageEvidenceNotes/ReviewEvidenceNoteViewModel.cs
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/ManageEvidenceNotes/ReviewEvidenceNoteViewModel.cs
@@ -1,9 +1,7 @@
 ﻿namespace EA.Weee.Web.Areas.Scheme.ViewModels.ManageEvidenceNotes
 {
     using EA.Weee.Web.Areas.Aatf.ViewModels;
-    using EA.Weee.Web.ViewModels.Shared;
     using System;
-    using NoteStatus = Core.AatfEvidence.NoteStatus;
 
     public class ReviewEvidenceNoteViewModel
     {
