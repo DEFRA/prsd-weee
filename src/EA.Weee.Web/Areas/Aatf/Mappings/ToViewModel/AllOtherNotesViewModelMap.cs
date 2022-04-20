914d2c41391ed360d5df45d22bc221e82f6adefa
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-04-20 09:57:45 +0100

    Refactor the list model to have base class to reuse code

--- src/EA.Weee.Web/Areas/Aatf/Mappings/ToViewModel/AllOtherNotesViewModelMap.cs
+++ src/EA.Weee.Web/Areas/Aatf/Mappings/ToViewModel/AllOtherNotesViewModelMap.cs
@@ -1,28 +1,21 @@
 ﻿namespace EA.Weee.Web.Areas.Aatf.Mappings.ToViewModel
 {
-    using System.Collections.Generic;
     using EA.Prsd.Core;
     using EA.Weee.Core.AatfEvidence;
     using Prsd.Core.Mapper;
     using ViewModels;
 
-    public class AllOtherNotesViewModelMap : IMap<EditDraftReturnNotesViewModelTransfer, AllOtherEvidenceNotesViewModel>
+    public class AllOtherNotesViewModelMap : ListOfNotesViewModelBase<AllOtherEvidenceNotesViewModel>, IMap<EditDraftReturnNotesViewModelTransfer, AllOtherEvidenceNotesViewModel>
     {
-        private readonly IMapper mapper;
-
-        public AllOtherNotesViewModelMap(IMapper mapper)
+        public AllOtherNotesViewModelMap(IMapper mapper) : base(mapper)
         {
-            this.mapper = mapper;
         }
 
         public AllOtherEvidenceNotesViewModel Map(EditDraftReturnNotesViewModelTransfer source)
         {
             Guard.ArgumentNotNull(() => source, source);
 
-            var model = new AllOtherEvidenceNotesViewModel
-            {
-                ListOfNotes = mapper.Map<List<EvidenceNoteRowViewModel>>(source.Notes)
-            };
+            var model = Map(source.Notes);
 
             foreach (var evidenceNoteRowViewModel in model.ListOfNotes)
             {
--- src/EA.Weee.Web/Areas/Aatf/Mappings/ToViewModel/EditDraftReturnNotesViewModelMap.cs
+++ src/EA.Weee.Web/Areas/Aatf/Mappings/ToViewModel/EditDraftReturnNotesViewModelMap.cs
@@ -1,30 +1,20 @@
 ﻿namespace EA.Weee.Web.Areas.Aatf.Mappings.ToViewModel
 {
-    using System.Collections.Generic;
-    using System.Linq;
     using EA.Prsd.Core;
     using Prsd.Core.Mapper;
     using ViewModels;
 
-    public class EditDraftReturnNotesViewModelMap : IMap<EditDraftReturnNotesViewModelTransfer, EditDraftReturnedNotesViewModel>
+    public class EditDraftReturnNotesViewModelMap : ListOfNotesViewModelBase<EditDraftReturnedNotesViewModel>, IMap<EditDraftReturnNotesViewModelTransfer, EditDraftReturnedNotesViewModel>
     {
-        private readonly IMapper mapper;
-
-        public EditDraftReturnNotesViewModelMap(IMapper mapper)
+        public EditDraftReturnNotesViewModelMap(IMapper mapper) : base(mapper)
         {
-            this.mapper = mapper;
         }
 
         public EditDraftReturnedNotesViewModel Map(EditDraftReturnNotesViewModelTransfer source)
         {
             Guard.ArgumentNotNull(() => source, source);
 
-            var model = new EditDraftReturnedNotesViewModel
-            {
-                ListOfNotes = mapper.Map<List<EvidenceNoteRowViewModel>>(source.Notes)
-            };
-
-            return model;
+            return Map(source.Notes);
         }
     }
 }
