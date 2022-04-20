ce2ad9e7eeb7be767622b6d43db9804abaeded6b
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-04-20 10:37:47 +0100

    Adds unit tests for View and Transfer Evidence Notes

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/Mapping/ViewAndTransferEvidenceViewModelMapTests.cs
@@ -0,0 +1,151 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Scheme.Mapping
+{
+    using AutoFixture;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.AatfEvidence;
+    using EA.Weee.Web.Areas.Aatf.Mappings.ToViewModel;
+    using EA.Weee.Web.Areas.Aatf.ViewModels;
+    using EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels;
+    using EA.Weee.Web.Areas.Scheme.ViewModels.ManageEvidenceNotes;
+    using FakeItEasy;
+    using FluentAssertions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+
+    public class ViewAndTransferEvidenceViewModelMapTests
+    {
+        public readonly ViewAndTransferEvidenceViewModelMap Map;
+        private readonly Fixture fixture;
+        private readonly IMapper mapper;
+
+        public ViewAndTransferEvidenceViewModelMapTests()
+        {
+            mapper = A.Fake<IMapper>();
+
+            Map = new ViewAndTransferEvidenceViewModelMap(mapper);
+
+            fixture = new Fixture();
+        }
+
+        [Fact]
+        public void Map_GiveListOfNotesIsNull_ArgumentNullExceptionExpected()
+        {
+            //act
+            var exception = Record.Exception(() => new ViewAndTransferEvidenceViewModelMapTransfer(Guid.NewGuid(), null, "Test"));
+
+            //assert
+            exception.Should().BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Map_GivenOrganisationGuidIsEmpty_ArgumentExceptionExpected()
+        {
+            //act
+            var exception = Record.Exception(() => new ViewAndTransferEvidenceViewModelMapTransfer(Guid.Empty, fixture.CreateMany<EvidenceNoteData>().ToList(), "Test"));
+
+            //assert
+            exception.Should().BeOfType<ArgumentException>();
+        }
+
+        [Fact]
+        public void Map_GivenSchemeNameIsNull_ArgumentNullExceptionExpected()
+        {
+            //act
+            var exception = Record.Exception(() => new ViewAndTransferEvidenceViewModelMapTransfer(Guid.NewGuid(), fixture.CreateMany<EvidenceNoteData>().ToList(), null));
+
+            //assert
+            exception.Should().BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Map_GivenListOfEvidenceNotes_MapperShouldBeCalled()
+        {
+            //arrange
+            var notes = new List<EvidenceNoteData>
+            {
+                 fixture.Create<EvidenceNoteData>(),
+                 fixture.Create<EvidenceNoteData>(),
+                 fixture.Create<EvidenceNoteData>()
+            };
+
+            var organisationId = Guid.NewGuid();
+
+            var transfer = new ViewAndTransferEvidenceViewModelMapTransfer(organisationId, notes, "Test");
+
+            //act
+            Map.Map(transfer);
+
+            // assert 
+            A.CallTo(() => mapper.Map<EditDraftReturnedNote>(A<EditDraftReturnedNotesModel>._)).MustHaveHappened(notes.Count, Times.Exactly);
+        }
+
+        [Fact]
+        public void Map_GivenAnEmptyListOfEvidenceNotes_MapperShouldNotBeCalled()
+        {
+            //arrange
+            var notes = new List<EvidenceNoteData>();
+
+            var organisationId = Guid.NewGuid();
+
+            var transfer = new ViewAndTransferEvidenceViewModelMapTransfer(organisationId, notes, "Test");
+
+            //act
+            Map.Map(transfer);
+
+            // assert 
+            A.CallTo(() => mapper.Map<EditDraftReturnedNote>(A<EditDraftReturnedNotesModel>._)).MustHaveHappened(0, Times.Exactly);
+        }
+
+        [Fact]
+        public void Map_GivenAnEmptyListOfEvidenceNotes_MustReturnAnEmptyModel()
+        {
+            //arrange
+            var notes = new List<EvidenceNoteData>();
+
+            var organisationId = Guid.NewGuid();
+
+            var transfer = new ViewAndTransferEvidenceViewModelMapTransfer(organisationId, notes, "Test");
+
+            //act
+            var result = Map.Map(transfer);
+
+            // assert 
+            result.EvidenceNotesDataList.Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public void Map_GivenListOfEvidenceNoteData_ShouldReturnMappedData()
+        {
+            //arrange
+            var notes = fixture.CreateMany<EvidenceNoteData>().ToList();
+
+            var returnedNotes = new List<EditDraftReturnedNote>
+            {
+                 fixture.Create<EditDraftReturnedNote>(),
+                 fixture.Create<EditDraftReturnedNote>(),
+                 fixture.Create<EditDraftReturnedNote>()
+            };
+
+            var model = new ViewAndTransferEvidenceViewModel();
+            model.EvidenceNotesDataList = returnedNotes;
+
+            var organisationId = Guid.NewGuid();
+
+            var transfer = new ViewAndTransferEvidenceViewModelMapTransfer(organisationId, notes, "Test");
+
+            foreach (var note in model.EvidenceNotesDataList)
+            {
+                A.CallTo(() => mapper.Map<EditDraftReturnedNote>(A<EditDraftReturnedNotesModel>._)).ReturnsNextFromSequence(note);
+            }
+
+            //act
+            var result = Map.Map(transfer);
+
+            // assert
+            result.EvidenceNotesDataList.Should().NotBeEmpty();
+            result.EvidenceNotesDataList.Should().BeEquivalentTo(returnedNotes);
+        }
+    }
+}
