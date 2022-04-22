5f24fa2cb7a9697ae532a66898364168d3e23f26
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-04-22 09:59:54 +0100

    Fix compile issue

--- src/EA.Weee.Requests/AatfEvidence/GetEvidenceNoteForAatfRequest.cs
+++ src/EA.Weee.Requests/AatfEvidence/GetEvidenceNoteForAatfRequest.cs
@@ -1,9 +1,6 @@
 ﻿namespace EA.Weee.Requests.AatfEvidence
 {
     using System;
-    using Core.AatfEvidence;
-    using EA.Prsd.Core;
-    using Prsd.Core.Mediator;
 
     [Serializable]
     public class GetEvidenceNoteForAatfRequest : GetEvidenceNoteBaseRequest
