52445663079520b15c8f71eb7d9538c35f681795
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-04-26 13:52:23 +0100

    Add unit tests for tab models

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Aatf/ViewModels/EditDraftReturnedNotesViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Aatf.ViewModels
+{
+    using FluentAssertions;
+    using Web.Areas.Aatf.ViewModels;
+    using Xunit;
+
+    public class EditDraftReturnedNotesViewModelTests
+    {
+        [Fact]
+        public void EditDraftReturnedNotesViewModel_Constructor_ShouldSetTab()
+        {
+            var model = new EditDraftReturnedNotesViewModel();
+
+            model.ActiveOverviewDisplayOption.Should().Be(ManageEvidenceOverviewDisplayOption.EditDraftAndReturnedNotes);
+        }
+
+        [Fact]
+        public void EditDraftReturnedNotesViewModel_Constructor_ShouldInitialiseList()
+        {
+            var model = new EditDraftReturnedNotesViewModel();
+
+            model.EvidenceNotesDataList.Should().NotBeNull();
+            model.EvidenceNotesDataList.Should().BeEmpty();
+        }
+    }
+}
