f234b722e3f67717eac7b661f857795477b743b3
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-04-27 09:26:28 +0100

    Move transfer evidence into its own controller

---
+++ src/EA.Weee.Web/Services/SessionService.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Weee.Web.Services
+{
+    using System.Web;
+    using Weee.Requests.Scheme;
+
+    public class SessionService : ISessionService
+    {
+        public void SetTransferNoteSessionObject(HttpSessionStateBase session, TransferEvidenceNoteRequest request)
+        {
+            session["TransferEvidenceNoteData"] = request;
+        }
+    }
+}
