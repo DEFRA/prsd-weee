46d74e0fae5109510ebc4e2bc3809a8c16650cb1
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-04-27 18:03:59 +0100

    commited SetNoteStatusRequestHandler Unit Tests

--- src/EA.Weee.RequestHandlers.Tests.Unit/AatfEvidence/SetNoteStatusRequestHandlerTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/AatfEvidence/SetNoteStatusRequestHandlerTests.cs
@@ -5,14 +5,14 @@
     using Domain.Scheme;
     using EA.Prsd.Core.Domain;
     using EA.Weee.Domain.Evidence;
+    using EA.Weee.RequestHandlers.AatfReturn;
     using EA.Weee.RequestHandlers.Security;
     using EA.Weee.Requests.Note;
     using FakeItEasy;
-    using RequestHandlers.Scheme;
+    using RequestHandlers.AatfEvidence;
     using System;
     using System.Security;
     using System.Threading.Tasks;
-    using RequestHandlers.AatfEvidence;
     using Weee.Tests.Core;
     using Xunit;
 
@@ -23,7 +23,9 @@
         private readonly IWeeeAuthorization authorization;
         private readonly Fixture fixture;
         private readonly Note note;
+        private readonly Note nullNote;
         private readonly Guid evidenceNoteId;
+        private readonly Scheme recipient;
         private readonly Guid recipientId;
 
         public SetNoteStatusRequestHandlerTests()
@@ -36,8 +38,11 @@
             A.Fake<IUserContext>();
             A.Fake<Scheme>();
             note = A.Fake<Note>();
+            nullNote = null;
+
             fixture.Create<Guid>();
             evidenceNoteId = fixture.Create<Guid>();
+            recipient = fixture.Create<Scheme>();
             recipientId = fixture.Create<Guid>();
         }
 
@@ -80,12 +85,95 @@
             A.CallTo(() => context.Notes.FindAsync(evidenceNoteId)).Returns(note);
 
             // Act
-            Func<Task<Guid>> action = () => handler.HandleAsync(message);
+            var result = handler.HandleAsync(message);
 
             // Assert
             var actual = note.Status;
             var expected = Domain.Evidence.NoteStatus.Approved;
             Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(AuthorizationBuilder.UserType.External, Core.AatfEvidence.NoteStatus.Approved)]
+        public void ExternalUser_WithIncorrectSchemeCannotSetApprovedStatus(AuthorizationBuilder.UserType userType, Core.AatfEvidence.NoteStatus status)
+        {
+            // Arrange
+            var authorization = AuthorizationBuilder.CreateFromUserType(userType);
+            var handler = new SetNoteStatusRequestHandler(context, userContext, authorization);
+            var noteId = new Guid("3C367528-AE93-427F-A4C5-E23F0D317633");
+            var recipientId = new Guid("66d5b5fa-28a7-4fa3-8426-6f284288921f");
+            SetNoteStatus message = new SetNoteStatus(noteId, status);
+            A.CallTo(() => note.Status).Returns(Domain.Evidence.NoteStatus.Approved);
+            A.CallTo(() => note.Recipient.Id).Returns(Guid.Empty);     // this guarantees the recipient id is different than scheme id
+            A.CallTo(() => context.Notes.FindAsync(evidenceNoteId)).Returns(note);
+
+            // Act
+            Func<Task<Guid>> action = () => handler.HandleAsync(message);
+
+            // Assert
+            Assert.ThrowsAsync<SecurityException>(action);
+        }
+
+        [Theory]
+        [InlineData(AuthorizationBuilder.UserType.External, Core.AatfEvidence.NoteStatus.Approved)]
+        public void ExternalUser_WithNoteNotFound_ThrowsException(AuthorizationBuilder.UserType userType, Core.AatfEvidence.NoteStatus status)
+        {
+            // Arrange
+            var authorization = AuthorizationBuilder.CreateFromUserType(userType);
+            var handler = new SetNoteStatusRequestHandler(context, userContext, authorization);
+            var noteId = new Guid("3C367528-AE93-427F-A4C5-E23F0D317633");
+            var recipientId = new Guid("66d5b5fa-28a7-4fa3-8426-6f284288921f");
+            SetNoteStatus message = new SetNoteStatus(noteId, status);
+            A.CallTo(() => note.Status).Returns(Domain.Evidence.NoteStatus.Approved);
+            A.CallTo(() => note.Recipient.Id).Returns(recipientId);
+            A.CallTo(() => context.Notes.FindAsync(evidenceNoteId)).Returns(nullNote);
+
+            // Act
+            Func<Task<Guid>> action = () => handler.HandleAsync(message);
+
+            // Assert
+            Assert.ThrowsAsync<SecurityException>(action);
+        }
+
+        [Theory]
+        [InlineData(AuthorizationBuilder.UserType.External, Core.AatfEvidence.NoteStatus.Approved)]
+        public void ExternalUser_WithNoteFound_ReturnsCorrectNoteId(AuthorizationBuilder.UserType userType, Core.AatfEvidence.NoteStatus status)
+        {
+            // Arrange
+            var authorization = AuthorizationBuilder.CreateFromUserType(userType);
+            var handler = new SetNoteStatusRequestHandler(context, userContext, authorization);
+            var newNote = fixture.Create<Note>();
+            A.CallTo(() => note.Status).Returns(Domain.Evidence.NoteStatus.Approved);
+            A.CallTo(() => note.Recipient.Id).Returns(recipientId);
+            A.CallTo(() => context.Notes.FindAsync(newNote.Id)).Returns(newNote);
+            SetNoteStatus message = new SetNoteStatus(newNote.Id, status);
+
+            // Act
+            var result = handler.HandleAsync(message);
+
+            // Assert
+            Assert.Equal(Guid.Empty, newNote.Id);
+        }
+
+        [Theory]
+        [InlineData(AuthorizationBuilder.UserType.External, Core.AatfEvidence.NoteStatus.Approved)]
+        public void ExternalUser_WithNoteUpdate_SaveChangesAsyncShouldBeCalled(AuthorizationBuilder.UserType userType, Core.AatfEvidence.NoteStatus status)
+        {
+            // Arrange
+            var authorization = AuthorizationBuilder.CreateFromUserType(userType);
+            var handler = new SetNoteStatusRequestHandler(context, userContext, authorization);
+            var newNote = A.Fake<Note>();
+            SetNoteStatus message = new SetNoteStatus(note.Id, status);
+            A.CallTo(() => newNote.Status).Returns(Domain.Evidence.NoteStatus.Approved);
+            A.CallTo(() => newNote.Recipient.Id).Returns(recipientId);
+            A.CallTo(() => context.Notes.FindAsync(note.Id)).Returns(newNote);
+            A.CallTo(() => newNote.UpdateStatus(NoteStatus.Approved, "changed by"));
+
+            // Act
+            var result = handler.HandleAsync(message);
+
+            // Assert
+            A.CallTo(() => context.SaveChangesAsync()).MustHaveHappenedOnceExactly();
         }
     }
 }
