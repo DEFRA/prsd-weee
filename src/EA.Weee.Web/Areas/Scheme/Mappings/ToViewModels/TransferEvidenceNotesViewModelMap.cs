5efd2c0c6fa61ed88ad4ddbde9cad06d523a5ac6
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-04-29 10:21:04 +0100

    WIP

---
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/TransferEvidenceNotesViewModelMap.cs
@@ -0,0 +1,36 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
+{
+    using Core.Shared;
+    using CuttingEdge.Conditions;
+    using EA.Prsd.Core.Mapper;
+    using Filters;
+    using Services.Caching;
+    using ViewModels;
+
+    public class TransferEvidenceNotesViewModelMap : IMap<TransferEvidenceNotesViewModelMapTransfer, TransferEvidenceNotesViewModel>
+    {
+        private readonly IWeeeCache cache;
+
+        public TransferEvidenceNotesViewModelMap(IMapper mapper, IWeeeCache cache)
+        {
+            this.cache = cache;
+        }
+
+        public TransferEvidenceNotesViewModel Map(TransferEvidenceNotesViewModelMapTransfer source)
+        {
+            Condition.Requires(source).IsNotNull();
+
+            var model = new TransferEvidenceNotesViewModel()
+            {
+                PcsId = source.OrganisationId,
+                RecipientName = AsyncHelpers.RunSync(async () => await cache.FetchSchemeName(source.Request.SchemeId))
+            };
+
+            foreach (var requestCategoryId in source.Request.CategoryIds)
+            {
+                model.CategoryValues.Add(new CategoryValue((Core.DataReturns.WeeeCategory)requestCategoryId));
+            }
+            return model;
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/TransferEvidenceNotesViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using Core.Shared;
+
+    public class TransferEvidenceNotesViewModel
+    {
+        public Guid PcsId { get; set; }
+
+        public Guid RecipientId { get; set; }
+
+        public string RecipientName { get; set; }
+
+        public List<CategoryValue> CategoryValues { get; set; }
+
+        public TransferEvidenceNotesViewModel()
+        {
+            CategoryValues = new List<CategoryValue>();
+        }
+    }
+}
