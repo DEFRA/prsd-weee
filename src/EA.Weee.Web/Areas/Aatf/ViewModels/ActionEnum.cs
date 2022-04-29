7d7ba24b5d4da3ec1dddeeaed12f1a891952e076
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-04-29 16:13:42 +0100

    Partial changes for Copy Paste Tonnage

--- src/EA.Weee.Web/Areas/Aatf/ViewModels/ActionEnum.cs
+++ src/EA.Weee.Web/Areas/Aatf/ViewModels/ActionEnum.cs
@@ -1,14 +1,10 @@
 ﻿namespace EA.Weee.Web.Areas.Aatf.ViewModels
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
-
     public enum ActionEnum
     {
         Save = 1,
         Submit = 2,
-        Edit = 3
+        Edit = 3,
+        CopyAndPaste = 4,
     }
 }
