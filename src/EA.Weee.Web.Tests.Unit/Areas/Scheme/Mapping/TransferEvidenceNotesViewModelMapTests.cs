44663fc58d958b437be059b7fb8c1aa34004b1a1
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-04 07:48:05 +0100

    Add TransferEvidenceNotesViewModelMapTransferTests

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/Mapping/TransferEvidenceNotesViewModelMapTests.cs
@@ -0,0 +1,28 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Scheme.Mapping
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using FakeItEasy;
+    using Prsd.Core.Mapper;
+    using Services.Caching;
+    using Web.Areas.Scheme.Mappings.ToViewModels;
+
+    public class TransferEvidenceNotesViewModelMapTests
+    {
+        private readonly IWeeeCache cache;
+        private readonly IMapper mapper;
+
+        private readonly TransferEvidenceNotesViewModelMap map;
+
+        public TransferEvidenceNotesViewModelMapTests()
+        {
+            cache = A.Fake<IWeeeCache>();
+            mapper = A.Fake<IMapper>();
+
+            map = new TransferEvidenceNotesViewModelMap(mapper, cache);
+        }
+    }
+}
