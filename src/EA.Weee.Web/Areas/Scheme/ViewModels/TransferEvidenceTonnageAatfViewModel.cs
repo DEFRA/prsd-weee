bbb905c36a2de92d274c1d342700abb2660a37df
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-04 16:37:17 +0100

    Some work on the transfer tonnage screen

---
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/TransferEvidenceTonnageAatfViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.ViewModels
+{
+    using System.ComponentModel;
+
+    public class TransferEvidenceTonnageAatfViewModel
+    {
+        public string Aatf { get; set; }
+
+        public string Received { get; set; }
+    }
+}
