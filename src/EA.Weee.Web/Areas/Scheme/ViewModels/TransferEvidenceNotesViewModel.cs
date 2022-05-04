26b056148f23e4852861897b7b597cdd364601cf
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-04 08:25:48 +0100

    Add TransferEvidenceNotesViewModelTests

--- src/EA.Weee.Web/Areas/Scheme/ViewModels/TransferEvidenceNotesViewModel.cs
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/TransferEvidenceNotesViewModel.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    using System.Data.Entity.Core.Metadata.Edm;
     using System.Linq;
     using Core.Shared;
     using Web.ViewModels.Shared;
