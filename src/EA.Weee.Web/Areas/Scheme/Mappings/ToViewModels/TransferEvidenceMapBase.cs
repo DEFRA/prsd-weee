ed6b1f7c474e623535a539587f8c6eb64eabdda5
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-04 12:04:13 +0100

    Refactor to have base classes for the transfer note and tonnage

---
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/TransferEvidenceMapBase.cs
@@ -0,0 +1,40 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
+{
+    using CuttingEdge.Conditions;
+    using Filters;
+    using Prsd.Core.Mapper;
+    using Services.Caching;
+    using ViewModels;
+    using Web.ViewModels.Shared;
+    using Web.ViewModels.Shared.Mapping;
+
+    public abstract class TransferEvidenceMapBase<T> where T : TransferEvidenceViewModelBase, new()
+    {
+        protected readonly IWeeeCache Cache;
+        protected readonly IMapper Mapper;
+
+        protected TransferEvidenceMapBase(IMapper mapper, IWeeeCache cache)
+        {
+            Mapper = mapper;
+            Cache = cache;
+        }
+
+        protected T MapBaseProperties(TransferEvidenceNotesViewModelMapTransfer source)
+        {
+            Condition.Requires(source).IsNotNull();
+
+            var model = new T()
+            {
+                PcsId = source.OrganisationId,
+                RecipientName = AsyncHelpers.RunSync(async () => await Cache.FetchSchemeName(source.Request.SchemeId))
+            };
+
+            foreach (var evidenceNoteData in source.Notes)
+            {
+                model.EvidenceNotesDataList.Add(Mapper.Map<ViewEvidenceNoteViewModel>(new ViewEvidenceNoteMapTransfer(evidenceNoteData, null)));
+            }
+
+            return model;
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/TransferEvidenceTonnageViewModelMap.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
+{
+    using CuttingEdge.Conditions;
+    using EA.Prsd.Core.Mapper;
+    using Services.Caching;
+    using ViewModels;
+
+    public class TransferEvidenceTonnageViewModelMap : TransferEvidenceMapBase<TransferEvidenceTonnageViewModel>, IMap<TransferEvidenceNotesViewModelMapTransfer, TransferEvidenceTonnageViewModel>
+    {
+        public TransferEvidenceTonnageViewModelMap(IMapper mapper, IWeeeCache cache) : base(mapper, cache)
+        {
+        }
+
+        public TransferEvidenceTonnageViewModel Map(TransferEvidenceNotesViewModelMapTransfer source)
+        {
+            Condition.Requires(source).IsNotNull();
+            Condition.Requires(source.Request).IsNotNull();
+
+            var model = MapBaseProperties(source);
+            
+            return model;
+        }
+    }
+}
