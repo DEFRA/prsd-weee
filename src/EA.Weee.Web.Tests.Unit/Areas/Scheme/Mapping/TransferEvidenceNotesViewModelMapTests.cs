3ec7f65b39e6338f7fd112034a64ca8401d40a13
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-04 08:13:56 +0100

    Add TransferEvidenceNotesViewModelMapTests

--- src/EA.Weee.Web.Tests.Unit/Areas/Scheme/Mapping/TransferEvidenceNotesViewModelMapTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/Mapping/TransferEvidenceNotesViewModelMapTests.cs
@@ -3,17 +3,25 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using AutoFixture;
+    using Core.AatfEvidence;
+    using Core.Helpers;
     using FakeItEasy;
+    using FluentAssertions;
     using Prsd.Core.Mapper;
     using Services.Caching;
     using Web.Areas.Scheme.Mappings.ToViewModels;
+    using Web.Areas.Scheme.ViewModels;
+    using Web.ViewModels.Shared;
+    using Web.ViewModels.Shared.Mapping;
+    using Weee.Requests.Scheme;
+    using Xunit;
 
     public class TransferEvidenceNotesViewModelMapTests
     {
         private readonly IWeeeCache cache;
         private readonly IMapper mapper;
+        private readonly Fixture fixture;
 
         private readonly TransferEvidenceNotesViewModelMap map;
 
@@ -21,8 +29,144 @@
         {
             cache = A.Fake<IWeeeCache>();
             mapper = A.Fake<IMapper>();
+            fixture = new Fixture();
 
             map = new TransferEvidenceNotesViewModelMap(mapper, cache);
+        }
+
+        [Fact]
+        public void Map_GivenNullSource_ArgumentNullExceptionExpected()
+        {
+            //act
+            var exception = Record.Exception(() => map.Map(null));
+            
+            //asset
+            exception.Should().BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Map_GivenSource_TransferEvidenceNotesViewModelShouldBeReturned()
+        {
+            //arrange
+            var source = GetTransferObject();
+
+            //act
+            var result = map.Map(source);
+
+            //assert
+            result.Should().BeOfType<TransferEvidenceNotesViewModel>();
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Map_GivenSource_SchemeValuesShouldBeSet()
+        {
+            //arrange
+            var source = GetTransferObject();
+
+            //act
+            var result = map.Map(source);
+
+            //assert
+            result.PcsId.Should().Be(source.OrganisationId);
+        }
+
+        [Fact]
+        public void Map_GivenSource_SchemeNameShouldBeRetrievedFromCacheAndSet()
+        {
+            //arrange
+            var schemeName = fixture.Create<string>();
+            var source = GetTransferObject();
+
+            A.CallTo(() => cache.FetchSchemeName(source.Request.SchemeId)).Returns(schemeName);
+
+            //act
+            var result = map.Map(source);
+
+            //assert
+            result.RecipientName.Should().Be(schemeName);
+        }
+
+        [Fact]
+        public void Map_GivenSource_CategoryValuesShouldBeSet()
+        {
+            //arrange
+            var source = GetTransferObject();
+
+            //act
+            var result = map.Map(source);
+
+            //assert
+            result.CategoryValues.Should().NotBeEmpty();
+            source.Request.CategoryIds.ForEach(c => result.CategoryValues.Should().Contain(cv => cv.CategoryId.Equals(c)));
+        }
+
+        [Fact]
+        public void Map_GivenSource_EvidenceNoteDataShouldBeMappedAndReturned()
+        {
+            //arrange
+            var notes = new List<EvidenceNoteData>()
+            {
+                fixture.Create<EvidenceNoteData>(),
+                fixture.Create<EvidenceNoteData>()
+            };
+
+            var viewEvidenceNoteViewModel = fixture.CreateMany<ViewEvidenceNoteViewModel>(2).ToList();
+
+            var request = fixture.Build<TransferEvidenceNoteRequest>()
+                .With(e => e.CategoryIds, new List<int>() { Core.DataReturns.WeeeCategory.ITAndTelecommsEquipment.ToInt() }).Create();
+
+            var organisationId = fixture.Create<Guid>();
+
+            var source = new TransferEvidenceNotesViewModelMapTransfer(notes, request, organisationId);
+
+            A.CallTo(() =>
+                    mapper.Map<ViewEvidenceNoteViewModel>(
+                        A<ViewEvidenceNoteMapTransfer>.That.Matches(
+                            v => v.EvidenceNoteData.Equals(notes.ElementAt(0)) && v.NoteStatus == null)))
+                .Returns(viewEvidenceNoteViewModel.ElementAt(0));
+
+            A.CallTo(() =>
+                    mapper.Map<ViewEvidenceNoteViewModel>(
+                        A<ViewEvidenceNoteMapTransfer>.That.Matches(
+                            v => v.EvidenceNoteData.Equals(notes.ElementAt(1)) && v.NoteStatus == null)))
+                .Returns(viewEvidenceNoteViewModel.ElementAt(1));
+
+            //act
+            var result = map.Map(source);
+
+            //assert
+            result.EvidenceNotesDataList.ElementAt(0).Should().BeEquivalentTo(viewEvidenceNoteViewModel.ElementAt(0));
+            result.EvidenceNotesDataList.ElementAt(1).Should().BeEquivalentTo(viewEvidenceNoteViewModel.ElementAt(1));
+        }
+
+        [Fact]
+        public void Map_GivenSource_SelectedEvidenceNotePairsShouldBeSet()
+        {
+            //arrange
+            var source = GetTransferObject();
+
+            //act
+            var result = map.Map(source);
+
+            //assert
+            result.SelectedEvidenceNotePairs.Should().NotBeEmpty();
+            foreach (var evidenceNoteData in source.Notes)
+            {
+                result.SelectedEvidenceNotePairs.Should()
+                    .Contain(p => p.Value.Equals(false) && p.Key.Equals(evidenceNoteData.Id));
+            }
+        }
+
+        private TransferEvidenceNotesViewModelMapTransfer GetTransferObject()
+        {
+            var notes = fixture.CreateMany<EvidenceNoteData>().ToList();
+            var request = fixture.Build<TransferEvidenceNoteRequest>()
+                .With(e => e.CategoryIds, new List<int>() { Core.DataReturns.WeeeCategory.ConsumerEquipment.ToInt(), Core.DataReturns.WeeeCategory.ITAndTelecommsEquipment.ToInt() }).Create();
+
+            var organisationId = fixture.Create<Guid>();
+
+            return new TransferEvidenceNotesViewModelMapTransfer(notes, request, organisationId);
         }
     }
 }
