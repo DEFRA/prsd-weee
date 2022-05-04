4bbbc267ad607f3517dd941ea0640979605f7338
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-05-04 17:57:02 +0100

    add Rejected/Returned options to Views. added Reason textarea with reason property on ReviewEvidenceNoteViewModel and Views. added Reason to the SetNoteStatus request. added Reason to Note.UpdateStatus method. added Reason to NoteStatusHistory collection.

--- src/EA.Weee.Web/ViewModels/Shared/EvidenceNoteViewModel.cs
+++ src/EA.Weee.Web/ViewModels/Shared/EvidenceNoteViewModel.cs
@@ -34,6 +34,9 @@
         [DisplayName("Date submitted")]
         public string SubmittedDate { get; set; }
 
+        [DisplayName("Date rejected")]
+        public string RejectedDate { get; set; }
+
         [DisplayName("Date returned")]
         public string ReturnedDate { get; set; }
 
