6107f33fd3b341b3d613fb3da330042867e3e6ec
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-05 18:01:19 +0100

    Tweaks for wave

--- src/EA.Weee.Web/Areas/Scheme/ViewModels/ManageEvidenceNotes/ReviewEvidenceNoteViewModel.cs
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/ManageEvidenceNotes/ReviewEvidenceNoteViewModel.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using Core.AatfEvidence;
     using Microsoft.Ajax.Utilities;
@@ -14,6 +15,7 @@
         [Required(ErrorMessage = "Select whether you want to approve, reject or return the evidence note")]
         public override string SelectedValue { get; set; }
 
+        [DisplayName("What is the reason you are rejecting or returning the evidence note?")]
         public string Reason { get; set; }
 
         public NoteStatus SelectedEnumValue
@@ -38,17 +40,12 @@
         {
         }
 
-        public Dictionary<string, string> HintItems
-        {
-            get
+        public Dictionary<string, string> HintItems =>
+            new Dictionary<string, string>
             {
-                return new Dictionary<string, string>
-                {
-                    { "Approve evidence note", null },
-                    { "Reject evidence note", "Reject an evidence note if you have received the evidence note by mistake or if there is a large number of updates to make that it is quicker to create a new evidence note." },
-                    { "Return evidence note", "Return an evidence note if there are some minor updates to be made by the AATF." }
-                };
-            }
-        }
+                { "Approve evidence note", null },
+                { "Reject evidence note", "Reject an evidence note if you have received the evidence note by mistake or if there is a large number of updates to make that it is quicker to create a new evidence note." },
+                { "Return evidence note", "Return an evidence note if there are some minor updates to be made by the AATF." }
+            };
     }
 }
