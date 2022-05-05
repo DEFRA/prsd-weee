a83e7819b08fed14cebd4d3f3672dee6b7b31bf9
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-05-05 13:57:06 +0100

    Near complete changes for Copy Paste tonnage

---
+++ src/EA.Weee.Web/Constant/EvidenceCopyPasteActionConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.Web.Constant
+{
+    public class EvidenceCopyPasteActionConstants
+    {
+        public const string CreateEvidenceNoteAction = "CreateEvidenceNote";
+        public const string EditEvidenceNoteAction = "EditEvidenceNote";
+        public const string ReturnedEvidenceNoteAction = "ReturnedEvidenceNote";
+    }
+}
--- src/EA.Weee.Web/ViewModels/Shared/EvidenceNoteViewModel.cs
+++ src/EA.Weee.Web/ViewModels/Shared/EvidenceNoteViewModel.cs
@@ -8,6 +8,7 @@
     using Core.AatfEvidence;
     using Core.Helpers;
 
+    [Serializable]
     public class EvidenceNoteViewModel
     {
         private readonly ICategoryValueTotalCalculator categoryValueCalculator;
