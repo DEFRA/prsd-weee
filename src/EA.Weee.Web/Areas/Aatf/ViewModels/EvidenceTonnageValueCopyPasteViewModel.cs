0bc465a2056417dd6a7a564b7b8962ac685e633d
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-05-05 13:57:06 +0100

    Near complete changes for Copy Paste tonnage

---
+++ src/EA.Weee.Web/Areas/Aatf/ViewModels/EvidenceTonnageValueCopyPasteViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Web.Areas.Aatf.ViewModels
+{
+    using EA.Weee.Core.AatfEvidence;
+    using System;
+
+    public class EvidenceTonnageValueCopyPasteViewModel
+    {
+        public Guid OrganisationId { get; set; }
+        public Guid AatfId { get; set; }
+        public Guid EvidenceId { get; set; }
+        public string Action { get; set; }
+        public string[] ReceievedPastedValues { get; set; }
+        public string[] ReusedPastedValues { get; set; }
+    }
+}
