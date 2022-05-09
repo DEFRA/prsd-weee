b1e6a0f971fbff54bb7a253f684668b7a07d1234
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-05-09 15:31:04 +0100

    Removed unused namespace

--- src/EA.Prsd.Core.Web.Mvc/RazorHelpers/Gds.DisplayFor.cs
+++ src/EA.Prsd.Core.Web.Mvc/RazorHelpers/Gds.DisplayFor.cs
@@ -1,12 +1,8 @@
 ﻿namespace EA.Prsd.Core.Web.Mvc.RazorHelpers
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Linq.Expressions;
     using System.Web.Mvc;
-    using System.Web.Mvc.Html;
-    using System.Web.Routing;
 
     public partial class Gds<TModel>
     {
