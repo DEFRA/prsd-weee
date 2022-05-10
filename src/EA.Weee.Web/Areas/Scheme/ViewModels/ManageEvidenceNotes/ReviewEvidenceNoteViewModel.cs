b85e839f95f1c6f873b82498e680d97d0c793055
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-05-10 17:09:38 +0100

    adjusted some tests to fit amended success messages from catalogue. removed full-stop symbols at end of messages and changed conditional to filter out blank spaces in Reason

--- src/EA.Weee.Web/Areas/Scheme/ViewModels/ManageEvidenceNotes/ReviewEvidenceNoteViewModel.cs
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/ManageEvidenceNotes/ReviewEvidenceNoteViewModel.cs
@@ -42,13 +42,13 @@
             new Dictionary<string, string>
             {
                 { "Approve evidence note", null },
-                { "Reject evidence note", "Reject an evidence note if the evidence has been sent to you by mistake or if there is a large number of updates to make that it is quicker to create a new evidence note." },
-                { "Return evidence note", "Return an evidence note if there are some minor updates to be made by the AATF." }
+                { "Reject evidence note", "Reject an evidence note if the evidence has been sent to you by mistake or if there is a large number of updates to make that it is quicker to create a new evidence note" },
+                { "Return evidence note", "Return an evidence note if there are some minor updates to be made by the AATF" }
             };
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (this.SelectedEnumValue == NoteStatus.Approved && this.Reason != null)
+            if (this.SelectedEnumValue == NoteStatus.Approved && string.IsNullOrEmpty(this.Reason))
             {
                 yield return new ValidationResult("A reason can only be entered if you are rejecting or returning the evidence note. Delete any text you have entered.");
             }
