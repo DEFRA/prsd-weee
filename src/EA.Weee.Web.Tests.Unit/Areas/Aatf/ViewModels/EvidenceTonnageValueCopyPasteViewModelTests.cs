b9d6c8b734cc09b191dd6ae300a1bfb94b083920
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-05-10 09:24:58 +0100

    Partial unit tests

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Aatf/ViewModels/EvidenceTonnageValueCopyPasteViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Aatf.ViewModels
+{
+    using Xunit;
+
+    public class EvidenceTonnageValueCopyPasteViewModelTests
+    {
+        //[Fact]
+        //public void ReceievedPastedValues_IsDecoratedWith_DisplayAttribute()
+        //{
+
+        //}
+
+        //[Fact]
+        //public void ReusedPastedValues_IsDecoratedWith_DisplayAttribute()
+        //{
+
+        //}
+    }
+}
