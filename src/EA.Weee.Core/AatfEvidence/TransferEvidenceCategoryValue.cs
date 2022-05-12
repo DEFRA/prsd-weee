8575d0586c097387b48dbb8498d37a4dba1eab70
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-12 11:18:39 +0100

    Changes to be able to validate the recieved and reused against the available amount

---
+++ src/EA.Weee.Core/AatfEvidence/TransferEvidenceCategoryValue.cs
@@ -0,0 +1,42 @@
+﻿namespace EA.Weee.Core.AatfEvidence
+{
+    using System;
+    using DataReturns;
+    using Validation;
+
+    [Serializable]
+    public class TransferEvidenceCategoryValue : EvidenceCategoryValue
+    {
+        public decimal? AvailableReceived { get; set; }
+
+        public decimal? AvailableReused { get; set; }
+
+        public Guid TransferTonnageId { get; set; }
+
+        [TonnageValue(nameof(CategoryId), "The tonnage value", true)]
+        [TonnageCompareValue(nameof(CategoryId), nameof(AvailableReceived))]
+        public sealed override string Received { get; set; }
+
+        [TonnageValue(nameof(CategoryId), "The tonnage value", true)]
+        //[TonnageCompareValue(nameof(CategoryId), nameof(Received))]
+        public sealed override string Reused { get; set; }
+
+        public TransferEvidenceCategoryValue()
+        {
+        }
+
+        public TransferEvidenceCategoryValue(WeeeCategory category,
+            Guid transferTonnageId,
+            decimal? availableReceived, 
+            decimal? availableReused, 
+            string received, 
+            string reused) : base(category)
+        {
+            TransferTonnageId = transferTonnageId;
+            AvailableReceived = availableReceived;
+            AvailableReused = availableReused;
+            Received = received;
+            Reused = reused;
+        }
+    }
+}
