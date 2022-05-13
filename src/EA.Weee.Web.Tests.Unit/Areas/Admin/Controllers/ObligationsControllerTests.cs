486e7667b553f5ef328dde8a1c7070d5f5996b61
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-05-13 12:32:10 +0100

    Adds unit tests and breaks broken unit test

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/ObligationsControllerTests.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.Controllers
+{
+    using EA.Weee.Web.Areas.Admin.Controllers;
+    using EA.Weee.Web.Areas.Admin.Controllers.Base;
+    using FluentAssertions;
+    using System.Web.Mvc;
+    using Xunit;
+
+    public class ObligationsControllerTests
+    {
+        [Fact]
+        public void Controller_ShouldInheritFromObligationsBaseController()
+        {
+            typeof(ObligationsController).Should().BeDerivedFrom<ObligationsBaseController>();
+        }
+
+        [Fact]
+        public void IndexGet_IsDecoratedWith_HttpGetAttribute()
+        {
+            typeof(ObligationsController).GetMethod("Index").Should()
+                .BeDecoratedWith<HttpGetAttribute>();
+        }
+    }
+}
