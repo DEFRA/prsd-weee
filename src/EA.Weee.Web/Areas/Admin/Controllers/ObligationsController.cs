f8af1d41358297d40ad1c4cce93d3e39e1bb18da
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-05-13 12:09:04 +0100

    Adds in a basic obligations controller, with a blank not built yet view

---
+++ src/EA.Weee.Web/Areas/Admin/Controllers/ObligationsController.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.Web.Areas.Admin.Controllers
+{
+    using EA.Weee.Web.Areas.Admin.Controllers.Base;
+    using System.Web.Mvc;
+
+    public class ObligationsController : ObligationsBaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
