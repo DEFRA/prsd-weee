81580c2fa26f95534b1cde2d7da2b409338977c2
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-13 12:40:05 +0100

    Create TransferTonnagesValidator along with unit tests

--- src/EA.Weee.RequestHandlers/AatfEvidence/TransferTonnagesValidator.cs
+++ src/EA.Weee.RequestHandlers/AatfEvidence/TransferTonnagesValidator.cs
@@ -1,11 +1,12 @@
 ﻿namespace EA.Weee.RequestHandlers.AatfEvidence
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using DataAccess.DataAccess;
     using Domain.Evidence;
-    using Requests.Scheme;
+    using Requests.AatfEvidence;
 
     public class TransferTonnagesValidator : ITransferTonnagesValidator
     {
@@ -16,31 +17,31 @@
             this.evidenceDataAccess = evidenceDataAccess;
         }
 
-        public async Task Validate(TransferEvidenceNoteRequest request)
+        public async Task Validate(List<TransferTonnageValue> transferValues)
         {
             var existingNoteTonnage =
-                await evidenceDataAccess.GetTonnageByIds(request.TransferValues.Select(t => t.TransferTonnageId)
+                await evidenceDataAccess.GetTonnageByIds(transferValues.Select(t => t.TransferTonnageId)
                     .ToList());
 
             foreach (var noteTonnage in existingNoteTonnage)
             {
+                // can't 
                 if (!noteTonnage.Received.HasValue)
                 {
-                    throw new InvalidOperationException();
+                    throw new InvalidOperationException($"Note tonnage with id {noteTonnage.Id} has null received");
                 }
 
-                var totalReceivedAvailable = noteTonnage.Received.Value - noteTonnage.NoteTransferTonnage
-                    .Where(nt =>
-                        nt.Received.HasValue && nt.TransferNote.Status.Value.Equals(NoteStatus.Approved.Value))
-                    .Sum(nt => nt.Received.Value);
+                var totalReceivedAvailable = 
+                    noteTonnage.Received.Value - (noteTonnage.NoteTransferTonnage != null && noteTonnage.NoteTransferTonnage.Any() ? 
+                                                 noteTonnage.NoteTransferTonnage.Where(nt => nt.Received.HasValue && nt.TransferNote.Status.Value.Equals(NoteStatus.Approved.Value)).Sum(nt => nt.Received.Value) : 0);
                 var requestedTonnage =
-                    request.TransferValues.First(t => t.TransferTonnageId.Equals(noteTonnage.Id));
+                    transferValues.First(t => t.TransferTonnageId.Equals(noteTonnage.Id));
 
                 if (requestedTonnage.FirstTonnage.HasValue && decimal
                         .Compare(requestedTonnage.FirstTonnage.Value, totalReceivedAvailable).Equals(1))
                 {
                     // can't transfer more than is available
-                    throw new InvalidOperationException();
+                    throw new InvalidOperationException($"The requested tonnage {requestedTonnage.FirstTonnage.Value} for note tonnage {noteTonnage.Id} is no longer available");
                 }
             }
         }
