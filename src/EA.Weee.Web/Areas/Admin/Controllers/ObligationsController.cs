e9601ebdbe9a2ae1360957250f9a0f75fb289a81
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-05-16 17:19:10 +0100

    Bug 44733 - feature flag Bug 44736 - Manage PCS obligations displayed to standard user

--- src/EA.Weee.Web/Areas/Admin/Controllers/ObligationsController.cs
+++ src/EA.Weee.Web/Areas/Admin/Controllers/ObligationsController.cs
@@ -1,10 +1,34 @@
 ﻿namespace EA.Weee.Web.Areas.Admin.Controllers
 {
+    using EA.Weee.Security;
     using EA.Weee.Web.Areas.Admin.Controllers.Base;
+    using Services;
+    using Services.Caching;
+    using System;
+    using System.Security.Claims;
     using System.Web.Mvc;
 
     public class ObligationsController : ObligationsBaseController
     {
+        private readonly IAppConfiguration configuration;
+
+        public ObligationsController(IAppConfiguration configuration, BreadcrumbService breadcrumb, IWeeeCache cache) : base(breadcrumb, cache)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!configuration.EnablePCSObligations)
+            {
+                throw new InvalidOperationException("PCS Obligations is not enabled.");
+            }
+
+            Breadcrumb.InternalActivity = "Manage PCS obligations";
+
+            base.OnActionExecuting(filterContext);
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
