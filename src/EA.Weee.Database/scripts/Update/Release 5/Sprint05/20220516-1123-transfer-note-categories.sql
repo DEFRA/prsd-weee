6c8e492ba433a8cae216c418d74f1539da4aa497
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-16 12:02:25 +0100

    Add note transfer category domain entity

---
+++ src/EA.Weee.Database/scripts/Update/Release 5/Sprint05/20220516-1123-transfer-note-categories.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [Evidence].[NoteTransferCategory](
+	[Id] [uniqueidentifier] NOT NULL,
+	[TransferNoteId] [uniqueidentifier] NOT NULL,
+	[CategoryId] [uniqueidentifier] NOT NULL,
+	[RowVersion] [timestamp] NOT NULL,
+ CONSTRAINT [PK_NoteTransferCategory_Id] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [UNQ_NoteTransferCategory_Category] UNIQUE NONCLUSTERED 
+(
+	[TransferNoteId] ASC,
+	[CategoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Evidence].[NoteTransferCategory]  WITH NOCHECK ADD  CONSTRAINT [FK_NoteTransferCategory_TransferNoteId] FOREIGN KEY([TransferNoteId])
+REFERENCES [Evidence].[Note] ([Id])
+GO
+
+ALTER TABLE [Evidence].[NoteTransferCategory] CHECK CONSTRAINT [FK_NoteTransferCategory_TransferNoteId]
+GO
