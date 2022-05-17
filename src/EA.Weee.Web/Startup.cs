5028fb62c31751749394ff4864a3958088786cd3
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-17 10:58:40 +0100

    Fix formattting issues and remove custom model binder

--- src/EA.Weee.Web/Startup.cs
+++ src/EA.Weee.Web/Startup.cs
@@ -56,7 +56,6 @@ namespace EA.Weee.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ModelBinders.Binders.DefaultBinder = new TrimModelBinder();
-            ModelBinders.Binders.Add(typeof(DateTime?), new CustomDateValidationModelBinder());
             DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(RequiredAttribute), typeof(WeeeRequiredAttributeAdapter));
 
             ApplicationVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
