01389a7cf0e8ab3e3f884be8d49faaa64ddbdfe8
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-05-17 10:03:54 +0100

    Partial changes for upload-obligations-page

---
+++ src/EA.Weee.Requests/Admin/Obligations/GetPcsObligationsCsv.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.Requests.Admin.Obligations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GetPcsObligationsCsv
+    {
+    }
+}
