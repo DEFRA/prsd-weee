59515ed78fec0c73e6dea30a73377f95cd900034
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-05-18 14:16:45 +0100

    Added schemeId when no results returned

---
+++ src/EA.Weee.RequestHandlers/Scheme/GetSchemeExternalByIdHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace EA.Weee.RequestHandlers.Scheme
+{
+    using Core.Scheme;
+    using DataAccess.DataAccess;
+    using Domain.Scheme;
+    using EA.Weee.RequestHandlers.Security;
+    using Prsd.Core.Mapper;
+    using Prsd.Core.Mediator;
+    using Requests.Scheme;
+    using System;
+    using System.Threading.Tasks;
+
+    internal class GetSchemeExternalByIdHandler : IRequestHandler<GetSchemeExternalById, SchemeData>
+    {
+         private readonly ISchemeDataAccess dataAccess;
+         private readonly IWeeeAuthorization authorization;
+         private readonly IMapper mapper;
+
+        public GetSchemeExternalByIdHandler(ISchemeDataAccess dataAccess,
+          IMapper mapper,
+          IWeeeAuthorization authorization)
+        {
+            this.dataAccess = dataAccess;
+            this.mapper = mapper;
+            this.authorization = authorization;
+        }
+
+        public async Task<SchemeData> HandleAsync(GetSchemeExternalById request)
+        {
+            authorization.EnsureCanAccessExternalArea();
+
+            var scheme = await dataAccess.GetSchemeOrDefault(request.SchemeId);
+
+            if (scheme == null)
+            {
+                string message = string.Format("No scheme was found with id \"{0}\".", request.SchemeId);
+                throw new ArgumentException(message);
+            }
+
+            return mapper.Map<Scheme, SchemeData>(scheme);
+        }
+    }
+}
