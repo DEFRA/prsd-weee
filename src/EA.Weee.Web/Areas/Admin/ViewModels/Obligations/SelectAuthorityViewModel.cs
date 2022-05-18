e255853a15b527303ad2d712af8bf737f56db2ea
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-05-18 16:43:42 +0100

    44484 Choose AA page (INTERNAL) - Unit Tests not included

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Obligations/SelectAuthorityViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Obligations
+{
+    using EA.Weee.Core.Shared;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class SelectAuthorityViewModel
+    {
+        [Required(ErrorMessage = "Select an appropriate authority")]
+        [Display(Name = "For which appropriate authority would you like to manage obligations?")]
+        public CompetentAuthority? SelectedAuthority { get; set; }
+
+        public IReadOnlyCollection<CompetentAuthority> PossibleValues { get; private set; }
+
+        public SelectAuthorityViewModel()
+        {
+            PossibleValues = new List<CompetentAuthority>()
+            {
+                CompetentAuthority.England,
+                CompetentAuthority.Scotland,
+                CompetentAuthority.NorthernIreland
+                //CompetentAuthority.Wales -- will be enabled in later dev -- check cshtml options
+            };
+        }
+    }
+}
