79d98c8f6cc11f6d2925ab250fc817c5aba84af9
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-05-19 12:11:53 +0100

    Adds upload obligations page, adds CSV request and handler

--- src/EA.Weee.RequestHandlers/Admin/Obligations/GetPcsObligationsCsvHandler.cs
+++ src/EA.Weee.RequestHandlers/Admin/Obligations/GetPcsObligationsCsvHandler.cs
@@ -1,6 +1,5 @@
 ﻿namespace EA.Weee.RequestHandlers.Admin.Obligations
 {
-    using EA.Prsd.Core;
     using EA.Prsd.Core.Mediator;
     using EA.Weee.Core.Admin;
     using EA.Weee.Core.Shared;
@@ -8,10 +7,9 @@
     using EA.Weee.Domain;
     using EA.Weee.Domain.Scheme;
     using EA.Weee.RequestHandlers.Security;
+    using EA.Weee.RequestHandlers.Shared;
     using EA.Weee.Requests.Admin.Obligations;
     using System;
-    using System.Data.Entity;
-    using System.Linq;
     using System.Threading.Tasks;
 
     public class GetPcsObligationsCsvHandler : IRequestHandler<GetPcsObligationsCsv, CSVFileData>
@@ -19,43 +17,46 @@
         private readonly IWeeeAuthorization authorization;
         private readonly WeeeContext context;
         private readonly CsvWriterFactory csvWriterFactory;
+        private readonly ICommonDataAccess dataAccess;
 
         public GetPcsObligationsCsvHandler(IWeeeAuthorization authorization,
             WeeeContext context,
-            CsvWriterFactory csvWriterFactory)
+            CsvWriterFactory csvWriterFactory,
+            ICommonDataAccess dataAccess)
         {
             this.authorization = authorization;
             this.context = context;
             this.csvWriterFactory = csvWriterFactory;
+            this.dataAccess = dataAccess;
         }
 
         public async Task<CSVFileData> HandleAsync(GetPcsObligationsCsv message)
         {
             authorization.EnsureCanAccessInternalArea();
 
-            var schemeAuthorityData = await context.UKCompetentAuthorities.Include(x => x.Schemes).Where(x => x.Id == message.AuthorityId).FirstOrDefaultAsync();
+            UKCompetentAuthority authority = await dataAccess.FetchCompetentAuthorityWithSchemes(message.Authority);
 
             var csvWriter = csvWriterFactory.Create<Scheme>();
             csvWriter.DefineColumn(ObligationCsvConstants.SchemeIdentifierColumnName, x => x.ApprovalNumber);
             csvWriter.DefineColumn(ObligationCsvConstants.SchemeNameColumnName, x => x.SchemeName);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat1ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat2ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat3ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat4ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat5ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat6ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat7ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat8ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat9ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat10ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat11ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat12ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat13ColumnName, null);
-            csvWriter.DefineColumn(ObligationCsvConstants.Cat14ColumnName, null);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat1ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat2ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat3ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat4ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat5ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat6ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat7ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat8ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat9ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat10ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat11ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat12ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat13ColumnName, x => string.Empty);
+            csvWriter.DefineColumn(ObligationCsvConstants.Cat14ColumnName, x => string.Empty);
 
-            var fileContent = csvWriter.Write(schemeAuthorityData.Schemes);
+            var fileContent = csvWriter.Write(authority.Schemes);
             var timestamp = DateTime.Now;
-            var fileName = $"{schemeAuthorityData.Abbreviation}_pcsobligationuploadtemplate{timestamp.ToString("dd/MM/yyyy_HHmm")}.csv";
+            var fileName = $"{authority.Abbreviation}_pcsobligationuploadtemplate{timestamp.ToString("dd/MM/yyyy_HHmm")}.csv";
 
             return new CSVFileData
             {
--- src/EA.Weee.Web/Areas/Admin/Controllers/ObligationsController.cs
+++ src/EA.Weee.Web/Areas/Admin/Controllers/ObligationsController.cs
@@ -10,15 +10,18 @@
     using System.Text;
     using System.Threading.Tasks;
     using System.Web.Mvc;
+    using EA.Weee.Core.Shared;
+    using EA.Weee.Web.Areas.Admin.ViewModels.Obligations;
 
     public class ObligationsController : ObligationsBaseController
     {
         private readonly IAppConfiguration configuration;
         private readonly Func<IWeeeClient> apiClient;
 
-        public ObligationsController(IAppConfiguration configuration, BreadcrumbService breadcrumb, IWeeeCache cache) : base(breadcrumb, cache)
+        public ObligationsController(IAppConfiguration configuration, BreadcrumbService breadcrumb, IWeeeCache cache, Func<IWeeeClient> apiClient) : base(breadcrumb, cache)
         {
             this.configuration = configuration;
+            this.apiClient = apiClient;
         }
 
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
@@ -36,16 +39,16 @@
         [HttpGet]
         public ActionResult SelectAuthority()
         {
-            return View("SelectAuthority", new ViewModels.Obligations.SelectAuthorityViewModel());
+            return View("SelectAuthority", new SelectAuthorityViewModel());
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Holding(ViewModels.Obligations.SelectAuthorityViewModel model)
+        public ActionResult SelectAuthority(SelectAuthorityViewModel model)
         {
             if (ModelState.IsValid)
             {
-                return View("Index", new ViewModels.Obligations.SelectAuthorityViewModel());
+                return View("UploadObligations", new UploadObligationsViewModel(model.SelectedAuthority.Value));
             }
             else
             {
@@ -54,23 +57,29 @@
         }
 
         [HttpGet]
-        public ActionResult Upload()
+        public ActionResult UploadObligations(CompetentAuthority authority)
         {
-            return View();
+            var model = new UploadObligationsViewModel(authority);
+
+            return View(model);
         }
 
         [HttpPost]
-        public ActionResult Upload(Guid authorityId)
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadObligations(UploadObligationsViewModel model)
         {
-            return View();
+            //SetBreadcrumb();
+            ViewBag.TriggerDownload = ModelState.IsValid;
+
+            return View(model);
         }
 
         [HttpGet]
-        public async Task<ActionResult> DownloadTemplate(Guid authorityId)
+        public async Task<ActionResult> DownloadTemplate(CompetentAuthority authority)
         {
             using (var client = apiClient())
             {
-                var fileData = await client.SendAsync(this.User.GetAccessToken(), new GetPcsObligationsCsv(authorityId));
+                var fileData = await client.SendAsync(this.User.GetAccessToken(), new GetPcsObligationsCsv(authority));
 
                 var data = new UTF8Encoding().GetBytes(fileData.FileContent);
                 return File(data, "text/csv", CsvFilenameFormat.FormatFileName(fileData.FileName));
