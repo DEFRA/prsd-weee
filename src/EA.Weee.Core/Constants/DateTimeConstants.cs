ee0006734c5e8243b5af2dd25b12053c17062c8c
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-05-23 11:36:56 +0100

    Move constant to the shared core project

--- src/EA.Weee.Core/Constants/DateTimeConstants.cs
+++ src/EA.Weee.Core/Constants/DateTimeConstants.cs
@@ -1,7 +1,8 @@
-﻿namespace EA.Weee.Web.Constant
+﻿namespace EA.Weee.Core.Constants
 {
     public static class DateTimeConstants
     {
         public static string GeneralDateTimeGMTFormat = "dd/MM/yyyy HH:mm:ss \"(GMT)\"";
+        public static string FilenameTimestampFormat = "_yyyy_dd/MM/yyyy_HHmm";
     }
 }
