8bfad17aa1ca6133bdfa0a6b74abeb64dc5f27fc
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-05-24 16:18:03 +0100

    Added unit tests for the other 2 filters in ManageEvidenceNoteViewModelTests

--- src/EA.Weee.Web.Tests.Unit/Areas/Aatf/ViewModels/ManageEvidenceNoteViewModelTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/Aatf/ViewModels/ManageEvidenceNoteViewModelTests.cs
@@ -15,5 +15,25 @@
             //assert
             model.FilterViewModel.Should().NotBeNull();
         }
+
+        [Fact]
+        public void ManageEvidenceNoteViewModel_Constructor_ShouldInitialiseRecipientWasteStatusViewModel()
+        {
+            //act
+            var model = new ManageEvidenceNoteViewModel();
+
+            //assert
+            model.RecipientWasteStatusFilterViewModel.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ManageEvidenceNoteViewModel_Constructor_ShouldInitialiseSubmittedDatesViewModel()
+        {
+            //act
+            var model = new ManageEvidenceNoteViewModel();
+
+            //assert
+            model.SubmittedDatesFilterViewModel.Should().NotBeNull();
+        }
     }
 }
