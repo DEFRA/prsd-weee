0c502ed8e541e8e511b91b05bacb9d12498fbc88
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-05-24 09:28:11 +0100

    Added unit tests for Manage Evidence Controller Added unit tests for the RecipientWasteStatus mapper and Submitted mapper

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Aatf/Mapping/ToViewModel/RecipientWasteStatusViewModelMapTests.cs
@@ -0,0 +1,79 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Aatf.Mapping.ToViewModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using AutoFixture;
+    using EA.Prsd.Core.Helpers;
+    using EA.Weee.Core.AatfEvidence;
+    using EA.Weee.Core.Scheme;
+    using EA.Weee.Web.Areas.Aatf.Mappings;
+    using EA.Weee.Web.Areas.Aatf.ViewModels;
+    using FluentAssertions;
+    using Xunit;
+
+    public class RecipientWasteStatusViewModelMapTests
+    {
+        private readonly RecipientWasteStatusViewModelMap mapper;
+
+        private Fixture fixture;
+
+        public RecipientWasteStatusViewModelMapTests()
+        {
+            fixture = new Fixture();
+
+            mapper = new RecipientWasteStatusViewModelMap();
+        }
+
+        [Fact]
+        public void Map_GivenNullSource_ArgumentNullExceptionExpected()
+        {
+            //act
+            var exception = Record.Exception(() => mapper.Map(null));
+
+            //assert
+            exception.Should().BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Map_GivenSource_PropertiesShouldBeSet()
+        {
+            //arrange
+            var source = fixture.Create<RecipientWasteStatusFilterBase>();
+
+            //act
+            var model = mapper.Map(source);
+
+            //assert
+            model.NoteStatusList.Should().BeEquivalentTo(GetNoteStatusList());
+            model.NoteStatusValue.Should().Be(source.NoteStatus);
+            model.WasteTypeList.Should().BeEquivalentTo(GetWasteTypeValuesList());
+            model.WasteTypeValue.Should().Be(source.WasteType);
+            model.SchemeListSL.Should().BeEquivalentTo(GetSchemeListDropDownForm(source.SchemeList));
+            model.ReceivedId.Should().Be(source.ReceivedId);
+        }
+
+        private IEnumerable<SelectListItem> GetNoteStatusList()
+        {
+            var statuses = new Dictionary<int, string>
+            {
+                { (int)NoteStatus.Submitted, NoteStatus.Submitted.ToString() },
+                { (int)NoteStatus.Approved, NoteStatus.Approved.ToString() },
+                { (int)NoteStatus.Rejected, NoteStatus.Rejected.ToString() },
+                { (int)NoteStatus.Void, NoteStatus.Void.ToString() },
+            };
+
+            return new SelectList(statuses, "Key", "Value");
+        }
+
+        private IEnumerable<SelectListItem> GetWasteTypeValuesList()
+        {
+            return new SelectList(EnumHelper.GetOrderedValues(typeof(WasteType)), "Key", "Value");
+        }
+
+        private IEnumerable<SelectListItem> GetSchemeListDropDownForm(IList<SchemeData> list)
+        {
+            return new SelectList(list, "Id", "SchemeName");
+        }
+    }
+}
