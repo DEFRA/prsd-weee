270327d0155c1db9197421018854e1c5793560ce
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-25 16:57:06 +0100

    Add CSH Helper and empty request handler

---
+++ src/EA.Weee.RequestHandlers/Admin/Obligations/SubmitSchemeObligationHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace EA.Weee.RequestHandlers.Admin.Obligations
+{
+    using System.Globalization;
+    using System.IO;
+    using System.Threading.Tasks;
+    using CsvHelper;
+    using Prsd.Core.Mediator;
+    using Requests.Admin.Obligations;
+
+    internal class SubmitSchemeObligationHandler : IRequestHandler<SubmitSchemeObligation, object>
+    {
+        public Task<object> HandleAsync(SubmitSchemeObligation message)
+        {
+            var data = new byte[1];
+
+            using (var ms = new MemoryStream(data))
+            {
+                ms.Seek(0, SeekOrigin.Begin);
+                using (var reader = new StreamReader(ms))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    //var records = csv.GetRecords<Foo>();
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/EA.Weee.Requests/Admin/Obligations/SubmitSchemeObligation.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.Requests.Admin.Obligations
+{
+    using Core.Admin;
+    using Prsd.Core.Mediator;
+
+    public class SubmitSchemeObligation : IRequest<object>
+    {
+    }
+}
