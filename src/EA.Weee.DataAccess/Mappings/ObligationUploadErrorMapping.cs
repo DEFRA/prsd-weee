d61eb0684d88211413a4251a3970a59ae72b2bd4
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-27 14:46:17 +0100

    EF configuration and test fix

---
+++ src/EA.Weee.DataAccess/Mappings/ObligationUploadErrorMapping.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.DataAccess.Mappings
+{
+    using System.Data.Entity.ModelConfiguration;
+    using Domain.Obligation;
+
+    internal class ObligationUploadErrorMapping : EntityTypeConfiguration<ObligationUploadError>
+    {
+        public ObligationUploadErrorMapping()
+        {
+            ToTable("ObligationUploadErrorMapping", "PCS");
+            HasKey(o => o.Id);
+            Property(o => o.ErrorType.Value).HasColumnName("ErrorType").IsRequired();
+            Property(o => o.SchemeIdentifier).IsRequired().HasColumnName("SchemeIdentifier").IsUnicode();
+            Property(o => o.SchemeName).IsRequired().HasColumnName("SchemeName").IsUnicode();
+            Property(o => o.Description).IsRequired().HasColumnName("Description");
+            Property(o => o.Category).IsRequired().HasColumnName("Category");
+
+            HasRequired(o => o.ObligationUpload).WithMany(o => o.ObligationUploadErrors).HasForeignKey(o => o.ObligationUploadId);
+        }
+    }
+}
--- src/EA.Weee.Domain/Obligation/ObligationUpload.cs
+++ src/EA.Weee.Domain/Obligation/ObligationUpload.cs
@@ -1,6 +1,39 @@
 ﻿namespace EA.Weee.Domain.Obligation
 {
-    public class ObligationUpload
+    using System;
+    using System.Collections.Generic;
+    using CuttingEdge.Conditions;
+    using Prsd.Core;
+    using Prsd.Core.Domain;
+    using User;
+
+    public partial class ObligationUpload : Entity
     {
+        public virtual DateTime UploadedDate { get; private set; }
+
+        public virtual string UploadedById { get; private set; }
+
+        public virtual string Data { get; private set; }
+
+        public virtual string FileName { get; private set; }
+
+        public virtual User UploadedBy { get; set; }
+
+        public virtual ICollection<ObligationUploadError> ObligationUploadErrors { get; protected set; }
+
+        public ObligationUpload(string uploadedById,
+            string data,
+            string fileName)
+        {
+            Condition.Requires(data).IsNotNullOrWhiteSpace();
+            Condition.Requires(fileName).IsNotNullOrWhiteSpace();
+            Condition.Requires(uploadedById).IsNotNullOrWhiteSpace();
+
+            Data = data;
+            FileName = fileName;
+            UploadedById = uploadedById;
+            UploadedDate = SystemTime.UtcNow;
+            ObligationUploadErrors = new List<ObligationUploadError>();
+        }
     }
 }
