2a25ea9687e802596b30978a28121f515be4f87d
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-27 17:03:34 +0100

    Updates to AddObligationUpload and related data access tests

---
+++ src/EA.Weee.DataAccess/DataAccess/ObligationDataAccess.cs
@@ -0,0 +1,40 @@
+﻿namespace EA.Weee.DataAccess.DataAccess
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Domain;
+    using Domain.Obligation;
+    using Prsd.Core.Domain;
+
+    public class ObligationDataAccess
+    {
+        private readonly WeeeContext context;
+        private readonly IUserContext userContext;
+        private readonly IGenericDataAccess genericDataAccess;
+
+        public ObligationDataAccess(WeeeContext context, 
+            IUserContext userContext, 
+            IGenericDataAccess genericDataAccess)
+        {
+            this.context = context;
+            this.userContext = userContext;
+            this.genericDataAccess = genericDataAccess;
+        }
+
+        public async Task<Guid> AddObligationUpload(UKCompetentAuthority ukCompetentAuthority,
+            string data, 
+            string fileName,
+            IList<ObligationUploadError> errors)
+        {
+            var obligationUpload = new ObligationUpload(ukCompetentAuthority,
+                userContext.UserId.ToString(), data, fileName, errors);
+
+            await genericDataAccess.Add(obligationUpload);
+
+            await context.SaveChangesAsync();
+
+            return obligationUpload.Id;
+        }
+    }
+}
--- src/EA.Weee.RequestHandlers/Admin/Obligations/SubmitSchemeObligationHandler.cs
+++ src/EA.Weee.RequestHandlers/Admin/Obligations/SubmitSchemeObligationHandler.cs
@@ -6,12 +6,14 @@
     using System.Threading.Tasks;
     using Core.Shared;
     using Core.Shared.CsvReading;
+    using CuttingEdge.Conditions;
     using Domain.Error;
     using Domain.Obligation;
     using Domain.Security;
     using Prsd.Core.Mediator;
     using Requests.Admin.Obligations;
     using Security;
+    using Shared;
     using Weee.Security;
     using ObligationCsvUpload = Core.Shared.CsvReading.ObligationCsvUpload;
 
@@ -23,20 +25,26 @@
         private readonly IObligationCsvReader obligationCsvReader;
         private readonly IObligationUploadValidator obligationUploadValidator;
         private readonly IWeeeAuthorization authorization;
+        private readonly ICommonDataAccess commonDataAccess;
 
         public SubmitSchemeObligationHandler(IObligationCsvReader obligationCsvReader, 
             IObligationUploadValidator obligationUploadValidator, 
-            IWeeeAuthorization authorization)
+            IWeeeAuthorization authorization, 
+            ICommonDataAccess commonDataAccess)
         {
             this.obligationCsvReader = obligationCsvReader;
             this.obligationUploadValidator = obligationUploadValidator;
             this.authorization = authorization;
+            this.commonDataAccess = commonDataAccess;
         }
 
         public async Task<Guid> HandleAsync(SubmitSchemeObligation request)
         {
             authorization.EnsureCanAccessInternalArea();
             authorization.EnsureUserInRole(Roles.InternalAdmin);
+
+            var authority = await commonDataAccess.FetchCompetentAuthority(request.Authority);
+            Condition.Requires(authority).IsNotNull();
 
             var errors = new List<ObligationUploadError>();
             var obligations = new List<ObligationCsvUpload>();
