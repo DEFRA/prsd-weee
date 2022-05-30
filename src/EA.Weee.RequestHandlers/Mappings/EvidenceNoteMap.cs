19b3d8100b458620830a6406562bf8761e1eaf10
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-05-30 18:10:34 +0100

    PR comments

--- src/EA.Weee.RequestHandlers/Mappings/EvidenceNoteMap.cs
+++ src/EA.Weee.RequestHandlers/Mappings/EvidenceNoteMap.cs
@@ -1,5 +1,6 @@
 ﻿namespace EA.Weee.RequestHandlers.Mappings
 {
+    using System.Collections.Generic;
     using System.Linq;
     using Core.AatfReturn;
     using Core.DataReturns;
@@ -27,6 +28,7 @@
 
         public EvidenceNoteData Map(Note source)
         {
+            var excludedStatuses = new List<Domain.Evidence.NoteStatus>() { Domain.Evidence.NoteStatus.Rejected, Domain.Evidence.NoteStatus.Void };
             return new EvidenceNoteData
             {
                 Id = source.Id,
@@ -39,8 +41,8 @@
                 WasteType = source.WasteType.HasValue ? (WasteType?)source.WasteType.Value : null,
                 EvidenceTonnageData = source.NoteTonnage.Select(t =>
                     new EvidenceTonnageData(t.Id, (WeeeCategory)t.CategoryId, t.Received, t.Reused,
-                    t.NoteTransferTonnage != null ? t.NoteTransferTonnage.Where(ntt => ntt.TransferNote.Status.Value != Domain.Evidence.NoteStatus.Rejected.Value && ntt.TransferNote.Status.Value != Domain.Evidence.NoteStatus.Void.Value).Select(ntt => ntt.Received).Sum() : null,
-                    t.NoteTransferTonnage != null ? t.NoteTransferTonnage.Where(ntt => ntt.TransferNote.Status.Value != Domain.Evidence.NoteStatus.Rejected.Value && ntt.TransferNote.Status.Value != Domain.Evidence.NoteStatus.Void.Value).Select(ntt => ntt.Reused).Sum() : null)).ToList(),
+                    t.NoteTransferTonnage != null ? t.NoteTransferTonnage.Where(ntt => !excludedStatuses.Contains(ntt.TransferNote.Status)).Select(ntt => ntt.Received).Sum() : null,
+                    t.NoteTransferTonnage != null ? t.NoteTransferTonnage.Where(ntt => !excludedStatuses.Contains(ntt.TransferNote.Status)).Select(ntt => ntt.Reused).Sum() : null)).ToList(),
                 SchemeData = mapper.Map<Scheme, SchemeData>(source.Recipient),
                 OrganisationData = mapper.Map<Organisation, OrganisationData>(source.Organisation),
                 AatfData = mapper.Map<Aatf, AatfData>(source.Aatf),
