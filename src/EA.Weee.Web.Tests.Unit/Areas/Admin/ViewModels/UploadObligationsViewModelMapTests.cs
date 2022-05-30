7d8cc6332a8892fd5675d8e6ff017f9c9430eda9
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-30 13:35:23 +0100

    Initial view obligation upload view updates

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/ViewModels/UploadObligationsViewModelMapTests.cs
@@ -0,0 +1,27 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.ViewModels
+{
+    using System;
+    using FluentAssertions;
+    using Web.Areas.Admin.Mappings.ToViewModel;
+    using Xunit;
+
+    public class UploadObligationsViewModelMapTests
+    {
+        private readonly UploadObligationsViewModelMap map;
+
+        public UploadObligationsViewModelMapTests()
+        {
+            map = new UploadObligationsViewModelMap();
+        }
+
+        [Fact]
+        public void Map_GivenNullSource_ArgumentNullExceptionExpected()
+        {
+            //act
+            var exception = Record.Exception(() => map.Map(null));
+
+            //assert
+            exception.Should().BeOfType<ArgumentNullException>();
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/UploadObligationsViewModelMap.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Web.Areas.Admin.Mappings.ToViewModel
+{
+    using CuttingEdge.Conditions;
+    using Prsd.Core.Mapper;
+    using ViewModels.Obligations;
+
+    public class UploadObligationsViewModelMap : IMap<UploadObligationsViewModelMapTransfer, UploadObligationsViewModel>
+    {
+        public UploadObligationsViewModel Map(UploadObligationsViewModelMapTransfer source)
+        {
+            Condition.Requires(source).IsNotNull();
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
