80ba9f0e40672f2c90f5ce9c6a2fe1a13f095c4d
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-30 13:35:23 +0100

    Initial view obligation upload view updates

---
+++ src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/UploadObligationsViewModelMapTransfer.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.Web.Areas.Admin.Mappings.ToViewModel
+{
+    using Core.Shared;
+
+    public class UploadObligationsViewModelMapTransfer
+    {
+        public CompetentAuthority CompetentAuthority { get; set; }
+    }
+}
