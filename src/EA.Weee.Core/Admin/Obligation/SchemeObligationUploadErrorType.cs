d41619b78c93d0746414f4d04bb436ebce3c4eea
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-30 12:34:24 +0100

    Get scheme obligation request handler with unit and integration test

---
+++ src/EA.Weee.Core/Admin/Obligation/SchemeObligationUploadErrorType.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.Core.Admin.Obligation
+{
+    using System.IO;
+
+    public enum SchemeObligationUploadErrorType
+    {
+        File = 1,
+        Scheme = 2,
+        Data = 3
+    }
+}
---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Mapping/SchemeObligationUploadDataTests.cs
@@ -0,0 +1,64 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Mapping
+{
+    using System.Collections.Generic;
+    using AutoFixture;
+    using Core.Admin.Obligation;
+    using Domain.Error;
+    using Domain.Lookup;
+    using Domain.Obligation;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Mappings;
+    using Xunit;
+
+    public class SchemeObligationUploadDataTests
+    {
+        private readonly SchemeObligationUploadDataMap map;
+
+        public SchemeObligationUploadDataTests()
+        {
+            map = new SchemeObligationUploadDataMap();
+        }
+
+        [Fact]
+        public void Map_GivenObligationUpload_SchemeObligationUploadDataShouldBeMapped()
+        {
+            //arrange
+            var obligationUploadErrors = new List<ObligationUploadError>()
+            {
+                new ObligationUploadError(ObligationUploadErrorType.Scheme, "scheme 1", "scheme 1 identifier",
+                    "error 1"),
+                new ObligationUploadError(ObligationUploadErrorType.File, "error 2"),
+                new ObligationUploadError(ObligationUploadErrorType.Data, WeeeCategory.ConsumerEquipment, "scheme 2",
+                    "scheme 2 identifier", "error 3")
+            };
+
+            var obligationUpload = A.Fake<ObligationUpload>();
+            A.CallTo(() => obligationUpload.ObligationUploadErrors).Returns(obligationUploadErrors);
+
+            //act
+            var result = map.Map(obligationUpload);
+
+            //assert
+            result.ErrorData.Count.Should().Be(3);
+            result.ErrorData.Should().Contain(e =>
+                e.Scheme == "scheme 1" && 
+                e.SchemeIdentifier == "scheme 1 identifier" &&
+                e.ErrorType == SchemeObligationUploadErrorType.Scheme &&
+                e.Category == null &&
+                e.Description == "error 1");
+            result.ErrorData.Should().Contain(e =>
+                e.Scheme == "scheme 2" && 
+                e.SchemeIdentifier == "scheme 2 identifier" &&
+                e.ErrorType == SchemeObligationUploadErrorType.Data &&
+                e.Category == WeeeCategory.ConsumerEquipment &&
+                e.Description == "error 3");
+            result.ErrorData.Should().Contain(e =>
+                e.Scheme == null &&
+                e.SchemeIdentifier == null &&
+                e.ErrorType == SchemeObligationUploadErrorType.File &&
+                e.Category == null &&
+                e.Description == "error 2");
+        }
+    }
+}
