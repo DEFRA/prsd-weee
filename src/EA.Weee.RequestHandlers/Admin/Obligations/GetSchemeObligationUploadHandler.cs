00081c39a0f978fc4037f34e32cb17f872814b71
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-30 12:34:24 +0100

    Get scheme obligation request handler with unit and integration test

---
+++ src/EA.Weee.RequestHandlers/Admin/Obligations/GetSchemeObligationUploadHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace EA.Weee.RequestHandlers.Admin.Obligations
+{
+    using System;
+    using System.Threading.Tasks;
+    using Core.Admin.Obligation;
+    using DataAccess.DataAccess;
+    using Domain.Obligation;
+    using Prsd.Core.Mapper;
+    using Prsd.Core.Mediator;
+    using Requests.Admin.Obligations;
+    using Security;
+    using Weee.Security;
+
+    internal class GetSchemeObligationUploadHandler : IRequestHandler<GetSchemeObligationUpload, SchemeObligationUploadData>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly IGenericDataAccess genericDataAccess;
+        private readonly IMapper mapper;
+
+        public GetSchemeObligationUploadHandler(IWeeeAuthorization authorization, IGenericDataAccess genericDataAccess, IMapper mapper)
+        {
+            this.authorization = authorization;
+            this.genericDataAccess = genericDataAccess;
+            this.mapper = mapper;
+        }
+
+        public async Task<SchemeObligationUploadData> HandleAsync(GetSchemeObligationUpload request)
+        {
+            authorization.EnsureCanAccessInternalArea();
+            authorization.EnsureUserInRole(Roles.InternalAdmin);
+
+            var obligationUpload = await genericDataAccess.GetById<ObligationUpload>(request.ObligationUploadId);
+
+            return mapper.Map<ObligationUpload, SchemeObligationUploadData>(obligationUpload);
+        }
+    }
+}
