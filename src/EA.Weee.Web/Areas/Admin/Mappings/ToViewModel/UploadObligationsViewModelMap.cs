03fd6f3a170c2e4c30f346f4c194aa10787aa74d
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-05-31 16:24:48 +0100

    Add unit tests for UploadObligationsViewModel and its mapper

--- src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/UploadObligationsViewModelMap.cs
+++ src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/UploadObligationsViewModelMap.cs
@@ -1,7 +1,10 @@
 ﻿namespace EA.Weee.Web.Areas.Admin.Mappings.ToViewModel
 {
+    using System.Collections.Generic;
     using CuttingEdge.Conditions;
     using Prsd.Core.Mapper;
+    using System.Linq;
+    using Core.Admin.Obligation;
     using ViewModels.Obligations;
 
     public class UploadObligationsViewModelMap : IMap<UploadObligationsViewModelMapTransfer, UploadObligationsViewModel>
@@ -11,7 +14,21 @@
             Condition.Requires(source).IsNotNull();
 
             var model = new UploadObligationsViewModel(source.CompetentAuthority);
+            var dataErrorTypes = new List<SchemeObligationUploadErrorType>() { SchemeObligationUploadErrorType.Data, SchemeObligationUploadErrorType.Scheme };
 
+            var displayDataError = false;
+            var displayFileError = false;
+
+            if (source.UploadData != null)
+            {
+                displayDataError = source.UploadData.ErrorData.Any(r => dataErrorTypes.Contains(r.ErrorType));
+                displayFileError =
+                    source.UploadData.ErrorData.Any(r => r.ErrorType == SchemeObligationUploadErrorType.File);
+            }
+
+            model.DisplayFormatError = displayFileError;
+            model.DisplayDataError = displayFileError != true && displayDataError;
+            
             return model;
         }
     }
