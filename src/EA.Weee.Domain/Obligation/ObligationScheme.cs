a7c216eeb0c6f2721106df2a885bd2a5326c7558
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-08 14:27:31 +0100

    Obligation scheme schema updates

---
+++ src/EA.Weee.Domain/Obligation/ObligationScheme.cs
@@ -0,0 +1,33 @@
+﻿namespace EA.Weee.Domain.Obligation
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Lookup;
+    using Prsd.Core.Domain;
+    using Scheme;
+
+    public partial class ObligationScheme : Entity
+    {
+        [ForeignKey("ObligationUploadId")]
+        public virtual ObligationUpload ObligationUpload { get; private set; }
+
+        public virtual Guid ObligationUploadId { get; private set; }
+
+        public virtual int ComplianceYear { get; private set; }
+
+        public virtual DateTime UpdatedDate { get; private set; }
+
+        [ForeignKey("SchemeId")]
+        public virtual Scheme Scheme { get; private set; }
+
+        public virtual Guid SchemeId { get; private set; }
+
+        public virtual WeeeCategory CategoryId { get; private set; }
+
+        public virtual decimal? Obligation { get; private set; }
+
+        public ObligationScheme()
+        {
+        }
+    }
+}
