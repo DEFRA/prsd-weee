3843672123d1ab74e907c6a74d91fed3c7b5e72d
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-10 15:47:43 +0100

    Additional integration tests

---
+++ src/EA.Weee.Integration.Tests/Builders/ObligationSchemeDbSetup.cs
@@ -0,0 +1,42 @@
+﻿namespace EA.Weee.Integration.Tests.Builders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Base;
+    using Domain.Obligation;
+
+    public class ObligationSchemeDbSetup : DbTestDataBuilder<ObligationScheme, ObligationSchemeDbSetup>
+    {
+        protected override ObligationScheme Instantiate()
+        {
+            var auth = DbContext.UKCompetentAuthorities.First(c => c.Name.Equals("Environment Agency"));
+            var user = DbContext.GetCurrentUser();
+            var scheme = DbContext.Schemes.First();
+            instance = new ObligationScheme(scheme, 2022);
+
+            return instance;
+        }
+
+        public ObligationSchemeDbSetup WithObligationAmounts(List<ObligationSchemeAmount> amounts)
+        {
+            instance.SetAmounts(amounts);
+
+            return this;
+        }
+
+        public ObligationSchemeDbSetup WithScheme(Guid schemeId)
+        {
+            instance.UpdateScheme(schemeId);
+
+            return this;
+        }
+
+        public ObligationSchemeDbSetup WithObligationUpload(Guid obligationUploadId)
+        {
+            instance.UpdateObligationUpload(obligationUploadId);
+
+            return this;
+        }
+    }
+}
