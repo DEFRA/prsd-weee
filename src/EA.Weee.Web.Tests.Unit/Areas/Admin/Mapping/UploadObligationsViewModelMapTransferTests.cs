ddd07ed070ecd550203d14b0fd7691cb75d0b602
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-13 10:53:19 +0100

    Add tests for UploadObligationsViewModelMap

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/Mapping/UploadObligationsViewModelMapTransferTests.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.Mapping
+{
+    using FluentAssertions;
+    using Web.Areas.Admin.Mappings.ToViewModel;
+    using Weee.Tests.Core;
+    using Xunit;
+
+    public class UploadObligationsViewModelMapTransferTests : SimpleUnitTestBase
+    {
+        [Fact]
+        public void UploadObligationsViewModelMapTransfer_Constructor_ShouldInitialiseProperties()
+        {
+            //act
+            var result = new UploadObligationsViewModelMapTransfer();
+
+            //assert
+            result.ObligationData.Should().BeEmpty();
+            result.ErrorData.Should().BeNull();
+        }
+    }
+}
