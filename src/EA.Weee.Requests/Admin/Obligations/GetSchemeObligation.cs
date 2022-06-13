4665d526e220e106bd958b62774e68fac2b07815
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-13 09:03:08 +0100

    New handler to retrieval of obligations and update to some tests

---
+++ src/EA.Weee.Requests/Admin/Obligations/GetSchemeObligation.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.Requests.Admin.Obligations
+{
+    using System.Collections.Generic;
+    using Core.Admin.Obligation;
+    using Core.Shared;
+    using Prsd.Core.Mediator;
+
+    public class GetSchemeObligation : IRequest<List<SchemeObligationData>>
+    {
+        public CompetentAuthority Authority { get; }
+
+        public int ComplianceYear { get; }
+
+        public GetSchemeObligation(CompetentAuthority authority, int complianceYear)
+        {
+            Authority = authority;
+            ComplianceYear = complianceYear;
+        }
+    }
+}
