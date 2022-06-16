2a5f6a1ff61e7f1fd0940c91bfaa3ebf5e93e081
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-16 11:56:48 +0100

    WIP initial star view of edit tonnages

---
+++ src/EA.Weee.Web/Areas/Scheme/Controllers/OutgoingTransfersController.cs
@@ -0,0 +1,56 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Controllers
+{
+    using Aatf.ViewModels;
+    using Core.AatfEvidence;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Requests.AatfEvidence;
+    using EA.Weee.Requests.Note;
+    using EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels;
+    using EA.Weee.Web.Areas.Scheme.ViewModels.ManageEvidenceNotes;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Infrastructure;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using Prsd.Core.Extensions;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using ViewModels;
+    using Web.ViewModels.Shared;
+    using Web.ViewModels.Shared.Mapping;
+    using Weee.Requests.Shared;
+
+    public class OutgoingTransfersController : SchemeEvidenceBaseController
+    {
+        private readonly Func<IWeeeClient> apiClient;
+        private readonly IMapper mapper;
+
+        public OutgoingTransfersController(IMapper mapper,
+            BreadcrumbService breadcrumb,
+            IWeeeCache cache,
+            Func<IWeeeClient> apiClient) : base(breadcrumb, cache)
+        {
+            this.mapper = mapper;
+            this.apiClient = apiClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EditTonnages(Guid pcsId, Guid evidenceNoteId)
+        {
+            await SetBreadcrumb(pcsId, BreadCrumbConstant.SchemeManageEvidence);
+
+            using (var client = apiClient())
+            {
+                var noteData = await client.SendAsync(User.GetAccessToken(), new GetTransferEvidenceNoteForSchemeRequest(evidenceNoteId));
+
+                var model =
+                    mapper.Map<OutgoingTransferEditTonnageViewModelTransfer, TransferEvidenceTonnageViewModel>(
+                        new OutgoingTransferEditTonnageViewModelTransfer(pcsId, noteData));
+
+                return this.View("EditTonnages", model);
+            }
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/OutgoingTransferEditTonnageViewModelTransfer.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
+{
+    using System;
+    using EA.Weee.Core.AatfEvidence;
+
+    public class OutgoingTransferEditTonnageViewModelTransfer : ViewTransferNoteViewModelMapTransfer
+    {
+        public OutgoingTransferEditTonnageViewModelTransfer(Guid schemeId, TransferEvidenceNoteData transferEvidenceNoteData) : base(schemeId, transferEvidenceNoteData, null)
+        {
+        }
+    }
+}
