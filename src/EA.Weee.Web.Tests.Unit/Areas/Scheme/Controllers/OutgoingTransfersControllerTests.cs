9813d4e653b2a0c22713a795c9b378728827bd45
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-16 11:56:48 +0100

    WIP initial star view of edit tonnages

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/Controllers/OutgoingTransfersControllerTests.cs
@@ -0,0 +1,68 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Scheme.Controllers
+{
+    using AutoFixture;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
+    using EA.Weee.Web.Areas.Scheme.Controllers;
+    using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using FakeItEasy;
+    using FluentAssertions;
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Web.Areas.Scheme.Requests;
+    using Weee.Tests.Core;
+    using Xunit;
+
+    public class OutgoingTransfersControllerTests : SimpleUnitTestBase
+    {
+        private readonly IWeeeClient weeeClient;
+        private readonly IMapper mapper;
+        private readonly OutgoingTransfersController outgoingTransferEvidenceController;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+        private readonly Guid organisationId;
+
+        public OutgoingTransfersControllerTests()
+        {
+            weeeClient = A.Fake<IWeeeClient>();
+            breadcrumb = A.Fake<BreadcrumbService>();
+            cache = A.Fake<IWeeeCache>();
+            mapper = A.Fake<IMapper>();
+            organisationId = Guid.NewGuid();
+
+            outgoingTransferEvidenceController = new OutgoingTransfersController(mapper, breadcrumb, cache, () => weeeClient);
+        }
+
+        [Fact]
+        public void OutgoingTransfersControllerInheritsCheckSchemeEvidenceBaseController()
+        {
+            typeof(OutgoingTransfersControllerTests).BaseType.Name.Should().Be(nameof(SchemeEvidenceBaseController));
+        }
+
+        [Fact]
+        public void EditTonnagesGet_ShouldHaveHttpGetAttribute()
+        {
+            typeof(OutgoingTransfersControllerTests).GetMethod("EditTonnages", new[] { typeof(Guid), typeof(Guid) }).Should()
+                .BeDecoratedWith<HttpGetAttribute>();
+        }
+
+        [Fact]
+        public async Task EditTonnagesGet_GivenValidOrganisation_BreadcrumbShouldBeSet()
+        {
+            // arrange 
+            var organisationName = "OrganisationName";
+
+            A.CallTo(() => cache.FetchOrganisationName(organisationId)).Returns(organisationName);
+
+            // act
+            await outgoingTransferEvidenceController.EditTonnages(organisationId, TestFixture.Create<Guid>());
+
+            // assert
+            breadcrumb.ExternalOrganisation.Should().Be(organisationName);
+            breadcrumb.ExternalActivity.Should().Be(BreadCrumbConstant.SchemeManageEvidence);
+        }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/OutgoingTransferEditTonnageViewModelMap.cs
@@ -0,0 +1,33 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
+{
+    using Filters;
+    using Prsd.Core.Mapper;
+    using Services.Caching;
+    using ViewModels;
+    using Web.ViewModels.Shared;
+    using Web.ViewModels.Shared.Mapping;
+
+    public class OutgoingTransferEditTonnageViewModelMap : TransferEvidenceTonnageViewModel, IMap<OutgoingTransferEditTonnageViewModelTransfer, TransferEvidenceTonnageViewModel>
+    {
+        private readonly IWeeeCache cache;
+        private readonly IMap<ViewTransferNoteViewModelMapTransfer, ViewTransferNoteViewModel> transferNoteMapper;
+
+        public OutgoingTransferEditTonnageViewModelMap(IWeeeCache cache, IMap<ViewTransferNoteViewModelMapTransfer, ViewTransferNoteViewModel> transferNoteMapper)
+        {
+            this.cache = cache;
+            this.transferNoteMapper = transferNoteMapper;
+        }
+
+        public TransferEvidenceTonnageViewModel Map(OutgoingTransferEditTonnageViewModelTransfer source)
+        {
+            var model = new TransferEvidenceTonnageViewModel
+            {
+                PcsId = source.SchemeId,
+                RecipientName = AsyncHelpers.RunSync(async () => await cache.FetchSchemeName(source.TransferEvidenceNoteData.RecipientSchemeData.Id)),
+                ViewTransferNoteViewModel = transferNoteMapper.Map(new ViewTransferNoteViewModelMapTransfer(source.SchemeId, source.TransferEvidenceNoteData, null))
+            };
+            
+            return model;
+        }
+    }
+}
