19937f9e83562a45ecaa081ed94e0b4238496b33
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-06-16 16:25:00 +0100

    Partial changes

---
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/ViewTransferEvidenceNoteTonnageDataViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.ViewModels
+{
+    using EA.Weee.Core.AatfEvidence;
+    using EA.Weee.Web.Extensions;
+
+    public class ViewTransferEvidenceNoteTonnageDataViewModel
+    {
+        public virtual int ReferenceId { get; set; }
+        public virtual NoteType Type { get; set; }
+        public virtual string ReferenceDisplay => $"{Type.ToDisplayString()}{ReferenceId}";
+        public virtual bool DisplayReferenceCell { get; set; }
+        public virtual string AatfName { get; set; }
+        public virtual string AatfApprovalNumber { get; set; }
+        public virtual bool DisplayAatfName { get; set; }
+        public virtual EvidenceCategoryValue CategoryValue { get; set; }
+    }
+}
