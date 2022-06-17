422b255947fc6588d665a85f26fee22618a25a7b
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-17 16:12:06 +0100

    Additional unit tests

---
+++ src/EA.Weee.Core.Tests.Unit/AatfEvidence/TransferEvidenceNoteDataTests.cs
@@ -0,0 +1,45 @@
+﻿namespace EA.Weee.Core.Tests.Unit.AatfEvidence
+{
+    using AutoFixture;
+    using Core.AatfEvidence;
+    using FluentAssertions;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DataReturns;
+    using Weee.Tests.Core;
+    using Xunit;
+
+    public class TransferEvidenceNoteDataTests : SimpleUnitTestBase
+    {
+        [Fact]
+        public void CategoryIds_GivenTransferEvidenceNoteTonnageDataIsNull_EmptyListShouldBeReturned()
+        {
+            //arrange
+            var model = new TransferEvidenceNoteData();
+
+            //act
+            var categories = model.CategoryIds;
+
+            //assert
+            categories.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void CategoryIds_GivenTransferEvidenceNoteTonnageDataHasValues_CorrectCategoriesShouldBeReturned()
+        {
+            //arrange
+            var model = new TransferEvidenceNoteData()
+            {
+                TransferEvidenceNoteTonnageData = TestFixture.CreateMany<TransferEvidenceNoteTonnageData>().ToList()
+            };
+
+            //act
+            var categories = model.CategoryIds;
+
+            //assert
+            categories.Count.Should().BeGreaterThan(0);
+            categories.Should().BeEquivalentTo(model.TransferEvidenceNoteTonnageData
+                .Select(t => t.EvidenceTonnageData.CategoryId).Cast<int>().ToList());
+        }
+    }
+}
