3e2673e92392c10bce5976463f71fd0cef31fddd
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-17 16:12:06 +0100

    Additional unit tests

--- src/EA.Weee.Web.Tests.Unit/Areas/Scheme/Controllers/OutgoingTransfersControllerTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/Controllers/OutgoingTransfersControllerTests.cs
@@ -10,9 +10,15 @@
     using FakeItEasy;
     using FluentAssertions;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using System.Web.Mvc;
+    using Core.AatfEvidence;
+    using Web.Areas.Scheme.Mappings.ToViewModels;
     using Web.Areas.Scheme.Requests;
+    using Web.Areas.Scheme.ViewModels;
+    using Weee.Requests.AatfEvidence;
     using Weee.Tests.Core;
     using Xunit;
 
@@ -24,6 +30,9 @@
         private readonly BreadcrumbService breadcrumb;
         private readonly IWeeeCache cache;
         private readonly Guid organisationId;
+        private readonly TransferEvidenceNoteData transferEvidenceNoteData;
+        private readonly List<EvidenceNoteData> evidenceNoteData;
+        private readonly TransferEvidenceTonnageViewModel transferEvidenceTonnageViewModel;
 
         public OutgoingTransfersControllerTests()
         {
@@ -34,6 +43,13 @@
             organisationId = Guid.NewGuid();
 
             outgoingTransferEvidenceController = new OutgoingTransfersController(mapper, breadcrumb, cache, () => weeeClient);
+
+            transferEvidenceNoteData = TestFixture.Create<TransferEvidenceNoteData>();
+            evidenceNoteData = TestFixture.CreateMany<EvidenceNoteData>().ToList();
+            transferEvidenceTonnageViewModel = TestFixture.Create<TransferEvidenceTonnageViewModel>();
+
+            A.CallTo(() => weeeClient.SendAsync(A<string>._, A<GetTransferEvidenceNoteForSchemeRequest>._))
+                .Returns(transferEvidenceNoteData);
         }
 
         [Fact]
@@ -49,7 +65,7 @@
                 .BeDecoratedWith<HttpGetAttribute>();
         }
 
-        [Fact(Skip = "TODO")]
+        [Fact]
         public async Task EditTonnagesGet_GivenValidOrganisation_BreadcrumbShouldBeSet()
         {
             // arrange 
@@ -63,6 +79,90 @@
             // assert
             breadcrumb.ExternalOrganisation.Should().Be(organisationName);
             breadcrumb.ExternalActivity.Should().Be(BreadCrumbConstant.SchemeManageEvidence);
+        }
+
+        [Fact]
+        public async Task EditTonnagesGet_GivenEvidenceNoteId_ShouldRetrieveTransferNote()
+        {
+            //arrange
+            var evidenceNoteId = TestFixture.Create<Guid>();
+
+            //act
+            await outgoingTransferEvidenceController.EditTonnages(organisationId, evidenceNoteId);
+
+            //assert
+            A.CallTo(() => weeeClient.SendAsync(A<string>._,
+                    A<GetTransferEvidenceNoteForSchemeRequest>.That.Matches(r => r.EvidenceNoteId == evidenceNoteId)))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task EditTonnagesGet_GivenTransferNoteData_TransferNotesShouldBeRetrieved()
+        {
+            //arrange
+            A.CallTo(() => weeeClient.SendAsync(A<string>._,
+                A<GetTransferEvidenceNoteForSchemeRequest>._)).Returns(transferEvidenceNoteData);
+
+            var categories = transferEvidenceNoteData.CategoryIds;
+            var existingNotes = transferEvidenceNoteData.TransferEvidenceNoteTonnageData.Select(t => t.OriginalNoteId).ToList();
+
+            //act
+            await outgoingTransferEvidenceController.EditTonnages(organisationId, TestFixture.Create<Guid>());
+
+            //assert
+            A.CallTo(() => weeeClient.SendAsync(A<string>._,
+                    A<GetEvidenceNotesForTransferRequest>.That.Matches(r => 
+                        r.OrganisationId == organisationId && r.Categories.SequenceEqual(categories) &&
+                        r.EvidenceNotes.SequenceEqual(existingNotes))))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task EditTonnagesGet_GivenTransferNoteAndTransferNotes_ModelMapperShouldBeCalled()
+        {
+            //arrange
+            A.CallTo(() => weeeClient.SendAsync(A<string>._,
+                A<GetTransferEvidenceNoteForSchemeRequest>._)).Returns(transferEvidenceNoteData);
+
+            A.CallTo(() => weeeClient.SendAsync(A<string>._,
+                A<GetEvidenceNotesForTransferRequest>._)).Returns(evidenceNoteData);
+
+            //act
+            await outgoingTransferEvidenceController.EditTonnages(organisationId, TestFixture.Create<Guid>());
+
+            //assert
+            A.CallTo(() => mapper.Map<TransferEvidenceNotesViewModelMapTransfer, TransferEvidenceTonnageViewModel>(
+                    A<TransferEvidenceNotesViewModelMapTransfer>.That.Matches(t => t.TransferAllTonnage == false &&
+                        t.TransferEvidenceNoteData ==
+                        transferEvidenceNoteData &&
+                        t.Request == null &&
+                        t.Notes.SequenceEqual(evidenceNoteData) &&
+                        t.OrganisationId == organisationId)))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task EditTonnagesGet_GivenMappedModel_ModelShouldBeReturned()
+        {
+            //arrange
+            A.CallTo(() => mapper.Map<TransferEvidenceNotesViewModelMapTransfer, TransferEvidenceTonnageViewModel>(
+                A<TransferEvidenceNotesViewModelMapTransfer>._)).Returns(transferEvidenceTonnageViewModel);
+
+            //act
+            var result = await outgoingTransferEvidenceController.EditTonnages(organisationId, TestFixture.Create<Guid>()) as ViewResult;
+
+            //assert
+            result.Model.Should().Be(transferEvidenceTonnageViewModel);
+        }
+
+        [Fact]
+        public async Task EditTonnagesGet_ShouldReturnView()
+        {
+            //act
+            var result = await outgoingTransferEvidenceController.EditTonnages(organisationId, TestFixture.Create<Guid>()) as ViewResult;
+
+            //assert
+            result.ViewName.Should().Be("EditTonnages");
         }
     }
 }
