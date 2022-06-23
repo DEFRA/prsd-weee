20f40a0c4cd4f895f5007ff9fcdabd6e03a36601
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-23 08:24:37 +0100

    Changes to aatf data mapping to allow current date to be used for property setting

---
+++ src/EA.Weee.RequestHandlers/Mappings/AatfDataWithSystemTimeMap.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.RequestHandlers.Mappings
+{
+    using Core.AatfReturn;
+    using Domain.AatfReturn;
+    using Prsd.Core.Mapper;
+
+    public class AatfDataWithSystemTimeMap : IMap<AatfWithSystemDateMapperObject, AatfData>
+    {
+        private readonly IMap<Aatf, AatfData> aatfMap;
+
+        public AatfDataWithSystemTimeMap(IMap<Aatf, AatfData> aatfMap)
+        {
+            this.aatfMap = aatfMap;
+        }
+
+        public AatfData Map(AatfWithSystemDateMapperObject source)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
