e8ca3bd4b5557c2e00daca61a9548311d612c996
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-23 08:24:37 +0100

    Changes to aatf data mapping to allow current date to be used for property setting

--- src/EA.Weee.RequestHandlers/AatfReturn/GetAatfInfoByOrganisationRequestHandler.cs
+++ src/EA.Weee.RequestHandlers/AatfReturn/GetAatfInfoByOrganisationRequestHandler.cs
@@ -12,29 +12,42 @@
     using System.Threading.Tasks;
     using Aatf;
     using DataAccess.DataAccess;
+    using Mappings;
+    using Prsd.Core;
 
     public class GetAatfInfoByOrganisationRequestHandler : IRequestHandler<GetAatfByOrganisation, List<AatfData>>
     {
         private readonly IAatfDataAccess aatfDataAccess;
-        private readonly IMap<Aatf, AatfData> mapper;
+        private readonly IMap<AatfWithSystemDateMapperObject, AatfData> mapper;
         private readonly IWeeeAuthorization authorization;
+        private readonly ISystemDataDataAccess systemDataDataAccess;
 
-        public GetAatfInfoByOrganisationRequestHandler(IMap<Aatf, AatfData> mapper, 
+        public GetAatfInfoByOrganisationRequestHandler(IMap<AatfWithSystemDateMapperObject, AatfData> mapper, 
             IAatfDataAccess aatfDataAccess, 
-            IWeeeAuthorization authorization)
+            IWeeeAuthorization authorization, 
+            ISystemDataDataAccess systemDataDataAccess)
         {
             this.mapper = mapper;
             this.aatfDataAccess = aatfDataAccess;
             this.authorization = authorization;
+            this.systemDataDataAccess = systemDataDataAccess;
         }
 
         public async Task<List<AatfData>> HandleAsync(GetAatfByOrganisation message)
         {
             authorization.EnsureInternalOrOrganisationAccess(message.OrganisationId);
 
+            var systemSettings = await systemDataDataAccess.Get();
+            var currentDate = SystemTime.Now;
+
+            if (systemSettings.UseFixedCurrentDate)
+            {
+                currentDate = systemSettings.FixedCurrentDate;
+            }
+
             var aatfs = await aatfDataAccess.GetAatfsForOrganisation(message.OrganisationId);
 
-            return aatfs.Select(a => mapper.Map(a)).ToList();
+            return aatfs.Select(a => mapper.Map(new AatfWithSystemDateMapperObject(a, currentDate))).ToList();
         }
     }
 }
---
+++ src/EA.Weee.RequestHandlers/Mappings/AatfWithSystemDateMapperObject.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Weee.RequestHandlers.Mappings
+{
+    using System;
+    using Core.AatfReturn;
+    using CuttingEdge.Conditions;
+    using Domain.AatfReturn;
+
+    public class AatfWithSystemDateMapperObject
+    {
+        public Aatf Aatf { get; private set; }
+
+        public DateTime SystemDateTime { get; private set; }
+
+        public AatfWithSystemDateMapperObject(Aatf aatf, DateTime systemDateTime)
+        {
+            Condition.Requires(aatf).IsNotNull();
+            Condition.Requires(systemDateTime).IsNotEqualTo(default);
+
+            Aatf = aatf;
+            SystemDateTime = systemDateTime;
+        }
+    }
+}
