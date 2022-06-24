699dd08398e450d8a27e862bdd7bc9c287000d94
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-24 10:02:16 +0100

    Refactor the transfer note and evidence note mapping

---
+++ src/EA.Weee.Core.Tests.Unit/AatfEvidence/EvidenceNoteDataTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Core.Tests.Unit.AatfEvidence
+{
+    using Core.AatfEvidence;
+    using FluentAssertions;
+    using Xunit;
+
+    public class EvidenceNoteDataTests
+    {
+        [Fact]
+        public void EvidenceNoteData_ShouldBeDerivedFromEvidenceNoteDataBase()
+        {
+            typeof(EvidenceNoteData).Should().BeDerivedFrom<EvidenceNoteDataBase>();
+        }
+    }
+}
