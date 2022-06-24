234c6a46bfd2b84bf972e659054fa289d8a95a3a
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-24 11:40:44 +0100

    Put framework in place for the review submitted transfer note

---
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/ReviewTransferNoteViewModelMap.cs
@@ -0,0 +1,29 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
+{
+    using Prsd.Core;
+    using Prsd.Core.Mapper;
+    using ViewModels;
+
+    public class ReviewTransferNoteViewModelMap : IMap<ViewTransferNoteViewModelMapTransfer, ReviewTransferNoteViewModel>
+    {
+        private readonly IMapper mapper;
+
+        public ReviewTransferNoteViewModelMap(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        public ReviewTransferNoteViewModel Map(ViewTransferNoteViewModelMapTransfer source)
+        {
+            Guard.ArgumentNotNull(() => source, source);
+
+            var model = new ReviewTransferNoteViewModel()
+            {
+                ViewTransferNoteViewModel = mapper.Map<ViewTransferNoteViewModel>(source),
+                OrganisationId = source.SchemeId
+            };
+
+            return model;
+        }
+    }
+}
