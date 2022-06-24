ace422bbaf7b36b7999f60683a5c09bfd7568366
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-24 16:10:40 +0100

    Fix failing tests

--- src/EA.Weee.RequestHandlers.Tests.DataAccess/Evidence/EvidenceDataAccessUnitTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.DataAccess/Evidence/EvidenceDataAccessUnitTests.cs
@@ -73,7 +73,7 @@
                                                                            n.CreatedDate.Equals(date) &&
                                                                            n.Organisation.Equals(organisation) &&
                                                                            n.Protocol == null &&
-                                                                           n.WasteType == null &&
+                                                                           n.WasteType == WasteType.HouseHold &&
                                                                            n.Recipient.Equals(scheme) &&
                                                                            n.StartDate.Equals(date) &&
                                                                            n.EndDate.Equals(date) &&
@@ -118,7 +118,7 @@
                                                                            n.CreatedDate.Equals(date) &&
                                                                            n.Organisation.Equals(organisation) &&
                                                                            n.Protocol == null &&
-                                                                           n.WasteType == null &&
+                                                                           n.WasteType == WasteType.HouseHold &&
                                                                            n.Recipient.Equals(scheme) &&
                                                                            n.StartDate.Equals(date) &&
                                                                            n.EndDate.Equals(date) &&
