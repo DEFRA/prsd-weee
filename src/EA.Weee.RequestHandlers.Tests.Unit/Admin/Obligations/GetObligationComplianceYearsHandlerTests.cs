aea09d993f4532100f04f44bbbbbd41a15b08f85
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-28 10:01:23 +0100

    Unit tests for GetObligationComplianceYearsHandler

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/Obligations/GetObligationComplianceYearsHandlerTests.cs
@@ -0,0 +1,161 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Admin.Obligations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security;
+    using System.Threading.Tasks;
+    using AutoFixture;
+    using Core.Shared;
+    using DataAccess.DataAccess;
+    using Domain;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Prsd.Core.Mapper;
+    using RequestHandlers.Admin.Obligations;
+    using RequestHandlers.Security;
+    using RequestHandlers.Shared;
+    using Requests.Admin.Obligations;
+    using Weee.Security;
+    using Weee.Tests.Core;
+    using Xunit;
+
+    public class GetObligationComplianceYearsHandlerTests : SimpleUnitTestBase
+    {
+        private GetObligationComplianceYearsHandler handler;
+        private readonly GetObligationComplianceYears request;
+        private readonly IWeeeAuthorization authorization;
+        private readonly ICommonDataAccess commonDataAccess;
+        private readonly IObligationDataAccess obligationDataAccess;
+        private readonly ISystemDataDataAccess systemDataDataAccess;
+
+        public GetObligationComplianceYearsHandlerTests()
+        {
+            authorization = A.Fake<IWeeeAuthorization>();
+            commonDataAccess = A.Fake<ICommonDataAccess>();
+            obligationDataAccess = A.Fake<IObligationDataAccess>();
+            systemDataDataAccess = A.Fake<ISystemDataDataAccess>();
+
+            request = new GetObligationComplianceYears(TestFixture.Create<CompetentAuthority>());
+
+            handler = new GetObligationComplianceYearsHandler(authorization, obligationDataAccess, commonDataAccess, systemDataDataAccess);
+        }
+
+        [Fact]
+        public async Task HandleAsync_NoInternalAccess_ThrowsSecurityException()
+        {
+            //arrange
+            var authorization = new AuthorizationBuilder().DenyInternalAreaAccess().Build();
+
+            handler = new GetObligationComplianceYearsHandler(authorization, obligationDataAccess, commonDataAccess, systemDataDataAccess);
+
+            //act
+            var exception = await Record.ExceptionAsync(async () => await handler.HandleAsync(request));
+
+            //assert
+            exception.Should().BeOfType<SecurityException>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_NotAnAdminUser_ThrowsSecurityException()
+        {
+            //arrange
+            var authorization = new AuthorizationBuilder().DenyAnyRole().Build();
+
+            handler = new GetObligationComplianceYearsHandler(authorization, obligationDataAccess, commonDataAccess, systemDataDataAccess);
+
+            //act
+            var exception = await Record.ExceptionAsync(async () => await handler.HandleAsync(request));
+
+            //assert
+            exception.Should().BeOfType<SecurityException>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_InternalAccess_ShouldBeChecked()
+        {
+            //act
+            await handler.HandleAsync(request);
+
+            //arrange
+            A.CallTo(() => authorization.EnsureCanAccessInternalArea()).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task HandleAsync_UserInAdminRole_ShouldBeChecked()
+        {
+            //act
+            await handler.HandleAsync(request);
+
+            //arrange
+            A.CallTo(() => authorization.EnsureUserInRole(Roles.InternalAdmin)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenRequest_CompetentAuthorityShouldBeRetrieved()
+        {
+            //act
+            await handler.HandleAsync(request);
+
+            //assert
+            A.CallTo(() => commonDataAccess.FetchCompetentAuthority(request.Authority)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenRequest_SystemDataTimeShouldBeRetrieved()
+        {
+            //act
+            await handler.HandleAsync(request);
+
+            //assert
+            A.CallTo(() => systemDataDataAccess.GetSystemDateTime()).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenRequest_ObligationComplianceYearsShouldBeRetrieved()
+        {
+            //arrange
+            var authority = TestFixture.Create<UKCompetentAuthority>();
+            A.CallTo(() => commonDataAccess.FetchCompetentAuthority(A<CompetentAuthority>._))
+                .Returns(authority);
+
+            //act
+            await handler.HandleAsync(request);
+
+            //assert
+            A.CallTo(() => obligationDataAccess.GetObligationComplianceYears(A<UKCompetentAuthority>.That.IsSameAs(authority))).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenRequestAndObligationYears_YearsShouldBeReturned()
+        {
+            //arrange
+            var years = TestFixture.CreateMany<int>().ToList();
+
+            A.CallTo(() => obligationDataAccess.GetObligationComplianceYears(A<UKCompetentAuthority>._)).Returns(years);
+
+            //act
+            var result = await handler.HandleAsync(request);
+
+            //assert
+            result.Should().BeEquivalentTo(years);
+        }
+
+        [Fact]
+        public async Task HandleAsync_GivenRequestAndObligationYearsAndCurrentYearIsNotInList_YearsShouldBeReturned()
+        {
+            //arrange
+            var years = new List<int>() { 2020, 2019 };
+            var currentDate = new DateTime(2021, 1, 1);
+
+            A.CallTo(() => obligationDataAccess.GetObligationComplianceYears(A<UKCompetentAuthority>._)).Returns(years);
+            A.CallTo(() => systemDataDataAccess.GetSystemDateTime()).Returns(currentDate);
+
+            //act
+            var result = await handler.HandleAsync(request);
+
+            //assert
+            result.Should().BeEquivalentTo(new List<int>() { 2021, 2020, 2019 });
+        }
+    }
+}
---
+++ src/EA.Weee.RequestHandlers/Admin/Obligations/GetObligationComplianceYearsHandler.cs
@@ -0,0 +1,52 @@
+﻿namespace EA.Weee.RequestHandlers.Admin.Obligations
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Admin.Obligation;
+    using DataAccess.DataAccess;
+    using Domain.Scheme;
+    using Prsd.Core.Mapper;
+    using Prsd.Core.Mediator;
+    using Requests.Admin.Obligations;
+    using Security;
+    using Shared;
+    using Weee.Security;
+
+    public class GetObligationComplianceYearsHandler : IRequestHandler<GetObligationComplianceYears, List<int>>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly IObligationDataAccess obligationDataAccess;
+        private readonly ICommonDataAccess commonDataAccess;
+        private readonly ISystemDataDataAccess systemDataAccess;
+
+        public GetObligationComplianceYearsHandler(IWeeeAuthorization authorization,
+            IObligationDataAccess obligationDataAccess,
+            ICommonDataAccess commonDataAccess, 
+            ISystemDataDataAccess systemDataAccess)
+        {
+            this.authorization = authorization;
+            this.obligationDataAccess = obligationDataAccess;
+            this.commonDataAccess = commonDataAccess;
+            this.systemDataAccess = systemDataAccess;
+        }
+
+        public async Task<List<int>> HandleAsync(GetObligationComplianceYears request)
+        {
+            authorization.EnsureCanAccessInternalArea();
+            authorization.EnsureUserInRole(Roles.InternalAdmin);
+
+            var authority = await commonDataAccess.FetchCompetentAuthority(request.Authority);
+
+            var systemDateTime = await systemDataAccess.GetSystemDateTime();
+
+            var complianceYears = await obligationDataAccess.GetObligationComplianceYears(authority);
+
+            if (!complianceYears.Contains(systemDateTime.Year))
+            {
+                complianceYears.Insert(0, systemDateTime.Year);
+            }
+
+            return complianceYears;
+        }
+    }
+}
