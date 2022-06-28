86cd65fef80c8512847af3bc03086a5330dfd0a5
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-06-28 15:30:28 +0100

    Fix for what data needs to be displayed for transfer note submitted by

--- src/EA.Weee.DataAccess/Mappings/OrganisationMapping.cs
+++ src/EA.Weee.DataAccess/Mappings/OrganisationMapping.cs
@@ -8,6 +8,8 @@
         public OrganisationMapping()
         {
             ToTable("Organisation", "Organisation");
+
+            HasMany(o => o.Schemes).WithRequired(o => o.Organisation).HasForeignKey(o => o.OrganisationId);
         }
     }
 }
