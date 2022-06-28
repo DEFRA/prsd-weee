c73b9822c6b856cace8ff2dc4b50adea1d32b2af
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-06-28 10:48:34 +0100

    Removed redundant models and mappers and re-use existing ones

--- src/EA.Weee.RequestHandlers/Admin/GetAllNotesRequestHandler.cs
+++ src/EA.Weee.RequestHandlers/Admin/GetAllNotesRequestHandler.cs
@@ -7,6 +7,7 @@
     using Domain.Evidence;
     using EA.Prsd.Core.Mapper;
     using EA.Prsd.Core.Mediator;
+    using EA.Weee.Core.AatfEvidence;
     using EA.Weee.Core.Admin;
     using EA.Weee.Core.Helpers;
     using EA.Weee.Requests.Admin;
@@ -15,7 +16,7 @@
     using Security;
     using NoteType = Domain.Evidence.NoteType;
 
-    public class GetAllNotesRequestHandler : IRequestHandler<GetAllNotes, List<AdminEvidenceNoteData>>
+    public class GetAllNotesRequestHandler : IRequestHandler<GetAllNotes, List<EvidenceNoteData>>
     {
         private readonly IWeeeAuthorization authorization;
         private readonly IEvidenceDataAccess noteDataAccess;
@@ -27,7 +28,7 @@
             this.noteDataAccess = noteDataAccess;
             this.mapper = mapper;
         }
-        public async Task<List<AdminEvidenceNoteData>> HandleAsync(GetAllNotes message)
+        public async Task<List<EvidenceNoteData>> HandleAsync(GetAllNotes message)
         {
             authorization.EnsureCanAccessInternalArea();
             authorization.EnsureUserInRole(Roles.InternalAdmin);
@@ -35,12 +36,12 @@
             var noteFilter = new NoteFilter(2022)
             {
                 NoteTypeFilter = message.NoteTypeFilterList.Select(x => x.ToDomainEnumeration<NoteType>()).ToList(),
-                AllowedStatuses = message.AllowedStatuses.Select(a => a.ToDomainEnumeration<NoteStatus>()).ToList(),
+                AllowedStatuses = message.AllowedStatuses.Select(a => a.ToDomainEnumeration<Domain.Evidence.NoteStatus>()).ToList(),
             };
 
             var notes = await noteDataAccess.GetAllNotes(noteFilter);
 
-            return mapper.Map<ListOfAdminEvidenceNoteDataMap>(new ListOfNotesMap(notes.OrderByDescending(n => n.CreatedDate).ToList())).ListOfAdminEvidenceNoteData;
+            return mapper.Map<ListOfEvidenceNoteDataMap>(new ListOfNotesMap(notes.OrderByDescending(n => n.CreatedDate).ToList())).ListOfEvidenceNoteData;
         }
     }
 }
--- src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/ViewAllEvidenceNotesMapModel.cs
+++ src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/ViewAllEvidenceNotesMapModel.cs
@@ -1,18 +1,17 @@
 ﻿namespace EA.Weee.Web.Areas.Admin.Mappings.ToViewModel
 {
-    using System;
     using System.Collections.Generic;
     using EA.Prsd.Core;
-    using EA.Weee.Core.Admin;
+    using EA.Weee.Core.AatfEvidence;
     using EA.Weee.Web.ViewModels.Shared;
 
     public class ViewAllEvidenceNotesMapModel
     {
-        public List<AdminEvidenceNoteData> Notes { get; set; }
+        public List<EvidenceNoteData> Notes { get; set; }
 
         public ManageEvidenceNoteViewModel ManageEvidenceNoteViewModel { get; set; }
 
-        public ViewAllEvidenceNotesMapModel(List<AdminEvidenceNoteData> notes,
+        public ViewAllEvidenceNotesMapModel(List<EvidenceNoteData> notes,
             ManageEvidenceNoteViewModel manageEvidenceNoteViewModel)
         {
             Guard.ArgumentNotNull(() => notes, notes);
