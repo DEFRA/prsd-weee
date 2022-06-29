5090125e5e6b5a47ba62f41ee198e7a2a600a0c9
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-06-29 15:39:14 +0100

    unit + integration tests done

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/ViewModels/ManageEvidenceNotesTabDisplayOptionsTests.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.ViewModels
+{
+    using EA.Weee.Core.Helpers;
+    using EA.Weee.Web.Areas.Admin.ViewModels.Shared;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ManageEvidenceNotesTabDisplayOptionsTests
+    {
+        [Fact]
+        public void ManageEvidenceNotesTabDisplayOptions_ShouldHaveCorrectDisplayAttributes()
+        {
+            //assert
+            ManageEvidenceNotesTabDisplayOptions.ViewAllEvidenceNotes.ToDisplayString().Should().Be("view-all-evidence-notes");
+            ManageEvidenceNotesTabDisplayOptions.ViewAllEvidenceTransfers.ToDisplayString().Should().Be("view-all-evidence-transfers");
+        }
+    }
+}
