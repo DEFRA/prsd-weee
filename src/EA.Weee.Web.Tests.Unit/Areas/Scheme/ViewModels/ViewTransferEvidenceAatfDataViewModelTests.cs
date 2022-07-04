b907f098f7db4a1164e62de27a57034df25829fa
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-07-04 15:27:32 +0100

    Fixes issues with notes still displaying if the have 0 categories

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/ViewModels/ViewTransferEvidenceAatfDataViewModelTests.cs
@@ -0,0 +1,44 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Scheme.ViewModels
+{
+    using AutoFixture;
+    using EA.Weee.Core.AatfEvidence;
+    using EA.Weee.Web.Areas.Scheme.ViewModels;
+    using FluentAssertions;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class ViewTransferEvidenceAatfDataViewModelTests
+    {
+        private readonly Fixture fixture;
+
+        public ViewTransferEvidenceAatfDataViewModelTests()
+        {
+            fixture = new Fixture();
+        }
+
+        [Fact]
+        public void ViewTransferEvidenceAatfDataViewModel_DisplayAatf_ShouldBeTrue_WhenThereAreNotesToBeDisplayed()
+        {
+            // arrange
+            var model = new ViewTransferEvidenceAatfDataViewModel();
+            var categoryValues = new List<EvidenceCategoryValue>() { fixture.Create<EvidenceCategoryValue>() };
+            var notesList = new List<ViewTransferEvidenceNoteTonnageDataViewModel>();
+            var note = new ViewTransferEvidenceNoteTonnageDataViewModel() { CategoryValues = categoryValues };
+            notesList.Add(note);
+            model.Notes = notesList;
+
+            // assert
+            model.DisplayAatf.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ViewTransferEvidenceAatfDataViewModel_DisplayAatf_ShouldBeFalse_WhenThereAreNoNotesToBeDisplayed()
+        {
+            // arrange
+            var model = new ViewTransferEvidenceAatfDataViewModel();
+
+            // assert
+            model.DisplayAatf.Should().BeFalse();
+        }
+    }
+}
