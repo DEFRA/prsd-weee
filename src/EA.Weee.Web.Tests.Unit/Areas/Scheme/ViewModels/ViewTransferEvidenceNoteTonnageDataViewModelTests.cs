0fbcb4dc2b55d2695f276b39a1a3aed7c4ded430
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-07-04 15:27:32 +0100

    Fixes issues with notes still displaying if the have 0 categories

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/ViewModels/ViewTransferEvidenceNoteTonnageDataViewModelTests.cs
@@ -0,0 +1,41 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Scheme.ViewModels
+{
+    using AutoFixture;
+    using EA.Weee.Core.AatfEvidence;
+    using EA.Weee.Web.Areas.Scheme.ViewModels;
+    using FluentAssertions;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class ViewTransferEvidenceNoteTonnageDataViewModelTests
+    {
+        private readonly Fixture fixture;
+
+        public ViewTransferEvidenceNoteTonnageDataViewModelTests()
+        {
+            fixture = new Fixture();
+        }
+
+        [Fact]
+        public void ViewTransferEvidenceNoteTonnageDataViewModel_DisplayTransferNote_ShouldBeTrue_IfNotesWithCategoriesPresent()
+        {
+            // arrange
+            var model = new ViewTransferEvidenceNoteTonnageDataViewModel();
+            model.CategoryValues = new List<EvidenceCategoryValue>() { fixture.Create<EvidenceCategoryValue>() };
+
+            // assert
+            model.DisplayTransferNote.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ViewTransferEvidenceNoteTonnageDataViewModel_DisplayTransferNote_ShouldBeFalse_IfNoNotesWithCategoriesPresent()
+        {
+            // arrange
+            var model = new ViewTransferEvidenceNoteTonnageDataViewModel();
+            model.CategoryValues = new List<EvidenceCategoryValue>();
+
+            // assert
+            model.DisplayTransferNote.Should().BeFalse();
+        }
+    }
+}
