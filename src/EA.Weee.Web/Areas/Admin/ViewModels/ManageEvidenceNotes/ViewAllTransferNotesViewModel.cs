0e1381cf41aacfef557154ab209b309a67591226
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-07-04 11:16:28 +0100

    Added view all transfers notes screen in internal admin area

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/ManageEvidenceNotes/ViewAllTransferNotesViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.ManageEvidenceNotes
+{
+    using EA.Weee.Web.Areas.Admin.ViewModels.Shared;
+
+    public class ViewAllTransferNotesViewModel : ManageEvidenceNotesViewModel
+    {
+        public ViewAllTransferNotesViewModel()
+           : base(ManageEvidenceNotesTabDisplayOptions.ViewAllEvidenceTransfers)
+        {
+        }
+    }
+}
