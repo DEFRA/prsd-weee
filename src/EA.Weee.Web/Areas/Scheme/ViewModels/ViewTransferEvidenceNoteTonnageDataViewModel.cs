9629a65b2bb3fb5450ad2d0bf2ff6060e8f3409a
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-05 16:01:39 +0100

    Revert changes made to zero or null values

--- src/EA.Weee.Web/Areas/Scheme/ViewModels/ViewTransferEvidenceNoteTonnageDataViewModel.cs
+++ src/EA.Weee.Web/Areas/Scheme/ViewModels/ViewTransferEvidenceNoteTonnageDataViewModel.cs
@@ -7,9 +7,15 @@
     public class ViewTransferEvidenceNoteTonnageDataViewModel
     {
         public virtual int ReferenceId { get; set; }
+
         public virtual NoteType Type { get; set; }
+
         public virtual string ReferenceDisplay => $"{Type.ToDisplayString()}{ReferenceId}";
+
         public virtual IList<EvidenceCategoryValue> CategoryValues { get; set; }
-        public virtual bool DisplayTransferNote => CategoryValues != null && CategoryValues.Count > 0;
+
+        public virtual bool DisplayTransferNote => true;
+
+        public NoteStatus Status { get; set; }
     }
 }
