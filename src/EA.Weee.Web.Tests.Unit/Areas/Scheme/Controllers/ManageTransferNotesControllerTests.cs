3e28a62b3f37937067e1e89d2b29140742461324
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-06 09:12:28 +0100

    Add in balancing scheme controller

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/Controllers/ManageTransferNotesControllerTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Scheme.Controllers
+{
+    using FluentAssertions;
+    using Web.Areas.Scheme.Controllers;
+    using Xunit;
+
+    public class ManageTransferNotesControllerTests
+    {
+        [Fact]
+        public void CheckManageTransferNotesControllerInheritsBalancingSchemeEvidenceBaseController()
+        {
+            typeof(ManageTransferNotesController).BaseType.Name.Should().Be(nameof(BalancingSchemeEvidenceBaseController));
+        }
+    }
+}
