48f7ec004854127fffb4a34b4f1676b2790a1fb4
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-08 17:00:28 +0100

    Fix issue with integration test roles

--- src/EA.Weee.Integration.Tests/Handlers/GetAllNotesRequestHandlerIntegrationTests.cs
+++ src/EA.Weee.Integration.Tests/Handlers/GetAllNotesRequestHandlerIntegrationTests.cs
@@ -148,7 +148,7 @@
                 var authority = Query.GetEaCompetentAuthority();
                 var role = Query.GetInternalUserRole();
 
-                if (!Query.CompetentAuthorityUserExists(UserId.ToString()))
+                if (!Query.CompetentAuthorityUserExists(UserId.ToString(), role.Id))
                 {
                     CompetentAuthorityUserDbSetup.Init().WithUserIdAndAuthorityAndRole(UserId.ToString(), authority.Id, role.Id)
                         .Create();
