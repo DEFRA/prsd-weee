eb7890f51d768a90fc32b6cf62dd6fb585e4ea5d
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-11 09:49:15 +0100

    Add back link to edit categories and recipient

--- src/EA.Weee.Web/Infrastructure/SchemeTransferEvidenceRedirect.cs
+++ src/EA.Weee.Web/Infrastructure/SchemeTransferEvidenceRedirect.cs
@@ -5,5 +5,6 @@
         public static string ViewDraftTransferEvidenceRouteName = "Scheme_view_transfer";
         public static string ReviewSubmittedTransferEvidenceRouteName = "Scheme_review_transfer";
         public static string ViewSubmittedTransferEvidenceRouteName = "Scheme_submitted_transfer";
+        public static string EditDraftTransferEvidenceRouteName = "Scheme_edit_transfer";
     }
 }
