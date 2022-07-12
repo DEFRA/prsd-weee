6ed1b9ba69f78072b36bd925387df0c4f43ba817
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-07-12 14:07:41 +0100

    Unit tests for the request and request handler added

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/ManageEvidenceNotes/Requests/GetEvidenceNoteTransfersForInternalUserRequestTests.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Admin.ManageEvidenceNotes.Requests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GetEvidenceNoteTransfersForInternalUserRequestTests
+    {
+    }
+}
