305d55263577784cbd157ffa0bb91cf6d25cfc0a
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-07-12 14:16:17 +0100

    Unit tests added

--- src/EA.Weee.RequestHandlers.Tests.Unit/Admin/ManageEvidenceNotes/Requests/GetEvidenceNoteTransfersForInternalUserRequestTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/ManageEvidenceNotes/Requests/GetEvidenceNoteTransfersForInternalUserRequestTests.cs
@@ -1,12 +1,37 @@
 ﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Admin.ManageEvidenceNotes.Requests
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using EA.Weee.Requests.Admin;
+    using FluentAssertions;
+    using Xunit;
 
     public class GetEvidenceNoteTransfersForInternalUserRequestTests
     {
+        [Fact]
+        public void GetEvidenceNoteTransfersForInternalUserRequest_ShouldHaveSerializableAttribute()
+        {
+            typeof(GetEvidenceNoteTransfersForInternalUserRequest).Should().BeDecoratedWith<SerializableAttribute>();
+        }
+
+        [Fact]
+        public void GetEvidenceNoteTransfersForInternalUserRequest_Constructor_GivenEmptyEvidenceNote()
+        {
+            var result = Record.Exception(() => new GetEvidenceNoteTransfersForInternalUserRequest(Guid.Empty));
+
+            result.Should().BeOfType<ArgumentException>();
+        }
+
+        [Fact]
+        public void GetEvidenceNoteTransfersForInternalUserRequest_GivenEvidenceNoteId_PropertiesShouldBeSet()
+        {
+            //arrange
+            var id = Guid.NewGuid();
+
+            //act
+            var result = new GetEvidenceNoteTransfersForInternalUserRequest(id);
+
+            //assert
+            result.EvidenceNoteId.Should().Be(id);
+        }
     }
 }
