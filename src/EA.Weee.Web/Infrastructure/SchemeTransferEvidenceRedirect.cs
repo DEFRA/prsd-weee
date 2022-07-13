6af5ce66d3883e341507a0dc3ecb04b1a3056271
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-07-13 15:46:33 +0100

    Adds returned/rejected/approved notes to view and transfer evidence tab Adds returned/rejected routes

--- src/EA.Weee.Web/Infrastructure/SchemeTransferEvidenceRedirect.cs
+++ src/EA.Weee.Web/Infrastructure/SchemeTransferEvidenceRedirect.cs
@@ -7,5 +7,7 @@
         public static string ViewSubmittedTransferEvidenceRouteName = "Scheme_submitted_transfer";
         public static string ViewApprovedTransferEvidenceRouteName = "Scheme_approved_transfer";
         public static string EditDraftTransferEvidenceRouteName = "Scheme_edit_transfer";
+        public static string ViewReturnedTransferEvidenceRouteName = "Scheme_returned_transfer";
+        public static string ViewRejectedTransferEvidenceRouteName = "Scheme_rejected_transfer";
     }
 }
