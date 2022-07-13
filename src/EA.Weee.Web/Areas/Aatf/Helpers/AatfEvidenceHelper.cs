c620c7ebc6d148751b55f6b700c1fc3c7363f2e0
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-13 11:56:07 +0100

    Changes to prevent create evidence for selected compliance year

--- src/EA.Weee.Web/Areas/Aatf/Helpers/AatfEvidenceHelper.cs
+++ src/EA.Weee.Web/Areas/Aatf/Helpers/AatfEvidenceHelper.cs
@@ -8,16 +8,16 @@
 
     public class AatfEvidenceHelper : IAatfEvidenceHelper
     {
-        public bool AatfCanEditCreateNotes(List<AatfData> aatfs, Guid aatfId, int? complianceYear)
+        public bool AatfCanEditCreateNotes(List<AatfData> aatfs, Guid aatfId, int complianceYear)
         {
             var currentAatf = aatfs.FirstOrDefault(a => a.Id == aatfId);
 
             Condition.Requires(currentAatf).IsNotNull();
 
             //is there an aatf for the org in the selected compliance year that can create / edit notes
-            var filter = aatfs.Where(a => a.AatfId == currentAatf.AatfId && a.CanCreateEditEvidence);
-
-            return complianceYear.HasValue ? filter.Any(a => a.ComplianceYear == complianceYear) : filter.Any();
+            return aatfs.Any(a => a.AatfId == currentAatf.AatfId && 
+                                          a.CanCreateEditEvidence &&
+                                          a.ComplianceYear == complianceYear);
         }
     }
 }
