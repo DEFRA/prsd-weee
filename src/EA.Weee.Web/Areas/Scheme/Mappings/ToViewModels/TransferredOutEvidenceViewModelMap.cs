6b6fc94397599917a8b6bd232fc98320156f6f6a
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-07-14 14:41:45 +0100

    47622 UI code - view rejected/returned evidence transfer for 47601 - Unit+Integration Tests will come in next PR

--- src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/TransferredOutEvidenceViewModelMap.cs
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/TransferredOutEvidenceViewModelMap.cs
@@ -22,7 +22,11 @@
 
             foreach (var evidenceNoteRowViewModel in model.EvidenceNotesDataList)
             {
-                evidenceNoteRowViewModel.DisplayViewLink = evidenceNoteRowViewModel.Status.Equals(NoteStatus.Draft) || evidenceNoteRowViewModel.Status.Equals(NoteStatus.Submitted) || evidenceNoteRowViewModel.Status.Equals(NoteStatus.Approved);
+                evidenceNoteRowViewModel.DisplayViewLink = evidenceNoteRowViewModel.Status.Equals(NoteStatus.Draft) ||
+                                                           evidenceNoteRowViewModel.Status.Equals(NoteStatus.Submitted) ||
+                                                           evidenceNoteRowViewModel.Status.Equals(NoteStatus.Rejected) ||
+                                                           evidenceNoteRowViewModel.Status.Equals(NoteStatus.Returned) ||
+                                                           evidenceNoteRowViewModel.Status.Equals(NoteStatus.Approved);
                 evidenceNoteRowViewModel.DisplayEditLink = evidenceNoteRowViewModel.Status.Equals(NoteStatus.Draft);
             }
 
