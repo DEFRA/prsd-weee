0c5d4668ba70b7a0145b0ae7c9f47182d953c282
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-14 14:54:22 +0100

    Temporary change until locked compliance year comes in

--- src/EA.Weee.RequestHandlers/Mappings/AatfDataWithSystemTimeMap.cs
+++ src/EA.Weee.RequestHandlers/Mappings/AatfDataWithSystemTimeMap.cs
@@ -41,8 +41,8 @@
 
             if (aatf.FacilityType == FacilityType.Aatf)
             {
-                if (WindowHelper.IsDateInComplianceYear(aatf.ComplianceYear, source.SystemDateTime)
-                    && approvalDateValid &&
+                //if (WindowHelper.IsDateInComplianceYear(aatf.ComplianceYear, source.SystemDateTime)
+                if (approvalDateValid &&
                     aatf.AatfStatus == AatfStatus.Approved)
                 {
                     canCreateEdit = true;
