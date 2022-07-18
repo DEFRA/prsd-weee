6c2ff49e22768c6314305e1e74f6edf935bb7932
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-18 18:04:28 +0100

    Updates to save validation

---
+++ src/EA.Weee.RequestHandlers/AatfEvidence/SaveEvidenceNoteRequestBase.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Weee.RequestHandlers.AatfEvidence
+{
+    using System;
+    using Core.Helpers;
+    using Domain.AatfReturn;
+
+    public abstract class SaveEvidenceNoteRequestBase
+    {
+        public void AatfIsValidToSave(Aatf aatf, DateTime systemDateTime)
+        {
+            if (aatf.AatfStatus != AatfStatus.Approved || !WindowHelper.IsDateInComplianceYear(aatf.ComplianceYear, systemDateTime) || (aatf.ApprovalDate.HasValue && aatf.ApprovalDate.Value.Date > systemDateTime.Date))
+            {
+                throw new InvalidOperationException(
+                    $"Aatf with Id {aatf.AatfId} has invalid status to save");
+            }
+        }
+    }
+}
