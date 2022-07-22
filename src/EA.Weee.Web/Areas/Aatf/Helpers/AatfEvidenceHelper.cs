0310821e9f30e3c7067c9c7b492e84521c51dd76
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-22 11:52:30 +0100

    Fix issues with redirection using back button

--- src/EA.Weee.Web/Areas/Aatf/Helpers/AatfEvidenceHelper.cs
+++ src/EA.Weee.Web/Areas/Aatf/Helpers/AatfEvidenceHelper.cs
@@ -5,9 +5,17 @@
     using System.Linq;
     using CuttingEdge.Conditions;
     using EA.Weee.Core.AatfReturn;
+    using Services;
 
     public class AatfEvidenceHelper : IAatfEvidenceHelper
     {
+        private readonly ConfigurationService configurationService;
+
+        public AatfEvidenceHelper(ConfigurationService configurationService)
+        {
+            this.configurationService = configurationService;
+        }
+
         public bool AatfCanEditCreateNotes(List<AatfData> aatfs, Guid aatfId, int complianceYear)
         {
             var currentAatf = aatfs.FirstOrDefault(a => a.Id == aatfId);
@@ -18,6 +26,16 @@
             return aatfs.Any(a => a.AatfId == currentAatf.AatfId && 
                                           a.CanCreateEditEvidence &&
                                           a.ComplianceYear == complianceYear);
+        }
+
+        public List<AatfData> GroupedValidAatfs(List<AatfData> aatfs)
+        {
+            return aatfs
+                .Where(a => a.EvidenceSiteDisplay && a.ApprovalDate.HasValue && a.ApprovalDate.Value.Date > configurationService.CurrentConfiguration.EvidenceNotesSiteSelectionDateFrom)
+                .GroupBy(a => a.AatfId)
+                .Select(a => a.OrderByDescending(a1 => a1.ComplianceYear).First())
+                .OrderBy(a => a.Name)
+                .ToList();
         }
     }
 }
