aa0ef04d7c3991a3ab6283a6596a1bde001b53aa
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-07-22 11:34:01 +0100

    Partial changes for View obligations evidence summary

---
+++ src/EA.Weee.Core/Admin/Obligation/ObligationEvidenceTotalValue.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Weee.Core.Admin.Obligation
+{
+    using EA.Weee.Core.Shared;
+
+    public class ObligationEvidenceTotalValue : CategoryValue
+    {
+        public virtual string Total { get; set; }
+        public virtual string Category2To10Total { get; set; }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Obligations/ViewObligationsAndEvidenceSummaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Obligations
+{
+    using EA.Weee.Core.Admin.Obligation;
+    using System;
+    using System.Collections.Generic;
+
+    public class ViewObligationsAndEvidenceSummaryViewModel
+    {
+        public int SelectedComplianceYear { get; set; }
+        public Guid SelectedPCS { get; set; }
+        public IList<ObligationEvidenceValue> ObligationEvidenceValues { get; set; }
+        public IList<ObligationEvidenceTotalValue> ObligationEvidenceTotalValues { get; set; }
+    }
+}
