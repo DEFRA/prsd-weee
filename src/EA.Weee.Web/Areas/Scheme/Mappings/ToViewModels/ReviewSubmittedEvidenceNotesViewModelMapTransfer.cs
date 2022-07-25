07303bcdca227718c663129d063036b4adedbc4e
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-25 13:45:51 +0100

    WIP merge from develop

--- src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/ReviewSubmittedEvidenceNotesViewModelMapTransfer.cs
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/ReviewSubmittedEvidenceNotesViewModelMapTransfer.cs
@@ -2,7 +2,6 @@
 {
     using EA.Weee.Core.AatfEvidence;
     using System;
-    using System.Collections.Generic;
     using EA.Weee.Web.ViewModels.Shared;
 
     public class ReviewSubmittedEvidenceNotesViewModelMapTransfer : BaseEvidenceNotesViewModelMapTransfer
