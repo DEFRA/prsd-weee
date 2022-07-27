90dfb5c32650e0a6fffe777efdd02a7b7a5a4293
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-07-27 11:04:19 +0100

    Refactor the note validation attributes

---
+++ src/EA.Weee.Web/Areas/Scheme/Attributes/CheckNoteAttributeBase.cs
@@ -0,0 +1,62 @@
+﻿namespace EA.Weee.Web.Areas.Scheme.Attributes
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Api.Client;
+    using Services.Caching;
+
+    public abstract class CheckNoteAttributeBase : ActionFilterAttribute
+    {
+        public IWeeeCache Cache { get; set; }
+
+        public Func<IWeeeClient> Client { get; set; }
+
+        public abstract Task OnAuthorizationAsync(ActionExecutingContext filterContext, Guid pcsId);
+
+        protected Guid TryGetPcsId(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.ActionParameters.TryGetValue("pcsId", out var idActionParameter))
+            {
+                throw new ArgumentException("No pcs ID was specified.");
+            }
+
+            if (!(Guid.TryParse(idActionParameter.ToString(), out var pcsIdActionParameter)))
+            {
+                throw new ArgumentException("The specified pcs ID is not valid.");
+            }
+
+            return pcsIdActionParameter;
+        }
+
+        protected Guid TryGetEvidenceNoteId(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.ActionParameters.TryGetValue("evidenceNoteId", out var idActionParameter))
+            {
+                throw new ArgumentException("No evidence note id specified");
+            }
+
+            if (!(Guid.TryParse(idActionParameter.ToString(), out var evidenceNoteIdActionParameter)))
+            {
+                throw new ArgumentException("The specified evidence note id is not valid.");
+            }
+
+            return evidenceNoteIdActionParameter;
+        }
+
+        protected int TryGetComplianceYear(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.ActionParameters.TryGetValue("complianceYear", out var idActionParameter))
+            {
+                throw new ArgumentException("No compliance year was specified.");
+            }
+
+            if (!(int.TryParse(idActionParameter.ToString(), out var complianceYearIdActionParameter)))
+            {
+                throw new ArgumentException("The specified compliance year is not valid.");
+            }
+
+            return complianceYearIdActionParameter;
+        }
+    }
+}
--- src/EA.Weee.Web/Areas/Scheme/Attributes/CheckTransferNoteAttributeBase.cs
+++ src/EA.Weee.Web/Areas/Scheme/Attributes/CheckTransferNoteAttributeBase.cs
@@ -2,12 +2,37 @@
 {
     using System;
     using System.Web.Mvc;
+    using System.Web.Routing;
     using Core.Helpers;
     using Core.Scheme;
     using Core.Shared;
+    using Filters;
 
-    public abstract class CheckTransferNoteAttributeBase : ActionFilterAttribute
+    public abstract class CheckTransferNoteAttributeBase : CheckNoteAttributeBase
     {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var pcsId = TryGetPcsId(context);
+
+            AsyncHelpers.RunSync(async () =>
+            {
+                try
+                {
+                    await OnAuthorizationAsync(context, pcsId);
+                }
+                catch
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary()
+                    {
+                        { "action", "Index" },
+                        { "controller", "ManageEvidenceNotes" }
+                    });
+                }
+            });
+
+            base.OnActionExecuting(context);
+        }
+
         public void ValidateSchemeAndWindow(SchemePublicInfo scheme, int complianceYear, DateTime date)
         {
             if (scheme.Status == SchemeStatus.Withdrawn)
