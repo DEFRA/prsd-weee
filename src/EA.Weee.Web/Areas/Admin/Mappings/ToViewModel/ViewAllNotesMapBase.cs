77553a48ef02ea038e95ee16be32f47e5880bac9
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-07-27 10:29:23 +0100

    Added compliance year filter to View All evidence notes for Admin area

--- src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/ViewAllNotesMapBase.cs
+++ src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/ViewAllNotesMapBase.cs
@@ -2,7 +2,9 @@
 {
     using System.Collections.Generic;
     using System.Linq;
+    using CuttingEdge.Conditions;
     using EA.Prsd.Core.Mapper;
+    using EA.Weee.Web.Extensions;
     using Web.ViewModels.Shared;
 
     public abstract class ViewAllNotesMapBase<T> where T : IManageEvidenceViewModel, new()
@@ -16,11 +18,21 @@
 
         protected T CreateModel(ViewAllEvidenceNotesMapTransfer source)
         {
+            Condition.Requires(source).IsNotNull();
+
+            var complianceYear =
+             source.ManageEvidenceNoteViewModel != null && source.ManageEvidenceNoteViewModel.SelectedComplianceYear > 0
+                 ? source.ManageEvidenceNoteViewModel.SelectedComplianceYear
+                 : source.CurrentDate.Year;
+
             var model = new T
             {
                 EvidenceNotesDataList = Mapper.Map<List<EvidenceNoteRowViewModel>>(source.NoteData.Results.ToList()),
                 ManageEvidenceNoteViewModel = source.ManageEvidenceNoteViewModel
             };
+
+            model.ManageEvidenceNoteViewModel.ComplianceYearList = ComplianceYearHelper.FetchCurrentComplianceYearsForEvidence(source.CurrentDate);
+            model.ManageEvidenceNoteViewModel.SelectedComplianceYear = complianceYear;
 
             return model;
         }
