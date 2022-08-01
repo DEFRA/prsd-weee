794413d94e3a3d41065149ce4b6a413ab1447ad4
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-08-01 18:41:59 +0100

    Adds partial changes for view obligation evidence summary

--- src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/ViewObligationsAndEvidenceSummaryViewModelMap.cs
+++ src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/ViewObligationsAndEvidenceSummaryViewModelMap.cs
@@ -1,11 +1,68 @@
 ﻿namespace EA.Weee.Web.Areas.Admin.Mappings.ToViewModel
 {
-    using System;
+    using CuttingEdge.Conditions;
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.Admin.Obligation;
+    using EA.Weee.Domain.Lookup;
+    using EA.Weee.Web.Areas.Admin.ViewModels.Obligations;
+    using EA.Weee.Web.ViewModels.Returns.Mappings.ToViewModel;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Web;
 
-    public class ViewObligationsAndEvidenceSummaryViewModelMap
+    public class ViewObligationsAndEvidenceSummaryViewModelMap : IMap<ViewObligationsAndEvidenceSummaryViewModelMapTransfer, ViewObligationsAndEvidenceSummaryViewModel>
     {
+        private readonly ITonnageUtilities tonnageUtilities;
+
+        public ViewObligationsAndEvidenceSummaryViewModelMap(ITonnageUtilities tonnageUtilities)
+        {
+            this.tonnageUtilities = tonnageUtilities;
+        }
+
+        public ViewObligationsAndEvidenceSummaryViewModel Map(ViewObligationsAndEvidenceSummaryViewModelMapTransfer source)
+        {
+            Condition.Requires(source).IsNotNull();
+
+            var model = new ViewObligationsAndEvidenceSummaryViewModel
+            {
+                ObligationEvidenceValues = new List<ObligationEvidenceValue>()
+            };
+
+            var excludedCategories = new List<WeeeCategory>()
+            {
+                WeeeCategory.LargeHouseholdAppliances,
+                WeeeCategory.DisplayEquipment,
+                WeeeCategory.CoolingApplicancesContainingRefrigerants,
+                WeeeCategory.GasDischargeLampsAndLedLightSources,
+                WeeeCategory.PhotovoltaicPanels
+            };
+
+            foreach (var summaryCategory in source.ObligationEvidenceSummaryData.ObligationEvidenceValues)
+            {
+                model.ObligationEvidenceValues.Add(new ObligationEvidenceValue(summaryCategory.CategoryId)
+                {
+                    Obligation = tonnageUtilities.CheckIfTonnageIsNull(summaryCategory.Obligation),
+                    Evidence = tonnageUtilities.CheckIfTonnageIsNull(summaryCategory.Evidence),
+                    Reused = tonnageUtilities.CheckIfTonnageIsNull(summaryCategory.Reuse),
+                    TransferredOut = tonnageUtilities.CheckIfTonnageIsNull(summaryCategory.TransferredOut),
+                    TransferredIn = tonnageUtilities.CheckIfTonnageIsNull(summaryCategory.TransferredIn),
+                    Difference = tonnageUtilities.CheckIfTonnageIsNull(summaryCategory.Difference < 0 ? 0 : summaryCategory.Difference)
+                });
+            }
+
+            model.ObligationTotal = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Sum(x => x.Obligation));
+            model.Obligation210Total = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Where(x => !excludedCategories.Contains((WeeeCategory)x.CategoryId)).Sum(x => x.Obligation));
+            model.EvidenceTotal = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Sum(x => x.Evidence));
+            model.Evidence210Total = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Where(x => !excludedCategories.Contains((WeeeCategory)x.CategoryId)).Sum(x => x.Evidence));
+            model.ReuseTotal = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Sum(x => x.Reuse));
+            model.Reuse210Total = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Where(x => !excludedCategories.Contains((WeeeCategory)x.CategoryId)).Sum(x => x.Reuse));
+            model.TransferredOutTotal = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Sum(x => x.TransferredOut));
+            model.TransferredOut210Total = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Where(x => !excludedCategories.Contains((WeeeCategory)x.CategoryId)).Sum(x => x.TransferredOut));
+            model.TransferredInTotal = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Sum(x => x.TransferredIn));
+            model.TransferredIn210Total = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Where(x => !excludedCategories.Contains((WeeeCategory)x.CategoryId)).Sum(x => x.TransferredIn));
+            model.DifferenceTotal = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Sum(x => x.Difference));
+            model.Difference210Total = tonnageUtilities.CheckIfTonnageIsNull(source.ObligationEvidenceSummaryData.ObligationEvidenceValues.Where(x => !excludedCategories.Contains((WeeeCategory)x.CategoryId)).Sum(x => x.Difference));
+
+            return model;
+        }
     }
 }
---
+++ src/EA.Weee.Web/Areas/Admin/Mappings/ToViewModel/ViewObligationsAndEvidenceSummaryViewModelMapTransfer.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Weee.Web.Areas.Admin.Mappings.ToViewModel
+{
+    using CuttingEdge.Conditions;
+    using EA.Weee.Core.Admin.Obligation;
+    using System;
+
+    public class ViewObligationsAndEvidenceSummaryViewModelMapTransfer
+    {
+        public Guid OrganisationId { get; private set; }
+
+        public Guid PcsId { get; private set; }
+
+        public ObligationEvidenceSummaryData ObligationEvidenceSummaryData { get; private set; }
+
+        public ViewObligationsAndEvidenceSummaryViewModelMapTransfer(Guid organisationId, Guid pcsId, ObligationEvidenceSummaryData obligationEvidenceSummaryData)
+        {
+            Condition.Requires(organisationId).IsNotEqualTo(Guid.Empty);
+            Condition.Requires(pcsId).IsNotEqualTo(Guid.Empty);
+            Condition.Requires(obligationEvidenceSummaryData).IsNotNull();
+
+            OrganisationId = organisationId;
+            PcsId = pcsId;
+            ObligationEvidenceSummaryData = obligationEvidenceSummaryData;
+        }
+    }
+}
