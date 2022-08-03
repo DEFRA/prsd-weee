b67ec0e32c55d947398d78d4bd9f154fd1c99a62
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-08-03 19:09:58 +0100

    Resolve issues with failing tests

--- src/EA.Weee.Web/Constant/SessionKeyConstant.cs
+++ src/EA.Weee.Web/Constant/SessionKeyConstant.cs
@@ -6,6 +6,5 @@
         public static string EditEvidenceViewModelKey = "editEvidenceNoteViewModel";
         public static string EditTransferTonnageViewModelKey = "editTransferTonnageViewModel";
         public static string FilterRecipientNameKey = "filterRecipientNameKey";
-        public static string AatfSelectedComplianceYear = "aatfSelectedComplianceYear";
     }
 }
