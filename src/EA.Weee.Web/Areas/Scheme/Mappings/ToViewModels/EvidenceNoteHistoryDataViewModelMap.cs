196dc2bac2c2a77ae5a58a4df85c3f82c8fc54ba
Nathan Hogan <Nathan.Hogan@Civica.co.uk>
2022-08-08 17:09:43 +0100

    PR Fixes

--- src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/EvidenceNoteHistoryDataViewModelMap.cs
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/EvidenceNoteHistoryDataViewModelMap.cs
@@ -1,16 +1,17 @@
 ﻿namespace EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels
 {
     using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.AatfEvidence;
     using EA.Weee.Web.Areas.Scheme.ViewModels;
     using System.Collections.Generic;
 
-    public class EvidenceNoteHistoryDataViewModelMap : IMap<EvidenceNoteHistoryDataViewModelMapTransfer, IList<EvidenceNoteHistoryViewModel>>
+    public class EvidenceNoteHistoryDataViewModelMap : IMap<IList<EvidenceNoteHistoryData>, IList<EvidenceNoteHistoryViewModel>>
     {
-        public IList<EvidenceNoteHistoryViewModel> Map(EvidenceNoteHistoryDataViewModelMapTransfer source)
+        public IList<EvidenceNoteHistoryViewModel> Map(IList<EvidenceNoteHistoryData> source)
         {
             var model = new List<EvidenceNoteHistoryViewModel>();
 
-            foreach (var historyData in source.NoteHistoryData)
+            foreach (var historyData in source)
             {
                 model.Add(new EvidenceNoteHistoryViewModel(historyData.Id, historyData.Reference, historyData.TransferredTo, historyData.Type, historyData.Status, historyData.SubmittedDate));
             }
