b5e2323668255068ecee94359d86186e4bb5393b
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-08-09 10:18:22 +0100

    48924 implementation for 48694 Void Transfer + confirmation + success message for internal

--- src/EA.Weee.Web/Areas/Admin/Controllers/ManageEvidenceNotesController.cs
+++ src/EA.Weee.Web/Areas/Admin/Controllers/ManageEvidenceNotesController.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net;
+    using System.Security.Claims;
     using System.Threading.Tasks;
     using System.Web.Mvc;
     using Core.AatfEvidence;
@@ -10,14 +12,17 @@
     using EA.Prsd.Core.Mapper;
     using EA.Weee.Api.Client;
     using EA.Weee.Core.Helpers;
+    using EA.Weee.Requests.AatfEvidence;
     using EA.Weee.Requests.Admin;
     using EA.Weee.Requests.Shared;
+    using EA.Weee.Security;
     using EA.Weee.Web.Areas.Admin.Controllers.Base;
     using EA.Weee.Web.Areas.Admin.Mappings.ToViewModel;
     using EA.Weee.Web.Areas.Admin.ViewModels.ManageEvidenceNotes;
     using EA.Weee.Web.Areas.Admin.ViewModels.Shared;
     using EA.Weee.Web.Areas.Scheme.Mappings.ToViewModels;
     using EA.Weee.Web.Constant;
+    using EA.Weee.Web.Filters;
     using EA.Weee.Web.Infrastructure;
     using EA.Weee.Web.Services;
     using EA.Weee.Web.Services.Caching;
@@ -85,6 +90,7 @@
         }
 
         [HttpGet]
+        [AuthorizeInternalClaims(Claims.InternalAdmin)]
         public async Task<ActionResult> ViewEvidenceNoteTransfer(Guid evidenceNoteId)
         {
             SetBreadcrumb(BreadCrumbConstant.ManageEvidenceNotesAdmin);
@@ -98,8 +104,92 @@
                 var model = mapper.Map<ViewTransferNoteViewModel>(new ViewTransferNoteViewModelMapTransfer(result.TransferredOrganisationData.Id,
                    result, TempData[ViewDataConstant.TransferEvidenceNoteDisplayNotification]));
 
+                model.CanVoid = IsInternalAdminUser();
+
                 return View(model);
             }
+        }
+
+        [HttpGet]
+        [AuthorizeInternalClaims(Claims.InternalAdmin)]
+        public async Task<ActionResult> VoidTransferNote(Guid transferEvidenceNoteId)
+        {
+            SetBreadcrumb(BreadCrumbConstant.ManageEvidenceNotesAdmin);
+
+            using (var client = apiClient())
+            {
+                var request = new GetEvidenceNoteTransfersForInternalUserRequest(transferEvidenceNoteId);
+
+                var transferNoteData = await client.SendAsync(User.GetAccessToken(), request);
+
+                if (transferNoteData.Type == NoteType.Transfer && transferNoteData.Status == NoteStatus.Approved)
+                {
+                    var model = mapper.Map<ViewTransferNoteViewModel>(new ViewTransferNoteViewModelMapTransfer(transferNoteData.TransferredOrganisationData.Id,
+                       transferNoteData, TempData[ViewDataConstant.TransferEvidenceNoteDisplayNotification]));
+
+                    model.CanVoid = IsInternalAdminUser();
+
+                    return View("VoidTransferNote", model);
+                }
+
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden, "This note is not a transfer note or has not been approved.");
+            }
+        }
+
+        [HttpPost]
+        [AuthorizeInternalClaims(Claims.InternalAdmin)]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> VoidTransferNote(ViewTransferNoteViewModel model)
+        {
+            SetBreadcrumb(BreadCrumbConstant.ManageEvidenceNotesAdmin);
+
+            using (var client = apiClient())
+            {
+                var request = new GetEvidenceNoteTransfersForInternalUserRequest(model.EvidenceNoteId);
+
+                var result = await client.SendAsync(User.GetAccessToken(), new VoidTransferNoteRequest(model.EvidenceNoteId, model.VoidedReason));
+
+                var transferNoteData = await client.SendAsync(User.GetAccessToken(), request);
+
+                if (transferNoteData.Type == NoteType.Transfer && transferNoteData.Status == NoteStatus.Void)
+                {
+                    model.CanVoid = IsInternalAdminUser();
+
+                    TempData[ViewDataConstant.TransferEvidenceNoteDisplayNotification] = true;
+
+                    model = mapper.Map<ViewTransferNoteViewModel>(new ViewTransferNoteViewModelMapTransfer(transferNoteData.TransferredOrganisationData.Id,
+                       transferNoteData, TempData[ViewDataConstant.TransferEvidenceNoteDisplayNotification]));
+
+                    return RedirectToAction("ViewTransferNote", "ManageEvidenceNotes", new { transferNoteId = model.EvidenceNoteId });
+                }
+            }
+
+            return View("VoidTransferNote", model);
+        }
+
+        [HttpGet]
+        [AuthorizeInternalClaims(Claims.InternalAdmin)]
+        public async Task<ActionResult> ViewTransferNote(Guid transferNoteId)
+        {
+            SetBreadcrumb(BreadCrumbConstant.ManageEvidenceNotesAdmin);
+
+            using (var client = apiClient())
+            {
+                var request = new GetEvidenceNoteTransfersForInternalUserRequest(transferNoteId);
+
+                var noteData = await client.SendAsync(User.GetAccessToken(), request);
+
+                var model = mapper.Map<ViewTransferNoteViewModel>(new ViewTransferNoteViewModelMapTransfer(noteData.TransferredOrganisationData.Id, 
+                    noteData, TempData[ViewDataConstant.TransferEvidenceNoteDisplayNotification]));
+
+                return View(model);
+            }
+        }
+
+        private bool IsInternalAdminUser()
+        {
+            var claimsPrincipal = new ClaimsPrincipal(this.User);
+            return claimsPrincipal.HasClaim(p => p.Value == Claims.InternalAdmin);
         }
 
         private async Task<ActionResult> ViewAllEvidenceNotes(IWeeeClient client, ManageEvidenceNoteViewModel manageEvidenceNoteViewModel, DateTime currentDate)
