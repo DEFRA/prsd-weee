449429c95faab47e8e4029f13edbf9059f3dd74d
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-08-10 17:24:50 +0100

    fix issue with unit test

--- src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/HomeControllerTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/HomeControllerTests.cs
@@ -169,7 +169,7 @@
         [InlineData(InternalUserActivity.ViewReports, "ChooseReport")]
         [InlineData(InternalUserActivity.ManagePcsCharges, "SelectAuthority")]
         [InlineData(InternalUserActivity.ManagePcsObligations, "SelectAuthority")]
-        [InlineData(InternalUserActivity.ViewPCSObligationAndEvidenceSummary, "Index")]
+        [InlineData(InternalUserActivity.ViewPCSObligationAndEvidenceSummary, "ViewObligationAndEvidenceSummary")]
         public void HttpPost_ChooseActivity_RedirectsToCorrectControllerAction(string selection, string action)
         {
             // Arrange
