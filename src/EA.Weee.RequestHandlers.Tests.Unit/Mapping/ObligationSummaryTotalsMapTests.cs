4ecb03c0a597f0cf82ae4d457bd8453a819ae936
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-08-12 15:27:16 +0100

    Add unit tests for GetObligationSummaryRequestHandler and unit test for mapper ObligationSummaryTotalsMap

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Mapping/ObligationSummaryTotalsMapTests.cs
@@ -0,0 +1,54 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Mapping
+{
+    using AutoFixture;
+    using Core.Helpers;
+    using DataAccess.StoredProcedure;
+    using FluentAssertions;
+    using Mappings;
+    using System;
+    using System.Linq;
+    using Weee.Tests.Core;
+    using Xunit;
+
+    public class ObligationSummaryTotalsMapTests : SimpleUnitTestBase
+    {
+        private readonly ObligationSummaryTotalsMap mapper;
+
+        public ObligationSummaryTotalsMapTests()
+        {
+            mapper = new ObligationSummaryTotalsMap();
+        }
+
+        [Fact]
+        public void Map_GivenNullSource_ArgumentNullExceptionExpected()
+        {
+            //act
+            var exception = Record.Exception(() => mapper.Map(null));
+
+            //assert
+            exception.Should().BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Map_GivenSource_DataShouldBeMapped()
+        {
+            //arrange
+            var source = TestFixture.CreateMany<ObligationEvidenceSummaryTotalsData>().ToList();
+
+            //act
+            var result = mapper.Map(source);
+
+            //assert
+            source.ForEach((s) =>
+            {
+                result.ObligationEvidenceValues.Should().Contain(o => o.Obligation == s.Obligation &&
+                                                                      o.CategoryId.ToInt() == s.CategoryId.ToInt() &&
+                                                                      o.TransferredOut == s.TransferredOut &&
+                                                                      o.Difference == s.ObligationDifference &&
+                                                                      o.Evidence == s.Evidence &&
+                                                                      o.TransferredIn == s.TransferredIn &&
+                                                                      o.Reuse == s.Reuse);
+            });
+        }
+    }
+}
---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/ObligationsControllerViewObligationAndEvidenceSummaryTests.cs
@@ -0,0 +1,54 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.Controllers
+{
+    using EA.Weee.Api.Client;
+    using EA.Weee.Web.Areas.Admin.Controllers;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using FakeItEasy;
+    using FluentAssertions;
+    using System;
+    using System.Web.Mvc;
+    using Prsd.Core.Mapper;
+    using Weee.Tests.Core;
+    using Xunit;
+
+    public class ObligationsControllerViewObligationAndEvidenceSummaryTests : SimpleUnitTestBase
+    {
+        private readonly IAppConfiguration configuration;
+        private readonly Func<IWeeeClient> apiClient;
+        private readonly IWeeeClient client;
+        private readonly IWeeeCache cache;
+        private readonly IMapper mapper;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly ObligationsController controller;
+
+        public ObligationsControllerViewObligationAndEvidenceSummaryTests()
+        {
+            configuration = A.Fake<IAppConfiguration>();
+            client = A.Fake<IWeeeClient>();
+            mapper = A.Fake<IMapper>();
+            apiClient = () => client;
+            breadcrumb = A.Fake<BreadcrumbService>();
+            cache = A.Fake<IWeeeCache>();
+
+            controller = new ObligationsController(configuration, breadcrumb, cache, apiClient, mapper);
+        }
+
+        [Fact]
+        public void ViewObligationAndEvidenceSummaryGet_IsDecoratedWith_HttpGetAttribute()
+        {
+            typeof(ObligationsController).GetMethod("ViewObligationAndEvidenceSummary", 
+                new[] { typeof(int?), typeof(Guid?) }).Should().BeDecoratedWith<HttpGetAttribute>();
+        }
+
+        [Fact]
+        public void ViewObligationAndEvidenceSummaryGet_ObligationComplianceYearsShouldBeRetrieved()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+    }
+}
