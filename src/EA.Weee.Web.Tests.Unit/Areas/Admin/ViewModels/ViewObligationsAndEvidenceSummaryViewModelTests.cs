b7b725bdd5fd45d656f95dc60575dd83866dab6e
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-08-23 16:30:01 +0100

    Unit tests for the models

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/ViewModels/ViewObligationsAndEvidenceSummaryViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.ViewModels
+{
+    using EA.Weee.Web.Areas.Admin.ViewModels.Obligations;
+    using EA.Weee.Web.Areas.Scheme.ViewModels.ManageEvidenceNotes;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ViewObligationsAndEvidenceSummaryViewModelTests
+    {
+        [Fact]
+        public void ViewObligationsAndEvidenceSummaryViewModelImplementsIObligationSummaryBase()
+        {
+            typeof(ViewObligationsAndEvidenceSummaryViewModel).Should().Implement<IObligationSummaryBase>();
+        }
+    }
+}
