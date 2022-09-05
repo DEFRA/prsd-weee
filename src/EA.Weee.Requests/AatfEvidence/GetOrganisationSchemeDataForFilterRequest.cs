2c2a30032c77add67ce4ceee9ba1f51846973c6e
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-05 15:48:45 +0100

    WIP fix scheme filter

---
+++ src/EA.Weee.Requests/AatfEvidence/GetOrganisationSchemeDataForFilterRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Weee.Requests.AatfEvidence
+{
+    using System;
+    using System.Collections.Generic;
+    using Core.Scheme;
+    using Prsd.Core.Mediator;
+
+    [Serializable]
+    public class GetOrganisationSchemeDataForFilterRequest : IRequest<List<OrganisationSchemeData>>
+    {
+        public Guid? OrganisationId { get; private set; }
+
+        public int ComplianceYear { get; private set; }
+    
+        public GetOrganisationSchemeDataForFilterRequest(Guid? organisationId, int complianceYear)
+        {
+            OrganisationId = organisationId;
+            ComplianceYear = complianceYear;
+        }
+    }
+}
