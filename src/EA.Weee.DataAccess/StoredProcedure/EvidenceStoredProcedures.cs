1d1fda8f14828fe0e8132510a010db5f755c5aab
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-06 16:19:36 +0100

    Changes for evidence note report extract

--- src/EA.Weee.DataAccess/StoredProcedure/EvidenceStoredProcedures.cs
+++ src/EA.Weee.DataAccess/StoredProcedure/EvidenceStoredProcedures.cs
@@ -17,26 +17,30 @@
         }
 
         public async Task<List<EvidenceNoteReportData>> GetEvidenceNoteOriginalTonnagesReport(
-            Guid? originatingOrganisationId, Guid? recipientOrganisationId)
+            int complianceYear, Guid? originatingOrganisationId, Guid? recipientOrganisationId)
         {
-            string queryString = "[Evidence].[getObligationEvidenceSummaryTotals] @ComplianceYear, @OrganisationId, @SchemeId ";
+            var queryString = "[Evidence].[getEvidenceNotesOriginalTonnage] @ComplianceYear, @OriginatingOrganisationId, @RecipientOrganisationId";
 
-            SqlParameter complianceYearParameter = new SqlParameter("@ComplianceYear", (short)complianceYear);
-            SqlParameter orgIdParameter = new SqlParameter("@OrganisationId", SqlDbType.UniqueIdentifier);
-            orgIdParameter.IsNullable = true;
-            orgIdParameter.Value = orgId.HasValue ? orgId.Value : (object)DBNull.Value;
-            SqlParameter pcsIdParameter = new SqlParameter("@SchemeId", SqlDbType.UniqueIdentifier);
-            pcsIdParameter.IsNullable = true;
-            pcsIdParameter.Value = pcsId.HasValue ? pcsId.Value : (object)DBNull.Value;
+            var complianceYearParameter = new SqlParameter("@ComplianceYear", (short)complianceYear);
+            var orgIdParameter = new SqlParameter("@OriginatingOrganisationId", SqlDbType.UniqueIdentifier)
+                {
+                    IsNullable = true,
+                    Value = originatingOrganisationId ?? (object)DBNull.Value
+                };
+            var pcsIdParameter = new SqlParameter("@RecipientOrganisationId", SqlDbType.UniqueIdentifier)
+                {
+                    IsNullable = true,
+                    Value = recipientOrganisationId ?? (object)DBNull.Value
+                };
 
-            return await context.Database.SqlQuery<ObligationEvidenceSummaryTotalsData>(queryString, complianceYearParameter, orgIdParameter, pcsIdParameter).ToListAsync();
+            return await context.Database.SqlQuery<EvidenceNoteReportData>(queryString, complianceYearParameter, orgIdParameter, pcsIdParameter).ToListAsync();
         }
 
         public async Task<List<AatfEvidenceSummaryTotalsData>> GetAatfEvidenceSummaryTotals(Guid aatfId, int complianceYear)
         {
-            string queryString = "[Evidence].[getAatfEvidenceSummaryTotals] @AatfId, @ComplianceYear";
-            SqlParameter aatfIdParameter = new SqlParameter("@AatfId", aatfId);
-            SqlParameter complianceYearParameter = new SqlParameter("@ComplianceYear", (short)complianceYear);
+            var queryString = "[Evidence].[getAatfEvidenceSummaryTotals] @AatfId, @ComplianceYear";
+            var aatfIdParameter = new SqlParameter("@AatfId", aatfId);
+            var complianceYearParameter = new SqlParameter("@ComplianceYear", (short)complianceYear);
 
             return await context.Database.SqlQuery<AatfEvidenceSummaryTotalsData>(queryString, aatfIdParameter, complianceYearParameter).ToListAsync();
         }
@@ -45,15 +49,19 @@
         {
             Condition.Requires(pcsId == null && orgId == null).IsFalse("pcsId and orgId cannot be both null");
             
-            string queryString = "[Evidence].[getObligationEvidenceSummaryTotals] @ComplianceYear, @OrganisationId, @SchemeId ";
+            var queryString = "[Evidence].[getObligationEvidenceSummaryTotals] @ComplianceYear, @OrganisationId, @SchemeId ";
 
-            SqlParameter complianceYearParameter = new SqlParameter("@ComplianceYear", (short)complianceYear);
-            SqlParameter orgIdParameter = new SqlParameter("@OrganisationId", SqlDbType.UniqueIdentifier);
-            orgIdParameter.IsNullable = true;
-            orgIdParameter.Value = orgId.HasValue ? orgId.Value : (object)DBNull.Value;
-            SqlParameter pcsIdParameter = new SqlParameter("@SchemeId", SqlDbType.UniqueIdentifier);
-            pcsIdParameter.IsNullable = true;
-            pcsIdParameter.Value = pcsId.HasValue ? pcsId.Value : (object)DBNull.Value;
+            var complianceYearParameter = new SqlParameter("@ComplianceYear", (short)complianceYear);
+            var orgIdParameter = new SqlParameter("@OrganisationId", SqlDbType.UniqueIdentifier)
+ {
+     IsNullable = true,
+     Value = orgId ?? (object)DBNull.Value
+ };
+            var pcsIdParameter = new SqlParameter("@SchemeId", SqlDbType.UniqueIdentifier)
+            {
+                IsNullable = true,
+                Value = pcsId ?? (object)DBNull.Value
+            };
 
             return await context.Database.SqlQuery<ObligationEvidenceSummaryTotalsData>(queryString, complianceYearParameter, orgIdParameter, pcsIdParameter).ToListAsync();
         }
