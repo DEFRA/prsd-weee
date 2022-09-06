d861715749536c987d4a5fa876a7c58f713e643f
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-06 11:39:31 +0100

    WIP

---
+++ src/EA.Weee.Web/Areas/Admin/Controllers/EvidenceReportsController.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Weee.Web.Areas.Admin.Controllers
+{
+    using Api.Client;
+    using Services;
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using ViewModels.EvidenceReports;
+
+    public class EvidenceReportsController : ReportsBaseController
+    {
+        private readonly ConfigurationService configurationService;
+
+        public EvidenceReportsController(Func<IWeeeClient> apiClient, BreadcrumbService breadcrumb, ConfigurationService configurationService) : 
+            base(apiClient, breadcrumb)
+        {
+            this.configurationService = configurationService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EvidenceNoteReport()
+        {
+            SetBreadcrumb();
+
+            var model = new EvidenceReportViewModel();
+
+            return View(model);
+        }
+    }
+}
