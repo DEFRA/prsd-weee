aaaefd95461efe6af36277b53cf6980fb2cd74d8
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-06 11:39:31 +0100

    WIP

---
+++ src/EA.Weee.Core/AatfEvidence/TonnageToDisplayReportEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Weee.Core.AatfEvidence
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TonnageToDisplayReportEnum
+    {
+        [Display(Name = "Original Tonnages")]
+        OriginalTonnages = 1
+    }
+}
