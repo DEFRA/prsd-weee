c2d9ea596b452c068b254fbe4415f3fffb50198d
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-16 16:24:31 +0100

    Prototype changes for cy of aard / pbs / scheme

--- src/EA.Weee.Web/ViewModels/Shared/Mapping/ManageEvidenceNoteTransfer.cs
+++ src/EA.Weee.Web/ViewModels/Shared/Mapping/ManageEvidenceNoteTransfer.cs
@@ -1,16 +1,16 @@
-﻿namespace EA.Weee.Web.Areas.Aatf.Mappings.ToViewModel
+﻿namespace EA.Weee.Web.ViewModels.Shared.Mapping
 {
     using System;
     using System.Collections.Generic;
-    using Core.AatfReturn;
-    using EA.Weee.Web.Extensions;
-    using Prsd.Core;
-    using ViewModels;
+    using EA.Prsd.Core;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Web.Areas.Aatf.ViewModels;
 
     [Serializable]
     public class ManageEvidenceNoteTransfer
     {
         public Guid OrganisationId { get; protected set; }
+
         public Guid AatfId { get; protected set; }
 
         public AatfData AatfData { get; protected set; }
@@ -42,6 +42,20 @@
             AatfData = aatfData;
             Aatfs = aatfs;
             AatfId = aatfId;
+            FilterViewModel = filterViewModel;
+            RecipientWasteStatusFilterViewModel = recipientWasteStatusFilterViewModel;
+            SubmittedDatesFilterViewModel = submittedDatesFilterViewModel;
+            ComplianceYear = complianceYear;
+            CurrentDate = currentDate;
+        }
+
+        public ManageEvidenceNoteTransfer(Guid organisationId,
+            FilterViewModel filterViewModel, RecipientWasteStatusFilterViewModel recipientWasteStatusFilterViewModel,
+            SubmittedDatesFilterViewModel submittedDatesFilterViewModel, int complianceYear, DateTime currentDate)
+        {
+            Guard.ArgumentNotDefaultValue(() => organisationId, organisationId);
+            
+            OrganisationId = organisationId;
             FilterViewModel = filterViewModel;
             RecipientWasteStatusFilterViewModel = recipientWasteStatusFilterViewModel;
             SubmittedDatesFilterViewModel = submittedDatesFilterViewModel;
