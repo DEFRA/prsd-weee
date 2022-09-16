fd98e9ab80c1d633c610ea002d170be27b801cdb
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-16 08:57:20 +0100

    Fix some TransferEvidenceNotesViewModelMap tests after redesign

--- src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/TransferEvidenceNotesViewModelMap.cs
+++ src/EA.Weee.Web/Areas/Scheme/Mappings/ToViewModels/TransferEvidenceNotesViewModelMap.cs
@@ -20,10 +20,11 @@
         public TransferEvidenceNotesViewModel Map(TransferEvidenceNotesViewModelMapTransfer source)
         {
             Condition.Requires(source).IsNotNull();
-            
+            Condition.Requires(source.AvailableNotes).IsNotNull();
+
             var model = MapBaseProperties(source);
 
-            foreach (var evidenceNoteData in source.AvailableNotes.Results.OrderByDescending(e => e.Reference))
+            foreach (var evidenceNoteData in source.AvailableNotes.Results)
             {
                 model.EvidenceNotesDataListPaged.Add(Mapper.Map<EvidenceNoteRowViewModel>(evidenceNoteData));
             }
@@ -33,44 +34,8 @@
 
             //TEST FOR THIS
             model.PageNumber = source.PageNumber;
-            //if (source.SessionEvidenceNotesId != null)
-            //{
-            //    model.SelectedEvidenceNotePairs.Where(c => source.SessionEvidenceNotesId.Contains(c.Key)).ToList()
-            //               .ForEach(c => c.Value = true);
-
-            //    model.SelectedEvidenceNotePairs = ReorderAndGetSelectedEvidenceIds(model);
-            //}
-
-            //if (source.TransferEvidenceNoteData != null)
-            //{
-            //    model.SelectedEvidenceNotePairs = ReorderAndGetSelectedEvidenceIds(model);
-            //}
-
-            //if (source.ExcludeEvidenceNoteIds != null)
-            //{
-            //    model.SelectedEvidenceNotePairs.Where(c => source.ExcludeEvidenceNoteIds.Contains(c.Key)).ToList()
-            //        .ForEach(c => c.Value = false);
-            //}
 
             return model;
         }
-
-        //private List<GenericControlPair<Guid, bool>> ReorderAndGetSelectedEvidenceIds(TransferEvidenceNotesViewModel model)
-        //{
-        //    //var selectedNoteIds = model.SelectedEvidenceNotePairs
-        //    //     .Where(s => s.Value)
-        //    //     .Select(s1 => s1.Key).ToList();
-
-        //    // order list by the selected notes first (in desc ref order) and then the rest of the notes in ref desc
-        //    var formattedList = new List<ViewEvidenceNoteViewModel>();
-        //    formattedList.AddRange(model.EvidenceNotesDataList.Where(e => selectedNoteIds.Contains(e.Id)).OrderByDescending(e => e.Reference).ToList());
-        //    formattedList.AddRange(model.EvidenceNotesDataList.Where(e => !selectedNoteIds.Contains(e.Id)).OrderByDescending(e => e.Reference).ToList());
-        //    model.EvidenceNotesDataList = formattedList;
-
-        //    // reorder the selected evidence note ids to ensure they match the list of notes
-        //    //return model.EvidenceNotesDataList.Select(e =>
-        //    //    new GenericControlPair<Guid, bool>(e.Id, selectedNoteIds.Contains(e.Id))).ToList();
-        //    return model.EvidenceNotesDataList.ToList();
-        //}
     }
 }
