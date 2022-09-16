2d7d931dda9f73290b53e5f4e6043cb91970576c
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-16 10:37:36 +0100

    Unit tests for TransferDeselectEvidenceNoteModel

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Scheme/ViewModels/TransferDeselectEvidenceNoteModelTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Scheme.ViewModels
+{
+    using FluentAssertions;
+    using Web.Areas.Scheme.ViewModels;
+    using Xunit;
+
+    public class TransferDeselectEvidenceNoteModelTests
+    {
+        [Fact]
+        public void TransferDeselectEvidenceNoteModel_ShouldDeriveFromTransferSelectEvidenceNoteModelBase()
+        {
+            typeof(TransferDeselectEvidenceNoteModel).Should().BeDerivedFrom<TransferSelectEvidenceNoteModelBase>();
+        }
+    }
+}
