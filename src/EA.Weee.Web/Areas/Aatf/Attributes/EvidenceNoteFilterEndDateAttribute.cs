7c0ce6442a936764e1462ab993bcd88d11227909
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-22 14:15:01 +0100

    Refactor to allow the filter dates to have their own attribute

---
+++ src/EA.Weee.Web/Areas/Aatf/Attributes/EvidenceNoteFilterEndDateAttribute.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Weee.Web.Areas.Aatf.Attributes
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EvidenceNoteFilterEndDateAttribute : EvidenceDateValidationBase
+    {
+        public string CompareDatePropertyName { get; set; }
+
+        public EvidenceNoteFilterEndDateAttribute(string compareDatePropertyName)
+        {
+            CompareDatePropertyName = compareDatePropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                // let the required field validation deal with the entering of the date
+                return ValidationResult.Success;
+            }
+
+            var thisDate = ((DateTime)value).Date;
+            var otherDate = (DateTime?)validationContext.ObjectType.GetProperty(CompareDatePropertyName)?.GetValue(validationContext.ObjectInstance, null);
+
+            return ValidateEndDate(otherDate, thisDate);
+        }
+    }
+}
