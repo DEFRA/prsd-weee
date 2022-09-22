0ac4721548e0232fe469898fa510ceec600adce2
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-22 13:35:03 +0100

    Add ability to cache aatf data

--- src/EA.Weee.Web/Services/IHttpContextService.cs
+++ src/EA.Weee.Web/Services/IHttpContextService.cs
@@ -1,7 +1,11 @@
 ﻿namespace EA.Weee.Web.Services
 {
+    using System;
+
     public interface IHttpContextService
     {
         string GetAccessToken();
+
+        bool HasOrganisationClaim(Guid organisationId);
     }
 }
