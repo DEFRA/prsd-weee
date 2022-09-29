419019009eb543276ec5981bf2f04252ac014cc9
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-09-29 09:44:59 +0100

    commit

--- src/EA.Weee.Integration.Tests/Handlers/SetNoteStatusRequestHandlerIntegrationTests.cs
+++ src/EA.Weee.Integration.Tests/Handlers/SetNoteStatusRequestHandlerIntegrationTests.cs
@@ -68,8 +68,8 @@
                 note.ApprovedRecipientSchemeName.Should().Be(note.Recipient.Scheme.SchemeName);
                 note.ApprovedRecipientAddress.Should().Be($"{note.Recipient.Scheme.SchemeName}<br/>{note.Recipient.OrganisationName}<br/>{note.Recipient.BusinessAddress.Address1}<br/>{note.Recipient.BusinessAddress.Address2}<br/>{note.Recipient.BusinessAddress.TownOrCity}<br/>{note.Recipient.BusinessAddress.CountyOrRegion}<br/>{note.Recipient.BusinessAddress.Postcode}");
                 
-                note.ApprovedTransfererAddress.Should().BeNull();     //TODO: fix
-                note.ApprovedTransfererSchemeName.Should().BeNull();  //TODO: fix
+                note.ApprovedTransfererAddress.Should().BeNull();     //TODO: will be fixed soon
+                note.ApprovedTransfererSchemeName.Should().BeNull();  //TODO: will be fixed soon
             };
         }
 
