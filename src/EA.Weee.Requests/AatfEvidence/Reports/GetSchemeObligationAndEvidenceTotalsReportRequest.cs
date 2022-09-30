2a4db362e50094afe76d7d10cfe7c2ac18f50c27
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-09-30 12:36:15 +0100

    Add appropriate authority to GetSchemeObligationAndEvidenceTotals stored procedure

--- src/EA.Weee.Requests/AatfEvidence/Reports/GetSchemeObligationAndEvidenceTotalsReportRequest.cs
+++ src/EA.Weee.Requests/AatfEvidence/Reports/GetSchemeObligationAndEvidenceTotalsReportRequest.cs
@@ -8,11 +8,14 @@
     {
         public Guid? SchemeId { get; private set; }
 
+        public Guid? AppropriateAuthorityId { get; private set; }
+
         public int ComplianceYear { get; private set; }
 
-        public GetSchemeObligationAndEvidenceTotalsReportRequest(Guid? schemeId, int complianceYear) 
+        public GetSchemeObligationAndEvidenceTotalsReportRequest(Guid? schemeId, Guid? appropriateAuthorityId, int complianceYear) 
         {
             SchemeId = schemeId;
+            AppropriateAuthorityId = appropriateAuthorityId;
             ComplianceYear = complianceYear;
         }
     }
