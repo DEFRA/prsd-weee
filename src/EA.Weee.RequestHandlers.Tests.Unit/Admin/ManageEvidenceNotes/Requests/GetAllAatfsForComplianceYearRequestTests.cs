8b64ae57a3c9a2fb0c574092ea0b25017126bf5e
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-10-03 16:17:18 +0100

    unit tests for the new get all aatfs handler and request

---
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/ManageEvidenceNotes/Requests/GetAllAatfsForComplianceYearRequestTests.cs
@@ -0,0 +1,31 @@
+﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Admin.ManageEvidenceNotes.Requests
+{
+    using System;
+    using AutoFixture;
+    using EA.Weee.Requests.Admin;
+    using EA.Weee.Tests.Core;
+    using FluentAssertions;
+    using Xunit;
+
+    public class GetAllAatfsForComplianceYearRequestTests : SimpleUnitTestBase
+    {
+        [Fact]
+        public void GetAllAatfsForComplianceYearRequest_ShouldHaveSerializableAttribute()
+        {
+            typeof(GetAllAatfsForComplianceYearRequest).Should().BeDecoratedWith<SerializableAttribute>();
+        }
+
+        [Fact]
+        public void GetAllAatfsForComplianceYearRequest_GivenComplianceYear_PropertiesShouldBeSet()
+        {
+            //arrange
+            var complianceYear = TestFixture.Create<int>();
+
+            //act
+            var result = new GetAllAatfsForComplianceYearRequest(complianceYear);
+
+            //assert
+            result.ComplianceYear.Should().Be(complianceYear);
+        }
+    }
+}
