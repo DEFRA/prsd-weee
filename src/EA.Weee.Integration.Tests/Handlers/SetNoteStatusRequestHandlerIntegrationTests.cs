1a738f808453162fdabe52f857b47bfc4f692f3d
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-10-05 10:11:57 +0100

    Fix issues with approval of notes

--- src/EA.Weee.Integration.Tests/Handlers/SetNoteStatusRequestHandlerIntegrationTests.cs
+++ src/EA.Weee.Integration.Tests/Handlers/SetNoteStatusRequestHandlerIntegrationTests.cs
@@ -18,7 +18,7 @@
     public class SetNoteStatusRequestHandlerIntegrationTests : IntegrationTestBase
     {
         [Component]
-        public class WhenIUpdateANoteStatusToApproved : SetNoteStatusRequestHandlerIntegrationTestBase
+        public class WhenIUpdateAnEvidenceNoteStatusToApproved : SetNoteStatusRequestHandlerIntegrationTestBase
         {
             private readonly Establish context = () =>
             {
@@ -68,13 +68,74 @@
                 note.ApprovedRecipientSchemeName.Should().Be(note.Recipient.Scheme.SchemeName);
                 note.ApprovedRecipientAddress.Should().Be($"{note.Recipient.Scheme.SchemeName}<br/>{note.Recipient.OrganisationName}<br/>{note.Recipient.BusinessAddress.Address1}<br/>{note.Recipient.BusinessAddress.Address2}<br/>{note.Recipient.BusinessAddress.TownOrCity}<br/>{note.Recipient.BusinessAddress.CountyOrRegion}<br/>{note.Recipient.BusinessAddress.Postcode}");
                 
-                note.ApprovedTransfererAddress.Should().BeNull();     //TODO: will be fixed soon
-                note.ApprovedTransfererSchemeName.Should().BeNull();  //TODO: will be fixed soon
+                note.ApprovedTransfererAddress.Should().BeNull();    
+                note.ApprovedTransfererSchemeName.Should().BeNull();
             };
         }
 
         [Component]
-        public class WhenIUpdateANoteStatusToReturned : SetNoteStatusRequestHandlerIntegrationTestBase
+        public class WhenIUpdateATransferNoteStatusToApproved : SetNoteStatusRequestHandlerIntegrationTestBase
+        {
+            private readonly Establish context = () =>
+            {
+                LocalSetup();
+
+                organisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(organisation.Id).Create();
+
+                var recipientOrganisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(recipientOrganisation.Id).Create();
+                
+                var transfererOrganisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(transfererOrganisation.Id).Create();
+
+                OrganisationUserDbSetup.Init().WithUserIdAndOrganisationId(UserId, recipientOrganisation.Id).Create();
+
+                note = TransferEvidenceNoteDbSetup.Init()
+                    .WithRecipient(recipientOrganisation.Id)
+                    .WithOrganisation(transfererOrganisation.Id)
+                    .With(n =>
+                    {
+                        n.UpdateStatus(NoteStatus.Submitted, UserId.ToString(), DateTime.UtcNow);
+                    })
+                    .Create();
+
+                request = new SetNoteStatusRequest(note.Id, Core.AatfEvidence.NoteStatus.Approved);
+            };
+
+            private readonly Because of = () =>
+            {
+                result = Task.Run(async () => await handler.HandleAsync(request)).Result;
+
+                note = Query.GetTransferEvidenceNoteById(note.Id);
+            };
+
+            private readonly It shouldHaveUpdatedTheEvidenceNoteWithStatusApproved = () =>
+            {
+                note.Status.Should().Be(NoteStatus.Approved);
+            };
+
+            private readonly It shouldHaveCreatedANoteStatusHistory = () =>
+            {
+                var history = Query.GetLatestNoteStatusHistoryForNote(note.Id);
+                history.ChangedById.Should().Be(UserId.ToString());
+                history.ChangedDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+                history.FromStatus.Should().Be(NoteStatus.Submitted);
+                history.ToStatus.Should().Be(NoteStatus.Approved);
+            };
+
+            private readonly It shouldHaveSetApprovedDetails = () =>
+            {
+                note.ApprovedRecipientSchemeName.Should().Be(note.Recipient.Scheme.SchemeName);
+                note.ApprovedRecipientAddress.Should().Be($"{note.Recipient.Scheme.SchemeName}<br/>{note.Recipient.OrganisationName}<br/>{note.Recipient.BusinessAddress.Address1}<br/>{note.Recipient.BusinessAddress.Address2}<br/>{note.Recipient.BusinessAddress.TownOrCity}<br/>{note.Recipient.BusinessAddress.CountyOrRegion}<br/>{note.Recipient.BusinessAddress.Postcode}");
+
+                note.ApprovedTransfererSchemeName.Should().Be(note.Organisation.Scheme.SchemeName);
+                note.ApprovedTransfererAddress.Should().Be($"{note.Organisation.Scheme.SchemeName}<br/>{note.Organisation.OrganisationName}<br/>{note.Organisation.BusinessAddress.Address1}<br/>{note.Organisation.BusinessAddress.Address2}<br/>{note.Organisation.BusinessAddress.TownOrCity}<br/>{note.Organisation.BusinessAddress.CountyOrRegion}<br/>{note.Organisation.BusinessAddress.Postcode}");
+            };
+        }
+
+        [Component]
+        public class WhenIUpdateAnEvidenceNoteStatusToReturned : SetNoteStatusRequestHandlerIntegrationTestBase
         {
             private readonly Establish context = () =>
             {
@@ -130,7 +191,67 @@
         }
 
         [Component]
-        public class WhenIUpdateANoteStatusToRejected : SetNoteStatusRequestHandlerIntegrationTestBase
+        public class WhenIUpdateATransferNoteStatusToReturned : SetNoteStatusRequestHandlerIntegrationTestBase
+        {
+            private readonly Establish context = () =>
+            {
+                LocalSetup();
+
+                organisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(organisation.Id).Create();
+
+                var transfererOrganisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(transfererOrganisation.Id).Create();
+
+                var recipientOrganisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(recipientOrganisation.Id).Create();
+                OrganisationUserDbSetup.Init().WithUserIdAndOrganisationId(UserId, recipientOrganisation.Id).Create();
+
+                note = TransferEvidenceNoteDbSetup.Init()
+                    .WithRecipient(recipientOrganisation.Id)
+                    .WithOrganisation(transfererOrganisation.Id)
+                    .With(n =>
+                    {
+                        n.UpdateStatus(NoteStatus.Submitted, UserId.ToString(), DateTime.UtcNow);
+                    })
+                    .Create();
+
+                request = new SetNoteStatusRequest(note.Id, Core.AatfEvidence.NoteStatus.Returned, "reason returned");
+            };
+
+            private readonly Because of = () =>
+            {
+                result = Task.Run(async () => await handler.HandleAsync(request)).Result;
+
+                note = Query.GetTransferEvidenceNoteById(note.Id);
+            };
+
+            private readonly It shouldHaveUpdatedTheEvidenceNoteWithStatusReturned = () =>
+            {
+                note.Status.Should().Be(NoteStatus.Returned);
+            };
+
+            private readonly It shouldHaveCreatedANoteStatusHistory = () =>
+            {
+                var history = Query.GetLatestNoteStatusHistoryForNote(note.Id);
+                history.ChangedById.Should().Be(UserId.ToString());
+                history.ChangedDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+                history.FromStatus.Should().Be(NoteStatus.Submitted);
+                history.ToStatus.Should().Be(NoteStatus.Returned);
+                history.Reason.Should().Be("reason returned");
+            };
+
+            private readonly It shouldHaveNotHaveSetApprovedDetails = () =>
+            {
+                note.ApprovedRecipientSchemeName.Should().BeNull();
+                note.ApprovedRecipientAddress.Should().BeNull();
+                note.ApprovedTransfererAddress.Should().BeNull();
+                note.ApprovedTransfererSchemeName.Should().BeNull();
+            };
+        }
+
+        [Component]
+        public class WhenIUpdateAnEvidenceNoteStatusToRejected : SetNoteStatusRequestHandlerIntegrationTestBase
         {
             private readonly Establish context = () =>
             {
@@ -186,7 +307,67 @@
         }
 
         [Component]
-        public class WhenIUpdateANoteStatusToSubmitted : SetNoteStatusRequestHandlerIntegrationTestBase
+        public class WhenIUpdateATransferNoteStatusToRejected : SetNoteStatusRequestHandlerIntegrationTestBase
+        {
+            private readonly Establish context = () =>
+            {
+                LocalSetup();
+
+                organisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(organisation.Id).Create();
+
+                var transfererOrganisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(transfererOrganisation.Id).Create();
+
+                var recipientOrganisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(recipientOrganisation.Id).Create();
+                OrganisationUserDbSetup.Init().WithUserIdAndOrganisationId(UserId, recipientOrganisation.Id).Create();
+
+                note = TransferEvidenceNoteDbSetup.Init()
+                    .WithRecipient(recipientOrganisation.Id)
+                    .WithOrganisation(transfererOrganisation.Id)
+                    .With(n =>
+                    {
+                        n.UpdateStatus(NoteStatus.Submitted, UserId.ToString(), DateTime.UtcNow);
+                    })
+                    .Create();
+
+                request = new SetNoteStatusRequest(note.Id, Core.AatfEvidence.NoteStatus.Rejected, "reason returned");
+            };
+
+            private readonly Because of = () =>
+            {
+                result = Task.Run(async () => await handler.HandleAsync(request)).Result;
+
+                note = Query.GetTransferEvidenceNoteById(note.Id);
+            };
+
+            private readonly It shouldHaveUpdatedTheEvidenceNoteWithStatusReturned = () =>
+            {
+                note.Status.Should().Be(NoteStatus.Rejected);
+            };
+
+            private readonly It shouldHaveCreatedANoteStatusHistory = () =>
+            {
+                var history = Query.GetLatestNoteStatusHistoryForNote(note.Id);
+                history.ChangedById.Should().Be(UserId.ToString());
+                history.ChangedDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+                history.FromStatus.Should().Be(NoteStatus.Submitted);
+                history.ToStatus.Should().Be(NoteStatus.Rejected);
+                history.Reason.Should().Be("reason returned");
+            };
+
+            private readonly It shouldHaveNotHaveSetApprovedDetails = () =>
+            {
+                note.ApprovedRecipientSchemeName.Should().BeNull();
+                note.ApprovedRecipientAddress.Should().BeNull();
+                note.ApprovedTransfererAddress.Should().BeNull();
+                note.ApprovedTransfererSchemeName.Should().BeNull();
+            };
+        }
+
+        [Component]
+        public class WhenIUpdateAnEvidenceNoteStatusToSubmitted : SetNoteStatusRequestHandlerIntegrationTestBase
         {
             private readonly Establish context = () =>
             {
@@ -212,6 +393,62 @@
                 result = Task.Run(async () => await handler.HandleAsync(request)).Result;
 
                 note = Query.GetEvidenceNoteById(note.Id);
+            };
+
+            private readonly It shouldHaveUpdatedTheEvidenceNoteWithStatusReturned = () =>
+            {
+                note.Status.Should().Be(NoteStatus.Submitted);
+            };
+
+            private readonly It shouldHaveCreatedANoteStatusHistory = () =>
+            {
+                var history = Query.GetLatestNoteStatusHistoryForNote(note.Id);
+                history.ChangedById.Should().Be(UserId.ToString());
+                history.ChangedDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+                history.FromStatus.Should().Be(NoteStatus.Draft);
+                history.ToStatus.Should().Be(NoteStatus.Submitted);
+                history.Reason.Should().BeNull();
+            };
+
+            private readonly It shouldHaveNotHaveSetApprovedDetails = () =>
+            {
+                note.ApprovedRecipientSchemeName.Should().BeNull();
+                note.ApprovedRecipientAddress.Should().BeNull();
+                note.ApprovedTransfererAddress.Should().BeNull();
+                note.ApprovedTransfererSchemeName.Should().BeNull();
+            };
+        }
+
+        [Component]
+        public class WhenIUpdateATransferNoteStatusToSubmitted : SetNoteStatusRequestHandlerIntegrationTestBase
+        {
+            private readonly Establish context = () =>
+            {
+                LocalSetup();
+
+                organisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(organisation.Id).Create();
+                OrganisationUserDbSetup.Init().WithUserIdAndOrganisationId(UserId, organisation.Id).Create();
+
+                var transfererOrganisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(transfererOrganisation.Id).Create();
+
+                var recipientOrganisation = OrganisationDbSetup.Init().Create();
+                SchemeDbSetup.Init().WithOrganisation(recipientOrganisation.Id).Create();
+
+                note = TransferEvidenceNoteDbSetup.Init()
+                    .WithRecipient(recipientOrganisation.Id)
+                    .WithOrganisation(transfererOrganisation.Id)
+                    .Create();
+
+                request = new SetNoteStatusRequest(note.Id, Core.AatfEvidence.NoteStatus.Submitted, null);
+            };
+
+            private readonly Because of = () =>
+            {
+                result = Task.Run(async () => await handler.HandleAsync(request)).Result;
+
+                note = Query.GetTransferEvidenceNoteById(note.Id);
             };
 
             private readonly It shouldHaveUpdatedTheEvidenceNoteWithStatusReturned = () =>
