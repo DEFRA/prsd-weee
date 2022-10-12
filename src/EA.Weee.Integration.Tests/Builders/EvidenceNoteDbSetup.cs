f4f3389a56bd46de64db7f63791c41a4b370956e
Meglena Novoselska <Meglena.Novoselska@civica.co.uk>
2022-10-12 14:50:11 +0100

    Fixed the integration test

--- src/EA.Weee.Integration.Tests/Builders/EvidenceNoteDbSetup.cs
+++ src/EA.Weee.Integration.Tests/Builders/EvidenceNoteDbSetup.cs
@@ -60,12 +60,6 @@
             return this;
         }
 
-        public EvidenceNoteDbSetup WithReference(int reference)
-        {
-            ObjectInstantiator<Note>.SetProperty(o => o.Reference, reference, instance);
-            return this;
-        }
-
         public EvidenceNoteDbSetup WithStatus(NoteStatus statusToUpdate, string user)
         {
             var date = SystemTime.UtcNow;
