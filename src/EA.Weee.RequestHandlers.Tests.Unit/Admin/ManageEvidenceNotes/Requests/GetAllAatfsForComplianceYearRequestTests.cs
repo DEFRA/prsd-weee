6b9d52d2b8e7bf59980eacf9ff567d14a8d2fa03
JohnPadgettCivica <john.padgett@civica.co.uk>
2022-11-17 10:18:45 +0000

    Update to add note type list to the lookup requests

--- src/EA.Weee.RequestHandlers.Tests.Unit/Admin/ManageEvidenceNotes/Requests/GetAllAatfsForComplianceYearRequestTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Admin/ManageEvidenceNotes/Requests/GetAllAatfsForComplianceYearRequestTests.cs
@@ -1,10 +1,13 @@
 ﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Admin.ManageEvidenceNotes.Requests
 {
     using System;
+    using System.Collections.Generic;
     using AutoFixture;
+    using Core.AatfEvidence;
     using EA.Weee.Requests.Admin;
     using EA.Weee.Tests.Core;
     using FluentAssertions;
+    using Weee.Requests.AatfEvidence;
     using Xunit;
 
     public class GetAllAatfsForComplianceYearRequestTests : SimpleUnitTestBase
@@ -16,7 +19,13 @@
         }
 
         [Fact]
-        public void GetAllAatfsForComplianceYearRequest_GivenComplianceYear_PropertiesShouldBeSet()
+        public void GetAllAatfsForComplianceYearRequest_ShouldDeriveFrom_EvidenceEntityIdDisplayNameDataBase()
+        {
+            typeof(GetAllAatfsForComplianceYearRequest).Should().BeDerivedFrom<EvidenceEntityIdDisplayNameDataBase>();
+        }
+
+        [Fact]
+        public void GetAllAatfsForComplianceYearRequest_GivenValues_PropertiesShouldBeSet()
         {
             //arrange
             var complianceYear = TestFixture.Create<int>();
@@ -26,6 +35,10 @@
 
             //assert
             result.ComplianceYear.Should().Be(complianceYear);
+            result.AllowedStatuses.Should().BeEquivalentTo(new List<NoteStatus>()
+            {
+                NoteStatus.Approved, NoteStatus.Rejected, NoteStatus.Submitted, NoteStatus.Returned, NoteStatus.Void
+            });
         }
     }
 }
