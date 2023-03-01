d3039ad85221d801c2d1f5ce80e97826f5b8977b
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2023-03-01 11:49:27 +0000

    CCN052 Bug fix changes as per client email

---
+++ src/EA.Weee.RequestHandlers/Aatf/GetAatfByIdHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace EA.Weee.RequestHandlers.Aatf
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.Core.AatfReturn;
+    using EA.Weee.Domain.AatfReturn;
+    using EA.Weee.RequestHandlers.AatfReturn.AatfTaskList;
+    using EA.Weee.RequestHandlers.Security;
+    using EA.Weee.Requests.Aatf;
+    using System.Threading.Tasks;
+
+    public class GetAatfByIdHandler : IRequestHandler<GetAatfById, AatfData>
+    {
+        private readonly IWeeeAuthorization authorization;
+        private readonly IFetchAatfDataAccess fetchDataAccess;
+        private readonly IMap<Aatf, AatfData> mapper;
+
+        public GetAatfByIdHandler(IWeeeAuthorization authorization, IMap<Aatf, AatfData> mapper, IFetchAatfDataAccess fetchDataAccess)
+        {
+            this.authorization = authorization;
+            this.mapper = mapper;
+            this.fetchDataAccess = fetchDataAccess;
+        }
+
+        public async Task<AatfData> HandleAsync(GetAatfById message)
+        {
+            authorization.EnsureCanAccessExternalArea();
+
+            var aatf = await fetchDataAccess.FetchById(message.AatfId);
+
+            return mapper.Map(aatf);
+        }
+    }
+}
