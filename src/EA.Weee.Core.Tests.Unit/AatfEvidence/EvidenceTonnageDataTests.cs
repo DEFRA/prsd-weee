96d908f172862dab7204c3123597d85e69a10b86
JohnPadgettCivica <john.padgett@civica.co.uk>
2023-03-02 15:51:22 +0000

    Fix test warning

--- src/EA.Weee.Core.Tests.Unit/AatfEvidence/EvidenceTonnageDataTests.cs
+++ src/EA.Weee.Core.Tests.Unit/AatfEvidence/EvidenceTonnageDataTests.cs
@@ -18,21 +18,6 @@
             fixture = new Fixture();
         }
 
-        [Theory]
-        [InlineData(5, null, 5)]
-        [InlineData(5, 0, 5)]
-        [InlineData(5, 5, 0)]
-        [InlineData(5, 4, 1)]
-        [InlineData(5, 6, 0)]
-        public void EvidenceNoteData_EvidenceTonnageDataAvailableReceivedGet_ShouldCalculate_CorrectValues(decimal total, decimal transferred, decimal expectedAvailable)
-        {
-            //Arrange
-            var noteData = new EvidenceTonnageData(fixture.Create<Guid>(), fixture.Create<WeeeCategory>(), total, null, transferred, null);
-
-            //Assert
-            noteData.AvailableReceived.Should().Be(expectedAvailable);
-        }
-
         public static IEnumerable<object[]> Values()
         {
             yield return new object[] { 5m, null, 5m };
@@ -40,6 +25,17 @@
             yield return new object[] { 5m, 5m, 0m };
             yield return new object[] { 5m, 4m, 1m };
             yield return new object[] { 5m, 6m, 0m };
+        }
+
+        [Theory]
+        [MemberData(nameof(Values))]
+        public void EvidenceNoteData_EvidenceTonnageDataAvailableReceivedGet_ShouldCalculate_CorrectValues(decimal total, decimal transferred, decimal expectedAvailable)
+        {
+            //Arrange
+            var noteData = new EvidenceTonnageData(fixture.Create<Guid>(), fixture.Create<WeeeCategory>(), total, null, transferred, null);
+
+            //Assert
+            noteData.AvailableReceived.Should().Be(expectedAvailable);
         }
 
         [Theory]
