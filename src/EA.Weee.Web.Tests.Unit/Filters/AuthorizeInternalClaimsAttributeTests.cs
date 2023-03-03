10875de2f4ef7afe3cf290add606bd9229c68ea4
JohnPadgettCivica <john.padgett@civica.co.uk>
2023-03-03 14:13:38 +0000

    Upgrade of packages in an attempt to resolve reference conflicts

--- src/EA.Weee.Web.Tests.Unit/Filters/AuthorizeInternalClaimsAttributeTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Filters/AuthorizeInternalClaimsAttributeTests.cs
@@ -9,6 +9,7 @@
     using System.Web;
     using System.Web.Mvc;
     using Xunit;
+    using AuthorizationContext = System.Web.Mvc.AuthorizationContext;
 
     public class AuthorizeInternalClaimsAttributeTests
     {
