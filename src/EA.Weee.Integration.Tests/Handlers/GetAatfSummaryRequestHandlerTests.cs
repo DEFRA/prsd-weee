b982ff5f2cbe422c5bd9a9793e7b50e0782b00f4
JohnPadgettCivica <john.padgett@civica.co.uk>
2023-03-06 14:30:53 +0000

    Resolve issues with Nunit packages

--- src/EA.Weee.Integration.Tests/Handlers/GetAatfSummaryRequestHandlerTests.cs
+++ src/EA.Weee.Integration.Tests/Handlers/GetAatfSummaryRequestHandlerTests.cs
@@ -14,6 +14,7 @@
     using EA.Prsd.Core;
     using FluentAssertions;
     using NUnit.Specifications;
+    using NUnit.Specifications.Categories;
     using Prsd.Core.Autofac;
     using Prsd.Core.Mediator;
     using Requests.AatfEvidence;
