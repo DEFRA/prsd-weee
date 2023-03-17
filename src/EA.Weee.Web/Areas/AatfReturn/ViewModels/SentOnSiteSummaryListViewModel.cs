2d58d4db68f89f94fef4606d4f7d68c631b9b8e6
JohnPadgettCivica <john.padgett@civica.co.uk>
2023-03-17 15:49:36 +0000

    Tidy up the AATF return address formatting code and make the addresses better for screen readers

--- src/EA.Weee.Web/Areas/AatfReturn/ViewModels/SentOnSiteSummaryListViewModel.cs
+++ src/EA.Weee.Web/Areas/AatfReturn/ViewModels/SentOnSiteSummaryListViewModel.cs
@@ -25,58 +25,6 @@
 
         [Required]
         [Range(typeof(bool), "true", "true", ErrorMessage = "Please select copy selection from previous quarter")]
-        public bool IsCopyChecked { get; set; }        
-
-        public string CreateLongAddress(AatfAddressData address)
-        {
-            if (address != null)
-            {
-                var siteAddressStringBuilder = new StringBuilder();
-
-                const string spanTagName = "span";
-                var addressNameSpan = new TagBuilder(spanTagName);
-                var address1Span = new TagBuilder(spanTagName);
-
-                addressNameSpan.SetInnerText($"{address.Name},");
-                address1Span.SetInnerText($"{address.Address1},");
-
-                siteAddressStringBuilder.Append(addressNameSpan);
-                siteAddressStringBuilder.Append(address1Span);
-
-                if (address.Address2 != null)
-                {
-                    var address2Span = new TagBuilder(spanTagName);
-                    address2Span.SetInnerText($"{address.Address2},");
-
-                    siteAddressStringBuilder.Append(address2Span);
-                }
-
-                var townOrCitySpan = new TagBuilder(spanTagName);
-                townOrCitySpan.SetInnerText($"{address.TownOrCity},");
-                siteAddressStringBuilder.Append(townOrCitySpan);
-
-                if (address.CountyOrRegion != null)
-                {
-                    var countySpan = new TagBuilder(spanTagName);
-                    countySpan.SetInnerText($"{address.CountyOrRegion},");
-                    siteAddressStringBuilder.Append(countySpan);
-                }
-
-                if (address.Postcode != null)
-                {
-                    var postCodeSpan = new TagBuilder(spanTagName);
-                    postCodeSpan.SetInnerText($"{address.Postcode},");
-                    siteAddressStringBuilder.Append(postCodeSpan);
-                }
-
-                var countrySpan = new TagBuilder(spanTagName);
-                countrySpan.SetInnerText(address.CountryName);
-                siteAddressStringBuilder.Append(countrySpan);
-
-                return siteAddressStringBuilder.ToString();
-            }
-
-            return "&nbsp";
-        }
+        public bool IsCopyChecked { get; set; }
     }
 }
