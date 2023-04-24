a57120318979b92be35d6c05a51d81be95dc1d3d
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2023-04-24 17:04:52 +0100

    Removed not required unit tests.

--- src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/ObligationsBaseControllerTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/ObligationsBaseControllerTests.cs
@@ -1,9 +1,7 @@
 ﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.Controllers
 {
-    using EA.Weee.Security;
     using EA.Weee.Web.Areas.Admin.Controllers.Base;
     using EA.Weee.Web.Areas.Scheme.Attributes;
-    using EA.Weee.Web.Filters;
     using FluentAssertions;
     using Xunit;
 
@@ -13,13 +11,6 @@
         public void Controller_ShouldInheritFromAdminBaseController()
         {
             typeof(ObligationsBaseController).Should().BeDerivedFrom<AdminController>();
-        }
-
-        [Fact]
-        public void Controller_IsDecoratedWith_AuthorizeInternalClaimsAttribute()
-        {
-            typeof(ObligationsBaseController).Should()
-                .BeDecoratedWith<AuthorizeInternalClaimsAttribute>(a => a.Match(new AuthorizeInternalClaimsAttribute(Claims.CanAccessInternalArea)));
         }
 
         [Fact]
