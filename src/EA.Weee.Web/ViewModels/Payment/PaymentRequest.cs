1c17ef6e688cb8a948959c5fc17f700c26d3f20a
cklee <chi.kuenLee@civica.co.uk>
2024-07-23 10:33:41 +0100

    gov uk pay prototype

---
+++ src/EA.Weee.Web/ViewModels/Payment/PaymentRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Weee.Web.ViewModels.Payment
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.Json.Serialization;
+
+    public class PaymentRequest
+    {
+        [Required] 
+        public decimal Amount { get; set; }
+        
+        public string Description { get; set; }
+        public string Reference { get; set; }
+
+        [JsonPropertyName("return_url")] 
+        public string ReturnUrl { get; set; }
+    }
+}
