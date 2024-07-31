93ad8f7b2836da45cc861288b875b5057899c003
John Padgett <John.Padgett@civica.co.uk>
2024-07-31 09:45:52 +0100

    temp

--- src/EA.Weee.Web/Controllers/Country.cs
+++ src/EA.Weee.Web/Controllers/Country.cs
@@ -1,10 +1,13 @@
 ﻿namespace EA.Weee.Web.Controllers
 {
+    using System.ComponentModel.DataAnnotations;
     using System.Text.Json.Serialization;
 
     public class Country
     {
         public string ISO { get; set; }
+
+        [Display(Name = "Country")]
         public string Name { get; set; }
     }
 }
