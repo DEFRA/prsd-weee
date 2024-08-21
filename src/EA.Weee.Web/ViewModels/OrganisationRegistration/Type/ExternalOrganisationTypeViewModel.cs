8676419d3fb2a3e44a28bba57017381bdc0516d2
Sean Mccallum <Sean.Mccallum@civica.co.uk>
2024-08-21 16:16:16 +0100

    70863 - TO1.0 - "Organisation Type" Screen

---
+++ src/EA.Weee.Web/ViewModels/OrganisationRegistration/Type/ExternalOrganisationTypeViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Weee.Web.ViewModels.OrganisationRegistration.Type
+{
+    using Core.Organisations;
+    using EA.Weee.Core.Shared;
+    using Shared;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ExternalOrganisationTypeViewModel : RadioButtonStringCollectionViewModel
+    {
+        public string SearchedText { get; set; }
+
+        [Required(ErrorMessage = "Select organisation type")]
+        public override string SelectedValue { get; set; }
+
+        public ExternalOrganisationTypeViewModel()
+            : base(CreateFromEnum<ExternalOrganisationType>().PossibleValues)
+        {
+        }
+
+        public ExternalOrganisationTypeViewModel(string searchText) : this()
+        {
+            SearchedText = searchText;
+        }
+    }
+}
