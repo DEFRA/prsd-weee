b9fab99a6549730163a7bfc9f46d99eb861bab9c
will-hope98 <william.hope@civica.co.uk>
2024-08-21 10:25:19 +0100

    70855- Tonnage screen & 71101 - Find your organisation  - New link / text (#1405)
    * Added link on no results found.
     And new tonnes screen
    
    * update test to trigger Post Action
    
    * Tweak to the display
    
    * Add govuk-link style
    
    ---------
    
    Co-authored-by: John Padgett <John.Padgett@civica.co.uk>

---
+++ src/EA.Weee.Web/ViewModels/OrganisationRegistration/Type/TonnageTypeViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace EA.Weee.Web.ViewModels.OrganisationRegistration.Type
+{
+    using Core.Organisations;
+    using Shared;
+    using System.ComponentModel.DataAnnotations;
+
+    public class TonnageTypeViewModel : RadioButtonStringCollectionViewModel
+    {
+        public string SearchedText { get; set; }
+
+        [Required(ErrorMessage = "Please indicate if you are a producer of Electrical or Electronic Equipment")]
+        public override string SelectedValue { get; set; }
+
+        public TonnageTypeViewModel()
+            : base(CreateFromEnum<TonnageType>().PossibleValues)
+        {
+        }
+
+        public TonnageTypeViewModel(string searchText) : this()
+        {
+            SearchedText = searchText;
+        }
+
+        public TonnageTypeViewModel(TonnageType tonnageType)
+        {
+            SelectedValue = CreateFromEnum(tonnageType).SelectedValue;
+        }
+    }
+}
