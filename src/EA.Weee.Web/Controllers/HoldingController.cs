37b7181c9c9dfdc26c46d484af90c37b1f06dccc
will-hope98 <william.hope@civica.co.uk>
2024-08-21 10:25:19 +0100

    70855- Tonnage screen & 71101 - Find your organisation  - New link / text (#1405)
    * Added link on no results found.
     And new tonnes screen
    
    * update test to trigger Post Action
    
    * Tweak to the display
    
    * Add govuk-link style
    
    ---------
    
    Co-authored-by: John Padgett <John.Padgett@civica.co.uk>

---
+++ src/EA.Weee.Web/Controllers/HoldingController.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Web.Controllers
+{
+    using System.Web.Mvc;
+    using Filters;
+    using Security;
+
+    [AuthorizeClaims(Claims.CanAccessExternalArea)]
+    public class HoldingController : Controller
+    {
+        // GET: AatfReturn/Holding
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
