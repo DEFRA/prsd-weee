d1c90d9bf8ea75481ec54beb48ed12b049c9a388
cklee <chi.kuenLee@civica.co.uk>
2024-08-23 14:07:45 +0100

    added organisation-registration/RepresentingCompanyDetails mvc

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/AddOrganisation/Details/RepresentingCompanyDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.AddOrganisation.Details
+{
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using AddAatf.Details;
+    using Core.DataStandards;
+
+    public class RepresentingCompanyDetailsViewModel : OrganisationViewModel
+    {
+        [Required]
+        [DisplayName("Company name")]
+        [StringLength(CommonMaxFieldLengths.DefaultString)]
+        public string CompanyName { get; set; }
+
+        [DisplayName("Business trading name, if different to company name")]
+        public override string BusinessTradingName { get; set; }
+
+        [Required(ErrorMessage = "Enter company registration number (CRN)")]
+        [StringLength(maximumLength: EnvironmentAgencyMaxFieldLengths.CompanyRegistrationNumber, MinimumLength = 7, ErrorMessage = "The Company registration number should be 7 to 15 characters long")]
+        [Display(Name = "Company registration number (CRN)")]
+        public string CompaniesRegistrationNumber { get; set; }
+    }
+}
