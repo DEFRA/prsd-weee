e462695a5b4e184bb653c99da4668ff9340590d8
John Padgett <John.Padgett@civica.co.uk>
2024-08-27 15:45:20 +0100

    Updates for persistance

--- src/EA.Weee.Core/Organisations/RegisteredCompanyDetailsViewModel.cs
+++ src/EA.Weee.Core/Organisations/RegisteredCompanyDetailsViewModel.cs
@@ -14,10 +14,5 @@
 
         [DisplayName("Trading name")]
         public override string BusinessTradingName { get; set; }
-
-        [Required(ErrorMessage = "Enter company registration number (CRN)")]
-        [StringLength(maximumLength: EnvironmentAgencyMaxFieldLengths.CompanyRegistrationNumber, MinimumLength = 7, ErrorMessage = "The Company registration number should be 7 to 15 characters long")]
-        [Display(Name = "Company registration number (CRN)")]
-        public string CompaniesRegistrationNumber { get; set; }
     }
 }
