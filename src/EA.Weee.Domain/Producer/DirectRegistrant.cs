b53514dcadc14493c547000369a4e9b395d03220
JohnPadgettCivica <43237115+JohnPadgettCivica@users.noreply.github.com>
2024-08-27 15:44:07 +0100

    Schema updates use notification address (#1414)
    Schema updates

---
+++ src/EA.Weee.Domain/Producer/DirectRegistrant.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Weee.Domain.Producer
+{
+    using EA.Prsd.Core.Domain;
+    using EA.Weee.Domain.Organisation;
+    using System;
+
+    public class DirectRegistrant : Entity
+    {
+        public virtual Guid OrganisationId { get; set; }
+        public virtual Guid? SICCodeId { get; set; }
+        public virtual Guid? BrandNameId { get; set; }
+        public virtual Guid? RepresentingCompanyId { get; set; }
+
+        public virtual Organisation Organisation { get; set; }
+        public virtual SICCode SICCode { get; set; }
+        public virtual BrandName BrandName { get; set; }
+        public virtual ProducerBusiness RepresentingCompany { get; set; }
+    }
+}
