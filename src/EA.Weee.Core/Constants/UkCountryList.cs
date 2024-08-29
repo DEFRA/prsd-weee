ae89ee21fcb5cf14a0dfcf92a2acb46b252abd33
John Padgett <John.Padgett@civica.co.uk>
2024-08-29 13:41:55 +0100

    Remove invalid country list

--- src/EA.Weee.Core/Constants/UkCountryList.cs
+++ src/EA.Weee.Core/Constants/UkCountryList.cs
@@ -9,7 +9,6 @@
         {
             Guid.Parse("DB83F5AB-E745-49CF-B2CA-23FE391B67A8"),
             Guid.Parse("4209EE95-0882-42F2-9A5D-355B4D89EF30"),
-            Guid.Parse("B2BA3EF1-DCCF-4871-B0F5-A83335ACBEE5"),
             Guid.Parse("184E1785-26B4-4AE4-80D3-AE319B103ACB"),
             Guid.Parse("7BFB8717-4226-40F3-BC51-B16FDF42550C")
         };
