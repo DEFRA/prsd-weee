dff8003c5460b68e45840ad00e90495ffc6c317d
John Padgett <John.Padgett@civica.co.uk>
2024-08-30 14:02:13 +0100

    Some fixes for redirect and model validation

--- src/EA.Weee.Core/Organisations/RepresentingCompanyDetailsViewModel.cs
+++ src/EA.Weee.Core/Organisations/RepresentingCompanyDetailsViewModel.cs
@@ -1,15 +1,12 @@
 namespace EA.Weee.Core.Organisations
 {
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
     using EA.Weee.Core.DataStandards;
     using EA.Weee.Core.Shared;
-    using EA.Weee.Core.Validation;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
 
     public class RepresentingCompanyDetailsViewModel
     {
-        public string OrganisationType { get; set; }
-
         [Required]
         [DisplayName("Producer name")]
         [StringLength(CommonMaxFieldLengths.DefaultString)]
@@ -19,15 +16,5 @@ namespace EA.Weee.Core.Organisations
         public string BusinessTradingName { get; set; }
 
         public AddressData Address { get; set; } = new AddressData();
-
-        [StringLength(CommonMaxFieldLengths.Telephone)]
-        [Display(Name = "Telephone number")]
-        [GenericPhoneNumber(ErrorMessage = "The telephone number can use numbers, spaces and some special characters (-+). It must be no longer than 20 characters.")]
-        public string Telephone { get; set; }
-
-        [Required]
-        [Display(Name = "Email address")]
-        [EmailAddress]
-        public string Email { get; set; }
     }
 }
