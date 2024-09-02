19aa5f76e4696bd68918b8bf345cea59ad161c15
John Padgett <John.Padgett@civica.co.uk>
2024-09-02 14:18:17 +0100

    Updates for representing company details

---
+++ src/EA.Weee.Database/scripts/Update/Release 7/Sprint1/20240932-1131-update-nullable-fields.sql
@@ -0,0 +1,4 @@
+
+ALTER TABLE [Organisation].[RepresentingCompany] ALTER COLUMN [Email] [nvarchar](256) NOT NULL;
+ALTER TABLE [Organisation].[RepresentingCompany] ALTER COLUMN [Telephone] [nvarchar](20) NOT NULL;
+ALTER TABLE [Organisation].[RepresentingCompany] ALTER COLUMN [Postcode] [nvarchar](10) NOT NULL;
