86fd682563d19da425be2ae178a8e010c42605c2
John Padgett <John.Padgett@civica.co.uk>
2024-09-02 14:28:40 +0100

    Fix strange merge issue

--- src/EA.Weee.Web/Controllers/OrganisationRegistrationController.cs
+++ src/EA.Weee.Web/Controllers/OrganisationRegistrationController.cs
@@ -303,6 +303,8 @@
             }
 
             await transactionService.CaptureData(User.GetAccessToken(), model);
+            await transactionService.CompleteTransaction(User.GetAccessToken());
+            await cache.InvalidateOrganisationSearch();
 
             return RedirectToAction("Index", typeof(HoldingController).GetControllerName());
         }
