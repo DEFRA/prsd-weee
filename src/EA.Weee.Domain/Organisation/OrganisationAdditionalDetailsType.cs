f2485e07027ffad957dbc5fe1a3bd1cecfec6e9f
John Padgett <John.Padgett@civica.co.uk>
2024-09-03 18:30:39 +0100

    Add additional organisation details enum type

---
+++ src/EA.Weee.Domain/Organisation/OrganisationAdditionalDetailsType.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Weee.Domain.Organisation
+{
+    using EA.Prsd.Core.Domain;
+
+    public class OrganisationAdditionalDetailsType : Enumeration
+    {
+        public static readonly OrganisationAdditionalDetailsType RegisteredCompany = new OrganisationAdditionalDetailsType(1, "Partner");
+        public static readonly OrganisationAdditionalDetailsType Partnership = new OrganisationAdditionalDetailsType(2, "Sole Trader");
+
+        protected OrganisationAdditionalDetailsType()
+        {
+        }
+
+        private OrganisationAdditionalDetailsType(int value, string displayName) : base(value, displayName)
+        {
+        }
+    }
+}
