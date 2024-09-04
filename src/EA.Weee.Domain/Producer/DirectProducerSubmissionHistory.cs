6096386c9b77d74478f0bc9ef83d02c0b3622930
John Padgett <John.Padgett@civica.co.uk>
2024-09-04 14:48:55 +0100

    Producer submission schema and EF updates

---
+++ src/EA.Weee.Domain/Producer/DirectProducerSubmissionHistory.cs
@@ -0,0 +1,34 @@
+﻿namespace EA.Weee.Domain.Producer
+{
+    using EA.Weee.Domain.Audit;
+    using EA.Weee.Domain.Organisation;
+    using System;
+    using EA.Weee.Domain.DataReturns;
+
+    public class DirectProducerSubmissionHistory : AuditableEntity
+    {
+        public virtual Guid DirectProducerSubmissionId { get; set; }
+
+        public virtual Guid? ServiceOfNoticeAddressId { get; set; }
+
+        public virtual Guid? AppropriateSignatoryId { get; set; }
+
+        public virtual DateTime? SubmittedDate { get; set; }
+
+        public virtual string CompanyName { get; set; }
+
+        public virtual string TradingName { get; set; }
+
+        public virtual string CompanyRegistrationNumber { get; set; }
+
+        public virtual DirectProducerSubmissionStatus DirectProducerSubmissionStatus { get; set; }
+
+        public virtual DirectProducerSubmission DirectProducerSubmission { get; set; }
+
+        public virtual Address ServiceOfNoticeAddress { get; set; }
+
+        public virtual Contact AppropriateSignatory { get; set; }
+
+        public virtual EeeOutputReturnVersion EeeOutputReturnVersion { get; set; }
+    }
+}
---
+++ src/EA.Weee.Domain/Producer/DirectProducerSubmissionStatus.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Weee.Domain.Producer
+{
+    using EA.Prsd.Core.Domain;
+
+    public class DirectProducerSubmissionStatus : Enumeration
+    {
+        public static readonly DirectProducerSubmissionStatus Incomplete = new DirectProducerSubmissionStatus(1, "Incomplete");
+        public static readonly DirectProducerSubmissionStatus Complete = new DirectProducerSubmissionStatus(2, "Submitted");
+
+        protected DirectProducerSubmissionStatus()
+        {
+        }
+
+        private DirectProducerSubmissionStatus(int value, string displayName)
+            : base(value, displayName)
+        {
+        }
+    }
+}
