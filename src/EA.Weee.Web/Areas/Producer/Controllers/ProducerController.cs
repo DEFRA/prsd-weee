18c78b6dc495e4c34e4dcdf5b37a0c486a1917ea
Sean Mccallum <Sean.Mccallum@civica.co.uk>
2024-09-06 17:28:21 +0100

    70929 - TO3.0 - Application Task List

--- src/EA.Weee.Web/Areas/Producer/Controllers/ProducerController.cs
+++ src/EA.Weee.Web/Areas/Producer/Controllers/ProducerController.cs
@@ -1,10 +1,12 @@
 ﻿namespace EA.Weee.Web.Areas.Producer.Controllers
 {
     using EA.Weee.Core;
-    using EA.Weee.Web.Areas.Producer.Filters;
-    using EA.Weee.Web.Controllers.Base;
-    using System.Web.Mvc;
     using EA.Weee.Core.DirectRegistrant;
+    using EA.Weee.Web.Areas.Producer.Filters;
+    using EA.Weee.Web.Areas.Producer.ViewModels;
+    using EA.Weee.Web.Controllers.Base;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
 
     [AuthorizeRouteClaims("directRegistrantId", WeeeClaimTypes.DirectRegistrantAccess)]
     public class ProducerController : ExternalSiteController
@@ -19,6 +21,27 @@
         [SmallProducerSubmissionContext]
         [HttpGet]
         public ActionResult TaskList()
+        {
+            var submission = SmallProducerSubmissionData.CurrentSubmission;
+
+            var model = new TaskListViewModel()
+            {
+                ProducerTaskModels = new List<ProducerTaskModel>
+                {
+                    new ProducerTaskModel { TaskLinkName = "Organisation details", Complete = submission.OrganizationDetailsComplete },
+                    new ProducerTaskModel { TaskLinkName = "Contact details", Complete = submission.ContactDetailsComplete },
+                    new ProducerTaskModel { TaskLinkName = "Service of notice", Complete = submission.ServiceOfNoticeComplete },
+                    new ProducerTaskModel { TaskLinkName = "Representing company details", Complete = submission.RepresentingCompanyDetailsComplete },
+                    new ProducerTaskModel { TaskLinkName = "EEE details", Complete = submission.EEEDetailsComplete }
+                }
+            };
+
+            return View(model);
+        }
+
+        [SmallProducerSubmissionContext]
+        [HttpPost]
+        public ActionResult TaskList()//check answers?
         {
             return View();
         }
