f1e4fd12550e1efd39a8f206abad0f2ca8e41892
Sean Mccallum <Sean.Mccallum@civica.co.uk>
2024-09-09 13:38:06 +0100

    70929 - TO3.0 - Application Task List

--- src/EA.Weee.Web/Areas/Producer/Controllers/ProducerController.cs
+++ src/EA.Weee.Web/Areas/Producer/Controllers/ProducerController.cs
@@ -4,14 +4,27 @@
     using EA.Weee.Core.DirectRegistrant;
     using EA.Weee.Web.Areas.Producer.Filters;
     using EA.Weee.Web.Areas.Producer.ViewModels;
+    using EA.Weee.Web.Constant;
     using EA.Weee.Web.Controllers.Base;
+    using EA.Weee.Web.Services;
+    using EA.Weee.Web.Services.Caching;
+    using System;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
     using System.Web.Mvc;
 
     [AuthorizeRouteClaims("directRegistrantId", WeeeClaimTypes.DirectRegistrantAccess)]
     public class ProducerController : ExternalSiteController
     {
         public SmallProducerSubmissionData SmallProducerSubmissionData;
+        private readonly BreadcrumbService breadcrumb;
+        private readonly IWeeeCache cache;
+
+        public ProducerController(BreadcrumbService breadcrumb, IWeeeCache cache)
+        {
+            this.breadcrumb = breadcrumb;
+            this.cache = cache;
+        }
 
         public ActionResult Index()
         {
@@ -20,7 +33,7 @@
 
         [SmallProducerSubmissionContext]
         [HttpGet]
-        public ActionResult TaskList()
+        public async Task<ActionResult> TaskList()
         {
             var submission = SmallProducerSubmissionData.CurrentSubmission;
 
@@ -28,22 +41,50 @@
             {
                 ProducerTaskModels = new List<ProducerTaskModel>
                 {
-                    new ProducerTaskModel { TaskLinkName = "Organisation details", Complete = submission.OrganizationDetailsComplete },
-                    new ProducerTaskModel { TaskLinkName = "Contact details", Complete = submission.ContactDetailsComplete },
-                    new ProducerTaskModel { TaskLinkName = "Service of notice", Complete = submission.ServiceOfNoticeComplete },
-                    new ProducerTaskModel { TaskLinkName = "Representing company details", Complete = submission.RepresentingCompanyDetailsComplete },
-                    new ProducerTaskModel { TaskLinkName = "EEE details", Complete = submission.EEEDetailsComplete }
+                    new ProducerTaskModel 
+                    { 
+                        TaskLinkName = "Organisation details", 
+                        Complete = submission.OrganizationDetailsComplete 
+                    },
+                    new ProducerTaskModel 
+                    { 
+                        TaskLinkName = "Contact details", 
+                        Complete = submission.ContactDetailsComplete 
+                    },
+                    new ProducerTaskModel 
+                    {
+                        TaskLinkName = "Service of notice", 
+                        Complete = submission.ServiceOfNoticeComplete 
+                    },
+                    new ProducerTaskModel 
+                    { 
+                        TaskLinkName = "Representing company details", 
+                        Complete = submission.RepresentingCompanyDetailsComplete 
+                    },
+                    new ProducerTaskModel 
+                    { 
+                        TaskLinkName = "EEE details", 
+                        Complete = submission.EEEDetailsComplete
+                    }
                 }
             };
+
+            await SetBreadcrumb(SmallProducerSubmissionData.OrganisationData.Id, BreadCrumbConstant.AatfReturn);
 
             return View(model);
         }
 
-        [SmallProducerSubmissionContext]
-        [HttpPost]
-        public ActionResult TaskList()//check answers?
+        private async Task SetBreadcrumb(Guid organisationId, string activity)
         {
-            return View();
+            breadcrumb.ExternalOrganisation = await cache.FetchOrganisationName(organisationId);
+            breadcrumb.ExternalActivity = activity;
+            breadcrumb.OrganisationId = organisationId;
+        }
+
+        [HttpGet]
+        public ActionResult CheckAnswers()
+        {
+            return View("CheckAnswers");
         }
     }
 }
