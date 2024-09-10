94240ccfc765555d9babd17d2558fe44dd61b5e1
John Padgett <John.Padgett@civica.co.uk>
2024-09-10 15:25:48 +0100

    Updates to persist sole trader details

--- src/EA.Weee.Integration.Tests/Handlers/DirectRegistrant/CompleteOrganisationTransactionHandlerIntegrationTests.cs
+++ src/EA.Weee.Integration.Tests/Handlers/DirectRegistrant/CompleteOrganisationTransactionHandlerIntegrationTests.cs
@@ -147,6 +147,7 @@
                     .With(o => o.AuthorisedRepresentative, YesNoType.No)
                     .With(o => o.OrganisationViewModel, soleTraderDetails)
                     .With(o => o.ContactDetailsViewModel, contactDetailsViewModel)
+                    .With(o => o.SoleTraderViewModel, soleTraderModel)
                     .Create();
 
                 OrganisationTransactionDbSetup.Init().WithModel(organisationTransactionData).Create();
@@ -235,6 +236,18 @@
                 directRegistrant.Address.Telephone.Should().Be(contactDetailsViewModel.AddressData.Telephone);
             };
 
+            private readonly It shouldHaveReturnedSoleTraderDetails = () =>
+            {
+                var directRegistrant = Query.GetDirectRegistrantByOrganisationId(result);
+                var additionalDetails = Query.GetAdditionalDetailsByRegistrantId(directRegistrant.Id);
+
+                additionalDetails.Should().NotBeEmpty();
+                additionalDetails.Count.Should().Be(1);
+                additionalDetails.ElementAt(0).FirstName.Should().Be(soleTraderModel.FirstName);
+                additionalDetails.ElementAt(0).LastName.Should().Be(soleTraderModel.LastName);
+                additionalDetails.ElementAt(0).Type.Should().Be(OrganisationAdditionalDetailsType.SoleTrader);
+            };
+
             private readonly It shouldHaveCreatedOrganisationUser = () =>
             {
                 var users = Query.GetOrganisationForUser(UserId.ToString());
@@ -302,10 +315,11 @@
                 var additionalDetails = Query.GetAdditionalDetailsByRegistrantId(directRegistrant.Id);
 
                 additionalDetails.Should().NotBeEmpty();
-                foreach (var additionalDetail in additionalDetails)
+                
+                foreach (var partnerModel in partnerViewModel)
                 {
-                    partnerViewModel.Should().Contain(c =>
-                        c.FirstName == additionalDetail.FirstName && c.LastName == additionalDetail.LastName);
+                    additionalDetails.Should().Contain(c =>
+                        c.FirstName == partnerModel.FirstName && c.LastName == partnerModel.LastName && c.Type == OrganisationAdditionalDetailsType.Partner);
                 }
             };
 
@@ -649,6 +663,7 @@
             protected static ContactDetailsViewModel contactDetailsViewModel;
             protected static Organisation organisation;
             protected static List<PartnerModel> partnerViewModel;
+            protected static SoleTraderViewModel soleTraderModel;
 
             public static IntegrationTestSetupBuilder LocalSetup()
             {
@@ -672,13 +687,8 @@
                 contactDetailsViewModel = fixture.Build<ContactDetailsViewModel>()
                     .With(r => r.AddressData, organisationContactAddress).Create();
 
-                var partnerModels = new List<PartnerModel> 
-                {
-                    new PartnerModel{ FirstName = "firstname1", LastName = "lastname1"},
-                    new PartnerModel{ FirstName = "firstname2", LastName = "lastname2"},
-                };
-
                 partnerViewModel = fixture.CreateMany<PartnerModel>().ToList();
+                soleTraderModel = fixture.Create<SoleTraderViewModel>();
 
                 var representingCompanyAddressDetails = fixture.Build<RepresentingCompanyAddressData>()
                     .With(r => r.CountryId, country.Id).Create();
