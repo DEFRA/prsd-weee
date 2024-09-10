fdbb2d85312978c4efe0d96e066d579f91212055
John Padgett <John.Padgett@civica.co.uk>
2024-09-10 14:15:32 +0100

    Initial version or organisation edit

--- src/EA.Weee.Web/Areas/Producer/Controllers/ProducerSubmissionController.cs
+++ src/EA.Weee.Web/Areas/Producer/Controllers/ProducerSubmissionController.cs
@@ -1,13 +1,22 @@
 ﻿namespace EA.Weee.Web.Areas.Producer.Controllers
 {
     using EA.Prsd.Core.Mapper;
+    using EA.Weee.Api.Client;
     using EA.Weee.Core;
     using EA.Weee.Core.DirectRegistrant;
+    using EA.Weee.Core.Helpers;
+    using EA.Weee.Core.Organisations.Base;
+    using EA.Weee.Core.Shared;
     using EA.Weee.Requests.Organisations.DirectRegistrant;
+    using EA.Weee.Requests.Shared;
     using EA.Weee.Web.Areas.Producer.Filters;
     using EA.Weee.Web.Areas.Producer.ViewModels;
     using EA.Weee.Web.Controllers.Base;
+    using EA.Weee.Web.Infrastructure;
     using EA.Weee.Web.Requests.Base;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
     using System.Web.Mvc;
 
     [AuthorizeRouteClaims("directRegistrantId", WeeeClaimTypes.DirectRegistrantAccess)]
@@ -16,37 +25,60 @@
         public SmallProducerSubmissionData SmallProducerSubmissionData;
 
         private readonly IMapper mapper;
-
-        private readonly IRequestCreator<EditOrganisationDetailsViewModel, EditProducerSubmissionAddressRequest>
+        private readonly IRequestCreator<EditOrganisationDetailsViewModel, EditOrganisationDetailsRequest>
             editOrganisationDetailsRequestCreator;
+        private readonly Func<IWeeeClient> apiClient;
 
-        public ProducerSubmissionController(IMapper mapper, IRequestCreator<EditOrganisationDetailsViewModel, EditProducerSubmissionAddressRequest> editOrganisationDetailsRequestCreator)
+        public ProducerSubmissionController(IMapper mapper, IRequestCreator<EditOrganisationDetailsViewModel, EditOrganisationDetailsRequest> editOrganisationDetailsRequestCreator, Func<IWeeeClient> apiClient)
         {
             this.mapper = mapper;
             this.editOrganisationDetailsRequestCreator = editOrganisationDetailsRequestCreator;
+            this.apiClient = apiClient;
         }
 
         [HttpGet]
         [SmallProducerSubmissionContext]
-        public ActionResult EditOrganisationDetails()
+        public async Task<ActionResult> EditOrganisationDetails()
         {
             var model = mapper.Map<SmallProducerSubmissionData, EditOrganisationDetailsViewModel>(SmallProducerSubmissionData);
+
+            var countries = await GetCountries();
+
+            model.Organisation.Address.Countries = countries;
 
             return View(model);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult EditOrganisationDetails(EditOrganisationDetailsViewModel model)
+        public async Task<ActionResult> EditOrganisationDetails(EditOrganisationDetailsViewModel model)
         {
             if (ModelState.IsValid)
             {
                 var request = editOrganisationDetailsRequestCreator.ViewModelToRequest(model);
 
-                // send the request
+                using (var client = apiClient())
+                {
+                    await client.SendAsync(User.GetAccessToken(), request);
+                }
+
+                return RedirectToAction(nameof(ProducerController.TaskList),
+                    typeof(ProducerController).GetControllerName());
             }
 
+            var countries = await GetCountries();
+
+            model.Organisation.Address.Countries = countries;
+
             return View(model);
+        }
+
+        private async Task<IList<CountryData>> GetCountries()
+        {
+            using (var client = apiClient())
+            {
+                return await client.SendAsync(User.GetAccessToken(), new GetCountries(false));
+            }
         }
     }
 }
