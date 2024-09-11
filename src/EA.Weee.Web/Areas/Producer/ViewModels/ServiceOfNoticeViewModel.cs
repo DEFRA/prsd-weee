652a7e3ebf442677c7cfefaa9e7708b75e972599
William Hope <william.hope@Civica.co.uk>
2024-09-11 09:37:35 +0100

    Added view and view model

---
+++ src/EA.Weee.Web/Areas/Producer/ViewModels/ServiceOfNoticeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EA.Weee.Web.Areas.Producer.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class ServiceOfNoticeViewModel
+    {
+    }
+}
