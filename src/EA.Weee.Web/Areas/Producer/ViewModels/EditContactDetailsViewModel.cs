7c907cc8ace51cb97c9c048b20a34cdec0a4c0de
cklee <chi.kuenLee@civica.co.uk>
2024-09-12 12:51:22 +0100

    add redirect to contact details

---
+++ src/EA.Weee.Web/Areas/Producer/ViewModels/EditContactDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Weee.Web.Areas.Producer.ViewModels
+{
+    using EA.Weee.Core.Organisations;
+    using EA.Weee.Core.Organisations.Base;
+    using System;
+    using System.Collections.Generic;
+
+    public class EditContactDetailsViewModel
+    {
+        public ContactDetailsViewModel ContactDetails { get; set; }
+
+        public Guid DirectRegistrantId { get; set; }
+
+        public Guid OrganisationId { get; set; }
+
+        public List<AdditionalContactModel> AdditionalContactModels { get; set; }
+
+        public bool HasAuthorisedRepresentitive { get; set; }
+    }
+}
