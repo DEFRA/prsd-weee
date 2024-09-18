1f2b218ad0553e389e432dff116e3480b4886ba4
William Hope <william.hope@Civica.co.uk>
2024-09-18 13:24:16 +0100

    Initial Check answers screen work.

---
+++ src/EA.Weee.Web/Areas/Producer/Mappings/ToViewModel/CheckAnswersMap.cs
@@ -0,0 +1,52 @@
+﻿namespace EA.Weee.Web.Areas.Producer.Mappings.ToViewModel
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.DirectRegistrant;
+    using EA.Weee.Core.Organisations;
+    using EA.Weee.Core.Shared;
+    using EA.Weee.Web.Areas.Producer.ViewModels;
+
+    public class CheckAnswersMap : IMap<SmallProducerSubmissionData, CheckAnswersViewModel>
+    {
+        private readonly IMapper mapper;
+
+        public CheckAnswersMap(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        public CheckAnswersViewModel Map(SmallProducerSubmissionData source)
+        {
+            var editOrganisationDetailsmodel =
+                            mapper.Map<SmallProducerSubmissionData, EditOrganisationDetailsViewModel>(source);
+            var editContactDetailsmodel =
+                            mapper.Map<SmallProducerSubmissionData, EditContactDetailsViewModel>(source);
+            var serviceOfNoticemodel =
+                            mapper.Map<SmallProducerSubmissionData, ServiceOfNoticeViewModel>(source);
+
+            RepresentingCompanyDetailsViewModel representingCompanyDetailsmodel = null;
+
+            if (source.HasAuthorisedRepresentitive)
+            {
+                representingCompanyDetailsmodel =
+                            mapper.Map<SmallProducerSubmissionData, RepresentingCompanyDetailsViewModel>(source);
+            }
+            var editEeeDatamodel =
+                            mapper.Map<SmallProducerSubmissionData, EditEeeDataViewModel>(source);
+
+            var viewModel = new CheckAnswersViewModel()
+            {
+                DirectRegistrantId = source.DirectRegistrantId,
+                HasAuthorisedRepresentitive = source.HasAuthorisedRepresentitive,
+                OrganisationId = source.OrganisationData.Id,
+                OrganisationDetails = editOrganisationDetailsmodel,
+                ContactDetails = editContactDetailsmodel,
+                ServiceOfNoticeData = serviceOfNoticemodel,
+                RepresentingCompanyDetails = representingCompanyDetailsmodel,
+                EeeData = editEeeDatamodel,
+            };
+
+            return viewModel;
+        }
+    }
+}
