e1e607759b070af8f034028593db4e6300f80ac7
cklee <chi.kuenLee@civica.co.uk>
2024-09-18 16:27:43 +0100

    restore changes

--- src/EA.Weee.Core/Organisations/Base/OrganisationViewModel.cs
+++ src/EA.Weee.Core/Organisations/Base/OrganisationViewModel.cs
@@ -25,8 +25,8 @@
 
         public ExternalAddressData Address { get; set; } = new ExternalAddressData() { CountryId = UkCountry.Ids.England };
 
-        [StringLength(maximumLength: EnvironmentAgencyMaxFieldLengths.CompanyRegistrationNumber, MinimumLength = 7, ErrorMessage = "The company registration number should be 7 to 15 characters long")]
-        [Display(Name = "company registration number (CRN)")]
+        [StringLength(maximumLength: EnvironmentAgencyMaxFieldLengths.CompanyRegistrationNumber, MinimumLength = 7, ErrorMessage = "The Company registration number should be 7 to 15 characters long")]
+        [Display(Name = "Company registration number (CRN)")]
         public string CompaniesRegistrationNumber { get; set; }
 
         [StringLength(CommonMaxFieldLengths.DefaultString)]
