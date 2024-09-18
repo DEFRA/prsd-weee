beb7b7fd87817c45abf7f5aa84087b783506f0e7
Sean Mccallum <Sean.Mccallum@civica.co.uk>
2024-09-18 17:00:29 +0100

    71111 - TO2.0 - View organisation - 'Organisation details' tab

--- src/EA.Weee.Web/Areas/Producer/Controllers/ProducerController.cs
+++ src/EA.Weee.Web/Areas/Producer/Controllers/ProducerController.cs
@@ -1,16 +1,21 @@
 ﻿namespace EA.Weee.Web.Areas.Producer.Controllers
 {
+    using EA.Prsd.Core.Mapper;
     using EA.Weee.Core;
     using EA.Weee.Core.DirectRegistrant;
+    using EA.Weee.Core.Organisations.Base;
+    using EA.Weee.Web.Areas.Admin.ViewModels.Scheme.Overview;
     using EA.Weee.Web.Areas.Producer.Filters;
     using EA.Weee.Web.Areas.Producer.ViewModels;
     using EA.Weee.Web.Constant;
     using EA.Weee.Web.Controllers.Base;
     using EA.Weee.Web.Services;
     using EA.Weee.Web.Services.Caching;
+    using iText.Kernel.XMP.Options;
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using System.Web.Helpers;
     using System.Web.Mvc;
 
     [AuthorizeRouteClaims("directRegistrantId", WeeeClaimTypes.DirectRegistrantAccess)]
@@ -19,11 +24,16 @@
         public SmallProducerSubmissionData SmallProducerSubmissionData;
         private readonly BreadcrumbService breadcrumb;
         private readonly IWeeeCache cache;
+        private readonly IMapper mapper;
 
-        public ProducerController(BreadcrumbService breadcrumb, IWeeeCache cache)
+        public ProducerController(
+            BreadcrumbService breadcrumb, 
+            IWeeeCache cache,
+            IMapper mapper)
         {
             this.breadcrumb = breadcrumb;
             this.cache = cache;
+            this.mapper = mapper;
         }
 
         public ActionResult Index()
@@ -107,9 +117,84 @@
 
         [SmallProducerSubmissionContext]
         [HttpGet]
-        public ActionResult OrganisationDetails()
+        public async Task<ActionResult> OrganisationDetails()
         {
-            return View(SmallProducerSubmissionData.OrganisationData.Id);
+            await SetBreadcrumb(SmallProducerSubmissionData.OrganisationData.Id, ProducerSubmissionConstant.ViewOrganisation);
+
+            return ViewOrganisation(this.SmallProducerSubmissionData);
+        }
+
+        [SmallProducerSubmissionContext]
+        [HttpGet]
+        public async Task<ActionResult> RouteTab(OrganisationDetailsDisplayOption option)
+        {
+            await SetBreadcrumb(SmallProducerSubmissionData.OrganisationData.Id, ProducerSubmissionConstant.ViewOrganisation);
+
+            if (option == OrganisationDetailsDisplayOption.OrganisationDetails)
+            {
+                return ViewOrganisation(this.SmallProducerSubmissionData);
+            }
+            if (option == OrganisationDetailsDisplayOption.ContactDetails)
+            {
+                return ContactDetails(this.SmallProducerSubmissionData);
+            }
+            if (option == OrganisationDetailsDisplayOption.ServiceOfNoticeDetails)
+            {
+                return ServiceOfNoticeDetails(this.SmallProducerSubmissionData);
+            }
+            if (option == OrganisationDetailsDisplayOption.RepresentedOrganisationDetails)
+            {
+                return RepresentedOrganisationDetails(this.SmallProducerSubmissionData);
+            }
+
+            return await OrganisationDetails();
+        }
+
+        public ActionResult ViewOrganisation(SmallProducerSubmissionData submissionsData)
+        {
+            var organisationVM = mapper.Map<SmallProducerSubmissionData, OrganisationViewModel>(SmallProducerSubmissionData);
+
+            var vm = new OrganisationDetailsTabsViewModel
+            {
+                ActiveOption = OrganisationDetailsDisplayOption.OrganisationDetails,
+                OrganisationViewModel = organisationVM,
+                SmallProducerSubmissionData = submissionsData,
+            };
+
+            return View("ViewOrganisation/OrganisationDetails", vm);
+        }
+
+        public ActionResult ContactDetails(SmallProducerSubmissionData submissionsData)
+        {
+            var vm = new OrganisationDetailsTabsViewModel
+            {
+                ActiveOption = OrganisationDetailsDisplayOption.ContactDetails,
+                SmallProducerSubmissionData = submissionsData
+            };
+
+            return View("ViewOrganisation/ContactDetails", vm);
+        }
+
+        public ActionResult ServiceOfNoticeDetails(SmallProducerSubmissionData submissionsData)
+        {
+            var vm = new OrganisationDetailsTabsViewModel
+            {
+                ActiveOption = OrganisationDetailsDisplayOption.ServiceOfNoticeDetails,
+                SmallProducerSubmissionData = submissionsData
+            };
+
+            return View("ViewOrganisation/ServiceOfNoticeDetails", vm);
+        }
+
+        public ActionResult RepresentedOrganisationDetails(SmallProducerSubmissionData submissionsData)
+        {
+            var vm = new OrganisationDetailsTabsViewModel
+            {
+                ActiveOption = OrganisationDetailsDisplayOption.RepresentedOrganisationDetails,
+                SmallProducerSubmissionData = submissionsData
+            };
+
+            return View("ViewOrganisation/RepresentedOrganisationDetails", vm);
         }
     }
 }
---
+++ src/EA.Weee.Web/Areas/Producer/Mappings/ToViewModel/OrganisationViewModelMap.cs
@@ -0,0 +1,47 @@
+﻿namespace EA.Weee.Web.Areas.Producer.Mappings.ToViewModel
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.DirectRegistrant;
+    using EA.Weee.Core.Organisations;
+    using EA.Weee.Core.Organisations.Base;
+
+    public class OrganisationViewModelMap : IMap<SmallProducerSubmissionData, OrganisationViewModel>
+    {
+        public OrganisationViewModel Map(SmallProducerSubmissionData source)
+        {
+            return new OrganisationViewModel()
+            {
+                Address = new Core.Organisations.ExternalAddressData
+                {
+                    Address1 = source.OrganisationData.BusinessAddress.Address1,
+                    Address2 = source.OrganisationData.BusinessAddress.Address2,
+                    CountryId = source.OrganisationData.BusinessAddress.CountryId,
+                    CountryName = source.OrganisationData.BusinessAddress.CountryName,
+                    Postcode = source.OrganisationData.BusinessAddress.Postcode,
+                    TownOrCity = source.OrganisationData.BusinessAddress.TownOrCity,
+                    CountyOrRegion = source.OrganisationData.BusinessAddress.CountyOrRegion,
+                    WebsiteAddress = source.OrganisationData.BusinessAddress.WebAddress
+                },
+                CompanyName = source.OrganisationData.Name,
+                BusinessTradingName = source.OrganisationData.TradingName,
+                CompaniesRegistrationNumber = source.OrganisationData.CompanyRegistrationNumber,
+                OrganisationType = MapOrganisationType(source.OrganisationData.OrganisationType)
+            };
+        }
+
+        private ExternalOrganisationType MapOrganisationType(OrganisationType organisationType)
+        {
+            switch (organisationType)
+            {
+                case OrganisationType.Partnership:
+                    return ExternalOrganisationType.Partnership;
+                case OrganisationType.RegisteredCompany:
+                    return ExternalOrganisationType.RegisteredCompany;
+                case OrganisationType.SoleTraderOrIndividual:
+                    return ExternalOrganisationType.SoleTrader;
+                default:
+                    return ExternalOrganisationType.RegisteredCompany;
+            }
+        }
+    }
+}
