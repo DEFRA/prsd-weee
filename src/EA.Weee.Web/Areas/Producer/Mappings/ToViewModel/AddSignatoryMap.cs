07d513417845dadcea63296279e9ef15d64ff94b
cklee <chi.kuenLee@civica.co.uk>
2024-09-20 14:27:46 +0100

    add AppropriateSignatory

---
+++ src/EA.Weee.Web/Areas/Producer/Mappings/ToViewModel/AddSignatoryMap.cs
@@ -0,0 +1,37 @@
+﻿namespace EA.Weee.Web.Areas.Producer.Mappings.ToViewModel
+{
+    using EA.Prsd.Core.Mapper;
+    using EA.Weee.Core.DirectRegistrant;
+    using EA.Weee.Core.Organisations;
+    using EA.Weee.Core.Shared;
+    using EA.Weee.Web.Areas.Producer.ViewModels;
+    using EA.Weee.Web.ViewModels.Shared;
+
+    public class AddSignatoryMap : IMap<SmallProducerSubmissionData, AppropriateSignatoryViewModel>
+    {
+        private readonly IMapper mapper;
+
+        public AddSignatoryMap(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        public AppropriateSignatoryViewModel Map(SmallProducerSubmissionData source)
+        {
+            var viewModel = new AppropriateSignatoryViewModel
+            {
+                DirectRegistrantId = source.DirectRegistrantId,
+                OrganisationId = source.OrganisationData.Id,
+                HasAuthorisedRepresentitive = source.HasAuthorisedRepresentitive
+            };
+
+            var contactPersonViewModel = viewModel.Contact = new ContactPersonViewModel();
+
+            contactPersonViewModel.FirstName = source.CurrentSubmission.ContactData.FirstName;
+            contactPersonViewModel.LastName = source.CurrentSubmission.ContactData.LastName;
+            contactPersonViewModel.Position = source.CurrentSubmission.ContactData.Position;
+
+            return viewModel;
+        }
+    }
+}
