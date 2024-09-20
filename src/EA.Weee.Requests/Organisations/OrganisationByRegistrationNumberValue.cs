f8015084ff9d0dad6c1a704c15266f0362aa3b85
Sean Mccallum <Sean.Mccallum@civica.co.uk>
2024-09-20 17:03:03 +0100

    76654 - TO1.0 - Automatic check for existing organisation

---
+++ src/EA.Weee.Requests/Organisations/OrganisationByRegistrationNumberValue.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Weee.Requests.Organisations
+{
+    using Core.Organisations;
+    using Prsd.Core;
+    using Prsd.Core.Mediator;
+    using System;
+
+    public class OrganisationByRegistrationNumberValue : IRequest<OrganisationData>
+    {
+        public string RegistrationNumber { get; private set; }
+
+        public OrganisationByRegistrationNumberValue(string registrationNumber)
+        {
+            Guard.ArgumentNotDefaultValue(() => registrationNumber, registrationNumber);
+
+            RegistrationNumber = registrationNumber;
+        }
+    }
+}
