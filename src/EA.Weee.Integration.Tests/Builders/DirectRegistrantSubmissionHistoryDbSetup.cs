e6d36110af18bcb21ca15a6480b2c41bf391ebe7
John Padgett <John.Padgett@civica.co.uk>
2024-09-25 10:03:19 +0100

    Temporary tidy the integration test

--- src/EA.Weee.Integration.Tests/Builders/DirectRegistrantSubmissionHistoryDbSetup.cs
+++ src/EA.Weee.Integration.Tests/Builders/DirectRegistrantSubmissionHistoryDbSetup.cs
@@ -1,17 +1,20 @@
 ﻿namespace EA.Weee.Integration.Tests.Builders
 {
     using Base;
-    using EA.Prsd.Core;
-    using EA.Weee.Domain.Evidence;
     using EA.Weee.Domain.Producer;
     using EA.Weee.Tests.Core;
-    using System.Globalization;
+    using System;
 
     public class DirectRegistrantSubmissionHistoryDbSetup : DbTestDataBuilder<DirectProducerSubmissionHistory, DirectRegistrantSubmissionHistoryDbSetup>
     {
         protected override DirectProducerSubmissionHistory Instantiate()
         {
-            instance = new DirectProducerSubmissionHistory();
+            instance = new DirectProducerSubmissionHistory
+            {
+                CompanyName = "new company name",
+                TradingName = "new trading name",
+                CompanyRegistrationNumber = "12345678"
+            };
 
             return instance;
         }
@@ -19,6 +22,34 @@
         public DirectRegistrantSubmissionHistoryDbSetup WithDirectProducerSubmission(DirectProducerSubmission submission)
         {
             ObjectInstantiator<DirectProducerSubmissionHistory>.SetProperty(o => o.DirectProducerSubmissionId, submission.Id, instance);
+
+            return this;
+        }
+
+        public DirectRegistrantSubmissionHistoryDbSetup WithContact(Guid contactId)
+        {
+            ObjectInstantiator<DirectProducerSubmissionHistory>.SetProperty(o => o.ContactId, contactId, instance);
+
+            return this;
+        }
+
+        public DirectRegistrantSubmissionHistoryDbSetup WithAddress(Guid addressId)
+        {
+            ObjectInstantiator<DirectProducerSubmissionHistory>.SetProperty(o => o.BusinessAddressId, addressId, instance);
+
+            return this;
+        }
+
+        public DirectRegistrantSubmissionHistoryDbSetup WithBrandName(string brandName)
+        {
+            ObjectInstantiator<DirectProducerSubmissionHistory>.SetProperty(o => o.BrandName, new BrandName(brandName), instance);
+
+            return this;
+        }
+
+        public DirectRegistrantSubmissionHistoryDbSetup WithAuthorisedRep(Guid authorisedRepId)
+        {
+            ObjectInstantiator<DirectProducerSubmissionHistory>.SetProperty(o => o.AuthorisedRepresentativeId, authorisedRepId, instance);
 
             return this;
         }
