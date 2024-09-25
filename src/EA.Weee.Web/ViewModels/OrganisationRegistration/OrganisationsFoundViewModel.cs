bd08fc37b7f2b0fe2d9fd2467d35f0aca4315cda
William Hope <william.hope@Civica.co.uk>
2024-09-25 16:07:28 +0100

    WIP

--- src/EA.Weee.Web/ViewModels/OrganisationRegistration/OrganisationsFoundViewModel.cs
+++ src/EA.Weee.Web/ViewModels/OrganisationRegistration/OrganisationsFoundViewModel.cs
@@ -1,9 +1,15 @@
 ﻿namespace EA.Weee.Web.ViewModels.OrganisationRegistration
 {
+    using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
 
     public class OrganisationsFoundViewModel
     {
         public IEnumerable<OrganisationFoundViewModel> OrganisationFoundViewModels { get; set; }
+
+        [Required(ErrorMessage = "You must choose an organisation")]
+        public Guid? SelectedOrganisationId { get; set; }
     }
 }
