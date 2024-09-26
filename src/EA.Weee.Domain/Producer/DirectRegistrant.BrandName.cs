c3cea6b4b950554a51da0d864d576184cb6d85ba
John Padgett <John.Padgett@civica.co.uk>
2024-09-26 15:07:34 +0100

    Fix issue with signatory submission

---
+++ src/EA.Weee.Domain/Producer/DirectRegistrant.BrandName.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Weee.Domain.Producer
+{
+    using EA.Prsd.Core;
+    using EA.Prsd.Core.Domain;
+    using EA.Weee.Domain.Organisation;
+
+    public partial class DirectRegistrant
+    {
+        public bool HasBrandName => BrandName != null;
+
+        public void AddOrUpdateBrandName(BrandName brandName)
+        {
+            Guard.ArgumentNotNull(() => brandName, brandName);
+
+            BrandName = brandName.OverwriteWhereNull(BrandName);
+        }
+    }
+}
