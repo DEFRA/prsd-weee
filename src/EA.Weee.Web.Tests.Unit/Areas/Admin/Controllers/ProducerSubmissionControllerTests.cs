8f6e797698640ebe4d1b3b8d438d9313c6589a18
John Padgett <John.Padgett@civica.co.uk>
2024-10-03 13:53:25 +0100

    Fix failing unit test

--- src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/ProducerSubmissionControllerTests.cs
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/ProducerSubmissionControllerTests.cs
@@ -1,10 +1,11 @@
 ﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.Controllers
 {
+    using EA.Weee.Web.Areas.Admin.Controllers;
     using FluentAssertions;
     using Web.Areas.Admin.Controllers.Base;
     using Xunit;
 
-    public class ProducerSubmissionController
+    public class ProducerSubmissionControllerUnitTests
     {
         [Fact]
         public void Controller_ShouldInheritFromAdminBaseController()
