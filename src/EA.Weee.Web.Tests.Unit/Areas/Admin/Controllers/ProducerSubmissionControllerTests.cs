e3c897bc9ce6ba129ff810ef9e8d02f8af23af1e
John Padgett <John.Padgett@civica.co.uk>
2024-10-03 11:36:07 +0100

    Add empty producer submission controller

---
+++ src/EA.Weee.Web.Tests.Unit/Areas/Admin/Controllers/ProducerSubmissionControllerTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Weee.Web.Tests.Unit.Areas.Admin.Controllers
+{
+    using FluentAssertions;
+    using Web.Areas.Admin.Controllers.Base;
+    using Xunit;
+
+    public class ProducerSubmissionController
+    {
+        [Fact]
+        public void Controller_ShouldInheritFromAdminBaseController()
+        {
+            typeof(ProducerSubmissionController).Should().BeDerivedFrom<AdminController>();
+        }
+    }
+}
