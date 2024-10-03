1fdd6093440efcae62bcb3ea29b2b7830f87131c
John Padgett <John.Padgett@civica.co.uk>
2024-10-03 11:44:43 +0100

    Fix syntax issue

--- src/EA.Weee.Web/Areas/Admin/Controllers/ProducerSubmissionController.cs
+++ src/EA.Weee.Web/Areas/Admin/Controllers/ProducerSubmissionController.cs
@@ -3,7 +3,6 @@
     using EA.Weee.Web.Areas.Admin.Controllers.Base;
     using System.Web.Mvc;
 
-
     public class ProducerSubmissionController : AdminController
     {
         // GET: Admin/ProducerSubmission
