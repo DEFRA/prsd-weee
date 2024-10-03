4b29a8d50eda7573c7c9909e6a7c75393c3c49af
John Padgett <John.Padgett@civica.co.uk>
2024-10-03 16:23:51 +0100

    WIP small producer cache

---
+++ src/EA.Weee.Core/Search/SmallProducerSearchResult.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.Core.Search
+{
+    public class SmallProducerSearchResult : SearchResult
+    {
+        public string RegistrationNumber { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/EA.Weee.RequestHandlers/Search/FetchSmallProducerSearchResultsForCache/FetchSmallProducerSearchResultsForCacheDataAccess.cs
@@ -0,0 +1,39 @@
+﻿namespace EA.Weee.RequestHandlers.Search.FetchSmallProducerSearchResultsForCache
+{
+    using EA.Weee.Core.Search;
+    using EA.Weee.DataAccess;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using EA.Weee.Domain.Producer;
+
+    public class FetchSmallProducerSearchResultsForCacheDataAccess : IFetchSmallProducerSearchResultsForCacheDataAccess
+    {
+        private readonly WeeeContext context;
+
+        public FetchSmallProducerSearchResultsForCacheDataAccess(WeeeContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IList<SmallProducerSearchResult>> FetchLatestProducers()
+        {
+            var results = await context.DirectProducerSubmissions
+                .Where(s => s.DirectProducerSubmissionStatus.Value == DirectProducerSubmissionStatus.Complete.Value)
+                .Select(s => s.RegisteredProducer)
+                .GroupBy(rp => rp.ProducerRegistrationNumber)
+                .Select(group => group.OrderByDescending(rp => rp.ComplianceYear).FirstOrDefault())
+                .OrderBy(rp => rp.ProducerRegistrationNumber)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return results.Select(r => new SmallProducerSearchResult()
+            {
+                RegistrationNumber = r.ProducerRegistrationNumber,
+                Name = r.CurrentSubmission.OrganisationName
+            })
+                .ToList();
+        }
+    }
+}
