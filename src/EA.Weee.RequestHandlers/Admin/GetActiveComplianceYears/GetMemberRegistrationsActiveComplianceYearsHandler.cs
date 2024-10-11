65e64b9812a984cc3e578cfccc248a9d487ea3e6
William Hope <william.hope@Civica.co.uk>
2024-10-11 14:03:57 +0100

    WIP

--- src/EA.Weee.RequestHandlers/Admin/GetActiveComplianceYears/GetMemberRegistrationsActiveComplianceYearsHandler.cs
+++ src/EA.Weee.RequestHandlers/Admin/GetActiveComplianceYears/GetMemberRegistrationsActiveComplianceYearsHandler.cs
@@ -1,5 +1,6 @@
 ﻿namespace EA.Weee.RequestHandlers.Admin.GetActiveComplianceYears
 {
+    using EA.Weee.DataAccess.DataAccess;
     using Prsd.Core.Mediator;
     using Requests.Admin.GetActiveComplianceYears;
     using Security;
@@ -10,16 +11,25 @@
     {
         private readonly IWeeeAuthorization authorization;
         private readonly IGetMemberRegistrationsActiveComplianceYearsDataAccess dataAccess;
+        private readonly IGetDirectProducerSubmissionActiveComplianceYearsDataAccess directProducerSubmissionActiveComplianceYearsDataAccess;
 
-        public GetMemberRegistrationsActiveComplianceYearsHandler(IWeeeAuthorization authorization, IGetMemberRegistrationsActiveComplianceYearsDataAccess dataAccess)
+        public GetMemberRegistrationsActiveComplianceYearsHandler(IWeeeAuthorization authorization, IGetMemberRegistrationsActiveComplianceYearsDataAccess dataAccess, IGetDirectProducerSubmissionActiveComplianceYearsDataAccess directProducerSubmissionActiveComplianceYearsDataAccess)
         {
             this.authorization = authorization;
             this.dataAccess = dataAccess;
+            this.directProducerSubmissionActiveComplianceYearsDataAccess = directProducerSubmissionActiveComplianceYearsDataAccess;
         }
 
         public async Task<List<int>> HandleAsync(GetMemberRegistrationsActiveComplianceYears message)
         {
             authorization.EnsureCanAccessInternalArea();
+
+            var years = await dataAccess.Get();
+
+            if (message.IncludeDirectRegistrantSubmissions)
+            {
+                years.AddRange(await directProducerSubmissionActiveComplianceYearsDataAccess.Get(0));
+            }
 
             return await dataAccess.Get();
         }
