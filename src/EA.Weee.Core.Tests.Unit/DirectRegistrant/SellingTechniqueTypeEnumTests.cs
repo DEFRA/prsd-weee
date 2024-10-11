7a6b26706596286154f4bb685422db185fbeef7f
cklee <chi.kuenLee@civica.co.uk>
2024-10-11 12:24:55 +0100

    refactor
    add SellingTechniqueTypeEnumTests

---
+++ src/EA.Weee.Core.Tests.Unit/DirectRegistrant/SellingTechniqueTypeEnumTests.cs
@@ -0,0 +1,27 @@
+﻿namespace EA.Weee.Core.DirectRegistrant
+{
+    using System;
+    using FluentAssertions;
+    using Prsd.Core.Helpers;
+    using Xunit;
+
+    public class SellingTechniqueTypeEnumTests
+    {
+        [Fact]
+        public void SellingTechniqueType_ShouldHaveSerializableAttribute()
+        {
+            typeof(SellingTechniqueType).Should().BeDecoratedWith<SerializableAttribute>();
+        }
+
+        [Fact]
+        public void SellingTechniqueType_ShouldHaveValues()
+        {
+            var values = EnumHelper.GetValues(typeof(SellingTechniqueType));
+            values.Count.Should().Be(3);
+
+            values.Should().Contain(c => c.Key.Equals(1) && c.Value.Equals("Direct selling to end user (mail, order, internet etc)"));
+            values.Should().Contain(c => c.Key.Equals(2) && c.Value.Equals("Indirect selling (other)"));
+            values.Should().Contain(c => c.Key.Equals(3) && c.Value.Equals("Direct selling to end user (mail, order, internet etc) and Indirect selling (other)"));
+        }
+    }
+}
