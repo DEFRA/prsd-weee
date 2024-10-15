e71261eae67d64539613d04c0541e9bb8c9d5d95
John Padgett <John.Padgett@civica.co.uk>
2024-10-15 16:14:55 +0100

    Update to selling technique enum to match the domain value

--- src/EA.Weee.Core/DirectRegistrant/SellingTechniqueType.cs
+++ src/EA.Weee.Core/DirectRegistrant/SellingTechniqueType.cs
@@ -4,12 +4,12 @@
     public enum SellingTechniqueType
     {
         [Display(Name = "Direct selling to end user (mail, order, internet etc)")]
-        DirectSellingToEndUser = 1,
+        DirectSellingToEndUser = 0,
 
         [Display(Name = "Indirect selling (other)")]
-        IndirectSellingToEndUser = 2,
+        IndirectSellingToEndUser = 1,
 
         [Display(Name = "Direct selling to end user (mail, order, internet etc) and Indirect selling (other)")]
-        Both = 3
+        Both = 2
     }
 }
