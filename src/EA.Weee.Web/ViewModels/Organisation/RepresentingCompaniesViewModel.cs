6477f9b8c6573b9abf9bce0d454c2439f21d1921
John Padgett <John.Padgett@civica.co.uk>
2024-10-16 13:58:24 +0100

    Initial version of authorised rep flow

---
+++ src/EA.Weee.Web/ViewModels/Organisation/RepresentingCompaniesViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Weee.Web.ViewModels.Organisation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    public class RepresentingCompaniesViewModel 
+    {
+        public Guid OrganisationId { get; set; }
+
+        [DisplayName("Which represented organisation would you like to perform activities for?")]
+        [Required(ErrorMessage = "Select a represented organisation to perform activities for")]
+        public Guid? SelectedDirectRegistrant { get; set; }
+
+        public IList<RepresentingCompany> Organisations { get; set; }
+    }
+}
