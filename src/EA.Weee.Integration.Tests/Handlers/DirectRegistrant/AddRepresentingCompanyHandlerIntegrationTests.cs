df9eb9afb8b34f55aa9e64753af6b3797d55e697
John Padgett <John.Padgett@civica.co.uk>
2024-10-17 14:52:04 +0100

    Redirect fix and add integration and unit tests

--- src/EA.Weee.Integration.Tests/Handlers/DirectRegistrant/AddRepresentingCompanyHandlerIntegrationTests.cs
+++ src/EA.Weee.Integration.Tests/Handlers/DirectRegistrant/AddRepresentingCompanyHandlerIntegrationTests.cs
@@ -2,16 +2,16 @@
 {
     using Autofac;
     using AutoFixture;
-    using Base;
+    using EA.Prsd.Core.Autofac;
+    using EA.Prsd.Core.Mediator;
     using EA.Weee.Core.Organisations;
     using EA.Weee.Domain;
     using EA.Weee.Domain.Organisation;
+    using EA.Weee.Integration.Tests.Base;
     using EA.Weee.Integration.Tests.Builders;
     using EA.Weee.Requests.Organisations.DirectRegistrant;
     using FluentAssertions;
     using NUnit.Specifications.Categories;
-    using Prsd.Core.Autofac;
-    using Prsd.Core.Mediator;
     using System;
     using System.Security;
 
@@ -25,10 +25,12 @@
                 LocalSetup();
 
                 var representingCompanyAddressDetails = fixture.Build<RepresentingCompanyAddressData>()
-                    .With(r => r.CountryId, country.Id).Create();
+                    .With(r => r.CountryId, country.Id)
+                    .Create();
 
                 representingCompanyDetails = fixture.Build<RepresentingCompanyDetailsViewModel>()
-                    .With(r => r.Address, representingCompanyAddressDetails).Create();
+                    .With(r => r.Address, representingCompanyAddressDetails)
+                    .Create();
 
                 originalDirectRegistrant = DirectRegistrantDbSetup.Init()
                     .WithOrganisation(organisation.Id)
@@ -51,22 +53,17 @@
                 newDirectRegistrant.Should().NotBeNull();
                 newDirectRegistrant.Address.Id.Should().Be(originalDirectRegistrant.AddressId.Value);
                 newDirectRegistrant.Contact.Id.Should().Be(originalDirectRegistrant.ContactId.Value);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.PrimaryName.Should()
-                    .Be(representingCompanyDetails.Address.Address1);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.SecondaryName.Should()
-                    .BeNullOrWhiteSpace();
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.Street.Should()
-                    .Be(representingCompanyDetails.Address.Address2);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.Town.Should()
-                    .Be(representingCompanyDetails.Address.TownOrCity);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.Locality.Should()
-                    .BeNullOrWhiteSpace();
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.PostCode.Should()
-                    .Be(representingCompanyDetails.Address.Postcode);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.AdministrativeArea.Should()
-                    .Be(representingCompanyDetails.Address.CountyOrRegion);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.CountryId.Should()
-                    .Be(representingCompanyDetails.Address.CountryId);
+
+                var overseasAddress = newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address;
+                overseasAddress.PrimaryName.Should().Be(representingCompanyDetails.Address.Address1);
+                overseasAddress.SecondaryName.Should().BeNullOrWhiteSpace();
+                overseasAddress.Street.Should().Be(representingCompanyDetails.Address.Address2);
+                overseasAddress.Town.Should().Be(representingCompanyDetails.Address.TownOrCity);
+                overseasAddress.Locality.Should().BeNullOrWhiteSpace();
+                overseasAddress.PostCode.Should().Be(representingCompanyDetails.Address.Postcode);
+                overseasAddress.AdministrativeArea.Should().Be(representingCompanyDetails.Address.CountyOrRegion);
+                overseasAddress.CountryId.Should().Be(representingCompanyDetails.Address.CountryId);
+
                 newDirectRegistrant.BrandName.Should().BeNull();
             };
         }
@@ -79,11 +76,13 @@
                 LocalSetup();
 
                 var representingCompanyAddressDetails = fixture.Build<RepresentingCompanyAddressData>()
-                    .With(r => r.CountryId, country.Id).Create();
+                    .With(r => r.CountryId, country.Id)
+                    .Create();
 
                 representingCompanyDetails = fixture.Build<RepresentingCompanyDetailsViewModel>()
-                    .With(r => r.Address, representingCompanyAddressDetails).Create();
-                
+                    .With(r => r.Address, representingCompanyAddressDetails)
+                    .Create();
+
                 originalDirectRegistrant = DirectRegistrantDbSetup.Init()
                     .WithOrganisation(organisation.Id)
                     .WithAddress(originalAddress.Id)
@@ -106,22 +105,17 @@
                 newDirectRegistrant.Should().NotBeNull();
                 newDirectRegistrant.Address.Id.Should().Be(originalDirectRegistrant.AddressId.Value);
                 newDirectRegistrant.Contact.Id.Should().Be(originalDirectRegistrant.ContactId.Value);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.PrimaryName.Should()
-                    .Be(representingCompanyDetails.Address.Address1);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.SecondaryName.Should()
-                    .BeNullOrWhiteSpace();
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.Street.Should()
-                    .Be(representingCompanyDetails.Address.Address2);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.Town.Should()
-                    .Be(representingCompanyDetails.Address.TownOrCity);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.Locality.Should()
-                    .BeNullOrWhiteSpace();
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.PostCode.Should()
-                    .Be(representingCompanyDetails.Address.Postcode);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.AdministrativeArea.Should()
-                    .Be(representingCompanyDetails.Address.CountyOrRegion);
-                newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address.CountryId.Should()
-                    .Be(representingCompanyDetails.Address.CountryId);
+
+                var overseasAddress = newDirectRegistrant.AuthorisedRepresentative.OverseasContact.Address;
+                overseasAddress.PrimaryName.Should().Be(representingCompanyDetails.Address.Address1);
+                overseasAddress.SecondaryName.Should().BeNullOrWhiteSpace();
+                overseasAddress.Street.Should().Be(representingCompanyDetails.Address.Address2);
+                overseasAddress.Town.Should().Be(representingCompanyDetails.Address.TownOrCity);
+                overseasAddress.Locality.Should().BeNullOrWhiteSpace();
+                overseasAddress.PostCode.Should().Be(representingCompanyDetails.Address.Postcode);
+                overseasAddress.AdministrativeArea.Should().Be(representingCompanyDetails.Address.CountyOrRegion);
+                overseasAddress.CountryId.Should().Be(representingCompanyDetails.Address.CountryId);
+
                 newDirectRegistrant.BrandName.Name.Should().Be("new brand name");
             };
         }
@@ -141,7 +135,7 @@
 
             private readonly Because of = () =>
             {
-                CatchExceptionAsync(() => authHandler.HandleAsync(new AddRepresentingCompany(organisation.Id, 
+                CatchExceptionAsync(() => authHandler.HandleAsync(new AddRepresentingCompany(organisation.Id,
                     fixture.Build<RepresentingCompanyDetailsViewModel>().Create())));
             };
 
