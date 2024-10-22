de0ee6ea7cd839b72d9632e26763c374e50e8f31
John Padgett <John.Padgett@civica.co.uk>
2024-10-22 16:18:23 +0100

    Start on return handler

---
+++ src/EA.Weee.Requests/Admin/DirectRegistrants/ReturnSmallProducerSubmission.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Weee.Requests.Admin.DirectRegistrants
+{
+    using CuttingEdge.Conditions;
+    using EA.Prsd.Core.Mediator;
+    using EA.Weee.Core.DirectRegistrant;
+    using System;
+
+    public class ReturnSmallProducerSubmission : IRequest<Guid>
+    {
+        public Guid DirectProducerSubmissionId { get; private set; }
+        public ReturnSmallProducerSubmission(Guid directProducerSubmissionId)
+        {
+            DirectProducerSubmissionId = directProducerSubmissionId;
+        }
+    }
+}
