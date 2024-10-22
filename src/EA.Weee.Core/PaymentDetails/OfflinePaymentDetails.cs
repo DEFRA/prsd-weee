de8fd9db05da59bbd9ef3fe860e3dff5c0b0cbfb
Sean Mccallum <Sean.Mccallum@civica.co.uk>
2024-10-22 14:31:50 +0100

    WIP

---
+++ src/EA.Weee.Core/PaymentDetails/OfflinePaymentDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EA.Weee.Core.PaymentDetails
+{
+    public class OfflinePaymentDetails
+    {
+        public string PaymentMethod { get; set; }
+
+        public DateTime PaymentRecievedDate { get; set; }
+
+        public string PaymentDetailsDescription { get; set; }
+
+     
+        public bool ConfirmPaymentMade { get; set; }
+    }
+}
---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Producers/PaymentDetailsViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Producers
+{
+    using System;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    public class PaymentDetailsViewModel
+    {
+        public string RegistrationNumber { get; set; }
+
+        [Required]
+        [DisplayName("Payment method")]
+        public string PaymentMethod { get; set; }
+
+        [Required]
+        [DisplayName("Payment recieved date")]
+        public UKGOVDate PaymentRecievedDate { get; set; }
+
+        [DisplayName("Payment details")]
+        public string PaymentDetailsDescription { get; set; }
+
+        [DisplayName("Confirm this payment has been made")]
+        public bool ConfirmPaymentMade { get; set; }
+    }
+}
