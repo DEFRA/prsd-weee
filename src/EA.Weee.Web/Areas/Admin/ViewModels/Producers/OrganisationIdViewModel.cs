09038d28c0a3577d3bca7d3ef8917d9ca8408b15
Sean Mccallum <Sean.Mccallum@civica.co.uk>
2024-10-25 17:41:49 +0100

    77609 - TO4.0 - Remove producer registration - completing the process WIP

---
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/Producers/OrganisationIdViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.Producers
+{
+    using System;
+
+    public class OrganisationIdViewModel
+    {
+        public Guid OrganisationId { get; set; }
+    }
+}
