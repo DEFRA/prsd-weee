75ad25ec94a47b68fa6e56655f9500415d17b0a1
Sean Mccallum <Sean.Mccallum@civica.co.uk>
2024-10-30 14:26:00 +0000

    77721 - TO4.0 - Organisation has no producer registrations

---
+++ src/EA.Weee.Web/Areas/Admin/Filters/AdminSmallProducerSubmissionSubmittedContextAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace EA.Weee.Web.Areas.Admin.Filters
+{
+    using EA.Weee.Api.Client;
+    using EA.Weee.Core.Helpers;
+    using System;
+    using System.Web.Mvc;
+
+    public class AdminSmallProducerSubmissionSubmittedContextAttribute : ActionFilterAttribute
+    {
+        public Func<IWeeeClient> Client { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = context.Controller as EA.Weee.Web.Areas.Admin.Controllers.ProducerSubmissionController;
+
+            var data = controller.SmallProducerSubmissionData;
+
+            controller.SmallProducerSubmissionData = data;
+
+            if (!data.AnySubmissions)
+            {
+                context.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary
+                {
+                    { "action", nameof(Areas.Admin.Controllers.ProducerSubmissionController.OrganisationHasNoSubmissions) },
+                    { "controller", typeof(Areas.Admin.Controllers.ProducerSubmissionController).GetControllerName() },
+                    { "organisationId", data.OrganisationData.Id }
+                });
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
