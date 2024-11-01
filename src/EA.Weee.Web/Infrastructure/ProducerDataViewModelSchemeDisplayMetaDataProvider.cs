cd419c2a16c79f4599a1b22e6b64a28c641b2a67
John Padgett <John.Padgett@civica.co.uk>
2024-11-01 09:11:08 +0000

    WIP

---
+++ src/EA.Weee.Web/Infrastructure/ProducerDataViewModelSchemeDisplayMetaDataProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace EA.Weee.Web.Infrastructure
+{
+    using EA.Weee.Web.Areas.Admin.ViewModels.SchemeReports;
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    
+    public class ProducerDataViewModelSchemeDisplayMetaDataProvider : DataAnnotationsModelMetadataProvider
+    {
+        protected override ModelMetadata CreateMetadata(
+            IEnumerable<Attribute> attributes,
+            Type containerType,
+            Func<object> modelAccessor,
+            Type modelType,
+            string propertyName)
+        {
+            var metadata = base.CreateMetadata(attributes, containerType, modelAccessor, modelType, propertyName);
+
+            if (containerType == typeof(ProducersDataViewModel) && propertyName == nameof(ProducersDataViewModel.SelectedSchemeId))
+            {
+                // Get the model instance
+                if (modelAccessor?.Invoke() is ProducersDataViewModel model)
+                {
+                    metadata.DisplayName = model.GetSchemeDisplayName();
+                }
+            }
+
+            return metadata;
+        }
+    }
+}
