ac32c196193bb9abdef247da866310a4d11cd015
John Padgett <John.Padgett@civica.co.uk>
2024-11-01 13:39:21 +0000

    Fix issue with PCS name label description

--- src/EA.Weee.Web/Areas/Admin/ViewModels/SchemeReports/ProducersDataViewModel.cs
+++ src/EA.Weee.Web/Areas/Admin/ViewModels/SchemeReports/ProducersDataViewModel.cs
@@ -1,48 +1,11 @@
 ﻿namespace EA.Weee.Web.Areas.Admin.ViewModels.SchemeReports
 {
     using System;
-    using System.Collections.Generic;
     using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
-    using System.Web.Mvc;
-    using Core.Shared;
 
-    public class ProducersDataViewModel
+    public class ProducersDataViewModel : ProducersDataViewModelBase
     {
-        private readonly string selectedSchemeDisplay;
-
-        public ProducersDataViewModel(string selectedSchemeDisplay = "PCS name or direct registrants")
-        {
-            this.selectedSchemeDisplay = selectedSchemeDisplay;
-        }
-
-        [Required(ErrorMessage = "Select a compliance year")]
-        [DisplayName("Compliance year")]
-        public int SelectedYear { get; set; }
-
-        public IEnumerable<SelectListItem> ComplianceYears { get; set; }
-
-        //[DisplayName("PCS name or direct registrants")]
-        public Guid? SelectedSchemeId { get; set; }
-
-        public string GetSchemeDisplayName()
-        {
-            return selectedSchemeDisplay;
-        }
-
-        public IEnumerable<SelectListItem> Schemes { get; set; }
-
-        [Required(ErrorMessage = "Select an obligation type")]
-        [Display(Name = "Obligation type")]
-        public ObligationType SelectedObligationType { get; set; }
-
-        public IEnumerable<SelectListItem> ObligationTypes
-        {
-            get
-            {
-                yield return new SelectListItem() { Text = "B2B" };
-                yield return new SelectListItem() { Text = "B2C" };
-            }
-        }
+        [DisplayName("PCS name")]
+        public new Guid? SelectedSchemeId { get; set; }
     }
 }
