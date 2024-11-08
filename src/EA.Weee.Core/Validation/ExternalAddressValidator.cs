b272cfa4a76a93aa2059753cb245889f3e3afc2b
John Padgett <John.Padgett@civica.co.uk>
2024-11-08 12:32:28 +0000

    Make postcode lookup a bit more flexible

--- src/EA.Weee.Core/Validation/ExternalAddressValidator.cs
+++ src/EA.Weee.Core/Validation/ExternalAddressValidator.cs
@@ -1,7 +1,6 @@
 ﻿namespace EA.Weee.Core.Validation
 {
     using EA.Weee.Core.Constants;
-    using EA.Weee.Core.Organisations;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
@@ -12,6 +11,15 @@
         public static readonly Regex UkPostcodeRegex = new Regex(
             @"^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})$",
             RegexOptions.Compiled);
+
+        private const string Area = @"[A-Za-z]{1,2}";  // One or two letters for the area
+        private const string DistrictDigit = @"[0-9]";  // First number
+        private const string OptionalSecondDigit = @"[0-9]?";  // Optional second number
+        private const string OptionalDistrictLetter = @"[A-Za-z]?";  // Optional letter
+        private const string OptionalSpace = @"\s?";  // Optional space
+        private const string OptionalInward = @"([0-9][A-Za-z]{2})?";  // Optional inward code
+
+        public static readonly Regex UkPartialPostcodeRegex = new Regex(@"^[A-Za-z]{1,2}[0-9]+.*$", RegexOptions.Compiled);
 
         public static IEnumerable<ValidationResult> Validate(Guid countryId, string postCode, string countryIdName, string postcodeName)
         {
@@ -29,5 +37,8 @@
                 }
             }
         }
+
+        public static bool IsValidPartialPostcode(string postcode) =>
+            !string.IsNullOrWhiteSpace(postcode) && UkPartialPostcodeRegex.IsMatch(postcode);
     }
 }
