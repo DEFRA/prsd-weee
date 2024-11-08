085732d3b1d29a5839501ec94892f1cda5c9324c
Sean Mccallum <Sean.Mccallum@civica.co.uk>
2024-11-08 17:09:33 +0000

    77418 - UJ1: Organisation details - validation

---
+++ src/EA.Weee.Core/Validation/RequiredWhenUKAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Weee.Core.Validation
+{
+    using EA.Weee.Core.Constants;
+    using EA.Weee.Core.Organisations;
+    using EA.Weee.Core.Organisations.Base;
+    using System.ComponentModel.DataAnnotations;
+
+    public class RequiredWhenUKAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = (OrganisationViewModel)validationContext.ObjectInstance;
+
+            if (model.OrganisationType == ExternalOrganisationType.RegisteredCompany && UkCountry.ValidIds.Contains(model.Address.CountryId))
+            {
+                var val = value as string;
+                return string.IsNullOrWhiteSpace(val)
+                    ? new ValidationResult($"Company registration number is required.")
+                    : ValidationResult.Success;
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
