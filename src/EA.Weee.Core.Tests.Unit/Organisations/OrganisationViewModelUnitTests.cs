ee26cffddd5d97ac9473126493dbebb96b947e42
John Padgett <John.Padgett@civica.co.uk>
2024-11-14 10:14:04 +0000

    Tweaks to organisation validation

--- src/EA.Weee.Core.Tests.Unit/Organisations/OrganisationViewModelUnitTests.cs
+++ src/EA.Weee.Core.Tests.Unit/Organisations/OrganisationViewModelUnitTests.cs
@@ -179,6 +179,7 @@
             validationResults.Should().BeEmpty();
         }
 
+        [Fact]
         public void EEEBrandNames_HasRequiredAttribute()
         {
             var t = typeof(OrganisationViewModel);
