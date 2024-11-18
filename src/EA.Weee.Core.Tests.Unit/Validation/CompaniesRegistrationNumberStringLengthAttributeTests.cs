efe8ea18d04f2c3062eebe7a7cf89c94a2ae2239
John Padgett <John.Padgett@civica.co.uk>
2024-11-18 09:05:56 +0000

    WIP

--- src/EA.Weee.Core.Tests.Unit/Validation/CompaniesRegistrationNumberStringLengthAttributeTests.cs
+++ src/EA.Weee.Core.Tests.Unit/Validation/CompaniesRegistrationNumberStringLengthAttributeTests.cs
@@ -10,7 +10,6 @@
 
     public class CompaniesRegistrationNumberStringLengthAttributeTests
     {
-        private readonly List<ValidationResult> validationResults = new List<ValidationResult>();
         private readonly CompaniesRegistrationNumberStringLengthAttribute attribute = new CompaniesRegistrationNumberStringLengthAttribute();
 
         [Theory]
@@ -22,7 +21,7 @@
         [InlineData(2, "456", "184E1785-26B4-4AE4-80D3-AE319B103ACB", "The company registration number should be 7 to 15 characters long")]
         [InlineData(3, "456", "184E1785-26B4-4AE4-80D3-AE319B103ACB", "The company registration number should be 7 to 15 characters long")]
         [InlineData(3, "456", "", "The company registration number should be 7 to 15 characters long")]
-        public void Validate_InvalidLength_ShouldReturnError(
+        public void Validate_InvalidLength_NonEditMode_ShouldReturnError(
             int orgType,
             string companiesRegistrationNumber,
             string countryId,
@@ -36,7 +35,8 @@
                 Address = new Core.Organisations.ExternalAddressData
                 {
                     CountryId = countryId == string.Empty ? Guid.NewGuid() : Guid.Parse(countryId)
-                }
+                },
+                Action = "Create" // Explicitly set non-edit mode
             };
 
             // Act
@@ -44,6 +44,35 @@
 
             // Assert
             result.ErrorMessage.Should().Be(errorMessage);
+        }
+
+        [Theory]
+        [InlineData(1, "456789", "184E1785-26B4-4AE4-80D3-AE319B103ACB")]
+        [InlineData(1, "7897898799", "DB83F5AB-E745-49CF-B2CA-23FE391B67A8")]
+        [InlineData(1, "123", "4209EE95-0882-42F2-9A5D-355B4D89EF30")]
+        [InlineData(2, "12345678901234567890", "184E1785-26B4-4AE4-80D3-AE319B103ACB")]
+        public void Validate_InvalidLength_EditMode_ShouldPass(
+            int orgType,
+            string companiesRegistrationNumber,
+            string countryId)
+        {
+            // Arrange
+            var viewModel = new OrganisationViewModel
+            {
+                CompaniesRegistrationNumber = companiesRegistrationNumber,
+                OrganisationType = (Core.Organisations.ExternalOrganisationType)orgType,
+                Address = new Core.Organisations.ExternalAddressData
+                {
+                    CountryId = Guid.Parse(countryId)
+                },
+                Action = "Edit" // Set edit mode
+            };
+
+            // Act
+            var result = attribute.GetValidationResult(viewModel.CompaniesRegistrationNumber, new ValidationContext(viewModel));
+
+            // Assert
+            result.Should().BeNull();
         }
 
         [Theory]
@@ -55,7 +84,7 @@
         [InlineData(2, "4567894", "184E1785-26B4-4AE4-80D3-AE319B103ACB")]
         [InlineData(3, "12345678", "184E1785-26B4-4AE4-80D3-AE319B103ACB")]
         [InlineData(3, "4567894", "")]
-        public void Validate_ValidLength_ShouldPass(
+        public void Validate_ValidLength_NonEditMode_ShouldPass(
            int orgType,
            string companiesRegistrationNumber,
            string countryId)
@@ -68,7 +97,8 @@
                 Address = new Core.Organisations.ExternalAddressData
                 {
                     CountryId = countryId == string.Empty ? Guid.NewGuid() : Guid.Parse(countryId)
-                }
+                },
+                Action = "Create" // Explicitly set non-edit mode
             };
 
             // Act
@@ -95,7 +125,8 @@
                 Address = new Core.Organisations.ExternalAddressData
                 {
                     CountryId = Guid.NewGuid()
-                }
+                },
+                Action = "Create" // Even in non-edit mode, these should pass
             };
 
             // Act
@@ -103,6 +134,29 @@
 
             // Assert
             result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Validate_NullAction_ShouldValidate()
+        {
+            // Arrange
+            var viewModel = new OrganisationViewModel
+            {
+                CompaniesRegistrationNumber = "123456", // Invalid length
+                OrganisationType = Core.Organisations.ExternalOrganisationType.RegisteredCompany,
+                Address = new Core.Organisations.ExternalAddressData
+                {
+                    CountryId = Guid.Parse("184E1785-26B4-4AE4-80D3-AE319B103ACB")
+                },
+                Action = null // Null action should be treated as non-edit mode
+            };
+
+            // Act
+            var result = attribute.GetValidationResult(viewModel.CompaniesRegistrationNumber, new ValidationContext(viewModel));
+
+            // Assert
+            result.Should().NotBeNull();
+            result.ErrorMessage.Should().Be("The company registration number should be 7 to 8 characters long");
         }
     }
 }
