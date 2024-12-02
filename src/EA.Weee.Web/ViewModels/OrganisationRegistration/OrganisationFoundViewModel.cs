9d89f1cc183173c709e87330c59ecadd398523c5
John Padgett <John.Padgett@civica.co.uk>
2024-12-02 11:46:02 +0000

    Changes to allow us to determine if an organisation has been migrated from npwd

--- src/EA.Weee.Web/ViewModels/OrganisationRegistration/OrganisationFoundViewModel.cs
+++ src/EA.Weee.Web/ViewModels/OrganisationRegistration/OrganisationFoundViewModel.cs
@@ -1,16 +1,18 @@
 ﻿namespace EA.Weee.Web.ViewModels.OrganisationRegistration
 {
-    using EA.Weee.Core.Organisations;
-    using Shared;
     using System;
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
 
     [Serializable]
     public class OrganisationFoundViewModel
     {
         public string OrganisationName { get; set; }
+
         public string CompanyRegistrationNumber { get; set; }
+
         public Guid OrganisationId { get; set; }
+
+        public bool NpwdMigrated { get; set; }
+
+        public bool NpwdMigratedComplete { get; set; }
     }
 }
