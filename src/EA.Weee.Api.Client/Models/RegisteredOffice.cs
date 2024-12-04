c18b1dc6ab5b77eb832201ffd5dfefa13f4a696c
John Padgett <John.Padgett@civica.co.uk>
2024-12-04 16:05:33 +0000

    Further tweak for companies house lookup

--- src/EA.Weee.Api.Client/Models/RegisteredOffice.cs
+++ src/EA.Weee.Api.Client/Models/RegisteredOffice.cs
@@ -4,6 +4,7 @@
     {
         public string BuildingName { get; set; }
         public string BuildingNumber { get; set; }
+        public string SubBuildingName { get; set; }
         public string Street { get; set; }
         public string Locality { get; set; }
         public string Town { get; set; }
