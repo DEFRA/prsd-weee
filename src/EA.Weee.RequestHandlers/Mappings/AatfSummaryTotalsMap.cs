eb8d8122ed0eb5754d8cc462e7395cd626a49bc8
Jonathan Stratford <jonathan.stratford@civica.co.uk>
2025-01-27 13:25:32 +0000

    [VapesAATFExternalEvidenceSummary] Extract maximum category id out to a property

--- src/EA.Weee.RequestHandlers/Mappings/AatfSummaryTotalsMap.cs
+++ src/EA.Weee.RequestHandlers/Mappings/AatfSummaryTotalsMap.cs
@@ -11,11 +11,13 @@
 
     internal class AatfSummaryTotalsMap : IMap<List<AatfEvidenceSummaryTotalsData>, List<EvidenceSummaryTonnageData>>
     {
+        private int MaxCategoryId => 14;
+
         public List<EvidenceSummaryTonnageData> Map(List<AatfEvidenceSummaryTotalsData> source)
         {
             Condition.Requires(source).IsNotNull();
 
-            return source.Where(s => s.CategoryId.ToInt() <= 14).Select(e => new EvidenceSummaryTonnageData((WeeeCategory)e.CategoryId, e.ApprovedReceived, e.ApprovedReused)).ToList();
+            return source.Where(s => s.CategoryId.ToInt() <= this.MaxCategoryId).Select(e => new EvidenceSummaryTonnageData((WeeeCategory)e.CategoryId, e.ApprovedReceived, e.ApprovedReused)).ToList();
         }
     }
 }
