72044e44156050154ba58bfb9352741a09591f4f
Jonathan Stratford <jonathan.stratford@civica.co.uk>
2025-02-06 18:50:08 +0000

    [VapesPCSObligationUploadTemplate] Add category 15 (Vapes) column to obligations CSV reader

--- src/EA.Weee.RequestHandlers/Admin/Obligations/ObligationCsvReader.cs
+++ src/EA.Weee.RequestHandlers/Admin/Obligations/ObligationCsvReader.cs
@@ -8,13 +8,13 @@
     public class ObligationCsvReader : IObligationCsvReader
     {
         private readonly IFileHelper fileHelper;
-        private const int NumberOfColumns = 16;
+        private const int NumberOfColumns = 17;
 
         private readonly string[] columnOrder = new[]
         {
             "Scheme Identifier",
             "Scheme Name", "Cat1 (t)", "Cat2 (t)", "Cat3 (t)", "Cat4 (t)", "Cat5 (t)", "Cat6 (t)", "Cat7 (t)", "Cat8 (t)",
-            "Cat9 (t)", "Cat10 (t)", "Cat11 (t)", "Cat12 (t)", "Cat13 (t)", "Cat14 (t)"
+            "Cat9 (t)", "Cat10 (t)", "Cat11 (t)", "Cat12 (t)", "Cat13 (t)", "Cat14 (t)", "Cat15 (t)"
         };
 
         public ObligationCsvReader(IFileHelper fileHelper)
