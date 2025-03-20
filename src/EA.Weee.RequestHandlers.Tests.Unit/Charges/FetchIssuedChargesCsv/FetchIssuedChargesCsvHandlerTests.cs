f6d4863eb79a72ced823e30dbe0a0261ea2259a9
Jonathan Stratford <jonathan.stratford@civica.co.uk>
2025-03-20 16:53:47 +0000

    [authority-issued=charge-changes] Improve existing test to verify the CSV column headers

--- src/EA.Weee.RequestHandlers.Tests.Unit/Charges/FetchIssuedChargesCsv/FetchIssuedChargesCsvHandlerTests.cs
+++ src/EA.Weee.RequestHandlers.Tests.Unit/Charges/FetchIssuedChargesCsv/FetchIssuedChargesCsvHandlerTests.cs
@@ -1,6 +1,8 @@
 ﻿namespace EA.Weee.RequestHandlers.Tests.Unit.Charges.FetchIssuedChargesCsv
 {
+    using System;
     using System.Security;
+    using System.Text;
     using System.Threading.Tasks;
     using Core.Shared;
     using FakeItEasy;
@@ -36,6 +38,28 @@
             var data = await handler.HandleAsync(A.Dummy<FetchIssuedChargesCsv>());
 
             Assert.NotEmpty(data.Data);
+
+            var result = Encoding.UTF8.GetString(data.Data);
+
+            var lines = result.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var expectedHeaders = string.Join(",", new[]
+            {
+            "Scheme name",
+            "Compliance year",
+            "Submission date and time (GMT)",
+            "Producer name",
+            "PRN",
+            "Charge value (GBP)",
+            "Charge band",
+            "Selling technique",
+            "Online market places charge value",
+            "Issued date",
+            "Reg. Off. or PPoB country",
+            "Includes annual charge"
+            });
+
+            Assert.Equal(expectedHeaders, lines[0]);
         }
     }
 }
