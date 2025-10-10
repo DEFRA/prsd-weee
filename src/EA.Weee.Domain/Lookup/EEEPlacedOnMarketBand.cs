44dcc1addba8befe03d3af30e53bd061625b6ac8
Ian Alvarado <ian.alvarado@civica.com>
2025-10-10 16:27:07 +0100

    PR changes required

--- src/EA.Weee.Domain/Lookup/EEEPlacedOnMarketBand.cs
+++ src/EA.Weee.Domain/Lookup/EEEPlacedOnMarketBand.cs
@@ -1,8 +1,13 @@
 ﻿namespace EA.Weee.Domain.Lookup
 {
+    using System.ComponentModel.DataAnnotations;
+
     public enum EEEPlacedOnMarketBand
     {
-        Morethanorequalto5TEEEplacedonmarket,
-        Lessthan5TEEEplacedonmarket,
+        [Display(Name = "More than or equal to 5 tonnes EEE placed on market")]
+        Morethanorequalto5TEEEplacedonmarket = 0,
+
+        [Display(Name = "Less than 5 tonnes EEE placed on market")]
+        Lessthan5TEEEplacedonmarket = 1,
     }
 }
