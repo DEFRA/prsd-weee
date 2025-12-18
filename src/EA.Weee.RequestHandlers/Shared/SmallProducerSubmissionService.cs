ad251ff35538927c2faf73bff80d6521b4fa74ab
SFWVDI13\SFW_SB <sreedhar.bangarugari@civica.co.uk>
2025-12-18 09:34:26 +0000

    2026 Direct Registrant Charge testing locally for 2026 change

--- src/EA.Weee.RequestHandlers/Shared/SmallProducerSubmissionService.cs
+++ src/EA.Weee.RequestHandlers/Shared/SmallProducerSubmissionService.cs
@@ -71,12 +71,12 @@
                     submissionData.CurrentSubmission.BusinessAddressData.CountryName.Equals("UK - Scotland") ||
                     submissionData.CurrentSubmission.BusinessAddressData.CountryName.Equals("UK - Wales"))
                 {
-                    var directRegistrantCharge = await smallProducerDataAccess.GetDirectRegistrantChargeByComplianceYear(SystemTime.UtcNow.Year, false);
+                    var directRegistrantCharge = await smallProducerDataAccess.GetDirectRegistrantChargeByComplianceYear(SystemTime.UtcNow.Year + 1, false);
                     submissionData.DirectRegistrantChargeAmount = directRegistrantCharge.ChargeAmount;
                 }
                 else
                 {
-                    var directRegistrantCharge = await smallProducerDataAccess.GetDirectRegistrantChargeByComplianceYear(SystemTime.UtcNow.Year, true);
+                    var directRegistrantCharge = await smallProducerDataAccess.GetDirectRegistrantChargeByComplianceYear(SystemTime.UtcNow.Year + 1, true);
                     submissionData.DirectRegistrantChargeAmount = directRegistrantCharge.ChargeAmount;
                 }
             }
