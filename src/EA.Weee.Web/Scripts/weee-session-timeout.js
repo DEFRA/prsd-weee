95ac1f5f38061ccfbd388c4c8c86f8d3ee43dc8d
SFWVDI13\SFW_SB <sreedhar.bangarugari@civica.co.uk>
2026-01-14 15:31:27 +0000

    session timeout script changes.

--- src/EA.Weee.Web/Scripts/weee-session-timeout.js
+++ src/EA.Weee.Web/Scripts/weee-session-timeout.js
@@ -1,93 +1,79 @@
 ﻿(function () {
     "use strict";
 
+    const $sessionDialog = $("#prsd-timeout-dialog, .prsd-timeout-overlay");
     let defaultTimeOutInMinutes;
     let warningTimeInMinutes;
-
     let sessionTimeoutInSeconds;
     let warningTimeInSeconds;
-
     let sessionWarningTimer;
     let sessionLogoutTimer;
     let countdownTimer;
-
     let urlPrefix = "";
     let eventBound = false;
 
-    const $sessionDialog = $("#prsd-timeout-dialog, .prsd-timeout-overlay");
+    // Generate URL based on the environment and URL prefix
+    function getUrl(endpoint) {
+        const baseUrl = location.protocol + '//' + location.host;
+        const prefix = urlPrefix || '';
+        const path = location.pathname.split('/')[1];
+
+        return location.host.includes('uat')
+            ? `${baseUrl}/${path}/${prefix}${endpoint}`
+            : `${baseUrl}/${prefix}${endpoint}`;
+    }
 
     function startSessionTimeout() {
-
         clearTimeout(sessionWarningTimer);
         clearTimeout(sessionLogoutTimer);
 
-        sessionWarningTimer = setTimeout(() => showSessionWarning(), (sessionTimeoutInSeconds - warningTimeInSeconds) * 1000);
-        sessionLogoutTimer = setTimeout(() => logout(), sessionTimeoutInSeconds * 1000);
+        sessionWarningTimer = setTimeout(showSessionWarning, (sessionTimeoutInSeconds - warningTimeInSeconds) * 1000);
+        sessionLogoutTimer = setTimeout(logout, sessionTimeoutInSeconds * 1000);
     }
 
     function showSessionWarning() {
-
-        clearTimeout(countdownTimer); // Stop any previous countdown
+        clearTimeout(countdownTimer);
         setTimeValue(warningTimeInSeconds - 1);
         $sessionDialog.show();
     }
 
     function setTimeValue(val) {
+        $("#prsd-timeout-countdown").html(formatTime(val));
 
-        let setVal = () => $("#prsd-timeout-countdown").html(formatTime(val));
-        setVal();
+        if (val < 1) return;
 
-        if (val < 1)
-            return;
-
-        countdownTimer = setTimeout(() => {
-            setTimeValue(--val);
-            setVal();
-        }, 1000);
+        countdownTimer = setTimeout(() => setTimeValue(--val), 1000);
     }
 
     function closeSessionWarning() {
-
-        clearTimeout(countdownTimer); // Stop countdown when modal is closed
+        clearTimeout(countdownTimer);
         $sessionDialog.hide();
     }
 
     async function post(url) {
+        const token = $('input[name="__RequestVerificationToken"]').val();
+        const formData = new URLSearchParams();
+        formData.append("__RequestVerificationToken", token);
 
-        let tokenName = "__RequestVerificationToken";
-        let token = $('input[name=' + tokenName + ']').val();
-
-        let formData = new URLSearchParams();
-        formData.append(tokenName, token)
-
-        await $.post({
-            url: url,
-            data: formData.toString(),
-            contentType: 'application/x-www-form-urlencoded;charset=UTF-8'
-        });
+        try {
+            await $.post({
+                url,
+                data: formData.toString(),
+                contentType: 'application/x-www-form-urlencoded;charset=UTF-8'
+            });
+        } catch (error) {
+            console.error('Error posting data', error);
+        }
     }
 
     async function logout() {
-
-        let signOutUrl = null;
-        let logOffUrl = null;
-
-        if (location.host.includes('uat')) {
-            logOffUrl = location.protocol + '//' + location.host + '/' + location.pathname.split('/')[1] + '/Account/SignOut';
-            signOutUrl = location.protocol + '//' + location.host + '/' + location.pathname.split('/')[1] + '/Account/SessionSignedOut';
-        }
-        else {
-            logOffUrl = location.protocol + '//' + location.host + '/Account/SignOut';
-            //logOffUrl = '/Account/SignOut';
-            signOutUrl = location.protocol + '//' + location.host + '/Account/SessionSignedOut';
-        }
-
+        const logOffUrl = getUrl('/Account/SignOut');
+        const signOutUrl = getUrl('/Account/SessionSignedOut');
         await post(logOffUrl);
         document.location.href = signOutUrl;
     }
 
     function setTimeWith(ltTimeoutInMinutes, lWarningTimeInMinutes) {
-
         defaultTimeOutInMinutes = ltTimeoutInMinutes;
         warningTimeInMinutes = lWarningTimeInMinutes;
 
@@ -96,54 +82,24 @@
     }
 
     function formatTime(seconds) {
-
-        let mins = Math.floor(seconds / 60);
-        let secs = seconds % 60;
-        let minutesResult = mins > 0 ? mins + ' minute(s) and ' + secs + ' seconds' : + secs + ' seconds';
-
-        return minutesResult;
+        const mins = Math.floor(seconds / 60);
+        const secs = seconds % 60;
+        return mins > 0 ? `${mins} minute(s) and ${secs} second(s)` : `${secs} second(s)`;
     }
 
     function start(timeoutInMinutes, warningBeforeInMinutes, authenticated, isInternal) {
+        if (authenticated === "False") return;
 
-        if (authenticated == "False")
-            return;
-
-        isInternal = isInternal == "True";
-
+        isInternal = isInternal === "True";
         setTimeWith(timeoutInMinutes, warningBeforeInMinutes);
         startSessionTimeout();
 
-        if (isInternal) {
-            urlPrefix = "/Admin"
-            $("#signoutForm").hide();
-            $("#signoutFormAdmin").show();
-        }
-        else {
-            $("#signoutForm").show();
-            $("#signoutFormAdmin").hide();
-        }
+        $("#signoutForm").toggle(!isInternal);
+        $("#signoutFormAdmin").toggle(isInternal);
 
         if (!eventBound) {
             $("#prsd-timeout-keep-signin-btn").click(async () => {
-                let extendSessionUrl = null;
-                if (location.host.includes('uat')) {
-                    if (urlPrefix == null) {
-                        extendSessionUrl = location.protocol + '//' + location.host + '/' + location.pathname.split('/')[1] + '/Account/ExtendSession';
-                    }
-                    else {
-                        extendSessionUrl = location.protocol + '//' + location.host + '/' + location.pathname.split('/')[1] + '/' + urlPrefix + '/Account/ExtendSession';
-                    }
-                }
-                else {
-                    if (urlPrefix == null) {
-                        extendSessionUrl = location.protocol + '//' + location.host + '/Account/ExtendSession';
-                    }
-                    else {
-                        extendSessionUrl = location.protocol + '//' + location.host + '/' + urlPrefix + '/Account/ExtendSession';
-                    }
-
-                }
+                const extendSessionUrl = getUrl('/Account/ExtendSession');
                 await post(extendSessionUrl);
 
                 clearTimeout(sessionWarningTimer);
@@ -154,6 +110,8 @@
                 startSessionTimeout();
                 closeSessionWarning();
             });
+
+            eventBound = true;
         }
     }
 
