94bbc466949a2789166935763e620783b656f9dd
SFWVDI13\SFW_SB <sreedhar.bangarugari@civica.co.uk>
2026-03-26 09:51:09 +0000

    Fixed build issue (Added [Lookup].[CompetentAuthority] table check)

--- src/EA.Weee.Database/scripts/Update/Release 8/Sprint6/20260309-1600-create-AnnualChargeByYear.sql
+++ src/EA.Weee.Database/scripts/Update/Release 8/Sprint6/20260309-1600-create-AnnualChargeByYear.sql
@@ -2,10 +2,11 @@
 	2027 Inflationary Charge Uplift - PCS Subsistence Fee
 	===================================================== */
 
-DECLARE @EAId UNIQUEIDENTIFIER = (SELECT Id FROM [Lookup].[CompetentAuthority] WHERE [Abbreviation] = 'EA');
-DECLARE @SEPAId UNIQUEIDENTIFIER = (SELECT Id FROM [Lookup].[CompetentAuthority] WHERE [Abbreviation] = 'SEPA');
-DECLARE @NIEAId UNIQUEIDENTIFIER = (SELECT Id FROM [Lookup].[CompetentAuthority] WHERE [Abbreviation]='NIEA');
-DECLARE @NRWId UNIQUEIDENTIFIER = (SELECT Id FROM [Lookup].[CompetentAuthority] WHERE [Abbreviation]='NRW');
+-- Use hardcoded GUIDs from 0004-InsertCompetentAuthorityData.sql
+DECLARE @EAId UNIQUEIDENTIFIER = 'A3C2D0DD-53A1-4F6A-99D0-1CCFC87611A8';
+DECLARE @SEPAId UNIQUEIDENTIFIER = '78F37814-364B-4FAE-BEB5-DB0439CBF177';
+DECLARE @NIEAId UNIQUEIDENTIFIER = '4EEE5942-01B2-4A4D-855A-34DEE1BBBF26';
+DECLARE @NRWId UNIQUEIDENTIFIER = '44C2F368-AA66-48F0-BBC9-A0ED34AD0951';
 
 DECLARE @Id1 UNIQUEIDENTIFIER = '8E5E9C81-6219-B50C-82CB-FF866969BC24';
 DECLARE @Id2 UNIQUEIDENTIFIER = '2A527D51-EFF7-4789-B985-CD43C64F03B3';
@@ -16,38 +17,50 @@ DECLARE @EffectiveFrom DATETIME = '2026-04-01';
 DECLARE @ComplianceYear INT = 2027;
 
 --EA
-IF NOT EXISTS (SELECT 1 FROM [Lookup].[AnnualChargeByYear] WHERE [ComplianceYear] = @ComplianceYear AND [EffectiveFrom] = @EffectiveFrom AND [CompetentAuthorityId] = @EAId)
+IF EXISTS (SELECT 1 FROM [Lookup].[CompetentAuthority] WHERE Id = @EAId)
 	BEGIN
-		INSERT INTO [Lookup].[AnnualChargeByYear]
-			([Id],[CompetentAuthorityId],[ComplianceYear],[AnnualChargeAmount],[EffectiveFrom])
-		VALUES
-			(@Id1, @EAId, @ComplianceYear, 13948.13, @EffectiveFrom)
+		IF NOT EXISTS (SELECT 1 FROM [Lookup].[AnnualChargeByYear] WHERE [ComplianceYear] = @ComplianceYear AND [EffectiveFrom] = @EffectiveFrom AND [CompetentAuthorityId] = @EAId)
+			BEGIN
+				INSERT INTO [Lookup].[AnnualChargeByYear]
+					([Id],[CompetentAuthorityId],[ComplianceYear],[AnnualChargeAmount],[EffectiveFrom])
+				VALUES
+					(@Id1, @EAId, @ComplianceYear, 13948.13, @EffectiveFrom)
+			END
 	END
 
 --SEPA
-IF NOT EXISTS (SELECT 1 FROM [Lookup].[AnnualChargeByYear] WHERE [ComplianceYear] = @ComplianceYear AND [EffectiveFrom] = @EffectiveFrom AND [CompetentAuthorityId] = @SEPAId)
+IF EXISTS (SELECT 1 FROM [Lookup].[CompetentAuthority] WHERE Id = @SEPAId)
 	BEGIN
-		INSERT INTO [Lookup].[AnnualChargeByYear]
-			([Id],[CompetentAuthorityId],[ComplianceYear],[AnnualChargeAmount],[EffectiveFrom])
-		VALUES
-			(@Id2, @SEPAId, @ComplianceYear, 0.00, @EffectiveFrom)
+		IF NOT EXISTS (SELECT 1 FROM [Lookup].[AnnualChargeByYear] WHERE [ComplianceYear] = @ComplianceYear AND [EffectiveFrom] = @EffectiveFrom AND [CompetentAuthorityId] = @SEPAId)
+			BEGIN
+				INSERT INTO [Lookup].[AnnualChargeByYear]
+					([Id],[CompetentAuthorityId],[ComplianceYear],[AnnualChargeAmount],[EffectiveFrom])
+				VALUES
+					(@Id2, @SEPAId, @ComplianceYear, 0.00, @EffectiveFrom)
+			END
 	END
 
 --NIEA
-IF NOT EXISTS (SELECT 1 FROM [Lookup].[AnnualChargeByYear] WHERE [ComplianceYear] = @ComplianceYear AND [EffectiveFrom] = @EffectiveFrom AND [CompetentAuthorityId] = @NIEAId)
+IF EXISTS (SELECT 1 FROM [Lookup].[CompetentAuthority] WHERE Id = @NIEAId)
 	BEGIN
-		INSERT INTO [Lookup].[AnnualChargeByYear]
-			([Id],[CompetentAuthorityId],[ComplianceYear],[AnnualChargeAmount],[EffectiveFrom])
-		VALUES
-			(@Id3, @NIEAId, @ComplianceYear, 0.00, @EffectiveFrom)
+		IF NOT EXISTS (SELECT 1 FROM [Lookup].[AnnualChargeByYear] WHERE [ComplianceYear] = @ComplianceYear AND [EffectiveFrom] = @EffectiveFrom AND [CompetentAuthorityId] = @NIEAId)
+			BEGIN
+				INSERT INTO [Lookup].[AnnualChargeByYear]
+					([Id],[CompetentAuthorityId],[ComplianceYear],[AnnualChargeAmount],[EffectiveFrom])
+				VALUES
+					(@Id3, @NIEAId, @ComplianceYear, 0.00, @EffectiveFrom)
+			END
 	END
 
 --NRW
-IF NOT EXISTS (SELECT 1 FROM [Lookup].[AnnualChargeByYear] WHERE [ComplianceYear] = @ComplianceYear AND [EffectiveFrom] = @EffectiveFrom AND [CompetentAuthorityId] = @NRWId)
+IF EXISTS (SELECT 1 FROM [Lookup].[CompetentAuthority] WHERE Id = @NRWId)
 	BEGIN
-		INSERT INTO [Lookup].[AnnualChargeByYear]
-			([Id],[CompetentAuthorityId],[ComplianceYear],[AnnualChargeAmount],[EffectiveFrom])
-		VALUES
-			(@Id4, @NRWId, @ComplianceYear, 0.00, @EffectiveFrom)
+		IF NOT EXISTS (SELECT 1 FROM [Lookup].[AnnualChargeByYear] WHERE [ComplianceYear] = @ComplianceYear AND [EffectiveFrom] = @EffectiveFrom AND [CompetentAuthorityId] = @NRWId)
+			BEGIN
+				INSERT INTO [Lookup].[AnnualChargeByYear]
+					([Id],[CompetentAuthorityId],[ComplianceYear],[AnnualChargeAmount],[EffectiveFrom])
+				VALUES
+					(@Id4, @NRWId, @ComplianceYear, 0.00, @EffectiveFrom)
+			END
 	END
 GO
