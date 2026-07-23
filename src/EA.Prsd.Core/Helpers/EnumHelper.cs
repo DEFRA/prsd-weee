d1871607d298f198fa1f939cf17a9beada1eeaa6
SFWVDI13\SFW_SB <sreedhar.bangarugari@civica.co.uk>
2026-07-23 09:43:15 +0100

    Added "Batteries in vapes" protocol dropdown to Evidence note functionality

--- src/EA.Prsd.Core/Helpers/EnumHelper.cs
+++ src/EA.Prsd.Core/Helpers/EnumHelper.cs
@@ -57,6 +57,15 @@
             return values.ToDictionary(v => v.Key, v => v.Value);
         }
 
+        public static Dictionary<int, string> GetOrderedNameByKey(Type enumType)
+        {
+            var values = GetValues(enumType).ToList();
+
+            values.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+            return values.ToDictionary(v => v.Key, v => v.Value);
+        }
+
         private static readonly ConcurrentDictionary<Type, EnumHelperMetaData> enumHelperMetaDataMap = new ConcurrentDictionary<Type, EnumHelperMetaData>();
 
         public static string GetDescription<TEnum>(TEnum value)
